760b8f77644dca10aa0d069881006b229f221d77
cbern <>
2010-11-16 18:25:55 +0000

    - ported recent developments done in the old cvs area.
    In a nutshell
    - The BaseMETFactory can now take a pt threshold parameter
    - MET sequence developed. However, it needs the particle flow PFCandidates in input
    - so PFCandidates added to the pat tuple
    - the test cfg now points to a new pat tuple in my public zone

--- CMGTools/Common/python/met_cff.py
+++ CMGTools/Common/python/met_cff.py
@@ -6,15 +6,43 @@ from CMGTools.Common.histograms.baseJetLorentzVector_cfi import baseJetLorentzVe
 
 # MHT from PFJets
 cmgMHTPFJets = cmgBaseMET.clone()
-cmgMHTPFJets.cfg.inputCollection = "selectedPatJetsPFlow" 
+cmgMHTPFJets.cfg.inputCollection = "cmgPFBaseJet" 
 mhtPFJetsLorentzVector = baseJetLorentzVector.clone()
 mhtPFJetsLorentzVector.inputCollection = 'cmgMHTPFJets'
 
+# MHT from PFJets, pt threshold 30
+cmgMHTPFJets30 = cmgMHTPFJets.clone()
+cmgMHTPFJets30.cfg.ptThreshold = 30.0
+mhtPFJets30LorentzVector = baseJetLorentzVector.clone()
+mhtPFJets30LorentzVector.inputCollection = 'cmgMHTPFJets30'
+
+# MHT from PFJets, pt threshold 50
+cmgMHTPFJets50 = cmgMHTPFJets.clone()
+cmgMHTPFJets50.cfg.ptThreshold = 50.0
+mhtPFJets50LorentzVector = baseJetLorentzVector.clone()
+mhtPFJets50LorentzVector.inputCollection = 'cmgMHTPFJets50'
+
+
+
 # MHT from CaloJets
 cmgMHTCaloJets = cmgBaseMET.clone()
-cmgMHTCaloJets.cfg.inputCollection = "selectedPatJets" 
+cmgMHTCaloJets.cfg.inputCollection = "cmgCaloBaseJet" 
 mhtCaloJetsLorentzVector = baseJetLorentzVector.clone()
 mhtCaloJetsLorentzVector.inputCollection = 'cmgMHTCaloJets'
+
+# MHT from CaloJets, pt threshold 30
+cmgMHTCaloJets30 = cmgMHTCaloJets.clone()
+cmgMHTCaloJets30.cfg.ptThreshold = 30.0
+mhtCaloJets30LorentzVector = baseJetLorentzVector.clone()
+mhtCaloJets30LorentzVector.inputCollection = 'cmgMHTCaloJets30'
+
+# MHT from CaloJets, pt threshold 50
+cmgMHTCaloJets50 = cmgMHTCaloJets.clone()
+cmgMHTCaloJets50.cfg.ptThreshold = 50.0
+mhtCaloJets50LorentzVector = baseJetLorentzVector.clone()
+mhtCaloJets50LorentzVector.inputCollection = 'cmgMHTCaloJets50'
+
+
 
 # MET from PFCandidates
 cmgMETPFCandidates = cmgBaseMET.clone()
@@ -22,15 +50,33 @@ cmgMETPFCandidates.cfg.inputCollection = "particleFlow"
 pfMETLorentzVector = baseJetLorentzVector.clone()
 pfMETLorentzVector.inputCollection = 'cmgMETPFCandidates'
 
+# MET from PFCandidates, pt threshold 2 (to remove pile-up)
+cmgMETPFCandidates2 = cmgMETPFCandidates.clone()
+cmgMETPFCandidates2.cfg.ptThreshold = 2.0
+pfMET2LorentzVector = baseJetLorentzVector.clone()
+pfMET2LorentzVector.inputCollection = 'cmgMETPFCandidates2'
+
+
+
 metHistograms = cms.Sequence(
     mhtPFJetsLorentzVector +
+    mhtPFJets30LorentzVector +
+    mhtPFJets50LorentzVector +
     mhtCaloJetsLorentzVector +
-    pfMETLorentzVector 
+    mhtCaloJets30LorentzVector +
+    mhtCaloJets50LorentzVector +
+    pfMETLorentzVector +
+    pfMET2LorentzVector
     )
 
 metSequence = cms.Sequence(
     cmgMHTPFJets +
+    cmgMHTPFJets30 +
+    cmgMHTPFJets50 +
     cmgMHTCaloJets +    
+    cmgMHTCaloJets30 +    
+    cmgMHTCaloJets50 +    
     cmgMETPFCandidates +
+    cmgMETPFCandidates2 +
     metHistograms
     )
