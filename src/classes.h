901be0b112e20d91c40ebadd7b4815ec95b0cbf0
wreece <>
2010-11-18 11:24:42 +0000

    Adds many further DiObjects and their factories, including Wmunu. Adds a few fixes to DiObjectFactory to support this.

--- src/classes.h
+++ src/classes.h
@@ -17,6 +17,11 @@
 namespace {
   struct CMGTools_AnalysisDataFormats {
 
+    cmg::DiElectron de_;
+    std::vector<cmg::DiElectron> dev;
+    edm::Wrapper<cmg::DiElectron> ede;
+    edm::Wrapper<std::vector<cmg::DiElectron> > edev;
+
     cmg::DiMuon d_;
     std::vector<cmg::DiMuon> dv;
     edm::Wrapper<cmg::DiMuon> ed;
@@ -26,6 +31,16 @@ namespace {
     std::vector<cmg::DiJet> djv;
     edm::Wrapper<cmg::DiJet> edj;
     edm::Wrapper<std::vector<cmg::DiJet> > edjv;
+    
+    cmg::DiPFJet dpfj_;
+    std::vector<cmg::DiPFJet> dpfjv;
+    edm::Wrapper<cmg::DiPFJet> epfdj;
+    edm::Wrapper<std::vector<cmg::DiPFJet> > epfdjv;
+    
+    cmg::DiTau dt_;
+    std::vector<cmg::DiTau> dtv;
+    edm::Wrapper<cmg::DiTau> edt;
+    edm::Wrapper<std::vector<cmg::DiTau> > edtv;
 
     cmg::Electron e_;
     std::vector<cmg::Electron> ev;
@@ -55,6 +70,11 @@ namespace {
     cmg::PFJetComponent  jetComp_;
     std::vector< cmg::PFJetComponent > vJetComps_;
     
+    cmg::QuadMuon qm_;
+    std::vector<cmg::QuadMuon> qmv;
+    edm::Wrapper<cmg::QuadMuon> eqm;
+    edm::Wrapper<std::vector<cmg::QuadMuon> > eqmv;
+    
     cmg::BaseMET met_;
     std::vector<cmg::BaseMET> metv_;
     edm::Wrapper<cmg::BaseMET> wmet_;
@@ -68,6 +88,22 @@ namespace {
     std::vector<cmg::PFCand> pfcandv_;
     edm::Wrapper<cmg::PFCand> wpfcand_;
     edm::Wrapper<std::vector<cmg::PFCand> > wpfcandv_;
+    
+    cmg::WENu wen;
+    std::vector<cmg::WENu> wenv_;
+    edm::Wrapper<cmg::WENu> wwen_;
+    edm::Wrapper<std::vector<cmg::WENu> > wwenv_;
+    
+    cmg::WMuNu wmun;
+    std::vector<cmg::WMuNu> wmunv_;
+    edm::Wrapper<cmg::WMuNu> wwmun_;
+    edm::Wrapper<std::vector<cmg::WMuNu> > wwmunv_;
+    
+    cmg::WTauNu wtn;
+    std::vector<cmg::WTauNu> wtnv_;
+    edm::Wrapper<cmg::WTauNu> wwtn_;
+    edm::Wrapper<std::vector<cmg::WTauNu> > wwtnv_;
+    
 	
   };
   
