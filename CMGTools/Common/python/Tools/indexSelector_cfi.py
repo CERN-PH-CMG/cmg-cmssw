25bf5abd3cd41dace7f469dd50cbfa1ccfae9fb1
wreece <>
2010-11-24 10:18:20 +0000

    Updates the SingleObjectCollectionSelector to make it safer.

--- CMGTools/Common/python/Tools/indexSelector_cfi.py
+++ CMGTools/Common/python/Tools/indexSelector_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-indexSelector = cms.EDProducer(
-                                "POCollectionSelector",
-                                inputCollection = cms.InputTag("cmgMuon"),
-                                index = cms.untracked.int32(-1)
-                                )
+indexSelector = cms.EDProducer("POCollectionSelector",
+                               inputCollection = cms.untracked.InputTag("cmgPFCand"),
+                               #copy only the first event
+                               index = cms.untracked.int32(0)
+                            )
 
