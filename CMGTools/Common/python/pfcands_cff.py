9993eb578a516cd9dbe7c8594bbd43a5af2c86fa
wreece <>
2010-12-09 12:48:52 +0000

    fixes a typo in the cfg

--- CMGTools/Common/python/pfcands_cff.py
+++ CMGTools/Common/python/pfcands_cff.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.factories.cmgPFCand_cfi import cmgPFCand
-from CMGTools.Common.histograms.pfcandsLorentzVector_cfi import pfcandsLorentzVector
+from CMGTools.Common.histograms.pfCandLorentzVector_cfi import pfCandLorentzVector
 
 pfcandsSequence = cms.Sequence(
     cmgPFCand +
-    pfcandsLorentzVector
+    pfCandLorentzVector
     )
