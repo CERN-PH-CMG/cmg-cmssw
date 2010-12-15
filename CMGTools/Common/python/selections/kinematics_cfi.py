9adae71e5adb41755534646974797a150cd04d8a
wreece <>
2010-12-15 22:07:59 +0000

    Removes a confusing cut, a warning, and a histogram, all for the tutorial

--- CMGTools/Common/python/selections/kinematics_cfi.py
+++ CMGTools/Common/python/selections/kinematics_cfi.py
@@ -1,14 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-additionalCut = cms.PSet(
-    pt = cms.string('pt()>50')
-    )
-
 kinematics = cms.PSet(
     pt = cms.string('pt() > 10'),
     eta = cms.string('abs(eta()) < 2.4'),
-    phi = cms.string('abs(phi()) < 10'),
-    addedCut = additionalCut.clone()
+    phi = cms.string('abs(phi()) < 3.2')
     )
 
                     
