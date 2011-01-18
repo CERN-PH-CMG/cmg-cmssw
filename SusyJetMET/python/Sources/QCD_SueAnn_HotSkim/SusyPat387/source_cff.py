5ec0303c596d7637948aa861451c68f41fc9e6da
cbern <>
2011-01-18 17:14:08 +0000

    added new sources adapted the RA2 sequence function to the new RA2 recipe

---
+++ SusyJetMET/python/Sources/QCD_SueAnn_HotSkim/SusyPat387/source_cff.py
@@ -0,0 +1,20 @@
+
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source(
+	"PoolSource",
+
+	noEventSort = cms.untracked.bool(True),
+	duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+	fileNames = cms.untracked.vstring()
+)
+source.fileNames.extend([
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD1000to1400.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD1400to1800.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD170to300.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD1800toInf.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD300to470.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD470to600.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD600to800.root',
+		'/store/cmst3/user/cbern/RA2SusyJetMET/QCD_SueAnn_HotSkim/SusyPat387/susypat_RA2_hotskim_QCD800to1000.root',
+])
