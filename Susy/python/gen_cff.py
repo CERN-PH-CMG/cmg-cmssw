cb30b65fd3ecb6c00ddd6a74d5b25914a1402429
cbern <>
2011-02-02 11:53:43 +0000

    first version, production LM0 working

---
+++ Susy/python/gen_cff.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.HepMCCandAlgos.genParticles_cfi import genParticles
+from RecoJets.Configuration.GenJetParticles_cff import genJetParticles
+from RecoJets.Configuration.RecoGenJets_cff import ak5GenJets
+from RecoMET.Configuration.GenMETParticles_cff import *
+from RecoMET.Configuration.RecoGenMET_cff import genMetTrue
+
+gen = cms.Sequence(
+    genParticles +
+    genJetParticles +
+    ak5GenJets +
+    genMETParticles +
+    genMetTrue 
+    )
