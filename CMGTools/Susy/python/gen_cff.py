80a2bef374254f09eb821313943665f08fc9879d
cbern <>
2011-02-03 15:34:37 +0000

    MET is now ok

--- CMGTools/Susy/python/gen_cff.py
+++ CMGTools/Susy/python/gen_cff.py
@@ -9,10 +9,10 @@ from RecoMET.Configuration.RecoGenMET_cff import genMetTrue
 from PhysicsTools.PFCandProducer.genForPF2PAT_cff import *
 
 gen = cms.Sequence(
-    genParticles + 
+    genParticles 
     # genForPF2PATSequence + 
     # genJetParticles +
     # ak5GenJets +
-    genMETParticles +
-    genMetTrue 
+#    genMETParticles +
+#    genMetTrue 
     )
