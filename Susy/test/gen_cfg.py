7e593ee6748fe1108c3ada869536ff94f47ccda3
cbern <>
2011-02-03 14:36:56 +0000

    the jets are ok. the "reconstructed" MET is much smaller than the gen met... investigating

--- Susy/test/gen_cfg.py
+++ Susy/test/gen_cfg.py
@@ -1,42 +1,64 @@
-import FWCore.ParameterSet.Config as cms
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
-process = cms.Process("GEN")
+import sys
+# import FWCore.ParameterSet.Config as cms
 
-# overriding source and various other things
+# changing process name 
+# process.setName_("GEN")
+
 
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
+
+# source 
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
-process.out = cms.OutputModule(
-    "PoolOutputModule",
-    outputCommands =  cms.untracked.vstring('keep *'),
-#    outputCommands =  cms.untracked.vstring(
-#      'keep recoPFCandidates_*_*_*',
-#      'keep *_genParticles_*_*',
-#      'keep *_genMetTrue_*_*'),
-    fileName = cms.untracked.string('gen.root'),
-    SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('p') )
-    )
-
-
-#process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
-#process.load("RecoJets.Configuration.GenJetParticles_cff")
-#process.load("RecoJets.Configuration.RecoGenJets_cff")
-
-#process.load("RecoMET.Configuration.GenMETParticles_cff")
-#process.load("RecoMET.Configuration.RecoGenMET_cff")
-
 process.load("CMGTools.Susy.GeneratorSources.pythia_LM0_cff")
+
+# gen sequences: convert gen particles to PFCandidates 
 
 process.load("CMGTools.Susy.gen_cff")
 process.load("CMGTools.Susy.genToPFConverter_cfi")
 
+# PF2PAT + PAT, working at gen level
+
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+from PhysicsTools.PatAlgos.tools.pfTools import *
+postfix = "PFlow"
+usePF2PAT(process,runPF2PAT=True, jetAlgo='AK5', runOnMC=True, postfix=postfix) 
+from CMGTools.Susy.PF2PATonGEN_cff import * 
+PF2PATonGEN(process)
+
+process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+
 process.p = cms.Path(
     process.ProductionFilterSequence + 
     process.gen + 
-    process.particleFlow
+    process.particleFlow +
+    process.PF2PATonGENSequence
     )
+
+
+
+process.outGen = cms.OutputModule(
+    "PoolOutputModule",
+#    outputCommands =  cms.untracked.vstring('keep *'),
+    outputCommands =  cms.untracked.vstring(
+      'keep recoPFCandidates_*_*_*',
+      'keep *_genParticles_*_*',
+      'keep *_genMetTrue_*_*'),
+    fileName = cms.untracked.string('gen.root'),
+    SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('p') )
+    )
+
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
+#process.load("PhysicsTools.PFCandProducer.PF2PAT_EventContent_cff")
+#process.out.outputCommands =  cms.untracked.vstring('drop *')
+process.out.fileName = 'PF2PATonGEN.root'
+process.out.outputCommands = cms.untracked.vstring( *patEventContentNoCleaning ) 
+
 
 process.outpath = cms.EndPath(
     process.out
