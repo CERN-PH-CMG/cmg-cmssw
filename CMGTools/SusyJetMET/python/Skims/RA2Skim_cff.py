15cf4dbf1e799304a9a5226336abb1536cedd4e6
cbern <>
2011-02-04 15:46:56 +0000

    adapted to changes in the common CMGTools

--- CMGTools/SusyJetMET/python/Skims/RA2Skim_cff.py
+++ CMGTools/SusyJetMET/python/Skims/RA2Skim_cff.py
@@ -1,11 +1,15 @@
 # selecting events with at least 3 jets with pT > 50 
 
-from CMGTools.Common.Skims.selEvents2HighPtJets_cff import *
-highPtJets.cut = 'pt()>50'
-filterHighPtJets.minNumber = 3 
+from CMGTools.Common.jet_cff import *
+from CMGTools.Common.skims.selEvents2HighPtJets_cff import *
+from CMGTools.Common.skims.selEventsHighMET_cff import *
+
+cmgPFJetSel.cut = 'pt()>50 && abs(eta())<2.5'
+cmgPFJetCount.minNumber = 3 
+
+cmgPFBaseJetSel.cut = cmgPFJetSel.cut 
 
 #COLIN: need to check how MHT is computed! eta cut
-from CMGTools.Common.Skims.selEventsHighMET_cff import *
 highMET.src = 'cmgMHTPFJets30'
 highMET.cut = ('pt()>150')
 
@@ -14,6 +18,8 @@ highMET.cut = ('pt()>150')
 # RA2 skimming sequence 
 
 RA2SkimSequence = cms.Sequence(
-    selEvents2HighPtJetsSequence +
+    cmgPFJetSel +
+    cmgPFJetCount +
+    cmgPFBaseJetSel +
     selEventsHighMETSequence
     )
