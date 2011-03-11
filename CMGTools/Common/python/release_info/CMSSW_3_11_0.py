d377772bf87b3c0f7f1f8205d2f65f299d37b3ab
wreece <>
2011-03-11 15:53:40 +0000

    adds some missing files

---
+++ CMGTools/Common/python/release_info/CMSSW_3_11_0.py
@@ -0,0 +1,4 @@
+{'UserCode/CMG/AnalysisDataFormats/CMGTools': ['HEAD', 'AnalysisDataFormats/CMGTools'],
+ 'UserCode/CMG/CMGTools/Common':              ['HEAD', 'CMGTools/Common'],
+ 'UserCode/CMG/CMGTools/RootTools':           ['HEAD', 'CMGTools/RootTools'],
+ 'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['HEAD', 'CMGTools/ZmumuJetsTutorial']}
