fd68ad8bf92ca1ccf35d2361063d7a177d3707bf
psilva <>
2011-03-14 10:55:29 +0000

    fixing scripts

--- HtoZZ2l2nu/test/scripts/localRun.sh
+++ HtoZZ2l2nu/test/scripts/localRun.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 ME=`whoami`
-MYCMSSWDIR=/afs/cern.ch/user/p/psilva/scratch0/CMSSW_3_8_7/src
+MYCMSSWDIR=/afs/cern.ch/user/p/psilva/scratch0/CMSSW_3_9_9_patch1/src
 cd ${MYCMSSWDIR}
 eval `scram r -sh`
 cd ${MYCMSSWDIR}/CMGTools/HtoZZ2l2nu/test
 
 localSrc=$1
 ffile=$2
+if [ -z $ffile]; then
+    ffile=0
+fi
 lfile=$3
+echo "Starting at first file: $ffile"
 cmsRun runCMGTuple_cfg.py $localSrc $ffile $lfile
 outdir="/castor/cern.ch/user/p/psilva/${localSrc}"
 output="/tmp/${ME}/${localSrc}_${ffile}_cmgTuple.root"
