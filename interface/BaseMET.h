afa7ca520f5ef0a5652a4ceb483bdd63d3047588
wreece <>
2011-03-15 16:57:55 +0000

    Adds a template specialisation so that cmg::BaseMet can be produced from pat::MET

--- interface/BaseMET.h
+++ interface/BaseMET.h
@@ -2,23 +2,30 @@
 #define _AnalysisDataFormats_CMGTools_BaseMET_h_
 
 #include "AnalysisDataFormats/CMGTools/interface/AbstractPhysicsObject.h"
+#include "AnalysisDataFormats/CMGTools/interface/UnSet.h"
 
 #include <vector>
 
 namespace cmg{
-
+    
   class BaseMET : public AbstractPhysicsObject{
   public:
-    BaseMET(const reco::LeafCandidate& cand, double sumEt ):
+    BaseMET(const reco::LeafCandidate& cand):
       AbstractPhysicsObject(cand),
-      sumEt_(sumEt) {}
+      sumEt_(UnSet(double)),
+      significance_(UnSet(double)){
+      }
 
     BaseMET() : sumEt_(-1) {}
 
     double sumEt() const {return sumEt_;}
+    double significance() const{return significance_;}
 		
   private:
     double sumEt_;
+    double significance_;
+    
+    template <class T> friend class BaseMETFactory;
 	
   };
 
