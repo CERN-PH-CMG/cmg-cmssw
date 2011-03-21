90502dbd0682242614de724f635e1003433d140b
wreece <>
2011-03-21 14:04:08 +0000

    Adds recipe for CMSSW 4_1_2_patch1, which is being used for Summer11

---
+++ Common/python/release_info/CMSSW_4_1_2_patch1.py
@@ -0,0 +1,4 @@
+{'UserCode/CMG/AnalysisDataFormats/CMGTools': ['wreece_210311', 'AnalysisDataFormats/CMGTools'],
+ 'UserCode/CMG/CMGTools/Common':              ['wreece_210311', 'CMGTools/Common'],
+ 'UserCode/CMG/CMGTools/RootTools':           ['wreece_210311', 'CMGTools/RootTools'],
+ 'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['wreece_210311c', 'CMGTools/ZmumuJetsTutorial']}
