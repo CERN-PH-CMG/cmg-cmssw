59beb84dfe9730a6924cdec011be05922371829f
wreece <>
2011-03-23 10:30:25 +0000

    Updates the recipe...

---
+++ Common/python/release_info/CMSSW_4_1_3.py
@@ -0,0 +1,9 @@
+{'UserCode/CMG/AnalysisDataFormats/CMGTools': ['wreece_210311', 'AnalysisDataFormats/CMGTools'],
+ 'UserCode/CMG/CMGTools/Common':              ['wreece_210311', 'CMGTools/Common'],
+ 'UserCode/CMG/CMGTools/RootTools':           ['wreece_210311b', 'CMGTools/RootTools'],
+ 'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['wreece_210311c', 'CMGTools/ZmumuJetsTutorial'],
+ ### now the PAT stuff itself - see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes41X#V08_06_01_09
+ 'CommonTools/CandAlgos':                     ['V00-01-01', 'CommonTools/CandAlgos'],
+ 'PhysicsTools/PFCandProducer':               ['V04-07-01-00', 'PhysicsTools/PFCandProducer'],
+ 'PhysicsTools/PatAlgos':                     ['V08-06-01-09', 'PhysicsTools/PatAlgos']  
+ }
