42655e2002689efcd5dec36c640997c16f6a272d
wreece <>
2011-03-23 16:34:13 +0000

    Adds a electron selection and removes the met significance

--- interface/BaseMET.h
+++ interface/BaseMET.h
@@ -12,18 +12,15 @@ namespace cmg{
   public:
     BaseMET(const reco::LeafCandidate& cand):
       AbstractPhysicsObject(cand),
-      sumEt_(UnSet(double)),
-      significance_(UnSet(double)){
+      sumEt_(UnSet(double)){
       }
 
     BaseMET() : sumEt_(-1) {}
 
     double sumEt() const {return sumEt_;}
-    double significance() const{return significance_;}
 		
   private:
     double sumEt_;
-    double significance_;
     
     template <class T> friend class BaseMETFactory;
 	
