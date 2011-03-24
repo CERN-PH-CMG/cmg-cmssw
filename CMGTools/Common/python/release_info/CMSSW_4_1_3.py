d9412e44d58b72c4f7e9d06ab5a0dc0f17f60608
wreece <>
2011-03-24 14:32:17 +0000

    Updates the recipe

--- CMGTools/Common/python/release_info/CMSSW_4_1_3.py
+++ CMGTools/Common/python/release_info/CMSSW_4_1_3.py
@@ -1,5 +1,5 @@
 {'UserCode/CMG/AnalysisDataFormats/CMGTools': ['wreece_230311', 'AnalysisDataFormats/CMGTools'],
- 'UserCode/CMG/CMGTools/Common':              ['wreece_230311', 'CMGTools/Common'],
+ 'UserCode/CMG/CMGTools/Common':              ['wreece_240311', 'CMGTools/Common'],
  'UserCode/CMG/CMGTools/RootTools':           ['wreece_230311', 'CMGTools/RootTools'],
  'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['wreece_230311', 'CMGTools/ZmumuJetsTutorial'],
  ### now the PAT stuff itself - see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes41X#V08_06_01_09
