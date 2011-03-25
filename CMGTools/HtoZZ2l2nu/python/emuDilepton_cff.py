0f2d3fcbd381679339b11f7799ce396e0d8470a7
psilva <>
2011-03-25 08:59:34 +0000

    removing old cfgs

--- CMGTools/HtoZZ2l2nu/python/emuDilepton_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CMGTools.HtoZZ2l2nu.cmgEmuDilepton_cfi import *
-from CMGTools.HtoZZ2l2nu.cmgEmuDileptonCount_cfi import *
-
-emuSequence = cms.Sequence(cmgEmuDilepton +
-                           cmgEmuDileptonCount
-                           )
-
