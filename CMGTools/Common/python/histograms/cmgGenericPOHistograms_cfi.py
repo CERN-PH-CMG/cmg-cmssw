f321603d9d9c6a9a85f1776ea60dbea77c59b1e2
cbern <>
2011-03-28 12:31:06 +0000

    * RCS file: /cvs_server/repositories/CMSSW/UserCode/CMG/CMGTools/Common/python/analysis_cff.py,v
    sequences should end by the string Sequence.
    
    * RCS file: /cvs_server/repositories/CMSSW/UserCode/CMG/CMGTools/Common/python/muon_cff.py,v
    
    using the generic PO histograms for muons as well.
    will need to change the name of the generic histogrammers, to make it obvious they fill histograms.
    
    * RCS file: /cvs_server/repositories/CMSSW/UserCode/CMG/CMGTools/Common/python/histograms/cmgGenericPOHistograms_cfi.py,v
    RCS file: /cvs_server/repositories/CMSSW/UserCode/CMG/CMGTools/Common/python/histograms/cmgMuonHistograms_cfi.py,v
    
    removed the histograms which in my opinion are useless.
    adapted the binning of some histograms
    
    * New PAT python directory, containing the base PF2PAT+PAT file, and the event content definition for PF2PAT+PAT. This file should be updated at each release.

--- CMGTools/Common/python/histograms/cmgGenericPOHistograms_cfi.py
+++ CMGTools/Common/python/histograms/cmgGenericPOHistograms_cfi.py
@@ -12,28 +12,12 @@ cmgGenericPOHistograms = cms.EDAnalyzer(
                high = cms.untracked.double(2)
                )
             ),
-        p = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('p()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               )
-            ),
         energy = cms.untracked.VPSet(
             cms.untracked.PSet( 
                var = cms.untracked.string('energy()'),
-               nbins = cms.untracked.int32(200),
+               nbins = cms.untracked.int32(100),
                low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               )
-            ),
-        et = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('et()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
+               high = cms.untracked.double(1000)
                )
             ),
         mass = cms.untracked.VPSet(
@@ -44,44 +28,12 @@ cmgGenericPOHistograms = cms.EDAnalyzer(
                high = cms.untracked.double(150)
                )
             ),
-        mt = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('mt()'),
-               nbins = cms.untracked.int32(150),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(150)
-               )
-            ),
-        px = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('px()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               )
-            ),            
-        py = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('py()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               )
-            ),            
-        pz = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('pz()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               )
-            ),            
         pt = cms.untracked.VPSet(
             cms.untracked.PSet( 
                var = cms.untracked.string('pt()'),
-               nbins = cms.untracked.int32(200),
+               nbins = cms.untracked.int32(100),
                low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
+               high = cms.untracked.double(500)
                )
             ),
         phi = cms.untracked.VPSet(
@@ -92,43 +44,13 @@ cmgGenericPOHistograms = cms.EDAnalyzer(
                high = cms.untracked.double(3.15)
                )
             ),        
-        theta = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('theta()'),
-               nbins = cms.untracked.int32(50),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(3.15)
-               )
-            ),
         eta = cms.untracked.VPSet(
             cms.untracked.PSet( 
                var = cms.untracked.string('eta()'),
                nbins = cms.untracked.int32(50),
-               low = cms.untracked.double(-3.15),
-               high = cms.untracked.double(3.15)
+               low = cms.untracked.double(-5),
+               high = cms.untracked.double(5)
                )
-            ),              
-        y = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('y()'),
-               nbins = cms.untracked.int32(50),
-               low = cms.untracked.double(-3.15),
-               high = cms.untracked.double(3.15)
-               )
-            ),
-        pt_eta = cms.untracked.VPSet(
-            cms.untracked.PSet( 
-               var = cms.untracked.string('pt()'),
-               nbins = cms.untracked.int32(200),
-               low = cms.untracked.double(0),
-               high = cms.untracked.double(200)
-               ),
-            cms.untracked.PSet( 
-               var = cms.untracked.string('eta()'),
-               nbins = cms.untracked.int32(50),
-               low = cms.untracked.double(-3.15),
-               high = cms.untracked.double(3.15)
-               )
-            ),                                
-        )
+            )         
+    )
     )
