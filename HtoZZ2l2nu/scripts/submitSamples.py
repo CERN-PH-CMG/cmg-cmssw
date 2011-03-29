b69e213390997f7817b0bc03285ea5461c321c93
psilva <>
2011-03-29 14:43:25 +0000

    adding submitter for batch

---
+++ HtoZZ2l2nu/scripts/submitSamples.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os,sys
+import json
+
+if(len(sys.argv)<2):
+    print 'submitSamples.py samples.json scriptToRunOnBatch.sh'
+
+from CMGTools.HtoZZ2l2nu.localPatTuples_cff import *
+
+#open the file which describes the sample
+samplesDB = sys.argv[1]
+jsonFile = open(samplesDB,'r')
+procList=json.load(jsonFile,encoding='utf-8').items()
+scriptToRun=sys.argv[2]
+
+#run over sample
+fperjob=5
+for proc in procList :
+
+    #run over processes
+    for desc in proc[1] :
+        
+        #run over items in process
+        data = desc['data']
+        for d in data :
+            tag = d['dtag']
+            files=getLocalSourceFor( tag )
+            if(files is None):continue
+            nfiles=len(getLocalSourceFor( tag ) )
+            njobs=nfiles/fperjob+1
+            for ijob in range(njobs) :
+                os.system('submit2batch.sh ' + scriptToRun + ' ' + tag + ' ' + str(ijob*fperjob) + ' ' + str(ijob*fperjob+fperjob))
