f9c4b9831a5e8401d78f0319576fecc262bf70aa
psilva <>
2011-04-04 15:45:56 +0000

    fixing vbtf electron selection

--- CMGTools/Common/python/selections/vbtfelectron_cfi.py
+++ CMGTools/Common/python/selections/vbtfelectron_cfi.py
@@ -5,7 +5,7 @@ def addVBTFSelection(var):
                     #isEcalDriven = cms.string('sourcePtr().ecalDrivenSeed()'),  #requires gsf core to be saved
                     numberOfInnerTrackerHitsLost = cms.string('sourcePtr().gsfTrack().isNonnull() && sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfLostHits()<2'),
                     conversionVeto = cms.string('sourcePtr().electronID(\"%s\")>4' % var),
-                    eid = cms.string('sourcePtr().electronID(\"%s\")==1 || sourcePtr().electronID(\"%s\")==3 || sourcePtr().electronID(\"%s\")==5' % (var,var,var))
+                    eid = cms.string('sourcePtr().electronID(\"%s\")==1 || sourcePtr().electronID(\"%s\")==3 || sourcePtr().electronID(\"%s\")==5 || sourcePtr().electronID(\"%s\")==7' % (var,var,var,var))
     )
     return vbtf
     
