cf6c7cb72374b9644a7e2c114a3e0716d35de463
wreece <>
2011-04-06 08:48:27 +0000

    Adds a cut inverter and updates the Razor beta selection

--- Common/python/skims/cmgCandSel_cfi.py
+++ Common/python/skims/cmgCandSel_cfi.py
@@ -1,5 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
+## e.g. invertSelector(cmgCandSel.clone())
+def invertSelector(sel):
+    """Takes a selector and returns a clone with the inverse selection"""
+    result = sel.clone()
+    result.cut.setValue('!(%s)' % sel.cut.value())
+    return result
+
 # do not really on the default cuts implemented here,
 # as they are subject to change. 
 # you should override these cuts in your analysis.
