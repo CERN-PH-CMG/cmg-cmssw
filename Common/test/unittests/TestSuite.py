7932d1aaa95d21a0481497e2eac74eefae1ad54f
jhegeman <>
2011-04-08 10:48:40 +0000

    Modified TestSuite.py a bit to make switching tests on/off a bit nicer.

--- Common/test/unittests/TestSuite.py
+++ Common/test/unittests/TestSuite.py
@@ -7,34 +7,40 @@ import unittest
 class CMGTestLoader(unittest.TestLoader):
 
     def suiteClass(self,foo):
-    
+
         testDir = os.path.expandvars(os.path.join('$CMSSW_BASE','src','CMGTools','Common','test','unittests'))
         if testDir not in sys.path:
             sys.path.append(testDir)
-    
-        import TestBuild
-        import TestElectron
-        import TestMET
-        import TestMuon
-        import TestJet
-        import TestWENu
-        import TestWMuNu
-    
-        loader = unittest.TestLoader()
+
+        # Just list whatever tests you want to run here.
+        tests = [
+#             "TestBuild",
+            "TestElectron",
+            "TestMET",
+            "TestMuon",
+            "TestJet",
+            "TestWENu",
+            "TestWMuNu",
+            "TestHemi",
+            "TestMultiJet"
+            ]
+
+        sep_line = "-" * 50
+        print sep_line
+        print "Going to run %d tests: %s" % (len(tests), ", ".join(tests))
+        print sep_line
+
+        modules = map(__import__, tests)
+
         suites = []
-        #suites.append(loader.loadTestsFromModule(TestBuild))
-        suites.append(loader.loadTestsFromModule(TestElectron))
-        suites.append(loader.loadTestsFromModule(TestMET))
-        suites.append(loader.loadTestsFromModule(TestMuon))
-        suites.append(loader.loadTestsFromModule(TestJet))
-        suites.append(loader.loadTestsFromModule(TestWENu))
-        suites.append(loader.loadTestsFromModule(TestWMuNu))
-    
+        for test in tests:
+            suites.append(unittest.defaultTestLoader.loadTestsFromName(test))
+
         tests = unittest.TestSuite()
         tests.addTests(suites)
 
         return tests
 
 if __name__ == '__main__':
-    
+
     unittest.main(testLoader=CMGTestLoader())
