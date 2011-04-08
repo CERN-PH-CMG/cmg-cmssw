b93ef5756d23365ce5d258f60c3a13e8d73e4602
jhegeman <>
2011-04-08 13:54:12 +0000

    Removing cuts from hemi factory.

--- CMGTools/Common/python/factories/cmgHemi_cfi.py
+++ CMGTools/Common/python/factories/cmgHemi_cfi.py
@@ -2,8 +2,6 @@
 
 import FWCore.ParameterSet.Config as cms
 
-from CMGTools.Common.selections.kinematics_cfi import kinematics
-
 hemiFactory = cms.PSet(
     inputCollection = cms.VInputTag("dummy"),
     maxCand = cms.uint32(30)
@@ -13,7 +11,6 @@ cmgHemi = cms.EDFilter("HemispherePOProducer",
                        cfg = hemiFactory.clone(
     inputCollection = cms.VInputTag("cmgPFJetSel")
     ),
-                       cuts = cms.PSet(kinematics = kinematics.clone())
                        )
 
 ######################################################################
