82b11098f3242f82b795d5935223a896e0d75953
lenzip <>
2011-04-10 14:22:37 +0000

    first commit of LP (lepton projection) sequence

---
+++ Susy/python/LP/LPEventContent_cff.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+LPEventContent = cms.untracked.vstring(
+  'keep *_LPcmgWMuNu_*_*',
+  'keep *_LPcmgPFBaseJetSel_*_*',
+  'keep *_LPcmgMHTPFJets30_*_*'
+)
---
+++ Susy/python/LP/LP_cff.py
@@ -0,0 +1,73 @@
+import FWCore.ParameterSet.Config as cms
+
+LPmuonCut = 'pt() > 20 && abs(eta()) < 2.1 && getSelection("cuts_vbtfmuon") && getSelection("cuts_isomuon")'
+LPlooseMuonCut = 'pt() > 10 && abs(eta()) < 2.4 && getSelection("cuts_vbtfmuon") && getSelection("cuts_isomuon")'
+#LPjetCut  = 'pt() > 20 & abs(eta()) < 3 && getSelection("cuts_looseJetId")'
+LPjetCut  = 'pt() > 20 & abs(eta()) < 3'
+LPwMtCut    = 'sqrt( pow(leg1().pt()+leg2.pt(), 2) - pow(leg1().px()+leg2.px(), 2) - pow(leg1().py()+leg2.py(), 2)) > 30'
+LPwPtCut    = 'pt() > 50'
+
+####SKIMMING
+from CMGTools.Common.skims.cmgCandSel_cfi import *
+from CMGTools.Common.skims.cmgCandCount_cfi import *
+
+#muons
+from CMGTools.Common.skims.cmgMuonSel_cfi import *
+LPcmgMuonSel   = cmgMuonSel.clone( src = 'cmgMuonSel', cut = LPmuonCut ) 
+LPcmgLooseMuonSel   = cmgMuonSel.clone( src = 'cmgMuonSel', cut = LPlooseMuonCut ) 
+LPcmgMuonCount1 = cmgCandCount.clone( src = 'LPcmgMuonSel', minNumber = 1)
+LPcmgMuonCount2 = cmgCandCount.clone( src = 'LPcmgLooseMuonSel', minNumber = 2)
+LPmuon = cms.Sequence( LPcmgMuonSel * LPcmgLooseMuonSel )
+LPmuonSkim = cms.Sequence( LPcmgMuonCount1 * ~LPcmgMuonCount2)
+
+#jets
+LPcmgPFBaseJetSel = cmgCandSel.clone( src = 'cmgPFBaseJetSel', cut = LPjetCut ) 
+LPcmgPFBaseJetCount = cmgCandCount.clone( src = 'LPcmgPFBaseJetSel', minNumber = 3 )
+LPjet = cms.Sequence( LPcmgPFBaseJetSel )
+LpjetSkim = cms.Sequence( LPcmgPFBaseJetCount )
+
+#W
+from CMGTools.Common.factories.cmgWMuNu_cfi import cmgWMuNu
+LPcmgWMuNu = cmgWMuNu.clone()
+LPcmgWMuNu.cfg.leg1Collection = 'cmgMuonSel'
+wmunuCuts = cms.PSet(
+    mt = cms.string( LPwMtCut ),
+    pt = cms.string( LPwPtCut )
+)
+LPcmgWMuNu.cuts = wmunuCuts 
+from CMGTools.Common.skims.cmgWMuNuSel_cfi import cmgWMuNuSel
+LPcmgWMuNuPtSel = cmgWMuNuSel.clone(src = 'LPcmgWMuNu', cut = 'getSelection("cuts_pt")')
+LPcmgWMuNuMtSel = cmgWMuNuSel.clone(src = 'LPcmgWMuNu', cut = 'getSelection("cuts_mt")')
+LPcmgWMuNuPtCount = cmgCandCount.clone( src = 'LPcmgWMuNuPtSel', minNumber = 1)
+LPcmgWMuNuMtCount = cmgCandCount.clone( src = 'LPcmgWMuNuMtSel', minNumber = 1)
+LPw = cms.Sequence( LPcmgWMuNu )
+LPwSkim = cms.Sequence( LPcmgWMuNuMtSel * LPcmgWMuNuPtSel * LPcmgWMuNuPtCount * LPcmgWMuNuMtCount )
+
+#ht 
+from CMGTools.Common.skims.cmgBaseMETSel_cfi import cmgBaseMETSel
+from CMGTools.Common.met_cff import cmgMHTPFJets30
+LPcmgMHTPFJets30      = cmgMHTPFJets30.clone() 
+LPcmgMHTPFJets30.cfg.inputCollection = 'LPcmgPFBaseJetSel'
+LPcmgMHTPFJets30Sel   = cmgBaseMETSel.clone( src = 'LPcmgMHTPFJets30', cut = 'sumEt()>100' )
+LPcmgMHTPFJets30Count = cmgCandCount.clone( src = 'LPcmgMHTPFJets30Sel', minNumber = 1 )
+LPht = cms.Sequence( LPcmgMHTPFJets30 * LPcmgMHTPFJets30Sel )
+LPhtSkim = cms.Sequence( LPcmgMHTPFJets30Count )
+
+LPobjectSequence = cms.Sequence( LPmuon * LPjet * LPht * LPw)
+#LPskimSequence   = cms.Sequence( LPmuonSkim * LPjetSkim * LPwSkim * LPhtSkim )
+LPskimSequence   = cms.Sequence( LPmuonSkim * LPwSkim )
+
+
+####HISTOGRAMMING
+from CMGTools.Common.histogram_cff import *
+from CMGTools.Common.histograms.cmgMuonHistograms_cfi import cmgMuonHistograms
+LPmhtPFJets30Histograms = mhtPFJets30Histograms.clone( inputCollection = 'LPcmgMHTPFJets30Sel' )
+
+LPmuonHistograms = cmgMuonHistograms.clone(inputCollection = 'cmgMuonSel')
+
+LPhistogrammingSequence = cms.Sequence(LPmhtPFJets30Histograms*LPmuonHistograms)
+
+#####General sequences
+LPSequence = cms.Sequence( LPobjectSequence * LPhistogrammingSequence )
+
+LPSkimSequence = cms.Sequence( LPobjectSequence * LPskimSequence)
