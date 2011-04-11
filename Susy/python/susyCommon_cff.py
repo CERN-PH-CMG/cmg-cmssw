e01e21a484382817dc799f2ac8423cb69fe8afc5
cbern <>
2011-04-11 15:01:27 +0000

    integrated single lepton analysis from Giulio

--- Susy/python/susyCommon_cff.py
+++ Susy/python/susyCommon_cff.py
@@ -4,15 +4,17 @@ from CMGTools.Common.analysis_cff import *
 from CMGTools.Susy.RA1.RA1_cff import *
 from CMGTools.Susy.RA2.RA2_cff import *
 from CMGTools.Susy.Razor.razor_cff import *
+from CMGTools.Susy.LP.LP_cff import *
 
 jetPtCut = 'pt()>20'
 cmgPFJetSel.cut = jetPtCut
 cmgPFBaseJetSel.cut = jetPtCut
 cmgCaloBaseJetSel.cut = jetPtCut
 
-fullyHadronicCommonSequence  = cms.Sequence(
+susyCommonSequence  = cms.Sequence(
     analysisSequence +
     RA1Sequence +
     RA2Sequence +
-    razorSequence
+    razorSequence +
+    LPSequence
     )
