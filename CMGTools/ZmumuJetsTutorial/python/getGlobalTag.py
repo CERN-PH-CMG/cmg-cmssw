4b8ee3f513ff24f2fd8edde6dd441082f76a3768
wreece <>
2011-04-11 08:20:02 +0000

    Updates this to use the CMSSW tool. Thanks to Florian for pointing this tool out to us.

--- CMGTools/ZmumuJetsTutorial/python/getGlobalTag.py
+++ CMGTools/ZmumuJetsTutorial/python/getGlobalTag.py
@@ -1,27 +1,12 @@
 import os
 
+from Configuration.PyReleaseValidation.autoCond import autoCond
+
 def getGlobalTag(runOnMC):
     """Return the global tag depending on the release"""
     
-    data = {'CMSSW_3_8_7':'GR_R_38X_V15::All',
-            'CMSSW_4_1_3':'GR_R_311_V2::All',
-            }
-    
-    mc = {'CMSSW_3_8_7':'START38_V14::All',
-          'CMSSW_4_1_3':'START311_V2::All'
-          }
-    
-    if not os.environ.has_key('CMSSW_VERSION'):
-        raise KeyError("Looking for CMSSW_VERSION, but not found. Did you do 'cmsenv'?")
-    version = os.environ.get('CMSSW_VERSION')
-
-    tag = None
     if runOnMC:
-        if not mc.has_key(version):
-            raise KeyError("Unknown CMSSW version '%s'. Global tag for MC not found." % version)
-        tag = mc[version]
+        #default to startup, but use mc otherwise
+        return autoCond.get('startup',autoCond['mc'])
     else:
-        if not data.has_key(version):
-            raise KeyError("Unknown CMSSW version '%s'. Global tag for data not found." % version)
-        tag = data[version]
-    return tag
+        return autoCond['com10']
