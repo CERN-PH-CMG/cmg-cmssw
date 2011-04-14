8a0b0a12c5a5a9e8fb197bf5e88e1ef5215334aa
cbern <>
2011-04-14 11:54:03 +0000

    setting up production... not finished

---
+++ Common/prod/myRelvalList_3_11.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os,sys
+
+scriptDir = os.getenv('CMSSW_BASE') + '/src/CMGTools/Common/scripts'
+sys.path.append( scriptDir ) 
+import castortools
+
+sys.path.append( '.' ) 
+
+from relvalDefinition import *
+
+relvals = relvalList()
+
+cmssw = 'CMSSW_3_11_2'
+relvals.add( cmssw, 'RelValQCD_FlatPt_15_3000','MC_311_V2' )
+
