06d3b93d97bbe7668cba789c7ccd4142e79f9c00
jhegeman <>
2011-04-18 15:39:37 +0000

    Added leading object variable histogramming and testing.

--- Common/python/histograms/cmgMultiJetHistograms_cfi.py
+++ Common/python/histograms/cmgMultiJetHistograms_cfi.py
@@ -50,6 +50,58 @@ cmgMultiJetHistograms = cms.EDAnalyzer(
                 title = cms.untracked.string("|#eta|")
                 )
             ),
+        pt_lead = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('leadPtr().pt()'),
+                nbins = cms.untracked.int32(100),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(200),
+                title = cms.untracked.string("p_{T, lead}")
+                )
+            ),
+        eta_lead = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('leadPtr().eta()'),
+                nbins = cms.untracked.int32(50),
+                low = cms.untracked.double(-3.),
+                high = cms.untracked.double(3.),
+                title = cms.untracked.string("#eta_{lead}")
+                )
+            ),
+        phi_lead = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('leadPtr().phi()'),
+                nbins = cms.untracked.int32(63),
+                low = cms.untracked.double(-3.15),
+                high = cms.untracked.double(3.15),
+                title = cms.untracked.string("#phi_{lead}")
+                )
+            ),
+        pt_eta_lead = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('leadPtr().pt()'),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(200),
+                nbins = cms.untracked.int32(50),
+                title = cms.untracked.string("p_{T, lead}")
+                ),
+            cms.untracked.PSet(
+                var = cms.untracked.string('abs(leadPtr().eta())'),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(2.5),
+                nbins = cms.untracked.int32(10),
+                title = cms.untracked.string("|#eta_{lead}|")
+                )
+            ),
+        numConstituents = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('numConstituents()'),
+                nbins = cms.untracked.int32(100),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(100),
+                title = cms.untracked.string("N_{constituents}")
+                )
+            ),
         deltaPhiStar = cms.untracked.VPSet(
             cms.untracked.PSet(
                 var = cms.untracked.string('deltaPhiStar()'),
@@ -57,6 +109,22 @@ cmgMultiJetHistograms = cms.EDAnalyzer(
                 low = cms.untracked.double(0.),
                 high = cms.untracked.double(3.15),
                 title = cms.untracked.string("#Delta#phi^{*}")
+                )
+            ),
+        pt_pt_lead = cms.untracked.VPSet(
+            cms.untracked.PSet(
+                var = cms.untracked.string('leadPtr().pt()'),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(200),
+                nbins = cms.untracked.int32(50),
+                title = cms.untracked.string("p_{T, lead}")
+                ),
+            cms.untracked.PSet(
+                var = cms.untracked.string('pt()'),
+                low = cms.untracked.double(0),
+                high = cms.untracked.double(200),
+                nbins = cms.untracked.int32(50),
+                title = cms.untracked.string("p_{T}")
                 )
             ),
         )
