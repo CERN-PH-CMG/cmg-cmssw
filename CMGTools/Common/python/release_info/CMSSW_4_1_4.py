7006a02944f25ed97b607d71d138fd7ea46d2308
cbern <>
2011-04-18 16:16:40 +0000

    defined release info for 4_1_4. needs testing

--- CMGTools/Common/python/release_info/CMSSW_4_1_4.py
+++ CMGTools/Common/python/release_info/CMSSW_4_1_4.py
@@ -2,8 +2,8 @@
  'UserCode/CMG/CMGTools/Common':              ['Colin_Apr18', 'CMGTools/Common'],
  'UserCode/CMG/CMGTools/RootTools':           ['Colin_Apr18', 'CMGTools/RootTools'],
  ### now the PAT stuff itself - see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes41X#V08_06_01_09
- 'DataFormats/PatCandidate':                     ['V06-04-01-04', 'DataFormats/PatCandidate']  
- 'FWCore/GuiBrowsers':                     ['V00-00-54', 'FWCore/GuiBrowsers']  
- 'PhysicsTools/PatAlgos':                     ['V08-06-01-18', 'PhysicsTools/PatAlgos']  
- 'PhysicsTools/PatUtils':                     ['V03-09-18', 'PhysicsTools/PatUtils']  
+ 'DataFormats/PatCandidate':                     ['V06-04-01-04', 'DataFormats/PatCandidate'],
+ 'FWCore/GuiBrowsers':                     ['V00-00-54', 'FWCore/GuiBrowsers'],
+ 'PhysicsTools/PatAlgos':                     ['V08-06-01-18', 'PhysicsTools/PatAlgos'],  
+ 'PhysicsTools/PatUtils':                     ['V03-09-18', 'PhysicsTools/PatUtils'],  
  }
