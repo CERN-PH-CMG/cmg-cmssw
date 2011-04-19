942d1256f995dfa7604e1965d0de30fe504225d3
cbern <>
2011-04-19 11:41:26 +0000

    new ttbar relval

--- CMGTools/Common/prod/myRelvalList_3_11.py
+++ CMGTools/Common/prod/myRelvalList_3_11.py
@@ -13,5 +13,6 @@ from relvalDefinition import *
 relvals = relvalList()
 
 cmssw = 'CMSSW_3_11_2'
-relvals.add( cmssw, 'RelValQCD_FlatPt_15_3000','MC_311_V2' )
+# relvals.add( cmssw, 'RelValQCD_FlatPt_15_3000','MC_311_V2' )
+relvals.add( cmssw, 'RelValTTbar','MC_311_V2' )
 
