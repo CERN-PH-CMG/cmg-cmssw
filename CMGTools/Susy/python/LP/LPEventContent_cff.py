fb57f7baf424deb32157ad07a4cf0d3093c905c5
lenzip <>
2011-04-20 12:15:04 +0000

    updated LP sequence

--- CMGTools/Susy/python/LP/LPEventContent_cff.py
+++ CMGTools/Susy/python/LP/LPEventContent_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 LPEventContent = cms.untracked.vstring(
   'keep *_LPWMuNu_*_*',
-  'keep *_LPPFBaseJetSel_*_*',
+  'keep *_LPPFJetSel_*_*',
+  'keep *_LPMHTPFJet30*Sel_*_*',
+  'keep *_LPMuMHT_*_*'
   # 'keep *_LPMHTPFJets30_*_*'
 )
--- CMGTools/Susy/python/LP/LP_cff.py
+++ CMGTools/Susy/python/LP/LP_cff.py
@@ -1,89 +1,95 @@
 import FWCore.ParameterSet.Config as cms
 
-LPMuonCut = 'pt() > 20 && abs(eta()) < 2.1 && getSelection("cuts_vbtfmuon") && getSelection("cuts_isomuon")'
-LPLooseMuonCut = 'pt() > 10 && abs(eta()) < 2.4 && getSelection("cuts_vbtfmuon") && getSelection("cuts_isomuon")'
-#LPJetCut  = 'pt() > 20 & abs(eta()) < 3 && getSelection("cuts_looseJetId")'
-LPJetCut  = 'pt() > 20 & abs(eta()) < 3'
-LPWMtCut    = 'sqrt( pow(leg1().pt()+leg2.pt(), 2) - pow(leg1().px()+leg2.px(), 2) - pow(leg1().py()+leg2.py(), 2)) > 30'
-LPWPtCut    = 'pt() > 50'
+LPMuonCut = 'pt() > 20 && abs(eta()) < 2.1'
+LPLooseMuonCut = 'pt() > 10 && abs(eta()) < 2.4'
+LPJetCut  = 'pt() > 30 & abs(eta()) < 3'
+#LPWMtCut    = 'sqrt( pow(leg1().pt()+leg2.pt(), 2) - pow(leg1().px()+leg2.px(), 2) - pow(leg1().py()+leg2.py(), 2)) > 30'
+LPHTLooseCut    = 'sumEt() > 150'
+LPHTTightCut    = 'sumEt() > 250'
+LPWMtCut    = 'mt() > 30'
+LPWPtLooseCut    = 'pt() > 50'
+LPWPtTightCut    = 'pt() > 150'
 
 ####SKIMMING
 from CMGTools.Common.skims.cmgCandSel_cfi import *
 from CMGTools.Common.skims.cmgCandCount_cfi import *
 
 #muons
-
 from CMGTools.Common.skims.cmgMuonSel_cfi import *
-LPMuon   = cmgMuonSel.clone( src = 'cmgMuonSel', cut = LPMuonCut ) 
-LPLooseMuon   = cmgMuonSel.clone( src = 'cmgMuonSel', cut = LPLooseMuonCut ) 
+LPMuon   = cmgMuonSel.clone( src = 'cmgMuon', cut = LPMuonCut ) 
+LPLooseMuon   = cmgMuonSel.clone( src = 'cmgMuon', cut = LPLooseMuonCut ) 
 LPMuonCount1 = cmgCandCount.clone( src = 'LPMuon', minNumber = 1)
 LPMuonCount2 = cmgCandCount.clone( src = 'LPLooseMuon', minNumber = 2)
 LPMuonSequence = cms.Sequence( LPMuon + LPLooseMuon )
-LPMuonSkimSequence = cms.Sequence( LPMuonCount1 + ~LPMuonCount2)
+LPMuonSkimSequence = cms.Sequence( LPMuonCount1 ) #+ ~LPMuonCount2)
 
 #jets for HT computation
-
 from CMGTools.Common.skims.cmgPFJetSel_cfi import *
-
 LPPFJetSel = cmgPFJetSel.clone( src = 'cmgPFJetSel', cut = LPJetCut ) 
-LPPFJetCount = cmgCandCount.clone( src = 'LPPFJetSel', minNumber = 3 )
-
-# LPJetSequence = cms.Sequence( LPPFJetSel )
-# LPJetSkimSequence = cms.Sequence( LPPFJetCount )
+LPPFJetCount = cmgCandCount.clone( src = 'LPPFJetSel', minNumber = 2 )
 
 #HT
-
 from CMGTools.Common.met_cff import cmgMHTPFJet30
-LPMHTPFJet20 = cmgMHTPFJet30.clone()
-LPMHTPFJet20.cfg.ptThreshold = 20.0
-LPMHTPFJet20.cfg.inputCollection = 'LPPFJetSel'
+LPMHTPFJet30 = cmgMHTPFJet30.clone()
+LPMHTPFJet30.cfg.ptThreshold = 30.0
+LPMHTPFJet30.cfg.inputCollection = 'LPPFJetSel'
+#HT including selected muons
+from CMGTools.Common.Tools.cmgBaseMETModifier_cfi import cmgBaseMETModifier
+LPLeptonicMHTPFJet30 = cmgBaseMETModifier.clone() 
+LPLeptonicMHTPFJet30.cfg.inputCollection = 'LPMuon'
+LPLeptonicMHTPFJet30.cfg.metCollection   = 'LPMHTPFJet30' 
+LPLeptonicMHTPFJet30.cfg.operator = '+'
 
 from CMGTools.Common.skims.cmgBaseMETSel_cfi import cmgBaseMETSel
 
-LPMHTPFJet20Sel = cmgBaseMETSel.clone( src = 'LPMHTPFJet20',
-                                       cut = 'sumEt()>100') 
+LPMHTPFJet30LooseSel = cmgBaseMETSel.clone( src = 'LPMHTPFJet30',
+                                            cut = LPHTLooseCut ) 
 
-LPMHTPFJet20Count = cmgCandCount.clone( src = 'LPMHTPFJet20Sel',
-                                        minNumber = 1 )
+LPMHTPFJet30LooseCount = cmgCandCount.clone( src = 'LMHTPFJet30LooseSel',
+                                             minNumber = 1 )
+
+LPMHTPFJet30TightSel = cmgBaseMETSel.clone( src = 'LPMHTPFJet30',
+                                            cut = LPHTTightCut )
+
+LPMHTPFJet30TightCount = cmgCandCount.clone( src = 'LMHTPFJet30TightSel',
+                                             minNumber = 1 )
 
 #W
-
 from CMGTools.Common.factories.cmgWMuNu_cfi import cmgWMuNu
 LPWMuNu = cmgWMuNu.clone()
 LPWMuNu.cfg.leg1Collection = 'LPMuon'
 wmunuCuts = cms.PSet(
+    muon = cms.string( 'leg1().getSelection(\"cuts_vbtfmuon\")'),
+    isolation = cms.string('leg1().getSelection(\"cuts_isomuon\")'),
     mt = cms.string( LPWMtCut ),
-    pt = cms.string( LPWPtCut )
+    ptLoose = cms.string( LPWPtLooseCut ),
+    ptTight = cms.string( LPWPtTightCut )
 )
 LPWMuNu.cuts = wmunuCuts 
-from CMGTools.Common.skims.cmgWMuNuSel_cfi import cmgWMuNuSel
-LPWMuNuPtSel = cmgWMuNuSel.clone(src = 'LPWMuNu', cut = 'getSelection("cuts_pt")')
-LPWMuNuMtSel = cmgWMuNuSel.clone(src = 'LPWMuNu', cut = 'getSelection("cuts_mt")')
-LPWMuNuPtCount = cmgCandCount.clone( src = 'LPWMuNuPtSel', minNumber = 1)
-LPWMuNuMtCount = cmgCandCount.clone( src = 'LPWMuNuMtSel', minNumber = 1)
-#LPWSequence = cms.Sequence( LPWMuNu )
 
-
-
+LPMuMHT = cmgWMuNu.clone()
+LPMuMHT.cfg.leg1Collection = 'LPMuon'
+LPMuMHT.cfg.leg2Collection = 'LPLeptonicMHTPFJet30'
+MuMHTCuts = cms.PSet(
+    muon = cms.string( 'leg1().getSelection(\"cuts_vbtfmuon\")'),
+    isolation = cms.string('leg1().getSelection(\"cuts_isomuon\")'),
+    mt = cms.string( LPWMtCut ),
+    ptLoose = cms.string( LPWPtLooseCut ),
+    ptTight = cms.string( LPWPtTightCut )
+)
+LPMuMHT.cuts = MuMHTCuts
 
 
 LPObjectSequence = cms.Sequence(
     LPMuonSequence +
     LPPFJetSel +
-    LPMHTPFJet20 +
-    LPMHTPFJet20Sel + 
-    # LPHTSequence + 
-    LPWMuNu + 
-    LPWMuNuMtSel +
-    LPWMuNuPtSel 
+    LPMHTPFJet30 +
+    LPMHTPFJet30LooseSel +
+    LPMHTPFJet30TightSel +
+    LPLeptonicMHTPFJet30 +
+    LPWMuNu +
+    LPMuMHT 
     )
-
-LPWSkimSequence = cms.Sequence(
-    LPWMuNuPtCount +
-    LPWMuNuMtCount
-    )
-
-#LPSkimSequence   = cms.Sequence( LPMuonSkim + LPJetSkim + LPWSkim + LPHTSkim )
 
 
 ####HISTOGRAMMING
@@ -103,15 +109,9 @@ LPSequence = cms.Sequence(
     LPHistogrammingSequence
     )
 
-# LPSkimSequence = cms.Sequence(
-    # LPObjectSequence +
-#    LPSkimSequence
-#    )
-
 LPSkimSequence   = cms.Sequence(
     LPObjectSequence + 
-    LPMuonSkimSequence +
-    LPWSkimSequence +
-    LPPFJetCount +
-    LPMHTPFJet20Count
+    LPMuonSkimSequence #+
+    #LPPFJetCount +
+    #LPMHTPFJet20Count
     )
