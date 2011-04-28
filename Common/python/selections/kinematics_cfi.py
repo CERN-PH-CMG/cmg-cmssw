97bbf1baf43fa8962c5beb716a5cad7f076ea97f
mgouzevi <>
2011-04-28 17:39:21 +0000

    remove dijet kinematics to dijet_cfi.py

--- Common/python/selections/kinematics_cfi.py
+++ Common/python/selections/kinematics_cfi.py
@@ -5,13 +5,3 @@ kinematics = cms.PSet(
     eta = cms.string('abs(eta()) < 2.4'),
     phi = cms.string('abs(phi()) < 3.2')
     )
-
-                    
-
-dijetKinematics = cms.PSet(
-    looseMass = cms.string('mass() > 200'),
-    tightMass = cms.string('mass() > 220'),
-    deltaEta = cms.string('abs(leg1().eta()-leg2.eta()) < 1.3'),
-    looseDeltaEta = cms.string('abs(leg1().eta()-leg2.eta()) < 1.7'),
-    tightDeltaEta = cms.string('abs(leg1().eta()-leg2.eta()) < 1.3')
-    )
