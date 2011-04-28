2a2920be1bec6c8dc09626c64f482e0729673c14
mgouzevi <>
2011-04-28 17:03:25 +0000

    Update for 4_1_3

--- CMGTools/DiJetHighMass/python/baseJets_cff.py
+++ CMGTools/DiJetHighMass/python/baseJets_cff.py
@@ -3,12 +3,15 @@ import FWCore.ParameterSet.Config as cms
 from CMGTools.Common.factories.cmgBaseJet_cfi import cmgBaseJet
 # dijet is not generic, no need for everybody to define it. Please put that in your cfg, or in a separate cff
 from CMGTools.Common.factories.cmgDiJet_cfi import cmgDiJet
+from CMGTools.Common.factories.cmgFatJet_cfi import cmgFatJet
+from CMGTools.Common.factories.cmgDiFatJet_cfi import cmgDiFatJet
 from CMGTools.Common.histograms.baseJetLorentzVector_cfi import baseJetLorentzVector
 from CMGTools.Common.skims.leadingCMGBaseJetSelector_cfi import leadingCMGBaseJetSelector
 
 patJet = 'selectedPatJets'
 baseJet = 'cmgBaseJet'
 baseLeadJet = 'cmgBaseleadjet'
+baseFatJet = 'cmgBaseFatJet'
 
 # Produce Base Jet
 cmgBaseJet.cfg.inputCollection = patJet
@@ -28,6 +31,14 @@ diBaseJetFactory = cms.PSet(
 cmgBaseDiJet = cmgDiJet.clone()
 cmgBaseDiJet.cfg = diBaseJetFactory
 
+cmgBaseFatJet = cmgFatJet.clone()
+cmgBaseFatJet.cfg.inputCollection = cms.InputTag(baseJet)
+
+
+cmgBaseDiFatJet = cmgDiFatJet.clone()
+cmgBaseDiFatJet.cfg.leg1Collection = cms.InputTag(baseFatJet)
+cmgBaseDiFatJet.cfg.leg2Collection = cms.InputTag(baseFatJet)
+
 jetBaseLorentzVector = baseJetLorentzVector.clone()
 jetBaseLorentzVector.inputCollection = baseJet
 
@@ -39,5 +50,7 @@ baseJetSequence = cms.Sequence(
     cmgBaseLeadJet +
     jetBaseLorentzVector +
     jetBaseLeadLorentzVector +
-    cmgBaseDiJet
+    cmgBaseDiJet +
+    cmgBaseFatJet +
+    cmgBaseDiFatJet
     )
