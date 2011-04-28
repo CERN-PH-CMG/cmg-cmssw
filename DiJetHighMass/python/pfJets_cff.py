fa623150b1d20a1f5fd0f08aeb87c15243ef5aa0
mgouzevi <>
2011-04-28 17:03:25 +0000

    Update for 4_1_3

--- DiJetHighMass/python/pfJets_cff.py
+++ DiJetHighMass/python/pfJets_cff.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from CMGTools.Common.factories.cmgPFJet_cfi import cmgPFJet
 # dijet is not generic, no need for everybody to define it. Please put that in your cfg, or in a separate cff
 from CMGTools.Common.factories.cmgDiJet_cfi import cmgDiJet
+from CMGTools.Common.factories.cmgFatJet_cfi import cmgFatJet
+from CMGTools.Common.factories.cmgDiFatJet_cfi import cmgDiFatJet
 from CMGTools.Common.histograms.pfJetLorentzVector_cfi import pfJetLorentzVector
 from CMGTools.Common.histograms.pfJetHistograms_cfi import pfJetHistograms
 from CMGTools.Common.skims.leadingCMGPFJetSelector_cfi import leadingCMGPFJetSelector
@@ -10,6 +12,7 @@ from CMGTools.Common.skims.leadingCMGPFJetSelector_cfi import leadingCMGPFJetSel
 patJet = 'selectedPatJets'
 pfJet = 'cmgPFJet'
 pfLeadJet = 'cmgPFLeadJet'
+pfFatJet = 'cmgPFFatJet'
 
 # Produce PF Base Jet
 cmgPFJet = cmgPFJet.clone()
@@ -30,6 +33,22 @@ diPFJetFactory = cms.PSet(
 cmgPFDiJet = cmgDiJet.clone()
 cmgPFDiJet.cfg = diPFJetFactory
 
+# Produce PFFatJets
+cmgFatJetFactory = cms.PSet(            
+    leg1Collection = cms.InputTag(pfLeadJet),
+    leg2Collection = cms.InputTag(pfLeadJet),
+    metCollection = cms.InputTag("cmgPFMET")
+    )
+
+cmgPFFatJet = cmgFatJet.clone()
+cmgPFFatJet.cfg.inputCollection = cms.InputTag(pfJet)
+
+
+cmgPFDiFatJet = cmgDiFatJet.clone()
+cmgPFDiFatJet.cfg.leg1Collection = cms.InputTag(pfFatJet)
+cmgPFDiFatJet.cfg.leg2Collection = cms.InputTag(pfFatJet)
+
+
 jetPFLorentzVector = pfJetLorentzVector.clone()
 jetPFLorentzVector.inputCollection = pfJet
 
@@ -49,5 +68,7 @@ pfJetSequence = cms.Sequence(
     jetPFLeadLorentzVector +
     jetPFHistograms +
     jetPFLeadHistograms +
-    cmgPFDiJet
+    cmgPFDiJet+
+    cmgPFFatJet+
+    cmgPFDiFatJet
     )
