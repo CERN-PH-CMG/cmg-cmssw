0f001429eb15df0e9c39d1e6061d367bf4470671
wreece <>
2011-05-06 12:57:06 +0000

    Adds first pass of TriggerObjectFactory

---
+++ Common/interface/TriggerObjectFactory.h
@@ -0,0 +1,38 @@
+#ifndef _CMGTools_CommonTools_TRIGGEROBJECTFACTORY_H_
+#define _CMGTools_CommonTools_TRIGGEROBJECTFACTORY_H_
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "AnalysisDataFormats/CMGTools/interface/TriggerObject.h"
+#include "CMGTools/Common/interface/Factory.h"
+#include "CMGTools/Common/interface/SettingTool.h"
+
+namespace cmg{
+
+class TriggerObjectFactory : public Factory<cmg::TriggerObject>{
+    public:
+        TriggerObjectFactory(const edm::ParameterSet& ps):
+            triggerResultsLabel_(ps.getParameter<edm::InputTag>("triggerResults")),
+            triggerObjectsLabel_(ps.getParameter<edm::InputTag>("triggerObjects")),
+            processName_(ps.getUntrackedParameter<std::string>("processName","")),
+            tagTriggerEvent_( "hltTriggerSummaryAOD" ){
+        }
+        virtual event_ptr create(const edm::Event&, const edm::EventSetup&) const;
+        
+        static std::string getProcessName(edm::Run&, const std::string&);
+        virtual bool beginRun(edm::Run&, edm::EventSetup const&);
+        
+    private:
+        const edm::InputTag triggerResultsLabel_;
+        const edm::InputTag triggerObjectsLabel_;
+        std::string processName_;
+        const std::string tagTriggerEvent_;
+        
+};
+
+}
+
+
+#endif /*_CMGTools_CommonTools_TRIGGEROBJECTFACTORY_H_*/
