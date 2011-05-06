fad8a25c03b2754445bafea70d294d09a5b6d20c
wreece <>
2011-05-06 11:59:51 +0000

    Fixes a bug in the eta selection

--- CMGTools/Common/python/selections/jetid_cfi.py
+++ CMGTools/Common/python/selections/jetid_cfi.py
@@ -2,13 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 # see for details https://twiki.cern.ch/twiki/bin/view/CMS/JetID
 
+#Selection thanks to Maxime
 looseJetId = cms.PSet(
+    h0Fraction = cms.string('component(5).fraction() < 0.99'),                      
     gammaFraction = cms.string('component(4).fraction() < 0.99'),
-    h0Fraction = cms.string('component(5).fraction() < 0.99'),
     nConstituents = cms.string('nConstituents() > 1'),
     #these only valid for abs(eta) < 2.4
     hFraction = cms.string('component(1).fraction() > 0 || abs(eta()) > 2.4'),
-    hChargedMultiplicity = cms.string('component(1).number() > 0 || abs(eta())'),
+    hChargedMultiplicity = cms.string('component(1).number() > 0 || abs(eta()) > 2.4'),
     eFraction = cms.string('component(2).fraction() < 0.99 || abs(eta()) > 2.4'),
     muFraction = cms.string('component(3).fraction() < 0.99 || abs(eta()) > 2.4'),
     )
