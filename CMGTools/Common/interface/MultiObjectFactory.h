7303dc6e2c05e57c27beb6c3f39998ee66c843a1
jhegeman <>
2011-05-06 11:38:50 +0000

    Properly treat cases in which the input collection is empty.

--- CMGTools/Common/interface/MultiObjectFactory.h
+++ CMGTools/Common/interface/MultiObjectFactory.h
@@ -64,15 +64,17 @@ cmg::MultiObjectFactory::create(edm::Event const& iEvent,
   edm::Handle<edm::View<reco::Candidate> > cands;
   iEvent.getByLabel(inputLabel_, cands);
 
-  std::vector<edm::Ptr<reco::Candidate> > tmp;
-  tmp.resize(cands->size());
-  for (size_t i = 0, n = cands->size(); i < n; ++i) {
-    tmp.at(i) = edm::Ptr<reco::Candidate>(cands, i);
-  }
+  if (cands->size() > 0) {
+    std::vector<edm::Ptr<reco::Candidate> > tmp;
+    tmp.resize(cands->size());
+    for (size_t i = 0, n = cands->size(); i < n; ++i) {
+      tmp.at(i) = edm::Ptr<reco::Candidate>(cands, i);
+    }
 
-  cmg::MultiObject object(tmp);
-  cmg::MultiObjectFactory::set(tmp, object);
-  result->push_back(object);
+    cmg::MultiObject object(tmp);
+    cmg::MultiObjectFactory::set(tmp, object);
+    result->push_back(object);
+  }
 
   return result;
 }
