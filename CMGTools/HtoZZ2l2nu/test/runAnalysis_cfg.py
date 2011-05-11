a74849305bf78c198a0ff0f8198f7140fb880a7e
psilva <>
2011-05-11 15:59:47 +0000

    adding public file

--- CMGTools/HtoZZ2l2nu/test/runAnalysis_cfg.py
+++ CMGTools/HtoZZ2l2nu/test/runAnalysis_cfg.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 runProcess = cms.PSet(
-    input = cms.string("/data/psilva/Higgs/DoubleMuon-v6.root"),
+    input = cms.string("/afs/cern.ch/user/p/psilva/public/DoubleMuon-v6.root"),
     evStart = cms.int32(0),
     evEnd = cms.int32(-1),
     dirName = cms.string("evAnalyzer/data"),
