221df398287bc3ff20d0f0bcfd88ef621f392238
jhegeman <>
2011-05-11 09:36:48 +0000

    Added RA1 event content.

--- CMGTools/Susy/python/susyEventContent_cff.py
+++ CMGTools/Susy/python/susyEventContent_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.eventContent.everything_cff import *
-from CMGTools.Susy.RA2.RA2EventContent_cff import *
 from CMGTools.Susy.RA1.RA1EventContent_cff import *
+from CMGTools.Susy.RA2.RA2EventContent_cff import *
 from CMGTools.Susy.Razor.razorEventContent_cff import *
 from CMGTools.Susy.LP.LPEventContent_cff import *
 
 susyEventContent = everything
+susyEventContent += RA1EventContent
 susyEventContent += RA2EventContent
 susyEventContent += razorEventContent
 susyEventContent += LPEventContent
