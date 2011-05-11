815f27a6e323a360c33a0ba12261ab8e129872d7
jhegeman <>
2011-05-11 12:47:47 +0000

    Various fixes to deltaPhiStar code, especially for hemispheres.

--- Common/interface/HemiFactory.h
+++ Common/interface/HemiFactory.h
@@ -9,12 +9,15 @@
 #include "AnalysisDataFormats/CMGTools/interface/CompoundTypes.h"
 #include "AnalysisDataFormats/CMGTools/interface/Hemisphere.h"
 #include "CMGTools/Common/interface/Factory.h"
+#include "CMGTools/Common/interface/SettingTool.h"
+#include "CMGTools/Common/interface/MultiObjectSettingTool.h"
 
 namespace cmg {
 
   typedef std::vector<edm::InputTag> VInputTag;
 
-  class HemisphereFactory : public Factory<cmg::Hemisphere> {
+  class HemisphereFactory : public Factory<cmg::Hemisphere>,
+    public SettingTool<std::vector<edm::Ptr<reco::Candidate> >, cmg::Hemisphere> {
 
   public:
     HemisphereFactory(const edm::ParameterSet& ps) :
@@ -25,12 +28,13 @@ namespace cmg {
     typedef cmg::Factory<cmg::Hemisphere>::event_ptr event_ptr;
     virtual event_ptr create(const edm::Event&,
                              const edm::EventSetup&) const;
+    void set(std::vector<edm::Ptr<reco::Candidate> > const& input,
+             cmg::Hemisphere& object) const;
 
   private:
-
     VInputTag const hemisphereLabel_;
-
     size_t maxNCand_;
+    MultiObjectSettingTool const multiObjectFactory_;
   };
 
 } // namespace cmg
