e631448830b2295fda54cd1513e05ace4535b2cd
wreece <>
2011-05-18 15:14:23 +0000

    Adds a tag for MC parton == 5

--- CMGTools/Common/python/selections/btaggedjet_cfi.py
+++ CMGTools/Common/python/selections/btaggedjet_cfi.py
@@ -5,12 +5,13 @@ import FWCore.ParameterSet.Config as cms
 #these working points are for 7 TeV
 
 trackCountingHighEffBJetTags = cms.PSet(
+    mcbparton = cms.string('abs(sourcePtr().partonFlavour()) == 5'),                                    
     loose = cms.string('btag() >= 1.7'),                                   
     medium = cms.string('btag() >= 3.3'),
     tight = cms.string('btag() >= 10.2')                                   
     )
 
-trackCountingHighPurBJetTags = cms.PSet(
+trackCountingHighPurBJetTags = trackCountingHighEffBJetTags.clone(
     loose = cms.string('btag() >= 1.19'),                                   
     medium = cms.string('btag() >= 1.93'),
     tight = cms.string('btag() >= 3.41')                                   
