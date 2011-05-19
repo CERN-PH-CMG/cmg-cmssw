03692af027a67b271ded7a1896604539d478eb52
wreece <>
2011-05-19 12:59:47 +0000

    Removes outdated and unsupported recipe

--- CMGTools/Common/python/release_info/CMSSW_4_2_2.py
+++
@@ -1,6 +0,0 @@
-{'UserCode/CMG/AnalysisDataFormats/CMGTools': ['HEAD', 'AnalysisDataFormats/CMGTools'],
- 'UserCode/CMG/CMGTools/Common':              ['HEAD', 'CMGTools/Common'],
- 'UserCode/CMG/CMGTools/RootTools':           ['HEAD', 'CMGTools/RootTools'],
- 'UserCode/CMG/CMGTools/ZmumuJetsTutorial':           ['HEAD', 'CMGTools/ZmumuJetsTutorial'],
- 'UserCode/RecoParticleFlow/PostProcessing':           ['Colin_May9', 'RecoParticleFlow/PostProcessing'],
-}
