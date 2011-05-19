d3e3e971870d372c62fe9fab3e8f6c3dcd682a27
wreece <>
2011-05-19 12:59:08 +0000

    Adds recipe from Colin

---
+++ Common/python/release_info/CMSSW_4_2_3_patch1.py
@@ -0,0 +1,7 @@
+{'UserCode/CMG/AnalysisDataFormats/CMGTools': ['Colin_May19', 'AnalysisDataFormats/CMGTools'],
+ 'UserCode/CMG/CMGTools/Common':              ['Colin_May19', 'CMGTools/Common'],
+ 'UserCode/CMG/CMGTools/RootTools':           ['HEAD', 'CMGTools/RootTools'],
+ 'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['HEAD', 'CMGTools/ZmumuJetsTutorial'],
+ 'PhysicsTools/PatAlgos':                     ['V08-06-22', 'PhysicsTools/PatAlgos'],
+ 'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_May19', 'RecoParticleFlow/PostProcessing'],
+}
