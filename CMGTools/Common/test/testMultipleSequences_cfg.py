d00f7dc919bfb70cac1b8c2efc0d398ee901743c
cbern <>
2011-05-25 13:50:45 +0000

    - clean up - reduced output file size by dropping metadata of processes prior this one (ANA). Gain: factor 2

--- CMGTools/Common/test/testMultipleSequences_cfg.py
+++ CMGTools/Common/test/testMultipleSequences_cfg.py
@@ -10,8 +10,9 @@ print sep_line
 
 process.setName_('ANA')
 
+runOnMC = False
+
 process.maxEvents = cms.untracked.PSet(
-#        input = cms.untracked.int32(-1)
         input = cms.untracked.int32(1000)
         )
 
@@ -24,19 +25,13 @@ process.source.fileNames = cms.untracked.vstring(
     'file:/afs/cern.ch/user/c/cbern/scratch0/patTuple_PF2PAT.root'
     )
 
-# process.load("CMGTools.Common.sources.relval.RelValQCD_FlatPt_15_3000.CMSSW_3_11_2.MC_311_V2.source_cff")
-# process.load("CMGTools.Common.sources.relval.RelValTTbar.CMSSW_3_11_2.MC_311_V2.source_cff")
-# process.source.fileNames = cms.untracked.vstring(
-#    'file:input.root'
-#    )
-# process.load("CMGTools.Common.sources.relval.RelValTTbar.CMSSW_3_11_2.MC_311_V2.source_cff")
 
 # output module for EDM event (ntuple)
 process.out.fileName = cms.untracked.string('tree_testMultipleSequences.root')
 from CMGTools.Common.eventContent.everything_cff import everything 
 process.out.outputCommands = cms.untracked.vstring( 'drop *')
 process.out.outputCommands.extend( everything ) 
-    
+process.out.dropMetaData = cms.untracked.string('PRIOR')
 
 #output file for histograms etc
 process.TFileService = cms.Service("TFileService",
@@ -46,25 +41,6 @@ process.TFileService = cms.Service("TFileService",
 # default analysis sequence    
 process.load('CMGTools.Common.analysis_cff')
 
-# now, we're going to tune the default analysis sequence to our needs
-# by modifying the parameters of the modules present in this sequence. 
-
-# Select events with 2 jet ...  
-# process.cmgPFJetCount.minNumber = 2
-# with pT > 50.
-# process.cmgPFJetSel.cut = "pt()>50"
-# and MET larger than 50
-# process.cmgPFMETSel.cut = "pt()>50"
-
-# note: we're reading ttbar events
-
-runStdPAT = False
-runOnMC = False
-
-# if not runStdPAT:
-#    process.analysisSequence.remove( process.caloJetSequence )
-#    process.analysisSequence.remove( process.caloMetSequence )
-
 if runOnMC:
     process.load("CMGTools.Common.runInfoAccounting_cfi")
     process.outpath += process.runInfoAccounting
@@ -72,21 +48,25 @@ if runOnMC:
 from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
 from CMGTools.Common.Tools.visitorUtils import replacePostfix
 
+print 'preparing sequence AK5LCCMG'
 
-cloneProcessingSnippet(process, getattr(process, 'analysisSequence'), 'AK5LC')
-replacePostfix(getattr(process,"analysisSequenceAK5LC"),'AK5','AK5LC') 
+cloneProcessingSnippet(process, getattr(process, 'analysisSequence'), 'AK5LCCMG')
+replacePostfix(getattr(process,"analysisSequenceAK5LCCMG"),'AK5','AK5LC') 
 
-cloneProcessingSnippet(process, getattr(process, 'analysisSequence'), 'AK7')
-replacePostfix(getattr(process,"analysisSequenceAK7"),'AK5','AK7') 
+print 'preparing sequence AK7CMG'
+
+cloneProcessingSnippet(process, getattr(process, 'analysisSequence'), 'AK7CMG')
+replacePostfix(getattr(process,"analysisSequenceAK7CMG"),'AK5','AK7') 
 
 from CMGTools.Common.Tools.tuneCMGSequences import * 
-tuneCMGSequences(process)
+tuneCMGSequences(process, postpostfix='CMG')
+
+print 'running'
 
 process.p = cms.Path(
-    # process.analysisSequence + 
     process.analysisSequence
-    + process.analysisSequenceAK5LC
-    + process.analysisSequenceAK7
+    + process.analysisSequenceAK5LCCMG
+    + process.analysisSequenceAK7CMG
 )
 
 process.GlobalTag.globaltag = cms.string(getGlobalTag(runOnMC))
