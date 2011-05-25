fa25d99b9f59a16a4873cab99a673507b46084b3
cbern <>
2011-05-25 16:25:43 +0000

    dropping patTaus, otherwise crash

--- CMGTools/Common/python/eventContent/everything_cff.py
+++ CMGTools/Common/python/eventContent/everything_cff.py
@@ -9,6 +9,7 @@ from CMGTools.Common.eventContent.runInfoAccounting_cff import runInfoAccounting
 patObjects = cms.untracked.vstring(
     'keep patMuons_selectedPat*_*_*',
     'keep patElectrons_selectedPat*_*_*',
+    'drop patTaus_selectedPat*_*_*',
     'keep cmgPhotons_selectedPat*_*_*'
     )
 
