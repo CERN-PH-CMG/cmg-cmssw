821d32e745ef43456547071e800940fbc400f6c6
cbern <>
2011-05-26 13:36:53 +0000

    - added PF cleaning. cleaning products in the event - added offlinePrimaryVertices to CMG tuple

---
+++ CMGTools/Common/python/eventContent/eventCleaning_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoParticleFlow.PostProcessing.pfPostProcessingEventContent_cff import pfPostProcessingEventContent
+
+
+eventCleaning = pfPostProcessingEventContent
+eventCleaning += cms.untracked.vstring('keep *_HBHENoiseFilterResultProducer_*_*')
+
