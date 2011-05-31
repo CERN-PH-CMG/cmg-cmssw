0d03aa488f22e6fb1e4a98fe76d2df607b150be1
cbern <>
2011-05-31 09:38:07 +0000

    - added a script - added trigger results to the CMG output

--- Common/python/eventContent/trigger_cff.py
+++ Common/python/eventContent/trigger_cff.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 trigger = cms.untracked.vstring(
-    'keep cmgTriggerObjects_cmgTriggerObjectSel*_*_*'                                   
+    'keep cmgTriggerObjects_cmgTriggerObjectSel*_*_*',
+    'keep *_TriggerResults_*_ANA',
+    'keep *_TriggerResults_*_PAT',
     )
 
