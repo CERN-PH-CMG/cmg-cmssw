03bb0c9146cfeaa12931da2326461d3bf63e3df0
jhegeman <>
2011-06-01 10:29:23 +0000

    Properly split header and src for MultiObjectFactory.

--- CMGTools/Common/interface/MultiObjectFactory.h
+++ CMGTools/Common/interface/MultiObjectFactory.h
@@ -5,20 +5,12 @@
 #include "CMGTools/Common/interface/SettingTool.h"
 #include "CMGTools/Common/interface/MultiObjectSettingTool.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
-#include "AnalysisDataFormats/CMGTools/interface/UnSet.h"
 #include "AnalysisDataFormats/CMGTools/interface/MultiObject.h"
-#include "DataFormats/Common/interface/View.h"
-#include "DataFormats/Math/interface/deltaPhi.h"
-#include "DataFormats/Math/interface/Vector.h"
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
-
-#include <limits>
-#include <cmath>
 
 //--------------------------------------------------------------------
 
@@ -47,45 +39,6 @@ namespace cmg {
   }; // class MultiObjectFactory
 
 } // namespace cmg
-
-//--------------------------------------------------------------------
-
-cmg::MultiObjectFactory::event_ptr
-cmg::MultiObjectFactory::create(edm::Event const& iEvent,
-                                edm::EventSetup const& iSetup) const
-{
-  typedef std::vector<cmg::MultiObject> collection;
-  cmg::MultiObjectFactory::event_ptr result(new collection);
-
-  // There really is not much to be done here, create a multi-object
-  // from the stuff in the input collection, then fill the few things
-  // in it and done.
-
-  edm::Handle<edm::View<reco::Candidate> > cands;
-  iEvent.getByLabel(inputLabel_, cands);
-
-  if (cands->size() > 0) {
-    std::vector<edm::Ptr<reco::Candidate> > tmp;
-    tmp.resize(cands->size());
-    for (size_t i = 0, n = cands->size(); i < n; ++i) {
-      tmp.at(i) = edm::Ptr<reco::Candidate>(cands, i);
-    }
-
-    cmg::MultiObject object(tmp);
-    cmg::MultiObjectFactory::set(tmp, object);
-    result->push_back(object);
-  }
-
-  return result;
-}
-
-//--------------------------------------------------------------------
-
-void cmg::MultiObjectFactory::set(std::vector<edm::Ptr<reco::Candidate> >
-                                  const& input, cmg::MultiObject& object) const
-{
-  multiObjectFactory_.set(input, &object);
-}
 
 //--------------------------------------------------------------------
 
