906bb51d00039502586bcff008c49ccc356da2df
cbern <>
2011-06-10 11:45:25 +0000

    new recipe

--- Common/python/release_info/CMSSW_4_2_3_exp_colin_june10.py
+++ Common/python/release_info/CMSSW_4_2_3_exp_colin_june10.py
@@ -1,9 +1,10 @@
-# https://twiki.cern.ch/twiki/bin/view/CMS/CMGToolsReleasesExperimental#Colin_May_31
+# https://twiki.cern.ch/twiki/bin/view/CMS/CMGToolsReleasesExperimental#Colin_June10
 {
-    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['Colin_June8', 'AnalysisDataFormats/CMGTools'],
-    'UserCode/CMG/CMGTools/Common':              ['Colin_June8', 'CMGTools/Common'],
+    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['Colin_Scripts', 'AnalysisDataFormats/CMGTools'],
+    'UserCode/CMG/CMGTools/Common':              ['Colin_Scripts', 'CMGTools/Common'],
     'UserCode/CMG/CMGTools/RootTools':           ['Colin_May25_MultSeqs', 'CMGTools/RootTools'],
-    'UserCode/CMG/CMGTools/Susy':                ['Colin_May31_revived', 'CMGTools/Susy'],
+    'UserCode/CMG/CMGTools/Susy':                ['Lucie_June10', 'CMGTools/Susy'],
+    'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['Colin_Scripts', 'CMGTools/ZmumuJetsTutorial'],
     'CommonTools/ParticleFlow':                  ['Colin_NoRhoOption', 'CommonTools/ParticleFlow'],
     'DataFormats/PatCandidates' :                ['V06-04-11', 'DataFormats/PatCandidates'],
     'PhysicsTools/PatAlgos':                     ['V08-06-28',   'PhysicsTools/PatAlgos'],
@@ -12,5 +13,4 @@
     'RecoJets/JetAlgorithms' :                   ['V04-01-00', 'RecoJets/JetAlgorithms'],
     'RecoJets/JetProducers' :                    ['V05-05-03', 'RecoJets/JetProducers'],
     'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June8', 'RecoParticleFlow/PostProcessing'],
-    'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['Colin_June8', 'CMGTools/ZmumuJetsTutorial'],
 }
