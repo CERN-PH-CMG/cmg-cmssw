e5de86d8936cf955d01b9268a5659c5b9f1b041a
psilva <>
2011-06-13 20:46:46 +0000

    adding other files

--- CMGTools/HtoZZ2l2nu/python/OutputConfiguration_cff.py
+++ CMGTools/HtoZZ2l2nu/python/OutputConfiguration_cff.py
@@ -8,27 +8,27 @@ def configureOutput(process,selPaths=['eePath', 'mumuPath', 'emuPath'],outFile='
     process.load('Configuration.EventContent.EventContent_cff')
     process.out.SelectEvents=cms.untracked.PSet( SelectEvents = cms.vstring(selPaths) )
     process.out.fileName = cms.untracked.string(outFile)
-    process.out.outputCommands = cms.untracked.vstring('drop *',
-                                                       'keep *_prunedGen_*_*',
-                                                       'keep *_genMetTrue_*_*',
-                                                       'keep edmMergeableCounter_*_*_*',
-                                                       'keep *_eeCandidates_*_*',
-                                                       'keep *_mumuCandidates_*_*',
-                                                       'keep *_emuCandidates_*_*',
-                                                       'keep *_offlinePrimaryVertices_*_*',
-                                                       'keep *_offlineBeamSpot_*_*',
-                                                       'keep *_TriggerResults_*_*',
-                                                       'keep *_addPileupInfo_*_*',
-                                                       'keep *_genEventScale_*_*',
-                                                       'keep GenRunInfoProduct_*_*_*',
-                                                       'keep GenEventInfoProduct_*_*_*',
-                                                       'keep *_flavorHistoryFilter_*_*',
-                                                       'keep recoGenJets_ak5GenJets_*_*',
-                                                       'keep double*_*_rho_'+process.name_(),
-                                                       'keep double*_*_sigma_'+process.name_(),
-                                                       'keep recoPFCandidates_particleFlow_*_*',
-                                                       )
-    process.out.outputCommands.extend( patEventContentNoCleaning )
-    process.out.outputCommands.extend( patTriggerStandAloneEventContent )
+    outputCmds=['drop *',
+                'keep *_prunedGen_*_*',
+                'keep *_genMetTrue_*_*',
+                'keep edmMergeableCounter_*_*_*',
+                'keep *_eeCandidates_*_*',
+                'keep *_mumuCandidates_*_*',
+                'keep *_emuCandidates_*_*',
+                'keep *_offlinePrimaryVertices_*_*',
+                'keep *_offlineBeamSpot_*_*',
+                'keep *_TriggerResults_*_*',
+                'keep *_addPileupInfo_*_*',
+                'keep *_genEventScale_*_*',
+                'keep GenRunInfoProduct_*_*_*',
+                'keep GenEventInfoProduct_*_*_*',
+                'keep *_flavorHistoryFilter_*_*',
+                'keep recoGenJets_ak5GenJets_*_*',
+                'keep double*_*_rho_'+process.name_(),
+                'keep double*_*_sigma_'+process.name_(),
+                'keep recoPFCandidates_particleFlow_*_*']
+    outputCmds.extend( patEventContentNoCleaning )
+    outputCmds.extend( patTriggerStandAloneEventContent )
+    process.out.outputCommands = outputCmds
 
 
