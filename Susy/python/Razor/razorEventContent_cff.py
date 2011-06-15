c638676a442a127194a3dff82b3d04db5c18040e
wreece <>
2011-06-15 15:00:59 +0000

    Tunes the Razor event selection

--- Susy/python/Razor/razorEventContent_cff.py
+++ Susy/python/Razor/razorEventContent_cff.py
@@ -4,4 +4,11 @@ from CMGTools.Common.eventContent.everything_cff import *
 
 razorEventContent = cms.untracked.vstring()
 
-razorEventContent += cms.untracked.vstring('keep *_razor*_*_*')
+razorEventContent += cms.untracked.vstring(
+                                           #'keep *_razor*_*_*'
+                                           'keep *_razorLeading*_*_*',
+                                           'keep cmgBaseMETs_razor*Met_*_*',
+                                           'keep cmgHemispheres_razorHemi*Box_*_*',
+                                           'keep cmgHemispherecmgHemispherecmgDiObjects_razorDiHemi*Box_*_*',
+                                           'keep cmgTriggerObjects_razorTriggerSel_*_*'
+                                           )
