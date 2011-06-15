e9dde5a52c78c9d2ed313f36d926b80442e3e1df
cbern <>
2011-06-15 14:38:59 +0000

    uncommented all relvals

--- CMGTools/Common/prod/myRelvalList_4_2.py
+++ CMGTools/Common/prod/myRelvalList_4_2.py
@@ -12,11 +12,10 @@ from relvalDefinition import *
 
 relvals = relvalList()
 
-
 relvals.add('/RelValZMM/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValZEE/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValTTbar/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValQCD_FlatPt_15_3000/CMSSW_4_2_3-MC_42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValZTT/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValLM1_sfts/CMSSW_4_2_3-MC_42_V12-v2/GEN-SIM-RECO')
-# relvals.add('/RelValH130GGgluonfusion/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValZEE/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValTTbar/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValQCD_FlatPt_15_3000/CMSSW_4_2_3-MC_42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValZTT/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValLM1_sfts/CMSSW_4_2_3-MC_42_V12-v2/GEN-SIM-RECO')
+relvals.add('/RelValH130GGgluonfusion/CMSSW_4_2_3-START42_V12-v2/GEN-SIM-RECO')
