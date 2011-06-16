cafe9f68a80b9b74b08562a0238ef329fa466ede
cbern <>
2011-06-16 10:13:05 +0000

    removed the TP filter until it works in tagging mode

--- CMGTools/Common/python/eventCleaning/eventCleaning_cff.py
+++ CMGTools/Common/python/eventCleaning/eventCleaning_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoParticleFlow.PostProcessing.selectGoodPFEvents_cff import *
 
+# HCAL noise filter
+
 from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import *
 
 # see https://hypernews.cern.ch/HyperNews/CMS/get/JetMET/1196.html
@@ -9,6 +11,15 @@ HBHENoiseFilterResultProducer.minIsolatedNoiseSumE = 999999.
 HBHENoiseFilterResultProducer.minNumIsolatedNoiseChannels = 999999
 HBHENoiseFilterResultProducer.minIsolatedNoiseSumEt = 999999.
 
+# ECAL missing channels
+
+# TP filter
+
+from JetMETAnalysis.ecalDeadCellTools.RA2TPfilter_cff import *
+
+ecalDeadCellTaggingSequence = cms.Sequence(
+    ecalDeadCellTPfilter
+    )
 
 # currently running:
 #  - PF inconsistent muon filter
@@ -17,9 +28,10 @@ eventCleaningSequence = cms.Sequence(
     # selectGoodPFEventsSequence +
     # note: the following will produce a boolean in the EDM but
     # does not filter events
-    #COLIN : good idea! should do the same for PF filters
     HBHENoiseFilterResultProducer +
     selectGoodPFEventsTaggingSequence
+    #COLIN: uncomment the following when the filter is running in tagging mode
+    # + ecalDeadCellTaggingSequence
     )
 
 # importing paths and output modules to select bad events in RECOSIM format
