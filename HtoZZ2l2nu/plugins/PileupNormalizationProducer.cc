47220058c21b209ed7c8a84dbd999c9feec79b7c
psilva <>
2011-06-27 20:29:47 +0000

    fixing

--- HtoZZ2l2nu/plugins/PileupNormalizationProducer.cc
+++ HtoZZ2l2nu/plugins/PileupNormalizationProducer.cc
@@ -17,9 +17,7 @@ public:
   virtual void produce( edm::Event &iEvent, const edm::EventSetup &iSetup) ;
 
 private:
-  std::map<std::string, edm::ParameterSet > objConfig;
-  bool useVertexDistribution_;
-  TH1D *vertexDataH_, *puUnfoldedH_;
+  TH1D *puUnfoldedH_;
 };
 
 
@@ -28,78 +26,18 @@ using namespace std;
 
 //
 PileupNormalizationProducer::PileupNormalizationProducer(const edm::ParameterSet &iConfig)
-  : useVertexDistribution_( iConfig.getParameter<bool>("useVertexDistribution") )
 {
   produces<float>("puWeight");
 
-  objConfig["Vertices"]= iConfig.getParameter<edm::ParameterSet>("Vertices");
-
-  //from observed vertex distribution ratio
-  vertexDataH_ = new TH1D("vertex_data","data;Vertices;Events",25,0.,25.);
-  vertexDataH_->SetBinContent(2,0.0338308);
-  vertexDataH_->SetBinContent(3,0.121082);
-  vertexDataH_->SetBinContent(4,0.203664);
-  vertexDataH_->SetBinContent(5,0.202303);
-  vertexDataH_->SetBinContent(6,0.158852);
-  vertexDataH_->SetBinContent(7,0.10762);
-  vertexDataH_->SetBinContent(8,0.072571);
-  vertexDataH_->SetBinContent(9,0.0408767);
-  vertexDataH_->SetBinContent(10,0.0278872);
-  vertexDataH_->SetBinContent(11,0.0269992);
-  vertexDataH_->SetBinContent(12,0.00431492);
-
-  puUnfoldedH_ = new TH1D("unfold_data","Unfolded data;Pileup;Events",25,0.,25.);
-  /*
-  //from unfolded vertex disribution
-  puUnfoldedH_->SetBinContent(1,0.136598);
-  puUnfoldedH_->SetBinContent(2,0.548235);
-  puUnfoldedH_->SetBinContent(3,1.26893);
-  puUnfoldedH_->SetBinContent(4,1.9627);
-  puUnfoldedH_->SetBinContent(5,2.10484);
-  puUnfoldedH_->SetBinContent(6,1.99065);
-  puUnfoldedH_->SetBinContent(7,1.73487);
-  puUnfoldedH_->SetBinContent(8,1.28593);
-  puUnfoldedH_->SetBinContent(9,1.00456);
-  puUnfoldedH_->SetBinContent(10,0.685606);
-  puUnfoldedH_->SetBinContent(11,0.508796);
-  puUnfoldedH_->SetBinContent(12,0.346366);
-  puUnfoldedH_->SetBinContent(13,0.236133);
-  puUnfoldedH_->SetBinContent(14,0.183413);
-  puUnfoldedH_->SetBinContent(15,0.0938161);
-  puUnfoldedH_->SetBinContent(16,0.102228);
-  puUnfoldedH_->SetBinContent(17,0.0537016);
-  puUnfoldedH_->SetBinContent(18,0.0273493);
-  puUnfoldedH_->SetBinContent(19,0.0240031);
-  puUnfoldedH_->SetBinContent(20,0.017485);
-  puUnfoldedH_->SetBinContent(21,0.0256616);
-  puUnfoldedH_->SetBinContent(22,0.00848088);
-  puUnfoldedH_->SetBinContent(23,0.0200775);
-  puUnfoldedH_->SetBinContent(24,0.035064);
-  puUnfoldedH_->SetBinContent(25,0.00959897);
-  */
-
-  //from estimatePileup.py
-  puUnfoldedH_->SetBinContent(1,0.29513);
-  puUnfoldedH_->SetBinContent(2,0.66352);
-  puUnfoldedH_->SetBinContent(3,1.57694);
-  puUnfoldedH_->SetBinContent(4,2.12325);
-  puUnfoldedH_->SetBinContent(5,2.58587);
-  puUnfoldedH_->SetBinContent(6,2.33852);
-  puUnfoldedH_->SetBinContent(7,1.84959);
-  puUnfoldedH_->SetBinContent(8,1.16579);
-  puUnfoldedH_->SetBinContent(9,0.630877);
-  puUnfoldedH_->SetBinContent(10,0.327577);
-  puUnfoldedH_->SetBinContent(11,0.158036);
-  puUnfoldedH_->SetBinContent(12,0.0816001);
-  puUnfoldedH_->SetBinContent(13,0.0373275);
-  puUnfoldedH_->SetBinContent(14,0.0180187);
-  puUnfoldedH_->SetBinContent(15,0.00976187);
-  puUnfoldedH_->SetBinContent(16,0.00472464);
-  puUnfoldedH_->SetBinContent(17,0.001945);
-  puUnfoldedH_->SetBinContent(18,0.000752556);
-  puUnfoldedH_->SetBinContent(19,0.000425988);
-  puUnfoldedH_->SetBinContent(20,0.000692033);
-
+  std::vector<double> weights= iConfig.getParameter<std::vector<double> >("normalizationDistribution");
+  puUnfoldedH_ = new TH1D("puunfoldedweights",";#Pileup;Weight",30,0,30);
+  cout << "pileup re-weighting:  {";
+  for(size_t iwgt=0; iwgt<weights.size(); iwgt++)
+    {
+      cout << weights[iwgt] << ";";
+      puUnfoldedH_->SetBinContent(iwgt+1,weights[iwgt]);
+    }
+  cout << "};" << endl;
 }
 
 //
@@ -110,10 +48,7 @@ void PileupNormalizationProducer::produce(edm::Event &iEvent, const edm::EventSe
 
   auto_ptr<float> puWeight(new float);
 
-  if(iEvent.isRealData())
-    {
-      *puWeight=1.0;
-    }
+  if(iEvent.isRealData())  *puWeight=1.0;
   else
     {
 
@@ -130,24 +65,9 @@ void PileupNormalizationProducer::produce(edm::Event &iEvent, const edm::EventSe
 	    }
 	}
 
-      //determine normalization
-      if(!useVertexDistribution_)
-	{
-	  float weight = puUnfoldedH_->GetBinContent( puUnfoldedH_->GetXaxis()->FindBin(npuevents) );
-	  *puWeight=weight;
-	}
-      else if(useVertexDistribution_)
-	{
-	  Handle<reco::VertexCollection> hVtx;
-	  iEvent.getByLabel(objConfig["Vertices"].getParameter<edm::InputTag>("source"), hVtx);
-	  std::vector<reco::VertexRef> selVertices = vertex::filter(hVtx,objConfig["Vertices"]);
-	  float weight = vertexDataH_->GetBinContent( selVertices.size()+1 );
-	  *puWeight=weight;
-	}
-      else
-	{
-	  *puWeight=1.0;
-	}
+      float weight = puUnfoldedH_->GetBinContent( puUnfoldedH_->GetXaxis()->FindBin(npuevents) );
+      *puWeight=weight;
+
     }
 
   iEvent.put(puWeight,"puWeight");
--- HtoZZ2l2nu/python/PileupNormalizationProducer_cfi.py
+++ HtoZZ2l2nu/python/PileupNormalizationProducer_cfi.py
@@ -1,7 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-from CMGTools.HtoZZ2l2nu.StandardSelections_cfi import *
+##
+## pileup normalization scenarios
+##
+def getNormalizationForScenario(scenario="Spring11"):
+    if(scenario=="Spring11") :
+        return cms.vdouble(0.29513,0.66352,1.57694,2.12325,2.58587,2.33852,1.84959,1.16579,0.630877,
+                                     0.327577,0.158036,0.0816001,0.0373275,0.0180187,0.00976187,0.00472464,0.001945,0.000752556,
+                                     0.000425988,0.000692033)
+    else :
+        return cms.vdouble(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    
+
 puWeights = cms.EDProducer("PileupNormalizationProducer",
-                           Vertices = BaseVertexSelection.clone(),
-                           useVertexDistribution = cms.bool(False)
+                           normalizationDistribution = getNormalizationForScenario("Spring11")
                            )
