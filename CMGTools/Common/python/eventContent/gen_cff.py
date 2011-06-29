4c2b0d5bdf30b8eb055660da5a4194b5abed436a
cbern <>
2011-06-29 14:40:49 +0000

    added pileUpInfo

--- CMGTools/Common/python/eventContent/gen_cff.py
+++ CMGTools/Common/python/eventContent/gen_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 gen = cms.untracked.vstring(
-    'keep recoGenParticles_genParticlesStatus3_*_*'                                   
+    'keep recoGenParticles_genParticlesStatus3_*_*',
+    'keep *_addPileupInfo_*_*',
     )
 
