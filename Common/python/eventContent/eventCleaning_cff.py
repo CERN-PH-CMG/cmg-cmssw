dbdcf93cdce968a10917313ca47f226e752d0b23
cbern <>
2011-06-30 11:35:54 +0000

    developments on the cleaning side. Still need to test

--- Common/python/eventContent/eventCleaning_cff.py
+++ Common/python/eventContent/eventCleaning_cff.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoParticleFlow.PostProcessing.pfPostProcessingEventContent_cff import pfPostProcessingEventContent
 
-
 eventCleaning = pfPostProcessingEventContent
 eventCleaning += cms.untracked.vstring(
-    'keep *_HBHENoiseFilterResultProducer_*_*',
+    'keep *_ecalDeadCellTPfilter_*_*',
+    'keep *_HBHENoiseFilterResultProducer*_*_*',
     'keep *_BeamHaloSummary_*_*'
     )
 
