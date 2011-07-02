4ce0c1108b756861f990bd213ae76779b81f0315
psilva <>
2011-07-02 21:51:10 +0000

    more stuff

--- HtoZZ2l2nu/src/TriggerFilter.cc
+++ HtoZZ2l2nu/src/TriggerFilter.cc
@@ -13,15 +13,15 @@ namespace trigger
     std::vector<std::string> triggerPaths = iConfig.getParameter<std::vector<std::string> >("triggerPaths");
 
     const pat::TriggerObjectStandAloneCollection &coll = muon->triggerObjectMatches();
-    cout << muon->pt() << " | ";
+    cout << muon->pt() << " | " << coll.size() ;
     for(pat::TriggerObjectStandAloneCollection::const_iterator objIt = coll.begin();
 	objIt != coll.end(); objIt++)
       {
 	std::vector< std::string > pathNames = objIt->filterLabels();
 	for(std::vector< std::string >::iterator pIt = pathNames.begin(); pIt != pathNames.end(); pIt++)
 	  cout << *pIt << " ";
-	cout << endl;
       }
+    cout << endl;
 
     return false;
   }
