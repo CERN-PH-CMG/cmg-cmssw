76789a01fa9962703cab27638f8e29576f1e6835
psilva <>
2011-07-03 21:56:27 +0000

    adding puffo met

--- HtoZZ2l2nu/src/TriggerFilter.cc
+++ HtoZZ2l2nu/src/TriggerFilter.cc
@@ -1,27 +1,39 @@
 #include "CMGTools/HtoZZ2l2nu/interface/ObjectFilters.h"
 #include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "PhysicsTools/PatUtils/interface/TriggerHelper.h"
 
 using namespace std;
 
 namespace trigger
 {
   //
-  bool isTriggerCandidate(const pat::Muon *muon, const edm::ParameterSet &iConfig)
+  bool isTriggerCandidate(edm::Event &iEvent, const edm::ParameterSet &iConfig)
   {
 
     //trigger paths and objects
     std::vector<std::string> triggerPaths = iConfig.getParameter<std::vector<std::string> >("triggerPaths");
 
-    const pat::TriggerObjectStandAloneCollection &coll = muon->triggerObjectMatches();
-    cout << muon->pt() << " | " << coll.size() ;
-    for(pat::TriggerObjectStandAloneCollection::const_iterator objIt = coll.begin();
-	objIt != coll.end(); objIt++)
+    // PAT trigger event
+    edm::Handle< pat::TriggerEvent > triggerEvent;
+    iEvent.getByLabel( "patTriggerEvent", triggerEvent );
+
+    // PAT object collection
+    edm::Handle< pat::MuonCollection > muons;
+    iEvent.getByLabel( "selectedPatMuonsPFlow" , muons );
+
+    // PAT trigger helper for trigger matching information
+    const pat::helper::TriggerMatchHelper matchHelper;
+
+    // loop over muon references (PAT muons have been used in the matcher in task 3)
+    for( size_t iMuon=0; iMuon<muons->size(); ++iMuon)
       {
-	std::vector< std::string > pathNames = objIt->filterLabels();
-	for(std::vector< std::string >::iterator pIt = pathNames.begin(); pIt != pathNames.end(); pIt++)
-	  cout << *pIt << " ";
+	const pat::TriggerObjectRef trigRef( matchHelper.triggerMatchObject( muons, iMuon, "muTriggerMatchPF", iEvent, *triggerEvent ) );
+	if ( trigRef.isAvailable() )
+	  {
+	    //cout << "found for muon=" << iMuon << endl;
+	  }
       }
-    cout << endl;
+    
 
     return false;
   }
