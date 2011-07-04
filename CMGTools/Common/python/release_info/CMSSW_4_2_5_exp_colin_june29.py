c4b0bfeb48fb3bb7bd352f0cf39b12bd7e6fb0fd
cbern <>
2011-07-04 14:04:52 +0000

    updated release info

--- CMGTools/Common/python/release_info/CMSSW_4_2_5_exp_colin_june29.py
+++ CMGTools/Common/python/release_info/CMSSW_4_2_5_exp_colin_june29.py
@@ -1,7 +1,7 @@
 # See https://savannah.cern.ch/task/index.php?20954
 {
     'UserCode/CMG/AnalysisDataFormats/CMGTools': ['Colin_JetIdTopProj_June29', 'AnalysisDataFormats/CMGTools'],
-    'UserCode/CMG/CMGTools/Common':              ['Colin_Jul1', 'CMGTools/Common'],
+    'UserCode/CMG/CMGTools/Common':              ['Colin_July4', 'CMGTools/Common'],
     'UserCode/CMG/CMGTools/RootTools':           ['Colin_Jul1', 'CMGTools/RootTools'],
     'UserCode/CMG/CMGTools/Susy':                ['Colin_Jul1', 'CMGTools/Susy'],
     'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['Colin_Scripts', 'CMGTools/ZmumuJetsTutorial'],
@@ -10,15 +10,13 @@
     'DataFormats/PatCandidates' :                ['V06-04-18', 'DataFormats/PatCandidates'],
     'FWCore/GuiBrowsers' :                       ['V00-00-56', 'FWCore/GuiBrowsers'],
     'JetMETAnalysis/ecalDeadCellTools':          ['Colin_TaggingMode_June30','JetMETAnalysis/ecalDeadCellTools'],
-    'PhysicsTools/PatAlgos':                     ['V08-06-36',   'PhysicsTools/PatAlgos'],
-    'PhysicsTools/PatExamples' :                 ['V00-05-17', 'PhysicsTools/PatExamples'],
+    'PhysicsTools/PatAlgos':                     ['V08-06-38',   'PhysicsTools/PatAlgos'],
+    'PhysicsTools/PatExamples' :                 ['V00-05-21', 'PhysicsTools/PatExamples'],
     'PhysicsTools/SelectorUtils' :               ['V00-03-17', 'PhysicsTools/SelectorUtils'],
     'RecoJets/Configuration' :                   ['V02-04-17', 'RecoJets/Configuration'],
-    'RecoJets/JetAlgorithms' :                   ['V04-01-00', 'RecoJets/JetAlgorithms'],
-    'RecoJets/JetProducers' :                    ['V05-05-03', 'RecoJets/JetProducers'],
     'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June23', 'RecoParticleFlow/PostProcessing'],
-    'RecoTauTag/Configuration' :                 ['V01-02-01', 'RecoTauTag/Configuration'],
-    'RecoTauTag/RecoTau' :                       ['V01-02-01', 'RecoTauTag/RecoTau'],
+    'RecoTauTag/Configuration' :                 ['V01-02-02', 'RecoTauTag/Configuration'],
+    'RecoTauTag/RecoTau' :                       ['V01-02-06', 'RecoTauTag/RecoTau'],
     'RecoTauTag/TauTagTools' :                   ['V01-02-00', 'RecoTauTag/TauTagTools'],
 }
 
