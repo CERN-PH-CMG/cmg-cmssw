5d6c723e3489e648991e81fe449ae4d6d9138d80
jhegeman <>
2011-07-06 13:49:14 +0000

    Fixes for tutorial for 4.2.3.

--- ZmumuJetsTutorial/prod/cmgTuple_cfg.py
+++ ZmumuJetsTutorial/prod/cmgTuple_cfg.py
@@ -1,69 +1,114 @@
-from PhysicsTools.PatAlgos.patTemplate_cfg import *
+######################################################################
+## Main cmsRun configuration file for the CMGTools tutorial.
+## See also:
+##   https://twiki.cern.ch/twiki/bin/view/CMS/CMGToolsTutorial
+######################################################################
+
 import FWCore.ParameterSet.Config as cms
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from CMGTools.Common.eventContent.everything_cff import everything
 from CMGTools.Common.Tools.getGlobalTag import getGlobalTag
+from CMGTools.Common.Tools.tuneCMGSequences import removeObject
+from CMGTools.Common.physicsObjectPrinter_cfi import physicsObjectPrinter
 
-import os.path
+##########
 
-runOnMC = True
+sep_line = "-" * 50
+print
+print sep_line
+print "CMGTools 'ZmumuJets' tutorial"
+print sep_line
+
+##########
+
+process.setName_("CMG")
+
+# Pick up the SingleMu data files.
+process.load("CMGTools.Common.sources.SingleMu.Run2011A_May10ReReco_v1.AOD.PAT_CMG_V2.source_PAT_cff")
+
+# One can limit the number of files to avoid some of the CASTOR
+# overhead (i.e., faster start-up).
+nFilesMax = 10
+print "WARNING: Restricting input to the first %d files (of the %d found)" % \
+      (nFilesMax, len(process.source.fileNames))
+if len(process.source.fileNames) > nFilesMax:
+    process.source.fileNames = process.source.fileNames[:nFilesMax - 1]
+
+print "Will process the following files:"
+for fileName in process.source.fileNames:
+    print "  %s" % fileName
+
+# One can limit the number of events to process.
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5000)
+    )
+tmp = process.maxEvents.input.value()
+if tmp > -1:
+    print "Will process at most %d events" % tmp
+
+# One can limit the number of lumi-sections to process.
+process.maxLuminosityBlocks = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+    )
+tmp = process.maxLuminosityBlocks.input.value()
+if tmp > -1:
+    print "Will process at most %d lumi-sections" % tmp
+
+# Output module for EDM events (ntuple).
+process.out.fileName = cms.untracked.string("tree_tutorial.root")
+process.out.outputCommands = cms.untracked.vstring("drop *")
+process.out.outputCommands.extend(everything)
+process.out.dropMetaData = cms.untracked.string("PRIOR")
+
+# Output file for histograms etc.
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("histograms_tutorial.root"))
+
+# Load the default analysis sequence.
+process.load("CMGTools.Common.analysis_cff")
+
+##########
+
+# Now tune the default analysis sequence to our needs by modifying the
+# parameters of the modules present in this sequence.
+
+# Switch on production of trigger objects.
+process.cmgTriggerObject.cfg.useTriggerObjects = cms.untracked.bool(True)
+
+# Remove the taus, as PFCandidate embedding does not work, hence an
+# exception in the cmg Tau factory.
+removeObject(process, "tau", "")
+
+# Select events with 2 jets ...
+# process.cmgPFJetCount.minNumber = 2
+# with pT > 50.
+# process.cmgPFJetSel.cut = "pt()>50"
+# and MET larger than 50
+# process.cmgPFMETSel.cut = "pt()>50"
+
+##########
+
+runOnMC = False
+
+if runOnMC:
+    process.load("CMGTools.Common.runInfoAccounting_cfi")
+    process.outpath += process.runInfoAccounting
+
+process.p = cms.Path(
+    process.analysisSequence
+    )
+
 process.GlobalTag.globaltag = cms.string(getGlobalTag(runOnMC))
 
-process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(-1)
-)
-process.source.fileNames = cms.untracked.vstring(
-        os.path.expandvars('file:/tmp/${USER}/patTuple_PATandPF2PAT.root')
-)
-process.setName_('CMG')
-process.out.fileName = cms.untracked.string('cmgTuple.root')
+process.schedule = cms.Schedule(
+    process.p,
+    process.outpath
+    )
 
-process.load('CMGTools.Common.cutSummary_cff')
-process.load('CMGTools.Common.electron_cff')
-process.load('CMGTools.Common.muon_cff')
-process.load('CMGTools.Common.diMuon_cff')
-process.load('CMGTools.Common.jet_cff')
-process.load('CMGTools.Common.runInfoAccounting_cfi')
+##########
 
-#config the pat trigger matching
-process.cmgMuon.cfg.inputCollection = cms.InputTag("triggeredPatMuons")
-
-from CMGTools.Common.eventContent.particleFlow_cff import particleFlow as particleFlowEventContent  
-from CMGTools.Common.eventContent.particleFlow_cff import particleFlowBase as particleFlowEventContentBase  
-process.out.outputCommands = cms.untracked.vstring( 'drop *')
-process.out.outputCommands.extend( particleFlowEventContent ) 
-process.out.outputCommands.extend( particleFlowEventContentBase )
-process.out.outputCommands.append( 'keep *_triggeredPatMuons_*_*' ) # we keep the pat::Muon
-
-#output file for histograms etc
-process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string("histograms.root"))
-
-### Produce a summary of cuts on the DiMuon
-##process.zmumusummary = process.cutSummaryMuon.clone(inputCollection = cms.InputTag("cmgDiMuon"))
-
-### Run the trigger matching selections
-##from CMGTools.ZmumuJetsTutorial.selections.muontrigger_cfi import muontrigger
-##process.cmgMuon.cuts.muontrigger = muontrigger.clone()
-###
-
-###The jet counting histograms
-##process.load('CMGTools.ZmumuJetsTutorial.histograms.jetcounting_cfi')
-##process.load('CMGTools.ZmumuJetsTutorial.skims.selEventsPFJet_cff')
-##process.jetCountingClean = process.jetCounting.clone(inputCollection = cms.InputTag("jetIDJet"))
-##process.jetCountingSequence = cms.Sequence(process.jetCounting+process.jetIDJet+process.jetCountingClean)
-### The Z skimming
-##process.load('CMGTools.ZmumuJetsTutorial.skims.selEventsZ_cff')
-###
-
-process.analysisSequence = cms.Sequence(
-    process.electronSequence +
-    process.muonSequence + 
-    process.diMuonSequence +
-    process.pfJetSequence +
-    process.runInfoAccounting +
-    process.cutSummaryMuon## + 
-##    process.zmumusummary +
-##    process.jetCountingSequence +
-##    process.selectedZSequence
-)
-process.p = cms.Path(process.analysisSequence)
+# How much feedback do we want from the framework?
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+######################################################################
