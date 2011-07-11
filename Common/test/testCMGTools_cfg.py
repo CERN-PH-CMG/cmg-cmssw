8d36a270adae50a7c160748b0bab5735f72c891d
wreece <>
2011-07-11 13:28:27 +0000

    Updates the input files

--- Common/test/testCMGTools_cfg.py
+++ Common/test/testCMGTools_cfg.py
@@ -23,7 +23,7 @@ process.maxLuminosityBlocks = cms.untracked.PSet(
 #     'file:../prod/patTuple_PF2PAT.root'
 #     )
 
-process.load("CMGTools.Common.sources.RelValZMM.CMSSW_4_2_3_START42_V12_v2.GEN_SIM_RECO.TestJune29.source_PAT_cff")
+process.load("CMGTools.Common.sources.RelValZMM.CMSSW_4_2_5_START42_V12_v1.GEN_SIM_RECO.source_cff")
 
 # process.load("CMGTools.Common.sources.SingleMu.Run2011A_May10ReReco_v1.AOD.PAT_CMG_V2.source_PAT_cff")
 
