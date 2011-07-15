b3f2d3250e06a2059be55f2196e5d795d12db22f
benitezj <>
2011-07-15 08:55:56 +0000

    added generator info to tau

--- CMGTools/Common/python/factories/cmgTau_cfi.py
+++ CMGTools/Common/python/factories/cmgTau_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from CMGTools.Common.factories.cmgLepton_cfi import leptonFactory
 tauFactory = cms.PSet(
        inputCollection = cms.InputTag("selectedPatTausAK5"),
-       leptonFactory = leptonFactory.clone()
+       leptonFactory = leptonFactory.clone(),
+       rhoPFJetsCollection = cms.InputTag("kt6PFJetsAK5")
        )
 
 cmgTau = cms.EDFilter("TauPOProducer",
