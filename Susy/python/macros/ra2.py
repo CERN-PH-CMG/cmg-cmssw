294611df9a33b1fa1b708c6d5f30f308d42032b6
cbern <>
2011-07-22 16:58:35 +0000

    dphi implemented

--- Susy/python/macros/ra2.py
+++ Susy/python/macros/ra2.py
@@ -3,7 +3,11 @@ objects = {
     'ht':'cmgBaseMETs_RA2MHTPFJet50Central__',
     'seles':'cmgElectrons_susyElectron__',
     'smus':'cmgMuons_susyMuon__',
-    'ra2jets':'cmgPFJets_RA2PFJet50Central__'
+    'ra2jets':'cmgPFJets_RA2PFJet50Central__',
+    'ra2jet0':'cmgPFJets_RA2Jet0__',
+    'ra2jet12':'cmgPFJets_RA2Jet12__',
+    'ra2dphi0':'float_RA2dPhi0__',
+    'ra2dphi12':'float_RA2dPhi12__'
     }
 
 from CMGTools.Susy.macros.jetId import *
