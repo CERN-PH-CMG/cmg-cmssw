299fa97593f54f47172c747766c75ec626917808
psilva <>
2011-08-05 15:52:45 +0000

    debugged MVA sequence

--- CMGTools/HtoZZ2l2nu/test/runAnalysis_cfg.py
+++ CMGTools/HtoZZ2l2nu/test/runAnalysis_cfg.py
@@ -1,18 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
+from CMGTools.HtoZZ2l2nu.MVAStudyConfig_cfi import *
+
 runProcess = cms.PSet(
-#    input = cms.string("/afs/cern.ch/user/p/psilva/scratch0/CMSSW_4_1_3_patch2/src/CMGTools/HtoZZ2l2nu/data/weighted/DYToMuMu_M-20_Spring11.root"),
-#    input = cms.string("/afs/cern.ch/user/p/psilva/scratch0/CMSSW_4_1_3_patch2/src/CMGTools/HtoZZ2l2nu/data/weighted/DYToEE_M-20_Spring11.root"),
     input = cms.string("data/GluGluToHToZZTo2L2Nu_M-200_Spring11.root"),
-#    input = cms.string("/data/psilva/Higgs/DYToMuMu_M-20_Spring11.root"),
-#    input = cms.string("/afs/cern.ch/user/p/psilva/scratch0/CMSSW_4_1_3_patch2/src/CMGTools/HtoZZ2l2nu/data/weighted/TTJets_madgraph_Spring11.root"),
-#    input = cms.string("../../datastore/psilva/HtoZZ/weighted/DoubleMuon-v9.root"),
     outdir = cms.string("./"),
-    useFitter=cms.bool(False),
+    xsec = cms.double(1),
     isMC = cms.bool(True),
+    saveSummaryTree = cms.bool(False),
+    useFitter=cms.bool(False),
     evStart = cms.int32(0),
     evEnd = cms.int32(10000),
     dirName = cms.string("evAnalyzer/data"),
     ptResolFileName = cms.string("${CMSSW_RELEASE_BASE}/src/CondFormats/JetMETObjects/data/Spring10_PtResolution_AK5PF.txt"),
-    phiResolFileName = cms.string("${CMSSW_RELEASE_BASE}/src/CondFormats/JetMETObjects/data/Spring10_PhiResolution_AK5PF.txt")
+    phiResolFileName = cms.string("${CMSSW_RELEASE_BASE}/src/CondFormats/JetMETObjects/data/Spring10_PhiResolution_AK5PF.txt"),
+    etaResolFileName = cms.string("${CMSSW_RELEASE_BASE}/src/CondFormats/JetMETObjects/data/Spring10_EtaResolution_AK5PF.txt"),
+    jesUncFileName = cms.string("${CMSSW_RELEASE_BASE}/src/CondFormats/JetMETObjects/data/Spring10_Uncertainty_AK5PF.txt"),
+    useMVA = cms.bool(True),
+    tmvaInput = redMETstudy
     )
