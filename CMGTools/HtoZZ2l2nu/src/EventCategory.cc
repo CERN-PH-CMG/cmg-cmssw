19a32c6a1939c7c9ce599b83e90cbb2eba38017c
querten <>
2011-08-05 13:47:07 +0000

    move CentralJet PtCut to 15GeV

--- CMGTools/HtoZZ2l2nu/src/EventCategory.cc
+++ CMGTools/HtoZZ2l2nu/src/EventCategory.cc
@@ -1,8 +1,8 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2011/08/05 10:37:05 $
- *  $Revision: 1.12 $
+ *  $Date: 2011/08/05 09:53:44 $
+ *  $Revision: 1.1 $
  *  \author L. Quertenmont
  */
 
@@ -16,16 +16,17 @@ EventCategory::~EventCategory(){}
 
 
 int EventCategory::Get(const PhysicsEvent_t& phys){
-    int NJetsEta25  = 0;
-    int NJetsEtaMax = 0;
+    int NJetsCentral  = 0;
+    int NJetsVBF = 0;
     for(size_t ijet=0; ijet<phys.jets.size(); ijet++) {
+      if(phys.jets[ijet].pt()<15)continue;
+      if(fabs(phys.jets[ijet].eta())<2.5){NJetsCentral++;}
       if(phys.jets[ijet].pt()<30)continue;
-      if(fabs(phys.jets[ijet].eta())<2.5){NJetsEta25++;}
-      NJetsEtaMax++;
+      NJetsVBF++;
     }
 
     bool isVBF = false;
-    if(NJetsEtaMax>=2){
+    if(NJetsVBF>=2){
        LorentzVector VBFSyst = phys.jets[0] + phys.jets[1];
        double dEta = fabs(phys.jets[0].eta() - phys.jets[1].eta());
        int NCentralJet = 0;  int NCentralLepton = 0;  int NBJets = 0;
@@ -37,8 +38,8 @@ int EventCategory::Get(const PhysicsEvent_t& phys){
     }
 
     if(isVBF        )return VBF;
-    if(NJetsEta25>=2)return GEQ2JETS;
-    if(NJetsEta25==1)return EQ1JETS;
+    if(NJetsCentral>=2)return GEQ2JETS;
+    if(NJetsCentral==1)return EQ1JETS;
     return EQ0JETS;
 }
 
