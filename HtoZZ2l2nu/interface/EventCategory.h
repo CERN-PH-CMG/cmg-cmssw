31d536b6df4dc2915a62eeded1a0a3a86755594a
querten <>
2011-08-05 08:54:56 +0000

    Add event categorizer

---
+++ HtoZZ2l2nu/interface/EventCategory.h
@@ -0,0 +1,69 @@
+#ifndef EventCategory_H
+#define EventCategory_H
+
+/** \class EventCategory
+ *  No description available.
+ *
+ *  $Date: 2011/08/05 10:10:56 $
+ *  $Revision: 1.0 $
+ *  \author L. Quertenmont P. Silva
+ */
+
+
+
+#include <utility>
+#include <vector>
+#include "CMGTools/HtoZZ2l2nu/interface/ZZ2l2nuSummaryHandler.h"
+#include "CMGTools/HtoZZ2l2nu/interface/ZZ2l2nuPhysicsEvent.h"
+
+const TString ZZ2l2nuCategoryLabel[] = {"eq0jets","eq1jets","geq2jets", "vbf"};
+
+class EventCategory {
+public:
+  enum CategoryType { EQ0JETS, EQ1JETS, GEQ2JETS, VBF };
+
+ 
+  /// Constructor
+  EventCategory();
+  /// Destructor
+  virtual ~EventCategory();
+
+  int Get(const PhysicsEvent_t& phys){
+      int NJetsEta25  = 0;
+      int NJetsEtaMax = 0;
+      for(size_t ijet=0; ijet<phys.jets.size(); ijet++) {
+        if(phys.jets[ijet].pt()<30)continue;
+        if(fabs(phys.jets[ijet].eta())<2.5){NJetsEta25++;}
+        NJetsEtaMax++;
+      }
+
+      bool isVBF = false;
+      if(NJetsEtaMax>=2){
+         LorentzVector VBFSyst = phys.jets[0] + phys.jets[1];
+         double dEta = fabs(phys.jets[0].eta() - phys.jets[1].eta());
+         int NCentralJet = 0;  int NCentralLepton = 0;  int NBJets = 0;
+         double MaxEta, MinEta;if(phys.jets[0].eta()<phys.jets[1].eta()){MinEta=phys.jets[0].eta(); MaxEta=phys.jets[1].eta();}else{MinEta=phys.jets[1].eta(); MaxEta=phys.jets[0].eta();}
+         for(size_t ijet=2; ijet<phys.jets.size(); ijet++) {if(phys.jets[ijet].pt()<30)continue; if(phys.jets[ijet].eta()>MinEta && phys.jets[ijet].eta()<MaxEta)NCentralJet++;  if(phys.jets[ijet].btag1>1.7)NBJets++; }
+         if(phys.leptons[0].eta()>MinEta && phys.leptons[0].eta()<MaxEta)NCentralLepton++;
+         if(phys.leptons[1].eta()>MinEta && phys.leptons[1].eta()<MaxEta)NCentralLepton++;
+         isVBF = (dEta>4.0) && (VBFSyst.M()>600) && (NCentralJet==0) && (NBJets==0) && (NCentralLepton==2);
+      }
+
+      if(isVBF        )return VBF;
+      if(NJetsEta25>=2)return GEQ2JETS;
+      if(NJetsEta25==1)return EQ1JETS;
+      return EQ0JETS;
+  }
+
+  TString GetLabel(int CategoryType){
+     return ZZ2l2nuCategoryLabel[CategoryType];
+  }
+
+  TString GetLabel(const PhysicsEvent_t& phys){
+     return ZZ2l2nuCategoryLabel[Get(phys)];
+  }
+
+
+};
+#endif
+
