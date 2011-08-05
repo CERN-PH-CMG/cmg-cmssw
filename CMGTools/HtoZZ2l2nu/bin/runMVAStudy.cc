b8ecda468bb337df1a55d1d0a88e3ecfab0ebf39
psilva <>
2011-08-05 10:04:54 +0000

    fixing br in ZZ, adding tmva categories

--- CMGTools/HtoZZ2l2nu/bin/runMVAStudy.cc
+++ CMGTools/HtoZZ2l2nu/bin/runMVAStudy.cc
@@ -20,13 +20,11 @@
 #include "CMGTools/HtoZZ2l2nu/interface/ReducedMETComputer.h"
 #include "CMGTools/HtoZZ2l2nu/interface/TransverseMassComputer.h"
 #include "CMGTools/HtoZZ2l2nu/interface/ProjectedMETComputer.h"
+#include "CMGTools/HtoZZ2l2nu/interface/TMVAUtils.h"
 
 #include "FWCore/FWLite/interface/AutoLibraryLoader.h"
 #include "FWCore/PythonParameterSet/interface/MakeParameterSets.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "TMVA/Factory.h"
-#include "TMVA/Tools.h"
 
 using namespace std;
 
@@ -47,13 +45,27 @@ int main(int argc, char *argv[])
   //configure      
   //
   const edm::ParameterSet &runProcess = edm::readPSetsFrom(argv[1])->getParameter<edm::ParameterSet>("runProcess");
-  int nJetsBin   = runProcess.getParameter<int>("nJetsBin");
   std::vector<int> evCategories = runProcess.getParameter<std::vector<int> >("evCategories");
   TString url    = runProcess.getParameter<std::string>("input");
   TString output = runProcess.getParameter<std::string>("output");
   std::vector<std::string> methodList = runProcess.getParameter<std::vector<std::string> >("methodList");
   std::vector<std::string> varsList = runProcess.getParameter<std::vector<std::string> >("varsList");
-  TString selCut = runProcess.getParameter<std::string>("selCut");
+  //variables for method definition (for now all methods share the same variables: todo split per category)
+  TString varsString("");
+  for(std::vector<std::string>::iterator it = varsList.begin(); it != varsList.end(); it++)
+    {
+      if(it != varsList.begin()) varsString += ":";
+      varsString += *it;
+    }
+  std::vector<TString> categVars(evCategories.size(),varsString);
+
+  //selection cuts for exclusive categories
+  std::vector<TCut> categDef(evCategories.size());
+  for(size_t i=0; i<evCategories.size(); i++)
+    {
+      TString catDef("eventCategory=="); catDef += i;
+      categDef[i]=TCut(catDef);
+    }
 
   std::vector<std::string> procList = runProcess.getParameter<std::vector<std::string> >("procList");
   std::vector<int>         procType = runProcess.getParameter<std::vector<int> >("procType");
@@ -84,10 +96,11 @@ int main(int argc, char *argv[])
   outTag.ReplaceAll(".root","");
   TMVA::Factory *factory = new TMVA::Factory( outTag.Data(), outputFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
   for(std::vector<std::string>::iterator it = varsList.begin(); it != varsList.end(); it++)  factory->AddVariable( *it, *it, "", 'F');
+  factory->AddSpectator( "eventCategory" );
 
-  const unsigned int nVariables = varsList.size();
+  const unsigned int nVariables = varsList.size()+1;
   std::vector<Double_t> vars( nVariables );
-  cout << "==> Start TMVAClassification with " << methodList.size() << " methods and " << nVariables << " variables" << endl;
+  cout << "==> Start TMVAClassification with " << methodList.size() << " methods x " << evCategories.size() << " categories and " << nVariables-1 << " variables" << endl;
 
   //reduced met computer
   ReducedMETComputer rmetComp(1., 1., 1., 1., 1.);
@@ -105,10 +118,12 @@ int main(int argc, char *argv[])
 	  ZZ2l2nuSummary_t &ev=evSummaryHandler.getEvent();
 	  PhysicsEvent_t phys=getPhysicsEventFrom(ev);
 
-	  if(find(evCategories.begin(),evCategories.end(),ev.cat)== evCategories.end()) continue;    
-	  int jetbin=ev.jn;
-	  if(jetbin>2) jetbin=2;
-	  if(jetbin != nJetsBin) continue;
+	  //consider only ee/mumu
+	  if(ev.cat!=1 && ev.cat!=2) continue;
+
+	  int eventCategory(ev.jn);
+	  if(eventCategory>=2) eventCategory=2;
+	  //add condition for vbf category
 
 	  //the kinematics
 	  LorentzVector zll = phys.leptons[0]+phys.leptons[1];
@@ -142,6 +157,7 @@ int main(int argc, char *argv[])
 	      if(*it=="redmetL")  vars[varCounter++] = redmetL;
 	      if(*it=="redmetT")  vars[varCounter++] = redmetT;
 	    }
+	  vars[varCounter++] = eventCategory;
 
 	  double weight=procWeight[iproc]*ev.weight;
 	  if(procType[iproc]==1)
@@ -158,120 +174,15 @@ int main(int argc, char *argv[])
     }
   cout << "\t Signal split in " << nsigtrain << " training events + " << nsigtest  << " test events" << endl
        << "\t Background split in " << nbkgtrain << " training events + " << nbkgtest  << " test events" << endl;
-     
-  // Set individual event weights (the variables must exist in the original TTree)
-  factory->SetSignalWeightExpression    ("scale1fb");
-  factory->SetBackgroundWeightExpression("scale1fb");
-
+   
   //use random splitting
-  factory->PrepareTrainingAndTestTree( TCut(selCut), TCut(selCut),"nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
-  
+  TCut selCut( categDef.size()==1 ?  categDef[0] : "");
+  factory->PrepareTrainingAndTestTree(selCut, selCut,"nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
+
   //
-  // check the methods to use
+  // book the methods to use (for the moment all variables are passed to all category methods - > needs change)
   //
-  // cut optimization
-  if (find(methodList.begin(),methodList.end(),"Cuts") != methodList.end())   
-    factory->BookMethod( TMVA::Types::kCuts, "Cuts","!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart" );
-  if (find(methodList.begin(),methodList.end(),"CutsD") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kCuts, "CutsD","!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=Decorrelate" );
-  if (find(methodList.begin(),methodList.end(),"CutsPCA") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kCuts, "CutsPCA","!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=PCA" );
-  if (find(methodList.begin(),methodList.end(),"CutsGA") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kCuts, "CutsGA","H:!V:FitMethod=GA:CutRangeMin[0]=-10:CutRangeMax[0]=10:VarProp[1]=FMax:EffSel:Steps=30:Cycles=3:PopSize=400:SC_steps=10:SC_rate=5:SC_factor=0.95" );
-  if (find(methodList.begin(),methodList.end(),"CutsSA") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kCuts, "CutsSA","!H:!V:FitMethod=SA:EffSel:MaxCalls=150000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale" );
-
-  // Likelihood 
-  if (find(methodList.begin(),methodList.end(),"Likelihood") != methodList.end())    
-    factory->BookMethod( TMVA::Types::kLikelihood, "Likelihood","H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=30:NSmoothBkg[0]=30:NSmoothBkg[1]=10:NSmooth=1:NAvEvtPerBin=40" );
-  if (find(methodList.begin(),methodList.end(),"LikelihoodD") != methodList.end())   
-    factory->BookMethod( TMVA::Types::kLikelihood, "LikelihoodD","!H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=40:VarTransform=Decorrelate" );
-  if (find(methodList.begin(),methodList.end(),"LikelihoodPCA") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kLikelihood, "LikelihoodPCA","!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=PCA" ); 
-  if (find(methodList.begin(),methodList.end(),"LikelihoodKDE") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kLikelihood, "LikelihoodKDE","!H:!V:!TransformOutput:PDFInterpol=KDE:KDEtype=Gauss:KDEiter=Adaptive:KDEFineFactor=0.3:KDEborder=None:NAvEvtPerBin=50" ); 
-  if (find(methodList.begin(),methodList.end(),"LikelihoodMIX") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kLikelihood, "LikelihoodMIX","!H:!V:!TransformOutput:PDFInterpolSig[0]=KDE:PDFInterpolBkg[0]=KDE:PDFInterpolSig[1]=KDE:PDFInterpolBkg[1]=KDE:PDFInterpolSig[2]=Spline2:PDFInterpolBkg[2]=Spline2:PDFInterpolSig[3]=Spline2:PDFInterpolBkg[3]=Spline2:KDEtype=Gauss:KDEiter=Nonadaptive:KDEborder=None:NAvEvtPerBin=50" ); 
-
-  // multi-dimensional probability density estimator
-  if (find(methodList.begin(),methodList.end(),"PDERS") != methodList.end())         
-    factory->BookMethod( TMVA::Types::kPDERS, "PDERS","!H:!V:NormTree=T:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600" );
-  if (find(methodList.begin(),methodList.end(),"PDERSD") != methodList.end())        
-    factory->BookMethod( TMVA::Types::kPDERS, "PDERSD","!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=Decorrelate" );
-  if (find(methodList.begin(),methodList.end(),"PDERSPCA") != methodList.end())      
-    factory->BookMethod( TMVA::Types::kPDERS, "PDERSPCA","!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=PCA" );
-  if (find(methodList.begin(),methodList.end(),"PDEFoam") != methodList.end())       
-    factory->BookMethod( TMVA::Types::kPDEFoam, "PDEFoam","H:!V:SigBgSeparate=F:TailCut=0.001:VolFrac=0.0333:nActiveCells=500:nSampl=2000:nBin=5:Nmin=100:Kernel=None:Compress=T" );
-  if (find(methodList.begin(),methodList.end(),"PDEFoamBoost") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kPDEFoam, "PDEFoamBoost","!H:!V:Boost_Num=30:Boost_Transform=linear:SigBgSeparate=F:MaxDepth=4:UseYesNoCell=T:DTLogic=MisClassificationError:FillFoamWithOrigWeights=F:TailCut=0:nActiveCells=500:nBin=20:Nmin=400:Kernel=None:Compress=T" );
-
-  // K-Nearest Neighbour classifier (KNN)
-  if (find(methodList.begin(),methodList.end(),"KNN") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kKNN, "KNN","H:nkNN=31:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim" );
-
-  // H-Matrix (chi2-squared) method
-  if (find(methodList.begin(),methodList.end(),"HMatrix") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kHMatrix, "HMatrix", "!H:!V" );
-
-  // Linear discriminant (same as Fisher discriminant)
-  if (find(methodList.begin(),methodList.end(),"LD") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kLD, "LD", "H:!V:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10" );
-
-  // Fisher discriminant (same as LD)
-  if (find(methodList.begin(),methodList.end(),"Fisher") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFisher, "Fisher", "H:!V:Fisher:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=40:NsmoothMVAPdf=10" );
-
-  // Fisher with Gauss-transformed input variables
-  if (find(methodList.begin(),methodList.end(),"FisherG") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kFisher, "FisherG", "H:!V:VarTransform=Gauss" );
-
-  // Composite classifier: ensemble (tree) of boosted Fisher classifiers
-  if (find(methodList.begin(),methodList.end(),"BoostedFisher") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kFisher, "BoostedFisher", "H:!V:Boost_Num=20:Boost_Transform=log:Boost_Type=AdaBoost:Boost_AdaBoostBeta=0.2" );
-  
-  // Function discrimination analysis (FDA) -- test of various fitters - the recommended one is Minuit (or GA or SA)
-  if (find(methodList.begin(),methodList.end(),"FDA_MC") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_MC","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:SampleSize=100000:Sigma=0.1" );
-  if (find(methodList.begin(),methodList.end(),"FDA_GA") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_GA","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1" );
-  if (find(methodList.begin(),methodList.end(),"FDA_SA") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_SA","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=SA:MaxCalls=15000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale" );
-  if (find(methodList.begin(),methodList.end(),"FDA_MT") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_MT","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=2:UseImprove:UseMinos:SetBatch" );
-  if (find(methodList.begin(),methodList.end(),"FDA_GAMT") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_GAMT","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:Cycles=1:PopSize=5:Steps=5:Trim" );
-  if (find(methodList.begin(),methodList.end(),"FDA_MCMT") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kFDA, "FDA_MCMT","H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:SampleSize=20" );
-
-  // TMVA ANN: MLP (recommended ANN) -- all ANNs in TMVA are Multilayer Perceptrons
-  if (find(methodList.begin(),methodList.end(),"MLP") != methodList.end())      
-    factory->BookMethod( TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator" );
-  if (find(methodList.begin(),methodList.end(),"MLPBFGS") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kMLP, "MLPBFGS", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:TrainingMethod=BFGS:!UseRegulator" );
-  if (find(methodList.begin(),methodList.end(),"MLPBNN") != methodList.end())   
-    factory->BookMethod( TMVA::Types::kMLP, "MLPBNN", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=500:HiddenLayers=N+3:TestRate=5:TrainingMethod=BFGS:UseRegulator" ); 
-  if (find(methodList.begin(),methodList.end(),"CFMlpANN") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kCFMlpANN, "CFMlpANN", "!H:!V:NCycles=2000:HiddenLayers=N+1,N"  ); // n_cycles:#nodes:#nodes:...  
-  if (find(methodList.begin(),methodList.end(),"TMlpANN") != methodList.end())  
-    factory->BookMethod( TMVA::Types::kTMlpANN, "TMlpANN", "!H:!V:NCycles=200:HiddenLayers=N+1,N:LearningMethod=BFGS:ValidationFraction=0.3"  ); // n_cycles:#nodes:#nodes:...
-
-  // Support Vector Machine
-  if (find(methodList.begin(),methodList.end(),"SVM") != methodList.end())      
-    factory->BookMethod( TMVA::Types::kSVM, "SVM", "Gamma=0.25:Tol=0.001:VarTransform=Norm" );
-
-  // Boosted Decision Trees
-  if (find(methodList.begin(),methodList.end(),"BDTG") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kBDT, "BDTG","!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:NNodesMax=5" );
-  if (find(methodList.begin(),methodList.end(),"BDT") != methodList.end())  
-    factory->BookMethod(TMVA::Types::kBDT,"BDT","!H:!V:NTrees=400:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:PruneMethod=CostComplexity:PruneStrength=20.0");
-  if (find(methodList.begin(),methodList.end(),"BDTB") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kBDT, "BDTB","!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=25:PruneMethod=NoPruning" );
-  if (find(methodList.begin(),methodList.end(),"BDTD") != methodList.end()) 
-    factory->BookMethod(TMVA::Types::kBDT,"BDTD","!H:!V:NTrees=400:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=25:PruneMethod=CostComplexity:PruneStrength=25.0:VarTransform=Decorrelate");
-
-  // RuleFit -- TMVA implementation of Friedman's method
-  if (find(methodList.begin(),methodList.end(),"RuleFit") != methodList.end()) 
-    factory->BookMethod( TMVA::Types::kRuleFit, "RuleFit","H:!V:RuleFitModule=RFTMVA:Model=ModRuleLinear:MinImp=0.001:RuleMinDist=0.001:NTrees=20:fEventsMin=0.01:fEventsMax=0.5:GDTau=-1.0:GDTauPrec=0.01:GDStep=0.01:GDNSteps=10000:GDErrScale=1.02" );
+  for(size_t i=0; i<methodList.size(); i++)  tmva::bookMethodWithCategories(factory,methodList[i],categDef,categVars);
 
   //
   // now train, test and evaluate
--- CMGTools/HtoZZ2l2nu/test/runMVAStudy_cfg.py
+++ CMGTools/HtoZZ2l2nu/test/runMVAStudy_cfg.py
@@ -1,11 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 runProcess = cms.PSet(
-    nJetsBin   = cms.int32(0),
-    evCategories = cms.vint32(1,2),
+    evCategories = cms.vint32(0,1,2),
     input      = cms.string("EventSummaries.root"),
-    output     = cms.string("CutOptimization_0jets.root"),
-    methodList = cms.vstring('CutsD'),
+    output     = cms.string("CutOptimization_redMet.root"),
+    methodList = cms.vstring('CutD'),
     varsList   = cms.vstring( 'redmetL','redmetT' ),
     selCut     = cms.string ( '' ),
     procList   = cms.vstring( 'VBFtoH200toZZto2L2Nu', 'GGtoH200toZZto2L2Nu', 'GGtoH200toWWto2L2Nu', 'DYJetsToLL'),
