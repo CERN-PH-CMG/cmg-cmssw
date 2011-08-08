57837b9d67791ef838b88040ed49a0daeb7440e0
psilva <>
2011-08-08 08:21:53 +0000

    adding missing stuff?

--- CMGTools/HtoZZ2l2nu/bin/G/getGammaPtWeights.py
+++ CMGTools/HtoZZ2l2nu/bin/G/getGammaPtWeights.py
@@ -29,31 +29,47 @@ for o,a in opts:
     elif o in('-i'): inputFile = a
                         
 ROOT.gSystem.Load('${CMSSW_BASE}/lib/${SCRAM_ARCH}/libCMGToolsHtoZZ2l2nu.so')
-from ROOT import showPlotsAndMCtoDataComparison, formatForCmsPublic, getNewCanvas, setStyle
+from ROOT import showPlots, formatForCmsPublic, getNewCanvas, setStyle
                                                                      
 #plots to retrieve
 #cats=['photon20','photon30','photon50','photon75','photon90','photon125']
 cats=['photon20','photon30','photon50','photon75','photon125']
 gammaPt = None
 zPt = None
+zTauTauPt = None
 
 #open file and get plots
 f = ROOT.TFile.Open(inputFile)
 for c in cats:
-    iGammaPt = f.Get('proc_1/'+c+'_qtvsnjets_1')
-    iZPt = f.Get('proc_2/'+c+'_qtvsnjets_2')
+    iGammaPt = f.Get('proc_3/'+c+'_qtvsnjets_3')
+    iZPt = f.Get('proc_1/'+c+'_qtvsnjets_1')
+    iZTauTauPt = f.Get('proc_2/'+c+'_qtvsnjets_2')
     if(gammaPt is None) :
         gammaPt = iGammaPt.Clone('gamma_pt')
         zPt = iZPt.Clone('z_pt')
+        zTauTauPt = iZTauTauPt.Clone('ztautau_pt')
     else :
         gammaPt.Add(iGammaPt)
         zPt.Add(iZPt)
+        zTauTauPt.Add(iZTauTauPt)
 gammaPt.SetDirectory(0)
 zPt.SetDirectory(0)
+zTauTauPt.SetDirectory(0)
+f.Close()
+
+#re-scale Z->ll and gamma to 1
+
+gammaPt.Scale(1./gammaPt.Integral())
+totalzll=zPt.Integral()
+#totalztautau=zTauTauPt.Integral()
+zPt.Scale(1./totalzll)
+zTauTauPt.Scale(1./totalzll)
+
 ptWeights=zPt.Clone('gammaptweight')
 ptWeights.Divide(gammaPt)
 ptWeights.SetDirectory(0)
-f.Close()
+ptWeights.GetYaxis().SetTitle("p_{T}^{Z#rightarrow ll}/p_{T}^{#gamma}")
+
 
 #show the output
 setStyle()
@@ -63,12 +79,15 @@ c.SetCanvasSize(600,600)
 
 #show
 stack=ROOT.TList()
-stack.Add(gammaPt)
+stack.Add(zPt)
+stack.Add(zTauTauPt)
 data=ROOT.TList()
-data.Add(zPt)
+data.Add(gammaPt)
 spimpose=ROOT.TList()
-leg=showPlotsAndMCtoDataComparison(c,stack,spimpose,data)
+leg=showPlots(c,stack,spimpose,data)
 formatForCmsPublic(c.cd(1),leg,'CMS preliminary',2)
+c.cd(2)
+ptWeights.Draw("colztext")
 c.cd()
 c.Modified()
 c.Update()
