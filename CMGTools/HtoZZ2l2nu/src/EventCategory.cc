9b7fe55e168035c7fce08aa1f9e5b439879c9b08
querten <>
2011-08-16 09:22:13 +0000

    Move to Pedro's BTagging

--- CMGTools/HtoZZ2l2nu/src/EventCategory.cc
+++ CMGTools/HtoZZ2l2nu/src/EventCategory.cc
@@ -1,8 +1,8 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2011/08/05 09:53:44 $
- *  $Revision: 1.1 $
+ *  $Date: 2011/08/05 13:47:07 $
+ *  $Revision: 1.2 $
  *  \author L. Quertenmont
  */
 
@@ -31,7 +31,7 @@ int EventCategory::Get(const PhysicsEvent_t& phys){
        double dEta = fabs(phys.jets[0].eta() - phys.jets[1].eta());
        int NCentralJet = 0;  int NCentralLepton = 0;  int NBJets = 0;
        double MaxEta, MinEta;if(phys.jets[0].eta()<phys.jets[1].eta()){MinEta=phys.jets[0].eta(); MaxEta=phys.jets[1].eta();}else{MinEta=phys.jets[1].eta(); MaxEta=phys.jets[0].eta();}
-       for(size_t ijet=2; ijet<phys.jets.size(); ijet++) {if(phys.jets[ijet].pt()<30)continue; if(phys.jets[ijet].eta()>MinEta && phys.jets[ijet].eta()<MaxEta)NCentralJet++;  if(phys.jets[ijet].btag1>1.7)NBJets++; }
+       for(size_t ijet=2; ijet<phys.jets.size(); ijet++) {if(phys.jets[ijet].pt()<30)continue; if(phys.jets[ijet].eta()>MinEta && phys.jets[ijet].eta()<MaxEta)NCentralJet++;  if(phys.jets[ijet].btag2>1.33 || phys.jets[ijet].btag3>1.74)NBJets++; }
        if(phys.leptons[0].eta()>MinEta && phys.leptons[0].eta()<MaxEta)NCentralLepton++;
        if(phys.leptons[1].eta()>MinEta && phys.leptons[1].eta()<MaxEta)NCentralLepton++;
        isVBF = (dEta>4.0) && (VBFSyst.M()>600) && (NCentralJet==0) && (NBJets==0) && (NCentralLepton==2);
