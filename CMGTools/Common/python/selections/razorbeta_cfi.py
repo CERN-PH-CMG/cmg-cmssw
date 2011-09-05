f75504da67a0e0eaf93ad102329c4315e15b2952
wreece <>
2011-09-05 12:25:47 +0000

    File no longer needed in 2011 variables

--- CMGTools/Common/python/selections/razorbeta_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-#See CMS-PAS-SUS-10-009
-razorbeta = cms.PSet(
-    useMR = cms.string('abs(leg1.p()-leg2.p()) <= abs(leg1.pz()-leg2.pz())'),
-    useMRPrime = cms.string('abs(leg1.p()-leg2.p()) > abs(leg1.pz()-leg2.pz())'),
-    betaR = cms.string('abs(betaR()) <= 0.99')
-    )
