7ef68520b41713f596a81e011b50af53f7c66ad7
lucieg <>
2011-09-06 13:08:39 +0000

    added genLeptons sequence to keep e, mu status 1 and 2, tau status 2

--- Common/python/gen_cff.py
+++ Common/python/gen_cff.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.generator.genParticlesStatus3_cfi import *
+from CMGTools.Common.generator.genLeptons_cff import *
 from CMGTools.Common.generator.listParticles_cfi import *
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
 genSequence = cms.Sequence(
     genParticlesStatus3
+    + genLeptonsSequence
     + listParticles
     )
