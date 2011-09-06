15995f019abc8c85ad92733d2bc071d9af1c3973
cbern <>
2011-09-06 10:10:53 +0000

    - added cut strings for RA2 delta phi cuts - developed alias system

--- CMGTools/Susy/python/macros/ra2.py
+++ CMGTools/Susy/python/macros/ra2.py
@@ -7,7 +7,8 @@ objects = {
     'ra2jet0':'cmgPFJets_RA2Jet0__',
     'ra2jet12':'cmgPFJets_RA2Jet12__',
     'ra2dphi0':'float_RA2dPhi0__',
-    'ra2dphi12':'float_RA2dPhi12__'
+    'ra2dphi1':'float_RA2dPhi1__',
+    'ra2dphi2':'float_RA2dPhi2__'
     }
 
 from CMGTools.Susy.macros.jetId import *
---
+++ CMGTools/Susy/python/macros/susy.py
@@ -0,0 +1,5 @@
+from CMGTools.Susy.macros.ra1 import *
+from CMGTools.Susy.macros.ra2 import *
+from CMGTools.Susy.macros.razor import *
+
+susy = dict( ra1.items() + ra2.items() + razor.items() )
