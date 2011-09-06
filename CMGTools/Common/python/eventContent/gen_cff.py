7640f1ee798bd7b4981316b9d5ad3a0ee64cdc38
palencia <>
2011-09-06 12:35:16 +0000

    Added GenEventInfoProduct for PDF syst.

--- CMGTools/Common/python/eventContent/gen_cff.py
+++ CMGTools/Common/python/eventContent/gen_cff.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 gen = cms.untracked.vstring(
     'keep recoGenParticles_genParticlesStatus3_*_*',
     'keep *_addPileupInfo_*_*',
+    'keep GenEventInfoProduct_*_*_*',
     )
 
