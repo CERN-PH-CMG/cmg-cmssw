0b46824be45592e2ec24874ba34dce42303b0e19
querten <>
2011-09-27 14:31:50 +0000

    update

--- CMGTools/HtoZZ2l2nu/bin/Eff/performanceComparison.sh
+++ CMGTools/HtoZZ2l2nu/bin/Eff/performanceComparison.sh
@@ -15,6 +15,6 @@ root -l -b << EOF
   gSystem->Load("libDataFormatsParticleFlowCandidate.so");
   gSystem->Load("libDataFormatsMath.so");
   gSystem->Load("libCMGToolsHtoZZ2l2nu.so");
-  //.x performanceComparison.C+("mumu","H(200)","Z-#gamma^{*}+jets#rightarrow ll", "../../test/plotter.root");
-  .x performanceComparison.C+("mumu","H(200)","ZZ|WW|WZ|Single top|t#bar{t}|W+jets|Z-#gamma^{*}+jets#rightarrow ll", "../../test/plotter.root");
+  .x performanceComparison.C+("mumu","H(200)","Z-#gamma^{*}+jets#rightarrow ll", "../../test/plotter.root");
+  //.x performanceComparison.C+("mumu","H(200)","ZZ|WW|WZ|Single top|t#bar{t}|W+jets|Z-#gamma^{*}+jets#rightarrow ll", "../../test/plotter.root");
 EOF                                   
