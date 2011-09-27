7d8432caf19c3fc06df826a94d416cd98507287f
querten <>
2011-09-27 09:43:44 +0000

    improve performanceComp code

--- CMGTools/HtoZZ2l2nu/bin/Eff/performanceComparison.C
+++ CMGTools/HtoZZ2l2nu/bin/Eff/performanceComparison.C
@@ -13,6 +13,27 @@
 
 using namespace std;
 
+TObject* GetObjectFromPath(TDirectory* File, std::string Path, bool GetACopy=false)
+{
+   TObject* toReturn = NULL;
+
+   size_t pos = Path.find("/");
+   if(pos < std::string::npos){
+      std::string firstPart = Path.substr(0,pos);
+      std::string endPart   = Path.substr(pos+1,Path.length());
+      TDirectory* TMP = (TDirectory*)File->Get(firstPart.c_str());
+      toReturn =  GetObjectFromPath(TMP,endPart,GetACopy);
+   }else{
+      toReturn = File->Get(Path.c_str());
+   }
+
+   if(!toReturn)printf("BUG: %s\n",Path.c_str());
+
+   if(GetACopy){ toReturn = toReturn->Clone();  }
+   return toReturn;
+}
+
+
 //
 TGraphAsymmErrors *getEfficiencyCurve(TH1 *signalProc, TH1 *bckgProc)
 { 
@@ -42,33 +63,37 @@ TGraphAsymmErrors *getEfficiencyCurve(TH1 *signalProc, TH1 *bckgProc)
 }
 
 //
-void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString fname="plotter.root")
+void performanceComparison(string evcat="mumu", string signal="H(200)", string background="Z-#gamma^{*}+jets#rightarrow ll",  string fname="../../test/plotter.root")
 {
   setStyle();
   gStyle->SetOptFit(0);
+  gStyle->SetPadTopMargin   (0.05);
+  gStyle->SetPadBottomMargin(0.10);
+  gStyle->SetPadRightMargin (0.05);
+  gStyle->SetPadLeftMargin  (0.15);
+  system("mkdir -p Img");
 
   int colors[]  = {1,  kGreen-3, kGreen+3, kBlue, kBlue, kRed+3, kRed};
   int styles[]  = {1,  2,        9,        9,     1,     1,      1};
   int markers[] = {20, 24,       21,       25,    22,    24,     20};
 
   //configure 
-  TString bckgName("#gamma");  
   TString meanFitFunc("pol3");
   TString rmsFitFunc("pol3");
   
   // TString names[]={"metvspu",       "minMetvspu",                       "minClusteredMetvspu",     "unclusteredMetvspu",     "superMinMetvspu"};
 //   TString titles[]={"E_{T}^{miss}", "min assoc. E_{T}^{miss}(charged)", "min assoc. E_{T}^{miss}", "min clean E_{T}^{miss}", "minimized E_{T}^{miss}"};
   
-  TString names[]  ={"metvspu",      "assocMetvspu",       "assocOtherVertexMetvspu",        "cleanMetvspu"};
-  TString titles[] ={"E_{T}^{miss}", "assoc-E_{T}^{miss}", "assoc-E_{T}^{miss} (other vtx)", "clean E_{T}^{miss}"};
+  string names[]  ={"met_metvspu",      "met_assocMetvspu",       "met_cleanMetvspu", "met_minAssocChargedMetvspu", "met_minAssocMetvspu", "met_minCleanMetvspu", "met_superMinMetvspu"};
+  TString titles[] ={"E_{T}^{miss}", "assoc-E_{T}^{miss}", "clean E_{T}^{miss}", "min{E_{T}^{miss},assoc E_{T}^{miss} (charged)}", "min{E_{T}^miss,assoc-E_{T}^{miss})", "min{E_{T}^{miss},clean-E_{T}^{miss}}", "min{E_{T}^{miss},assoc-E_{T}^{miss},clean-E_{T}^{miss},central-E_{T}^{miss}}"};
+
 
   //   TString names[]={"njetsvspu","njetsincvspu"};
   //   TString titles[]={"Central jets","All jets"};
   
-  int ntouse=sizeof(names)/sizeof(TString);
+  int ntouse=sizeof(names)/sizeof(string);
 
-
-  TFile *f=TFile::Open(fname);
+  TFile *f=TFile::Open(fname.c_str());
   std::vector<TGraphAsymmErrors*> bckgMeanList, bckgRMSList;
   std::vector<TGraphAsymmErrors *> incEffGrList;
   std::vector< std::vector<TGraphAsymmErrors *> > effGrList; 
@@ -79,43 +104,20 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
   effBins.push_back(std::pair<int,int>(10,25));
   for(int i=0; i<ntouse; i++)
     {
-      TString name=names[i];
+      string name=names[i];
       TString title=titles[i];
       TString idxStr("");
       idxStr += i;
-      
-      TH2D *bckgProc=0;
-      TH2D *signalProc=0;
-      TIter next(f->GetListOfKeys());
-      TObject *obj=0;
-      int idir(0);
-      while(1)
-	{
-          idir++;
-          obj=next();
-          if(obj==0) break;
-          TString dirname=obj->GetName();
-          TString hname=dirname +"/"+evcat+"_" + name;
-          TH1 *h = (TH1 *) f->Get(hname);
-          TString tit=h->GetTitle();
-          if(tit.Contains(bckgName)) 
-	    {
-	      bckgProc = (TH2D *) h->Clone("bckg_"+idxStr);
-	      bckgProc->SetDirectory(0);
-	      cout << " -bckg: " << tit << endl; 
-	    }
-	   else if(tit.Contains(mpoint))
-	    {
-	      if(signalProc==0) 
-		{
-		  signalProc = (TH2D *) h->Clone("signal_"+idxStr);
-		  signalProc->SetDirectory(0);
-		}
-	      else 
-		signalProc->Add(h);
-	      cout << " -signal: " << tit << endl;  
-	    }
-	}
+
+      TH2D *signalProc= (TH2D*) GetObjectFromPath(f, signal    +"#rightarrow VV" + "/"+evcat+"_"+name, true);    
+      TH2D *bckgProc=NULL;
+      char* BckgSamples = new char [background.size()+1];  strcpy (BckgSamples, background.c_str());
+      char* BckgSubSample = strtok (BckgSamples,"|");
+      while (BckgSubSample != NULL){
+         if(!bckgProc){bckgProc =    (TH2D*)GetObjectFromPath(f, string(BckgSubSample)+"/"+evcat+"_"+name, true);
+         }else{        bckgProc->Add((TH2D*)GetObjectFromPath(f, string(BckgSubSample)+"/"+evcat+"_"+name, true));}
+         BckgSubSample = strtok (NULL, "|");
+      }
 
       //inclusive efficiency
       int nxbins=bckgProc->GetXaxis()->GetNbins();
@@ -124,7 +126,7 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
       TH1*bckgProj   = ((TH2 *)bckgProc)->ProjectionY("bckg_py",1,nxbins);
       fixExtremities(bckgProj,true,true);
       TGraphAsymmErrors *gr = getEfficiencyCurve(signalProj, bckgProj );
-      gr->SetName(names[i]+"_eff");
+      gr->SetName((names[i]+"_eff").c_str());
       gr->SetTitle(titles[i]);
       gr->SetLineColor(colors[i]);
       gr->SetLineStyle(styles[i]);
@@ -136,8 +138,7 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
 
       //efficiency per bins
       std::vector<TGraphAsymmErrors *> ieffGrList;
-      for(size_t ibin=0; ibin<effBins.size(); ibin++) 
-	{ 
+      for(size_t ibin=0; ibin<effBins.size(); ibin++){ 
 	  signalProj = ((TH2 *)signalProc)->ProjectionY("sig_py",effBins[ibin].first,effBins[ibin].second);
 	  fixExtremities(signalProj,true,true);
 	  bckgProj   = ((TH2 *)bckgProc)->ProjectionY("bckg_py",effBins[ibin].first,effBins[ibin].second);
@@ -145,7 +146,7 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
 
 	  //compute efficiency
 	  gr = getEfficiencyCurve(signalProj, bckgProj );
-	  gr->SetName(names[i]+"_eff");
+	  gr->SetName((names[i]+"_eff").c_str());
 	  gr->SetTitle(titles[i]);
 	  gr->SetLineColor(colors[i]);
 	  gr->SetLineStyle(styles[i]);
@@ -154,12 +155,12 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
 	  gr->SetMarkerStyle(markers[i]);
 	  gr->SetLineWidth(2);
 	  ieffGrList.push_back( gr );
-	}
+      }
       effGrList.push_back( ieffGrList );
 
       //built the performance curves 
       TGraphAsymmErrors *bckgMean = new TGraphAsymmErrors;
-      bckgMean->SetName(names[i]+"_mean");
+      bckgMean->SetName((names[i]+"_mean").c_str());
       bckgMean->SetTitle(titles[i]);
       bckgMean->SetLineColor(colors[i]);
       bckgMean->SetLineStyle(styles[i]);
@@ -170,7 +171,7 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
       bckgMeanList.push_back(bckgMean);
 
       TGraphAsymmErrors *bckgRMS = new TGraphAsymmErrors;
-      bckgRMS->SetName(names[i]+"_rms");
+      bckgRMS->SetName((names[i]+"_rms").c_str());
       bckgRMS->SetTitle(titles[i]);
       bckgRMS->SetLineColor(colors[i]);
       bckgRMS->SetLineStyle(styles[i]);
@@ -197,18 +198,17 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
       bckgRMS->Fit(rmsFitFunc,"Q+","",2,nxbins);
     }
 
-  
-
   //pileup robustness
-  TCanvas *puC = getNewCanvas("puC","Pileup robustness",false);
-  puC->SetWindowSize(1000,500);
+  TCanvas *puC = new TCanvas("puC", "Pileup robustness", 1000,500);
+//getNewCanvas("puC","Pileup robustness",false);
+//puC->SetWindowSize(1000,500);
   puC->Divide(2,1);  
 
   TPad *p=(TPad *)puC->cd(1);
   for(size_t ip=0; ip<bckgMeanList.size(); ip++)    bckgMeanList[ip]->Draw(ip==0 ? "ap" : "p" );
   bckgMeanList[0]->GetXaxis()->SetTitle("Pileup");
   bckgMeanList[0]->GetYaxis()->SetTitle("Average");
-  TLegend *leg=p->BuildLegend();
+  TLegend *leg=p->BuildLegend(0.20, 0.60, 0.60, 0.90);
   formatForCmsPublic(p,leg,"CMS simulation, Z#rightarrow ll",ntouse);
 
   p=(TPad *)puC->cd(2);
@@ -216,14 +216,15 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
   bckgRMSList[0]->GetXaxis()->SetTitle("Pileup");
   bckgRMSList[0]->GetYaxis()->SetTitle("RMS");
 
-
-  TString legTit="CMS simulation\\"+mpoint;
+  TString legTit="CMS simulation\\"+TString(signal.c_str());
   legTit.ReplaceAll("H(","m_{H^{0}}=");
   legTit.ReplaceAll(")"," GeV/c^{2}");
-  
+  puC->SaveAs("Img/puC.png");
+  delete puC;
+
   //inclusive efficiency
-  TCanvas *effC = getNewCanvas("effC","Inclusive efficiency",false);
-  effC->SetWindowSize(500,500);
+  TCanvas *effC =  new TCanvas("effC", "Inclusive efficiency", 500,500);//getNewCanvas("effC","Inclusive efficiency",false);
+//  effC->SetWindowSize(500,500);
   effC->cd();
   effC->SetLogy();
   TGraph *effFrame=new TGraph;
@@ -237,16 +238,16 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
   effFrame->GetYaxis()->SetTitle("#varepsilon(instr. background)");
 
   for(size_t ip=0; ip<incEffGrList.size(); ip++)   incEffGrList[ip]->Draw("pl");
-  leg=effC->BuildLegend();
+  leg=effC->BuildLegend(0.20, 0.60, 0.60, 0.90);
   formatForCmsPublic(effC,leg,legTit,ntouse);
-
+  effC->SaveAs("Img/effC.png");
+  delete effC;
 
   //efficiency degradation  
-  effC = getNewCanvas("effpuC","Differential efficiency",false);
-  effC->SetWindowSize(400*effBins.size(),400);
+  effC = new TCanvas("effpuC", "Differential efficiency", 500*effBins.size(),500);//getNewCanvas("effpuC","Differential efficiency",false);
   effC->Divide(effBins.size(),1);
   for(size_t ip=0; ip<effBins.size(); ip++) 
-    {
+   {
       p=(TPad *)effC->cd(ip+1);
       p->SetLogy();
       effFrame->Draw("ap");
@@ -264,18 +265,22 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
       for(int i=0; i<ntouse; i++) effGrList[i][ip]->Draw("pl");
 
       if(ip>0) continue;
-      leg=p->BuildLegend();
+      leg=p->BuildLegend(0.15, 0.60, 0.60, 0.95);
       formatForCmsPublic(p,leg,legTit,ntouse);
     }
+    effC->SaveAs("Img/effpuC.png");
+    delete effC;
 
   //another view of efficiency degradation
-  effC = getNewCanvas("effpertypeC","Differential efficiency per type",false);
-  effC->SetWindowSize(400*ntouse,400);
-  effC->Divide(ntouse,1);
+//  effC = getNewCanvas("effpertypeC","Differential efficiency per type",false);
+//  effC->SetWindowSize(400*ntouse,400);
+//  effC->Divide(ntouse,1);
   for(int ip=0; ip<ntouse; ip++) 
     {
-      p=(TPad *)effC->cd(ip+1);
-      p->SetLogy();
+       effC = new TCanvas("effpertypeC","Differential efficiency per type",500,500);
+       effC->SetLogy();
+//      p=(TPad *)effC->cd(ip+1);
+//      p->SetLogy();
       effFrame->Draw("ap");
       
       TPaveText *pave = new TPaveText(0.05,0.96,0.5,0.99,"NDC");
@@ -300,10 +305,13 @@ void comparePerformance(TString evcat="mumu", TString mpoint="H(200)",  TString 
 	}
       
       incEffGrList[ip]->Draw("pl");
+      effC->SaveAs((string("Img/effpertypeC_") + names[ip] + ".png").c_str());
+      delete effC;
     }
 
 //       if(ip>0) continue;
 //       leg=p->BuildLegend();
 //       formatForCmsPublic(p,leg,legTit,ntouse);
+
 }
 
