32a26a83d8f5111a4d24048cbd8c629c87e961c5
cbern <>
2011-09-30 13:13:21 +0000

    new experimental release

---
+++ Common/python/release_info/CMSSW_4_2_8_exp_V2_3_0.py
@@ -0,0 +1,18 @@
+# See https://savannah.cern.ch/task/index.php?20954
+{
+    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_isolation_30Sep11', 'AnalysisDataFormats/CMGTools'],
+    'UserCode/CMG/CMGTools/Common':              ['cbern_isolation_30Sep11', 'CMGTools/Common'],
+    'UserCode/CMG/CMGTools/RootTools':           ['cbern_isolation_30Sep11', 'CMGTools/RootTools'],
+    'CommonTools/ParticleFlow':                  ['B4_2_X_V00-03-00', 'CommonTools/ParticleFlow'],
+    'UserCode/csander/DataFormats/AnomalousEcalDataFormats' :     ['V110523_BE', 'DataFormats/AnomalousEcalDataFormats'],
+    'DataFormats/PatCandidates' :                ['cbern_isolation_V06-04-19-01', 'DataFormats/PatCandidates'],
+    'JetMETAnalysis/ecalDeadCellTools':          ['Colin_TaggingMode_June30','JetMETAnalysis/ecalDeadCellTools'],
+    'PhysicsTools/PatAlgos':                     ['B4_2_X_cbern_isolation_29Sept11_d',   'PhysicsTools/PatAlgos'],
+    'PhysicsTools/SelectorUtils' :               ['V00-03-24', 'PhysicsTools/SelectorUtils'],
+    'RecoJets/Configuration' :                   ['V02-04-17', 'RecoJets/Configuration'],
+    'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June23', 'RecoParticleFlow/PostProcessing'],
+    'RecoTauTag/Configuration' :                 ['V01-02-02', 'RecoTauTag/Configuration'],
+    'RecoTauTag/RecoTau' :                       ['V01-02-06', 'RecoTauTag/RecoTau'],
+    'RecoTauTag/TauTagTools' :                   ['V01-02-00', 'RecoTauTag/TauTagTools'],
+}
+
