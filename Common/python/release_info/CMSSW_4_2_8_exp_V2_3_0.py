e7aa2c6d4d211f6f77c46878432aa86101b03567
cbern <>
2011-10-04 08:40:36 +0000

    updated recipe

--- Common/python/release_info/CMSSW_4_2_8_exp_V2_3_0.py
+++ Common/python/release_info/CMSSW_4_2_8_exp_V2_3_0.py
@@ -1,14 +1,15 @@
 # See https://savannah.cern.ch/task/index.php?20954
 {
     'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_isolation_30Sep11', 'AnalysisDataFormats/CMGTools'],
-    'UserCode/CMG/CMGTools/Common':              ['cbern_isolation_30Sep11', 'CMGTools/Common'],
+    'UserCode/CMG/CMGTools/Common':              ['cbern_Oct4', 'CMGTools/Common'],
     'UserCode/CMG/CMGTools/RootTools':           ['cbern_isolation_30Sep11', 'CMGTools/RootTools'],
     'CommonTools/ParticleFlow':                  ['B4_2_X_V00-03-00', 'CommonTools/ParticleFlow'],
     'UserCode/csander/DataFormats/AnomalousEcalDataFormats' :     ['V110523_BE', 'DataFormats/AnomalousEcalDataFormats'],
-    'DataFormats/PatCandidates' :                ['cbern_isolation_V06-04-19-01', 'DataFormats/PatCandidates'],
+    'DataFormats/PatCandidates' :                ['V06-04-19-02', 'DataFormats/PatCandidates'],
     'JetMETAnalysis/ecalDeadCellTools':          ['Colin_TaggingMode_June30','JetMETAnalysis/ecalDeadCellTools'],
-    'PhysicsTools/PatAlgos':                     ['B4_2_X_cbern_isolation_29Sept11_d',   'PhysicsTools/PatAlgos'],
+    'PhysicsTools/PatAlgos':                     ['V08-06-44',   'PhysicsTools/PatAlgos'],
     'PhysicsTools/SelectorUtils' :               ['V00-03-24', 'PhysicsTools/SelectorUtils'],
+    'PhysicsTools/UtilAlgos' :                   ['V08-02-14', 'PhysicsTools/UtilAlgos'],
     'RecoJets/Configuration' :                   ['V02-04-17', 'RecoJets/Configuration'],
     'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June23', 'RecoParticleFlow/PostProcessing'],
     'RecoTauTag/Configuration' :                 ['V01-02-03', 'RecoTauTag/Configuration'],
