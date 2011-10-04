fe283ecae059cb739a445800dc3ddf781db7b84b
querten <>
2011-10-04 08:56:47 +0000

    increase met array size

--- HtoZZ2l2nu/interface/ZZ2l2nuPhysicsEvent.h
+++ HtoZZ2l2nu/interface/ZZ2l2nuPhysicsEvent.h
@@ -55,7 +55,7 @@ typedef std::vector<PhysicsObject_Gamma>  PhysicsObjectGammaCollection;
 //
 struct PhysicsEvent_t
 {
-  LorentzVector met[12];
+  LorentzVector met[13];
   LorentzVector vtx;
   LorentzVector gamma;
   PhysicsObjectJetCollection jets;
