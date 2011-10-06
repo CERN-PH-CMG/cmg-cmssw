3e01eb9ab66032c680ace05975cba485caeaa606
cbern <>
2011-10-06 13:49:08 +0000

    dropped redundant triggerobject collections, and the collection of pat::Muons

--- CMGTools/Common/python/eventContent/trigger_cff.py
+++ CMGTools/Common/python/eventContent/trigger_cff.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 trigger = cms.untracked.vstring(
-    'keep cmgTriggerObjects_cmgTriggerObjectSel*_*_*',
+    'drop cmgTriggerObjects_*_*_*',
+    'keep cmgTriggerObjects_cmgTriggerObjectSel_*_*',
     'keep *_TriggerResults_*_ANA',
     'keep *_TriggerResults_*_PAT',
     )
