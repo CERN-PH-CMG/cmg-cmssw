8bc3048eeb3bd9689a3acad1525054d3dc0d2dec
wreece <>
2011-10-07 12:37:18 +0000

    V2_3_0

---
+++ CMGTools/Common/python/release_info/CMSSW_4_2_8.py
@@ -0,0 +1,19 @@
+# See https://savannah.cern.ch/task/index.php?20954
+{
+    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_isolation_30Sep11', 'AnalysisDataFormats/CMGTools'],
+    'UserCode/CMG/CMGTools/Common':              ['wreece_071011', 'CMGTools/Common'],
+    'UserCode/CMG/CMGTools/RootTools':           ['cbern_iso_06Oct11', 'CMGTools/RootTools'],
+    'CommonTools/ParticleFlow':                  ['B4_2_X_V00-03-00', 'CommonTools/ParticleFlow'],
+    'UserCode/csander/DataFormats/AnomalousEcalDataFormats' :     ['V110523_BE', 'DataFormats/AnomalousEcalDataFormats'],
+    'DataFormats/PatCandidates' :                ['V06-04-19-02', 'DataFormats/PatCandidates'],
+    'JetMETAnalysis/ecalDeadCellTools':          ['Colin_TaggingMode_June30','JetMETAnalysis/ecalDeadCellTools'],
+    'PhysicsTools/PatAlgos':                     ['V08-06-44',   'PhysicsTools/PatAlgos'],
+    'PhysicsTools/SelectorUtils' :               ['V00-03-24', 'PhysicsTools/SelectorUtils'],
+    'PhysicsTools/UtilAlgos' :                   ['V08-02-14', 'PhysicsTools/UtilAlgos'],
+    'RecoJets/Configuration' :                   ['V02-04-17', 'RecoJets/Configuration'],
+    'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June23', 'RecoParticleFlow/PostProcessing'],
+    'RecoTauTag/Configuration' :                 ['V01-02-03', 'RecoTauTag/Configuration'],
+    'RecoTauTag/RecoTau' :                       ['V01-02-07', 'RecoTauTag/RecoTau'],
+    'RecoTauTag/TauTagTools' :                   ['V01-02-00', 'RecoTauTag/TauTagTools'],
+    'RecoLuminosity/LumiDB' :                    ['V03-03-00', 'RecoLuminosity/LumiDB'],
+}
