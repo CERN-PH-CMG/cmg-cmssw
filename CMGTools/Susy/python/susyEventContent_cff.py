86b41dbf2a4f9f0523b10b90625fb72f4f03c0b5
wreece <>
2011-10-13 15:41:27 +0000

    Adds simple V+Jets type selection for W+3 jets

--- CMGTools/Susy/python/susyEventContent_cff.py
+++ CMGTools/Susy/python/susyEventContent_cff.py
@@ -4,6 +4,7 @@ from CMGTools.Common.eventContent.everything_cff import *
 from CMGTools.Susy.RA1.RA1EventContent_cff import *
 from CMGTools.Susy.RA2.RA2EventContent_cff import *
 from CMGTools.Susy.Razor.razorEventContent_cff import *
+from CMGTools.Susy.LeptonicStop.leptonicStopEventContent_cff import *
 from CMGTools.Susy.LP.LPEventContent_cff import *
 from CMGTools.Susy.common.eventContent_cff import eventContent as commonEventContent
 
@@ -11,6 +12,7 @@ susyEventContent = everything
 susyEventContent += RA1EventContent
 susyEventContent += RA2EventContent
 susyEventContent += razorEventContent
+susyEventContent += leptonicStopEventContent
 susyEventContent += LPEventContent
 susyEventContent += commonEventContent
  
--- CMGTools/Susy/python/susy_cff.py
+++ CMGTools/Susy/python/susy_cff.py
@@ -6,6 +6,7 @@ from CMGTools.Susy.RA1.RA1_cff import *
 from CMGTools.Susy.RA2.RA2_cff import *
 from CMGTools.Susy.Razor.razor_cff import *
 from CMGTools.Susy.LP.LP_cff import *
+from CMGTools.Susy.LeptonicStop.leptonicstop_cff import *
 from CMGTools.Common.countingSequences_cff import *
 
 multijetPath = cms.Path(
@@ -16,6 +17,16 @@ multijetPath = cms.Path(
 multijetTriggerPath = cms.Path(
     susySequence +
     multijetTriggerSkimSequence
+    )
+
+leptonicStopPath = cms.Path(
+    susySequence +
+    leptonicStopSkimSequence
+    )
+
+leptonicStopTriggerPath = cms.Path(
+    susySequence +
+    leptonicStopTriggerSkimSequence
     )
 
 RA1Path = cms.Path(
