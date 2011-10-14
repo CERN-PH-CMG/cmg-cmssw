11eac1baab0b2f812b222357d6170a90cae426af
cbern <>
2011-10-14 12:31:25 +0000

    cosmetics

--- H2TauTau/h2TauTauInit.py
+++ H2TauTau/h2TauTauInit.py
@@ -1,11 +1,3 @@
-# to initialize your environment, make sure you have defined the following allias:
-# for bash: 
-# alias h2TauTauInit='python -i $CMSSW_BASE/src/CMGTools/H2TauTau/h2TauTauInit.py'
-# then, do:
-# cmgInit 'dir1/your_rootfiles*.root'
-# and then:
-#
-
 print 'initializing h->tau tau root environment'
 
 # aliases
@@ -13,3 +5,7 @@ from CMGTools.RootTools.cmgInit import *
 from CMGTools.H2TauTau.macros.h2TauTau import *
 
 aliases = AliasSetter(events, h2TauTau, 'H2TAUTAU')
+
+events.Draw('tauMu.obj.mass()>>hWelcome','tauMu.obj.getSelection("cuts_baseline")','',10000)
+events.GetHistogram().SetTitle('Welcome to the H#rightarrow#tau#tau analysis!')
+gPad.Modified()
