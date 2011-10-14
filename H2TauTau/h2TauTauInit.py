4837ece165a07b25db707c2b1c2eb27d6a07568b
cbern <>
2011-10-14 14:11:49 +0000

    removed limit on the number of events

--- H2TauTau/h2TauTauInit.py
+++ H2TauTau/h2TauTauInit.py
@@ -6,6 +6,6 @@ from CMGTools.H2TauTau.macros.h2TauTau import *
 
 aliases = AliasSetter(events, h2TauTau, 'H2TAUTAU')
 
-events.Draw('tauMu.obj.mass()>>hWelcome','tauMu.obj.getSelection("cuts_baseline")','',10000)
+events.Draw('tauMu.obj.mass()>>hWelcome','tauMu.obj.getSelection("cuts_baseline")','')
 events.GetHistogram().SetTitle('Welcome to the H#rightarrow#tau#tau analysis!')
 gPad.Modified()
