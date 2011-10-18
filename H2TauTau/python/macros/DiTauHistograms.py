685aa111740f1cc409217ca26a781dcfc0b1c8c4
cbern <>
2011-10-18 15:57:11 +0000

    added new macros

---
+++ H2TauTau/python/macros/DiTauHistograms.py
@@ -0,0 +1,30 @@
+from ROOT import TTree, TH1F, TH2F, TProfile, TLegend, gDirectory
+
+class DiTauHistograms:
+    def __init__(self, name):
+        self.name = name
+
+        self.h_pzeta = TH1F(name+'_h_pzeta', ';p_{#zeta} (GeV)', 150, -200,100)
+        self.h_vismass = TH1F(name+'_h_vismass', ';m_{vis} (GeV)', 100,0,200)
+
+        self.hists = []
+        self.hists.append( self.h_pzeta )
+        self.hists.append( self.h_vismass )
+        
+        
+    def fillHistos(self, tree, cut='', nEvents=999999999):
+        print 'filling histograms: ' + self.name
+        tree.Draw('tauMu.obj.pZeta()>>'+self.h_pzeta.GetName(),cut,'goff',nEvents)
+        tree.Draw('tauMu.obj.mass()>>'+self.h_vismass.GetName(),cut,'goff',nEvents)
+        
+    def formatHistos(self, style ):
+        for hist in self.hists:
+            style.formatHisto( hist )
+            
+            
+    def Write(self, dir ):
+        self.dir = dir.mkdir( self.name )
+        self.dir.cd()
+        for hist in self.hists:
+            hist.Write()
+        dir.cd()
---
+++ H2TauTau/python/macros/LegHistograms.py
@@ -0,0 +1,26 @@
+from ROOT import TTree, TH1F, TH2F, TProfile, TLegend, gDirectory
+
+class LegHistograms:
+    def __init__(self, name, leg):
+        self.name = name
+        self.leg = leg
+
+        self.h_pt = TH1F(name+'_h_pt', ';p_{T} (GeV)', 100, 0,200)
+
+        self.hists = []
+        self.hists.append( self.h_pt )
+        
+    def fillHistos(self, tree, cut='', nEvents=999999999):
+        print 'filling histograms: ' + self.name
+        tree.Draw('tauMu.obj.%s().pt()>>%s' % (self.leg, self.h_pt.GetName()),cut,'goff',nEvents)
+        
+    def formatHistos(self, style ):
+        for hist in self.hists:
+            style.formatHisto( hist )
+            
+    def Write(self, dir ):
+        self.dir = dir.mkdir( self.name )
+        self.dir.cd()
+        for hist in self.hists:
+            hist.Write()
+        dir.cd()
