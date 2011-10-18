ab6a6c51b1b64b82c65ad535b0f0c663c3c70720
cbern <>
2011-10-18 10:55:09 +0000

    now using function CMGTools/Production

--- H2TauTau/python/macros/h2TauTau.py
+++ H2TauTau/python/macros/h2TauTau.py
@@ -1,7 +1,8 @@
 diTau = {
     'tauMuAll':'cmgTaucmgMuoncmgDiObjects_cmgTauMu__',
     'tauMu':'cmgTaucmgMuoncmgDiObjects_cmgTauMuSel__',
-    'tauE':'cmgTaucmgElectroncmgDiObjects_cmgTauESel__'    
+    'tauE':'cmgTaucmgElectroncmgDiObjects_cmgTauESel__', 
+    'diTau':'cmgTaucmgTaucmgDiObjects_cmgDiTauSel__'    
     }
 
 
