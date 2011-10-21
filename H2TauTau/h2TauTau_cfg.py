2c7b55fa765387a10553e185df3ebee84ad95523
cbern <>
2011-10-21 12:16:55 +0000

    added applyJSON cfg. applying json in h2TauTau_cfg.py

--- H2TauTau/h2TauTau_cfg.py
+++ H2TauTau/h2TauTau_cfg.py
@@ -3,37 +3,43 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 ##########
 
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.maxLuminosityBlocks = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(1000)
     )
+
+numberOfFilesToProcess = 10
+
+dataset_user = 'cmgtools' 
+# dataset_name = '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0'
+dataset_name = '/TauPlusX/Run2011A-PromptReco-v6/AOD/V2/PAT_CMG_V2_3_0'
 
 ##########
 
-# Message logger setup.
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# Input  & JSON             -------------------------------------------------
 
 
 process.setName_('H2TAUTAU')
 
 from CMGTools.Production.datasetToSource import *
 process.source = datasetToSource(
-    'cmgtools',
-    '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0',
-    # '/VBF_HToTauTau_M-115_7TeV-powheg-pythia6-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0',
-    # '/TauPlusX/Run2011A-PromptReco-v6/AOD/V2/PAT_CMG_V2_3_0',
+    dataset_user,
+    dataset_name,
     'tree.*root') 
 
-process.source.fileNames = process.source.fileNames[:20]
 
-print 'processing:'
-print process.source.fileNames
+process.source.fileNames = process.source.fileNames[:numberOfFilesToProcess]
 
-ext = 'CMG'
+if dataset_name.find('Run201')>-1:
+    from CMGTools.Common.Tools.applyJSON_cff import *
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-178078_7TeV_PromptReco_Collisions11_JSON.txt'
+    applyJSON(process, json )
 
-outFileNameExt = ext
+print process.source
+
+# Sequence & path definition -------------------------------------------------
 
 process.load('CMGTools.H2TauTau.Colin.h2TauTau_cff')
 
@@ -46,12 +52,14 @@ process.schedule = cms.Schedule(
     )
 
 
+
+
+# OUTPUT definition ----------------------------------------------------------
+
+outFileNameExt = 'CMG'
 process.out.fileName = cms.untracked.string('h2TauTau_tree_%s.root' %  outFileNameExt)
 from CMGTools.H2TauTau.eventContent.tauMu_cff import tauMu as tauMuEventContent
 process.out.outputCommands.extend( tauMuEventContent ) 
-# process.out.outputCommands = ['drop *']
-# process.out.dropMetaData = cms.untracked.string('ALL')
-
 process.out.SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('tauMuPath') )
 
 process.TFileService = cms.Service(
@@ -62,3 +70,8 @@ process.TFileService = cms.Service(
 print process.out.dumpPython()
 
 print 'output file: ', process.out.fileName
+
+
+# Message logger setup.
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
