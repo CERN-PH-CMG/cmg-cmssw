cb81bf382e15b0fb4d295af1c525b8ebe5f8f81d
cbern <>
2011-10-21 12:16:55 +0000

    added applyJSON cfg. applying json in h2TauTau_cfg.py

--- CMGTools/H2TauTau/merge_cfg.py
+++ CMGTools/H2TauTau/merge_cfg.py
@@ -1,13 +1,8 @@
+# cfg to merge root files together
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("COPY")
-
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-    '/store/cmst3/user/cmgtools/CMG/VBF_HToTauTau_M-115_7TeV-powheg-pythia6-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0/tree_CMG_0.root'
-    )
-)
-
 
 
 from CMGTools.Production.datasetToSource import *
