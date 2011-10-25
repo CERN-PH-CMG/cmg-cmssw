bf295af33beed2699acafd2c47018d2b6c12fd59
cbern <>
2011-10-25 14:49:02 +0000

    improved cfg

--- CMGTools/Common/test/testCMGTools_cfg.py
+++ CMGTools/Common/test/testCMGTools_cfg.py
@@ -21,14 +21,16 @@ process.maxLuminosityBlocks = cms.untracked.PSet(
     )
 
 
+runOnMC = True
 
-# process.load("CMGTools.Common.sources.RelValZMM.CMSSW_4_2_5_START42_V12_v1.GEN_SIM_RECO.source_cff")
-# process.load("CMGTools.Common.sources.SingleMu.Run2011A_May10ReReco_v1.AOD.PAT_CMG_V2.source_PAT_cff")
-process.load("CMGTools.Common.sources.DoubleMu.Run2011A_ZMu_PromptSkim_v6.RAW_RECO.PAT_CMG_V2_3_0_JecPileUpDecoupled.source_cff")
 
-# process.source.fileNames = cms.untracked.vstring(
-#     'file:../prod/patTuple_PF2PAT.root'
-#     )
+
+from CMGTools.Production.datasetToSource import *
+process.source = datasetToSource(
+    'cmgtools',
+    '/ZZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0',
+    'patTuple_PF2PAT.*root'
+    ) 
 
 # reading the first 10 files:
 nFiles = 10
@@ -56,8 +58,9 @@ process.load('CMGTools.Common.analysis_cff')
 # by modifying the parameters of the modules present in this sequence. 
 
 # removing the taus, as PFCandidate embedding does not work, hence an exception in the cmg Tau factory - not needed anymore
-# from CMGTools.Common.Tools.tuneCMGSequences import removeObject
-# removeObject( process, 'tau', '') 
+
+from CMGTools.Common.Tools.tuneCMGSequences import removeObject
+removeObject( process, 'tau', '') 
 
 # Select events with 2 jet ...  
 # process.cmgPFJetCount.minNumber = 2
@@ -68,25 +71,19 @@ process.load('CMGTools.Common.analysis_cff')
 
 # note: we're reading ttbar events
 
-runOnMC = False
-
-if runOnMC:
-    process.load("CMGTools.Common.runInfoAccounting_cfi")
-    process.outpath += process.runInfoAccounting
-
-import os 
-rootfile_dir = os.environ['CMSSW_BASE'] + 'src/CMGTools/Common/data'
-process.vertexWeight = cms.EDFilter(
-    "VertexWeightProducer",
-    src = cms.InputTag(''),
-    inputHistMC = cms.string( rootfile_dir + '/Pileup_Summer11MC.root'),
-    inputHistData = cms.string( rootfile_dir + '/Pileup_2011_EPS_8_jul.root'),
-    )
 
 process.p = cms.Path(
-    process.vertexWeight + 
     process.analysisSequence
 )
+
+if runOnMC:
+    process.load("CMGTools.Common.generator.vertexWeight.vertexWeight_cff")
+    process.p += process.vertexWeightSequence
+    process.load("CMGTools.Common.runInfoAccounting_cff")
+    process.outpath += process.runInfoAccountingSequence
+
+
+
 process.GlobalTag.globaltag = cms.string(getGlobalTag(runOnMC))
 
 process.schedule = cms.Schedule(
