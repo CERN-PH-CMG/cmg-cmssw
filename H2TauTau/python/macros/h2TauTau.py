bbbc91aa49a1239af69a5069176b4d7e9f461bc8
cbern <>
2011-10-27 11:15:03 +0000

    reorganized init in RootTools. preparing scripts here - not finished

--- H2TauTau/python/macros/h2TauTau.py
+++
@@ -1,9 +0,0 @@
-diTau = {
-    'tauMuAll':'cmgTaucmgMuoncmgDiObjects_cmgTauMu__',
-    'tauMu':'cmgTaucmgMuoncmgDiObjects_cmgTauMuSel__',
-    'tauE':'cmgTaucmgElectroncmgDiObjects_cmgTauESel__', 
-    'diTau':'cmgTaucmgTaucmgDiObjects_cmgDiTauSel__'    
-    }
-
-
-h2TauTau = dict(  diTau.items() )
