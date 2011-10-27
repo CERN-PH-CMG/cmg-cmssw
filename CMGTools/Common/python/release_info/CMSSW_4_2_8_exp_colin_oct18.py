22655711bc962e2139b7795acac4a5c978dd7d60
cbern <>
2011-10-27 11:19:13 +0000

    updated recipe

--- CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_colin_oct18.py
+++ CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_colin_oct18.py
@@ -1,9 +1,9 @@
 # See https://savannah.cern.ch/task/index.php?20954
 {
-    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_pzeta_18Oct11', 'AnalysisDataFormats/CMGTools'],
-    'UserCode/CMG/CMGTools/Common':              ['cbern_pzeta_18Oct11', 'CMGTools/Common'],
-    'UserCode/CMG/CMGTools/Production':          ['cbern_start_18Oct11', 'CMGTools/Production'],
-    'UserCode/CMG/CMGTools/RootTools':           ['cbern_tagAndProbe_14Oct11', 'CMGTools/RootTools'],
+    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_jetcor_27Oct11', 'AnalysisDataFormats/CMGTools'],
+    'UserCode/CMG/CMGTools/Common':              ['cbern_jetcor_27Oct11', 'CMGTools/Common'],
+    'UserCode/CMG/CMGTools/Production':          ['cbern_scripts_25Oct11', 'CMGTools/Production'],
+    'UserCode/CMG/CMGTools/RootTools':           ['cbern_Plot_27Oct11', 'CMGTools/RootTools'],
     'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['cbern_tutorial_12Oct11', 'CMGTools/ZmumuJetsTutorial'],
     'CommonTools/ParticleFlow':                  ['B4_2_X_cbern_dbetaoff_14Oct11', 'CommonTools/ParticleFlow'],
     'UserCode/csander/DataFormats/AnomalousEcalDataFormats' :     ['V110523_BE', 'DataFormats/AnomalousEcalDataFormats'],
@@ -13,11 +13,11 @@
     'PhysicsTools/SelectorUtils' :               ['V00-03-24', 'PhysicsTools/SelectorUtils'],
     'PhysicsTools/UtilAlgos' :                   ['V08-02-14', 'PhysicsTools/UtilAlgos'],
     'RecoJets/Configuration' :                   ['V02-04-17', 'RecoJets/Configuration'],
+    'RecoLuminosity/LumiDB' :                    ['V03-03-00', 'RecoLuminosity/LumiDB'],
     'UserCode/RecoParticleFlow/PostProcessing':  ['Colin_June23', 'RecoParticleFlow/PostProcessing'],
     'RecoTauTag/Configuration' :                 ['V01-02-03', 'RecoTauTag/Configuration'],
     'RecoTauTag/RecoTau' :                       ['V01-02-07', 'RecoTauTag/RecoTau'],
     'RecoTauTag/TauTagTools' :                   ['V01-02-00', 'RecoTauTag/TauTagTools'],
-    'RecoLuminosity/LumiDB' :                    ['V03-03-00', 'RecoLuminosity/LumiDB'],
     'UserCode/TauAnalysis/SVFitStandAlone' :     ['cbern_start_18Oct11', 'TauAnalysis/SVFitStandAlone'],
 }
 
