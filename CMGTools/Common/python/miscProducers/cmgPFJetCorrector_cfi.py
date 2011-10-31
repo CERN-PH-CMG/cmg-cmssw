2bf764f4507c522391b676d01343d884cf81e717
kkousour <>
2011-10-31 14:46:13 +0000

    first implementation of the JetEnergyCorrector configuration

--- CMGTools/Common/python/miscProducers/cmgPFJetCorrector_cfi.py
+++ CMGTools/Common/python/miscProducers/cmgPFJetCorrector_cfi.py
@@ -5,5 +5,8 @@ cmgPFJetCorrector = cms.EDProducer(
     # make sure your jet and rho collections are compatible
     src = cms.InputTag( 'cmgPFJetSel' ),
     rho = cms.InputTag( 'kt6PFJetsAK5:rho' ),
+    payload = cms.string('AK5PFchs'),
+    levels = cms.vstring('L1FastJet','L2Relative','L3Absolute','L2L3Residual'),
+    sort = cms.bool(True),
     verbose = cms.untracked.bool( False )
 )
