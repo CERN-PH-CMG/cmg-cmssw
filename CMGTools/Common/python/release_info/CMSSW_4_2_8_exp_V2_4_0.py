fb2ef046b19973d386ba5403c119082c61837f45
cbern <>
2011-10-31 15:32:16 +0000

    updated the recipe for Kostas

--- CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_V2_4_0.py
+++ CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_V2_4_0.py
@@ -1,7 +1,7 @@
 # See https://savannah.cern.ch/task/index.php?20954
 {
-    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_jetcor_27Oct11', 'AnalysisDataFormats/CMGTools'],
-    'UserCode/CMG/CMGTools/Common':              ['wreece_311011', 'CMGTools/Common'],
+    'UserCode/CMG/AnalysisDataFormats/CMGTools': ['kkousour_311011', 'AnalysisDataFormats/CMGTools'],
+    'UserCode/CMG/CMGTools/Common':              ['kkousour_311011', 'CMGTools/Common'],
     'UserCode/CMG/CMGTools/Production':          ['cbern_cleaning_31Oct11', 'CMGTools/Production'],
     'UserCode/CMG/CMGTools/RootTools':           ['cbern_Plot_27Oct11', 'CMGTools/RootTools'],
     'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['cbern_tutorial_12Oct11', 'CMGTools/ZmumuJetsTutorial'],
