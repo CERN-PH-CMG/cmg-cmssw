3cc3caac144858462962f91a861bea278e5340a5
cbern <>
2011-11-01 14:33:39 +0000

    reorganized RootTools. added DataMC, plus test histograms

---
+++ CMGTools/RootTools/python/DataMC/DataMCPlot.py
@@ -0,0 +1,199 @@
+from operator import itemgetter, attrgetter
+import copy
+
+from ROOT import TH1, THStack, TLegend, TLine
+
+from CMGTools.RootTools.DataMC.Histogram import Histogram
+from CMGTools.RootTools.DataMC.Stack import Stack
+
+
+
+class DataMCPlot:
+    '''Handles a Data vs MC plot.
+
+    Features a list of histograms (some of them being stacked),
+    and several Drawing functions.
+    '''
+    
+    def __init__(self, name):
+        self.histosDict = {}
+        self.histos = []
+        self.name = name
+        self.legend = TLegend(0.13,0.66,0.44,0.89)
+        self.legendOn = True
+        self.stack = None
+        
+    def AddHistogram(self, name, histo, layer=0, legendLine=None):
+        '''Add a ROOT histogram, with a given name.
+
+        Histograms will be drawn by increasing layer.'''
+        tmp = Histogram(name, histo, layer)
+        self.histos.append( tmp )
+        self.histosDict[name] = tmp
+        tmp.AddEntry( self.legend, legendLine)
+        
+    def _SortedHistograms(self):
+        '''Returns the histogram dictionary, sorted by increasing layer.'''
+        byLayer = sorted( self.histos, key=attrgetter('layer') )
+        return byLayer
+
+
+    def Hist(self, histName):
+        '''Returns an histogram.
+
+        Print the DataMCPlot object to see which histograms are available.'''
+        return self.histosDict[histName]
+
+    def DrawNormalized(self, opt=''):
+        '''All histograms are drawn as PDFs, even the stacked ones'''
+        same = ''
+        for hist in self._SortedHistograms():
+            hist.obj.DrawNormalized( same + opt)
+            if same == '':
+                same = 'same'
+        self.DrawLegend()
+
+    def Draw(self, opt = ''):
+        '''All histograms are drawn.'''
+        same = ''
+        for hist in self._SortedHistograms():
+            hist.Draw( same + opt)
+            if same == '':
+                same = 'same'
+        self.DrawLegend()
+
+    def DrawLegend(self):
+        '''Draw the legend.'''
+        if self.legendOn:
+            self.legend.Draw('same')
+                
+    def DrawRatio(self):
+        '''Draw ratios : h_i / h_0.
+
+        h_0 is the histogram with the smallest layer, and h_i, i>0 are the other histograms.
+        if the DataMCPlot object contains N histograms, N-1 ratio plots will be drawn.
+        To take another histogram as the denominator, change the layer of this histogram by doing:
+        dataMCPlot.Hist("histName").layer = -99 '''
+        same = ''
+        denom = None
+        self.ratios = []
+        for hist in self._SortedHistograms():
+            if denom == None:
+                denom = hist
+                continue
+            ratio = copy.deepcopy( hist )
+            ratio.obj.Divide( denom.obj )
+            ratio.obj.Draw( same )
+            self.ratios.append( ratio )
+            if same == '':
+                same = 'same'
+        self.DrawLegend()
+
+    def DrawRatioStack(self,opt=''):
+        '''Draw ratios.
+
+        The stack is considered as a single histogram.'''
+        denom = None
+        histForRatios = []
+        for hist in self._SortedHistograms():
+            if denom == None:
+                denom = hist
+                continue
+            histForRatios.append( hist )
+        self._BuildStack( histForRatios )
+        self.stack.Divide( denom.obj )
+        self.stack.Draw(opt)
+        self.ratios = []
+        for hist in self.nostack:
+            print 'nostack ', hist
+            ratio = copy.deepcopy( hist )
+            ratio.obj.Divide( denom.obj )
+            ratio.obj.Draw('same')
+            self.ratios.append( ratio )
+        self.DrawLegend()
+        self.DrawRatioLines(denom)
+                
+    def DrawNormalizedRatioStack(self,opt=''):
+        '''Draw ratios.
+
+        The stack is considered as a single histogram.
+        All histograms are normalized before computing the ratio'''
+        denom = None
+        histForRatios = []
+        for hist in self._SortedHistograms():
+            # taking the first histogram (lowest layer)
+            # as the denominator histogram. 
+            if denom == None:
+                denom = copy.deepcopy(hist)
+                continue
+            # other histograms will be divided by the denominator
+            histForRatios.append( hist )
+        self._BuildStack( histForRatios )
+        self.stack.Normalize()
+        denom.Normalize()
+        self.stack.Divide( denom.weighted )
+        self.stack.Draw(opt)
+        self.ratios = []
+        for hist in self.nostack:
+            print 'nostack ', hist
+            ratio = copy.deepcopy( hist )
+            ratio.Normalize()
+            ratio.obj.Divide( denom.weighted )
+            ratio.obj.Draw('same')
+            self.ratios.append( ratio )        
+        self.DrawLegend()
+        self.DrawRatioLines(denom)
+
+
+    def DrawRatioLines(self, hist, frac=0.2):
+        '''Draw a line at y = 1, at 1+frac, and at 1-frac.
+
+        hist is used to get the y axis range.'''
+        xmin = hist.obj.GetXaxis().GetXmin()
+        xmax = hist.obj.GetXaxis().GetXmax()
+        line = TLine()
+        line.DrawLine(xmin, 1, xmax, 1)
+        line.DrawLine(xmin, 1+frac, xmax, 1+frac)
+        line.DrawLine(xmin, 1-frac, xmax, 1-frac)
+        
+                
+    def DrawStack(self, opt=''):
+        '''Draw all histograms, some of them in a stack.
+
+        if Histogram.stack is True, the histogram is put in the stack.'''
+        self._BuildStack(self._SortedHistograms())
+        self.stack.Draw( opt )
+        for hist in self.nostack:
+            hist.Draw('same')
+        self.DrawLegend()
+
+    def DrawNormalizedStack(self, opt=''):
+        '''Draw all histograms, some of them in a stack.
+
+        if Histogram.stack is True, the histogram is put in the stack.
+        all histograms out of the stack, and the stack itself, are shown as PDFs.'''
+        self._BuildStack(self._SortedHistograms())
+        self.stack.DrawNormalized( opt )
+        for hist in self.nostack:
+            hist.obj.DrawNormalized('same')
+        self.DrawLegend()
+
+    def _BuildStack(self, hists):
+        '''build a stack from a list of Histograms.
+
+        The histograms for which Histogram.stack is False are put in self.nostack'''
+        self.stack = None
+        self.stack = Stack(self.name+'_stack')
+        self.nostack = []
+        for hist in hists:
+            if hist.stack:
+                self.stack.Add( hist )
+            else:
+                self.nostack.append(hist)
+    
+    def __str__(self):
+        tmp =  'DataMCPlot: ' + self.name + '\n' 
+        for hist in self._SortedHistograms():
+            tmp += '\t' + str(hist) + '\n'
+        return tmp
+
--- CMGTools/RootTools/python/Style.py
+++ CMGTools/RootTools/python/Style.py
@@ -6,13 +6,19 @@ class Style:
                  markerColor = 1,
                  lineStyle = 1,
                  lineColor = 1,
-                 lineWidth = 2 ):
+                 lineWidth = 2,
+                 fillColor = None,
+                 fillStyle = 3001 ):
         self.markerStyle_ = markerStyle
         self.markerColor_ = markerColor
         self.lineStyle_ = lineStyle
         self.lineColor_ = lineColor
         self.lineWidth_ = lineWidth
-        
+        if fillColor is None:
+            self.fillColor_ = lineColor
+        else:
+            self.fillColor_ = fillColor
+        self.fillStyle_ = fillStyle
         
     def formatHisto( self, hist, title=None):
         hist.SetMarkerStyle( self.markerStyle_ )
@@ -20,6 +26,8 @@ class Style:
         hist.SetLineStyle( self.lineStyle_ )
         hist.SetLineColor( self.lineColor_ )
         hist.SetLineWidth( self.lineWidth_ )
+        hist.SetFillColor( self.fillColor_ )
+        hist.SetFillStyle( self.fillStyle_ )
         hist.GetXaxis().SetTitleSize(0.05)
         hist.GetYaxis().SetTitleSize(0.05)
         hist.GetYaxis().SetTitleOffset(1.5)
@@ -31,14 +39,16 @@ def formatPad( pad ):
     pad.SetLeftMargin(0.15)
     pad.SetBottomMargin(0.12)
 
-# the following are the styles to be used by everybody.
+
+# the following standard files are defined and ready to be used.
 # more standard styles can be added on demand.
+# user defined styles can be created in the same way in any python module
 
 sBlack = Style()
 sBlue = Style(lineColor=4, markerColor=4)
 sGreen = Style(lineColor=8, markerColor=8)
 sRed = Style(lineColor=2, markerColor=2)
-sYellow = Style(lineColor=1, markerColor=5)
+sYellow = Style(lineColor=1, markerColor=5, fillColor=5)
 
 
 sBlackSquares = Style(markerStyle = 21)
