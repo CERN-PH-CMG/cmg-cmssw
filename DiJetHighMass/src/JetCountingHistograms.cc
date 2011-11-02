1ac7443eaebd59fe9557d93f4f227554eed31d0c
mgouzevi <>
2011-11-02 15:56:16 +0000

    Put in coherence with V2_4_0

--- DiJetHighMass/src/JetCountingHistograms.cc
+++ DiJetHighMass/src/JetCountingHistograms.cc
@@ -17,7 +17,7 @@ void cmg::JetCountingHistograms::fill(const type& cand){
 void cmg::JetCountingHistograms::fill(const edm::Event& iEvent, const edm::EventSetup&){
 
     edm::Handle<view> cands;
-    iEvent.getByLabel(label_,cands);
+    iEvent.getByLabel(labelX_,cands);
     
     for(view::const_iterator it = cands->begin(); it != cands->end(); ++it){
         fill(*it);   
