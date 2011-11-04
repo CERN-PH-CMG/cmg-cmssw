c226290f6b976f6f4215043656ebd0a2bcb31f48
benitezj <>
2011-11-04 10:13:04 +0000

    added pileup weights

--- Common/python/generator/vertexWeight/vertexWeightMay10ReReco_cfi.py
+++ Common/python/generator/vertexWeight/vertexWeightMay10ReReco_cfi.py
@@ -4,10 +4,11 @@ import os
 rootfile_dir = os.environ['CMSSW_BASE'] + '/src/CMGTools/Common/data'
 centraldir = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/PileUp'
 
-vertexWeightEPSJul8 = cms.EDProducer(
+vertexWeightMay10ReReco = cms.EDProducer(
     "VertexWeightProducer",
     verbose = cms.untracked.bool( False ),
     src = cms.InputTag('addPileupInfo'),
     inputHistMC = cms.string( rootfile_dir + '/Pileup_Summer11MC.root'),
-    inputHistData = cms.string( centraldir + '/Pileup_2011_EPS_8_jul.root'),
+    inputHistData = cms.string( centraldir + '/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.pileup.root'),
     )
+
--- Common/python/generator/vertexWeight/vertexWeight_cff.py
+++ Common/python/generator/vertexWeight/vertexWeight_cff.py
@@ -1,7 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.generator.vertexWeight.vertexWeightEPSJul8_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeightLeptonPhoton_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeightMay10ReReco_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeightPromptRecov4_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeight05AugReReco_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeightPromptRecov6_cfi import *
+from CMGTools.Common.generator.vertexWeight.vertexWeight2invfb_cfi import *
 
 vertexWeightSequence = cms.Sequence(
     vertexWeightEPSJul8
+    +vertexWeightLeptonPhoton
+    +vertexWeightMay10ReReco
+    +vertexWeightPromptRecov4
+    +vertexWeight05AugReReco
+    +vertexWeightPromptRecov6
+    +vertexWeight2invfb
     )
