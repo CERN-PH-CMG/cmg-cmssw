c5c98a884cda766d832e9d0ba97a873abdd32dbc
palencia <>
2011-11-07 16:31:26 +0000

    Adding PV, scraping and totalKinematic filters in tagging mode

--- Common/python/gen_cff.py
+++ Common/python/gen_cff.py
@@ -6,10 +6,12 @@ from CMGTools.Common.generator.listParticles_cfi import *
 from CMGTools.Common.generator.vertexWeight.vertexWeight_cff import *
 
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
-from GeneratorInterface.GenFilters.TotalKinematicsFilter_cfi import *
+#from GeneratorInterface.GenFilters.TotalKinematicsFilter_cfi import *
+from CMGTools.Common.eventCleaning.totalKinematicsFilterCMG_cfi import *
 
 genSequence = cms.Sequence(
-    totalKinematicsFilter
+    #totalKinematicsFilter     
+    totalKinematicsFilterCMG     
     + genParticlesStatus3
     + genLeptonsSequence
     + listParticles
