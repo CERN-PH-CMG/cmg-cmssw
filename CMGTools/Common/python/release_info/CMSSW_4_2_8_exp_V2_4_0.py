ac84947c7e7af84729d980131d81d1232a1b198e
wreece <>
2011-11-09 15:33:15 +0000

    Updates the recipe for EOS related things

--- CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_V2_4_0.py
+++ CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_V2_4_0.py
@@ -2,7 +2,7 @@
 {
     'UserCode/CMG/AnalysisDataFormats/CMGTools': ['cbern_h2tautauvars_31Oct11', 'AnalysisDataFormats/CMGTools'],
     'UserCode/CMG/CMGTools/Common':              ['enrique_taggingFilters_07nov11', 'CMGTools/Common'],
-    'UserCode/CMG/CMGTools/Production':          ['wreece_071111', 'CMGTools/Production'],
+    'UserCode/CMG/CMGTools/Production':          ['wreece_091111', 'CMGTools/Production'],
     'UserCode/CMG/CMGTools/RootTools':           ['cbern_rootdirstyle_01Nov11', 'CMGTools/RootTools'],
     'UserCode/CMG/CMGTools/ZmumuJetsTutorial':   ['cbern_tutorial_12Oct11', 'CMGTools/ZmumuJetsTutorial'],
     'CommonTools/ParticleFlow':                  ['B4_2_X_cbern_dbetaoff_14Oct11', 'CommonTools/ParticleFlow'],
