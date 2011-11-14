c3400dc1746db7c9ff08ddc1817b499f0a03c156
cbern <>
2011-11-14 11:48:39 +0000

    integrated skeleton for SVFit mass reconstruction

--- H2TauTau/python/objects/object_cff.py
+++ H2TauTau/python/objects/object_cff.py
@@ -4,6 +4,8 @@ from CMGTools.Common.diTau_cff import *
 from CMGTools.Common.generator.metRecoilCorrection.metRecoilCorrection_cff import *
 from CMGTools.Common.factories.cmgTauMuCor_cfi import *
 
+from CMGTools.H2TauTau.objects.tauMuSVFit_cfi import tauMuSVFit as cmgTauMuCorSelSVFit
+
 from CMGTools.H2TauTau.objects.tauMuCuts_cff import * 
 
 cmgTauMu.cuts = tauMuCuts.clone()
@@ -15,7 +17,7 @@ cmgTauMuCorSel = cmgTauMuSel.clone( src = 'cmgTauMuCor' )
 
 cmgTauMuBaselineSel = cmgTauMuSel.clone( src = 'cmgTauMuSel',
                                          cut = 'getSelection("cuts_baseline")' )
-cmgTauMuCorBaselineSel = cmgTauMuBaselineSel.clone( src = 'cmgTauMuCorSel' ) 
+cmgTauMuCorBaselineSel = cmgTauMuBaselineSel.clone( src = 'cmgTauMuCorSelSVFit' ) 
 
 recoilCorMETTauMu =  recoilCorrectedMET.clone( recBosonSrc = 'cmgTauMuSel')
 cmgTauMuCor.cfg.metCollection = 'recoilCorMETTauMu'
@@ -26,6 +28,7 @@ tauMuSequence = cms.Sequence( cmgTauMu +
                               recoilCorMETTauMu +
                               cmgTauMuCor + 
                               cmgTauMuCorSel +
+                              cmgTauMuCorSelSVFit  + 
                               cmgTauMuCorBaselineSel )
 
 
