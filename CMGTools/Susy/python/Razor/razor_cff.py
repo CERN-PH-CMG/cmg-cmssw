10bbfec5bc95ce301377ec2062e9ad160dbe7aef
wreece <>
2011-11-14 16:15:48 +0000

    Updates the Razor-B selection with some thought on B-jets

--- CMGTools/Susy/python/Razor/razor_cff.py
+++ CMGTools/Susy/python/Razor/razor_cff.py
@@ -6,57 +6,62 @@ from CMGTools.Common.skims.cmgCandMerge_cfi import *
 from CMGTools.Common.physicsObjectPrinter_cfi import physicsObjectPrinter
 
 # Current loose trigger requires pt > 40; eta < 3; R > 0.14; MR > 150
-#take the leading selected electrons + muons
-from CMGTools.Common.skims.leadingCMGMuonSelector_cfi import leadingCMGMuonSelector
+
+############### Muons
 from CMGTools.Common.skims.cmgMuonSel_cfi import *
 #for box selection
 #the muon trigger requires an 8 gev muon - HLT_Mu8_R005_MR200_v* 
-razorMuonLoose = cmgMuonSel.clone(src = "cmgMuonSel", cut = "(pt() > 10.) && (abs(eta()) < 2.4) && getSelection('cuts_vbtfmuon_isGlobal') && getSelection('cuts_vbtfmuon_numberOfValidTrackerHits')")
+razorMuonLoose = cmgMuonSel.clone(src = "cmgMuonSel", cut = "(pt() > 10.) && (abs(eta()) < 2.4) &&"\
+                                            "getSelection('cuts_vbtfmuon_isGlobal') && getSelection('cuts_vbtfmuon_numberOfValidTrackerHits')")
 razorMuonTight = cmgMuonSel.clone(src = "razorMuonLoose", cut = "(abs(eta()) < 2.1) && relIso(0.5)<0.15 && getSelection('cuts_vbtfmuon')")
 
-razorLeadingMuon = leadingCMGMuonSelector.clone(inputCollection = "razorMuonTight", index = cms.int32(1))
 razorMuonSequence = cms.Sequence(
     razorMuonLoose*
-    razorMuonTight+                                 
-    razorLeadingMuon
+    razorMuonTight
     )
 
+############### Electrons
 from CMGTools.Common.skims.cmgElectronSel_cfi import *
 #for box selection
-razorElectronLoose = cmgElectronSel.clone(src = "cmgElectronSel", cut = '(pt()> 10.) && (abs(eta()) < 2.5) && (abs(eta()) < 1.4442 || abs(eta()) > 1.566) && getSelection("cuts_vbtf95ID")')
-razorElectronTight = cmgElectronSel.clone(src = "razorElectronLoose", cut = '(pt() > 20) && getSelection("cuts_vbtf80ID") && (relIso() < 0.2) && (abs(dxy()) < 0.02)')
-from CMGTools.Common.skims.leadingCMGElectronSelector_cfi import leadingCMGElectronSelector
-razorLeadingElectron = leadingCMGElectronSelector.clone(inputCollection = "razorElectronTight", index = cms.int32(1))
+razorElectronLoose = cmgElectronSel.clone(src = "cmgElectronSel", cut = '(pt()> 10.) && (abs(eta()) < 2.5) &&'\
+                                            '(abs(eta()) < 1.4442 || abs(eta()) > 1.566) && getSelection("cuts_vbtf95ID")')
+razorElectronTight = cmgElectronSel.clone(src = "razorElectronLoose", cut = '(pt() > 20) && getSelection("cuts_vbtf80ID")'\
+                                            '&& (relIso() < 0.2) && (abs(dxy()) < 0.02)')
 # the electron trigger is HLT_Ele10_CaloIdL_CaloIsoVL_TrkIdVL_R005_MR200_v*
 razorElectronSequence = cms.Sequence(
     razorElectronLoose*
-    razorElectronTight+                                   
-    razorLeadingElectron
+    razorElectronTight                                   
     )
 
+############### Jets
 # id the jets
 from CMGTools.Common.skims.cmgPFJetSel_cfi import *
-razorPFJetSel = cmgPFJetSel.clone( src = 'cmgPFJetSel', cut = 'pt()>60 && abs(eta)<3.0' )
-# we need at least one jet
-razorPFJetCount = cmgCandCount.clone( src = 'razorPFJetSel', minNumber = 1 )
-# filter out B-tagged jets for latter use 
-razorPFBJetSel = cmgPFJetSel.clone( src = 'razorPFJetSel', cut = 'getSelection("cuts_btag_loose")' )
-
 #ID at lower pt threshold - used to veto event - the number of jets that fail loose jet ID
-razorPFJetSelID = cmgPFJetSel.clone( src = 'cmgPFJetSel', cut = '(pt()>30 && abs(eta)<3.0) && (!getSelection("cuts_looseJetId"))' )
+razorPFJetSelID = cmgPFJetSel.clone( src = 'cmgPFJetSel', cut = '(pt()>20 && abs(eta)<3.0) && (!getSelection("cuts_looseJetId"))' )
 razorPFJetIDCount = cmgCandCount.clone( src = 'razorPFJetSelID', minNumber = 1 ) #filter inverted below
 
-#combine leading leptons with jets
-razorPFJetsWithLeadingLeptons = cmgCandMerge.clone(
-    src = cms.VInputTag(
-      cms.InputTag("razorLeadingElectron"),
-      cms.InputTag("razorLeadingMuon"),
-      cms.InputTag("razorPFJetSel"),                    
-    )
-    )
-#skim on leading objects - i.e. at least one jet and a tight lepton, or two jets
-razorLeadingObjectCount = cmgCandCount.clone( src = 'razorPFJetsWithLeadingLeptons', minNumber = 2 )
+#cut in HLTRHemisphere - works as electrons are included in jets offline also
+#cut harder on eta so that jets are in tracker for tagging
+razorPFJetSel = cmgPFJetSel.clone( src = 'cmgPFJetSel', cut = 'pt()>40 && abs(eta)<2.4' )
+#clean jets of muons using deltaR
+from CMGTools.Common.miscProducers.deltaRJetMuons_cfi import deltaRJetMuons
+razorPFJetsMuonVeto = deltaRJetMuons.clone(
+    inputCollection = cms.InputTag('razorPFJetSel'),#only require kinematics, not ID
+    vetoCollection = cms.InputTag('razorMuonLoose')
+)
+# we need at least two jets made of real jets or electrons
+razorPFJetCount = cmgCandCount.clone( src = 'razorPFJetsMuonVeto', minNumber = 2 )
+# filter out B-tagged jets for latter use 
+razorPFBJetSel = cmgPFJetSel.clone( src = 'razorPFJetsMuonVeto', cut = 'getSelection("cuts_btag_loose")' )
 
+razorJetSequence = cms.Sequence(
+    razorPFJetSel+
+    razorPFJetSelID+
+    razorPFJetsMuonVeto*
+    razorPFBJetSel    
+)
+
+############### Trigger
 #make a skim on the HLT - should match all Razor triggers
 from CMGTools.Common.skims.cmgTriggerObjectSel_cfi import *
 razorTriggerSel = cmgTriggerObjectSel.clone(
@@ -75,6 +80,7 @@ razorTriggerSequence = cms.Sequence(
 #    razorTriggerInfo
     )
 
+############### Hemispheres
 #make the hemispheres
 from CMGTools.Common.factories.cmgHemi_cfi import cmgHemi
 from CMGTools.Common.factories.cmgDiHemi_cfi import cmgDiHemi
@@ -84,7 +90,7 @@ from CMGTools.Common.factories.cmgDiHemi_cfi import cmgDiHemi
 razorHemiHadBox = cmgHemi.clone(
     cfg = cmgHemi.cfg.clone(
     inputCollection = cms.VInputTag(
-      cms.InputTag("razorPFJetSel")#only require kinematics, not ID
+      cms.InputTag("razorPFJetsMuonVeto")
       ),
       balanceAlgorithm = cms.uint32(1)#use the MassBalance algo
     )
@@ -100,8 +106,8 @@ razorDiHemiHadBox = cmgDiHemi.clone(
     #these are a little looser than the analysis cuts to give some sidebands                            
     razor = cms.PSet(
                      deltaPhi = cms.string('deltaPhi(leg1().phi(),leg2().phi()) < 2.8'),
-                     mr = cms.string('mR() >= 195'),
-                     r = cms.string('R() >= 0.16')
+                     mr = cms.string('mR() >= 200'),
+                     r = cms.string('R() >= 0.14')
     )
     )      
 )
@@ -121,14 +127,6 @@ razorHadronicBoxSequence = cms.Sequence(
 #    razorDiHemiHadBoxInfo                                
     )
 ### now the Mu* box
-#clean jets of muons using deltaR
-from CMGTools.Common.miscProducers.deltaRJetMuons_cfi import deltaRJetMuons
-razorPFJetsMuonVeto = deltaRJetMuons.clone(
-    inputCollection = cms.InputTag('razorPFJetSel'),#only require kinematics, not ID
-    vetoCollection = cms.InputTag('razorMuonLoose')
-)
-# filter out B-tagged jets for latter use 
-razorPFBJetsMuonVeto = cmgPFJetSel.clone( src = 'razorPFJetsMuonVeto', cut = 'getSelection("cuts_btag_loose")' )
 #recalculate the MET
 from CMGTools.Common.Tools.cmgBaseMETModifier_cfi import cmgBaseMETModifier
 razorMuStarMet = cmgBaseMETModifier.clone(
@@ -139,27 +137,15 @@ razorMuStarMet = cmgBaseMETModifier.clone(
     )
 )
 #make the hemispheres using only jets that are not muons
-razorHemiMuStarBox = razorHemiHadBox.clone(
-    cfg = razorHemiHadBox.cfg.clone(
-    inputCollection = cms.VInputTag(
-      cms.InputTag("razorPFJetsMuonVeto")
-      ),
-    )
-)
 razorDiHemiMuStarBox = razorDiHemiHadBox.clone(
     cfg = razorDiHemiHadBox.cfg.clone(
-       leg1Collection = cms.InputTag('razorHemiMuStarBox'),
-       leg2Collection = cms.InputTag('razorHemiMuStarBox'),                                      
        metCollection = cms.InputTag('razorMuStarMet')                 
     ),       
     )
 razorDiHemiMuStarBoxSel = razorDiHemiHadBoxSel.clone( src = 'razorDiHemiMuStarBox' )
 razorDiHemiHistogramsMuStarBox = razorDiHemiHistogramsHadBox.clone(inputCollection = 'razorDiHemiMuStarBox')
 razorMuStarBoxSequence = cms.Sequence(
-    razorPFJetsMuonVeto* 
-    razorPFBJetsMuonVeto*
     razorMuStarMet* 
-    razorHemiMuStarBox*
     razorDiHemiMuStarBox*
     razorDiHemiMuStarBoxSel                                
     )
@@ -172,13 +158,6 @@ razorSelectedDiHemi = cmgCandMerge.clone(
     )
 )
 razorSelectedCount = cmgCandCount.clone( src = 'razorSelectedDiHemi', minNumber = 1 )
-
-razorJetSequence = cms.Sequence(
-    razorPFJetSel+
-    razorPFJetSelID+
-    razorPFBJetSel+
-    razorPFJetsWithLeadingLeptons
-)
 
 from CMGTools.Susy.Razor.razorBoxDef_cff import razorBoxDef
 razorBoxDef = razorBoxDef.clone(
@@ -211,8 +190,8 @@ razorObjectSequence = cms.Sequence(
 from CMGTools.Susy.histograms.pfBJetHistograms_cff import pfBJetHistograms
 from CMGTools.Susy.histograms.pfMultiJetHistograms_cff import pfMultiJetHistograms
 #BJet histograms
-razorBJetHistograms = pfBJetHistograms.clone(inputCollection = cms.InputTag("razorPFJetSel"))
-razorMultiJetHistograms = pfMultiJetHistograms.clone(inputCollection = cms.InputTag("razorPFJetSel"))
+razorBJetHistograms = pfBJetHistograms.clone(inputCollection = cms.InputTag("razorPFJetsMuonVeto"))
+razorMultiJetHistograms = pfMultiJetHistograms.clone(inputCollection = cms.InputTag("razorPFJetsMuonVeto"))
 
 razorHistogrammingSequence  = cms.Sequence(
     #Razor histograms                                       
@@ -233,10 +212,8 @@ razorSkimSequence = cms.Sequence(
     razorObjectSequence + 
     # require no jets to fail ID
     ~razorPFJetIDCount + #note the inversion - veto events with jets that fail the ID
-    # require at least on high pt jet
+    # require at least two high pt jets
     razorPFJetCount+
-    # require another jet or a tight lepton
-    razorLeadingObjectCount+
     # loose cuts on R and MR
     razorSelectedDiHemi*
     razorSelectedCount
