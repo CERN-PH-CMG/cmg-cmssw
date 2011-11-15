b5b4e49ae890b1bdd14f6b4ed24fde523d27c0a3
cbern <>
2011-11-15 18:19:52 +0000

    restored good version of this script

--- CMGTools/Common/scripts/releaseInfo.py
+++ CMGTools/Common/scripts/releaseInfo.py
@@ -8,7 +8,6 @@ import pprint as pp
 widerPrinter = pp.PrettyPrinter(width=120, indent=2)
 pprint = widerPrinter.pprint
 
-relInfoFileName = 'test.py'
 
 
 def showtags():
@@ -27,63 +26,102 @@ def showtags():
 
 
 def relInfoTags(fileName):
-    '''Get tags from a relinfo file'''
-    relInfoDict = eval( file(fileName).read() )
+    '''Get tags from a relinfo file.
+
+    The return tuple is not very nice...
+    Will need to be updated anyway when the conversion to the new format is complete'''
+    relInfo = eval( file(fileName).read() )
     packtags = []
-    for cvsmod, tagpack in relInfoDict.iteritems():
-        packtags.append( (tagpack[1], tagpack[0]) )
-    return sorted(packtags), relInfoDict
-
-localArea = showtags()
-print 'local area:'
-pprint(localArea)
-
-print '-'*70
-
-relInfo, relInfoDict = relInfoTags( relInfoFileName )
-print 'rel info'
-pprint(relInfo)
-
-
-print '-'*70
-
-localAreaSet = set( [ packtag[0] for packtag in localArea ] )
-relInfoSet = set( [packtag[0] for packtag in relInfo] )
-
-print 'elements in local area but not in relinfo:'
-localOnly = localAreaSet.difference(relInfoSet) 
-pprint(localOnly)
-
-print 'elements in relinfo but not in local area:'
-relInfoOnly = relInfoSet.difference(localAreaSet)
-pprint(relInfoOnly)
+    for package, cvsmod, tag in relInfo:
+        packtags.append( (package, cvsmod, tag) )
+    return sorted(packtags)
 
 
 
-new_relInfo = []
-localAreaPacks = [ pack for pack, tag in localArea]
-for cvsmod, tagpack in relInfoDict.items():
-    oldtag = tagpack[0]
-    oldpack = tagpack[1]
-    #print 'OLD:', oldpack, oldtag
-    newtag = None
-    for pack, tag in localArea:
-        if pack == oldpack:
-            newtag = tag
-            break
-    if newtag is None:
-        print 'Package', oldpack, 'not in local area -> removed from relinfo'
-        continue
-    # print 'NEW', oldpack, newtag
-    new_relInfo.append( (oldpack, cvsmod, newtag ) )
+def relInfo( fileName, verbose = False):
 
-# now adding the tags that are in the local area but not in the old release info
-for package, tag in localArea:
-    if package in localOnly:
-        new_relInfo.append( (package, package, tag ) ) 
+    sep_line = '-'*70
+    
+    # getting the package, tag list from showtags
+    localArea = showtags()
+    if verbose:
+        print 'local area:'
+        pprint(localArea)
+        print sep_line
+
+    # get the release_info (to be updated)
+    relInfo = relInfoTags( relInfoFileName )
+    if verbose:
+        print 'rel info'
+        pprint(relInfo)
+        print sep_line
+
+    # find the tags that are in the local area but not in the release info
+    localAreaSet = set( [ packtag[0] for packtag in localArea ] )
+    relInfoSet = set( [packtag[0] for packtag in relInfo] )
+    localOnly = localAreaSet.difference(relInfoSet) 
+    relInfoOnly = relInfoSet.difference(localAreaSet)
+    if verbose:
+        print 'elements in local area but not in relinfo:'
+        pprint(localOnly)
+        print 
+        print 'elements in relinfo but not in local area:'
+        pprint(relInfoOnly)
+        print sep_line
         
-    # print localAreaPacks.get( pack )
+    # build the new release_info.
+    # we start from the information from the old one, which
+    # knows about the cvs module to use for each package
+    new_relInfo = []
+    localAreaPacks = [ pack for pack, tag in localArea]
+    for oldpack, cvsmod, oldtag in relInfo:
+        #print 'OLD:', oldpack, oldtag
+        newtag = None
+        for pack, tag in localArea:
+            if pack == oldpack:
+                newtag = tag
+                break
+        if newtag is None:
+            print 'Package', oldpack, 'not in local area -> removed from relinfo'
+            continue
+        # print 'NEW', oldpack, newtag
+        new_relInfo.append( (oldpack, cvsmod, newtag ) )
 
-pprint( sorted(new_relInfo) )
+    # now adding the tags that are in the local area
+    # but not in the old release info.
+    # for these ones, we just use the package name as cvs module.
+    added = []
+    for package, tag in localArea:
+        if package in localOnly:
+            new_relInfo.append( (package, package, tag ) )
+            added.append( (package, tag) )
 
+    pprint( sorted(new_relInfo) )
+
+    if len(added)>0:
+        print '# The following packages were not in the input release_info.'
+        print '# Please make sure that the CVS module is correct!'
+        for package, tag in added:
+            print '#\t', package.ljust(40), tag 
+    
+if __name__ == '__main__':
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.usage = """
+    %prog  <release_info_file>
+    Prints a version of the release_info updated with the information from the local area.
+    """
+    parser.add_option("-v", "--verbose", dest="verbose", default=False,
+                      help='verbose')
+
+    (options,args) = parser.parse_args()
+
+    if len(args)!=1:
+        print 'please supply a release_info file.'
+        sys.exit(1)
+
+    relInfoFileName = args[0]
+    relInfo( relInfoFileName, options.verbose ) 
 
