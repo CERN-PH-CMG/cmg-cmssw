04d7b5e3254672b81c63747f019e24a16f82f7a4
wreece <>
2011-11-15 15:22:46 +0000

    Adds support for a magic user name

--- CMGTools/Production/python/castorBaseDir.py
+++ CMGTools/Production/python/castorBaseDir.py
@@ -2,8 +2,18 @@
 import os
 import CMGTools.Production.eostools as castortools
 
-def castorBaseDir( user=os.environ['USER'], area = 'user'):
+def castorBaseDir( user=os.environ['USER'], area = None):
     """Gets the top level directory to use for writing for 'user'"""
+    
+    if area is None:
+        #magic string for group area
+        tokens = user.split('_')
+        if tokens and len(tokens) > 1:
+            user = tokens[0]
+            area = tokens[1]
+        else:
+            area = 'user'
+    
     d = '/store/cmst3/%s/%s/CMG' % (area,user)
     exists = castortools.fileExists( castortools.lfnToCastor(d) )
     if exists:
