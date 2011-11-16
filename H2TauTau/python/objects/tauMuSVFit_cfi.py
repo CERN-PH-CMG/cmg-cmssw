47561ef26ef37b33161072f95a837ee028a59170
cbern <>
2011-11-16 07:49:26 +0000

    using met from di-object in svfit

--- H2TauTau/python/objects/tauMuSVFit_cfi.py
+++ H2TauTau/python/objects/tauMuSVFit_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 tauMuSVFit = cms.EDProducer(
     "DiTauWithSVFitProducer",
     diTauSrc = cms.InputTag("cmgTauMuCorPreSel"),
-    metSrc = cms.InputTag("cmgPFMET"),
+    # metSrc = cms.InputTag("cmgPFMET"),
     metsigSrc = cms.InputTag("PFMETSignificanceAK5"),
     verbose = cms.untracked.bool( False )
     )
