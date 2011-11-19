5ea2122c326ae5bd177a94a5ff001c42a5c88c39
cbern <>
2011-11-19 18:28:38 +0000

    added a comment

--- CMGTools/Common/python/factories/cmgTauMuCor_cfi.py
+++ CMGTools/Common/python/factories/cmgTauMuCor_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 cmgTauMuCorFactory = cms.PSet(
+    # leg1 and leg2 are dummy collections here
     leg1Collection = cms.InputTag(''),
     leg2Collection = cms.InputTag(''),
     metCollection = cms.InputTag('recoilCorrectedMET'),
