4725fd74aa1b5369bad1c90f9f113a5109388f7e
wreece <>
2011-11-23 13:55:38 +0000

    Missing file

---
+++ CMGTools/Susy/python/MultiJet/multijetEventContent_cff.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+from CMGTools.Common.eventContent.everything_cff import *
+
+multijetEventContent = cms.untracked.vstring()
+
+multijetEventContent += cms.untracked.vstring(
+#                                           'keep *_multi*_*_*',
+                                            'keep *_multiPFJetSel60_*_*',
+                                            'keep *_multiPFBJetSel60_*_*',
+                                            'keep *_multiMuonLoose_*_*',
+                                            'keep *_multiPFJetsMuonVeto_*_*',
+                                            'keep *_multiPFJetsMuonRequired_*_*',
+                                            'keep *_multiPFBJetsMuonRequired_*_*',
+                                           )
