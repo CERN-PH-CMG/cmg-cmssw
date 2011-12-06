672efd3b435aa79159a13aa7707804b599e46065
wreece <>
2011-12-06 12:48:47 +0000

    lates macro for maxime

--- CMGTools/Susy/macros/MultiJet/multijetHistograms.py
+++ CMGTools/Susy/macros/MultiJet/multijetHistograms.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python
 
 import ROOT as rt
-import sys, math
+import sys, math, glob
 from DataFormats.FWLite import Events, Handle
 from CMGTools.RootTools import RootFile
 
+def listDirectory(dir):
+    return glob.glob('%s/*.root' % dir)
+
+def getFiles(datasets, user, pattern):
+    
+    files = []
+    
+    for d in datasets:
+        ds = datasetToSource(
+                             user,
+                             d,
+                             pattern
+                             )
+        files.extend(ds.fileNames)
+
+    #root://eoscms//eos/cms/store/cmst3/user/wreece/CMG/MultiJet/Run2011A-PromptReco-v6/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211/susy_tree_CMG_0.root
+    return ['root://eoscms//eos/cms%s' % f for f in files]
+
 if __name__ == '__main__':
+
+    from CMGTools.Production.datasetToSource import *
+#    files = getFiles(
+#                     ['/MultiJet/Run2011A-PromptReco-v6/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211',
+#                      '/MultiJet/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211',
+#                      '/MultiJet/Run2011A-May10ReReco-v1/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211',
+#                      '/MultiJet/Run2011A-05Aug2011-v1/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211',
+#                      '/MultiJet/Run2011B-PromptReco-v1/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim/wreece_051211'
+#                      ],
+#                      'wreece',
+#                      'susy_tree_CMG_[0-9]+.root'
+#                     )
+    files = getFiles(
+                     ['/TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_4_0/wreece_281111/razorMJ/wreece_051211',
+                      ],
+                      'wreece',
+                      'susy_tree_CMG_[0-9]+.root'
+                     )
+    #
 
     # https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideAboutPythonConfigFile#VarParsing_Example
     from FWCore.ParameterSet.VarParsing import VarParsing
     options = VarParsing ('python')
     options.outputFile = 'multijetHistograms.root'
+    
+    options.register ('inputDirectory',
+                  None, # default value
+                  VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.varType.string,          # string, int, or float
+                  "A directory to read root files from")
+    
     options.parseArguments()
+    #print options.inputFiles
+    options.inputFiles = files
 
     # use Varparsing object
     events = Events(options)
@@ -33,14 +79,22 @@ if __name__ == '__main__':
     ptMu = rt.TH1D('ptMu','pT of selected muons',15,10,200)
     store.add(ptMu)
     
-    ptMuJ = rt.TH1D('ptMuJ','pT of selected muons within jets',15,10,200)
+    ptMuJ = rt.TH1D('ptMuJ','pT of selected muons within jets',30,10,300)
     store.add(ptMuJ)
     
-    deltaRMuJ = rt.TH1D('deltaRMuJ','deltaR between the muon and the b jet',15,0,200)
-    store.add(ptMuJ)
+    deltaRMuJ = rt.TH1D('deltaRMuJ','deltaR between the muon and the b jet',40,0,4)
+    store.add(deltaRMuJ)
     
     ptMuJet = rt.TH2D('ptMuJet','pt of muon and jet',15,50,600,15,50,600)
     store.add(ptMuJet)
+    
+    tcheAllBJets = rt.TH1D('tcheAllBJets','TCHE',50,0,50)
+    store.add(tcheAllBJets)
+    
+    tcheMuBJets = rt.TH1D('tcheMuBJets','TCHE',50,0,50)
+    store.add(tcheMuBJets)
+    
+#    ssvhptAllBJets = rt.TH1D('ssvhptAllBJets','TCHE',50,0,50)
     
     def deltaR(a,b):
         deta = a.eta() - b.eta()
@@ -48,8 +102,17 @@ if __name__ == '__main__':
         dr = deta*deta + dphi*dphi
         return math.sqrt(dr)
 
+    event_count = 0
+
     # loop over events
     for event in events:
+        
+        if not event_count % 1000:
+            print 'Event %d' % event_count
+        event_count += 1
+        
+#        if event_count > 100000:
+#            break
         
         #start by testing that the Razor path fired this event
         event.getByLabel(('TriggerResults','','SUSY'),pathTriggerH)
@@ -64,10 +127,10 @@ if __name__ == '__main__':
         event.getByLabel(('multiMuonLoose'), muonH)
         muons = muonH.product()
         
-        event.getByLabel(('multiPFBJetSel60'), allPFBJetsH)
-        allPFBJets = allPFBJetsH.product()
+        event.getByLabel(('cmgPFJetSel'), allPFBJetsH)
+        allPFBJets = [j for j in allPFBJetsH.product() if (j.btag(0) >= 3.3 and j.pt() >= 40 and abs(j.eta()) <= 2.5)]
         
-        event.getByLabel(('multiPFBJetsMuonRequired'), muonPFBJetsH)
+        event.getByLabel(('multiPFBJetsMuonRequiredTCHEM'), muonPFBJetsH)
         muonPFBJets = muonPFBJetsH.product()
         
         for m in muons:
@@ -75,12 +138,16 @@ if __name__ == '__main__':
         
         for b in allPFBJets:
             ptAllBJets.Fill(b.pt())
+            tcheAllBJets.Fill(b.btag(0))
             
         for b in muonPFBJets:
             ptMuBJets.Fill(b.pt())
+            tcheMuBJets.Fill(b.btag(0))
             
             for m in muons:
-                if deltaR(b,m) < 0.5:
+                dr = deltaR(b,m)
+                deltaRMuJ.Fill(dr)
+                if dr < 0.5:
                     ptMuJet.Fill(m.pt(),b.pt())
                     ptMuJ.Fill(m.pt())
                 
