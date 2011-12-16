36238c4c2260df81ff366634920ece6fc5fb27ff
wreece <>
2011-12-16 15:55:57 +0000

    Removes more of the paths from the skim

--- Susy/susy_cfg.py
+++ Susy/susy_cfg.py
@@ -1,9 +1,11 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 ##########
+runOnMC = False
 from CMGTools.Common.Tools.applyJSON_cff import applyJSON
 json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt'
-applyJSON(process, json )
+if not runOnMC:
+    applyJSON(process, json )
 
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
@@ -20,12 +22,13 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 doSkimHighMET = False
 cut_highMET = 'pt()>50'
-process.setName_('SUSY')
+#process.setName_('SUSY')
+process.setName_('MJSkim')
 
 from CMGTools.Production.datasetToSource import *
 process.source = datasetToSource(
-    'cmgtools',
-    '/MultiJet/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0',
+    'wreece',
+    '/MultiJet/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0/L1MultiJetSkim',
     #'/TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_3_0',
     'tree_CMG_[0-9]+\\.root'
     ) 
@@ -46,18 +49,17 @@ process.load('CMGTools.Susy.susy_cff')
 # process.susySchedule.remove( process.runInfoAccountingPath )
 
 process.schedule = cms.Schedule(
-    process.leptonicStopPath,
-    process.leptonicStopTriggerPath,
+#    process.leptonicStopPath,
+#    process.leptonicStopTriggerPath,
     process.multijetPath,
     process.multijetTriggerPath,                                
 #    process.RA1Path,
 #    process.RA2Path,
-    process.razorPath,
-    process.razorEleMuPath,
-    process.razorTriggerPath,
+#    process.razorPath,
+#    process.razorEleMuPath,
+#    process.razorTriggerPath,
 #    process.LPPath,
     process.razorMJPath,
-    process.razorMJTriggerPath,
     process.outpath
     )
 
@@ -67,14 +69,17 @@ process.schedule = cms.Schedule(
 from CMGTools.Susy.susyEventContent_cff import susyEventContent
 process.out.fileName = cms.untracked.string('susy_tree_%s.root' %  outFileNameExt)
 process.out.outputCommands = cms.untracked.vstring('drop *')
-process.out.outputCommands += susyEventContent
 from CMGTools.Common.eventContent.eventCleaning_cff import eventCleaning
 process.out.outputCommands.extend( eventCleaning )
+process.out.outputCommands += susyEventContent
 
-process.out.SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('leptonicStopPath','leptonicStopTriggerPath','multijetPath','multijetTriggerPath','razorEleMuPath','razorPath','razorTriggerPath','razorMJPath','razorMJTriggerPath') )
+#process.out.SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('leptonicStopPath','leptonicStopTriggerPath','multijetPath','multijetTriggerPath','razorEleMuPath','razorPath','razorTriggerPath','razorMJPath','razorMJTriggerPath') )
+process.out.SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('multijetPath','razorMJPath') )
+
 #plot the correlations between the selection paths
 process.load('CMGTools.Common.histograms.triggerCorrelationHistograms_cfi')
-process.triggerCorrelationHistograms.names = cms.untracked.vstring('leptonicStopPath','leptonicStopTriggerPath','multijetPath','multijetTriggerPath','razorEleMuPath','razorPath','razorTriggerPath','razorMJPath','razorMJTriggerPath')
+#process.triggerCorrelationHistograms.names = cms.untracked.vstring('leptonicStopPath','leptonicStopTriggerPath','multijetPath','multijetTriggerPath','razorEleMuPath','razorPath','razorTriggerPath','razorMJPath','razorMJTriggerPath')
+process.triggerCorrelationHistograms.names = cms.untracked.vstring('multijetPath','razorMJPath')
 process.schedule.append( process.triggerCorrelationHistogramsEndPath )
 
 process.TFileService = cms.Service(
