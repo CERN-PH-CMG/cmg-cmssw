30a1c96bc625ff7a8f95c78d73e868ac00fce996
cbern <>
2012-01-05 17:11:54 +0000

    new Bleeding edge recipe

--- Common/python/release_info/CMSSW_4_2_8_exp_colin_BleedingEdge.py
+++ Common/python/release_info/CMSSW_4_2_8_exp_colin_BleedingEdge.py
@@ -1,17 +1,19 @@
-[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_tauEOverP_29Nov11'),
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_vertexWeight2_fromJose_29Nov11'),
-  ('CMGTools/H2TauTau', 'UserCode/CMG/CMGTools/H2TauTau', 'cbern_tauEOverP_29Nov11'),
-  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_fromWill_25Nov11'),
+# Package CMGTools/H2TauTau not in local area -> removed from relinfo
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_ptd_05Jan12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_ptd_05Jan12'),
+  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_CMSDataset_04Jan12'),
   ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_mcyield_29Nov11'),
   ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'cbern_tutorial_12Oct11'),
-  ('CommonTools/ParticleFlow', 'CommonTools/ParticleFlow', 'B4_2_X_cbern_dbetaoff_14Oct11'),
+  ('CommonTools/ParticleFlow', 'CommonTools/ParticleFlow', 'B4_2_X_cbern_dbetaoff_04Jan12_PFPileUpSpeed'),
   ('DataFormats/AnomalousEcalDataFormats', 'UserCode/csander/DataFormats/AnomalousEcalDataFormats', 'V110523_BE'),
   ('DataFormats/PatCandidates', 'DataFormats/PatCandidates', 'V06-04-19-02'),
   ('GeneratorInterface/GenFilters', 'GeneratorInterface/GenFilters', 'V00-11-00-01'),
   ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
+  ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-16'),
   ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-06-48'),
   ('PhysicsTools/SelectorUtils', 'PhysicsTools/SelectorUtils', 'V00-03-24'),
   ('PhysicsTools/UtilAlgos', 'PhysicsTools/UtilAlgos', 'V08-02-14'),
+  ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-14'),
   ('RecoJets/Configuration', 'RecoJets/Configuration', 'V02-04-17'),
   ('RecoLuminosity/LumiDB', 'RecoLuminosity/LumiDB', 'V03-03-07'),
   ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23'),
@@ -19,3 +21,7 @@
   ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-02-07'),
   ('RecoTauTag/TauTagTools', 'RecoTauTag/TauTagTools', 'V01-02-00'),
   ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
+# The following packages were not in the input release_info.
+# Please make sure that the CVS module is correct!
+#	PhysicsTools/HepMCCandAlgos              V11-03-16
+#	PhysicsTools/Utilities                   V08-03-14
