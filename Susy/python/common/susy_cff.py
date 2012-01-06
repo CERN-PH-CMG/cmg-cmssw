65800b5e30b5de65ae57b69c9d46fac265f2294e
wreece <>
2012-01-06 14:15:15 +0000

    Changes the balance algo and trys to work around the bool reading problem

--- Susy/python/common/susy_cff.py
+++ Susy/python/common/susy_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.analysis_cff import *
 from CMGTools.Susy.common.susyLepton_cff import *
+from CMGTools.Susy.common.boolToInt_cff import *
 from CMGTools.Susy.common.jetId_cff import *
 from CMGTools.Susy.RA1.RA1_cff import *
 from CMGTools.Susy.RA2.RA2_cff import *
@@ -11,6 +12,7 @@ from CMGTools.Susy.LP.LP_cff import *
 
 susySequence  = cms.Sequence(
     susyLeptonSequence +
+    boolToIntSequence +
     jetIdSequence + 
     RA1Sequence +
     RA2Sequence +
