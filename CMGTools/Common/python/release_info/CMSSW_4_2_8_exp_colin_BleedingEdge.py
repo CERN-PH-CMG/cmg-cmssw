2e7db5ca92c7dd824e9b1e69481878ee6056f5b6
cbern <>
2012-01-09 16:38:47 +0000

    updated bleeding edge with new trigger matching from Jose

--- CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_colin_BleedingEdge.py
+++ CMGTools/Common/python/release_info/CMSSW_4_2_8_exp_colin_BleedingEdge.py
@@ -1,6 +1,5 @@
-# Package CMGTools/H2TauTau not in local area -> removed from relinfo
 [ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_ptd_05Jan12'),
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_ptd_05Jan12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'jose_TrigObjs_9Jan12'),
   ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_CMSDataset_04Jan12'),
   ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_mcyield_29Nov11'),
   ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'cbern_tutorial_12Oct11'),
@@ -13,7 +12,7 @@
   ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-06-48'),
   ('PhysicsTools/SelectorUtils', 'PhysicsTools/SelectorUtils', 'V00-03-24'),
   ('PhysicsTools/UtilAlgos', 'PhysicsTools/UtilAlgos', 'V08-02-14'),
-  ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-14'),
+  ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-16'),
   ('RecoJets/Configuration', 'RecoJets/Configuration', 'V02-04-17'),
   ('RecoLuminosity/LumiDB', 'RecoLuminosity/LumiDB', 'V03-03-07'),
   ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23'),
@@ -21,7 +20,3 @@
   ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-02-07'),
   ('RecoTauTag/TauTagTools', 'RecoTauTag/TauTagTools', 'V01-02-00'),
   ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
-# The following packages were not in the input release_info.
-# Please make sure that the CVS module is correct!
-#	PhysicsTools/HepMCCandAlgos              V11-03-16
-#	PhysicsTools/Utilities                   V08-03-14
