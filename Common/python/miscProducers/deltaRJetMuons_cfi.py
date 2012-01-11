b23b2d428f7254354d6009d0135c68b520b2c505
bonato <>
2012-01-11 11:38:31 +0000

    Improved DeltaRVetoProducer

--- Common/python/miscProducers/deltaRJetMuons_cfi.py
+++ Common/python/miscProducers/deltaRJetMuons_cfi.py
@@ -3,6 +3,12 @@ import FWCore.ParameterSet.Config as cms
 deltaRJetMuons = cms.EDProducer(
     "DeltaRVetoProducerPFJet",
     inputCollection = cms.InputTag('cmgPFJetSel'),
-    vetoCollection = cms.InputTag('cmgMuonSel'),
-    minDeltaR = cms.double(0.3)
-)
+     MatchingParams = cms.VPSet(
+                                cms.PSet(                                     
+                                    vetoCollection=cms.InputTag("cmgMuonSel"),
+                                    minDeltaR=cms.double(0.3),
+                                    removeMatchedObject=cms.bool(True)
+                                    )
+                                ),
+    verbose = cms.untracked.bool(True)
+    )
