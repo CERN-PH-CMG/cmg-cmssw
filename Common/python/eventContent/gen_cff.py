c2b21b130a1b01e5efce4d96d7a74b55dc73e2ca
cbern <>
2012-01-13 09:58:01 +0000

    added gen leptons status 1 and 2 to eventcontent

--- Common/python/eventContent/gen_cff.py
+++ Common/python/eventContent/gen_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 gen = cms.untracked.vstring(
     'keep recoGenParticles_genParticlesStatus3_*_*',
+    'keep recoGenParticles_genLeptonsStatus2_*_*',
+    'keep recoGenParticles_genLeptonsStatus1_*_*',    
     'keep recoGenJetedmPtrcmgPhysicsObjectWithPtrs_genJetSel_*_*',
     'keep *_addPileupInfo_*_*',
     'keep GenEventInfoProduct_*_*_*',
