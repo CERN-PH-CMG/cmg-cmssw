5f9d55a8914856b4f657c0883ff1c11fd8d0e69b
cbern <>
2012-01-20 12:52:43 +0000

    PFAOD study tools

---
+++ Production/PFAOD/V2_cfg.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+sys.path.append( os.getcwd() )
+from base import *
+
+import __main__
+
+tag = 'V2'
+
+output = '_'.join( [sampleStr, tag] )
+output += '.root'
+
+print 'output file', output 
+
+process.out.fileName = output 
+
+process.out.outputCommands.extend(
+    [
+      'keep GenEventInfoProduct_*_*_*',
+      'keep *_ak5GenJets_*_*',
+      'keep *_ak5CaloJets_*_*',
+      'keep *_ak5JetID_*_*',
+      'keep *_ak5JetExtender_*_*',
+      'keep *_ak7GenJets_*_*',
+      'keep *_ak7CaloJets_*_*',
+      'keep *_ak7JetID_*_*',
+      'keep *_ak7JetExtender_*_*',
+      #------- PFJet collections --------
+      'keep *_kt6PFJets_rho_*',
+      'keep *_kt6PFJets_sigma_*',
+      'keep *_ak5PFJets_*_*',        'keep *_ak7PFJets_*_*',
+      #------- Trigger collections ------
+      'keep edmTriggerResults_TriggerResults_*_*',
+      'keep *_hltTriggerSummaryAOD_*_*',
+      'keep L1GlobalTriggerObjectMapRecord_*_*_*',
+      'keep L1GlobalTriggerReadoutRecord_*_*_*',
+      #------- Various collections ------
+      'keep *_EventAuxilary_*_*',
+      'keep *_offlinePrimaryVertices_*_*',
+      'keep *_offlinePrimaryVerticesWithBS_*_*',
+      #------- MET collections ----------
+      'keep *_met_*_*',
+      'keep *_pfMet_*_*'
+    ])
---
+++ Production/PFAOD/run.py
@@ -0,0 +1,54 @@
+import os
+
+# start from a PFAOD file
+
+inputs = [
+    'AOD_TTJets.root',
+    'AOD_DoubleMu.root',
+    'AOD_DoubleElectron.root',
+    'AOD_HT.root'
+    ]
+
+# run a set of cfgs on that file
+
+cfgs = [
+    'photons_cfg.py',
+    'all_cfg.py',
+    'noCastor_cfg.py',
+    'tevMuons_cfg.py',
+    'bare_cfg.py',
+    'V2_cfg.py',
+    'ak7calo_cfg.py',
+    'caloClusters_cfg.py'
+    ]
+
+# cfgs = cfgs[:1]
+
+def prepareTestBench(input):
+    outputDir = 'Out_' + os.path.splitext( input )[0]
+    print 'preparing output directory', outputDir
+    try:
+        os.mkdir(outputDir)
+    except OSError:
+        pass
+    def copy( file ):
+        os.system( ' '.join( ['cp',file,outputDir] ))
+    copy(input)
+    map( copy, cfgs)
+    copy( 'base.py' )
+    return outputDir 
+
+def processInput(input):
+    print 'processing', input
+    outputDir = prepareTestBench(input) 
+    baseDir = os.getcwd()
+    os.chdir( outputDir )
+    for cfg in cfgs:
+        print cfg
+        cmd = ['cmsRun', cfg, input]
+        os.system( ' '.join(cmd) ) 
+    os.chdir( baseDir )
+        
+for input in inputs:
+    processInput(input)
+
