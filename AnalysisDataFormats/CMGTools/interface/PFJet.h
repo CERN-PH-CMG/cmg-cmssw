e682354cd286b975f5e8147b36e82fc063cad640
cbern <>
2012-02-02 14:46:58 +0000

    - leptons : can now use charged particle isolation - documentation added

--- AnalysisDataFormats/CMGTools/interface/PFJet.h
+++ AnalysisDataFormats/CMGTools/interface/PFJet.h
@@ -17,6 +17,18 @@ namespace cmg {
   //forward def needed
   class PFJet;
   
+  /** Class representing PFJets in the CMG framework.
+      
+  In addition to the attributes from the BaseJet mother class, 
+  PFJets contain a vector of PFJetComponent, indexed by the particle type 
+  defined in reco::PFCandidate. 
+  
+  Each component contains:
+  - fraction of the jet energy carried by this type of constituents (e.g. charged hadrons)
+  - number of such constituents in the jet
+  - energy sum of such constituents
+  - pt sum of such consistuents
+  */
   class PFJet : public BaseJet {
   public:
     ///number of PFCandidates types, see PFCandidate.h
@@ -27,9 +39,19 @@ namespace cmg {
 
     virtual ~PFJet(){}
     
+    /// \return the component corresponding to this type of PFCandidate.
+    /// for example, do:
+    ///  jet.component( reco::PFCandidate::h)  
+    /// or 
+    ///  jet.component( 1 ) 
+    /// to get the PFJetComponent corresponding to charged hadrons
     const PFJetComponent& component(unsigned ci) const; 
-
+    
+    /// \return total number of consistuents in the jet
     const int nConstituents() const;
+
+    /// \return the ptd variable, for quark-gluon jet discrimination 
+    /// \sum pt^2 / (\sum pt)^2
     float ptd() const {return ptd_;}
 
     friend class PFJetFactory;
