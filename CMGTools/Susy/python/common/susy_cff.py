150a3c36e137779288e6be6bf9d733a6ab727d09
wreece <>
2012-02-02 15:42:09 +0000

    Adds the PDF dumper to the process

--- CMGTools/Susy/python/common/susy_cff.py
+++ CMGTools/Susy/python/common/susy_cff.py
@@ -4,6 +4,7 @@ from CMGTools.Common.analysis_cff import *
 from CMGTools.Susy.common.susyLepton_cff import *
 from CMGTools.Susy.common.boolToInt_cff import *
 from CMGTools.Susy.common.jetId_cff import *
+from CMGTools.Susy.common.dumpPDFWeights_cff import *
 from CMGTools.Susy.RA1.RA1_cff import *
 from CMGTools.Susy.RA2.RA2_cff import *
 from CMGTools.Susy.Razor.razor_cff import *
@@ -11,5 +12,6 @@ from CMGTools.Susy.LP.LP_cff import *
 
 
 susySequence  = cms.Sequence(
-    boolToIntSequence
+    boolToIntSequence+
+    dumpPdfWeights
     )
