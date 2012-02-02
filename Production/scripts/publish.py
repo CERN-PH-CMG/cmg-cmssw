fad6cd9b6e190a8ed911a1a8e457e0b1336f68d2
pmeckiff <>
2012-02-02 17:54:35 +0000

    Fixed multi submit problem

--- Production/scripts/publish.py
+++ Production/scripts/publish.py
@@ -15,6 +15,7 @@ from DBSAPI.dbsApi import DbsApi
 from DBSAPI.dbsApiException import *
 from DBSAPI.dbsException import *
 from CMGTools.Production.publish import publish
+from optparse import *
 
 
 
@@ -30,46 +31,48 @@ Example:
 publish.py -F cbern /VBF_HToTauTau_M-120_7TeV-powheg-pythia6-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM/V2/PAT_CMG_V2_5_0_Test_v2
 """
     
+    group = OptionGroup(parser, "Publish Options", """These options are to be used alongside the publish.py
+    without affecting DBS options""")
     # If user is not specified default is current user
     # This option will be used to find dataset on castor, and assign dataset on savannah
-    parser.add_option("-F", "--fileown", 
+    group.add_option("-F", "--fileown", 
                       dest="fileown",
                       help="User who is the files owner on Castor." ,
                       default=os.environ['USER'] )
     # If specified is used to log in to savannah (only required if user that created the dataset,
     # is different to user publishing it)
-    parser.add_option("-u", "--user",
+    group.add_option("-u", "--user",
                       action = "store",
                       dest="user",
                       help="Specify the username to access both the DBS and savannah servers. Default is $USER.",
                       default=os.environ['USER'] )
     # If the purpose is to test the software use this parameter, it will not be recognised by the
     # non-testing algorithm
-    parser.add_option("-T", "--test",
+    group.add_option("-T", "--test",
                       action = "store_true",
                       dest="test",
                       help="Flag task as a test",
                       default=False )
     # If user wants to add their own comments
-    parser.add_option("-C", "--comment",
+    group.add_option("-C", "--comment",
                       action = "store",
                       dest="commented",
                       help="Take comment as arg",
                       default = None)
     # If user wants to add their own comments
-    parser.add_option("-D", "--dbs",
+    group.add_option("-D", "--dbs",
                       action = "store_true",
                       dest="dbs",
                       help="Use DBS",
                       default = False)
     # If user wants to add multiple datasets from file
-    parser.add_option("-M", "--multi",
+    group.add_option("-M", "--multi",
                       action = "store_true",
                       dest="multi",
                       help="""Argument is now LFN to location of .txt file
                       Entries in the file should be on independant lines in the form: dataset fileowner 'comment'""",
                       default = False)
-    
+    parser.add_option_group(group)
     
     (options, args) = optManager.getOpt()
     options.url="http://cmsphys05.cern.ch:8081/cms_dbs_prod_local_01/servlet/DBSServlet"
@@ -81,7 +84,7 @@ publish.py -F cbern /VBF_HToTauTau_M-120_7TeV-powheg-pythia6-tauola/Summer11-PU_
     else: dbsApi = None
     # Allow no more than one argument
     if len(args)!=1:
-        parser.print_help()
+        group.print_help()
         sys.exit(1)
     
     
@@ -99,11 +102,17 @@ publish.py -F cbern /VBF_HToTauTau_M-120_7TeV-powheg-pythia6-tauola/Summer11-PU_
             try:
                 fileown = None
                 dataset = line.split(" ")[0]
-                if not re.search("---", dataset):
-                    fileown = line.split("'")[0].split(" ")[1]
-                comment = None
+                token = '"'
                 if len(line.split("'"))>1:
-                    comment = line.rstrip("'").split("'")[1]
+                	token = "'"
+                if not re.search("---", dataset):
+                	if len(line.split(token)[0].split(" "))> 1:
+                		fileown = line.split(token)[0].split(" ")[1]
+                	else:
+                		fileown = options.fileown
+                comment = None
+                if len(line.split(token))>1:
+                    comment = line.rstrip(token).split(token)[1]
                 publish(dataset,fileown,comment,options.test,dbsApi,options.user,password)
             except Exception as err:
                 print err, "\nDataset not published"
