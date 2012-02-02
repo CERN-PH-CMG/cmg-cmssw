25ca26812f40e4d32555917a08d0dafb61cdc6e3
cbern <>
2012-02-02 10:36:01 +0000

    now compatible with 44. removed AK5LC and AK7

--- Common/python/Tools/getGlobalTag.py
+++ Common/python/Tools/getGlobalTag.py
@@ -9,8 +9,10 @@ def getGlobalTag(runOnMC):
         #default to startup, but use mc otherwise
         #return autoCond.get('startup',autoCond['mc'])
         #return 'START42_V13::All'
-        return 'START42_V17::All'
+        #return 'START42_V17::All'
+        return 'START44_V10::All'
     else:
         #return autoCond['com10']
         #return 'GR_R_42_V19::All'
-        return 'GR_R_42_V23::All'
+        # return 'GR_R_42_V23::All'
+        return 'GR_R_44_V13::All'
--- Common/python/eventCleaning/HBHENoiseFilterResultProducer2010_cfi.py
+++ Common/python/eventCleaning/HBHENoiseFilterResultProducer2010_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 HBHENoiseFilterResultProducer2010 = cms.EDProducer(
     'HBHENoiseFilterResultProducer',
-    label = cms.InputTag('hcalnoise','','RECO'),
+    noiselabel = cms.InputTag('hcalnoise'),
     minRatio = cms.double(0.70),#use old E2/E10 cut
     maxRatio = cms.double(0.96),#use old E2/E10 cut
     useTS4TS5 = cms.bool(False),#do not use R45 cut 
@@ -18,4 +18,5 @@ HBHENoiseFilterResultProducer2010 = cms.EDProducer(
     minIsolatedNoiseSumE = cms.double(999999.),
     minIsolatedNoiseSumEt = cms.double(999999.)
     )
+
 
