ccc33da35dd436a206e3903bf2515757f537eb49
cbern <>
2012-02-02 10:41:30 +0000

    44X bleeding edge

---
+++ Common/python/release_info/CMSSW_4_4_3_exp_colin_BleedingEdge.py
@@ -0,0 +1,24 @@
+# Package CMGTools/H2TauTau not in local area -> removed from relinfo
+# Package CommonTools/ParticleFlow not in local area -> removed from relinfo
+# Package DataFormats/AnomalousEcalDataFormats not in local area -> removed from relinfo
+# Package DataFormats/PatCandidates not in local area -> removed from relinfo
+# Package GeneratorInterface/GenFilters not in local area -> removed from relinfo
+# Package PhysicsTools/PatUtils not in local area -> removed from relinfo
+# Package PhysicsTools/SelectorUtils not in local area -> removed from relinfo
+# Package PhysicsTools/UtilAlgos not in local area -> removed from relinfo
+# Package RecoJets/Configuration not in local area -> removed from relinfo
+# Package RecoTauTag/TauTagTools not in local area -> removed from relinfo
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_pzeta_17Jan11'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_44X_02Feb12'),
+  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_02Feb12'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_fwlite_b_01Feb12'),
+  ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'jhegeman_V2401_16Jan12'),
+  ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
+  ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-16'),
+  ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-07-38-02'),
+  ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-16'),
+  ('RecoLuminosity/LumiDB', 'RecoLuminosity/LumiDB', 'V03-03-07'),
+  ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23'),
+  ('RecoTauTag/Configuration', 'RecoTauTag/Configuration', 'V01-02-12'),
+  ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-02-16'),
+  ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
