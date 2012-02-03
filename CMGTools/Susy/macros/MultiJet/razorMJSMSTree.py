7bc5af4ad2cd64ddbb8fd4a2879c618b1b15135d
wreece <>
2012-02-03 14:20:47 +0000

    Updates the various dataset scripts

--- CMGTools/Susy/macros/MultiJet/razorMJSMSTree.py
+++ CMGTools/Susy/macros/MultiJet/razorMJSMSTree.py
@@ -5,14 +5,8 @@ import os, sys, math, glob
 from DataFormats.FWLite import Events, Handle
 from CMGTools.RootTools import RootFile
 
-def listDirectory(dir, inputFiles, maxFiles):
-    if not os.path.exists(dir):
-        raise Exception("The directory '%s' does not exist" % dir)
-    files = glob.glob('%s/*.root' % dir)
-    if len(files) > maxFiles:
-        files = files[:maxFiles]
-    inputFiles.extend(files)
-    return len(files)
+import numpy as n
+from ROOT import std
 
 def getFiles(datasets, user, pattern):
     
@@ -35,11 +29,6 @@ if __name__ == '__main__':
     from FWCore.ParameterSet.VarParsing import VarParsing
     options = VarParsing ('python')
     options.outputFile = None
-    options.register ('inputDirectory',
-                  None, # default value
-                  VarParsing.multiplicity.singleton, # singleton or list
-                  VarParsing.varType.string,          # string, int, or float
-                  "A directory to read root files from")
     options.register ('outputDirectory',
                   '.', # default value
                   VarParsing.multiplicity.singleton, # singleton or list
@@ -57,12 +46,9 @@ if __name__ == '__main__':
                   "The maximum number of files to read")
     
     options.parseArguments()
-    if options.inputDirectory is not None:
-        listDirectory(options.inputDirectory, options.inputFiles, options.maxFiles)
-        
     if True:
         names = [f for f in options.datasetName.split('/') if f]
-        name = '%s-%s-%s.root' % (names[0],names[1],names[-1])
+        name = '%s-%s-%s-SMS.root' % (names[0],names[1],names[-1])
         options.outputFile = os.path.join(options.outputDirectory,name)
         
     files = getFiles(
@@ -78,42 +64,32 @@ if __name__ == '__main__':
 
     rt.gROOT.ProcessLine("""
 struct Variables{\
-    Double_t mR;\
-    Double_t R;\
-    Double_t Rsq;\
-    Double_t mRMB;\
-    Double_t RsqMB;\
+    Double_t MR;\
+    Double_t MR_JES_UP;\
+    Double_t MR_JES_DOWN;\
+    Double_t RSQ;\
+    Double_t RSQ_JES_UP;\
+    Double_t RSQ_JES_DOWN;\
     Double_t maxTCHE;\
-    Double_t maxSSVP;\
-    Double_t nextTCHE;\
-    Double_t nextSSVP;\
-    Double_t met;\
-    Double_t jet1Pt;\
-    Double_t jet2Pt;\
-    Double_t jet3Pt;\
-    Double_t jet4Pt;\
-    Double_t jet5Pt;\
-    Double_t jet6Pt;\
-    Double_t jet1Eta;\
-    Double_t jet2Eta;\
-    Double_t jet3Eta;\
-    Double_t jet4Eta;\
-    Double_t jet5Eta;\
-    Double_t jet6Eta;\
-    Double_t hemi1Mass;\
-    Double_t hemi2Mass;\
-    Double_t hemi1MassMB;\
-    Double_t hemi2MassMB;\
+    Double_t maxTCHE_PT;\
+    Double_t maxTCHE_ETA;\
     Double_t mStop;\
     Double_t mLSP;\
 };""")
-    
+
+    rt.gROOT.ProcessLine("""
+struct Arrays{\
+    std::vector<double> CTEQ66_Wi;\
+    std::vector<double> MRST2006NNLO_Wi;\
+};""")
+
     rt.gROOT.ProcessLine("""
 struct Info{\
     Int_t event;\
     Int_t run;\
     Int_t lumi;\
     Int_t nJet;\
+    Int_t nBJet;\
     Int_t nMuonLoose;\
     Int_t nMuonTight;\
     Int_t nElectronLoose;\
@@ -121,59 +97,40 @@ struct Info{\
     Int_t nTauLoose;\
     Int_t nTauTight;\
     Int_t nVertex;\
-    Int_t hemisphereBalance;\
 };""")
     
-    filter_tags = [('ecalDeadCellTPfilter'),
-                   ('HBHENoiseFilterResultProducer2010'),
-                   ('HBHENoiseFilterResultProducer2011IsoDefault'),
-                   ('HBHENoiseFilterResultProducer2011NonIsoRecommended'),
-                   ('eeNoiseFilter'),
-                   ('goodPrimaryVertexFilter'),
-                   ('greedyMuonsTagging'),
-                   ('inconsistentMuonsTagging'),
-                   ('recovRecHitFilter'),
-                   ('scrapingFilter'),
-                   ('trackingFailureFilter')]
-
     rt.gROOT.ProcessLine("""
 struct Filters{\
     Bool_t triggerFilter;\
     Bool_t quadTriggerFilter;\
     Bool_t sixTriggerFilter;\
     Bool_t eightTriggerFilter;\
-    Bool_t l1MultiJetFilter;\
     Bool_t selectionFilter;\
-    Bool_t ecalDeadCellTPfilter;\
-    Bool_t HBHENoiseFilterResultProducer2010;\
-    Bool_t HBHENoiseFilterResultProducer2011IsoDefault;\
-    Bool_t HBHENoiseFilterResultProducer2011NonIsoRecommended;\
-    Bool_t eeNoiseFilter;\
-    Bool_t goodPrimaryVertexFilter;\
-    Bool_t greedyMuonsTagging;\
-    Bool_t inconsistentMuonsTagging;\
-    Bool_t recovRecHitFilter;\
-    Bool_t scrapingFilter;\
-    Bool_t trackingFailureFilter;\
 };""")
 
     from ROOT import Variables, Info, Filters
 
-#    output = rt.TFile.Open(options.outputFile,'recreate')
     tree = rt.TTree('RMRTree','Multijet events')
     tree.SetDirectory(0)
     def setAddress(obj, flag):
         for branch in dir(obj):
             if branch.startswith('__'): continue
-            tree.Branch(branch,rt.AddressOf(obj,branch),'%s/%s' % (branch,flag) )
+            if flag:
+                tree.Branch(branch,rt.AddressOf(obj,branch),'%s/%s' % (branch,flag) )
+            else:
+                tree.Branch(branch,rt.AddressOf(obj,branch),branch)
     
     filters = Filters()
     info = Info()
     vars = Variables()
+    CTEQ66_Wi = std.vector('double')()
+    MRST2006NNLO_Wi = std.vector('double')()
     
     setAddress(filters,'O')
     setAddress(info,'I')
     setAddress(vars,'D')
+    tree.Branch('CTEQ66_Wi',CTEQ66_Wi)
+    tree.Branch('MRST2006NNLO_Wi',MRST2006NNLO_Wi)
 
     # use Varparsing object
     events = Events(options)
@@ -191,6 +148,7 @@ struct Filters{\
     triggerH = Handle('std::vector<cmg::TriggerObject>')
     countH = Handle('int')
     filterH = Handle('int')
+    pdfH = Handle('std::vector<double>')
     
     # path trigger
     pathTriggerH = Handle("edm::TriggerResults")
@@ -206,11 +164,14 @@ struct Filters{\
         info.event = event.object().id().event()
         info.lumi = event.object().id().luminosityBlock()
         info.run = event.object().id().run()
-
+        
+        CTEQ66_Wi.clear()
+        MRST2006NNLO_Wi.clear()
+        
         if (count % 1000) == 0:
             print count,'run/lumi/event',info.run,info.lumi,info.event
             tree.AutoSave()
-        count += 1    
+        count += 1
 
         event.getByLabel(('TriggerResults','','MJSkim'),pathTriggerH)
         pathTrigger = pathTriggerH.product()
@@ -220,7 +181,7 @@ struct Filters{\
         path = pathTrigger.wasrun(pathTriggerNames.triggerIndex('razorMJPath')) and \
                pathTrigger.accept(pathTriggerNames.triggerIndex('razorMJPath'))
         filters.selectionFilter = path
-        if not path: continue
+        #if not path: continue
 
         event.getByLabel(('razorMJPFJetSel30'),jetSel30H)
         if not jetSel30H.isValid(): continue
@@ -235,55 +196,33 @@ struct Filters{\
         filters.eightTriggerFilter = hlt.getSelectionRegExp("^HLT_EightJet[0-9]+.*_v[0-9]+$")
         filters.l1MultiJetFilter = hlt.getSelectionRegExp("^HLT_L1MultiJet_v[0-9]+$")
         filters.triggerFilter = filters.quadTriggerFilter or filters.sixTriggerFilter or filters.eightTriggerFilter
-        #if not filters.l1MultiJetFilter: continue
             
-        #for f in filter_tags:
-        #    event.getByLabel(f,filterH)
-        #    result = filterH.product()[0]
-        #    setattr(filters,f,result)
-
-        for i in xrange(len(jets)):
-            name = 'jet%iPt' % (i + 1)
-            if hasattr(vars,name):
-                jet = jets[i]
-                setattr(vars,name,jet.pt())
-                setattr(vars,'jet%iEta'%(i+1),jet.eta())
-
-        tche = sorted([j.btag(0) for j in jets if abs(j.eta()) <= 2.4])
-        ssvp = sorted([j.btag(5) for j in jets if abs(j.eta()) <= 2.4])
-
-        if len(tche) < 2:
-            continue
-
-        vars.maxTCHE = tche[-1]
-        vars.maxSSVP = ssvp[-1]
-        vars.nextTCHE = tche[-2]
-        vars.nextSSVP = ssvp[-2]
-
-        event.getByLabel(('cmgPFMET'),metH)
-        met = metH.product()[0]
-        vars.met = met.et()
-
-        
-        event.getByLabel(('razorMJDiHemiHadBoxTop'),hemiHadH)
-        if not len(hemiHadH.product()): continue
-        hemiHad = hemiHadH.product()[0]
-
-        vars.R = hemiHad.R()
-        vars.Rsq = hemiHad.Rsq()
-        vars.mR = hemiHad.mR()
-        vars.hemi1Mass = hemiHad.leg1().mass()
-        vars.hemi2Mass = hemiHad.leg2().mass()
-        info.hemisphereBalance = (10*hemiHad.leg1().numConstituents()) + hemiHad.leg2().numConstituents()
+        tche = sorted([(j.pt(),j) for j in jets if abs(j.eta()) <= 2.4 and j.btag(0) >= 3.3])
+        if tche:
+            vars.maxTCHE = tche[-1][1].btag(0)
+            vars.maxTCHE_PT = tche[-1][1].pt()
+            vars.maxTCHE_ETA = tche[-1][1].eta()
+        info.nBJet = len(tche)
         
         #also get the old definition
         event.getByLabel(('razorMJDiHemiHadBox'),hemiHadH)
         if len(hemiHadH.product()):
             hemi = hemiHadH.product()[0]
-            vars.RsqMB = hemi.Rsq()
-            vars.mRMB = hemi.mR()
-            vars.hemi1MassMB = hemi.leg1().mass()
-            vars.hemi2MassMB = hemi.leg2().mass()
+            vars.RSQ = hemi.Rsq()
+            vars.MR = hemi.mR()
+
+        event.getByLabel(('razorMJDiHemiHadBoxUp'),hemiHadH)
+        if len(hemiHadH.product()):
+            hemi = hemiHadH.product()[0]
+            vars.RSQ_JES_UP = hemi.Rsq()
+            vars.MR_JES_UP = hemi.mR()
+
+        event.getByLabel(('razorMJDiHemiHadBoxDown'),hemiHadH)
+        if len(hemiHadH.product()):
+            hemi = hemiHadH.product()[0]
+            vars.RSQ_JES_DOWN = hemi.Rsq()
+            vars.MR_JES_DOWN = hemi.mR()
+            
 
         event.getByLabel(('cmgElectronSel'),electronH)
         event.getByLabel(('cmgMuonSel'),muonH)
@@ -322,9 +261,16 @@ struct Filters{\
             masses = map(float,parameters.split('_')[-2:])
             vars.mStop = masses[0]
             vars.mLSP = masses[1]
-        tree.Fill()
 
-#    tree.SetDirectory(output)
-#    tree.Write()
-#    output.Close()
+        #dump the PDF weights
+        event.getByLabel(('dumpPdfWeights','cteq66'),pdfH)
+        for w in pdfH.product():
+            CTEQ66_Wi.push_back(w)
+        event.getByLabel(('dumpPdfWeights','MRST2006nnlo'),pdfH)
+        for w in pdfH.product():
+            MRST2006NNLO_Wi.push_back(w)
+
+        tree.Fill()
+        if count > 1000: break
+
     store.write()
