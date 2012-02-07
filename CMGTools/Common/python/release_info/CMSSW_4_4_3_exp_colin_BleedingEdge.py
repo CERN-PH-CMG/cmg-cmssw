79cf35732d19cfc1c7ad3f96bd717ae52b99f664
cbern <>
2012-02-07 17:17:09 +0000

    new experimental recipe

--- CMGTools/Common/python/release_info/CMSSW_4_4_3_exp_colin_BleedingEdge.py
+++ CMGTools/Common/python/release_info/CMSSW_4_4_3_exp_colin_BleedingEdge.py
@@ -1,8 +1,8 @@
-[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_chargedPartIso_02Feb12'),
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_44X_02Feb12'),
-  ('CMGTools/H2TauTau', 'UserCode/CMG/CMGTools/H2TauTau', 'cbern_chargedParticleIso_02Feb12'),
-  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_02Feb12'),
-  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_chargedParticleIso_02Feb12'),
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_newDiTaus_03Feb12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_newDiTaus_03Feb12'),
+  ('CMGTools/H2TauTau', 'UserCode/CMG/CMGTools/H2TauTau', 'cbern_port2_07Feb12'),
+  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'cbern_forPete_07Feb12'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_fwliteanalysis_07Feb12'),
   ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'jhegeman_V2401_16Jan12'),
   ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
   ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-16'),
@@ -13,6 +13,3 @@
   ('RecoTauTag/Configuration', 'RecoTauTag/Configuration', 'V01-02-12'),
   ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-02-16'),
   ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
-# The following packages were not in the input release_info.
-# Please make sure that the CVS module is correct!
-#	CMGTools/H2TauTau                        cbern_chargedParticleIso_02Feb12
