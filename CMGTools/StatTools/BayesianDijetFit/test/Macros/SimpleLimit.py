d05ad684fa454df96e26d807ca9ef9e19dea2061
mgouzevi <>
2012-02-16 15:24:25 +0000

    added setDefaultStyle

--- CMGTools/StatTools/BayesianDijetFit/test/Macros/SimpleLimit.py
+++ CMGTools/StatTools/BayesianDijetFit/test/Macros/SimpleLimit.py
@@ -31,7 +31,11 @@ import os
 ## os.system(command)
 
 
-command = "dijetStatsNewBackground 1100 0 0 Qstar_qW_ak5_fat 1"
+#command = "dijetStatsNewBackground 1100 0 0 Qstar_qW_ak5_fat 1"
+#print command 
+#os.system(command)
+
+command = "dijetStatsNewBackground 3000 0 0 QBH_ak5_fat 1"
 print command 
 os.system(command)
 
---
+++ CMGTools/StatTools/BayesianDijetFit/test/Macros/setDefaultStyle.C
@@ -0,0 +1,66 @@
+#include "TStyle.h"
+void setDefaultStyle()
+{
+  TStyle *defaultStyle = new TStyle("defaultStyle","Default Style");
+  defaultStyle->SetOptStat(0000);
+  defaultStyle->SetOptFit(000); 
+  defaultStyle->SetPalette(1);
+  /////// pad ////////////
+  defaultStyle->SetPadBorderMode(1);
+  defaultStyle->SetPadBorderSize(1);
+  defaultStyle->SetPadColor(0);
+  defaultStyle->SetPadTopMargin(0.05);
+  defaultStyle->SetPadBottomMargin(0.13);
+  defaultStyle->SetPadLeftMargin(0.13);
+  defaultStyle->SetPadRightMargin(0.02);
+  /////// canvas /////////
+  defaultStyle->SetCanvasBorderMode(0);
+  defaultStyle->SetCanvasColor(0);
+  defaultStyle->SetCanvasDefH(600);
+  defaultStyle->SetCanvasDefW(600);
+  /////// frame //////////
+  defaultStyle->SetFrameBorderMode(0);
+  defaultStyle->SetFrameBorderSize(1);
+  defaultStyle->SetFrameFillColor(0); 
+  defaultStyle->SetFrameLineColor(1);
+  /////// label //////////
+  defaultStyle->SetLabelOffset(0.005,"XY");
+  defaultStyle->SetLabelSize(0.05,"XY");
+  defaultStyle->SetLabelFont(42,"XY");
+  /////// title //////////
+  defaultStyle->SetTitleOffset(1.1,"X");
+  defaultStyle->SetTitleSize(0.01,"X");
+  defaultStyle->SetTitleOffset(1.25,"Y");
+  defaultStyle->SetTitleSize(0.05,"Y");
+  defaultStyle->SetTitleFont(42, "XYZ");
+  /////// various ////////
+  defaultStyle->SetNdivisions(505,"Y");
+  defaultStyle->SetLegendBorderSize(0);  // For the axis titles:
+
+    defaultStyle->SetTitleColor(1, "XYZ");
+    defaultStyle->SetTitleFont(42, "XYZ");
+    defaultStyle->SetTitleSize(0.06, "XYZ");
+ 
+    // defaultStyle->SetTitleYSize(Float_t size = 0.02);
+    defaultStyle->SetTitleXOffset(0.9);
+    defaultStyle->SetTitleYOffset(1.05);
+    // defaultStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+
+    // For the axis labels:
+    defaultStyle->SetLabelColor(1, "XYZ");
+    defaultStyle->SetLabelFont(42, "XYZ");
+    defaultStyle->SetLabelOffset(0.007, "XYZ");
+    defaultStyle->SetLabelSize(0.04, "XYZ");
+
+    // For the axis:
+    defaultStyle->SetAxisColor(1, "XYZ");
+    defaultStyle->SetStripDecimals(kTRUE);
+    defaultStyle->SetTickLength(0.03, "XYZ");
+    defaultStyle->SetNdivisions(510, "XYZ");
+    defaultStyle->SetPadTickX(1);   // To get tick marks on the opposite side of the frame
+    defaultStyle->SetPadTickY(1);
+  
+  
+  
+  defaultStyle->cd();
+}
