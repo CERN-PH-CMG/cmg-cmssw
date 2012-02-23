500abe68aa39214580ab03a45a7d4a585d6245fb
psilva <>
2012-02-23 21:23:24 +0000

    modifying dilepton skim

--- HtoZZ2l2nu/python/DileptonFilterSequences_cff.py
+++ HtoZZ2l2nu/python/DileptonFilterSequences_cff.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 ##
 ## dilepton filters
 ##
-def addDileptonFilters(process,postfix='PFlow'):
+def addDileptonFilters(process):
 
     #leptons
     process.llPreselectionCounter = cms.EDProducer("EventCountProducer")
     process.llSelectionCounter = process.llPreselectionCounter.clone()
     process.lCandidates = cms.EDFilter("CandViewRefSelector",
                                        src = cms.InputTag("particleFlow"),
-                                       cut = cms.string("(abs(pdgId())==11 || abs(pdgId())==13) && pt>20")
+                                       cut = cms.string("(abs(pdgId())==11 || abs(pdgId())==13) && pt>20 && abs(eta)<2.5")
                                        )
     process.lCandCounter = cms.EDFilter("CandViewCountFilter", src = cms.InputTag("lCandidates"), minNumber = cms.uint32(2))
     process.llCandidates = cms.EDProducer("CandViewShallowCloneCombiner",
@@ -28,26 +28,28 @@ def addDileptonFilters(process,postfix='PFlow'):
                                                 )
     print " *** ll CandidateSequences is defined"
     
-    
-    
-    #photons
-#    process.photonPreselectionCounter = process.llPreselectionCounter.clone()
-#    process.photonSelectionCounter = process.llPreselectionCounter.clone()
-#    process.photonCandidates = cms.EDFilter("GenericPFCandidateSelector",
-#                                            src = cms.InputTag("pfAllPhotonsPFlow"),
-#                                            cut = cms.string("et>20 && abs(eta)<2.4 && (abs(eta)<1.4442|| abs(eta)>1.566) && "
-#                                                             "photonRef.isNonnull && "
-#                                                             "photonRef.hasPixelSeed && "
-#                                                             "photonRef.trkSumPtSolidConeDR03<5 && photonRef.ecalRecHitSumEtConeDR03<10 && photonRef.hcalTowerSumEtConeDR03<5")  
-#                                            )
-#    process.countPhotons = cms.EDFilter("CandViewCountFilter",
-#                                        minNumber = cms.uint32(1),
-#                                        maxNumber = cms.uint32(999999),
-#                                        src = cms.InputTag("photonCandidates")
-#                                        )
-#    process.photonCandidateSequence = cms.Sequence( process.photonPreselectionCounter*
-#                                                    process.photonCandidates*
-#                                                    process.countPhotons*
-#                                                    process.photonSelectionCounter )
+##
+## photon filter
+##
+def addPhotonFilters(process):
+    process.photonPreselectionCounter = cms.EDProducer("EventCountProducer")
+    process.photonSelectionCounter = process.photonPreselectionCounter.clone()
+    process.photonCandidates = cms.EDFilter("CandViewRefSelector",
+                                            src = cms.InputTag("particleFlow"),
+                                            cut = cms.string("abs(pdgId())==22 && "
+                                                             "et>20 && abs(eta)<2.4 && (abs(eta)<1.4442|| abs(eta)>1.566) && "
+                                                             "photonRef.isNonnull && "
+                                                             "photonRef.hasPixelSeed && "
+                                                             "photonRef.trkSumPtSolidConeDR03<5 && photonRef.ecalRecHitSumEtConeDR03<10 && photonRef.hcalTowerSumEtConeDR03<5")  
+                                            )
+    process.countPhotons = cms.EDFilter("CandViewCountFilter",
+                                        minNumber = cms.uint32(1),
+                                        maxNumber = cms.uint32(999999),
+                                        src = cms.InputTag("photonCandidates")
+                                        )
+    process.photonCandidateSequence = cms.Sequence( process.photonPreselectionCounter*
+                                                    process.photonCandidates*
+                                                    process.countPhotons*
+                                                    process.photonSelectionCounter )
+    print " *** photon CandidateSequences is defined"
 
-#    process.photonPath = cms.Path(process.startCounter * process.photonCandidateSequence * process.patDefaultSequence )
