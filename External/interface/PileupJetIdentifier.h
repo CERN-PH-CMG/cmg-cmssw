0be6d9ae9c6bab143761bd5d4c8e4c459edd1216
musella <>
2012-02-29 16:03:57 +0000

    Importing PilepJetIdentfier class

---
+++ External/interface/PileupJetIdentifier.h
@@ -0,0 +1,83 @@
+//--------------------------------------------------------------------------------------------------
+// $Id $
+//
+// PileupJetIdentifier
+//
+// Author: P. Musella, P. Harris
+//--------------------------------------------------------------------------------------------------
+
+#ifndef PileupJetIdentifier_h
+#define PileupJetIdentifier_h
+
+#include "DataFormats/JetReco/interface/Jet.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "TMVA/Tools.h"
+#include "TMVA/Reader.h"
+
+namespace pat { class Jet; }
+
+#define DECLARE_VARIABLE(NAME,TYPE)		\
+	private: \
+	TYPE NAME ## _;			\
+	public: \
+	const TYPE & NAME() const { return NAME ## _; }
+
+
+class PileupJetIdentifier {
+public:
+	PileupJetIdentifier();
+	~PileupJetIdentifier(); 
+	
+	void computeIdVariables(const reco::Jet * jet, const reco::Vertex *);
+	void computeIdVariablesFromPat(const pat::Jet * jet, const reco::Vertex *);
+	
+	DECLARE_VARIABLE(nCharged   ,Float_t);
+	DECLARE_VARIABLE(nNeutrals,Float_t);
+
+	DECLARE_VARIABLE(chgEMfrac  ,Float_t);
+	DECLARE_VARIABLE(neuEMfrac  ,Float_t);
+	DECLARE_VARIABLE(chgHadrfrac,Float_t);
+	DECLARE_VARIABLE(neuHadrfrac,Float_t);
+	
+	DECLARE_VARIABLE(d0         ,Float_t);   
+	DECLARE_VARIABLE(dZ         ,Float_t);  
+	DECLARE_VARIABLE(nParticles ,Float_t);  
+	DECLARE_VARIABLE(leadPt     ,Float_t);  
+	DECLARE_VARIABLE(leadEta    ,Float_t);  
+	DECLARE_VARIABLE(leadPhi    ,Float_t);  
+	DECLARE_VARIABLE(secondPt   ,Float_t);  
+	DECLARE_VARIABLE(secondEta  ,Float_t);  
+	DECLARE_VARIABLE(secondPhi  ,Float_t);  
+	DECLARE_VARIABLE(leadNeutPt ,Float_t);  
+	DECLARE_VARIABLE(leadNeutEta,Float_t);  
+	DECLARE_VARIABLE(leadNeutPhi,Float_t);  
+	DECLARE_VARIABLE(leadEmPt   ,Float_t);  
+	DECLARE_VARIABLE(leadEmEta  ,Float_t);  
+	DECLARE_VARIABLE(leadEmPhi  ,Float_t);  
+	DECLARE_VARIABLE(leadChPt   ,Float_t);  
+	DECLARE_VARIABLE(leadChEta  ,Float_t);  
+	DECLARE_VARIABLE(leadChPhi  ,Float_t);  
+	DECLARE_VARIABLE(leadFrac   ,Float_t);  
+
+	DECLARE_VARIABLE(dRLeadCent ,Float_t);  
+	DECLARE_VARIABLE(dRLead2nd  ,Float_t);  
+	DECLARE_VARIABLE(dRMean     ,Float_t);  
+	DECLARE_VARIABLE(dRMeanNeut ,Float_t);  
+	DECLARE_VARIABLE(dRMeanEm   ,Float_t);  
+	DECLARE_VARIABLE(dRMeanCh   ,Float_t);  
+
+	DECLARE_VARIABLE(ptD,Float_t);
+	
+	std::string dumpVariables() const;
+
+private:
+	void resetVariables();
+	std::map<std::string,Float_t *> variables_;
+	Float_t impactParTkThreshod_;
+};
+
+#undef DECLARE_VARIABLE
+
+#endif
