ddf09f74aafb6adff4f650ab08d4020a53ad8734
cbern <>
2012-03-09 16:27:07 +0000

    added a function to select the trigger objects of a given path (and possibly filter)

--- CMGTools/RootTools/python/utils/TriggerMatching.py
+++ CMGTools/RootTools/python/utils/TriggerMatching.py
@@ -8,11 +8,19 @@ def triggerMatched( object, triggerObjects, path, filter, dR2Max=0.089999):
     # to speed up the code, could sort the triggerObjects by decreasing pT
     # when they are produced
     for trigObj in triggerObjects:
-        # import pdb; pdb.set_trace()
         if trigObj.hasSelection( path ) and \
                trigObj.hasSelection( filter ) and \
                deltaR2( eta, phi, trigObj.eta(), trigObj.phi() ) < dR2Max:
             return True
     return False
 
-    
+
+def selTriggerObjects( trigObjs, path, filter=None):
+    selObjs = []
+    for trigObj in trigObjs:
+        if not trigObj.hasSelection( path ): continue
+        if filter is not None and \
+           not trigObj.hasSelection( filter ): continue
+        selObjs.append( trigObj )
+    return selObjs
+   
