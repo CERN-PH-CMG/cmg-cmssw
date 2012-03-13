cb6c66488bb4c2969ec08407e7a5fd8fad60a8c0
musella <>
2012-03-13 21:09:27 +0000

    importing JetIDAnalysis package

---
+++ UserArea/JetIDAnalysis/test/env.sh
@@ -0,0 +1,3 @@
+alias rootDir='python -i $CMSSW_BASE/src/CMGTools/RootTools/python/RootDir.py'
+alias httMultiLoop='python -i $CMSSW_BASE/src/CMGTools/RootTools/python/fwlite/MultiLoop.py'
+alias rootInit='python -i $CMSSW_BASE/src/CMGTools/RootTools/python/RootInit.py'
