5d74547b1e36cae0c7acecaa3da308add0237e8b
wreece <>
2012-03-13 13:11:29 +0000

    Adds a flag so that we can run on cmg trees also

--- CMGTools/Susy/susy_cfg.py
+++ CMGTools/Susy/susy_cfg.py
@@ -1,19 +1,20 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 ##########
-runOnMC = True
+runOnMC = False
 from CMGTools.Common.Tools.applyJSON_cff import applyJSON
 json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt'
 if not runOnMC:
     applyJSON(process, json )
 ##########
 skimEvents = True
+runPAT = False
 
 ##########
 from CMGTools.Common.Tools.getGlobalTag import getGlobalTag
 process.GlobalTag.globaltag = cms.string(getGlobalTag(runOnMC))
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.maxLuminosityBlocks = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -29,9 +30,11 @@ process.setName_('MJSkim')
 from CMGTools.Production.datasetToSource import *
 process.source = datasetToSource(
     'cmgtools',
-    #'/MultiJet/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0',
-    '/SMS-T2tt_Mstop-225to1200_mLSP-50to1025_7TeV-Pythia6Z/Summer11-PU_START42_V11_FastSim-v1/AODSIM/V2/PAT_CMG_V2_4_0',
-    'patTuple_PF2PAT_[0-9]+\\.root'
+    #'/HT/Run2011B-PromptReco-v1/AOD/PAT_CMG_V2_5_0',
+    '/MultiJet/Run2011A-May10ReReco-v1/AOD/V2/PAT_CMG_V2_5_0',
+    #'/SMS-T2tt_Mstop-225to1200_mLSP-50to1025_7TeV-Pythia6Z/Summer11-PU_START42_V11_FastSim-v1/AODSIM/V2/PAT_CMG_V2_4_0',
+    #'patTuple_PF2PAT_[0-9]+\\.root'
+    'tree_CMG_[0-9]+\\.root'
     ) 
 #process.source.fileNames = process.source.fileNames[:10]
 
@@ -46,10 +49,12 @@ outFileNameExt = ext
 
 #rerun the cmg stuff
 process.p = cms.Path()
-process.load('CMGTools.Common.analysis_cff')
-from CMGTools.Common.Tools.tuneCMGSequences import * 
-tuneCMGSequences(process, postpostfix='CMG')
-process.p += process.analysisSequence
+
+if runPAT:
+    process.load('CMGTools.Common.analysis_cff')
+    from CMGTools.Common.Tools.tuneCMGSequences import * 
+    tuneCMGSequences(process, postpostfix='CMG')
+    process.p += process.analysisSequence
 
 process.load('CMGTools.Susy.susy_cff')
 process.load('CMGTools.Susy.common.susy_cff')
@@ -61,7 +66,7 @@ process.schedule = cms.Schedule(
     process.multijetPathRazorTrigger,
     process.multijetPathHTTrigger,
     process.multijetPathL1Passthrough,
-    process.multijetPathL1Seed,
+    #process.multijetPathL1Seed,
     process.outpath
     )
 if runOnMC:
@@ -77,9 +82,12 @@ process.out.outputCommands.extend( eventCleaning )
 process.out.outputCommands += susyEventContent
 
 SelectEvents = cms.vstring('multijetPath','multijetPathNoTrigger','multijetPathMultijetTrigger',\
-                               'multijetPathRazorTrigger','multijetPathL1Passthrough','multijetPathL1Seed','multijetPathHTTrigger')
+                               'multijetPathRazorTrigger','multijetPathL1Passthrough','multijetPathHTTrigger')
 if not skimEvents:
     SelectEvents.append('p')
+if runPAT:
+    SelectedEvents.append('multijetPathL1Seed')
+
 process.out.SelectEvents = cms.untracked.PSet( SelectEvents = SelectEvents )
 
 
