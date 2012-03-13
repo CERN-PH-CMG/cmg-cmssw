60be11b2fca5f5189470ff7d71aa067bd53e8c0a
pmeckiff <>
2012-03-13 15:48:59 +0000

    Now publishes to CMGDB

--- Production/python/publish.py
+++ Production/python/publish.py
@@ -3,7 +3,7 @@
 ## @ CERN, Meyrin
 ## September 27th 2011
 
-import os, getpass, sys, re
+import os, getpass, sys, re, signal
 from CMGTools.Production.castorToDbsFormatter import CastorToDbsFormatter
 from CMGTools.Production.publishController import PublishController
 from DBSAPI.dbsProcessedDataset import DbsProcessedDataset
@@ -15,10 +15,10 @@ from DBSAPI.dbsApiException import *
 from DBSAPI.dbsException import *
 from CMGTools.Production.nameOps import *
 from CMGTools.Production.findDSOnSav import getTaskID
+from CMGTools.Production.fileOps import FileOps
 
 
-
-def publish(dsName,fileown,comment,test,dbsApi,user,password, force):
+def publish(dsName,fileown,comment,test,dbsApi,user,password, force, checkGroups):
     
     # if len(dsName.lstrip(os.sep).rstrip(os.sep).split(os.sep)) == 3 and dsName.split(os.sep)[0].endswith("%"):
 #     	# Get list of samples
@@ -34,8 +34,14 @@ def publish(dsName,fileown,comment,test,dbsApi,user,password, force):
     	
     if re.search("---",dsName):
         fileown = getDbsUser(dsName)
-        dsName = getCastor(dsName)
+        dsName2 = getCastor(dsName)
+        if dsName2 is None:
+            print "\nError, "+dsName+" is not valid, please use valid name\n"
+            return None
+        else:
+            dsName = dsName2
     
+    	
     if len(dsName.lstrip(os.sep).rstrip(os.sep).split(os.sep)) < 3:
     	print "Error, "+dsName+" is not valid, please use valid name"
     	return None
@@ -44,75 +50,105 @@ def publish(dsName,fileown,comment,test,dbsApi,user,password, force):
     	return None
     
     
-    # Store full dataset name
-    
-    opts = dict()
     formatter = CastorToDbsFormatter(dsName,fileown)
     procds =formatter.getDataset()
-    dayMonthYear = date.fromtimestamp(int(procds['DateCreated'])).strftime('%d-%B-%Y').split("-")
-    opts['planned_starting_date_dayfd']=dayMonthYear[0].lstrip("0")
-    opts['planned_starting_date_monthfd']=dayMonthYear[1]    
-    opts['planned_starting_date_yearfd']=dayMonthYear[2]
-    if test:
-    	opts['category_id']='101'
-    else:opts['category_id']='103'
-    opts['assigned_to']=fileown
-    opts['summary'] = procds['PathList'][0]
-    
-    
     try:
+    #if True:
+        # Store full dataset name
+    
+        opts = dict()
+        print "\n---Publish Dataset---"
+        print "Retrieving dataset information...\n"
+        
+
+        def timeout_handler(signum, frame):
+            raise OSError("Restarting connection to EOS, due to timeout\n")
+        completed = False
+        fileOps = None
+        
+        while True:
+            signal.signal(signal.SIGALRM, timeout_handler) 
+            signal.alarm(60) # triger alarm in 3 seconds
+        
+            try:
+                fileOps = None
+                fileOps = FileOps(dsName, fileown ,force, checkGroups)
+                break
+            except OSError as io:
+                print io.args[0]
+                signal.alarm(0)
+        signal.alarm(0)
+                
+                
+        formatter = CastorToDbsFormatter(dsName,fileOps.getUser())
+        procds =formatter.getDataset()
+        dayMonthYear = date.fromtimestamp(int(procds['DateCreated'])).strftime('%d-%B-%Y').split("-")
+        opts['planned_starting_date_dayfd']=dayMonthYear[0].lstrip("0")
+        opts['planned_starting_date_monthfd']=dayMonthYear[1]    
+        opts['planned_starting_date_yearfd']=dayMonthYear[2]
+        if test:
+    	    opts['category_id']='101'
+        else:opts['category_id']='103'
+        opts['assigned_to']=fileown
+        opts['summary'] = procds['PathList'][0]
+    
+    
+    
     	dbsID = None
     	parentDbsID = None
     	taskID = None
     	parentTaskID = None
+    	loginClear = False
     	status = 'Failed'
     	publishController = PublishController(user, password, dbsApi, force)
-    	if not publishController.loginValid():
+    	try:
+    	    loginClear = publishController.loginValid()
+    	except KeyboardInterrupt:
+    	    raise
+    	except:
+    	    loginClear = publishController.loginValid()
+    	if loginClear is False:
     		print "User authentication failed, exiting\n\n"
     		return None
+    	
     	print "\n------DataSet------\n"
-    	print "DBS name: ",getDbsWithUser(dsName, fileown)
-        print "EOS name: ",dsName
+    	print "CMGDB name:",getDbsWithUser(dsName, fileOps.getUser())
+        print "EOS name:  ",dsName
     	if procds['ParentList'][0].endswith("---*") and dbsApi is not None:
     		procds['ParentList'][0] = publishController.chooseParent(procds['ParentList'][0], opts['category_id'])
     	if dbsApi is not None:
     		print "\n--------DBS--------\n"
     		publishController.dbsPublish(procds)
     	print "\n------Savanah------\n"
-    	(taskID, parentTaskID) = publishController.savannahPublish(procds, opts, comment)
+    	(taskID, parentTaskID) = publishController.savannahPublish(procds, opts, comment, fileOps)
+    	try:
+    	    if taskID is None: taskID = getTaskID(procds['PathList'][0], opts['category_id'], user, password, False)
+    	except:
+    	    taskID = getTaskID(procds['PathList'][0], opts['category_id'], user, password, False)
+    	parentTaskID = None
     	if taskID is not None: status = 'Success'
     	if publishController.cmgdbOnline():
     		print "\n-------CMGDB-------\n"
     		cmgdbid = None
-    		
-    		try:
-    			if taskID is not None:
-    				cmgdbid = publishController.cmgdbPublish(procds, dbsID, taskID, test)
+    		if True:
+    		#try:
+    			cmgdbid = publishController.cmgdbPublish(procds, dbsID, taskID, test, fileOps)
     			if parentTaskID is not None:
-    				publishController.cmgdbPublish(procds, parentDbsID, parentTaskID, test)
+    				publishController.cmgdbPublish(procds, int(parentDbsID), int(parentTaskID), test)
     			
-    		except ImportError:
-    			print "cx_Oracle not properly installed"
-    			return None
-    	return {'Status':status, 'Savannah':taskID,'CMGDB ID':cmgdbid,'Dataset':procds['PathList'][0], 'EOS Dataset':dsName,'File Owner':fileown}
+    		#except ImportError:
+    			#print "cx_Oracle not properly installed"
+    			#return None
+    	return {'Status':status, 'Savannah':taskID,'CMGDB ID':cmgdbid,'DBS Dataset':procds['PathList'][0], 'EOS Dataset':dsName,'File Owner':fileown}
     except ValueError as err:
-        print err, '.\nDataset not published'
+        print err.args, '.\nDataset not published'
         return None
     except NameError as err:
+    #else:
         print err.args[0]
         savannah = None
-        if len(err.args)>1:
-            print "Logging dataset: "+err.args[1] 
-            f = open("/afs/cern.ch/user/"+user[0]+"/"+user+"/public/obsoleteDatasets.txt","a")
             
-            if len(err.args)>2:
-                f.write(err.args[1]+" https://savannah.cern.ch/task/?"+str(err.args[2])+"\n")
-                savannah = "https://savannah.cern.ch/task/?"+str(err.args[2])
-            else:
-                f.write(err.args[1])
-            f.close()
-            
-        return {'Status':'Failed','Savannah':savannah, 'DBS Dataset':procds['PathList'][0], 'EOS Dataset':dsName, 'File Owner':fileown}
+        return {'Status':'Failed','Savannah':savannah,'CMGDB ID':None, 'DBS Dataset':procds['PathList'][0], 'EOS Dataset':dsName, 'File Owner':fileown}
 
 
 
