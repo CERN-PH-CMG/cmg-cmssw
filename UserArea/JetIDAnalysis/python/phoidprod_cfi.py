808f312375d0178ea16e9a126381955fb401bc4e
adavidzh <>
2012-03-15 02:15:56 +0000

    made a decent cfi

--- UserArea/JetIDAnalysis/python/phoidprod_cfi.py
+++ UserArea/JetIDAnalysis/python/phoidprod_cfi.py
@@ -1,4 +1,12 @@
 import FWCore.ParameterSet.Config as cms
+from HiggsAnalysis.HiggsTo2photons.hggPhotonIDCuts_cfi import *
 
-demo = cms.EDProducer('PhoIDProd'
+PhoIDProd = cms.EDProducer('PhoIDProd',
+                                   hggPhotonIDConfiguration = cms.PSet(hggPhotonIDCuts),
+                                   vtxCollection = cms.InputTag("offlinePrimaryVerticesWithBS"),
+                                   eleCollection = cms.InputTag("gsfElectrons"),
+                                   tkCollection = cms.InputTag("generalTracks"),
+                                   pfCollection = cms.InputTag("particleFlow"),
+                                   rhoCollection = cms.InputTag("kt6PFJets","rho"),
 )
+
--- UserArea/JetIDAnalysis/test/test_phoidprod_cfg.py
+++ UserArea/JetIDAnalysis/test/test_phoidprod_cfg.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-from HiggsAnalysis.HiggsTo2photons.hggPhotonIDCuts_cfi import *
+
+#from CMG.JetIDAnalysis.phoidprod_cfi import *
 
 process = cms.Process("PHOTONTAG")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("CMG.JetIDAnalysis.phoidprod_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -15,20 +17,10 @@ process.source = cms.Source("PoolSource",
     )
 )
 
-process.PhoIDProd = cms.EDProducer('PhoIDProd',
-                                   hggPhotonIDConfiguration = cms.PSet(hggPhotonIDCuts),
-                                   vtxCollection = cms.InputTag("offlinePrimaryVerticesWithBS"),
-                                   eleCollection = cms.InputTag("gsfElectrons"),
-                                   tkCollection = cms.InputTag("generalTracks"),
-                                   pfCollection = cms.InputTag("particleFlow"),
-                                   rhoCollection = cms.InputTag("kt6PFJets","rho"),
-)
-
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('MyPickedEventsWithProduct.root')
 )
 
-  
 process.p = cms.Path(process.PhoIDProd)
 
 process.e = cms.EndPath(process.out)
