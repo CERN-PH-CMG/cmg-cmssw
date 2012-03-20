5c6801417447f846019403da7322d328cf938372
malberti <>
2012-03-20 18:44:06 +0000

    new cfg template

---
+++ UserArea/JetIDAnalysis/test/scripts_martina/jetanalyzerTEMPLATE_data_cfg.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
+
+
+process = cms.Process("analysis")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(LISTOFFILES),
+    skipEvents = cms.untracked.uint32(0)                        
+)
+
+
+# muon filter
+process.load('PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi')
+
+process.MuonsFilter = countPatMuons.clone(
+    src       = cms.InputTag("selectedPatMuonsPFlow"),
+    minNumber = cms.uint32(2)
+)
+
+#jet analyzer 
+from CMG.JetIDAnalysis.jetanalyzer_cfi import *
+
+
+# PF jet collection
+process.pfjetanalyzer = jetanalyzer.clone(
+    JetTag   = cms.InputTag("selectedPatJets",""),            
+    dataFlag = cms.untracked.bool(True)
+)
+
+# CHS jet collection
+process.chspfjetanalyzer = jetanalyzer.clone(
+    JetTag   = cms.InputTag("selectedPatJetsPFlow",""),
+    dataFlag = cms.untracked.bool(True)
+)
+
+process.TFileService = cms.Service("TFileService", 
+    fileName = cms.string("OUTPUTFILENAME"),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+process.ana = cms.Sequence(process.pfjetanalyzer+process.chspfjetanalyzer)
+process.p = cms.Path(process.MuonsFilter*process.ana)
