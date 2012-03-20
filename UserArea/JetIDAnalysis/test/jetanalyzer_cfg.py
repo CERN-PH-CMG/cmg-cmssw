df3fb8a5586a34713411b906ad074947042c3d92
malberti <>
2012-03-20 17:23:06 +0000

    changes to run on both jet collections

--- UserArea/JetIDAnalysis/test/jetanalyzer_cfg.py
+++ UserArea/JetIDAnalysis/test/jetanalyzer_cfg.py
@@ -7,16 +7,17 @@ from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
 process = cms.Process("analysis")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
 #       'file:/data1/malberti/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/Fall11-PU_Chamonix12_START44_V10-v2/AODSIM/PAT_CMG_V3_0_0/patTuple_PF2PAT_999.root'
-       'file:/data1/malberti/MC_DYJetsToLL_97_1.root'
-#        'root://eoscms//eos/cms/store/cmst3/user/querten/12_02_20_HZZ2l2v_pat/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/MC_DYJetsToLL_96_1.root',
+#       'file:/data1/malberti/MC_DYJetsToLL_97_1.root'
+        'file:/tmp/malberti/MC_DYJetsToLL_9_1.root',
 #        'root://eoscms//eos/cms/store/cmst3/user/querten/12_02_20_HZZ2l2v_pat/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/MC_DYJetsToLL_97_1.root',
 #        'root://eoscms//eos/cms/store/cmst3/user/querten/12_02_20_HZZ2l2v_pat/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/MC_DYJetsToLL_98_1.root'
        ),
@@ -32,12 +33,21 @@ process.MuonsFilter = countPatMuons.clone(
     minNumber = cms.uint32(2)
 )
 
+from CMG.JetIDAnalysis.jetanalyzer_cfi import *
 
-process.load("CMG.JetIDAnalysis.jetanalyzer_cfi")
+process.pfjetanalyzer = jetanalyzer.clone(
+    JetTag      = cms.InputTag("selectedPatJets",""),            
+)
+
+process.chspfjetanalyzer = jetanalyzer.clone(
+    JetTag      = cms.InputTag("selectedPatJetsPFlow",""),            
+)
 
 process.TFileService = cms.Service("TFileService", 
     fileName = cms.string("mytree.root"),
     closeFileFast = cms.untracked.bool(True)
 )
 
-process.p = cms.Path(process.MuonsFilter*process.jetanalyzer)
+process.ana = cms.Sequence(process.pfjetanalyzer+process.chspfjetanalyzer)
+process.p = cms.Path(process.MuonsFilter*process.ana)
+
