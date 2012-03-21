30d64c56fb10cc8c9b3c56e2cc5a92ae9c580802
pmeckiff <>
2012-03-21 08:42:33 +0000

    Made menu clearer and queries are now copy/pasteable

--- Production/scripts/getInfo.py
+++ Production/scripts/getInfo.py
@@ -65,23 +65,13 @@ Here are some example queries explained:
     -- First, use the trunc() method to truncate the timestamp to the desired length
     -- Then, use the to_timestamp() method in order to change the date you want to a timestamp
 
-        "SELECT distinct(tags.tag_id), tags.tag, tags.package_name 
-        FROM tags 
-        INNER JOIN tags_in_sets ON tags_in_sets.tag_id = tags.tag_id 
-        JOIN dataset_details ON dataset_details.tagset_id = tags_in_sets.tagset_id 
-        WHERE dataset_details.nice_username = 'cmgtools' 
-        AND trunc(dataset_details.date_recorded) = to_timestamp('07-03-2012','DD-MM-YYYY') 
-        ORDER BY tags.tag_id"
+        "SELECT distinct(tags.tag_id), tags.tag, tags.package_name FROM tags INNER JOIN tags_in_sets ON tags_in_sets.tag_id = tags.tag_id JOIN dataset_details ON dataset_details.tagset_id = tags_in_sets.tagset_id WHERE dataset_details.nice_username = 'cmgtools' AND trunc(dataset_details.date_recorded) = to_timestamp('07-03-2012','DD-MM-YYYY') ORDER BY tags.tag_id"
     
     If you want to get a list of dataset names from the same time period, again from cmgtools,
     - You would first select the details you want to see, e.g dataset_fraction
     - Then you would use the same WHERE clause as in the previous example
         
-        "SELECT dataset_id, path_name, date_recorded, dataset_fraction 
-        FROM dataset_details 
-        WHERE trunc(date_recorded) = to_timestamp('07-03-2012','DD-MM-YYYY') 
-        AND nice_username = 'cmgtools' 
-        ORDER BY dataset_id"
+        "SELECT dataset_id, path_name, date_recorded, dataset_fraction FROM dataset_details WHERE trunc(date_recorded) = to_timestamp('07-03-2012','DD-MM-YYYY') AND nice_username = 'cmgtools' ORDER BY dataset_id"
         
     If you want a list of datasets that used a certain tag
     - First specify the fields you want to select, in this case dataset id and name
@@ -89,22 +79,14 @@ Here are some example queries explained:
     - Then perform another join with the tags table, and specify the WHERE clause
     - Finally specify what to order the list by
     
-        "SELECT dataset_details.dataset_id, dataset_details.path_name 
-        FROM dataset_details 
-        INNER JOIN tags_in_sets on tags_in_sets.tagset_id = dataset_details.tagset_id 
-        JOIN tags on tags.tag_id = tags_in_sets.tag_id 
-        WHERE tags.tag = 'B4_2_X_V00-03-00' 
-        AND tags.package_name = 'CommonTools/ParticleFlow'
-        ORDER BY dataset_details.dataset_id"
+        "SELECT dataset_details.dataset_id, dataset_details.path_name FROM dataset_details INNER JOIN tags_in_sets on tags_in_sets.tagset_id = dataset_details.tagset_id JOIN tags on tags.tag_id = tags_in_sets.tag_id WHERE tags.tag = 'B4_2_X_V00-03-00' AND tags.package_name = 'CommonTools/ParticleFlow' ORDER BY dataset_details.dataset_id"
     
     If you want to find the missing files on a particular dataset
     - First specify what you want to select
     - Then join on dataset id
     - Then specify which dataset
         
-        "SELECT missing_files.missing_file from missing_files
-        INNER JOIN dataset_details on dataset_details.dataset_id = missing_files.dataset_id
-        WHERE cmgdb_name = '/QCD_Pt-20to30_EMEnriched_TuneZ2_7TeV-pythia6/Fall11-PU_S6_START44_V9B-v1--V3---cmgtools_group/AODSIM'"
+        "SELECT missing_files.missing_file from missing_files INNER JOIN dataset_details on dataset_details.dataset_id = missing_files.dataset_id WHERE cmgdb_name = '/QCD_Pt-20to30_EMEnriched_TuneZ2_7TeV-pythia6/Fall11-PU_S6_START44_V9B-v1--V3---cmgtools_group/AODSIM'"
 
 Usage: 
 -----
@@ -116,15 +98,24 @@ Suggestions for more useful alias' are always welcome
 Please experiment and email Peter Meckiffe with your suggestions for alias'
 Currently the list is as follows:
 getTags <cmgdb_name>
+    SELECT distinct(tags.tag_id), tags.tag, tags.package_name from tags INNER JOIN tags_in_sets ON tags.tag_id = tags_in_sets.tag_id JOIN dataset_details ON dataset_details.tagset_id = tags_in_sets.tagset_id WHERE dataset_details.cmgdb_name = 'ARG1' ORDER BY tags.tag_id
 getDatasetsAtDate <DD-MM-YYYY>
+    SELECT distinct(dataset_id), cmgdb_name FROM dataset_details WHERE trunc(date_recorded) = TO_TIMESTAMP('ARG1','DD-MM-YYYY') ORDER BY dataset_id
 getDatasetsAtDateWithUser <DD-MM-YYYY> <fileowner>
+    SELECT distinct(dataset_id), cmgdb_name FROM dataset_details WHERE trunc(date_recorded) = TO_TIMESTAMP('ARG1','DD-MM-YYYY') and nice_username = 'ARG2' ORDER BY dataset_id
 getDatasetsWithUser <fileowner>
+    SELECT distinct(dataset_id), cmgdb_name FROM dataset_details WHERE nice_username = 'ARG1' ORDER BY dataset_id
 getMissingFiles <cmgdb_name>
+    SELECT distinct(missing_files.missing_file) FROM missing_files INNER JOIN dataset_details ON dataset_details.dataset_id = missing_files.dataset_id WHERE dataset_details.cmgdb_name = 'ARG1'
 getDuplicateFiles <cmgdb_name>
+    SELECT distinct(duplicate_files.duplicate_file) FROM duplicate_files INNER JOIN dataset_details ON dataset_details.dataset_id = duplicate_files.dataset_id WHERE dataset_details.cmgdb_name = 'ARG1'
 getBadJobs <cmgdb_name>
+    SELECT distinct(bad_jobs.bad_job) FROM bad_jobs INNER JOIN dataset_details ON dataset_details.dataset_id = bad_jobs.bad_job WHERE dataset_details.cmgdb_name = 'ARG1'
 getBadFiles <cmgdb_name>
+    SELECT distinct(bad_files.bad_file) FROM bad_files INNER JOIN dataset_details ON dataset_details.dataset_id = bad_files.dataset_id WHERE dataset_details.cmgdb_name = 'ARG1'
 getDatasetInfo <cmgdb_name>
-
+    SELECT path_name, lfn, nice_username, dataset_entries, dataset_fraction, date_recorded FROM dataset_details WHERE cmgdb_name = 'ARG1'
+    
 e.g.
 getInfo.py -a getTags /QCD_Pt-20to30_EMEnriched_TuneZ2_7TeV-pythia6/Fall11-PU_S6_START44_V9B-v1--V3---cmgtools_group/AODSIM
 
