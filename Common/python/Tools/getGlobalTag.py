4f1f343d2747ca0f9cd169a2a68ecf5253bc7023
lucieg <>
2012-03-21 17:07:24 +0000

    added 5X tags, use autocond

--- Common/python/Tools/getGlobalTag.py
+++ Common/python/Tools/getGlobalTag.py
@@ -1,19 +1,24 @@
 import os
 
-from Configuration.PyReleaseValidation.autoCond import autoCond
+from Configuration.AlCa.autoCond import autoCond
 
 def getGlobalTag(runOnMC):
     """Return the global tag depending on the release"""
     
     if runOnMC:
         #default to startup, but use mc otherwise
-        #return autoCond.get('startup',autoCond['mc'])
+        return autoCond.get('startup',autoCond['mc'])
         #return 'START42_V13::All'
         #return 'START42_V17::All'
         # return 'START44_V10::All'
-        return 'START44_V12::All'
+        #return 'START44_V12::All'
+        #return 'START50_V15A::All' # for 512
+        #return 'START52_V4A::All'  # for 52X
     else:
-        #return autoCond['com10']
+        return autoCond['com10']
         #return 'GR_R_42_V19::All'
         # return 'GR_R_42_V23::All'
-        return 'GR_R_44_V13::All'
+        # return 'GR_R_44_V13::All'
+        # return 'GR_R_50_V12::All' # for 512
+        # return 'GR_R_52_V4::All'  # for 52X
+
