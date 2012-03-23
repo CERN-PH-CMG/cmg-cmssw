b630f0f35d7cb0076c1a6dc8a2de4db8e41ec2d7
botta <>
2012-03-23 17:40:18 +0000

    new genParticle Collection

--- CMGTools/Common/python/eventContent/gen_cff.py
+++ CMGTools/Common/python/eventContent/gen_cff.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 gen = cms.untracked.vstring(
-    'keep recoGenParticles_genParticlesStatus3_*_*',
-    'keep recoGenParticles_genLeptonsStatus2_*_*',
-    'keep recoGenParticles_genLeptonsStatus1_*_*',    
+    #'keep recoGenParticles_genParticlesStatus3_*_*',
+    'keep recoGenParticles_genParticlesPruned_*_*',
+    #'keep recoGenParticles_genLeptonsStatus2_*_*',
+    #'keep recoGenParticles_genLeptonsStatus1_*_*',    
     'keep recoGenJetedmPtrcmgPhysicsObjectWithPtrs_genJetSel_*_*',
     'keep recoGenJets_tauGenJetsSelectorAllHadrons*_*_PAT',
     'keep *_addPileupInfo_*_*',
