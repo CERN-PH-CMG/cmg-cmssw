9f454e3a03344741e942e29ac93f34d3ca4c4c89
psilva <>
2012-03-29 17:27:18 +0000

    factorizing cfi/updating for 52x

---
+++ UserArea/JetIDAnalysis/python/SingleQuarkGun_cfi.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import pythiaUESettingsBlock
+
+generator = cms.EDProducer("Pythia6PtYDistGun",
+                           maxEventsToPrint = cms.untracked.int32(5),
+                           pythiaHepMCVerbosity = cms.untracked.bool(True),
+                           pythiaPylistVerbosity = cms.untracked.int32(1),
+                           PGunParameters = cms.PSet(  ParticleID = cms.vint32(1),
+                                                       kinematicsFile = cms.FileInPath('CMG/JetIDAnalysis/data/ptgun.root'),
+                                                       PtBinning = cms.int32(100000),
+                                                       YBinning = cms.int32(500),
+                                                       MinPt = cms.double(3.0),
+                                                       MaxPt = cms.double(300.0),
+                                                       MinY = cms.double(-5.0),
+                                                       MaxY = cms.double(5.0),
+                                                       MinPhi = cms.double(-3.14159265359),
+                                                       MaxPhi = cms.double(3.14159265359),
+                                                       ),
+                           Verbosity = cms.untracked.int32(0),
+                           psethack = cms.string('single quark pt 3-300'),
+                           firstRun = cms.untracked.uint32(1),
+                           PythiaParameters = cms.PSet(  pythiaUESettings = pythiaUESettingsBlock
+                                                         parameterSets = cms.vstring('pythiaUESettings')
+                                                         )
+                           )
