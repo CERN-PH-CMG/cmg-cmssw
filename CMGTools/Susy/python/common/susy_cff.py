369690969c9053c862f18610d5aee89e86f97356
wreece <>
2012-04-03 11:49:05 +0000

    Adds 2011 trigger emulation based on cutting the trigger objects

--- CMGTools/Susy/python/common/susy_cff.py
+++ CMGTools/Susy/python/common/susy_cff.py
@@ -5,11 +5,12 @@ from CMGTools.Susy.common.boolToInt_cff import *
 from CMGTools.Susy.common.jetId_cff import *
 from CMGTools.Susy.common.dumpPDFWeights_cff import *
 from CMGTools.Susy.common.simpleGenInfo_cff import *
-
+from CMGTools.Susy.common.emulate2011Trigger_cff import *
 
 susyGenSequence  = cms.Sequence(
     dumpPdfWeights+
-    simpleGenInfo
+    simpleGenInfo+
+    emulate2011Trigger
     )
 
 susyDataSequence  = cms.Sequence(
