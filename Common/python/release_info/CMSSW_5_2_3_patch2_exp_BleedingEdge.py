c217caa8f9b1e40848b0b67dbecdbca54b86df3f
bonato <>
2012-04-04 12:49:55 +0000

    Add 5_2_3_patch2 file

---
+++ Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
@@ -0,0 +1,15 @@
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'wreece_160312'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_eventContent_19Mar12'),
+  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'pmeckiff16mar2012'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_delta_19Mar12'),
+  ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'jhegeman_V2401_16Jan12'),
+  ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
+  ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-20'),
+  ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-08-25'),
+  ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-18'),
+  ('RecoLuminosity/LumiDB', 'RecoLuminosity/LumiDB', 'V03-04-02'),
+  ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23'),
+  ('RecoTauTag/Configuration', 'RecoTauTag/Configuration', 'V01-03-04'),
+  ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-04-06'),
+  ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
+
