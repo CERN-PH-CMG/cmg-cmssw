68b1cb37b2f3cc92aa8cfd4f6a97c8d1a152a364
bonato <>
2012-04-04 13:35:42 +0000

    Tag of CMGTools/RootUtils for 5XY

--- Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
+++ Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
@@ -1,7 +1,7 @@
 [ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'wreece_160312'),
   ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_eventContent_19Mar12'),
   ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'pmeckiff16mar2012'),
-  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_delta_19Mar12'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_hzz_21Mar12 '),
   ('CMGTools/ZmumuJetsTutorial', 'UserCode/CMG/CMGTools/ZmumuJetsTutorial', 'jhegeman_V2401_16Jan12'),
   ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
   ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-20'),
