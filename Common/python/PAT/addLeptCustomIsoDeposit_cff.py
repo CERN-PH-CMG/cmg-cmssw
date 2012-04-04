adca1e99d3b22a6f09861b138eeae9a4d4971605
namapane <>
2012-04-04 02:18:56 +0000

    move to newer mechanism for user iso, as old one (muonUserIsolation.user) is not properly supported for the case useParticleFlow=true

--- Common/python/PAT/addLeptCustomIsoDeposit_cff.py
+++ Common/python/PAT/addLeptCustomIsoDeposit_cff.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 electronUserIsolation  = cms.PSet(
-    user = cms.VPSet(
-    cms.PSet( src = cms.InputTag("eleIsoFromDepsTkOptimized5") ),
-    cms.PSet( src = cms.InputTag("eleIsoFromDepsTkOptimized7") ),
-    )         
+    user = cms.VInputTag(
+        cms.InputTag("eleIsoFromDepsTkOptimized5"),
+        cms.InputTag("eleIsoFromDepsTkOptimized7"),
+    )
 )
 
 muonUserIsolation  = cms.PSet(
-    user = cms.VPSet(
-    cms.PSet( src = cms.InputTag("muIsoFromDepsTkOptimized") )  
-    )         
+    user = cms.VInputTag(
+        cms.InputTag("muIsoFromDepsTkOptimized")
+    )
 )
 
 def addElectronCustomIsoDeposit( process, sequenceName, postfix ):
@@ -18,9 +18,9 @@ def addElectronCustomIsoDeposit( process, sequenceName, postfix ):
     Adds custom isoDeposit to the PAT electron
     """
 
-    userIso = getattr(process, "patElectrons" + postfix).userIsolation
-    if not userIso.hasParameter('user') : userIso.user = electronUserIsolation.user
-    else : userIso.user += electronUserIsolation.user
+    isoValues = getattr(process, "patElectrons" + postfix).isolationValues
+    if not isoValues.hasParameter('user') : isoValues.user = electronUserIsolation.user
+    else : isoValues.user += electronUserIsolation.user
 #    print userIso ###
 
     userIsoD = getattr(process, "patElectrons" + postfix).isoDeposits
@@ -42,9 +42,9 @@ def addMuonCustomIsoDeposit( process, sequenceName, postfix ):
     Adds custom isoDeposit to the PAT muons
     """
 
-    userIso = getattr(process, "patMuons" + postfix).userIsolation
-    if not userIso.hasParameter('user') : userIso.user = muonUserIsolation.user
-    else : userIso.user += muonUserIsolation.user
+    isoValues = getattr(process, "patMuons" + postfix).isolationValues
+    if not isoValues.hasParameter('user') : isoValues.user = muonUserIsolation.user
+    else : isoValues.user += muonUserIsolation.user
 #    print userIso ###
 
     userIsoD = getattr(process, "patMuons" + postfix).isoDeposits
