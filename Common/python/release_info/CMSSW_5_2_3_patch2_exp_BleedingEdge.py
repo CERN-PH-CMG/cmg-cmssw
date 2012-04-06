888c490679b1b94c11f3871b4f98891930dd157d
cbern <>
2012-04-06 13:20:51 +0000

    prototype bleeding edge for 523

--- Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
+++ Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
@@ -1,22 +1,18 @@
-[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_sip_27Mar12'), #
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_05Apr12_2'), #
-  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'pmeckiff3apr2012'), #
-  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_05Apr12'), #
-  ('CommonTools/ParticleFlow', 'CommonTools/ParticleFlow', 'V00-03-07-01'), # 
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_sip_27Mar12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'B52X'),
+  ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'pmeckiff3apr2012'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_05Apr12'),
+  ('CommonTools/ParticleFlow', 'CommonTools/ParticleFlow', 'V00-03-07-01'),
   ( 'EgammaCalibratedGsfElectrons/CalibratedElectronAlgos',
     'UserCode/ClaudeCharlot/EgammaCalibratedGsfElectrons/CalibratedElectronAlgos',
-    'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'), #
+    'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'),
   ( 'EgammaCalibratedGsfElectrons/CalibratedElectronProducers',
     'UserCode/ClaudeCharlot/EgammaCalibratedGsfElectrons/CalibratedElectronProducers',
-    'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'), #
-  ('GeneratorInterface/GenFilters', 'GeneratorInterface/GenFilters', 'V00-11-00-01'),
+    'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'),
   ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
-  ('PhysicsTools/HepMCCandAlgos', 'PhysicsTools/HepMCCandAlgos', 'V11-03-16'),
-  ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-08-25'), #
-  ('PhysicsTools/PatUtils', 'PhysicsTools/PatUtils', 'V03-09-18-03'), #
+  ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-08-25'),
+  ('PhysicsTools/PatUtils', 'PhysicsTools/PatUtils', 'V03-09-18-03'),
   ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-16'),
-  ('RecoLuminosity/LumiDB', 'RecoLuminosity/LumiDB', 'V03-03-07'),
+  ('RecoParticleFlow/PFProducer', 'RecoParticleFlow/PFProducer', 'V15-01-05'),
   ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23'),
-  ('RecoTauTag/Configuration', 'RecoTauTag/Configuration', 'V01-02-12'),
-  ('RecoTauTag/RecoTau', 'RecoTauTag/RecoTau', 'V01-02-16'),
   ('TauAnalysis/SVFitStandAlone', 'UserCode/TauAnalysis/SVFitStandAlone', 'joseNov14')]
