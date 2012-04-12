68c34448bcc9aaad6d592f969ecfaf6f5fb6e2f2
musella <>
2012-04-12 18:53:26 +0000

    Added basePtVtxCat and baseNoVtxCat weights

--- CMGTools/External/python/puJetIDAlgo_cff.py
+++ CMGTools/External/python/puJetIDAlgo_cff.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 from CMGTools.External.JetIdParams_cfi import *
 
-dRdRProfMultBetaFull = cms.PSet( 
+basePtVtxCat = cms.PSet( 
  impactParTkThreshold = cms.untracked.double(1.) ,
- tmvaWeights = cms.untracked.string("CMGTools/External/data/TMVAClassification_BDT_dRdRProfMultBetaFull.weights_postproc.xml"),
- tmvaMethod  = cms.untracked.string("BDT_dRdRProfMultBetaFull"),
+ tmvaWeights = cms.untracked.string("CMGTools/External/data/TMVAClassification_BDT_basePtVtxCat.weights.xml"),
+ tmvaMethod  = cms.untracked.string("BDT_basePtVtxCat"),
  version = cms.untracked.int32(-1),
  tmvaVariables = cms.untracked.vstring(
     "dRMean",
@@ -13,12 +13,39 @@ dRdRProfMultBetaFull = cms.PSet(
     "frac03",
     "frac04",
     "frac05",
-    "nParticles",
+    "nNeutrals",
     "beta",
     "betaStar",
+    "dZ",
+    "nCharged",
     ),
  tmvaSpectators = cms.untracked.vstring(
     "nvtx",
+    "jetPt",
+    "jetEta",
+    )
+ )
+
+baseNoVtxCat = cms.PSet( 
+ impactParTkThreshold = cms.untracked.double(1.) ,
+ tmvaWeights = cms.untracked.string("CMGTools/External/data/TMVAClassification_BDT_baseNoVtxCat.weights.xml"),
+ tmvaMethod  = cms.untracked.string("BDT_baseNoVtxCat"),
+ version = cms.untracked.int32(-1),
+ tmvaVariables = cms.untracked.vstring(
+    "dRMean",
+    "frac01",
+    "frac02",
+    "frac03",
+    "frac04",
+    "frac05",
+    "nNeutrals",
+    "nvtx",
+    "beta",
+    "betaStar",
+    "dZ",
+    "nCharged",
+    ),
+ tmvaSpectators = cms.untracked.vstring(
     "jetPt",
     "jetEta",
     )
