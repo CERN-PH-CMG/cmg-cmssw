569bf69fa0b1b21b04f62404ad30380b683c8c31
abis <>
2012-04-13 07:49:27 +0000

    changed way of determining the full path of the mva weights file from os.environ["CMSSW_BASE"] to edm::FileInPath::fullPath()

--- CMGTools/Common/python/miscProducers/cmgPatElectronProducer_cfi.py
+++ CMGTools/Common/python/miscProducers/cmgPatElectronProducer_cfi.py
@@ -9,17 +9,15 @@ import FWCore.ParameterSet.Config as cms
 
 import os 
 
-basePath = os.environ['CMSSW_BASE'] + '/src/CMGTools/Common/data/ElectronMVAWeights/'
-
 cmgPatElectronProducer = cms.EDProducer(
     "CMGPATElectronProducer",
     src = cms.InputTag( 'selectedPatElectronsAK5' ),
     redEBRecHits = cms.InputTag( 'reducedEcalRecHitsEB' ),
     redEERecHits = cms.InputTag( 'reducedEcalRecHitsEE' ),
-    d0LowPt = cms.string( basePath + "Subdet0LowPt_NoIPInfo_BDTG.weights.xml" ),
-    d1LowPt = cms.string( basePath + "Subdet1LowPt_NoIPInfo_BDTG.weights.xml" ),
-    d2LowPt = cms.string( basePath + "Subdet2LowPt_NoIPInfo_BDTG.weights.xml" ),
-    d0HighPt = cms.string( basePath + "Subdet0HighPt_NoIPInfo_BDTG.weights.xml" ),
-    d1HighPt = cms.string( basePath + "Subdet1HighPt_NoIPInfo_BDTG.weights.xml" ),
-    d2HighPt = cms.string( basePath + "Subdet2HighPt_NoIPInfo_BDTG.weights.xml" )   
+    d0LowPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet0LowPt_NoIPInfo_BDTG.weights.xml" ),
+    d1LowPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet1LowPt_NoIPInfo_BDTG.weights.xml" ),
+    d2LowPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet2LowPt_NoIPInfo_BDTG.weights.xml" ),
+    d0HighPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet0HighPt_NoIPInfo_BDTG.weights.xml" ),
+    d1HighPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet1HighPt_NoIPInfo_BDTG.weights.xml" ),
+    d2HighPt = cms.FileInPath("CMGTools/Common/data/ElectronMVAWeights/Subdet2HighPt_NoIPInfo_BDTG.weights.xml" )   
 )
