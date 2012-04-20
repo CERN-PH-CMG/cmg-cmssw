e124a50b1debf98a6e67760af45716d49dc6f7a7
malberti <>
2012-04-20 11:39:16 +0000

    add also rewighted nvtx histos

--- UserArea/JetIDAnalysis/bin/LanciaAll.sh
+++ UserArea/JetIDAnalysis/bin/LanciaAll.sh
@@ -28,3 +28,13 @@ MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjet
 MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEout 30 50
 MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HF    30 50
 
+
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  20 30 1
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 20 30 1
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    20 30 1
+
+
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  30 50 1
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 30 50 1
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    30 50 1
+
