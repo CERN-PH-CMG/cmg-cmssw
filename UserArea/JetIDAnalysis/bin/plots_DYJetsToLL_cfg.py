05fdb38d483f8334069d349f3b5e55e04f75224d
malberti <>
2012-04-20 11:36:43 +0000

    clean up

--- UserArea/JetIDAnalysis/bin/plots_DYJetsToLL_cfg.py
+++ UserArea/JetIDAnalysis/bin/plots_DYJetsToLL_cfg.py
@@ -12,14 +12,11 @@ def getListOfFiles(expr, baseDir, filePattern):
 
 process = cms.Process("MakeJetIDPlots")
 
-
-#baseDir = '/store/cmst3/user/musella/JETID_NTUPLES/30_03_2012/'
 baseDir = '/store/cmst3/user/malberti/JETID_NTUPLES/10_04_2012/'
 filePattern = '*.root'
 
 process.Input = cms.PSet(
     inputFile = cms.vstring(
-    #"root://eoscms//eos/cms/store/cmst3/user/malberti/JETID_NTUPLES/26_03_2012/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/JetAnalysisTree_merged.root"),
     getListOfFiles('{baseDir}/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/{filePattern}',baseDir=baseDir, filePattern=filePattern)[:10]),
     inputTree = cms.string("chspfjetanalyzer"),
 )
@@ -27,7 +24,7 @@ process.Input = cms.PSet(
 process.Output = cms.PSet(
     outputRootFilePath = cms.string("../test/scripts_martina/rootfiles/"),
     #outputRootFilePath = cms.string("./"),
-    outputRootFileName = cms.string("histos_DYJetsToLL_chspfjets.root")
+    outputRootFileName = cms.string("histos_DYJetsToLL_pfjets.root")
 )
 
 process.Options = cms.PSet(
@@ -42,9 +39,4 @@ process.Options = cms.PSet(
     maxJetPt   = cms.double(500.),
     minJetEta  = cms.double(0.0),
     maxJetEta  = cms.double(5),
-
-    impactParTkThreshold = cms.double(0.) ,
-    tmvaWeights = cms.untracked.string("CMGTools/External/data/mva_JetID.weights.xml"),
-    tmvaMethod  = cms.untracked.string("JetID"),                 
-
 )
