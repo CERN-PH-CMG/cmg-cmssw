b066bf1e95e5978c3502711da5c43c9ce2def27e
cbern <>
2012-04-26 15:53:53 +0000

    new bleeding edge: - pile-up jet id - new cfg prototype - bug fixes in gui browser

--- CMGTools/Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
+++ CMGTools/Common/python/release_info/CMSSW_5_2_3_patch2_exp_BleedingEdge.py
@@ -1,19 +1,25 @@
-[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'hinzmann_patcontent_19Apr12'),
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'jose_April21'),
+[ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_pujetid_26Apr12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_pujetid_26Apr12'),
+  ('CMGTools/External', 'UserCode/CMG/CMGTools/External', 'V00-00-09'),
   ('CMGTools/Production', 'UserCode/CMG/CMGTools/Production', 'joseApril21'),
-  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_12Apr12'),
+  ('CMGTools/RootTools', 'UserCode/CMG/CMGTools/RootTools', 'cbern_forpietro_26Apr12'),
   ('CommonTools/ParticleFlow', 'CommonTools/ParticleFlow', 'V00-03-07-01'),
-  ('DataFormats/PatCandidates', 'DataFormats/PatCandidates', 'V06-05-00'),
-  ('EGamma/EGammaAnalysisTools','UserCode/EGamma/EGammaAnalysisTools','V00-00-05'),
+  ('DataFormats/PatCandidates', 'DataFormats/PatCandidates', 'cbern_25Apr12'),
+  ('EGamma/EGammaAnalysisTools', 'UserCode/EGamma/EGammaAnalysisTools', 'V00-00-05'),
   ( 'EgammaCalibratedGsfElectrons/CalibratedElectronAlgos',
     'UserCode/ClaudeCharlot/EgammaCalibratedGsfElectrons/CalibratedElectronAlgos',
     'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'),
   ( 'EgammaCalibratedGsfElectrons/CalibratedElectronProducers',
     'UserCode/ClaudeCharlot/EgammaCalibratedGsfElectrons/CalibratedElectronProducers',
     'Shervin20022012_2011Jan16ReRec0_and_Shervin05032012_Fall11MC_smearing'),
+  ('FWCore/GuiBrowsers', 'FWCore/GuiBrowsers', 'V00-00-60'),
   ('JetMETAnalysis/ecalDeadCellTools', 'JetMETAnalysis/ecalDeadCellTools', 'Colin_TaggingMode_June30'),
   ('PhysicsTools/PatAlgos', 'PhysicsTools/PatAlgos', 'V08-09-02'),
   ('PhysicsTools/PatUtils', 'PhysicsTools/PatUtils', 'V03-09-18-03'),
   ('PhysicsTools/Utilities', 'PhysicsTools/Utilities', 'V08-03-16'),
   ('RecoParticleFlow/PFProducer', 'RecoParticleFlow/PFProducer', 'V15-01-05'),
   ('RecoParticleFlow/PostProcessing', 'UserCode/RecoParticleFlow/PostProcessing', 'Colin_June23')]
+# The following packages were not in the input release_info.
+# Please make sure that the CVS module is correct!
+#	CMGTools/External                        V00-00-09
+#	FWCore/GuiBrowsers                       V00-00-60
