d668f1a67053cfd2e353a84c323162b7d0537c12
musella <>
2012-04-26 07:31:04 +0000

    Integration of pile-up ID information in cmg::PFJet

--- src/PFJet.cc
+++ src/PFJet.cc
@@ -16,3 +16,24 @@ const int cmg::PFJet::nConstituents() const{
   return nConstituents;
 
 }
+
+float cmg::PFJet::puMva(const std::string & name) const
+{
+	int index = puIdIndex(name);
+	if( index == -1 ) return UnSet(float);
+	return puMvas_.at(index);
+}
+
+int   cmg::PFJet::puId(const std::string & name) const
+{
+	int index = puIdIndex(name);
+	if( index == -1 ) return UnSet(int);
+	return puIds_.at(index);
+}
+
+int cmg::PFJet::puIdIndex(const std::string & name) const
+{
+	const PuIdNames::const_iterator index = std::find( puIdNames_.begin(), puIdNames_.end(), name);
+	if( index == puIdNames_.end() ) { return -1; }
+	return index - puIdNames_.begin();
+}
