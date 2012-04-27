196c71fc628b862c2cdc1a8105fa62fe6b218d15
pharris <>
2012-04-27 19:05:51 +0000

    Cut parameters

--- External/python/JetIdParams_cfi.py
+++ External/python/JetIdParams_cfi.py
@@ -87,4 +87,44 @@ EmptyJetIdParams = cms.PSet(
     Pt2030_Loose   = cms.vdouble(-999.,-999.,-999.,-999.),
     Pt3050_Loose   = cms.vdouble(-999.,-999.,-999.,-999.)
 )
+PuJetIdCutBased_wp = cms.PSet(
+    #4 Eta Categories  0-2.5 2.5-2.75 2.75-3.0 3.0-5.0
+    #betaStarClassic/log(nvtx-0.64) Values
+    #Tight Id
+    Pt010_BetaStarTight    = cms.vdouble( 0.15,0.15,0.15,0.15),
+    Pt1020_BetaStarTight   = cms.vdouble( 0.15,0.15,0.15,0.15),
+    Pt2030_BetaStarTight   = cms.vdouble( 0.15,0.15,0.15,0.15),
+    Pt3050_BetaStarTight   = cms.vdouble( 0.15,0.15,0.15,0.15),
+    
+    #Medium Id => Daniele
+    Pt010_BetaStarMedium   = cms.vdouble( 0.2, 0.2,  0.2, 0.2),
+    Pt1020_BetaStarMedium  = cms.vdouble( 0.2, 0.2,  0.2, 0.2),
+    Pt2030_BetaStarMedium  = cms.vdouble( 0.2, 0.2,  0.2, 0.2),
+    Pt3050_BetaStarMedium  = cms.vdouble( 0.2, 0.2,  0.2, 0.2),
+    
+    #Loose Id    Pt010_BetaStarLoose    = cms.vdouble( 0.2 , 0.3,  0.3, 0.3),
+    Pt1020_BetaStarLoose   = cms.vdouble( 0.2 , 0.3,  0.3, 0.3),
+    Pt2030_BetaStarLoose   = cms.vdouble( 0.2 , 0.3,  0.3, 0.3),
+    Pt3050_BetaStarLoose   = cms.vdouble( 0.2 , 0.3,  0.3, 0.3)
+
+    #RMS variable
+     #Tight Id
+    Pt010_RMSTight         = cms.vdouble( 0.06, 0.07, 0.04, 0.05),
+    Pt1020_RMSTight        = cms.vdouble( 0.06, 0.07, 0.04, 0.05),
+    Pt2030_RMSTight        = cms.vdouble( 0.05, 0.07, 0.03, 0.045),
+    Pt3050_RMSTight        = cms.vdouble( 0.05, 0.06, 0.03, 0.04),
+    
+    #Medium Id => Daniele
+    Pt010_RMSMedium        = cms.vdouble( 0.06, 0.03, 0.03, 0.04),
+    Pt1020_RMSMedium       = cms.vdouble( 0.06, 0.03, 0.03, 0.04),
+    Pt2030_RMSMedium       = cms.vdouble( 0.06, 0.03, 0.03, 0.04),
+    Pt3050_RMSMedium       = cms.vdouble( 0.06, 0.03, 0.03, 0.04),
+    
+    #Loose Id
+    Pt010_RMSLoose         = cms.vdouble( 0.08, 0.07, 0.05, 0.07),
+    Pt1020_RMSLoose        = cms.vdouble( 0.08, 0.07, 0.05, 0.07),
+    Pt2030_RMSLoose        = cms.vdouble( 0.08, 0.07, 0.04, 0.055),
+    Pt3050_RMSLoose        = cms.vdouble( 0.07, 0.06, 0.04, 0.05)
+    )
+
 
