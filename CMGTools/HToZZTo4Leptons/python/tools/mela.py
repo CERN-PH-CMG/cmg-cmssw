0e8cb3efbd5fd69e24cee4f33fca4f655585487c
cbern <>
2012-04-30 11:27:50 +0000

    added subclass

--- CMGTools/HToZZTo4Leptons/python/tools/mela.py
+++ CMGTools/HToZZTo4Leptons/python/tools/mela.py
@@ -8,8 +8,17 @@ gSystem.Load("libCMGToolsHToZZTo4Leptons")
 from ROOT import MELAAngles
 # need to make a MELAEstimator on the same model
 
+# you can even create a python class for high level stuff
+class LvMELAAngles(MELAAngles):
+    def __init__(self, lv1, lv2, lv3, lv4):
+        print lv1.Pt()
+        # here you could pass the variables necessary to the base class,
+        # to its constructor.
+        super(LvMELAAngles, self).__init__()
 
 if __name__ == '__main__':
 
-    melaAngles = MELAAngles()
-
+    from ROOT import TLorentzVector
+    lv1 = TLorentzVector(1,2,3,4)
+    melaAngles = LvMELAAngles(lv1, lv1, lv1, lv1)
+    
