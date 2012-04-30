59410c60dc725be4b7eb3ab24eaed1820f2fed93
cbern <>
2012-04-30 11:06:40 +0000

    added python binder for MELA, and prototype MELA class

---
+++ HToZZTo4Leptons/interface/MELAAngles.h
@@ -0,0 +1,13 @@
+#ifndef __CMGTools_HToZZTo4Leptons_MELAAngles_h__ 
+#define __CMGTools_HToZZTo4Leptons_MELAAngles_h__ 
+
+
+class MELAAngles {
+
+ public:
+
+  MELAAngles();
+
+};
+
+#endif
---
+++ HToZZTo4Leptons/src/classes.h
@@ -0,0 +1,10 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+# include "CMGTools/HToZZTo4Leptons/interface/MELAAngles.h"
+
+namespace {
+  struct CMGTools_HToZZTo4Leptons {
+
+    MELAAngles  melaAngles;
+
+  };
+}
