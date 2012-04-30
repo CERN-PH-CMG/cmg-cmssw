4ff2cdc15e13fa57a84697d83a8bbd3e32b28e8f
hinzmann <>
2012-04-30 15:53:32 +0000

    add jet substructure sequence

--- CMGTools/Common/python/PAT/addJetSubstructure_cff.py
+++ CMGTools/Common/python/PAT/addJetSubstructure_cff.py
@@ -1,38 +1,74 @@
 import FWCore.ParameterSet.Config as cms
-from PhysicsTools.PatAlgos.tools.jetTools import *
-from RecoJets.JetProducers.ak5PFJetsPruned_cfi import ak5PFJetsPruned
 
-def addJetSubstructureSequence( process , jetCorrections , runOnMC ):
-    process.ak5PrunedPFlow = ak5PFJetsPruned.clone(
-        src = process.pfJetsAK5.src,
-        doAreaFastjet = cms.bool(True)
-        )
-    addJetCollection(process, 
-                 cms.InputTag('ak5PrunedPFlow'),         
-                 'AK5Pruned', 'PF',
-                 doJTA=False,            # Run Jet-Track association & JetCharge
-                 doBTagging=False,       # Run b-tagging
-                 jetCorrLabel= ('AK5PFchs', cms.vstring(jetCorrections)),
-                 doType1MET=False,
-                 doL1Cleaning=False,
-                 doL1Counters=False,
-                 genJetCollection = cms.InputTag("ak5GenJetsNoNu"),
-                 doJetID = False
-                 )
-    process.patJetsAK5PrunedPF.addDiscriminators = cms.bool(False)
-    process.patJetsAK5PrunedPF.embedCaloTowers = cms.bool(False)
-    process.patJetsAK5PrunedPF.embedGenJetMatch = cms.bool(False)
-    process.patJetsAK5PrunedPF.embedGenPartonMatch = cms.bool(False)
-    process.patJetsAK5PrunedPF.addGenPartonMatch = cms.bool(False)
-    process.patJetsAK5PrunedPF.getJetMCFlavour = cms.bool(False)
-    process.patJetsAK5PrunedPF.embedPFCandidates = cms.bool(False)
-    process.patJetCorrFactorsAK5PrunedPF.rho = cms.InputTag("kt6PFJets","rho")
-    process.patJetsAK5PrunedPF.embedGenJetMatch = cms.bool(runOnMC)
-    process.patJetsAK5PrunedPF.addGenJetMatch = cms.bool(runOnMC)
-    if runOnMC:
-        process.jetSubstructureSequence = cms.Sequence(process.ak5PrunedPFlow*process.patJetGenJetMatchAK5PrunedPF*process.patJetCorrFactorsAK5PrunedPF*process.patJetsAK5PrunedPF*process.selectedPatJetsAK5PrunedPF)
-    else:
-        process.jetSubstructureSequence = cms.Sequence(process.ak5PrunedPFlow*process.patJetCorrFactorsAK5PrunedPF*process.patJetsAK5PrunedPF*process.selectedPatJetsAK5PrunedPF)
-    # disable embedding of genjets in PAT jets to avoid duplication of the genjet collection
-    if runOnMC:
-        process.patJetsAK5PrunedPF.embedGenJetMatch=False
+# JETS PRUNED ----------------------------
+
+# for jet substructure you need extra tags in 4XX and 52X:
+# for 4XX:
+# scram setup $CMS_PATH/slc5_amd64_gcc434/external/fastjet-toolfile/1.0-cms7/etc/scram.d/fastjet.xml
+# addpkg RecoJets/JetAlgorithms V04-04-00
+# addpkg RecoJets/JetProducers V05-10-02
+# for 52X:
+# addpkg RecoJets/JetProducers V05-10-02
+
+from RecoJets.JetProducers.ak5PFJetsPruned_cfi import ak5PFJetsPruned
+ak5PFJetsCHSpruned = ak5PFJetsPruned.clone(
+    src = 'pfNoPileUp',
+    doAreaFastjet = cms.bool(True)
+    )
+
+jetSource = 'ak5PFJetsCHSpruned'
+
+# corrections 
+from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
+patJetCorrFactorsCHSpruned = patJetCorrFactors.clone()
+patJetCorrFactorsCHSpruned.src = jetSource
+# will need to add L2L3 corrections in the cfg
+patJetCorrFactorsCHSpruned.levels = ['L1Offset', 'L2Relative', 'L3Absolute']
+patJetCorrFactorsCHSpruned.payload = 'AK5PF'
+
+# parton and gen jet matching
+
+from PhysicsTools.PatAlgos.mcMatchLayer0.jetMatch_cfi import *
+patJetPartonMatchCHSpruned = patJetPartonMatch.clone()
+patJetPartonMatchCHSpruned.src = jetSource
+patJetGenJetMatchCHSpruned = patJetGenJetMatch.clone()
+patJetGenJetMatchCHSpruned.src = jetSource
+patJetGenJetMatchCHSpruned.matched = 'ak5GenJetsNoNu'
+
+from PhysicsTools.PatAlgos.mcMatchLayer0.jetFlavourId_cff import *
+patJetPartonAssociationCHSpruned = patJetPartonAssociation.clone()
+patJetPartonAssociationCHSpruned.jets = jetSource
+
+# pat jets
+
+from PhysicsTools.PatAlgos.producersLayer1.jetProducer_cfi import *
+patJetsCHSpruned = patJets.clone()
+patJetsCHSpruned.jetSource = jetSource
+patJetsCHSpruned.addJetCharge = False
+patJetsCHSpruned.embedCaloTowers = False
+patJetsCHSpruned.embedPFCandidates = False
+patJetsCHSpruned.addAssociatedTracks = False
+patJetsCHSpruned.addBTagInfo = False
+patJetsCHSpruned.addDiscriminators = False
+patJetsCHSpruned.getJetMCFlavour = False
+patJetsCHSpruned.jetCorrFactorsSource = cms.VInputTag(cms.InputTag('patJetCorrFactorsCHSpruned'))
+patJetsCHSpruned.genPartonMatch = cms.InputTag('patJetPartonMatchCHSpruned')
+patJetsCHSpruned.genJetMatch = cms.InputTag('patJetGenJetMatchCHSpruned')
+
+from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
+selectedPatJetsCHSpruned = selectedPatJets.clone()
+selectedPatJetsCHSpruned.src = 'patJetsCHSpruned'
+selectedPatJetsCHSpruned.cut = 'pt()>10'
+
+jetMCSequenceCHSpruned = cms.Sequence(
+    patJetPartonMatchCHSpruned +
+    patJetGenJetMatchCHSpruned
+    )
+
+PATCMGJetSequenceCHSpruned = cms.Sequence(
+    ak5PFJetsCHSpruned +
+    jetMCSequenceCHSpruned +
+    patJetCorrFactorsCHSpruned +
+    patJetsCHSpruned +
+    selectedPatJetsCHSpruned
+    )
