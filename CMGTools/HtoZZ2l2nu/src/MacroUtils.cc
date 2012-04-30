4810c0cd5889e0ed09ecb27da3adf495dd7d16db
querten <>
2012-04-30 10:18:05 +0000

    vbf bug fix

--- CMGTools/HtoZZ2l2nu/src/MacroUtils.cc
+++ CMGTools/HtoZZ2l2nu/src/MacroUtils.cc
@@ -38,6 +38,8 @@ double weightVBF(std::string SampleName, double m_gen, double mass){
    bool isZZ = (SampleName.find("ZZ")!=std::string::npos) ;
 
    double decay_width = -1;
+
+/*
    double WeightInt  = -1;
          if(m_gen == 130){    decay_width =   0.00487;   if(isZZ){WeightInt = 1.000001; }else{ WeightInt =0.999999; }
    }else if(m_gen == 140){    decay_width =   0.00812;   if(isZZ){WeightInt = 1.000000; }else{ WeightInt =1.000000; }
@@ -47,14 +49,40 @@ double weightVBF(std::string SampleName, double m_gen, double mass){
    }else if(m_gen == 180){    decay_width =   0.63000;   if(isZZ){WeightInt = 1.000000; }else{ WeightInt =1.000000; }
    }else if(m_gen == 190){    decay_width =   1.04000;   if(isZZ){WeightInt = 1.000000; }else{ WeightInt =1.000000; }
    }else if(m_gen == 200){    decay_width =   1.43000;   if(isZZ){WeightInt = 0.999749; }else{ WeightInt =0.999183; }
+   }else if(m_gen == 250){    decay_width =   4.04000;   if(isZZ){WeightInt = 0.999749; }else{ WeightInt =0.999183; }
    }else if(m_gen == 300){    decay_width =   8.43000;   if(isZZ){WeightInt = 0.987308; }else{ WeightInt =0.989306; }
+   }else if(m_gen == 350){    decay_width =  15.20000;   if(isZZ){WeightInt = 0.987308; }else{ WeightInt =0.989306; }
    }else if(m_gen == 400){    decay_width =  29.20000;   if(isZZ){WeightInt = 0.951238; }else{ WeightInt =0.950328; }
+   }else if(m_gen == 450){    decay_width =  46.95000;   if(isZZ){WeightInt = 0.951238; }else{ WeightInt =0.950328; }
    }else if(m_gen == 500){    decay_width =  68.00000;   if(isZZ){WeightInt = 0.881467; }else{ WeightInt =0.883517; }
+   }else if(m_gen == 550){    decay_width =  93.15000;   if(isZZ){WeightInt = 0.881467; }else{ WeightInt =0.883517; }
    }else if(m_gen == 600){    decay_width = 123.00000;   if(isZZ){WeightInt = 0.792347; }else{ WeightInt =0.807353; }
+   }
+*/
+
+   double WeightInt  = -1;
+         if(m_gen == 130){    decay_width =   0.00487; 
+   }else if(m_gen == 140){    decay_width =   0.00812; 
+   }else if(m_gen == 150){    decay_width =   0.01730; 
+   }else if(m_gen == 160){    decay_width =   0.08290; 
+   }else if(m_gen == 170){    decay_width =   0.38000; 
+   }else if(m_gen == 180){    decay_width =   0.63000; 
+   }else if(m_gen == 190){    decay_width =   1.04000; 
+   }else if(m_gen == 200){    decay_width =   1.43000; 
+   }else if(m_gen == 250){    decay_width =   4.04000; 
+   }else if(m_gen == 300){    decay_width =   8.43000; 
+   }else if(m_gen == 350){    decay_width =  15.20000; 
+   }else if(m_gen == 400){    decay_width =  29.20000; 
+   }else if(m_gen == 450){    decay_width =  46.95000; 
+   }else if(m_gen == 500){    decay_width =  68.00000;
+   }else if(m_gen == 550){    decay_width =  93.15000;
+   }else if(m_gen == 600){    decay_width = 123.00000;
    }
 
    double s = pow(mass,2);
    double weight_BW = s/pow(m_gen,2) * (pow(s-pow(m_gen,2),2)+ pow(m_gen*decay_width,2)) / (pow(s-pow(m_gen,2),2)+ pow(m_gen*decay_width*s/(pow(m_gen,2)),2));
-   return weight_BW / WeightInt;
+   double toReturn = weight_BW;
+   if(toReturn<0){toReturn=1.0;  printf("weightVBF<0=%f\n",toReturn);}
+   return toReturn;
 }
 
