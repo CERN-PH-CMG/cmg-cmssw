b13e525d1c34f31371c218bcd3fc1871c8e86132
hinzmann <>
2012-05-03 13:20:27 +0000

    change electron rejection setting for tautau hadronic

--- H2TauTau/python/objects/diTauCuts_cff.py
+++ H2TauTau/python/objects/diTauCuts_cff.py
@@ -6,15 +6,15 @@ from CMGTools.H2TauTau.objects.tauCuts_cff import getTauCuts
 diTauCuts = cms.PSet(
     baseline = cms.PSet(
          mass = cms.string('mass()>10'),
-         tau1Leg = getTauCuts('leg1','diTau').clone(),
-         tau2Leg = getTauCuts('leg2','diTau').clone(),
+         tau1Leg = getTauCuts('leg1','diTau1').clone(),
+         tau2Leg = getTauCuts('leg2','diTau2').clone(),
          ),
     # this cut is kept out of the baseline for now, until it is studied.
     # caloMuVeto = cms.string('leg1().eOverP()>0.2'),
     skimming = cms.PSet(
          mass = cms.string('mass()>10'),
-         tau1LegSkimCuts = getTauCuts('leg1', 'diTau', skim=True).clone(),
-         tau2LegSkimCuts = getTauCuts('leg2', 'diTau', skim=True).clone()
+         tau1LegSkimCuts = getTauCuts('leg1', 'diTau1', skim=False).clone(),
+         tau2LegSkimCuts = getTauCuts('leg2', 'diTau2', skim=False).clone()
          )
     )
 
