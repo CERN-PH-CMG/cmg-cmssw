c9b453290a8423114d6dd3303e32bb8a2788509b
cbern <>
2012-05-05 13:39:16 +0000

    fixed a bug on tkmet/nopumet

--- Common/python/release_info/CMSSW_4_4_4_exp_colin_BleedingEdge.py
+++ Common/python/release_info/CMSSW_4_4_4_exp_colin_BleedingEdge.py
@@ -1,5 +1,5 @@
 [ ('AnalysisDataFormats/CMGTools', 'UserCode/CMG/AnalysisDataFormats/CMGTools', 'cbern_tauid_04May12'),
-  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_tauid_04May12'),
+  ('CMGTools/Common', 'UserCode/CMG/CMGTools/Common', 'cbern_tkmet_05May12'),
   ('CMGTools/External', 'UserCode/CMG/CMGTools/External', 'V00-02-02'),
   ('CMGTools/H2TauTau', 'UserCode/CMG/CMGTools/H2TauTau', 'cbern_b_12Apr12'),
   ('CMGTools/HToZZTo4Leptons', 'UserCode/CMG/CMGTools/HToZZTo4Leptons', 'cbern_12Apr12'),
