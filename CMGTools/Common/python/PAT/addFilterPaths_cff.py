a45eda71f468629d669079841c9fd37d4022a718
wreece <>
2012-05-09 11:07:38 +0000

    Includes the filters again

--- CMGTools/Common/python/PAT/addFilterPaths_cff.py
+++ CMGTools/Common/python/PAT/addFilterPaths_cff.py
@@ -8,33 +8,32 @@ totalKinematicsFilterPath = cms.Path(totalKinematicsFilter)
 
 #these filters are from this page: https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFilters
 
-# 44X does not work
-# from the RecoMET package
-# from RecoMET.METFilters.EcalDeadCellBoundaryEnergyFilter_cfi import EcalDeadCellBoundaryEnergyFilter
-# EcalDeadCellBoundaryEnergyFilterPath = cms.Path(EcalDeadCellBoundaryEnergyFilter)
+#from the RecoMET package
+from RecoMET.METFilters.EcalDeadCellBoundaryEnergyFilter_cfi import EcalDeadCellBoundaryEnergyFilter
+EcalDeadCellBoundaryEnergyFilterPath = cms.Path(EcalDeadCellBoundaryEnergyFilter)
 
-# from RecoMET.METFilters.EcalDeadCellDeltaRFilter_cfi import simpleDRfilter
-# simpleDRfilterPath = cms.Path(simpleDRfilter)
+from RecoMET.METFilters.EcalDeadCellDeltaRFilter_cfi import simpleDRfilter
+simpleDRfilterPath = cms.Path(simpleDRfilter)
 
-#from RecoMET.METFilters.EcalDeadCellTriggerPrimitiveFilter_cfi import EcalDeadCellTriggerPrimitiveFilter
-#EcalDeadCellTriggerPrimitiveFilter = cms.Path(EcalDeadCellTriggerPrimitiveFilter)
+from RecoMET.METFilters.EcalDeadCellTriggerPrimitiveFilter_cfi import EcalDeadCellTriggerPrimitiveFilter
+EcalDeadCellTriggerPrimitiveFilterPath = cms.Path(EcalDeadCellTriggerPrimitiveFilter)
 
-#from RecoMET.METFilters.greedyMuonPFCandidateFilter_cfi import greedyMuonPFCandidateFilter
-#greedyMuonPFCandidateFilterPath = cms.Path(greedyMuonPFCandidateFilter)
+from RecoMET.METFilters.greedyMuonPFCandidateFilter_cfi import greedyMuonPFCandidateFilter
+greedyMuonPFCandidateFilterPath = cms.Path(greedyMuonPFCandidateFilter)
 
-#from RecoMET.METFilters.hcalLaserEventFilter_cfi import hcalLaserEventFilter
-#hcalLaserEventFilterPath = cms.Path(hcalLaserEventFilter)
+from RecoMET.METFilters.hcalLaserEventFilter_cfi import hcalLaserEventFilter
+hcalLaserEventFilterPath = cms.Path(hcalLaserEventFilter)
 
-# from RecoMET.METFilters.inconsistentMuonPFCandidateFilter_cfi import inconsistentMuonPFCandidateFilter
-# inconsistentMuonPFCandidateFilterPath = cms.Path(inconsistentMuonPFCandidateFilter)
+from RecoMET.METFilters.inconsistentMuonPFCandidateFilter_cfi import inconsistentMuonPFCandidateFilter
+inconsistentMuonPFCandidateFilterPath = cms.Path(inconsistentMuonPFCandidateFilter)
 
-# from RecoMET.METFilters.trackingFailureFilter_cfi import trackingFailureFilter
-# trackingFailureFilter.VertexSource = "offlinePrimaryVertices"
-# trackingFailureFilter.JetSource = 'ak5PFJets'
-# trackingFailureFilterPath = cms.Path(trackingFailureFilter)
+from RecoMET.METFilters.trackingFailureFilter_cfi import trackingFailureFilter
+trackingFailureFilter.VertexSource = "offlinePrimaryVertices"
+trackingFailureFilter.JetSource = 'ak5PFJets'
+trackingFailureFilterPath = cms.Path(trackingFailureFilter)
 
-#from RecoMET.METAnalyzers.CSCHaloFilter_cfi import CSCTightHaloFilter
-#CSCTightHaloFilterPath = cms.Path(CSCTightHaloFilter)
+from RecoMET.METAnalyzers.CSCHaloFilter_cfi import CSCTightHaloFilter
+CSCTightHaloFilterPath = cms.Path(CSCTightHaloFilter)
 
 from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import HBHENoiseFilter
 HBHENoiseFilterPath = cms.Path(HBHENoiseFilter)
