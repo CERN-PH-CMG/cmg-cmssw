f7ae8286a9bc49dc4a788be403fe8caf890f92a4
wreece <>
2012-05-09 11:35:28 +0000

    Removes the old filters

--- CMGTools/Common/python/eventContent/eventCleaning_cff.py
+++ CMGTools/Common/python/eventContent/eventCleaning_cff.py
@@ -4,14 +4,6 @@ from RecoParticleFlow.PostProcessing.pfPostProcessingEventContent_cff import pfP
 
 eventCleaning = pfPostProcessingEventContent
 eventCleaning += cms.untracked.vstring(
-    'keep *_ecalDeadCellTPfilter_*_*',
-    'keep *_HBHENoiseFilterResultProducer*_*_*',
-    'keep *_BeamHaloSummary_*_*',
-    'keep *_recovRecHitFilter_*_*',
-    'keep *_eeNoiseFilter_*_*',
-    'keep *_trackingFailureFilter_*_*',
-    'keep *_goodPrimaryVertexFilter_*_*',
-    'keep *_scrapingFilter_*_*',
-    'keep *_totalKinematicsFilterCMG_*_*'
+    'keep edmTriggerResults_TriggerResults_*_*'
     )
 
