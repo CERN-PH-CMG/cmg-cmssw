64a69f58ee9beba4c5d33e06615b7d27f15737f6
pjanot <>
2012-05-10 09:49:20 +0000

    A photon factory that takes selected PF photons to turn them into cmg Photon, with isolation information

--- CMGTools/Common/interface/PhotonFactory.h
+++ CMGTools/Common/interface/PhotonFactory.h
@@ -7,18 +7,68 @@
 #include "AnalysisDataFormats/CMGTools/interface/Photon.h"
 #include "CMGTools/Common/interface/Factory.h"
 
+#include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
+#include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
+//#include "DataFormats/PatCandidates/interface/Isolation.h"
+
+#include "CMGTools/Common/interface/SpecificIsolation.h"
+
+
 namespace cmg {
+
+  typedef edm::Handle< edm::ValueMap<reco::IsoDeposit> > IsoDepositMap;
+  typedef edm::Handle< edm::ValueMap<double> > IsolationValueMap;
+  typedef reco::isodeposit::AbsVetos AbsVetos;
+   
 
   class PhotonFactory : public Factory<cmg::Photon> {
   public:
     PhotonFactory(const edm::ParameterSet& ps):
-      photonLabel_(ps.getParameter<edm::InputTag>("inputCollection"))
-      {}
+      photonLabel_(ps.getParameter<edm::InputTag>("inputCollection")),
+      isoDepWithChargedLabel_(ps.getParameter<edm::InputTag>("isoDepWithCharged")),
+      isoDepWithPhotonsLabel_(ps.getParameter<edm::InputTag>("isoDepWithPhotons")),
+      isoDepWithNeutralLabel_(ps.getParameter<edm::InputTag>("isoDepWithPU")),
+      isoDepWithPULabel_(ps.getParameter<edm::InputTag>("isoDepWithNeutral")),
+      maxAbsoluteIsolation_(ps.getParameter<double>("maxAbsoluteIsolation")),
+      maxRelativeIsolation_(ps.getParameter<double>("maxRelativeIsolation"))
+	{
+	  chargedHadronIsoPar_.initialize( ps.getParameter<edm::ParameterSet>("chargedHadronIsoPar"));
+	  neutralHadronIsoPar_.initialize( ps.getParameter<edm::ParameterSet>("neutralHadronIsoPar"));
+	  photonsIsoPar_.initialize( ps.getParameter<edm::ParameterSet>("photonsIsoPar"));
+	  puIsoPar_.initialize( ps.getParameter<edm::ParameterSet>("puIsoPar"));
+	  allIsoPar_.initialize( ps.getParameter<edm::ParameterSet>("allIsoPar"));
+	}
+
     virtual event_ptr create(const edm::Event&, const edm::EventSetup&);
-/*     virtual void set(const pat::PhotonPtr& input, cmg::Photon* const output) const; */
+    virtual void set(const pat::PhotonPtr& input, cmg::Photon* const output) const;
 
   private:
     const edm::InputTag photonLabel_;
+    const edm::InputTag isoDepWithChargedLabel_;
+    const edm::InputTag isoDepWithPhotonsLabel_;
+    const edm::InputTag isoDepWithNeutralLabel_;
+    const edm::InputTag isoDepWithPULabel_;
+
+    IsoDepositMap isoDepWithCharged, isoDepWithPhotons, isoDepWithNeutral, isoDepWithPU;
+
+    /// parameters for charged hadron isolation value
+    SpecificIsolation chargedHadronIsoPar_;
+    
+    /// parameters for neutral hadron isolation value
+    SpecificIsolation neutralHadronIsoPar_;
+    
+    /// parameters for photon isolation value
+    SpecificIsolation photonsIsoPar_;
+   
+    /// parameters for PU isolation value
+    SpecificIsolation puIsoPar_;
+   
+    /// parameters for particle isolation without strip veto.
+    SpecificIsolation allIsoPar_;
+
+    double maxAbsoluteIsolation_;
+    double maxRelativeIsolation_;
+   
   };
 
 } // namespace cmg
--- CMGTools/Common/python/factories/cmgPhoton_cfi.py
+++ CMGTools/Common/python/factories/cmgPhoton_cfi.py
@@ -5,14 +5,96 @@ import FWCore.ParameterSet.Config as cms
 from CMGTools.Common.selections.kinematics_cfi import kinematics
 
 photonFactory = cms.PSet(
-    inputCollection = cms.InputTag("dummy"),
-    )
+    inputCollection = cms.InputTag("pfSelectedPhotons"),
+    isoDepWithCharged = cms.InputTag("isoDepPhotonWithCharged"),
+    isoDepWithPhotons = cms.InputTag("isoDepPhotonWithPhotons"),
+    isoDepWithNeutral = cms.InputTag("isoDepPhotonWithNeutral"),
+    isoDepWithPU = cms.InputTag("isoDepPhotonWithPU"),
+    #maxAbsoluteIsolation = cms.double(10.),
+    #maxRelativeIsolation = cms.double(1.0),
+    maxAbsoluteIsolation = cms.double(1E9),
+    maxRelativeIsolation = cms.double(1E9),
+    ## each IsoPar has a coneSize parameter (radius of the cone inside which
+    ## the isolation is computed) plus a VPSet called vetoes, made of any of the following
+    ## vetoes: ThresholdVeto, ConeVeto, RectangularEtaPhiVeto (more vetoes can be implemented if needed)
+    ## look at DataFormats/RecoCandidate/src/IsoDepositVetos.cc for their meaning.
+    chargedHadronIsoPar=cms.PSet(
+      coneSize=cms.double(0.3),
+      vetoes=cms.VPSet(
+         cms.PSet(
+            #see task #15859 for discussion of this veto
+            type=cms.string("RectangularEtaPhiVeto"),
+            dEta=cms.double(0.01),# eta width of the strip to be vetoed
+            dPhi=cms.double(0.20) # phi width of the strip to be vetoed
+         ),
+         cms.PSet(
+            type=cms.string("ThresholdVeto"),
+            threshold=cms.double(0.2)# pt of the particles to be vetoed
+         ),
+      ),
+    ),
+
+    neutralHadronIsoPar=cms.PSet(
+      coneSize=cms.double(0.3),
+      vetoes=cms.VPSet(
+         cms.PSet(
+            #see task #15859 for discussion of this veto
+            type=cms.string("RectangularEtaPhiVeto"),
+            dEta=cms.double(0.01),# eta width of the strip to be vetoed
+            dPhi=cms.double(0.20) # phi width of the strip to be vetoed
+         ),
+         cms.PSet(
+            type=cms.string("ThresholdVeto"),
+            threshold=cms.double(0.2)# pt of the particles to be vetoed
+         ),
+      ),
+    ),
+
+    photonsIsoPar=cms.PSet(
+      coneSize=cms.double(0.3),
+      vetoes=cms.VPSet(
+         cms.PSet(
+            #see task #15859 for discussion of this veto
+            type=cms.string("RectangularEtaPhiVeto"),
+            dEta=cms.double(0.01),# eta width of the strip to be vetoed
+            dPhi=cms.double(0.20) # phi width of the strip to be vetoed
+         ),
+         cms.PSet(
+            type=cms.string("ThresholdVeto"),
+            threshold=cms.double(0.2)# pt of the particles to be vetoed
+         ),
+      ),
+    ),                          
+
+    puIsoPar=cms.PSet(
+      coneSize=cms.double(0.3),
+      vetoes=cms.VPSet(
+         cms.PSet(
+            type=cms.string("ThresholdVeto"),
+            threshold=cms.double(0.2)# pt of the particles to be vetoed
+         ),
+      ),
+    ),                          
+
+    allIsoPar=cms.PSet(
+      coneSize=cms.double(0.3),
+      vetoes=cms.VPSet(
+         cms.PSet(
+            type=cms.string("ThresholdVeto"),
+            threshold=cms.double(0.2)# pt of the particles to be vetoed
+         ),
+      ),
+    ),                          
+)
 
 cmgPhoton = cms.EDFilter("PhotonPOProducer",
                          cfg = photonFactory.clone(
-    inputCollection = cms.InputTag("selectedPatPhotonsAK5")
-    ),
-                         cuts = cms.PSet(kinematics = kinematics.clone())
+                         ),
+                         cuts = cms.PSet(kinematics = kinematics.clone(
+                              pt = cms.string('pt() > 2'),
+                              eta = cms.string('abs(eta()) < 2.5'),
+                              phi = cms.string('abs(phi()) < 3.2')
+                         ))
                          )
 
 ######################################################################
