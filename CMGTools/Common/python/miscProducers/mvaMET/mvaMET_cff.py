d84a83e071f694d52c6a3c75cabbe788116317c6
cbern <>
2012-05-10 14:54:24 +0000

    corrections

--- CMGTools/Common/python/miscProducers/mvaMET/mvaMET_cff.py
+++ CMGTools/Common/python/miscProducers/mvaMET/mvaMET_cff.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.Common.eventCleaning.goodPVFilter_cfi import goodPVFilter 
-from CMGTools.Common.miscProducers.mvaMET.mvaMETMuMu_cfi import * 
+from CMGTools.Common.miscProducers.mvaMET.mvaMETTauMu_cfi import * 
 
 mvaMETSequence = cms.Sequence(
     goodPVFilter + 
-    mvaMETMuMu
+    mvaMETTauMu
     )
