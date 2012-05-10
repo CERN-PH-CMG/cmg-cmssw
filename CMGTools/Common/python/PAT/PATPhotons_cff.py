d7b247a53b683b4cbbb237452564db5fb0c41359
pjanot <>
2012-05-10 13:23:40 +0000

    Fix a cut on the selected photons, and a missing component name for isodeposits (valid only in 530)

--- CMGTools/Common/python/PAT/PATPhotons_cff.py
+++ CMGTools/Common/python/PAT/PATPhotons_cff.py
@@ -2,19 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 # isolation (PF only, must not bourred !)
 
-# from CommonTools.ParticleFlow.Isolation.pfPhotonIsolation_cff import *
 from CMGTools.Common.PAT.photonPFIsolationDeposits_cff import *
 from CommonTools.ParticleFlow.ParticleSelectors.pfSelectedPhotons_cfi import *
-# from CommonTools.ParticleFlow.Isolation.pfIsolatedPhotons_cfi import *
 from CMGTools.Common.photon_cff import *
 
 sourcePhotons = 'pfSelectedPhotons'
-
-# the name of the track muon deposits changed
-# from CMGTools.Common.Tools.cmsswRelease import cmsswIs44X
-# tkDeposits = cms.InputTag("muons","muIsoDepositTk")
-# if cmsswIs44X():
-#    tkDeposits = cms.InputTag("muIsoDepositTk")
+pfSelectedPhotons.cut = cms.string("pt>2&&abs(eta)<2.5")
 
 phPFIsoDepositCharged.src = sourcePhotons
 phPFIsoDepositChargedAll.src = sourcePhotons
@@ -22,17 +15,7 @@ phPFIsoDepositNeutral.src = sourcePhotons
 phPFIsoDepositGamma.src = sourcePhotons
 phPFIsoDepositPU.src = sourcePhotons
 
-pfPhotonIsoDepositsSequence = cms.Sequence(
-    phPFIsoDepositCharged   +
-    phPFIsoDepositChargedAll   +
-    phPFIsoDepositNeutral   +
-    phPFIsoDepositGamma   +
-    phPFIsoDepositPU
-)
-
 PATPhotonSequence = cms.Sequence(
     pfSelectedPhotons+
-    pfPhotonIsoDepositsSequence
-    # +
-    # pfIsolatedPhotons
+    photonPFIsolationDepositsSequence
     )
