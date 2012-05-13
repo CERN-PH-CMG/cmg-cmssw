01d07cc524169323afdbeb38024f0ae234aa1145
bachtis <>
2012-05-13 01:13:52 +0000

    Adding new Tree producer

--- CMGTools/HToZZTo4Leptons/python/analyzers/FourLeptonTreeProducer.py
+++ CMGTools/HToZZTo4Leptons/python/analyzers/FourLeptonTreeProducer.py
@@ -1,80 +1,12 @@
-from CMGTools.RootTools.analyzers.TreeAnalyzer import TreeAnalyzer
+from CMGTools.HToZZTo4Leptons.analyzers.TreeProducer import TreeProducer
+import numpy
+import ROOT
 
-class FourLeptonTreeProducer( TreeAnalyzer ):
+class FourLeptonTreeProducer( TreeProducer ):
     '''Tree producer for the H->ZZ->4 lepton analysis.
 
     FIXME: A SINGLE TREE FOR ALL ANALYZERS
     Some of the functions in this class should be made available to everybody.'''
-    def var(self, varName ):
-        self.tree.addVar('float', varName)
-
-    def fill(self, varName, value ):
-        setattr( self.tree.s, varName, value )
-
-
-    def bookLepton(self, pName ):
-        self.var('{pName}_Pt'.format(pName=pName))
-        self.var('{pName}_Eta'.format(pName=pName))
-        self.var('{pName}_Phi'.format(pName=pName))
-        self.var('{pName}_Charge'.format(pName=pName))
-        self.var('{pName}_PdgId'.format(pName=pName))
-        self.var('{pName}_SIP3D'.format(pName=pName))
-        self.var('{pName}_IsoDB'.format(pName=pName))
-        self.var('{pName}_IsoEA'.format(pName=pName))
-        self.var('{pName}_Mass'.format(pName=pName))
-
-
-    def fillBasic(self, pName,particle ):
-        self.fill('{pName}_Pt'.format(pName=pName), particle.pt() )
-        self.fill('{pName}_Eta'.format(pName=pName), particle.eta() )
-        self.fill('{pName}_Phi'.format(pName=pName), particle.phi() )
-        self.fill('{pName}_Charge'.format(pName=pName), particle.charge() )
-        self.fill('{pName}_PdgId'.format(pName=pName), particle.pdgId() )
-        self.fill('{pName}_Mass'.format(pName=pName), particle.mass())
-
-    def fillLepton(self, pName,particle,event ):
-        self.fillBasic(pName,particle)
-        self.fill('{pName}_SIP3D'.format(pName=pName), particle.sip3D() )
-        self.fill('{pName}_IsoDB'.format(pName=pName), particle.absIso(0.5) )
-        if hasattr(event,'rho'):
-            self.fill('{pName}_IsoEA'.format(pName=pName), particle.absEffAreaIso(event.rho,self.cfg_ana.effectiveAreas) )
-
-    def fillEventInfo(self, iEvent,event ):
-        self.fill('EVENT',iEvent.eventAuxiliary().id().event())
-        self.fill('RUN',iEvent.eventAuxiliary().id().run())
-        self.fill('LUMI',iEvent.eventAuxiliary().id().luminosityBlock())
-        if hasattr(event,'met'):
-            self.fill('met',event.met.pt())
-        if hasattr(event,'rho'):
-            self.fill('rho',event.rho)
-        if hasattr(event,'step'):
-            self.fill('step',event.step)
-        if hasattr(event,'skim'):
-            self.fill('skim',event.skim)
-        
-    def bookBoson(self, pName ):
-        self.var('{pName}_Pt'.format(pName=pName))
-        self.var('{pName}_Eta'.format(pName=pName))
-        self.var('{pName}_Phi'.format(pName=pName))
-        self.var('{pName}_Charge'.format(pName=pName))
-        self.var('{pName}_Mass'.format(pName=pName))
-
-    def fillBoson(self, pName,particle ):
-        self.fillBasic(pName,particle)
-
-
-
-    def bookEventInfo(self):    
-        self.tree.addVar('int', "RUN")
-        self.tree.addVar('int', "LUMI")
-        self.tree.addVar('int', "EVENT")
-        self.var('met')
-        self.var('rho')
-        self.var('step')
-        self.var('skim')
-            
-
-    
     def declareVariables(self):
 
         #Book event Info
@@ -107,14 +39,10 @@ class FourLeptonTreeProducer( TreeAnalyzer ):
         self.bookLepton("ZFR_leg1")
         self.bookLepton("ZFR_leg2")
         self.bookLepton("ZFR_probe")
-        self.tree.addVar('int', "ZFR_nProbes")
-
-
-        
-        self.tree.book()
-
+        self.var("ZFR_nProbes",int)
 
     def process(self, iEvent, event):
+        self.reset()
 
         #get the analyzer event
         subevent = getattr( event, self.cfg_ana.anaName )
@@ -151,4 +79,6 @@ class FourLeptonTreeProducer( TreeAnalyzer ):
             if len(subevent.leptonsForFakeRate)>0:
                 self.fillLepton("ZFR_probe",subevent.leptonsForFakeRate[0],subevent)
  
-        self.tree.fill()
+        self.tree.Fill()
+
+
