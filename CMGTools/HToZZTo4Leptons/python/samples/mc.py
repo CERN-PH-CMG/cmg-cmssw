d0bc86f937e0538b99319811f07828b5bea21389
bachtis <>
2012-05-16 16:09:59 +0000

    *** empty log message ***

---
+++ CMGTools/HToZZTo4Leptons/python/samples/mc.py
@@ -0,0 +1,154 @@
+import CMGTools.RootTools.fwlite.Config as cfg
+
+GGH120 = cfg.MCComponent(
+    name = 'GGH120',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+GGH130 = cfg.MCComponent(
+    name = 'GGH130',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+GGH150 = cfg.MCComponent(
+    name = 'GGH150',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+GGH200 = cfg.MCComponent(
+    name = 'GGH200',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+GGH350 = cfg.MCComponent(
+    name = 'GGH350',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+GGH400 = cfg.MCComponent(
+    name = 'GGH400',
+    files = [],
+    xSection = 1,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+
+
+ZZ4mu = cfg.MCComponent(
+    name = 'ZZ4mu',
+    files = [],
+    xSection = 0.01290,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZ4e = cfg.MCComponent(
+    name = 'ZZ4e',
+    files = [],
+    xSection = 0.01290,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZ4tau = cfg.MCComponent(
+    name = 'ZZ4tau',
+    files = [],
+    xSection = 0.01290,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZ2e2mu = cfg.MCComponent(
+    name = 'ZZ2e2mu',
+    files = [],
+    xSection = 0.02442,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZ2e2tau = cfg.MCComponent(
+    name = 'ZZ2e2tau',
+    files = [],
+    xSection = 0.02442,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZ2mu2tau = cfg.MCComponent(
+    name = 'ZZ2mu2tau',
+    files = [],
+    xSection = 0.02442,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZGG4L = cfg.MCComponent(
+    name = 'ZZGG4L',
+    files = [],
+    xSection = 0.00174,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+ZZGG2L2L = cfg.MCComponent(
+    name = 'ZZGG2L2L',
+    files = [],
+    xSection = 0.00348,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+
+
+DYJets = cfg.MCComponent(
+    name = 'DYJets',
+    files = [],
+    xSection = 3048.,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+DYJetsLowMass = cfg.MCComponent(
+    name = 'DYJetsLowMass',
+    files = [],
+    xSection = 3048.,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1)
+
+
+TTJets = cfg.MCComponent(
+    name = 'TTJets',
+    files = [],
+    xSection = 17.32,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1 )
+
+WZ = cfg.MCComponent(
+    name = 'WZ',
+    files = [],
+    xSection =0.595 ,
+    nGenEvents = 1,
+    triggers = [],
+    effCorrFactor = 1 )
+
+
+
+
