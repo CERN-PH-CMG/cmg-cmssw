d6d85432e4ff6f023c54db63f7b6facd2236fe2b
bachtis <>
2012-05-16 15:44:56 +0000

    *** empty log message ***

--- HToZZTo4Leptons/python/samples/data_2011.py
+++ HToZZTo4Leptons/python/samples/data_2011.py
@@ -1,49 +1,42 @@
 import CMGTools.RootTools.fwlite.Config as cfg
 import os
 
-json = '/'.join( [ os.environ['CMSSW_BASE'],
-                   'src/CMGTools/HToZZTo4Leptons/json/Cert_PRL_2011.txt' ] ) 
-
-
-#FIXME: json!
-#FIXME: add cross triggers
-#FIXME: read torino files directly ? 
-
-data_DoubleElectron_Run2011A_16Jan2012 = cfg.DataComponent(
-    name = 'data_DoubleElectron_Run2011A_16Jan2012',
+json ='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt'
+#json='/afs/cern.ch/user/n/ndefilip//public/CertTot2011.txt'
+data_DoubleElectronA = cfg.DataComponent(
+    name = 'data_DoubleElectronA',
     files = [],
-    intLumi = 4700-2511,
+    intLumi = 1,
     triggers = [],
     json = json
     )
 
-data_DoubleMu_Run2011A_16Jan2012 = cfg.DataComponent(
-    name = 'data_DoubleMu_Run2011A_16Jan2012',
+data_DoubleMuA = cfg.DataComponent(
+    name = 'data_DoubleMuA',
     files = [],
-    intLumi = 4700-2511,
+    intLumi = 1,
     triggers = [],
     json = json
     )
 
-# see http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/Torino/ZZAnalysis/AnalysisStep/test/Plotter/Luminosity.txt?revision=1.36&view=markup
 
-data_DoubleElectron_Run2011B_16Jan2012 = cfg.DataComponent(
-    name = 'data_DoubleElectron_Run2011B_16Jan2012',
+data_DoubleElectronB = cfg.DataComponent(
+    name = 'data_DoubleElectronB',
     files = [],
-    intLumi = 2511,
+    intLumi = 1,
     triggers = [],
     json = json
     )
 
-data_DoubleMu_Run2011B_16Jan2012 = cfg.DataComponent(
-    name = 'data_DoubleMu_Run2011B_16Jan2012',
+data_DoubleMuB = cfg.DataComponent(
+    name = 'data_DoubleMuB',
     files = [],
-    intLumi = 2511,
+    intLumi = 1,
     triggers = [],
     json = json
     )
 
-data_DoubleMu_2011 = [data_DoubleMu_Run2011A_16Jan2012,
-                      data_DoubleMu_Run2011B_16Jan2012]
-data_DoubleElectron_2011 = [data_DoubleElectron_Run2011A_16Jan2012,
-                            data_DoubleElectron_Run2011B_16Jan2012]
+data_DoubleMu_2011 = [data_DoubleMuA,
+                      data_DoubleMuB]
+data_DoubleElectron_2011 = [data_DoubleElectronA,
+                            data_DoubleElectronB]
