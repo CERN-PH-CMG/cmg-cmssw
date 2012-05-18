854a7ecc58129469d2d92fb36b226b9104e5a78a
bachtis <>
2012-05-18 23:38:57 +0000

    *** empty log message ***

--- RootTools/python/physicsobjects/PhysicsObjects.py
+++ RootTools/python/physicsobjects/PhysicsObjects.py
@@ -2,6 +2,8 @@ import math
 
 from CMGTools.RootTools.physicsobjects.TauDecayModes import tauDecayModes
 
+
+
 class PhysicsObject(object):
     '''Extends the cmg::PhysicsObject functionalities.'''
 
@@ -41,6 +43,12 @@ class Lepton( PhysicsObject):
 
     def absIsoFromEA(self,rho,effectiveArea1 = None,effectiveArea2 = None):
 
+        #First remove FSR
+        photonIso = self.photonIso()
+        if hasattr(self,'fsrPhoton'):
+            photon=self.fsrPhoton
+            photonIso=photonIso-photon.pt()
+                
         ea1 = rho
         ea2 = rho
         if effectiveArea1 is not None:
@@ -50,17 +58,47 @@ class Lepton( PhysicsObject):
                     ea1 = ea1 * element['area']
                     break
         else:
-            return self.chargedHadronIso()+max(0.,self.photonIso()+self.neutralHadronIso()-ea1)
+            return self.chargedHadronIso()+max(0.,photonIso+self.neutralHadronIso()-ea1)
 
         if effectiveArea2 is not None:
             for element in effectiveArea2:
                 if abs(self.eta())>= element['etaMin'] and \
                    abs(self.eta())< element['etaMax']:
                     ea2 = ea2 * element['area']
-            return self.chargedHadronIso()+max(0.,self.photonIso()-ea1)+max(0.,self.neutralHadronIso()-ea2)
+            return self.chargedHadronIso()+max(0.,photonIso-ea1)+max(0.,self.neutralHadronIso()-ea2)
         else:
-            return self.chargedHadronIso()+max(0.,self.photonIso()+self.neutralHadronIso()-ea1)
-                
+            return self.chargedHadronIso()+max(0.,photonIso+self.neutralHadronIso()-ea1)
+
+
+    #OVerload Delta beta as well
+    def absIso(self,dBetaFactor = 0,allCharged=0):
+        if dBetaFactor>0 and self.puChargedHadronIso()<0:
+            return -1
+        neutralIso = self.neutralHadronIso()+self.photonIso()
+
+        #Recover FSR
+        if hasattr(self,'fsrPhoton'):
+            photon=self.fsrPhoton
+            neutralIso=neutralIso-photon.pt()
+        
+
+
+        corNeutralIso = neutralIso - dBetaFactor * self.puChargedHadronIso();
+        charged = self.chargedHadronIso();
+        if  allCharged:
+            charged = self.chargedAllIso();
+
+
+        return charged + max(corNeutralIso,0)
+
+  
+
+    def  relIso(self,dBetaFactor=0, allCharged=0):
+         abs = self.absIso(dBetaFactor, allCharged)/self.pt();
+         if abs >0:
+             return abs
+         else:
+             return -1
 
 
     def relEffAreaIso(self,rho):
@@ -68,6 +106,9 @@ class Lepton( PhysicsObject):
 
     def relEffAreaIso(self,rho):
         return self.absEffAreaIso(rho)/self.pt()
+
+
+
 
 
 class Muon( Lepton ):
@@ -114,7 +155,6 @@ class Muon( Lepton ):
 
     def absEffAreaIso(self,rho,effectiveAreas):
         return self.absIsoFromEA(rho,effectiveAreas.muon)
-
 
         
 class Electron( Lepton ):
