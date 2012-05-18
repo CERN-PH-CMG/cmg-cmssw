41f07c7a1f51704e1a38c7281354024e0d31a2e3
musella <>
2012-05-18 12:32:09 +0000

    Fixed JEC

--- External/src/PileupJetIdAlgo.cc
+++ External/src/PileupJetIdAlgo.cc
@@ -303,6 +303,9 @@ PileupJetIdentifier PileupJetIdAlgo::computeIdVariables(const reco::Jet * jet, f
 	if( patjet != 0 && jec == 0. ) { // if this is a pat jet and no jec has been passed take the jec from the object
 		jec = patjet->pt()/patjet->correctedJet(0).pt();
 	}
+	if( jec < 0. ) {
+		jec = 1.;
+	}
 	constituents_type constituents = pfjet ? pfjet->getPFConstituents() : patjet->getPFConstituents();
 	
 	reco::PFCandidatePtr lLead, lSecond, lLeadNeut, lLeadEm, lLeadCh, lTrail;
