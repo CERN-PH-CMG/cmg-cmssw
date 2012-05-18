75cd6a734bce42c6c5ccb20bf163632222b0a07d
govoni <>
2012-05-18 20:23:08 +0000

    tau ID and ele ID for the sync

--- H2TauTau/python/proto/analyzers/TauEleAnalyzer.py
+++ H2TauTau/python/proto/analyzers/TauEleAnalyzer.py
@@ -2,7 +2,8 @@ from CMGTools.RootTools.analyzers.DiLeptonAnalyzer import DiLeptonAnalyzer
 from CMGTools.RootTools.fwlite.AutoHandle import AutoHandle
 from CMGTools.RootTools.physicsobjects.DiObject import TauElectron
 from CMGTools.RootTools.physicsobjects.PhysicsObjects import Electron
-from CMGTools.H2TauTau.proto.analyzers.CountLeptons import electronAccept
+from CMGTools.H2TauTau.proto.analyzers.CountLeptons import electronAccept, testEleLooseLorenzo, testEleLoosePhil
+
 
 class TauEleAnalyzer( DiLeptonAnalyzer ):
 
@@ -20,6 +21,11 @@ class TauEleAnalyzer( DiLeptonAnalyzer ):
             'cmgElectronSel',
             'std::vector<cmg::Electron>'
             )
+        self.handles['vertices'] = AutoHandle(
+            'offlinePrimaryVertices',
+            'vector<reco::Vertex>'
+            )
+
 
     def testLeg1(self, leg):
         return self.testTau(leg) and \
@@ -27,47 +33,71 @@ class TauEleAnalyzer( DiLeptonAnalyzer ):
 
 
     def testLeg2(self, leg):
-        return self.testElectron(leg) and \
-               super( TauEleAnalyzer, self).testLeg2( leg )
-
-##     def testTau(self, tau):
-##         '''Returns True if a tau passes a set of cuts.
-##         Can be used in testLeg1 and testLeg2, in child classes.     
-##         WARNING: the electron filter should be used only in the electron channel.'''
-##         if tau.decayMode() == 0 and \
-##                tau.calcEOverP() < 0.2: #reject electrons faking taus in 2011B
-##             return False
-##         else:
-##             return True
-
-    def testElectronMITMVA(self, leg):
-        eta = abs( leg.eta() )
-        lmva = -99999
-        if   eta<1: lmva = 0.945
-        elif eta<1.5: lmva = 0.942
-        elif eta<2.5: lmva = 0.878
+        if leg.dxy() >= 0.045 : return False
+        if leg.dz()  >= 0.2   : return False
+        if leg.pt () > self.cfg_ana.pt2 and \
+           abs( leg.eta() ) < self.cfg_ana.eta2 and \
+           testEleLoosePhil (leg, self.cfg_ana.pt2) and \
+           leg.relIso (0.5) < self.cfg_ana.iso2: 
+            return True
         else:
-            # out of acceptance, electron is rejected
             return False
-        # in acceptance
-        return leg.mvaMIT() > lmva
+            
+#           self.testVertex (leg) and 
 
-    def testElectronDaniele(self, leg):
+        
+    def testElectronTwiki_2012(self, leg):
+        '''reference numbers form here https://twiki.cern.ch/twiki/bin/view/CMS/HiggsToTauTauWorking2012#2012_Baseline_Selection'''
+        nInnerHits = leg.sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()
+        if nInnerHits != 0 : return False
+        if leg.sourcePtr().passConversionVeto() == False : return False 
+        vertices = self.handles['vertices'].product()
+        #PG FIME take the one from the cmg electron
+        legDZ = leg.sourcePtr().gsfTrack().dz (vertices[0].position())
+        if abs(legDZ) > 0.1 : return False
         eta = abs( leg.eta() )
-        lmva = -99999
-        # import pdb; pdb.set_trace()
-        if   eta<1: lmva = 0.0013
-        elif eta<1.5: lmva = 0.0425
-        elif eta<2.5: lmva = 0.0225
+        if eta > 2.1 : return False
+        lmvaID = -99999 # identification
+        lmvaIS = -99999 # isolation
+        if leg.pt() < 20 :
+            if   eta<0.8:   
+                lmvaID = 0.925
+                lmvaIS = 0.815
+            elif eta<1.479: 
+                lmvaID = 0.915
+                lmvaIS = 0.785
+            else :          
+                lmvaID = 0.965
+                lmvaIS = 0.825
         else:
-            # out of acceptance, electron is rejected
+            if   eta<0.8:   
+                lmvaID = 0.905
+                lmvaIS = 0.805
+            elif eta<1.479: 
+                lmvaID = 0.955
+                lmvaIS = 0.815
+            else :          
+                lmvaID = 0.975
+                lmvaIS = 0.705
+        return testEleLooseLorenzo(leg) and \
+               leg.sourcePtr().electronID('mvaNonTrigV0') > lmvaID and \
+               leg.sourcePtr().userFloat('mvaIsoRings') > lmvaIS
+
+
+    def testTau(self, tau):
+        '''Returns True if a tau passes a set of cuts.
+        Can be used in testLeg1 and testLeg2, in child classes.'''
+        if tau.decayMode() == 0 and \
+               tau.calcEOverP() < 0.2: #reject muons faking taus in 2011B #PG FIXME should I put this in?
             return False
-        # in acceptance
-        return leg.mvaDaniele() > lmva
-        
-    def testElectron(self, leg):
-        # return self.testElectronDaniele(leg)
-        return leg.getSelection('cuts_vbtf80ID')
-        
+        return tau.tauID("byLooseIsoMVA")==True and \
+               tau.tauID("againstElectronMVA")==True and \
+               tau.tauID("againstElectronMedium")==True and \
+               tau.tauID("againstElectronMVA")==True
+        # byLooseCombinedIsolationDeltaBetaCorr
+
+
     def leptonAccept(self, leptons):
         return electronAccept( leptons )
+
+
