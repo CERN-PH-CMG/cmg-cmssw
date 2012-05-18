d942b93c5bdd172a0e95a3ee765bb6b733bf8581
ssekmen <>
2012-05-18 16:33:57 +0000

    took out jetBProbabilityBJetTags, added combinedSecondaryVertexMVABJetTags

--- CMGTools/Common/python/factories/cmgBaseJet_cfi.py
+++ CMGTools/Common/python/factories/cmgBaseJet_cfi.py
@@ -9,10 +9,10 @@ baseJetFactory = cms.PSet(
                               'trackCountingHighEffBJetTags',#0
                               'trackCountingHighPurBJetTags',#1
                               'jetProbabilityBJetTags',#2
-                              'jetBProbabilityBJetTags',#3
-                              'simpleSecondaryVertexHighEffBJetTags',#4
-                              'simpleSecondaryVertexHighPurBJetTags',#5
-                              'combinedSecondaryVertexBJetTags'#6
+                              'simpleSecondaryVertexHighEffBJetTags',#3
+                              'simpleSecondaryVertexHighPurBJetTags',#4
+                              'combinedSecondaryVertexBJetTags',#5
+                              'combinedSecondaryVertexMVABJetTags'#6
                               ),
        fillJec = cms.bool(True),
        fillJecUncertainty = cms.bool(True),
