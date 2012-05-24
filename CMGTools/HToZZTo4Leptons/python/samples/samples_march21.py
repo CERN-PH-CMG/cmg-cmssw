9c23dc0963cfdcf8b3401e1978580b0a81d4330f
bachtis <>
2012-05-24 19:38:13 +0000

    *** empty log message ***

--- CMGTools/HToZZTo4Leptons/python/samples/samples_march21.py
+++
@@ -1,8 +0,0 @@
-from CMGTools.HToZZTo4Leptons.samples.getFiles import getFiles
-from CMGTools.HToZZTo4Leptons.samples.mc_higgs import *
-
-pat = 'PAT_CMG_V3_1_0'
-filePattern = 'tree.*root'
-
-Hig120GluGlu.files = getFiles('/GluGluToHToZZTo4L_M-120_7TeV-powheg-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM/{pat}'.format(pat=pat), 'cbern', filePattern)
-
