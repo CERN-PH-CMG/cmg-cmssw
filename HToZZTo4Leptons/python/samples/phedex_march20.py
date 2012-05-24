1420876ea21b20fbe3a343cfaf05d9cbefa38f5f
bachtis <>
2012-05-24 19:38:13 +0000

    *** empty log message ***

--- HToZZTo4Leptons/python/samples/phedex_march20.py
+++
@@ -1,7 +0,0 @@
-from CMGTools.HToZZTo4Leptons.samples.getFiles import getFiles
-from CMGTools.HToZZTo4Leptons.samples.mc_higgs import *
-
-filePattern = '.*root'
-
-Hig120GluGlu.files = getFiles('/GluGluToHToZZTo4L_M-120_7TeV-powheg-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM', 'CMS', filePattern)
-
