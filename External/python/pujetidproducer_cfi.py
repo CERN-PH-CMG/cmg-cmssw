5deb2e9fc13987eea04812d2316a9d114205c076
pharris <>
2012-05-27 12:04:56 +0000

    added PhilV1 to producer

--- External/python/pujetidproducer_cfi.py
+++ External/python/pujetidproducer_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from CMGTools.External.puJetIDAlgo_cff import * 
 
-stdalgos_4x = cms.VPSet(simple,   full,   cutbased)
-stdalgos_5x = cms.VPSet(simple_5x,full_5x,cutbased)
+stdalgos_4x = cms.VPSet(simple,   full,   cutbased,PhilV1)
+stdalgos_5x = cms.VPSet(simple_5x,full_5x,cutbased,PhilV1)
 
 chsalgos_4x = cms.VPSet(simple,   full,   cutbased)
 chsalgos_5x = cms.VPSet(simple_5x_chs,full_5x_chs,cutbased)
