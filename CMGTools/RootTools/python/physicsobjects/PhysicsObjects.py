766d8a6965bda828376e3b372da1149e699d4a28
cbern <>
2012-05-28 14:33:08 +0000

    added a loose jet id function

--- CMGTools/RootTools/python/physicsobjects/PhysicsObjects.py
+++ CMGTools/RootTools/python/physicsobjects/PhysicsObjects.py
@@ -29,7 +29,17 @@ class PhysicsObject(object):
 
 
 class Jet( PhysicsObject):
-    pass
+    def looseJetId(self):
+        '''This jet id is slightly different from the one present in the cmg::PFJet on May 28.
+        no cut on the mu fraction.
+        '''
+        return self.getSelection('cuts_looseJetId_h0Fraction') and \
+               self.getSelection('cuts_looseJetId_gammaFraction') and \
+               self.getSelection('cuts_looseJetId_nConstituents') and \
+               self.getSelection('cuts_looseJetId_hFraction') and \
+               self.getSelection('cuts_looseJetId_hChargedMultiplicity') and \
+               self.getSelection('cuts_looseJetId_eFraction')
+               
 
 
 class GenJet( PhysicsObject):
