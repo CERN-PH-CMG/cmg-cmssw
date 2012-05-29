133786aa97b2d5b7213740d56ea91ac53142d2d2
musella <>
2012-05-29 13:46:17 +0000

    Imported 2012 JEC preview

--- CMGTools/External/python/jec_2012_cff.py
+++ CMGTools/External/python/jec_2012_cff.py
@@ -1,8 +1,16 @@
 from CondCore.DBCommon.CondDBSetup_cfi import *
 
 ## Preview JEC 2012
-def use2012JecPreview(process):
+def use2012JecPreview(process,data=False):
     process.load("CondCore.DBCommon.CondDBCommon_cfi")
+    if data:
+        jec_db="Summer12_V1_DATA.db"
+        ret='Summer12_V1_DATA'
+        tag='JetCorrectorParametersCollection_Summer12_V1_DATA_AK5PF'
+    else:
+        jec_db="Summer12_V1_MC.db"
+        ret='Summer12_V1_MC'
+        tag='JetCorrectorParametersCollection_Summer12_V1_MC_AK5PF'
     process.jec = cms.ESSource("PoolDBESSource",
                                DBParameters = cms.PSet(messageLevel = cms.untracked.int32(0)
                                                        ),
@@ -10,10 +18,13 @@ def use2012JecPreview(process):
                                toGet = cms.VPSet(
         cms.PSet(
         record = cms.string('JetCorrectionsRecord'),
-        tag    = cms.string('JetCorrectorParametersCollection_Jec12_V7_AK5PF'),
+        ## tag    = cms.string('JetCorrectorParametersCollection_Jec12_V7_AK5PF'),
+        tag    = cms.string(tag),
         label  = cms.untracked.string('AK5PF')
         ),
         ),
-                               connect = cms.string('sqlite_fip:CMGTools/External/data/Jec12_V7.db')
+                               
+                               connect = cms.string('sqlite_fip:CMGTools/External/data/%s' % jec_db)
                                )
     process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+    return ret
