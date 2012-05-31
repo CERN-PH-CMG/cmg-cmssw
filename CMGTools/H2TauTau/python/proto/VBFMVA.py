d95710cc23ff94e09fb33f5025f4bb94d34b4adc
govoni <>
2012-05-31 09:40:39 +0000

    VBF MVA

---
+++ CMGTools/H2TauTau/python/proto/VBFMVA.py
@@ -0,0 +1,12 @@
+from CMGTools.RootTools.RootTools import loadLibs
+from ROOT import gSystem
+
+loadLibs()
+
+gSystem.Load("libCMGToolsH2TauTau")
+
+from ROOT import VBFMVA as VBFMVA_CC
+
+class VBFMVA:
+    def __init__ (self, weightFile) :
+        self.vbfMvaCalc = VBFMVA_CC(weightFile) 
