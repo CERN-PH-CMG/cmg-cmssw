ea61017d7f040d2ebb531badb6d9f5f560652793
cbern <>
2012-06-01 13:46:40 +0000

    added option to use cache or not in getFiles function

--- H2TauTau/python/proto/samples/getFiles.py
+++ H2TauTau/python/proto/samples/getFiles.py
@@ -1,6 +1,6 @@
-def getFiles(dataset, user, pattern):
+def getFiles(dataset, user, pattern, useCache=True):
     from CMGTools.Production.datasetToSource import datasetToSource
     # print 'getting files for', dataset,user,pattern
-    ds = datasetToSource( user, dataset, pattern, True )
+    ds = datasetToSource( user, dataset, pattern, useCache )
     files = ds.fileNames
     return ['root://eoscms//eos/cms%s' % f for f in files]
