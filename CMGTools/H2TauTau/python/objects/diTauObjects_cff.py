08f44a0defa0368378b2ff4cad4d346528e3c7ed
hinzmann <>
2012-06-01 12:11:58 +0000

    latest plot code from Andreas

--- CMGTools/H2TauTau/python/objects/diTauObjects_cff.py
+++ CMGTools/H2TauTau/python/objects/diTauObjects_cff.py
@@ -19,7 +19,7 @@ cmgDiTau.cfg.leg1Collection = 'cmgTauScaler'
 cmgDiTau.cfg.leg2Collection = 'cmgTauScaler'
 
 # preselection 
-cmgDiTauPreSel = cmgDiTauSel.clone( cut = 'getSelection("cuts_baseline")')
+cmgDiTauPreSel = cmgDiTauSel.clone( cut = 'getSelection("cuts_baseline") && leg1().pt>35 && leg2().pt>35')
 
 # full selection
 cmgDiTauFullSel = cmgDiTauSel.clone( src = 'cmgDiTauPreSel',
@@ -35,27 +35,81 @@ diTauStdSequence = cms.Sequence( cmgTauScaler +
 
 # this is done for preselected di-taus
 
-# recoil correction
+# mva MET
 
-# disable recol correction sind never studied
-#recoilCorMETDiTau =  recoilCorrectedMETDiTau.clone( recBosonSrc = 'cmgDiTauPreSel')
+from CMGTools.Common.eventCleaning.goodPVFilter_cfi import goodPVFilter
+from CMGTools.Common.miscProducers.mvaMET.mvaMET_cff import *
+from CMGTools.Common.factories.cmgBaseMETFromPFMET_cfi import cmgBaseMETFromPFMET
+
+mvaMETDiTau = cms.EDProducer(
+    "MVAMETProducerDiTau",
+    pfmetSrc = cms.InputTag('pfMetForRegression'),
+    tkmetSrc = cms.InputTag('tkMet'),
+    nopumetSrc = cms.InputTag('nopuMet'),
+    pucmetSrc = cms.InputTag('pcMet'),
+    pumetSrc = cms.InputTag('puMet'),
+    recBosonSrc = cms.InputTag('cmgDiTauPreSel'),
+    jetSrc = cms.InputTag('cmgPFJetSel2'),
+    leadJetSrc = cms.InputTag('cmgPFBaseJetLead'),
+    vertexSrc = cms.InputTag('goodPVFilter'),
+    nJetsPtGt1Src = cms.InputTag('nJetsPtGt1'),
+    rhoSrc = cms.InputTag('kt6PFJets','rho'),
+    enable = cms.bool(True),
+    verbose = cms.untracked.bool( False )
+    #COLIN: make delta R a parameter
+    )
+
+
+mvaMETDiTau.recBosonSrc = 'cmgDiTauPreSel'
+
+mvaBaseMETDiTau = cmgBaseMETFromPFMET.clone()
+mvaBaseMETDiTau.cfg.inputCollection = 'mvaMETDiTau'
+
+cmgDiTauMVAPreSel = cmgDiTauCor.clone()
+cmgDiTauMVAPreSel.cfg.metCollection = 'mvaBaseMETDiTau'
+cmgDiTauMVAPreSel.cfg.diObjectCollection = 'cmgDiTauPreSel'
+
+mvaMETSequence = cms.Sequence(
+    goodPVFilter + 
+    mvaMETDiTau +
+    mvaBaseMETDiTau+
+    cmgDiTauMVAPreSel
+    )
+
+# recoil correction
+#doMVAMet = False
+#metForRecoil = 'cmgPFMETRaw'
+#diTausForRecoil = 'cmgDiTauPreSel'
+#if doMVAMet:
+#    # in this case the MET will be taken from the di-tau
+#    metForRecoil = 'mvaBaseMETDiTau'
+#    diTausForRecoil = 'cmgDiTauMVAPreSel'
+
+#recoilCorMETDiTau =  recoilCorrectedMETDiTau.clone(
+#    recBosonSrc = diTausForRecoil,
+#    metSrc = metForRecoil
+#    )
 
 #cmgDiTauCorPreSel = cmgDiTauCor.clone()
 #cmgDiTauCorPreSel.cfg.metCollection = 'recoilCorMETDiTau'
-#cmgDiTauCorPreSel.cfg.diObjectCollection = 'cmgDiTauPreSel'
+#cmgDiTauCorPreSel.cfg.diObjectCollection = diTausForRecoil
 
 # SVFit
 
 cmgDiTauCorSVFitPreSel = diTauSVFit.clone()
-cmgDiTauCorSVFitPreSel.diTauSrc = 'cmgDiTauPreSel' #'cmgDiTauCorPreSel'
+cmgDiTauCorSVFitPreSel.diTauSrc = 'cmgDiTauMVAPreSel'#'cmgDiTauPreSel' #cmgDiTauCorPreSel
 
 cmgDiTauCorSVFitFullSel = cmgDiTauFullSel.clone( src = 'cmgDiTauCorSVFitPreSel' ) 
 
 
-diTauCorSVFitSequence = cms.Sequence( #recoilCorMETDiTau +
+diTauCorSVFitSequence = cms.Sequence( mvaMETSequence +
+                                      #recoilCorMETDiTau +
                                       #cmgDiTauCorPreSel +
                                       cmgDiTauCorSVFitPreSel +
                                       cmgDiTauCorSVFitFullSel )
+
+#if doMVAMet:
+#    diTauCorSVFitSequence.insert(0, mvaMETSequence)
 
 diTauSequence = cms.Sequence( diTauStdSequence +
                               diTauCorSVFitSequence
--- CMGTools/H2TauTau/python/objects/diTauSVFit_cfi.py
+++ CMGTools/H2TauTau/python/objects/diTauSVFit_cfi.py
@@ -4,6 +4,8 @@ diTauSVFit = cms.EDProducer(
     "TauTauWithSVFitProducer",
     diTauSrc = cms.InputTag("cmgDiTauCorPreSel"),
     # metSrc = cms.InputTag("cmgPFMET"),
-    metsigSrc = cms.InputTag("pfMetSignificance"),
+    #metsigSrc = cms.InputTag("pfMetSignificance"),
+    metsigSrc = cms.InputTag("mvaMETDiTau"),
+    SVFitVersion =  cms.int32(1), # 1 for 2011 version , 2 for new 2012 (slow) version
     verbose = cms.untracked.bool( False )
     )
