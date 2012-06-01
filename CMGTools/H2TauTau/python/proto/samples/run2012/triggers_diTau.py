efd598c5176a480a083fd33d5cdf2e850c12cc32
hinzmann <>
2012-06-01 17:08:34 +0000

    samples for 2012 tau tau

---
+++ CMGTools/H2TauTau/python/proto/samples/run2012/triggers_diTau.py
@@ -0,0 +1,22 @@
+data_triggers_2012A = [
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v1',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v2',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v3',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v4',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v1',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
+    ]
+
+data_triggers_2012B = [
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v1',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v2',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v3',
+    'HLT_DoubleMediumIsoPFTau25_Trk5_eta2p1_Jet30_v4',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v1',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
+    ]
+
+mc_triggers_spring12 = [
+    #'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v1',
+    #'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
+    ]
