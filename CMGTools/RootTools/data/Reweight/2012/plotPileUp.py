14c85cb3428cefe3509ca1aa0cb9b4b700d5ee77
cbern <>
2012-06-04 15:15:51 +0000

    finalized PU reweighting stuff

--- CMGTools/RootTools/data/Reweight/2012/plotPileUp.py
+++ CMGTools/RootTools/data/Reweight/2012/plotPileUp.py
@@ -1,11 +1,36 @@
+import sys
+import copy
 from CMGTools.RootTools.RootTools import * 
-from ROOT import TFile
+from ROOT import TFile, TH1F
 
+fileNames = sys.argv[1:]
+hists = []
 files = []
 
-def plot(fileName):
+
+def getHisto(fileName):
     files.append( TFile(fileName) ) 
     pu = files[-1].Get('pileup')
     pu.Scale( 1/pu.Integral() )
-    pu.Draw()
+    hists.append( pu )
+
+
+def weight( hs, weights ):
+    start = True
+    outh = None
+    totWeight = 0
+    # import pdb; pdb.set_trace()
+    for h, weight in zip(hs, weights):
+        if start:
+            outh = copy.deepcopy(h)
+            outh.Reset()
+            start = False
+        outh.Add(h, weight)
+        totWeight += weight
+    outh.Scale(1/totWeight)
+    return outh
+        
+
+for fname in fileNames:
+    getHisto(fname)
 
