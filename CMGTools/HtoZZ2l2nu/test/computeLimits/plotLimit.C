8f815499cb69fbbd3f7d982f2d3238b1148f5861
querten <>
2012-06-11 05:23:03 +0000

    optimization of categories

--- CMGTools/HtoZZ2l2nu/test/computeLimits/plotLimit.C
+++ CMGTools/HtoZZ2l2nu/test/computeLimits/plotLimit.C
@@ -22,66 +22,22 @@
 #include "TGraphAsymmErrors.h"
 #include "TMultiGraph.h"
 #include "TPaveText.h"
-#include "../../../../UserCode/mschen/LandS/test/fitRvsCLs.C"
 
-struct Limit {double Obs; double Exp_minus2s; double Exp_minus1s; double Exp_Mean; double Exp_plus1s; double Exp_plus2s; double Exp_Median;};
-Limit GetLimit(TString inFileName, TString plotName, TString sfile="bands", double mH = -1){
-        Limit toReturn;
-        toReturn.Obs = 1000.0;
-        toReturn.Exp_minus2s = 1000.0;
-        toReturn.Exp_minus1s = 1000.0;
-        toReturn.Exp_Mean    = 1000.0;
-        toReturn.Exp_Median  = 1000.0;
-        toReturn.Exp_plus1s = 1000.0;
-        toReturn.Exp_plus2s = 1000.0;
+//tree variables
+double Tmh, Tlimit, TlimitErr; float TquantExp;
 
-        FILE* pFile = fopen(inFileName.Data(),"r");
-        if(!pFile){printf("######  FILE %s DOES NOT EXIST\n",inFileName.Data());return toReturn;}
-        fclose(pFile);
-
-	double m2s =	extractLimitAtQuantile(inFileName, plotName+"_-2sigma", 0.0275 );
-//	double m2s_err = limitErr;
-	double m1s =	extractLimitAtQuantile(inFileName, plotName+"_-1sigma", 0.16 );
-//	double m1s_err = limitErr;
-	double med =	extractLimitAtQuantile(inFileName, plotName+"_median", 0.5 );
-	double med_err = limitErr;
-	double p1s =	extractLimitAtQuantile(inFileName, plotName+"_1sigma", 0.84 );
-//	double p1s_err = limitErr;
-	double p2s =	extractLimitAtQuantile(inFileName, plotName+"_2sigma", 0.975 );
-//	double p2s_err = limitErr;
-	double dat =	extractLimitAtQuantile(inFileName, plotName+"_observed", -1 );
-//	double dat_err = limitErr;
-/*
-	cout<<"EXPECTED LIMIT BANDS from(obs, -2s,-1s,median,1s,2s) mass= "<<mH<<": ";
-	cout<<dat<<"+/-"<<dat_err<<", ";
-	cout<<m2s<<"+/-"<<m2s_err<<", "<<m1s<<"+/-"<<m1s_err<<", ";
-	cout<<med<<"+/-"<<med_err<<", ";
-	cout<<p1s<<"+/-"<<p1s_err<<", "<<p2s<<"+/-"<<p2s_err<<endl;
-
-	cout<<"Observed data limit: "<<dat<<endl;
-	cout<<"Observed data limit: "<<dat<<" +/- "<<dat_err<<endl;
-	cout<<"expected median limit: "<<med<<" +/- "<<med_err<<endl;
-	SaveResults(sfile, mH, dat, dat_err, 0, 0, m2s, m1s, med, 0, p1s, p2s);
-*/
-
-printf("XXX ERRRO = %f\n",med_err);
-
-
-        if(p1s<med)p1s=med;
-        if(p2s<p1s)p2s=p1s;
-
-
-
-        toReturn.Obs = dat;
-        toReturn.Exp_minus2s = m2s;
-        toReturn.Exp_minus1s = m1s;
-        toReturn.Exp_Mean    = med;
-        toReturn.Exp_Median  = med;
-        toReturn.Exp_plus1s = p1s;
-        toReturn.Exp_plus2s = p2s;
-        return toReturn;
+void fillLimitArray(TTree* tree, float Quantil, double* limitArray, double* massArray=NULL){
+  int i=0;
+  for(int ientry=0;ientry<tree->GetEntriesFast();ientry++){
+     tree->GetEntry(ientry);
+     if(TquantExp==Quantil){
+        //printf("Quantil = %f - mH=%f --> %f\n",TquantExp,Tmh,Tlimit);
+        limitArray[i]=Tlimit;
+        if(massArray){massArray[i]=Tmh;} 
+        i++;
+     }
+  }
 }
-
 
 
 TCutG* GetErrorBand(string name, int N, double* Mass, double* Low, double* High)
@@ -89,21 +45,10 @@ TCutG* GetErrorBand(string name, int N, double* Mass, double* Low, double* High)
    TCutG* cutg = new TCutG(name.c_str(),2*N+2);
    cutg->SetFillColor(kGreen-7);
    int I = 0;
-   for(int i=0;i<N;i++){
-      cutg->SetPoint(I,Mass[i], Low[i]);
-      I++;
-   }
-
-   cutg->SetPoint(I,Mass[N-1], Low[N-1]);
-   I++;
-
-   cutg->SetPoint(I,Mass[N-1], High[N-1]);
-   I++;
-
-   for(int i=0;i<N;i++){
-        cutg->SetPoint(I,Mass[N-1-i], High[N-1-i]);
-        I++;
-   }
+   for(int i=0;i<N;i++){cutg->SetPoint(I,Mass[i]    , Low[i]     );I++; }
+                        cutg->SetPoint(I,Mass[N-1]  , Low[N-1]   );I++;
+                        cutg->SetPoint(I,Mass[N-1]  , High[N-1]  );I++;
+   for(int i=0;i<N;i++){cutg->SetPoint(I,Mass[N-1-i], High[N-1-i]);I++;}
    return cutg;
 }
 
@@ -165,63 +110,44 @@ void plotLimit(TString outputName="Limit", TString inputs="", double energy=7, d
    gStyle->SetNdivisions(505);
 
 
-   std::vector<string> files;
-   FILE* pFile = fopen(inputs.Data(),"r");
-   if(!pFile){printf("######  FILE %s DOES NOT EXIST\n",inputs.Data());exit(0);}
-   while(1){
-      char buff[2048];
-      if(fscanf(pFile,"%s",buff)==EOF)break;
-      files.push_back(buff);
-      printf("%s\n", buff);
-   }
-   fclose(pFile);
+   TFile* file = TFile::Open(inputs);
+   printf("Looping on %s\n",inputs.Data());
+   if(!file) return;
+   if(file->IsZombie()) return;
+   TTree* tree = (TTree*)file->Get("limit");
 
-   int N = files.size();
+   tree->GetBranch("mh"              )->SetAddress(&Tmh      );
+   tree->GetBranch("limit"           )->SetAddress(&Tlimit   );
+   tree->GetBranch("limitErr"        )->SetAddress(&TlimitErr);
+   tree->GetBranch("quantileExpected")->SetAddress(&TquantExp);
 
-   double* ObsLimit   = new double[N];
-   double* ExpLimitm2 = new double[N];
-   double* ExpLimitm1 = new double[N];
-   double* ExpLimit   = new double[N];
-   double* ExpLimitp1 = new double[N];
-   double* ExpLimitp2 = new double[N];
+   int N = tree->GetEntriesFast() / 6 ;// 6Limits per mass point (observed, meand , +-1sigma, +-2sigma)
    double* MassAxis   = new double[N];
-   double* MassAxisExp= new double[N];
+   double* ObsLimit   = new double[N];  fillLimitArray(tree,-1   ,ObsLimit,MassAxis);
+   double* ExpLimitm2 = new double[N];  fillLimitArray(tree,0.025,ExpLimitm2);
+   double* ExpLimitm1 = new double[N];  fillLimitArray(tree,0.160,ExpLimitm1);
+   double* ExpLimit   = new double[N];  fillLimitArray(tree,0.500,ExpLimit  );
+   double* ExpLimitp1 = new double[N];  fillLimitArray(tree,0.840,ExpLimitp1);
+   double* ExpLimitp2 = new double[N];  fillLimitArray(tree,0.975,ExpLimitp2);
+   file->Close();
+
+
 
    FILE* pFileSum = fopen((outputName+"_LimitSummary").Data(),"w");
-
-   int NExp = 0;
    for(int i=0;i<N;i++){
-      string massStr = files[i].substr(files[i].rfind('/')-3,3);
-      int mass; sscanf(massStr.c_str(),"%d",&mass);
-      Limit limits = GetLimit(files[i],string("Higgs")+massStr.c_str());
-
-         //fprintf(pFileSum, "Mass=%s --> Limits are %lf %lf %lf %lf %lf %lf %lf\n",massStr.c_str(), limits.Obs,limits.Exp_minus2s, limits.Exp_minus1s, limits.Exp_Mean, limits.Exp_plus1s, limits.Exp_plus2s, limits.Exp_Median);
-         fprintf(pFileSum, "$%s$ & $%6.2f$ & $[%6.2f,%6.2f]$ & $[%6.2f,%6.2f]$ & $%6.2f$ \\\\\\hline\n",massStr.c_str(), limits.Exp_Median, limits.Exp_minus1s, limits.Exp_plus1s, limits.Exp_minus2s,  limits.Exp_plus2s, limits.Obs );
-         MassAxis[i]   = mass;
-         ObsLimit[i]   = limits.Obs;
-
-//         if(int(MassAxis[i])%10==0){
-            MassAxisExp[NExp]= mass;
-            ExpLimitm2[NExp] = limits.Exp_minus2s;
-            ExpLimitm1[NExp] = limits.Exp_minus1s;
-            ExpLimit[NExp]   = limits.Exp_Mean;
-            ExpLimitp1[NExp] = limits.Exp_plus1s;
-            ExpLimitp2[NExp] = limits.Exp_plus2s;
-           // fprintf(pFileSum,"%f %f %f+-%f\n",MassAxis[i],ObsLimit[i],ExpLimit[NExp],(ExpLimitp1[NExp]-ExpLimitm1[NExp])/2.0);
-            NExp++;
-//         }
+      fprintf(pFileSum, "$%7.3f$ & $%6.2f$ & $[%6.2f,%6.2f]$ & $[%6.2f,%6.2f]$ & $%6.2f$ \\\\\\hline\n",MassAxis[i], ExpLimit[i], ExpLimitm1[i], ExpLimitp1[i], ExpLimitm2[i],  ExpLimitp2[i], ObsLimit[i]);
    }
    fclose(pFileSum);
 
 
    TGraph* TGObsLimit   = new TGraph(N,MassAxis,ObsLimit);  TGObsLimit->SetLineWidth(2);
-   TGraph* TGExpLimit   = new TGraph(NExp,MassAxisExp,ExpLimit);  TGExpLimit->SetLineWidth(2); TGExpLimit->SetLineStyle(2);
-   TCutG* TGExpLimit1S = GetErrorBand("1S", NExp, MassAxisExp, ExpLimitm1, ExpLimitp1);  
-   TCutG* TGExpLimit2S = GetErrorBand("2S", NExp, MassAxisExp, ExpLimitm2, ExpLimitp2);  TGExpLimit2S->SetFillColor(5);
+   TGraph* TGExpLimit   = new TGraph(N,MassAxis,ExpLimit);  TGExpLimit->SetLineWidth(2); TGExpLimit->SetLineStyle(2);
+   TCutG* TGExpLimit1S = GetErrorBand("1S", N, MassAxis, ExpLimitm1, ExpLimitp1);  
+   TCutG* TGExpLimit2S = GetErrorBand("2S", N, MassAxis, ExpLimitm2, ExpLimitp2);  TGExpLimit2S->SetFillColor(5);
 
 
    printf("Obs Limits for Model are: ");for(int i=0;i<N;i++){if(int(MassAxis[i])%50!=0)continue; printf("%f ",ObsLimit[i]);}printf("\n");
-   printf("Exp Limits for Model are: ");for(int i=0;i<NExp;i++){printf("%f+-%f ",ExpLimit[i], (ExpLimitp1[i]-ExpLimitm1[i]))/2.0;}printf("\n");
+   printf("Exp Limits for Model are: ");for(int i=0;i<N;i++){printf("%f+-%f ",ExpLimit[i], (ExpLimitp1[i]-ExpLimitm1[i]))/2.0;}printf("\n");
 
    TGExpLimit->SetLineColor(2);  TGExpLimit->SetLineStyle(1);
    TGObsLimit->SetLineWidth(2);  TGObsLimit->SetMarkerStyle(20);
