9e866f0e38699a22e5da79927b9b95fba5d73499
malberti <>
2012-06-12 08:23:08 +0000

    small update

--- UserArea/JetIDAnalysis/bin/LanciaAll.sh
+++ UserArea/JetIDAnalysis/bin/LanciaAll.sh
@@ -1,12 +1,26 @@
+
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets ALL   20 100
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets ALL   20 100
+
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets TK    20 100
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  20 100
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 20 100
+MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    20 100
+
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets TK    20 100
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEin  20 100
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEout 20 100
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HF    20 100
+
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets TK    10 20
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  10 20
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 10 20
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    10 20
 
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets TK    10 20
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEin  10 20
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEout 10 20
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HF    10 20
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets TK    10 20
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEin  10 20
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEout 10 20
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HF    10 20
 
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets TK    20 30
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  20 30
@@ -14,11 +28,11 @@ MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HE
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    20 30
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets ALL   20 30
 
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets TK    20 30
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEin  20 30
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEout 20 30
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HF    20 30
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets ALL   20 30
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets TK    20 30
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEin  20 30
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEout 20 30
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HF    20 30
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets ALL   20 30
 
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets TK    30 50
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  30 50
@@ -26,21 +40,19 @@ MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HE
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    30 50
 MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets ALL   30 50
 
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets TK    30 50
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEin  30 50
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HEout 30 50
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets HF    30 50
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets ALL   30 50
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets TK    30 50
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEin  30 50
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HEout 30 50
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets HF    30 50
+MakeJetIDPlots plots_DoubleMu2012_cfg.py pfjetanalyzer histos_DoubleMu2012_pfjets ALL   30 50
 
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets ALL       20 100
-MakeJetIDPlots plots_DoubleMu2011_cfg.py pfjetanalyzer histos_DoubleMu2011_pfjets ALL   20 100
 
 # reweighted nvtx
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  20 30 1
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 20 30 1
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    20 30 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  20 30 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 20 30 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    20 30 1
 
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  30 50 1
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 30 50 1
-MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    30 50 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEin  30 50 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HEout 30 50 1
+#MakeJetIDPlots plots_DYJetsToLL_cfg.py pfjetanalyzer histos_DYJetsToLL_pfjets HF    30 50 1
 
