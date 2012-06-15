81ce8ce21344100ca3b920f915f3f4941081fdc7
govoni <>
2012-06-15 13:16:20 +0000

    2011 tauele triggers bigfix

--- H2TauTau/python/triggerMap.py
+++ H2TauTau/python/triggerMap.py
@@ -7,21 +7,29 @@ pathsAndFilters = {
     "HLT_IsoMu15_LooseIsoPFTau15_v8": ("hltPFTau15TrackLooseIso","hltSingleMuIsoL3IsoFiltered15"),
     "HLT_IsoMu15_LooseIsoPFTau15_v9": ("hltPFTau15TrackLooseIso","hltSingleMuIsoL3IsoFiltered15"),
     # 2011 TauEle Ele15_*_LooseIsoPFTau15
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v1": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v2": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v4": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v6": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v8": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v9": ("hltPFTau15TrackLooseIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v1": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v2": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),    
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v4": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v6": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v8": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau15_v9": ("hltOverlapFilterIsoEle15IsoPFTau15",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),                                                                                                                  
     # 2011 TauEle Ele15_*_TightIsoPFTau20
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TightIsoPFTau20_v1": ("hltPFTauTighIso20TrackTightIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TightIsoPFTau20_v2": ("hltPFTauTighIso20TrackTightIso", "HLTEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TightIsoPFTau20_v1": ("hltOverlapFilterIsoEle15TightIsoPFTau20", "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TightIsoPFTau20_v2": ("hltOverlapFilterIsoEle15TightIsoPFTau20", "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele18_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TightIsoPFTau20_v2": ("hltOverlapFilterIsoEle18TightIsoPFTau20", "hltEle18CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v4": ("hltOverlapFilterIsoEle15IsoPFTau20",      "hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v6": ("hltOverlapFilterIsoEle15IsoPFTau20",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v8": ("hltOverlapFilterIsoEle15IsoPFTau20",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    "HLT_Ele15_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v9": ("hltOverlapFilterIsoEle15IsoPFTau20",      "hltEle15CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
+    #PG old name for the lepton filter: hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter 
+    # quello nuovo l'ho trovato nei menu 10^33
     # 2011 TauEle Ele18_*_MediumIsoPFTau20 
-    "HLT_Ele18_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v1": ("hltPFTauMediumIso20TrackMediumIso", "HLTEle18CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele18_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v1": ("hltOverlapFilterIsoEle18MediumIsoPFTau20", "hltEle18CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilter"),
     # 2011 TauEle Ele20_*_MediumIsoPFTau20 
-    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v1": ("hltPFTauMediumIso20TrackMediumIso", "HLTEle20CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v5": ("hltPFTauMediumIso20TrackMediumIso", "HLTEle20CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
-    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v6": ("hltPFTauMediumIso20TrackMediumIso", "HLTEle20CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter"),
+    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v1": ("hltOverlapFilterIsoEle20MediumIsoPFTau20", "hltEle20CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilterL1SingleEG18orL1SingleEG20"),
+    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v5": ("hltOverlapFilterIsoEle20MediumIsoPFTau20", "hltEle20CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilterL1SingleEG18orL1SingleEG20"),
+    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v6": ("hltOverlapFilterIsoEle20MediumIsoPFTau20", "hltEle20CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilterL1SingleEG18orL1SingleEG20"),
+    "HLT_Ele20_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_MediumIsoPFTau20_v7": ("hltOverlapFilterIsoEle20MediumIsoPFTau20", "hltEle20CaloIdVTCaloIsoTTrkIdTTrkIsoTTrackIsoFilterL1SingleEG18orL1SingleEG20"),
     # the following is used for synchronization
     "HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v1": ("hltPFTau20TrackLooseIso","hltSingleMuIsoL3IsoFiltered15"), 
     "HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v1": ("hltPFTau20TrackLooseIso","hltSingleMuIsoL1s14L3IsoFiltered15eta2p1"),
@@ -33,7 +41,6 @@ pathsAndFilters = {
     # note: pdgId for taus incorrectly set to 0...
     'HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v*': ( ('hltOverlapFilterIsoMu18LooseIsoPFTau20', [15,0]), ('hltOverlapFilterIsoMu18LooseIsoPFTau20',[13])),
     'HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v*': ( ('hltOverlapFilterIsoMu17LooseIsoPFTau20', [15,0]), ('hltOverlapFilterIsoMu17LooseIsoPFTau20',[13])),
-
     # hadronic tau triggers 2011
     'HLT_DoubleIsoPFTau20_Trk5_v1': ("hltFilterDoubleIsoPFTau25Trk5LeadTrack5IsolationL1HLTMatched","hltFilterDoubleIsoPFTau25Trk5LeadTrack5IsolationL1HLTMatched"),
     'HLT_DoubleIsoPFTau20_Trk5_v2': ("hltFilterDoubleIsoPFTau25Trk5LeadTrack5IsolationL1HLTMatched","hltFilterDoubleIsoPFTau25Trk5LeadTrack5IsolationL1HLTMatched"),
