9cdcd885abcdc7646e6b95969d737d5eade55bb9
govoni <>
2012-06-25 08:56:16 +0000

    default is now the new svfit

--- CMGTools/H2TauTau/python/objects/tauEleSVFit_cfi.py
+++ CMGTools/H2TauTau/python/objects/tauEleSVFit_cfi.py
@@ -6,6 +6,6 @@ tauEleSVFit = cms.EDProducer(
     # metSrc = cms.InputTag("cmgPFMET"),
     metsigSrc = cms.InputTag("pfMetSignificance"),
     # metsigSrc = cms.InputTag("mvaMETTauEle"),
-    SVFitVersion =  cms.int32(1), # 1 for 2011 version , 2 for new 2012 (slow) version
+    SVFitVersion =  cms.int32(2), # 1 for 2011 version , 2 for new 2012 (slow) version
     verbose = cms.untracked.bool( False )    
     )
