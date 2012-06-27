056ec0fc7825bd9f3479842fd013902ab80c6760
cbern <>
2012-06-27 06:21:08 +0000

    organized for systematic studies

--- CMGTools/H2TauTau/Colin/test_tauMu_2012_cfg.py
+++ CMGTools/H2TauTau/Colin/test_tauMu_2012_cfg.py
@@ -2,15 +2,16 @@ import copy
 import os 
 import CMGTools.RootTools.fwlite.Config as cfg
 from CMGTools.H2TauTau.triggerMap import pathsAndFilters
+from CMGTools.H2TauTau.proto.samples.sampleShift import selectShift
 from CMGTools.RootTools.RootTools import * 
 
-period = 'Period_2012AB'
-shift = True
+# 'Nom', 'Up', 'Down', or None
+shift = None
 
 puFileDir = os.environ['CMSSW_BASE'] + '/src/CMGTools/RootTools/data/Reweight/2012'
 puFileData = None
 puFileMC = '/'.join([puFileDir, 'MyMCPileupHistogram_true.root'])
-puFileData = '/'.join([puFileDir, 'MyDataPileupHistogram_true_AB_190456_196531.root'])
+puFileData = '/'.join([puFileDir, 'MyDataPileupHistogram_true_AB_start_196509.root'])
 
 vertexFileDir = os.environ['CMSSW_BASE'] + '/src/CMGTools/RootTools/data/Reweight/2012/Vertices'
 vertexFileData = '/'.join([vertexFileDir, 'vertices_data_2012A_2012B_start_195947.root'])
@@ -24,16 +25,6 @@ mc_muEffWeight_mc = 'effMu2012MC'
 mc_tauEffWeight = 'effTau2012AB'
 mc_muEffWeight = 'effMu2012AB'
     
-
-## if period == 'Period_2012A':
-##     puFileData = '/'.join([puFileDir, 'MyDataPileupHistogram_true_A.root'])
-##     mc_tauEffWeight = 'effTau2012A'
-##     mc_muEffWeight = 'effMu2012A'
-## elif period == 'Period_2012B':
-##     puFileData = '/'.join([puFileDir, 'MyDataPileupHistogram_true_B.root'])
-##     mc_tauEffWeight = 'effTau2012B'
-##     mc_muEffWeight = 'effMu2012B'
-## elif period == 'Period_2012AB':
 
 
 jsonAna = cfg.Analyzer(
@@ -78,6 +69,11 @@ TauMuAna = cfg.Analyzer(
     triggerMap = pathsAndFilters
     )
 
+dyJetsFakeAna = cfg.Analyzer(
+    'DYJetsFakeAnalyzer',
+    leptonType = 13
+    )
+
 tauWeighter = cfg.Analyzer(
     'LeptonWeighter_tau',
     effWeight = mc_tauEffWeight,
@@ -94,7 +90,7 @@ muonWeighter = cfg.Analyzer(
     effWeightMC = mc_muEffWeight_mc,
     lepton = 'leg2',
     verbose = False,
-    disable = False,
+    disable = True,
     recEffVersion = None
     )
 
@@ -121,10 +117,8 @@ treeProducer = cfg.Analyzer(
 
 #########################################################################################
 
+# from CMGTools.H2TauTau.proto.samples.run2012.tauMu_ColinJun25 import * 
 from CMGTools.H2TauTau.proto.samples.run2012.tauMu_ColinMay30 import * 
-# from CMGTools.H2TauTau.proto.samples.tauMu_ColinMay18_CHS import * 
-# from CMGTools.H2TauTau.proto.samples.tauMu_ColinMay18 import * 
-# from CMGTools.H2TauTau.proto.samples.tauMu_ColinMay15 import * 
 
 #########################################################################################
 
@@ -132,12 +126,10 @@ MC_list = copy.copy(MC)
 data_list = copy.copy(data_list_2012)
 embed_list = copy.copy(embed_list_2012)
 
-if shift: 
-    MC_list = copy.copy(MC_up)
-    MC_list.extend(MC_down)
-    embed_list = copy.copy(embed_list_2012_up)
-    embed_list.extend(embed_list_2012_down)
-    data_list = []
+MC_list.extend(MC_up)
+MC_list.extend(MC_down)
+embed_list.extend(embed_list_2012_up)
+embed_list.extend(embed_list_2012_down)
 
 mc_jet_scale = 1.
 mc_jet_smear = 0.
@@ -151,9 +143,13 @@ for mc in MC_list:
         mc.splitFactor = 100
         mc.fakes = True
     elif mc.name.find('WJets')!=-1:
-        mc.splitFactor = 20
+        mc.splitFactor = 50
     elif mc.name.find('TTJets')!=-1:
         mc.splitFactor = 80
+    elif mc.name.find('WW')!=-1 or \
+         mc.name.find('WZ')!=-1 or \
+         mc.name.find('ZZ')!=-1:
+        mc.splitFactor = 50
     elif mc.name.find('HiggsVBF120')!=-1 or \
          mc.name.find('HiggsGGH120')!=-1:
         mc.splitFactor = 50
@@ -166,20 +162,25 @@ for emb in embed_list:
     emb.splitFactor = 10
 
 data_Run2012A.splitFactor = 40
-data_Run2012B_start_194479.splitFactor = 40
+data_Run2012B_start_194479.splitFactor = 50
 data_Run2012B_194480_195016.splitFactor = 40
 data_Run2012B_195017_195947.splitFactor = 40 
+data_Run2012B_195948_196509.splitFactor = 50
 
 selectedComponents =  copy.copy(MC_list)
 selectedComponents.extend( data_list )
 selectedComponents.extend( embed_list )
-    
+
+selectedComponents = [data_Run2012B_start_194479]
+## selectedComponents.extend(data_list)
+## selectedComponents.extend(embed_list)
 
 sequence = cfg.Sequence( [
     jsonAna,
     triggerAna,
     vertexAna,
     TauMuAna,
+    dyJetsFakeAna,
     vbfAna,
     pileUpAna,
     embedWeighter, 
@@ -189,10 +190,10 @@ sequence = cfg.Sequence( [
    ] )
 
 
-test = 0
+test = 1
 if test==1:
-    # comp = embed_Run2012B_195147_196070
-    comp = data_Run2012A
+    # comp = DYJets
+    comp = data_Run2012B_195948_196509
     comp.files = comp.files[:10]
     selectedComponents = [comp]
     comp.splitFactor = 1
@@ -202,16 +203,11 @@ elif test==2:
         comp.files = comp.files[:5]
 
 
+if shift:
+    selectedComponents = selectShift(selectedComponents, shift)
+
+# selectedComponents = mc_diboson
+
 config = cfg.Config( components = selectedComponents,
                      sequence = sequence )
 
-def printComps():
-    nJobs = 0
-    for c in config.components:
-        print c.name, c.splitFactor, len(c.files)
-        if len(c.files):
-            print c.files[0]
-            nJobs += c.splitFactor
-    print '-'*70
-    print '# components = ', len(config.components)
-    print '# jobs       = ', nJobs
