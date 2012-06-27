7e90030a523cdc0910a89f160fff3329bf493f9f
malgeri <>
2012-06-27 12:41:13 +0000

    new version (more customizable)

--- LEP3/htt_plot.py
+++ LEP3/htt_plot.py
@@ -13,11 +13,19 @@ from array import array
 
 
 # format: file,xsec(fb),tag for legenda
+#latest ok
+#mclist=[
+#    ["htt/Hig125_134/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",200.,"HZ"],
+#    ["htt/ZZ_2/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",1446.,"ZZ"],
+#    ["htt/WW_8/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",14080.,"WW"],
+#    ["htt/QQ_1/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",50000.,"QQ"]
+#    ]
+
 mclist=[
-    ["htt/Hig125_105/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",200.,"HZ"],
-    ["htt/ZZ/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",1446.,"ZZ"],
-    ["htt/WW/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",14080.,"WW"],
-    ["htt/QQ/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",16750.,"QQ"]
+    ["htt/Hig125_138/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",200.,"HZ"],
+    ["htt/ZZ_5/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",1446.,"ZZ"]
+#    ["htt/WW_8/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",14080.,"WW"],
+#    ["htt/QQ_1/htttreeproducer_httanalyzer/htttreeproducer_httanalyzer_tree.root",50000.,"QQ"]
     ]
 
 
@@ -25,41 +33,67 @@ mclist=[
 lumi=500
 
 # step at which the plot should be made
-stepplot=5
+stepplot=7
 
-# define histograms
+# define special histograms
 step_h=[]
 genrec_s3_t1=[]
 genrec_s3_t2=[]
-massh_h=[]
-massz_h=[]
 mzh_h2=[]
 
+# Define all others
+# syntax: name, variable, nibn,xmin,xmax,visualize
+
+step_label=["all","njet>4","ejet>10","2 taucand","2 good taucand","jet sele","mzmh cut","btag"]
+
+h1_list=[
+    ["Hmass_" ,"event.recHMass" ,200,0,400,True],
+    ["Zmass_" ,"event.recZMass" ,200,0,400,True],
+    ["mvis_"  ,"event.mvis"     ,200,0,400,True],
+    ["evis_"  ,"event.evis"     ,200,0,400,True],
+    ["ntrt1_" ,"event.t1recNtrk", 10,0, 10,True],
+    ["ntrt2_" ,"event.t2recNtrk", 10,0, 10,True],
+    ["ntrt_"  ,"event.t2recNtrk+event.t1recNtrk", 10,0, 10,True],
+    ["pz_"    ,"event.pz"  ,  100,-100, 100,True],
+    ["chi2_"  ,"event.chi2", 1000, 0,10000, True],
+    ["wwmin_" ,"event.wwMin",  50, 0,  100, True],
+    ["zzmin_" ,"event.zzMin",  50, 0,  100, True],
+    ["btag_tt","event.btag_tt",50, -10, 10, True],
+    ["btag_jj","event.btag_jj",50, -10, 10, True]
+    ]
+    
+    
+
+
+h1glob=[]
 for index in range(0,len(mclist)):
     mc=mclist[index]
     tag=mc[2]
+
     step_h.append(TH1F("step_"+tag,"step_"+tag,10,0,10))
     step_h[index].SetLineColor(index+2)
+    step_h[index].SetLineWidth(2)
     step_h[index].SetMarkerColor(index+2)
-#    step_h[index].SetFillColor(index+2)
-    massh_h.append(TH1F("Hmass_"+tag,"Hmass_"+tag,200,0,400))
-    massh_h[index].SetLineColor(index+2)
-    massh_h[index].SetMarkerColor(index+2)
-#    massh_h[index].SetFillColor(index+2)
-    massz_h.append(TH1F("Zmass_"+tag,"Zmass_"+tag,200,0,400))
-    massz_h[index].SetLineColor(index+2)
-    massz_h[index].SetMarkerColor(index+2)
-#    massz_h[index].SetFillColor(index+2)
-
+    for bin in range(1,len(step_label)+1):
+        step_h[index].GetXaxis().SetBinLabel(bin,step_label[bin-1])    
+#        step_h[index].LabelsOption("v","X")
+        
     mzh_h2.append(TH2F("mzvsmh_"+tag,"mzvsmh_"+tag,200,0,400,200,0,400))
     mzh_h2[index].SetLineColor(index+2)
     mzh_h2[index].SetMarkerColor(index+2)
 #    mzh_h2[index].SetFillColor(index+2)
 
-
     genrec_s3_t1.append(TH1F("genrec_s3_t1_"+tag,"genrec_s3_t1_"+tag,100,-1,1))
     genrec_s3_t2.append(TH1F("genrec_s3_t2_"+tag,"genrec_s3_t2_"+tag,100,-1,1))
-                 
+
+    h1loc=[]
+    for h1 in range(0,len(h1_list)):
+        param=h1_list[h1]
+        h1loc.append(TH1F(param[0]+tag,param[0]+tag,param[2],param[3],param[4]))  
+        h1loc[len(h1loc)-1].SetLineColor(index+2)
+        h1loc[len(h1loc)-1].SetLineWidth(2)
+        h1loc[len(h1loc)-1].SetMarkerColor(index+2)
+    h1glob.append(h1loc)            
 
 # now loop on tree and project
 for index,mc in enumerate(mclist):
@@ -74,6 +108,8 @@ for index,mc in enumerate(mclist):
     weight=xsec*lumi/nevents
     
     print weight,nevents
+    h1loc=h1glob[index]
+    
     for event  in tree:
         for bin in range(0,int(event.step)+1):
             if index==0:
@@ -89,10 +125,13 @@ for index,mc in enumerate(mclist):
 #                    print 'ok'
                     genrec_s3_t1[index].Fill(event.genRecDistance1)
                     genrec_s3_t2[index].Fill(event.genRecDistance2)
-            massh_h[index].Fill(event.recHMass)   
-            massz_h[index].Fill(event.recZMass)   
             mzh_h2[index].Fill(event.recHMass,event.recZMass)
 
+            for i,h1 in enumerate(h1loc):
+                param=h1_list[i]
+#                print param[1]
+                h1.Fill(eval(param[1]))
+            
     treefile.Close()
     # renormalize step_h histo for efficiencies
     norm=step_h[index].GetBinContent(1)
@@ -103,6 +142,13 @@ for index,mc in enumerate(mclist):
         
     
 # now we can plot them
+
+# first prepare legenda
+yheaderstart=.95-.023*len(mclist)
+leg_hist = TLegend(0.7,yheaderstart,.98,.98);
+leg_hist.SetFillColor(0)# Have a white background
+
+
 c1=TCanvas("c1","c1",800,600)
 c1.SetFillColor(kWhite)
 c1.SetFillStyle(1001)
@@ -110,8 +156,6 @@ c1.SetFillStyle(1001)
  
 first=True
 # legenda
-yheaderstart=.95-.023*len(mclist)
-leg_hist = TLegend(0.7,yheaderstart,.98,.98);
 
 #c1.Divide(1,2)
 for index in range(0,len(mclist)):
@@ -122,39 +166,32 @@ for index in range(0,len(mclist)):
         opt=""
     print index,opt
     step_h[index].Draw(opt)
+    leg_hist.AddEntry(step_h[index],mc[2],"l")
 #    c1.cd(1)
 #    genrec_s3_t1[index].Draw(opt)
 #    c1.cd(2)
 #    genrec_s3_t2[index].Draw(opt)
 
-    leg_hist.AddEntry(step_h[index],mc[2],"l")
-
-leg_hist.SetFillColor(0)# Have a white background
 leg_hist.Draw() 
 c1.Print("cut_chain.png")
 
-c2=TCanvas("c2","c2",800,600)
-stackh_h = THStack("HmassS", "HmassS")
-for index in range(len(mclist)-1,-1,-1):
-    print index
-    stackh_h.Add(massh_h[index])
-stackh_h.Draw()
-c2.Print("hmass.png")
+canv=[]
+for i,h1 in enumerate(h1_list):
+    plot=h1[5]
+    if not plot:
+        continue
+    tag=h1[0]
+    canv.append(TCanvas("c_"+tag,"c_"+tag,800,600))
+    stackh_h = THStack(tag, tag)
+    for index in range(len(mclist)-1,-1,-1):
+        h1loc=h1glob[index]
+        print index
+        stackh_h.Add(h1loc[i])
+    stackh_h.Draw()
+    leg_hist.Draw()
+    canv[len(canv)-1].Print("hmass.png")
 
 
-c3=TCanvas("c3","c3",800,600)
-stackz_h = THStack("ZmassS", "ZmassS")
-for index in range(len(mclist)-1,-1,-1):
-    print index
-    stackz_h.Add(massz_h[index])
-stackz_h.Draw()
-c3.Print("zmass.png")
-
-c4=TCanvas("c4","c4",800,600)
-c4.cd()
-mzh_h2[0].Draw()
-c4.Update()
-c4.Print("mzmh.png")
 
 
 #stackmzh_h2 = THStack("mzmh", "mzmh")
