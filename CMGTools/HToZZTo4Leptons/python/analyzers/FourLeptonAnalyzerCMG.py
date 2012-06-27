c960e5ab5e7b03ec0a173a46a1ee5e2450be4008
bachtis <>
2012-06-27 22:25:56 +0000

    Resurrecting combinatorial analyzer'

--- CMGTools/HToZZTo4Leptons/python/analyzers/FourLeptonAnalyzerCMG.py
+++ CMGTools/HToZZTo4Leptons/python/analyzers/FourLeptonAnalyzerCMG.py
@@ -1,3 +1,4 @@
+
 import operator 
 import itertools
 import copy
@@ -16,7 +17,11 @@ from CMGTools.HToZZTo4Leptons.tools.CutFlowMaker import CutFlowMaker
 
 from CMGTools.HToZZTo4Leptons.analyzers.FourLeptonAnalyzerBase import FourLeptonAnalyzerBase
 from CMGTools.HToZZTo4Leptons.tools.OverlapCleaner import OverlapCleaner 
+
 from CMGTools.HToZZTo4Leptons.tools.FSRRecovery import FSRRecovery
+
+from CMGTools.RootTools.utils.DeltaR import deltaR
+
 
 
         
@@ -30,20 +35,24 @@ class FourLeptonAnalyzerCMG( FourLeptonAnalyzerBase ):
 
     def beginLoop(self):
         super(FourLeptonAnalyzerCMG,self).beginLoop()
-
+        
 
        
     def process(self, iEvent, event):
+        super(FourLeptonAnalyzerCMG,self).process(iEvent,event)
+        
         self.readCollections( iEvent )
         vertex = event.goodVertices[0]
-
 
         # creating a "sub-event" for this analyzer
         myEvent = Event(event.iEv)
         setattr(event, self.name, myEvent)
         event = myEvent
         event.step = 0
+
+        #set the vertex
         event.vertex=vertex
+
 
         #startup counter
         self.counters.counter('FourLepton').inc('all events')
@@ -54,17 +63,20 @@ class FourLeptonAnalyzerCMG( FourLeptonAnalyzerBase ):
         event.rhoEle = self.handles['rhoEle'].product()[0]
         self.rhoMu = event.rhoMu
         self.rhoEle = event.rhoEle
-       
-        #Get photons
-        self.buildPhotonList( event )
 
         
-        #Build lepton lists and apply skim
+        #Get photons
+        self.buildPhotonList( event )
+        #get leptons
         self.buildLeptonList( event )
+        #get jets
+        self.buildJetList(event)
 
+        #prune jets
+        event.selectedJets = filter(lambda x:x.pt()>30 and abs(x.eta())<4.5,event.jets)
+        
         #create a cut flow
         cutFlow = CutFlowMaker(self.counters.counter("FourLepton"),event,event.leptons1,event.leptons2)
-
         #Cuts :Apply minimal criteria like sip<100 and min Pt and eta and require at least two leptons 
         passed=cutFlow.applyDoubleCut(self.testLeptonSkim1,self.testLeptonSkim2,'lepton preselection',2,'skimmedLeptons1','skimmedLeptons2')
         if passed: event.step += 1
@@ -77,33 +89,28 @@ class FourLeptonAnalyzerCMG( FourLeptonAnalyzerBase ):
         cleanOverlap = OverlapCleaner(event.skimmedLeptons,0.05,11,13,self.testMuonCleaning)
         passed = cutFlow.applyCut(cleanOverlap,'electron cross cleaning',2,'cleanLeptons')
 
+
         #make lepton combinations 
         event.leptonPairs = self.findPairsWithFSR(cutFlow.obj1,event.photons)
         cutFlow.setSource1(event.leptonPairs)
         
-        #require that   M>40 and OS/SF
+        #require that    OS/SF
         passed=cutFlow.applyCut(self.testZSkim,'2l at least one z',1,'zBosons')
 
-        #Apply also M<120 cut for comparing with others
-        passed=cutFlow.applyCut(self.testZ1Mass,'2l MZ less than 120',1,'zBosonsMass')
-
-
-        #Now we will do the fake rate thing. We will apply full ID on the Z
-        #Then find the best Z . Then find the leptons that dont belong to the Z
-        #We will use those leptons for fake rate measurement
+        #require tight ID
         passed=cutFlow.applyCut(self.testZ1TightID,'2l  tight ID',1,'zBosonsTightID')
 
+        #Apply also M>40 and M<120 cut for comparing with others
+        passed=cutFlow.applyCut(self.testZ1Mass,'2l MZ less than 120',1,'zBosonsMass')
 
         #if no zs throw the event
         if not passed: return self.cfg_ana.keep
         event.step += 1
 
-
         #Find the best of the Zs
         if passed:
-            event.bestZForFakeRate = self.bestZBosonByMass(event.zBosonsTightID)
+            event.bestZForFakeRate = self.bestZBosonByMass(cutFlow.obj1)
 
-            
             #####FAKE RATE MEASUREMENT BUSINESS#############
             ###############################################
             
@@ -114,60 +121,104 @@ class FourLeptonAnalyzerCMG( FourLeptonAnalyzerBase ):
             #Require 1 lepton !
 
             # So lets make a collection for those
-            if len(event.leptonsForFakeRate)==1:
-                for lepton in event.leptonsForFakeRate:
-                    if  hasattr(self.cfg_ana,"FSR"):
-                        fsrAlgo=FSRRecovery(self.cfg_ana.FSR)
-                        fsrAlgo.setPhotons(event.photons)
-                        fsrAlgo.setLeg(lepton)
-                        fsrAlgo.recoverLeg()
+            if len(event.leptonsForFakeRate)==1 and \
+               deltaR(event.leptonsForFakeRate[0].eta(),event.leptonsForFakeRate[0].phi(), \
+                      event.bestZForFakeRate.leg1.eta(),event.bestZForFakeRate.leg1.phi())>0.02 and \
+               deltaR(event.leptonsForFakeRate[0].eta(),event.leptonsForFakeRate[0].phi(), \
+                      event.bestZForFakeRate.leg2.eta(),event.bestZForFakeRate.leg2.phi())>0.02:
+                minmass=True
+                if event.leptonsForFakeRate[0].charge()+event.bestZForFakeRate.leg1.charge()==0:
+                    if (event.leptonsForFakeRate[0].p4()+event.bestZForFakeRate.leg1.p4()).M()<self.cfg_ana.minMass:
+                        minmass=False
+                if event.leptonsForFakeRate[0].charge()+event.bestZForFakeRate.leg2.charge()==0:
+                    if (event.leptonsForFakeRate[0].p4()+event.bestZForFakeRate.leg2.p4()).M()<self.cfg_ana.minMass:
+                        minmass=False
+                        
+
+                if  hasattr(self.cfg_ana,"FSR"):
+                    fsrAlgo=FSRRecovery(self.cfg_ana.FSR)
+                    fsrAlgo.setPhotons(event.photons)
+                    fsrAlgo.setLeg(event.leptonsForFakeRate[0])
+                    fsrAlgo.recoverLeg()
+                if not minmass:
+                    event.leptonsForFakeRate=[]
             else:
                 event.leptonsForFakeRate=[]
 
-            
-        #OK in the post analysis process we will use those leptons to measure the fake rate
-        #Nothing else to be done for that
 
         #Now create four Lepton candidtes upstream. Use all permutations and not combinations
         #of leptons so we can pick the best Z1 and Z2
         event.fourLeptons = self.findQuadsWithFSR(event.cleanLeptons,event.photons)
+
         #Sort them by M1 near Z and My highest Pt sum
         event.sortedFourLeptons = self.sortFourLeptons(event.fourLeptons)
+
         cutFlow.setSource1(event.sortedFourLeptons)
 
-        #Next Step : Apply Loose Lepton Selection
+        #Ghost Suppression
+        passed=cutFlow.applyCut(self.testFourLeptonGhostSuppression,'ghost suppression',1,'fourLeptonsGhostSup')
+
+        #tight ID for Z1
+        passed=cutFlow.applyCut(self.testFourLeptonTightIDZ1,'4l z1 tight ID',1,'fourLeptonsTightIDZ1')
+
+        #Next Step : Apply Tight Lepton Selection for Z1 
         passed=cutFlow.applyCut(self.testFourLeptonLooseID,'4l loose lepton id',1,'fourLeptonsLooseID')
+
         #Require Z1 OS/SF and mass cuts
         passed=cutFlow.applyCut(self.testFourLeptonZ1,'4l pair 1 built',1,'fourLeptonsZ1')
-        #Apply Tight ID for Z1
-        passed=cutFlow.applyCut(self.testFourLeptonTightIDZ1,'4l  Z1 tightID',1,'fourLeptonsTightZ1')
-        #Search for second SF pair
-        passed=cutFlow.applyCut(self.testFourLeptonSF,'4l pair 2  SF',1,'fourLeptonsSFZ2')
-        #Mass cuts for second lepton
-        passed=cutFlow.applyCut(self.testFourLeptonMassZ2,'4l pair 2 mass cut',1,'fourLeptonsMassZ2')
+
         #Pt Cuts (CAREFUL: The correct cut is : Any combination of leptons must be 20/10 not the Z1 ones
         passed=cutFlow.applyCut(self.testFourLeptonPtThr,'4l Pt Thresholds',1,'fourLeptonsFakeRateApp')
-        #####THIS IS OUR FAKE RATE APPLICATION REGION
+
+        #calculate mela and vbf
+        for fl in event.fourLeptonsFakeRateApp:
+            fl.mela=self.mela.calculate(fl)
+            self.calculateJets(fl,event.selectedJets)
+
         if passed:
             event.higgsCandLoose = cutFlow.obj1[0]
-        #Search for second OS pair
+
+        #OS Z2    
         passed=cutFlow.applyCut(self.testFourLeptonOS,'4l pair 2  OS',1,'fourLeptonsOSZ2')
 
-        #Now apply tight ID in all leptons
-        passed=cutFlow.applyCut(self.testFourLeptonTightID,'4l tight lepton tightid',1,'fourLeptonsTightID')
-        
+        #tight ID    
+        passed=cutFlow.applyCut(self.testFourLeptonTightID,'4l tight lepton id',1,'fourLeptonsTightID')
+
+        #Mass cuts for second lepton
+        passed=cutFlow.applyCut(self.testFourLeptonMassZ2,'4l pair 2 mass cut',1,'fourLeptonsMassZ2')
+
         #QCD suppression
         passed=cutFlow.applyCut(self.testFourLeptonMinOSMass,'4l QCD suppression',1,'fourLeptonsQCDSuppression')
 
         #Z -> 4 l phase space
         passed=cutFlow.applyCut(self.testFourLeptonMassZ,'4l Z phase space',1,'fourLeptonsZPhaseSpace')
 
+
+        #Z2 Mass Tight Cut
+        passed=cutFlow.applyCut(lambda x: x.leg2.mass()>12.,'4l Tight Mass2',1,'fourLeptonsTightZ2')
+
+        event.otherLeptons=[]
+        event.otherTightLeptons=[]
         if passed:
             event.higgsCand = cutFlow.obj1[0]
-
+            event.otherLeptons=copy.copy(event.cleanLeptons)
+            event.otherLeptons.remove(event.higgsCand.leg1.leg1)
+            event.otherLeptons.remove(event.higgsCand.leg1.leg2)
+            event.otherLeptons.remove(event.higgsCand.leg2.leg1)
+            event.otherLeptons.remove(event.higgsCand.leg2.leg2)
+            event.otherLeptons = filter(lambda x:x.pt()>10,event.otherLeptons)
+            event.otherTightLeptons = filter(self.testLeptonTight1,event.otherLeptons)
+            metV = TLorentzVector(event.met.px(),event.met.py(),event.met.pz(),event.met.energy())
+            event.recoil = (-metV-event.higgsCand).Pt()
 
         #ZZ phase smace
         passed=cutFlow.applyCut(self.testFourLeptonMass,'4l H phase space',1,'fourLeptonsHPhaseSpace')
+
+
+        passed=cutFlow.applyCut(lambda x: x.mela>0.1,'MELA',1,'fourLeptonsMELA')
+
+        passed=cutFlow.applyCut(lambda x: hasattr(x.leg1,'fsrPhoton') or hasattr(x.leg2,'fsrPhoton') ,'FSR',1,'fourLeptonsWithFSR')
+
 
 
         
@@ -176,13 +227,9 @@ class FourLeptonAnalyzerCMG( FourLeptonAnalyzerBase ):
 
 
 
-    def getFourLeptonZ2SumPt(self, hBoson):
-        sum=0
-        if hasattr(hBoson.leg2,'fsrPhoton'):
-            if hBoson.leg2.fsrDR1()<hBoson.leg2.fsrDR2():
-                sum = (hBoson.leg2.leg1.p4()+hBoson.leg2.fsrPhoton.p4()).pt()+hBoson.leg2.leg2.pt()
-            else:    
-                sum = (hBoson.leg2.leg2.p4()+hBoson.leg2.fsrPhoton.p4()).pt()+hBoson.leg2.leg1.pt()
-        else:    
-            sum = hBoson.leg2.leg1.pt()+hBoson.leg2.leg2.pt()
-        return sum    
+#    def testFourLeptonTightID(self, fourLepton):
+#        if hasattr(fourLepton.leg1,'fsrPhoton'):
+#            fourLepton.leg2.fsrPhoton =fourLepton.leg1.fsrPhoton 
+#        if hasattr(fourLepton.leg2,'fsrPhoton'):
+##            fourLepton.leg1.fsrPhoton =fourLepton.leg2.fsrPhoton 
+#        return self.testZ1TightID(fourLepton.leg1) and self.testZ2TightID(fourLepton.leg2)
