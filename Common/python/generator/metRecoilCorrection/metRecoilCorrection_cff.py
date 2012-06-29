53ca138be324c1c493f2fda6ea1b9653523ad827
cbern <>
2012-06-29 13:53:03 +0000

    selecting jets recoil with PF and PU jet id

--- Common/python/generator/metRecoilCorrection/metRecoilCorrection_cff.py
+++ Common/python/generator/metRecoilCorrection/metRecoilCorrection_cff.py
@@ -8,8 +8,9 @@ from CMGTools.Common.generator.genWorZ_cfi import *
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
 from CMGTools.Common.skims.cmgPFJetSel_cfi import cmgPFJetSel
-cmgPFJetForRecoil = cmgPFJetSel.clone( cut = 'pt()>30 && abs(eta)<4.5',
-                                       src = 'cmgPFJetSel')
+cmgPFJetForRecoil = cmgPFJetSel.clone(
+    cut = 'pt()>30 && abs(eta)<4.5 && getSelection("cuts_looseJetId") && passLooseFullPuJetId()',
+    src = 'cmgPFJetSel')
 
 
 metRecoilCorrectionInputSequence = cms.Sequence(
