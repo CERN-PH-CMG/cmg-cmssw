2ee89a6b9307288a70d60394e9fdcffe37882dbb
cbern <>
2012-07-05 15:20:51 +0000

    cleanup and new functionalities in fileComparator

--- CMGTools/RootTools/python/utils/fileComparator.py
+++ CMGTools/RootTools/python/utils/fileComparator.py
@@ -1,6 +1,8 @@
 import time
 import os
+import re
 from CMGTools.RootTools.PyRoot import *
+from CMGTools.RootTools.utils.file_dir import file_dir, file_dir_names
 
 def mkdir_p(path):
     try:
@@ -13,34 +15,38 @@ def mkdir_p(path):
 
 class Comparator(object):
 
-    def __init__(self, info1, info2):
+    def __init__(self, info1, info2, filter):
         self.info1 = info1
         self.info2 = info2
         self.legend = None
+        self.filter = re.compile(filter)
 
     def browse(self):
+        maindir = 'Plots_Comparator'
+        if os.path.isdir(maindir):
+            os.system( 'rm -r ' + maindir)
+        os.mkdir(maindir)
         for h1name, h1 in sorted(self.info1.hists.iteritems()):
             h2 = self.info2.hists.get(h1name, None)
             if h2 is None:
                 pass
             # print h1name, 'not in', d2dir.GetName()
             else:
-                h1.SetTitle(h1name)
-                h2.SetTitle(h1name)
-                plotdir = '/'.join(['Plots_Comparator',os.path.dirname(h1name)])
+                plotdir = '/'.join([maindir,os.path.dirname(h1name)])
                 try:
                     mkdir_p( plotdir )
                 except:
                     pass
-                self.drawHists(h1, h2)
-                pngname = '/'.join(['Plots_Comparator',h1name+'.png'])
-                print pngname
-                gPad.SaveAs(pngname)
+                h1.SetTitle(h1name)
+                h2.SetTitle(h1name)
+                if not self.filter.search( h1name ):
+                    print 'Skipping', h1name
+                    continue
+                self.drawHists(h1, h2, h1name)
                 res = raw_input('')
                 
-
-    def drawHists(self, h1, h2):
-        print 'Draw', h1.GetName()
+    def drawHists(self, h1, h2, h1name):
+        print 'Draw', h1name
         h1.SetMarkerColor(4)
         h2.SetMarkerColor(1)                    
         h1.SetLineColor(4)
@@ -49,24 +55,27 @@ class Comparator(object):
         h2.SetLineColor(1)
         maximum = max(h1.GetBinContent(h1.GetMaximumBin()),
                       h2.GetBinContent(h2.GetMaximumBin()))
-        print maximum
         h1.Draw('E2')
         h2.Draw('same')
         if self.legend is None:
             self.legend = TLegend(0.5, 0.65, 0.89, 0.8)
-            self.legend.AddEntry(h1, self.info1.tfile.GetName(), 'pf')
-            self.legend.AddEntry(h2, self.info2.tfile.GetName(), 'p')
+            self.legend.AddEntry(h1, self.info1.name, 'pf')
+            self.legend.AddEntry(h2, self.info2.name, 'p')
         h1.GetYaxis().SetRangeUser(0.001, maximum*1.2)
         self.legend.Draw('same')
         gPad.Update()
+        pngname = '/'.join(['Plots_Comparator',h1name+'.png'])
+        print pngname
+        gPad.SaveAs(pngname)
+        return True
 
         
 class FlatFile(object):
-    def __init__(self, tfile):
-        self.tfile = tfile
+    def __init__(self, tdir, name):
+        self.tdir = tdir
+        self.name = name
         self.hists = {}
-#         self.flatten( self.tfile, self.tfile.GetName(), self.hists )
-        self.flatten( self.tfile, '.', self.hists )
+        self.flatten( self.tdir, '.', self.hists )
         
     def flatten(self, dir, mothername, hists):
         for key in dir.GetListOfKeys():
@@ -82,8 +91,26 @@ class FlatFile(object):
 if __name__ == '__main__':
     import sys
 
-    file1 = FlatFile( TFile(sys.argv[1]) )
-    file2 = FlatFile( TFile(sys.argv[2]) )
-    comparator = Comparator(file1, file2)
+    from optparse import OptionParser
+    
+    parser = OptionParser()
+    parser.usage = '''
+    fileComparator.py <file1[:dir1]> <file2[:dir2]> 
+    '''
+    parser.add_option("-f", "--filter", 
+                      dest="filter", 
+                      help="Filtering regexp pattern to select histograms.",
+                      default='.*')
+
+    (options,args) = parser.parse_args()
+
+    
+    f1, d1 = file_dir(args[0])
+    f2, d2 = file_dir(args[1])
+    name1 = '/'.join( [f1.GetName(), d1.GetName()])
+    name2 = '/'.join( [f2.GetName(), d2.GetName()])
+    file1 = FlatFile( d1, name1)
+    file2 = FlatFile( d2, name2)
+    comparator = Comparator(file1, file2, options.filter)
     comparator.browse()
 
