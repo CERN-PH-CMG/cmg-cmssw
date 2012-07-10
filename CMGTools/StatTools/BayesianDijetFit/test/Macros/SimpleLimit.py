767eca4a82fdab8203cdde52c00c3640b97ecb3d
mgouzevi <>
2012-07-10 18:42:40 +0000

    Updated

--- CMGTools/StatTools/BayesianDijetFit/test/Macros/SimpleLimit.py
+++ CMGTools/StatTools/BayesianDijetFit/test/Macros/SimpleLimit.py
@@ -7,9 +7,9 @@ import os
 ## command = "dijetStatsNewBackground 1100 0 0 RSGraviton_ak5_QQtoQQ_fat30 0"
 ## print command 
 ## os.system(command)                         
-#command = "dijetStatsNewBackground 1200 0 0 Qstar_ak5_fat30 1"
-#print command 
-#os.system(command)
+command = "dijetStatsNewBackground 1200 0 0 Qstar_ak5_fat30 1"
+print command 
+os.system(command)
 
 ## command = "dijetStatsNewBackground 1100 0 0 RSGraviton_HLT_ak5_GGtoGG_fat30 0"
 ## print command 
@@ -35,9 +35,9 @@ import os
 #print command 
 #os.system(command)
 
-command = "dijetStatsNewBackground 3000 0 0 QBH_ak5_fat 1"
-print command 
-os.system(command)
+## command = "dijetStatsNewBackground 3000 0 0 QBH_ak5_fat 1"
+## print command 
+## os.system(command)
 
 
 print 'end' 
