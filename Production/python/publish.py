cc7d6fefa49e3374a058009243ac57ca59a67377
pmeckiff <>
2012-07-17 13:28:20 +0000

    New format with separate file group information added New class structure with DatasetInformation class providing all information about dataset in the form of dict object

--- Production/python/publish.py
+++ Production/python/publish.py
@@ -7,7 +7,7 @@ import os, sys, re
 from CMGTools.Production.publishController import PublishController
 from CMGTools.Production.nameOps import *
 from CMGTools.Production.castorBaseDir import getUserAndArea
-from CMGTools.Production.datasetAttributeAddressBook import *
+from CMGTools.Production.datasetInformation import DatasetInformation
 
 
 def publish(dsName,fileown,comment,test,user,password, force, checkGroups, savannah, run_range = None):
@@ -24,7 +24,24 @@ def publish(dsName,fileown,comment,test,user,password, force, checkGroups, savan
 	checked if the Dataset does not exist in the use space
 	'savannah' takes True/False on whether Savannah publish is desired
 	"""
-
+	# Validate name, and escape if name is invalidate
+	# Convert name to EOS format (castor)
+	if re.search("---",dsName):
+		fileown = getDbsUser(dsName)
+		dsName2 = getCastor(dsName)
+		if dsName2 is None:
+			print "\nError, "+dsName+" is not valid, please use valid name\n"
+			return None
+		else:
+			dsName = dsName2
+	
+	# Check the length of the dataset name	
+	if len(dsName.lstrip(os.sep).rstrip(os.sep).split(os.sep)) < 3:
+		print "Error, "+dsName+" is not valid, please use valid name"
+		return None
+	elif len(dsName.lstrip(os.sep).rstrip(os.sep).split(os.sep)) < 4:
+		print "Dataset "+ dsName + "is a CMS base dataset and cannot be published, please use DAS."
+		return None
 	
 	datasetDetails = None
 	user, area = getUserAndArea(user)
@@ -38,6 +55,7 @@ def publish(dsName,fileown,comment,test,user,password, force, checkGroups, savan
 		
 		# Initialise PublishController
 		publishController = PublishController(user, password)
+		
 		# Attempt Login, if unexplained fail occurs, retry
 		loginClear = False
 		try:
@@ -51,31 +69,40 @@ def publish(dsName,fileown,comment,test,user,password, force, checkGroups, savan
 		        raise
 		
 		# Get DS Information
-		datasetDetails = buildAttributeDictionary(dsName, fileown ,force, checkGroups, run_range, comment, test, user, password)
-		if datasetDetails is None: return None
+		datasetDetails = DatasetInformation(dsName, fileown ,comment ,force,test, user, password, run_range)
 		
+		if datasetDetails is None: return None
+		datasetDetails.buildAllReports()
+		if datasetDetails.dataset_details is None: return None
+		
+		# Print dataset names
+		print "\n------DataSet------\n"
+		print "CMGDB name:",datasetDetails.dataset_details['CMGDBName']
+		print "Sample name:  ",datasetDetails.dataset_details['SampleName']
+		print "Total Jobs: ",datasetDetails.dataset_details['TotalJobs']
+		print "Directory size: ",datasetDetails.dataset_details['DirectorySizeInTB']," TB"
+		print ""
+		for group_name in datasetDetails.dataset_details['FileGroups']:
+			print datasetDetails.createFileGroupDetailString(group_name)
 		# If login fails return None
 		if loginClear is False:
 			print "User authentication failed, exiting\n\n"
 			return None
 		
-		# Print dataset names
-		print "\n------DataSet------\n"
-		print "CMGDB name:",datasetDetails['CMGDBName']
-		print "EOS name:  ",datasetDetails['EOSName']
+		
 		
 		## Savannah operations
 		print "\n------Savanah------\n"
 		
-		if savannah or datasetDetails['taskID'] == None:
-			(datasetDetails['taskID'], datasetDetails['parentTaskID']) = publishController.savannahPublish(datasetDetails)
+		if savannah or datasetDetails.dataset_details['TaskID'] == None:
+			(datasetDetails.dataset_details['TaskID'], datasetDetails.dataset_details['ParentTaskID']) = publishController.savannahPublish(datasetDetails.dataset_details)
 		else:
 		    print "NO SAVANNAH PUBLISH REQUIRED"
 		
-		if datasetDetails['taskID'] is not None: status = 'Success'
+		if datasetDetails.dataset_details['TaskID'] is not None: status = 'Success'
 		if publishController.cmgdbOnline():
 			print "\n-------CMGDB-------\n"
-			cmgdbid = publishController.cmgdbPublish(datasetDetails)
+			cmgdbid = publishController.cmgdbPublish(datasetDetails.dataset_details)
 			
 		return datasetDetails
 	except KeyboardInterrupt:
