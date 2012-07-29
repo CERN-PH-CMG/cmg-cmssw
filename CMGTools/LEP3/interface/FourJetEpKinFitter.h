4289cb2c7f633dc85f6db7b4815392b994d70fb8
botta <>
2012-07-29 22:47:59 +0000

    adding interface for getS() function

--- CMGTools/LEP3/interface/FourJetEpKinFitter.h
+++ CMGTools/LEP3/interface/FourJetEpKinFitter.h
@@ -18,6 +18,8 @@ class FourJetEpKinFitter : public TKinFitter {
 				    LorentzVector jet3,
 				    LorentzVector jet4 );
 
+  double getChi2();
+
   double ErrEt(double Et, double Eta); 
   double ErrEta(double Et, double Eta); 
   double ErrPhi(double Et, double Eta); 
