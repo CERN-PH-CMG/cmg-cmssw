bbe587c7c6d1b4f1a9d93be5b71140fb58990d3b
benitezj <>
2012-08-06 13:59:38 +0000

    *** empty log message ***

--- H2TauTau/json/computeLumi.sh
+++ H2TauTau/json/computeLumi.sh
@@ -33,19 +33,28 @@ which lumiCalcVersion
 #lumiCalcVersion -i $Json2011B recorded --hltpath  HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v6 | grep HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v6
 
 ###-------2012-----------------------------
-export Json2012ICHEP="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196509_8TeV_PromptReco_Collisions12_JSON.txt"
-echo $Json2012ICHEP
+#export Json2012="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196509_8TeV_PromptReco_Collisions12_JSON.txt"
+export Json2012="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-199754_8TeV_PromptReco_Collisions12_JSON.txt"
+#export Json2012="./Cert_190456-199429_8TeV_PromptReco_Collisions12_JSON_Run2012c.txt"
+echo $Json2012
 
-##mu-tau
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v4 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v4
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v5 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v5
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v6 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v6
-#
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2 | grep HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2
+######mu-tau
+##2012A
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v4 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v4
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v5 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v5
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v6 | grep HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v6
+##2012B
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2 | grep HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2
+##2012C
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v3 | grep HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v3 #199273
+#| HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v3 |      22076 | 1.783(/fb) | 1.783(/fb) |
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v6 | grep HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v6 #199696
+#| HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v6 |       2039 | 153.752(/pb) | 153.752(/pb) |
 
-##e-tau
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v4 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v4 
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v5 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v5 
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v6 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v6 
-#
-#lumiCalcVersion -i $Json2012ICHEP recorded --hltpath HLT_Ele22_eta2p1_WP90Rho_LooseIsoPFTau20_v2 | grep  HLT_Ele22_eta2p1_WP90Rho_LooseIsoPFTau20_v2
+######e-tau
+###2012A
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v4 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v4 
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v5 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v5 
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v6 | grep HLT_Ele20_CaloIdVT_CaloIsoRhoT_TrkIdT_TrkIsoT_LooseIsoPFTau20_v6 
+###2012B
+#lumiCalcVersion -i $Json2012 recorded --hltpath HLT_Ele22_eta2p1_WP90Rho_LooseIsoPFTau20_v2 | grep  HLT_Ele22_eta2p1_WP90Rho_LooseIsoPFTau20_v2
