be55bf52b0d863beb70527add8762c81fa2f4c72
wreece <>
2012-08-07 15:15:09 +0000

    updated jet ID

--- CMGTools/Common/python/selections/jetId_cfi.py
+++ CMGTools/Common/python/selections/jetId_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # see for details https://twiki.cern.ch/twiki/bin/view/CMS/JetID
+# charged hadron h (1), e(2), mu(3), gamma(4), neutral hadron h0 (5)
 
 veryLooseJetId99 = cms.PSet(
     h0Fraction = cms.string('component(5).fraction() < 0.99'),                      
@@ -20,17 +21,15 @@ veryLooseJetId95gamma = cms.PSet(
     gammaFraction = cms.string('component(4).fraction() < 0.95')  
     )
 
-
 #Selection thanks to Maxime
 looseJetId = cms.PSet(
-    h0Fraction = cms.string('component(5).fraction() < 0.99'),                      
+    h0Fraction = cms.string('component(5).fraction() < 0.99'),      
     gammaFraction = cms.string('component(4).fraction() < 0.99'),
     nConstituents = cms.string('nConstituents() > 1'),
-    #these only valid for abs(eta) < 2.4
+    #these only valid for abs(eta) < 2.4 - i.e. they always pass for eta > 2.4
     hFraction = cms.string('component(1).fraction() > 0 || abs(eta()) > 2.4 '),
-    hChargedMultiplicity = cms.string('component(1).number() > 0 || abs(eta()) > 2.4 '),
+    chargedMultiplicity = cms.string('( component(1).number() + component(2).number() + component(3).number() ) > 0 || abs(eta()) > 2.4 '),
     eFraction = cms.string('component(2).fraction() < 0.99 || abs(eta()) > 2.4 '),
-    muFraction = cms.string('component(3).fraction() < 0.99 || abs(eta()) > 2.4 '),
     )
 
 mediumJetId = looseJetId.clone()
