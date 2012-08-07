c3b6a9cdca3f50e64b47a94bed7a3cf9d47dce3c
botta <>
2012-08-07 16:54:07 +0000

    add new class

--- CMGTools/LEP3/src/classes.h
+++ CMGTools/LEP3/src/classes.h
@@ -1,4 +1,5 @@
 #include "CMGTools/LEP3/interface/FourJetEpKinFitter.h"
+#include "CMGTools/LEP3/interface/FourJetEpMKinFitter.h"
 #include "CMGTools/LEP3/interface/DiJetMKinFitter.h"
 #include <TLorentzVector.h>
 
@@ -7,6 +8,7 @@ namespace {
 
     FourJetEpKinFitter fourJetEpKinFitter; 
     DiJetMKinFitter diJetMKinFitter;
+    FourJetEpMKinFitter fourJetEpMKinFitter;
     std::pair<TLorentzVector,TLorentzVector> pairOfTLV;
     std::vector<TLorentzVector> vectorOfTLV;
 
