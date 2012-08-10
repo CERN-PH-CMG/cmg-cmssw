b62408b8e04771e648633213600829ff4b97757c
tjkim <>
2012-08-10 15:00:05 +0000

    remove obsolete electron ids and add new cut based id working point cut string selection

--- CMGTools/Common/python/factories/cmgElectron_cfi.py
+++ CMGTools/Common/python/factories/cmgElectron_cfi.py
@@ -10,9 +10,15 @@ electronFactory = cms.PSet(
        electronMVAFile = cms.FileInPath("CMGTools/Common/data/TMVA_BDTSimpleCat.weights.xml")
        )
 
-from CMGTools.Common.selections.vbtfelectron_cfi import *
-from CMGTools.Common.selections.cicelectron_cfi import *
-from CMGTools.Common.selections.isolation_cfi import *
+#from CMGTools.Common.selections.vbtfelectron_cfi import *
+#from CMGTools.Common.selections.cicelectron_cfi import *
+#from CMGTools.Common.selections.isolation_cfi import *
+
+# see mva pre-selection id : https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentification
+# see cut-based id working point : https://twiki.cern.ch/twiki/bin/view/CMS/EgammaCutBasedIdentification
+# NOTE: for cut-based id, vtx and isolation cut are NOT included.
+
+from CMGTools.Common.selections.cutidelectron_cfi import *
 
 cmgElectron = cms.EDFilter("ElectronPOProducer",
     cfg = electronFactory.clone(),
@@ -21,31 +27,38 @@ cmgElectron = cms.EDFilter("ElectronPOProducer",
        isEB = cms.string('sourcePtr().isEB()'),
        isEE = cms.string('sourcePtr().isEE()'),
        # requires that the id variables have been filled
-       vbtf60ID      = vbtfelectron60ID.clone(),
-       vbtf70ID      = vbtfelectron70ID.clone(),
-       vbtf80ID      = vbtfelectron80ID.clone(),
-       vbtf90ID      = vbtfelectron90ID.clone(),
-       vbtf95ID      = vbtfelectron95ID.clone(),
-       vbtf60CR      = vbtfelectron60CR.clone(),
-       vbtf70CR      = vbtfelectron70CR.clone(),
-       vbtf80CR      = vbtfelectron80CR.clone(),
-       vbtf90CR      = vbtfelectron90CR.clone(),
-       vbtf95CR      = vbtfelectron95CR.clone(),       
-       veryLooseID   = cicVeryLooseID.clone(),
-       looseID       = cicLooseID.clone(),
-       mediumID      = cicMediumID.clone(),
-       tightID       = cicTightID.clone(),
-       superTightID  = cicSuperTightID.clone(),
-       veryLooseCR   = cicVeryLooseCR.clone(),
-       looseCR       = cicLooseCR.clone(),
-       mediumCR      = cicMediumCR.clone(),
-       tightCR       = cicTightCR.clone(),
-       superTightCR  = cicSuperTightCR.clone(),
-       veryLooseIP   = cicVeryLooseIP.clone(),
-       looseIP       = cicLooseIP.clone(),
-       mediumIP      = cicMediumIP.clone(),
-       tightIP       = cicTightIP.clone(),
-       superTightIP  = cicSuperTightIP.clone(),
-       isoelectron = isolation.clone(),
+       vetoNoVtx          = vetoNoVtx.clone(),
+       looseNoVtx         = looseNoVtx.clone(),
+       mediumNoVtx        = mediumNoVtx.clone(),
+       tightNoVtx         = tightNoVtx.clone(),
+       premvaTrig             = premvaTrig.clone(),
+
+#obsolete ids : not supported by EGM POG in 2012
+#       vbtf60ID      = vbtfelectron60ID.clone(),
+#       vbtf70ID      = vbtfelectron70ID.clone(),
+#       vbtf80ID      = vbtfelectron80ID.clone(),
+#       vbtf90ID      = vbtfelectron90ID.clone(),
+#       vbtf95ID      = vbtfelectron95ID.clone(),
+#       vbtf60CR      = vbtfelectron60CR.clone(),
+#       vbtf70CR      = vbtfelectron70CR.clone(),
+#       vbtf80CR      = vbtfelectron80CR.clone(),
+#       vbtf90CR      = vbtfelectron90CR.clone(),
+#       vbtf95CR      = vbtfelectron95CR.clone(),       
+#       veryLooseID   = cicVeryLooseID.clone(),
+#       looseID       = cicLooseID.clone(),
+#       mediumID      = cicMediumID.clone(),
+#       tightID       = cicTightID.clone(),
+#       superTightID  = cicSuperTightID.clone(),
+#       veryLooseCR   = cicVeryLooseCR.clone(),
+#       looseCR       = cicLooseCR.clone(),
+#       mediumCR      = cicMediumCR.clone(),
+#       tightCR       = cicTightCR.clone(),
+#       superTightCR  = cicSuperTightCR.clone(),
+#       veryLooseIP   = cicVeryLooseIP.clone(),
+#       looseIP       = cicLooseIP.clone(),
+#       mediumIP      = cicMediumIP.clone(),
+#       tightIP       = cicTightIP.clone(),
+#       superTightIP  = cicSuperTightIP.clone(),
+#       isoelectron = isolation.clone(),
        )
 )
