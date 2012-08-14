1741dd018eb5be94c233d2f40a2847ce65936a26
hinzmann <>
2012-08-14 13:41:12 +0000

    5fb data ntuple3

--- UserArea/TNM/Ntuples/TNMc1/bin/chi_analysis.cc
+++ UserArea/TNM/Ntuples/TNMc1/bin/chi_analysis.cc
@@ -12,6 +12,8 @@
 #include "pdg.h"
 #endif
 
+#include "DataFormats/Math/interface/deltaR.h"
+
 using namespace std;
 //-----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -117,18 +119,28 @@ int main(int argc, char** argv)
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v3", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v3);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v4", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v4);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v5", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v5);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT500_v1", triggerresultshelper_HLT_HT500_v1);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT500_v2", triggerresultshelper_HLT_HT500_v2);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT500_v3", triggerresultshelper_HLT_HT500_v3);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT550_v1", triggerresultshelper_HLT_HT550_v1);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT550_v2", triggerresultshelper_HLT_HT550_v2);
-  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT550_v3", triggerresultshelper_HLT_HT550_v3);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v6", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v6);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v7", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v7);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v8", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v9", triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v1", triggerresultshelper_HLT_HT650_v1);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v2", triggerresultshelper_HLT_HT650_v2);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v3", triggerresultshelper_HLT_HT650_v3);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v4", triggerresultshelper_HLT_HT650_v4);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v5", triggerresultshelper_HLT_HT650_v5);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v6", triggerresultshelper_HLT_HT650_v6);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v7", triggerresultshelper_HLT_HT650_v7);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v8", triggerresultshelper_HLT_HT650_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT650_v9", triggerresultshelper_HLT_HT650_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v1", triggerresultshelper_HLT_HT750_v1);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v2", triggerresultshelper_HLT_HT750_v2);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v3", triggerresultshelper_HLT_HT750_v3);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v4", triggerresultshelper_HLT_HT750_v4);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v5", triggerresultshelper_HLT_HT750_v5);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v6", triggerresultshelper_HLT_HT750_v6);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v7", triggerresultshelper_HLT_HT750_v7);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v8", triggerresultshelper_HLT_HT750_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_HT750_v9", triggerresultshelper_HLT_HT750_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v1", triggerresultshelper_HLT_PFHT650_v1);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v2", triggerresultshelper_HLT_PFHT650_v2);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v3", triggerresultshelper_HLT_PFHT650_v3);
@@ -137,18 +149,25 @@ int main(int argc, char** argv)
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v6", triggerresultshelper_HLT_PFHT650_v6);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v7", triggerresultshelper_HLT_PFHT650_v7);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v8", triggerresultshelper_HLT_PFHT650_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT650_v9", triggerresultshelper_HLT_PFHT650_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v1", triggerresultshelper_HLT_PFHT700_v1);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v2", triggerresultshelper_HLT_PFHT700_v2);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v3", triggerresultshelper_HLT_PFHT700_v3);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v4", triggerresultshelper_HLT_PFHT700_v4);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v5", triggerresultshelper_HLT_PFHT700_v5);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v6", triggerresultshelper_HLT_PFHT700_v6);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v7", triggerresultshelper_HLT_PFHT700_v7);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v8", triggerresultshelper_HLT_PFHT700_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT700_v9", triggerresultshelper_HLT_PFHT700_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v1", triggerresultshelper_HLT_PFHT750_v1);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v2", triggerresultshelper_HLT_PFHT750_v2);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v3", triggerresultshelper_HLT_PFHT750_v3);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v4", triggerresultshelper_HLT_PFHT750_v4);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v5", triggerresultshelper_HLT_PFHT750_v5);
   stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v6", triggerresultshelper_HLT_PFHT750_v6);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v7", triggerresultshelper_HLT_PFHT750_v7);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v8", triggerresultshelper_HLT_PFHT750_v8);
+  stream.select("edmTriggerResultsHelper_TriggerResults_HLT.HLT_PFHT750_v9", triggerresultshelper_HLT_PFHT750_v9);
   stream.select("edmTriggerResultsHelper_TriggerResults_PAT.hcalLaserEventFilterPath", triggerresultshelper_hcalLaserEventFilterPath);
   stream.select("edmTriggerResultsHelper_TriggerResults_PAT.noscrapingFilterPath", triggerresultshelper_noscrapingFilterPath);
   stream.select("edmTriggerResultsHelper_TriggerResults_PAT.primaryVertexFilterPath", triggerresultshelper_primaryVertexFilterPath);
@@ -195,13 +214,13 @@ int main(int argc, char** argv)
   // Declare histograms
   //---------------------------------------------------------------------------
 
-  std::vector<double> massBins = {400,600,800,1000,1200,1500,1900,2400,3000,4000,7000};
-  std::vector<TH1F*> hists;
+  std::vector<double> massBins = {400,600,800,1000,1200,1500,1900,2400,3000,4000,5000,7000};
 
   TCanvas c1("c1","c1",200,200);
   TH1F* h1=new TH1F("dijet_mass","M_{jj}",65,1500,8000);
   h1->Sumw2();
   
+  std::vector<TH1F*> hists;
   for ( size_t j = 0; j < (massBins.size()-1); ++j )
   {
       std::stringstream name;
@@ -210,9 +229,74 @@ int main(int argc, char** argv)
       hists[j]->Sumw2();
   }
   
+  std::vector<TH1F*> histspt1;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "pt1";
+      histspt1.push_back(new TH1F(name.str().c_str(),name.str().c_str(),30,0,3000));
+      histspt1[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histspt2;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "pt2";
+      histspt2.push_back(new TH1F(name.str().c_str(),name.str().c_str(),30,0,3000));
+      histspt2[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histsy1;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "y1";
+      histsy1.push_back(new TH1F(name.str().c_str(),name.str().c_str(),25,-2.5,2.5));
+      histsy1[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histsy2;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "y2";
+      histsy2.push_back(new TH1F(name.str().c_str(),name.str().c_str(),25,-2.5,2.5));
+      histsy2[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histsyboost;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "yboost";
+      histsyboost.push_back(new TH1F(name.str().c_str(),name.str().c_str(),12,0,1.2));
+      histsyboost[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histsmetsumet;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "metsumet";
+      histsmetsumet.push_back(new TH1F(name.str().c_str(),name.str().c_str(),20,0,1.00001));
+      histsmetsumet[j]->Sumw2();
+  }
+  
+  std::vector<TH1F*> histsdphi;
+  for ( size_t j = 0; j < (massBins.size()-1); ++j )
+  {
+      std::stringstream name;
+      name << "dijet_" << massBins[j] << "_" << massBins[j+1] << "_" << "dphi";
+      histsdphi.push_back(new TH1F(name.str().c_str(),name.str().c_str(),20,0,3.15));
+      histsdphi[j]->Sumw2();
+  }
+  
   //---------------------------------------------------------------------------
   // Loop over events
   //---------------------------------------------------------------------------
+
+  std::cout << "event..." << std::endl;
 
   for(int entry=0; entry < nevents; ++entry)
 	{
@@ -226,6 +310,8 @@ int main(int argc, char** argv)
 	  // addEvent if you wish to save only the selected objects.
 	  
 	  // fillObjects();
+	 
+	  if(entry%100000==0) std::cout << entry << "..." << std::endl;
 	  
 	  // ---------------------
 	  // -- event selection --
@@ -236,19 +322,23 @@ int main(int argc, char** argv)
 	     (jethelper2_pt[1]>30)&&
 	     (fabs(jethelper2_rapidity[0])<2.5)&&
 	     (fabs(jethelper2_rapidity[1])<2.5)&&
-	     (fabs(jethelper2_rapidity[1]-jethelper2_rapidity[1])<1.3)&&
-	     (eventhelperextra_dijet_invmass>944)&&
+             (fabs(jethelper2_rapidity[0]+jethelper2_rapidity[1])/2.<1.11)&&
+             (exp(fabs(jethelper2_rapidity[0]-jethelper2_rapidity[1]))<16)&&
 	     
-	     (jethelper2_neutralHadronEnergyFraction[0]<0.99)&&
-	     (jethelper2_neutralEmEnergyFraction[0]<0.99)&&
+	     //(jethelper2_neutralHadronEnergyFraction[0]<0.99)&&
+	     (jethelper2_neutralHadronEnergyFraction[0]<0.90)&&
+	     //(jethelper2_neutralEmEnergyFraction[0]<0.99)&&
+	     (jethelper2_neutralEmEnergyFraction[0]<0.90)&&
 	     (jethelper2_nConstituents[0]>1)&&
 	     ((fabs(jethelper2_eta[0])>2.4)||
 	      ((jethelper2_chargedHadronEnergyFraction[0]>0)&&
 	       (jethelper2_chargedMultiplicity[0]>0)&&
 	       (jethelper2_chargedEmEnergyFraction[0]<0.99)))&&
 	     
-	     (jethelper2_neutralHadronEnergyFraction[1]<0.99)&&
-	     (jethelper2_neutralEmEnergyFraction[1]<0.99)&&
+	     //(jethelper2_neutralHadronEnergyFraction[1]<0.99)&&
+	     (jethelper2_neutralHadronEnergyFraction[1]<0.90)&&
+	     //(jethelper2_neutralEmEnergyFraction[1]<0.99)&&
+	     (jethelper2_neutralEmEnergyFraction[1]<0.90)&&
 	     (jethelper2_nConstituents[1]>1)&&
 	     ((fabs(jethelper2_eta[1])>2.4)||
 	      ((jethelper2_chargedHadronEnergyFraction[1]>0)&&
@@ -258,12 +348,16 @@ int main(int argc, char** argv)
 	     (triggerresultshelper_primaryVertexFilterPath)&&
 	     (triggerresultshelper_noscrapingFilterPath)&&
 	     (triggerresultshelper_hcalLaserEventFilterPath)&&
-             
+
 	     ((triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v1)||
 	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v2)||
 	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v3)||
 	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v4)||
 	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v5)||
+	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v6)||
+	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v7)||
+	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v8)||
+	      (triggerresultshelper_HLT_FatDiPFJetMass750_DR1p1_Deta1p5_v9)||
 	      (triggerresultshelper_HLT_PFHT650_v1)||
 	      (triggerresultshelper_HLT_PFHT650_v2)||
 	      (triggerresultshelper_HLT_PFHT650_v3)||
@@ -272,24 +366,43 @@ int main(int argc, char** argv)
 	      (triggerresultshelper_HLT_PFHT650_v6)||
 	      (triggerresultshelper_HLT_PFHT650_v7)||
 	      (triggerresultshelper_HLT_PFHT650_v8)||
+	      (triggerresultshelper_HLT_PFHT650_v9)||
 	      (triggerresultshelper_HLT_PFHT700_v1)||
 	      (triggerresultshelper_HLT_PFHT700_v2)||
 	      (triggerresultshelper_HLT_PFHT700_v3)||
 	      (triggerresultshelper_HLT_PFHT700_v4)||
 	      (triggerresultshelper_HLT_PFHT700_v5)||
 	      (triggerresultshelper_HLT_PFHT700_v6)||
+	      (triggerresultshelper_HLT_PFHT700_v7)||
+	      (triggerresultshelper_HLT_PFHT700_v8)||
+	      (triggerresultshelper_HLT_PFHT700_v9)||
 	      (triggerresultshelper_HLT_PFHT750_v1)||
 	      (triggerresultshelper_HLT_PFHT750_v2)||
 	      (triggerresultshelper_HLT_PFHT750_v3)||
 	      (triggerresultshelper_HLT_PFHT750_v4)||
 	      (triggerresultshelper_HLT_PFHT750_v5)||
 	      (triggerresultshelper_HLT_PFHT750_v6)||
+	      (triggerresultshelper_HLT_PFHT750_v7)||
+	      (triggerresultshelper_HLT_PFHT750_v8)||
+	      (triggerresultshelper_HLT_PFHT750_v9)||
 	      (triggerresultshelper_HLT_HT650_v1)||
 	      (triggerresultshelper_HLT_HT650_v2)||
 	      (triggerresultshelper_HLT_HT650_v3)||
+	      (triggerresultshelper_HLT_HT650_v4)||
+	      (triggerresultshelper_HLT_HT650_v5)||
+	      (triggerresultshelper_HLT_HT650_v6)||
+	      (triggerresultshelper_HLT_HT650_v7)||
+	      (triggerresultshelper_HLT_HT650_v8)||
+	      (triggerresultshelper_HLT_HT650_v9)||
 	      (triggerresultshelper_HLT_HT750_v1)||
 	      (triggerresultshelper_HLT_HT750_v2)||
-	      (triggerresultshelper_HLT_HT750_v3))
+	      (triggerresultshelper_HLT_HT750_v3)||
+	      (triggerresultshelper_HLT_HT750_v4)||
+	      (triggerresultshelper_HLT_HT750_v5)||
+	      (triggerresultshelper_HLT_HT750_v6)||
+	      (triggerresultshelper_HLT_HT750_v7)||
+	      (triggerresultshelper_HLT_HT750_v8)||
+	      (triggerresultshelper_HLT_HT750_v9))
 	    )) continue;
 	   
 	  // ---------------------
@@ -305,12 +418,22 @@ int main(int argc, char** argv)
           for ( size_t j = 0; j < (massBins.size()-1); ++j )
           {
               if((eventhelperextra_dijet_invmass>=massBins[j])&&
-	         (eventhelperextra_dijet_invmass<massBins[j+1])&&
-		 (fabs(jethelper2_rapidity[0]+jethelper2_rapidity[1])<1.11))
+	         (eventhelperextra_dijet_invmass<massBins[j+1]))
+              {
                   hists[j]->Fill(exp(fabs(jethelper2_rapidity[0]-jethelper2_rapidity[1])), weight);
+                  histspt1[j]->Fill(jethelper2_pt[0], weight);
+                  histspt2[j]->Fill(jethelper2_pt[1], weight);
+                  histsy1[j]->Fill(jethelper2_rapidity[0], weight);
+                  histsy2[j]->Fill(jethelper2_rapidity[1], weight);
+                  histsyboost[j]->Fill(fabs(jethelper2_rapidity[0]+jethelper2_rapidity[1])/2., weight);
+                  histsmetsumet[j]->Fill(met2_et/met2_sumEt, weight);
+                  histsdphi[j]->Fill(reco::deltaPhi(jethelper2_phi[0],jethelper2_phi[1]));
+              }
           }
   
 	}
+
+  std::cout << "done" << std::endl;
 
   h1->Write();
   h1->Draw("histe");
@@ -320,12 +443,18 @@ int main(int argc, char** argv)
   for ( size_t j = 0; j < (massBins.size()-1); ++j )
   {
       hists[j]->Write();
+      histspt1[j]->Write();
+      histspt2[j]->Write();
+      histsy1[j]->Write();
+      histsy2[j]->Write();
+      histsyboost[j]->Write();
+      histsmetsumet[j]->Write();
+      histsdphi[j]->Write();
       hists[j]->Draw("histe");
       std::stringstream name;
       name << cmdline.outputfilename.substr(0,cmdline.outputfilename.size()-5) << "_" << massBins[j] << "_" << massBins[j+1] << "_" << "chi.pdf";
       gPad->SetLogy(false);
       c1.SaveAs(name.str().c_str());
-      hists[j]->Delete();
   }
 
   stream.close();
--- UserArea/TNM/Ntuples/TNMc1/cpsamples.py
+++ UserArea/TNM/Ntuples/TNMc1/cpsamples.py
@@ -29,8 +29,12 @@ dataset_names = [
 #'/QstarToJJ_M-3500_TuneZ2star_8TeV-pythia6/Summer12-PU_S7_START52_V9-v2/AODSIM/V5/PAT_CMG_V5_4_0/NTUPLE2',
 #'/QstarToJJ_M-4500_TuneZ2star_8TeV-pythia6/Summer12-PU_S7_START52_V9-v2/AODSIM/V5/PAT_CMG_V5_4_0/NTUPLE2',
 
+'/HT/Run2012A-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_190605-194076/NTUPLE3',
+'/JetHT/Run2012B-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_start-194479/NTUPLE3',
+'/JetHT/Run2012B-PromptReco-v1/AOD/PAT_CMG_V5_4_0_runrange_194480-195016/NTUPLE3',
+'/JetHT/Run2012B-PromptReco-v1/AOD/PAT_CMG_V5_4_0_runrange_195017-196531/NTUPLE3',
 #'/Jet/Run2012A-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_190605-194076/NTUPLE2',
-'/HT/Run2012A-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_190605-194076/NTUPLE2',
+#'/HT/Run2012A-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_190605-194076/NTUPLE2',
 #'/JetHT/Run2012B-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_start-194479/NTUPLE2',
 #'/JetHT/Run2012B-PromptReco-v1/AOD/PAT_CMG_V5_4_0_runrange_194480-195016/NTUPLE2',
 #'/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/Summer12-PU_S7_START52_V9-v5/AODSIM/V5/PAT_CMG_V5_4_0/NTUPLE2',
@@ -49,7 +53,7 @@ for dataset_name in dataset_names:
     print source.fileNames
     directory="/tmp/hinzmann/"+dataset_name.replace("/","_")
     os.system("mkdir "+directory)
-    filelist=open("fileList"+dataset_name.replace("/","_")+".txt","w")
+    filelist=open("fileList"+dataset_name.replace("/","_")+"_local.txt","w")
     for filename in source.fileNames:
         filelist.write(directory+"/"+filename.split("/")[-1]+"\n")
         print "cmsStage "+filename+" "+directory
