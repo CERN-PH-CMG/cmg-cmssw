3b39aa4bce44d72d11673fe500ba712b61163d8f
bachtis <>
2012-08-27 23:19:47 +0000

    *** empty log message ***

--- CMGTools/HToZZTo4Leptons/scripts/hzz4LPack.py
+++ CMGTools/HToZZTo4Leptons/scripts/hzz4LPack.py
@@ -33,9 +33,15 @@ if __name__ == '__main__':
     directory=args[0]
     
     #First unpack tyhe counter and get the events
-    counterFile=open(directory+'/'+options.trigcounter)
-    counter=pickle.load(counterFile)
-    events=counter[0][1]
+    if os.path.exists(directory+'/'+options.trigcounter):
+        counterFile=open(directory+'/'+options.trigcounter)
+        counter=pickle.load(counterFile)
+        events=counter[0][1]
+    else:
+        print 'problem in file/ cannot count events in ',directory
+        print 'If is data you can ignore'
+        events=-1
+        
 
     output['events'] = events
     output['weight'] = options.sigma/float(events)
