97e330ea7a139a4f42b09ed201bf0433b8e6081a
hinzmann <>
2012-09-17 13:48:05 +0000

    add 2012C data

--- CMGTools/H2TauTau/python/proto/samples/run2012/triggers_diTau.py
+++ CMGTools/H2TauTau/python/proto/samples/run2012/triggers_diTau.py
@@ -16,6 +16,13 @@ data_triggers_2012B = [
     'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
     ]
 
+data_triggers_2012C = [
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v1',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v3',
+    'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v4',
+    ]
+
 mc_triggers_spring12 = [
     #'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v1',
     #'HLT_DoubleMediumIsoPFTau30_Trk5_eta2p1_Jet30_v2',
