e53c04bc1c08388d4a4de06df9384a714fa5c9e5
cbern <>
2012-09-21 15:31:48 +0000

    simplified json handling.

---
+++ H2TauTau/python/officialJSONS.py
@@ -0,0 +1,64 @@
+
+may10 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt'
+aug05 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt'
+pr2011 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt'
+
+pr2012AB = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196531_8TeV_PromptReco_Collisions12_JSON.txt'
+pr2012C = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-199754_8TeV_PromptReco_Collisions12_JSON.txt'
+
+jsonMap = {
+    #
+    # 2011A 10May
+    # 
+    '/TauPlusX/Run2011A-May10ReReco-v1/AOD':may10,	
+    '/Tau/Run2011A-May10ReReco-v1/AOD':may10,	        
+    '/DoubleMu/StoreResults-DoubleMu_2011A_10May2011_v1_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':may10,    
+    '/DoubleMu/StoreResults-DoubleMu_2011A_May10thRR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':may10,    
+    '/DoubleMu/StoreResults-DoubleMu_2011A_10May2011_v1_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':may10,
+    #
+    # 2011A PR V4
+    #
+    '/TauPlusX/Run2011A-PromptReco-v4/AOD':pr2011,
+    '/Tau/Run2011A-PromptReco-v4/AOD':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_PR_v4_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_PR_v4_embedded_trans1_tau116_ptmu1_13had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_PR_v4_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    #
+    # 2011A 05Aug
+    # 
+    '/TauPlusX/Run2011A-05Aug2011-v1/AOD':aug05,
+    '/Tau/Run2011A-05Aug2011-v1/AOD':aug05,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_05Aug2011_v1_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':aug05,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_Aug05thRR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':aug05,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_05Aug2011_v1_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':aug05,
+    #
+    # 2011A 03Oct (PR V6)
+    #
+    '/TauPlusX/Run2011A-03Oct2011-v1/AOD':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_03Oct2011_v1_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_03Oct2011_v1_embedded_trans1_tau116_ptmu1_13had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011A_03Oct2011_v1_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    #
+    # 2011B PR V1
+    # 
+    '/TauPlusX/Run2011B-PromptReco-v1/AOD':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    # 
+    # 2012A & B
+    #
+    '/TauPlusX/Run2012A-PromptReco-v1/AOD':pr2012AB,
+    '/TauPlusX/Run2012B-PromptReco-v1/AOD':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012A_PromptReco_v1_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run193752to195135_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run195147to196070_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012A_PromptReco_v1_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run193752to195135_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run195147to196070_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    #
+    # 2012C
+    #
+    '/TauPlusX/Run2012C-PromptReco-v1/AOD':pr2012C,
+    '/TauPlusX/Run2012C-PromptReco-v2/AOD':pr2012C,
+    }
--- H2TauTau/python/tools/jsonPickOld.py
+++ H2TauTau/python/tools/jsonPickOld.py
@@ -1,7 +1,10 @@
 import os
 import re 
 
-from CMGTools.H2TauTau.officialJSONS import jsonMap
+# dirReprocessing = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing'
+# dirPrompt = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt'
+defaultDict = '/'.join( [ os.environ['CMSSW_BASE'],
+                          'src/CMGTools/H2TauTau/json/officialJSONS.txt' ] )
 
 def lfnToDataset( lfn ):
     '''If lfn contains A/CMG/B, returns /B. Otherwise, returns lfn.'''
@@ -13,16 +16,30 @@ def lfnToDataset( lfn ):
         return dataset
     return lfn
 
-def jsonPick( dataset ):
+def jsonPick( dataset, samplesAndJSONs=None):
 
     dataset = lfnToDataset(dataset)
-
+    # find dictionary, if not provided by user
+    if samplesAndJSONs is None:
+        samplesAndJSONs = defaultDict 
+    # reading dictionary
+    jsonDictFile = open( samplesAndJSONs )
+    jsons = {}
+    for line in jsonDictFile:
+        try:
+            sample, json = line.split()
+        except ValueError:
+            continue
+        jsons[sample] = json
+    # dir = dirReprocessing
+    # if 'Prompt' in dataset:
+    #     dir = dirPrompt
     # stripping out the last part of the dataset name
     # to keep only the base official dataset name
     dsfields = dataset.lstrip('/').split('/')[0:3]
     # print dsfields
     baseDataSet = '/'+'/'.join( dsfields )
-    jsonFile = jsonMap[ baseDataSet ]
+    jsonFile = jsons[ baseDataSet ]
     jsonAbsPath = jsonFile
     if not os.path.isfile(jsonAbsPath):
         raise ValueError( ' '.join([jsonAbsPath,
@@ -30,7 +47,10 @@ def jsonPick( dataset ):
     return jsonAbsPath
 
 if __name__ == '__main__':
-
+    print 'reading from:'
+    os.system('cat ' + defaultDict)
+    print
+    print 
     samples = ['/TauPlusX/Run2011A-May10ReReco-v1/AOD/foo/bar',
                '/TauPlusX/Run2011B-PromptReco-v1/AOD/blah',
                '/store/cmst3/user/cmgtools/CMG/TauPlusX/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0/tree_CMG_648.root',
