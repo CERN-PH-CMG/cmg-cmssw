1efe39445169c3e68adaac8cdd12b6bc01401990
bachtis <>
2012-09-22 11:05:16 +0000

    Cleanup

--- HToZZTo4Leptons/src/classes.h
+++ HToZZTo4Leptons/src/classes.h
@@ -1,10 +1,9 @@
 #include "DataFormats/Common/interface/Wrapper.h"
-# include "CMGTools/HToZZTo4Leptons/interface/Mela.h"
+
 
 
 namespace {
   struct CMGTools_HToZZTo4Leptons {
-    Mela mela;
 
   };
 }
