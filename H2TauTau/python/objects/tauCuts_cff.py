e7c6551fe9555e9bc805be220abe4e8b41c3aed9
govoni <>
2012-09-25 14:17:44 +0000

    put analysis cuts in the skim for the tau ele case, to speed up the skimming

--- H2TauTau/python/objects/tauCuts_cff.py
+++ H2TauTau/python/objects/tauCuts_cff.py
@@ -11,11 +11,22 @@ def getTauCuts( leg, channel='tauMu'):
         pt = cms.string('{leg}().pt()>{ptCut}'.format(leg=leg, ptCut=ptCut)),
         eta = cms.string('abs({leg}().eta())<{etaCut}'.format(leg=leg, etaCut=etaCut))
         )
-    id = cms.PSet(
-        decay = cms.string('{leg}().tauID("decayModeFinding")'.format(leg=leg)),
-        )
     iso = cms.string('{leg}().tauID("byRawIsoMVA") > 0.5'.format(leg=leg))
-    
+    if channel == 'tauMu':
+        id = cms.PSet(
+            decay = cms.string('{leg}().tauID("decayModeFinding")'.format(leg=leg)),
+            )
+    elif channel == 'tauEle':    
+        id = cms.PSet(
+            decay = cms.string('{leg}().tauID("decayModeFinding")'.format(leg=leg)),
+            eleRejection1 = cms.string('{leg}().tauID("againstElectronMVA") > 0.5'.format(leg=leg)),
+            eleRejection2 = cms.string('{leg}().tauID("againstElectronMedium") > 0.5'.format(leg=leg)),
+            muRejection2 = cms.string('{leg}().tauID("againstMuonLoose") > 0.5'.format(leg=leg))
+            )
+    else :
+        id = cms.PSet(
+            decay = cms.string('{leg}().tauID("decayModeFinding")'.format(leg=leg)),
+            )
 
     tauCuts = cms.PSet(
         kinematics = kinematics.clone(),
