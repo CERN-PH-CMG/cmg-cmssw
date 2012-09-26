26a539dc9192bbd3b5fc70f21624ddc64f5969b0
bachtis <>
2012-09-26 23:06:51 +0000

    *** empty log message ***

---
+++ CMGTools/HToZZTo4Leptons/python/samples/ComponentCreator.py
@@ -0,0 +1,52 @@
+import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.Production.datasetToSource import datasetToSource
+from CMGTools.Production.datasetInformation import DatasetInformation
+
+class ComponentCreator(object):
+    def makeMCComponent(self,name,dataset,user,pattern):
+        
+         component = cfg.MCComponent(
+             name = name,
+             files = self.getFiles(dataset,user,pattern),
+             xSection = 1,
+             nGenEvents = 1,
+             triggers = [],
+             effCorrFactor = 1,
+#             skimEfficiency = self.getSkimEfficiency(dataset,user)
+         )
+
+ #        print 'Skim Efficiency for ',name,'=', component.skimEfficiency
+         return component
+
+
+    def makeDataComponent(self,name,datasets,json,user,pattern):
+         files=[]
+
+         for dataset in datasets:
+             files=files+self.getFiles(dataset,user,pattern)
+        
+         component = cfgDataComponent(
+             name = name,
+             files = files,
+             intLumi=1,
+             triggers = [],
+             json=json
+         )
+
+         return component
+
+
+    def getFiles(self,dataset, user, pattern):
+        # print 'getting files for', dataset,user,pattern
+        ds = datasetToSource( user, dataset, pattern, True )
+        files = ds.fileNames
+        return ['root://eoscms//eos/cms%s' % f for f in files]
+
+
+    def getSkimEfficiency(self,dataset,user):
+       info=DatasetInformation(dataset,user,'',False,False,'','','')
+       fraction=info.dataset_details['PrimaryDatasetFraction']
+       if fraction<0.001:
+           print 'ERROR FRACTION IS ONLY ',fraction
+       return fraction    
+       
