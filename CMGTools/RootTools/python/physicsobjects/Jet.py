2c775314785ba1a42a6dec8f44dd5f5d27888c09
cbern <>
2012-10-03 17:45:10 +0000

    reverted PU jet ID to old WP

--- CMGTools/RootTools/python/physicsobjects/Jet.py
+++ CMGTools/RootTools/python/physicsobjects/Jet.py
@@ -1,11 +1,29 @@
 from CMGTools.RootTools.physicsobjects.PhysicsObject import *
 
+loose_WP = [
+    (0, 2.5, -0.8),
+    (2.5, 2.75, -0.74),
+    (2.75, 3.0, -0.68),
+    (3.0, 5.0, -0.77),
+    ]
+
+
 class Jet( PhysicsObject):
+
     def looseJetId(self):
-        '''This jet id is slightly different from the one present in the cmg::PFJet on May 28.
-        no cut on the mu fraction.
+        '''PF Jet ID
         '''
         return self.getSelection('cuts_looseJetId') 
+
+    def puJetId(self):
+        '''Full mva PU jet id with old loose working point used by Josh'''
+        puMva = self.puMva('full')
+        eta = self.eta()
+        for etamin, etamax, cut in loose_WP:
+            if eta>=etamin and eta<etamax: continue
+            else: return puMva>cut
+        
+
 
 class GenJet( PhysicsObject):
     pass
