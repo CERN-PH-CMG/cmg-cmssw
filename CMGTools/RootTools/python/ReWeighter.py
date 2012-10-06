eb5385437268da6bb3a9e7eb3199bf3416d930f5
cbern <>
2012-10-06 14:26:37 +0000

    added a tree 1D reweighter

---
+++ CMGTools/RootTools/python/ReWeighter.py
@@ -0,0 +1,20 @@
+class ReWeighter(object):
+
+    def __init__(self, name, hto, hfrom):
+        self.hto = hto
+        self.hfrom = hfrom
+        self.hto.Scale(1/hto.Integral())
+        self.hfrom.Scale(1/hfrom.Integral())
+        self.weights = self.hto.Clone( '_'.join(['weights',name]))
+        self.weights.Divide(hfrom)
+        
+    def weightStr(self, var):
+        cuts = []
+        for i in range(1, self.weights.GetNbinsX()+1):
+            xmin = self.weights.GetBinLowEdge(i)
+            xmax = self.weights.GetBinLowEdge(i) + self.weights.GetBinWidth(i)
+            weight = self.weights.GetBinContent(i)
+            print i, xmin, xmax, weight
+            cut = '({var}>={xmin} && {var}<{xmax})*{weight}'.format(var=var, xmin=xmin, xmax=xmax, weight=weight)
+            cuts.append(cut)
+        return ' + '.join(cuts)
