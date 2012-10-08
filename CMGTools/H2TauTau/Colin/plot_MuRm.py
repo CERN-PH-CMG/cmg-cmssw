cd1d8d322aaf5f7ac409f47e4c03326eeb605e2e
cbern <>
2012-10-08 10:56:46 +0000

    improvements

--- CMGTools/H2TauTau/Colin/plot_MuRm.py
+++ CMGTools/H2TauTau/Colin/plot_MuRm.py
@@ -7,17 +7,20 @@ if __name__ == '__main__':
     import pprint
     import shelve
     
-    a1,p1 = sys.argv[1].split(':')
-    a2,p2 = sys.argv[2].split(':')
+    p1 = sys.argv[1]
+    p2 = sys.argv[2]
+    a1 = p1.split('/')[0]
+    a2 = p2.split('/')[0]
     patterns = [ (a1, p1), (a2, p2) ]
     trees = getTrees('H2TauTauTreeProducerTauMu', patterns)
     pprint.pprint(trees)
+    trees[a1].SetTitle( a1 )
+    trees[a2].SetTitle( a2 )
 
-    # cut = cat_Inc_RlxTauIso + '&& nJets20>0'
-    cut = cat_Inc_RlxTauIso
+    cut = cat_Inc.replace('l1_looseMvaIso>0.5', 'l1_rawMvaIso>-0.7')
 
-    if 1:
-        h1, h2 = draw('mt',cat_Inc, trees['t1'], trees['t2'])
+    if 0:
+        h1, h2 = draw('mt', cut, trees[a1], trees[a2])
         mtrw = ReWeighter('mt', h1, h2)
         weightFile = shelve.open('weights.txt')
         weightFile['weights'] = mtrw
@@ -25,8 +28,10 @@ if __name__ == '__main__':
     else:
         weightFile = shelve.open('weights.txt')
         mtrw = weightFile['weights']
-        h1, h2 = draw('mt', '({cut})*({weight})'.format(cut=cut, weight=mtrw.weightStr('mt')),
-                      trees['t1'], trees['t2'])
+        wei = mtrw.weightStr('mt')
+        h1, h2 = draw('mt', cut,  
+                      trees[a1], trees[a2],
+                      w1=wei, w2=wei, normalize=False)
         
     
     
