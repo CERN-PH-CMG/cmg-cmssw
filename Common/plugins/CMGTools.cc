cceb9b5100ac3af75e535a6a91c4ce4a0f89a28f
cbern <>
2012-10-08 12:06:34 +0000

    deltar veto producers cleanup

--- Common/plugins/CMGTools.cc
+++ Common/plugins/CMGTools.cc
@@ -76,6 +76,11 @@ DEFINE_FWK_MODULE(CmgDiMuonDiElectronSelector);
 DEFINE_FWK_MODULE(CmgQuadElectronSelector);
 // DEFINE_FWK_MODULE(CmgTriMuonMETSelector);
 
+DEFINE_FWK_MODULE(DeltaRVetoProducerBaseJet);
+DEFINE_FWK_MODULE(DeltaRVetoProducerPFJet);
+DEFINE_FWK_MODULE(DeltaRVetoProducerElectron);
+DEFINE_FWK_MODULE(DeltaRVetoProducerMuon);
+DEFINE_FWK_MODULE(DeltaRVetoProducerTau);
 
 DEFINE_FWK_MODULE(GenericBaseMETHistograms);
 DEFINE_FWK_MODULE(GenericPOHistograms);
