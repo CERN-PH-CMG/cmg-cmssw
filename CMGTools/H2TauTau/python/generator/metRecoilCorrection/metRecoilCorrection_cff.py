93039e2aea600b3b0b975eb447148ea1944ef425
govoni <>
2012-10-09 11:53:31 +0000

    first commit

---
+++ CMGTools/H2TauTau/python/generator/metRecoilCorrection/metRecoilCorrection_cff.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+from CMGTools.H2TauTau.generator.metRecoilCorrection.recoilCorrectedMETTauMu_cfi import *
+from CMGTools.H2TauTau.generator.metRecoilCorrection.recoilCorrectedMETTauEle_cfi import *
+from CMGTools.H2TauTau.generator.metRecoilCorrection.recoilCorrectedMETMuEle_cfi import *
+from CMGTools.H2TauTau.generator.metRecoilCorrection.recoilCorrectedMETDiTau_cfi import *
+from CMGTools.Common.generator.genWorZ_cfi import *
+from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
+
+from CMGTools.Common.generator.metRecoilCorrection.metRecoilCorrection_cff import cmgPFJetForRecoil, metRecoilCorrectionInputSequence
+
+metRecoilCorrectionSequence2012 = cms.Sequence(
+    metRecoilCorrectionInputSequence +
+    recoilCorrectedMETTauMu2012 + 
+    recoilCorrectedMETTauEle2012 +
+    recoilCorrectedMETMuEle2012
+    # Recoil corrections for DiTau case not studied at all yet
+    #recoilCorrectedMETDiTau2012
+    )
