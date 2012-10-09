2dd6d9c3e19869a15470210aaa385857acb89047
govoni <>
2012-10-09 11:18:50 +0000

    first commit

--- Common/python/generator/metRecoilCorrection/recoilCorrectedMETTauMu_cfi.py
+++ Common/python/generator/metRecoilCorrection/recoilCorrectedMETTauMu_cfi.py
@@ -8,10 +8,10 @@ as we need to add the momentum of both leptons to the recoil to get the MET.
 import FWCore.ParameterSet.Config as cms
 
 import os 
-rootfile_dir = os.environ['CMSSW_BASE'] + '/src/CMGTools/Common/data/metRecoilCorrection/'
+rootfile_dir = os.environ['CMSSW_BASE'] + '/src/CMGTools/H2TauTau/data/metRecoilCorrection/'
 
-recoilCorrectedMETTauMu = cms.EDProducer(
-    "RecoilCorrectedMETProducerTauMu",
+recoilCorrectedMETTauMu2012 = cms.EDProducer(
+    "RecoilCorrectedMETProducer2012TauMu",
     metSrc = cms.InputTag('cmgPFMET'),
     # the tau is on the first leg and the muon on the second leg
     recBosonSrc = cms.InputTag('cmgTauMuSel'),
@@ -22,11 +22,10 @@ recoilCorrectedMETTauMu = cms.EDProducer(
     leptonLeg = cms.int32(2),
     # 1: type 1; 2 : type 2; 0 : all (use 1)
     correctionType = cms.int32(1),
-    # fileCorrectTo = cms.string(rootfile_dir + 'recoilfit_zjets_ltau_njet.root'),
-    fileCorrectTo = cms.string(rootfile_dir + 'recoilfit_wjets_njet.root'),
+    fileCorrectTo = cms.string(rootfile_dir + 'recoilfit_wjets53X_20pv_njet.root'),
     # you should not have to change the files below
-    fileZmmData = cms.string(rootfile_dir + 'recoilfit_datamm_njet.root'),
-    fileZmmMC = cms.string(rootfile_dir + 'recoilfit_zmm42X_njet.root'),
+    fileZmmData = cms.string(rootfile_dir + 'recoilfit_datamm53X_20pv_njet.root'),
+    fileZmmMC = cms.string(rootfile_dir + 'recoilfit_zmm53X_20pv_njet.root'),
     enable = cms.bool(True),
     verbose = cms.untracked.bool( False )
     #COLIN: make delta R a parameter
