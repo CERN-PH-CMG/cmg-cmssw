cf64da0391ea2974a23cb99b6a1fbb3b8630b6fa
cbern <>
2012-10-10 09:51:41 +0000

    extending json picking tools, added dcs jsons

--- H2TauTau/python/tools/jsonPick.py
+++ H2TauTau/python/tools/jsonPick.py
@@ -2,6 +2,7 @@ import os
 import re 
 
 from CMGTools.H2TauTau.officialJSONS import jsonMap
+from CMGTools.H2TauTau.dcsJSONS import dcsJsonMap
 
 def lfnToDataset( lfn ):
     '''If lfn contains A/CMG/B, returns /B. Otherwise, returns lfn.'''
@@ -13,7 +14,7 @@ def lfnToDataset( lfn ):
         return dataset
     return lfn
 
-def jsonPick( dataset ):
+def jsonPick( dataset, useDCS=False):
 
     dataset = lfnToDataset(dataset)
 
@@ -22,7 +23,25 @@ def jsonPick( dataset ):
     dsfields = dataset.lstrip('/').split('/')[0:3]
     # print dsfields
     baseDataSet = '/'+'/'.join( dsfields )
-    jsonFile = jsonMap[ baseDataSet ]
+
+    jmap = jsonMap
+    if useDCS:
+        jmap = dcsJsonMap
+        
+    # jsonFile = jmap[ baseDataSet ]
+    jsonFiles = []
+    jsonFile = None
+    for key, value in  jmap.iteritems():
+        pat = re.compile(key)
+        if pat.match(baseDataSet):
+            jsonFiles.append(value)
+    if len(jsonFiles)==0:
+        raise ValueError('No json file found')
+    elif len(jsonFiles)>1:
+        raise ValueError('Too many json files found')
+    else:
+        jsonFile = jsonFiles[0]
+            
     jsonAbsPath = jsonFile
     if not os.path.isfile(jsonAbsPath):
         raise ValueError( ' '.join([jsonAbsPath,
@@ -33,12 +52,14 @@ if __name__ == '__main__':
 
     samples = ['/TauPlusX/Run2011A-May10ReReco-v1/AOD/foo/bar',
                '/TauPlusX/Run2011B-PromptReco-v1/AOD/blah',
+               '/TauPlusX/Run2012B-PromptReco-v1/AOD/blah',
                '/store/cmst3/user/cmgtools/CMG/TauPlusX/Run2011A-PromptReco-v4/AOD/V2/PAT_CMG_V2_4_0/tree_CMG_648.root',
                'should_fail_for_this_sample_name']
 
     for sample in samples:
         print 'Sample', sample
-        print '\tJSON =', jsonPick( sample )
+        print '\tJSON (DCS)     =', jsonPick( sample, True)
+        print '\tJSON (cert)=', jsonPick( sample, False)
 
 
     
