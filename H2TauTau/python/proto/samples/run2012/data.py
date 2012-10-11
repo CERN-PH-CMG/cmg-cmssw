3e938e2529c63c32c5d3eee82b6d06f147f29735
cbern <>
2012-10-11 14:02:56 +0000

    updated lumis, using my own measurements

--- H2TauTau/python/proto/samples/run2012/data.py
+++ H2TauTau/python/proto/samples/run2012/data.py
@@ -8,7 +8,7 @@ data_Run2012A = cfg.DataComponent(
     # | HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v4 |       2809 |  96.404(/pb) |  96.404(/pb) |
     # | HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v5 |       5258 | 316.016(/pb) | 316.016(/pb) |
     # | HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v6 |       6514 | 395.991(/pb) | 395.991(/pb) |
-    intLumi = 96.404+316.016+ 395.991, # 806.194 from Colin
+    intLumi = 806.194, # 806.194 from Colin
     triggers = [],
     json = None
     )
@@ -26,7 +26,7 @@ data_Run2012B = cfg.DataComponent(
     name = 'data_Run2012B',
     files = [],
     # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2 |      51089 | 4.428(/fb) | 4.428(/fb) |
-    intLumi = 4428., # 4420. from Colin
+    intLumi = 4420., # 4420. from Colin
     triggers = [],
     json = None
     )
@@ -35,7 +35,7 @@ data_Run2012C_v1 = cfg.DataComponent(
     name = 'data_Run2012C_v1',
     files = [],
     # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v3 |       6719 | 495.003(/pb) | 495.003(/pb) |
-    intLumi = 495.003, # 493.975 from Colin
+    intLumi = 493.975, # 493.975 from Colin
     triggers = [],
     json = None
     )
@@ -43,12 +43,7 @@ data_Run2012C_v1 = cfg.DataComponent(
 data_Run2012C_v2 = cfg.DataComponent( # update
     name = 'data_Run2012C_v2',
     files = [],
-    # what is the v2? 
-    # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v2 |      49752 |   4.252(/fb) |   4.252(/fb) |
-    # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v3 |      17991 |   1.464(/fb) |   1.464(/fb) |
-    # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v6 |      46975 |   4.358(/fb) |   4.358(/fb) |
-    # | HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v7 |       3425 | 281.155(/pb) | 281.155(/pb) |
-    intLumi = 1464 + 4358 + 281.155, #  5720.0 from Colin
+    intLumi = 6397, #  from Colin
     triggers = [],
     json = None
     )
