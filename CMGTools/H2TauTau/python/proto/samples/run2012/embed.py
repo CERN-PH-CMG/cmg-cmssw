a6ad320267e27966ace26cb303b5a49f84a0fa4a
govoni <>
2012-10-15 08:37:46 +0000

    added json

--- CMGTools/H2TauTau/python/proto/samples/run2012/embed.py
+++ CMGTools/H2TauTau/python/proto/samples/run2012/embed.py
@@ -5,15 +5,13 @@ from   CMGTools.H2TauTau.proto.samples.getFiles import getFiles
 
 vertexFileDir = os.environ['CMSSW_BASE'] + '/src/CMGTools/RootTools/data/Reweight/2012/Vertices'
 
-json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196509_8TeV_PromptReco_Collisions12_JSON.txt'
-
 
 embed_Run2012A_13Jul2012_v1 = cfg.EmbedComponent(
     name = 'embed_Run2012A_13Jul2012_v1',
     files = [],
     intLumi = 1.0, # dummy
     triggers = [],
-    json = json,
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON.txt'
     )
 
 embed_Run2012A_recover_06Aug2012_v1 = cfg.EmbedComponent(
@@ -21,7 +19,7 @@ embed_Run2012A_recover_06Aug2012_v1 = cfg.EmbedComponent(
     files = [],
     intLumi = 1.0, # dummy
     triggers = [],
-    json = json,
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'
     )
 
 embed_Run2012B_13Jul2012_v4 = cfg.EmbedComponent(
@@ -29,7 +27,7 @@ embed_Run2012B_13Jul2012_v4 = cfg.EmbedComponent(
     files = [],
     intLumi = 1.0, # dummy
     triggers = [],
-    json = json,
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON.txt' 
     )
     
 embed_Run2012C_24Aug2012_v1 = cfg.EmbedComponent(
@@ -37,7 +35,7 @@ embed_Run2012C_24Aug2012_v1 = cfg.EmbedComponent(
     files = [],
     intLumi = 1.0, # dummy
     triggers = [],
-    json = json,
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'
     )
     
 embed_Run2012C_PromptReco_v2 = cfg.EmbedComponent(
@@ -45,7 +43,7 @@ embed_Run2012C_PromptReco_v2 = cfg.EmbedComponent(
     files = [],
     intLumi = 1.0, # dummy
     triggers = [],
-    json = json,
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON.txt'
     )
     
     
