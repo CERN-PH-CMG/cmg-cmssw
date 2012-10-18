c336e18fdf736b4532872b454d2937781d7626f1
govoni <>
2012-10-18 15:53:07 +0000

    simplify the code

--- CMGTools/H2TauTau/python/proto/plotter/plot_H2TauTauDataMC_TauEle_Inclusive.py
+++ CMGTools/H2TauTau/python/proto/plotter/plot_H2TauTauDataMC_TauEle_Inclusive.py
@@ -29,23 +29,6 @@ XMIN  = 0
 XMAX  = 200
 
 
-def plotPurity (orig, num, den, plotname):
-    local = copy.deepcopy(orig)
-    h_num = local.Hist(num).weighted
-    h_den = local.Hist(den).weighted
-    h_num.Divide(h_den)
-    can0 = TCanvas ('can_'+plotname,'',100,100,600,600)
-    bkg = can0.DrawFrame(h_num.GetXaxis().GetXmin(),0,h_num.GetXaxis().GetXmax(),1.2)
-    bkg.GetXaxis().SetTitle(orig.histName)
-    h_num.SetFillColor(2)
-    h_num.Draw('histsame')
-    h_num.Draw('same')
-    can0.Print(plotname+'.png','png')
-
-
-# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
-
-
 def replaceShapeInclusive(plot, var, anaDir,
                           comp, weights, 
                           cut, weight,
@@ -77,7 +60,7 @@ def replaceShapeInclusive(plot, var, anaDir,
 def makePlot( var, anaDir, selComps, weights, wJetScaleSS, wJetScaleOS,
               nbins=None, xmin=None, xmax=None,
               cut='', weight='weight', embed=False, shift=None, replaceW=False,
-              useExclusiveVV=True):
+              VVgroup = None):
     
     print 'making the plot:', var, 'cut', cut
 
@@ -87,6 +70,9 @@ def makePlot( var, anaDir, selComps, weights, wJetScaleSS, wJetScaleOS,
                            cut=oscut, weight=weight,
                            embed=embed, shift=shift, treeName = 'H2TauTauTreeProducerTauEle')
     osign.Hist(EWK).Scale( wJetScaleOS )
+    if VVgroup != None:
+         osign.Group('VV', cfg.VVgroup)
+         
     if replaceW:
         osign = replaceShapeInclusive(osign, var, anaDir,
                                       selComps['WJets'], weights, 
@@ -98,6 +84,8 @@ def makePlot( var, anaDir, selComps, weights, wJetScaleSS, wJetScaleOS,
                            cut=sscut, weight=weight,
                            embed=embed, shift=shift, treeName = 'H2TauTauTreeProducerTauEle')
     ssign.Hist(EWK).Scale( wJetScaleSS ) 
+    if VVgroup != None:
+         ssign.Group('VV', cfg.VVgroup)
     # import pdb; pdb.set_trace()
     if replaceW:
         ssign = replaceShapeInclusive(ssign, var, anaDir,
@@ -105,26 +93,8 @@ def makePlot( var, anaDir, selComps, weights, wJetScaleSS, wJetScaleOS,
                                       sscut, weight,
                                       embed, shift=shift)
 
-    ssQCD, osQCD = getQCD( ssign, osign, 'Data', 1.10 ) #PG scale value according to the note
+    ssQCD, osQCD = getQCD( ssign, osign, 'Data', 1.06 ) #PG scale value according Jose, 18/10
         
-    plotPurity (ssQCD,'QCD','Data','SS_QCD_purity')
-    plotPurity (ssQCD,'WJets','Data','SS_WJets_purity')
-
-#    if 0:
-#        qcd_yield = osQCD.Hist('QCD').Integral()
-#        
-#        sscut_qcdshape = cut.replace('l2_relIso05<0.1','l2_relIso05>0.2').replace('l1_looseMvaIso>0.5', 'l1_rawMvaIso>-0.75') + ' && diTau_charge!=0' 
-#        ssign_qcdshape = H2TauTauDataMC(var, anaDir,
-#                                        selComps, weights, nbins, xmin, xmax,
-#                                        cut=sscut_qcdshape, weight=weight,
-#                                        embed=embed, shift=shift, treeName = 'H2TauTauTreeProducerTauEle')
-#        qcd_shape = copy.deepcopy( ssign_qcdshape.Hist('Data') )    
-#        qcd_shape.Normalize()
-#        qcd_shape.Scale(qcd_yield)
-#        # qcd_shape.Scale( qcd_yield )
-#        osQCD.Replace('QCD', qcd_shape)
-
-    osQCD.Group('VV', cfg.VVgroup)
     osQCD.Group('EWK', ['WJets', 'Ztt_ZJ','VV'])
     osQCD.Group('Higgs 125', ['HiggsVBF125', 'HiggsGGH125', 'HiggsVH125'])
     return ssign, osign, ssQCD, osQCD
@@ -133,23 +103,27 @@ def makePlot( var, anaDir, selComps, weights, wJetScaleSS, wJetScaleOS,
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 
-def drawAll(cut, plots, embed, selComps, weights, fwss, fwos,useExcusiveVV):
+def drawAll(cut, plots, embed, selComps, weights, fwss, fwos, VVgroup = None):
     '''See plotinfo for more information'''
     for plot in plots.values():
-        print plot.var
+        print 'PLOTTING',plot.var
         ss, os, ssQ, osQ = makePlot( plot.var, anaDir,
                                      selComps, weights, fwss, fwos,
                                      plot.nbins, plot.xmin, plot.xmax,
-                                     cut, weight=weight, embed=embed,
-                                     useExclusiveVV=useExclusiveVV)
+                                     cut, weight = weight, embed = embed,
+                                     VVgroup = VVgroup)
 
         osQ.legendOn = False
         print 'drawing ', plot.var
-        draw(osQ, False, 'TauEle')
-        plot.ssign = cp(ss)
-        plot.osign = cp(os)
-        plot.ssQCD = cp(ssQ)
-        plot.osQCD = cp(osQ)
+        draw(osQ, False, 'TauEle', plotprefix = 'CTRL')
+	ss = None
+	os = None
+	ssQ = None
+	osQ = None
+#        plot.ssign = cp(ss)
+#        plot.osign = cp(os)
+#        plot.ssQCD = cp(ssQ)
+#        plot.osQCD = cp(osQ)
         time.sleep(1)
 
 
@@ -217,9 +191,11 @@ if __name__ == '__main__':
     parser.add_option("-p", "--plots", 
                       dest="plots", 
                       help="plots: set it to true to make control plots",
+                      action="store_true",
                       default=False)
-    
+
     (options,args) = parser.parse_args()
+
     if len(args) != 2:
         parser.print_help()
         sys.exit(1)
@@ -281,22 +257,28 @@ if __name__ == '__main__':
             if comp.name == 'ZZ4l' : continue
         comps.append( comp )
     aliases = None
-#    if useWJetsSoup:
-#        aliases = {'WJetsSoup':'WJets'}
 
     cfg.config.components = comps
 
     selComps, weights, zComps = prepareComponents(anaDir, cfg.config, None, 
                                                   options.embed, 'TauEle', options.higgs)
 
-#    import pdb ; pdb.set_trace()
 
-#    pickles = readPickles(anaDir, cfg.config, options.embed, 'TauMu', options.higgs)
-#    print 'TTJets',pickles['TTJets']['all events'][1]
+    #PG apply the TT scale factor to the cross-section
+    #PG ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-#    for pick in pickles:
-#        print 'READ',pick,pickles[pick]['all events'][1]
-#        selComps[pick].totEvents = pickles[pick]['all events'][1]
+    TTfactor = None
+    if 'Xcat_J0X' in cutstring    : TTfactor = 1.08
+    elif 'Xcat_J1X' in cutstring  : TTfactor = 1.01
+    elif 'Xcat_IncX' in cutstring : TTfactor = 1.08
+    else :
+        TTfactor = 1.03
+        print 'using 2J scale factor for TTJets'
+
+    #PG when I will use the cut "cat_VBF" in my code, I can change this here above
+
+    #PG WARNING assuming the TTbar will not change name
+    selComps['TTJets'].xSection = selComps['TTJets'].xSection * TTfactor
 
 
     #PG (STEP 1) evaluate the WJets contribution from high mT sideband
@@ -313,113 +295,29 @@ if __name__ == '__main__':
     #PG ss   = mt plot with the scaled W, according to fwss
     #PG os   = mt plot with the scaled W, according to fwos
 
-    #PG save the sidebands to be able and get numbers for the systematics
-#    currdir = copy(gDirectory)
-    wsyst_file = TFile ('wsyst.root','recreate')
-    wsyst_file.cd()
-    os.Hist('WJets').weighted.Write('W_os_WJets')
-    os.Hist('Data - DY - TT').weighted.Write('W_os_Data')    
-    wsyst_file.Close()
-#    currdir.cd()
-
-    can, pad, padr = buildCanvas()
-    draw(ss, False, 'TauEle', plotprefix = 'W_ss')
-    draw(os, False, 'TauEle', plotprefix = 'W_os')
-
     #PG final drawing
     #PG ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-    if (options.plots == 'True') :
+    if (options.plots == True) :
+        print 'CONTOL PLOTS'
         drawAll(options.cut, plots_TauEle, options.embed, selComps, weights, fwss, fwos,
-                useExclusiveVV=options.useExcusiveVV)
-        #PG this does not work yet, it does not get the DY right
+                VVgroup = cfg.VVgroup)
     else :
     
         ssign, osign, ssQCD, osQCD = makePlot( options.hist, anaDir, selComps, weights, 
                                                fwss, fwos, NBINS, XMIN, XMAX, 
-                                               options.cut, weight=weight, embed=options.embed, 
-                                               replaceW=replaceW,useExclusiveVV=options.useExcusiveVV )
+                                               options.cut, weight=weight, embed=options.embed,
+                                               VVgroup = cfg.VVgroup,
+                                               replaceW=replaceW)
         # ssign = all cuts, same sign, before QCD estimate
         # osign = all cuts, opposite sign, before QCD estimate
         # ssQCD = all cuts, same sign, after QCD estimate, i.e. the QCD is in
         # osQCD = all cuts, opposite sign, after QCD estimate, i.e. the QCD is in
-        draw(ssign, False, 'TauEle', 'QCD_ss')
-        draw(osign, False, 'TauEle', 'QCD_os')
-
-        #PG save the QCD sideband to be able and get numbers for the systematics
-#        currdir = copy(gDirectory)
-        qcdsyst_file = TFile ('qcdsyst.root','recreate')
-        qcdsyst_file.cd()
-        ssQCD.Hist('QCD').weighted.Write('ssQCD_QCD')
-        ssQCD.Hist('Data').weighted.Write('ssQCD_Data')
-        ssQCD.Hist('Ztt').weighted.Write('ssQCD_Ztt')
-        ssQCD.Hist('TTJets').weighted.Write('ssQCD_TTJets')
-        ssQCD.Hist('WJets').weighted.Write('ssQCD_WJets')
-        try:
-            ssQCD.Hist('Ztt_ZL').weighted.Write('QCD_ss_Ztt_ZL')
-            ssQCD.Hist('Ztt_ZJ').weighted.Write('QCD_ss_Ztt_ZJ')
-        except:
-            print 'cannot find Ztt_Fakes in QCD SS plot saving'
-            print plot
-            pass    
-        qcdsyst_file.Close()   
-#        currdir.cd()
+        # draw(ssign, False, 'TauEle', 'QCD_ss')
+        # draw(osign, False, 'TauEle', 'QCD_os')
 
 #        osQCD.legendOn = False
+        can, pad, padr = buildCanvas()
         draw(osQCD, options.blind, 'TauEle')
         datacards(osQCD, cutstring, shift, 'eTau')
 
-        #PG errors for datacards
-        #PG ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
-        
-        #PG WJets
-#        WJets_stats_component = 1. / math.sqrt(osQCD.Hist('WJets').obj.GetEntries())
-#        WJets_tot_error = sqsum(WJets_stats_component, fwos_error/fwos)
-#        print 'WJets total relative error', round(WJets_tot_error, 2)
-#        #PG FIXME missing the error due to other bkg subtraction in the fwos_error calculation
-#        #PG FIXME move getpickels in prepare components
-#        #PG FIXME and assign to the compontents the number of intial events
-#        #PG FIXME directly there, so that I am sure it can be used also to estimate
-#        #PG FIXME the uncertainty on fos within plotmod.py
-#        
-#        #PG TTbar
-#        TTJets_eff = osQCD.Hist('TTJets').obj.GetEntries() / selComps['TTJets'].totEvents
-#        TTJet_eff_error = math.sqrt(TTJets_eff * (1 - TTJets_eff) / selComps['TTJets'].totEvents)
-#        print 'TTjets relative eff error', TTJet_eff_error / TTJets_eff
-#        #PG missing the error on the scale factor and the error on the total cross-section
-#        #PG for 2011 and 2012 separately
-#        
-#        #PG QCD
-#        QCD_norm_component = ssQCD.Hist('QCD').weighted.Integral()
-#        print 'QCD norm component', round(QCD_norm_component,0)
-#        QCD_extrap_scale = 1.10
-#        QCD_extrap_error = 0.10 / QCD_extrap_scale
-#        print 'QCD extrapolation factor (and relative error)',round(QCD_extrap_scale,2),'+-',round(QCD_extrap_error,2)
-#        
-#        #PG Z+jets
-#        ZJets_total_stats = selComps['Ztt'].totEvents * \
-#                            selComps['Ztt'].tree.GetEntries('isFake == 0') / selComps['Ztt'].tree.GetEntries()
-#        if options.embed :
-#            ZJets_total_stats = sum(comp.totEvents for name,comp in selComps.iteritems() if comp.isEmbed)
-#        ZJets_eff = osQCD.Hist('Ztt').obj.GetEntries() / ZJets_total_stats
-#        ZJets_eff_error = math.sqrt(ZJets_eff * (1 - ZJets_eff) / ZJets_total_stats)
-#        print 'ZJets relative eff error:',round (ZJets_eff_error / ZJets_eff,2)
-#        #PG FIXME normalization error is missing
-#
-#        #PG Z+jets, lepton tau fake
-#        tauEle_fake_error = 0.2
-#        ZJets_ZL_total_stats = selComps['Ztt'].totEvents * \
-#                               selComps['Ztt'].tree.GetEntries('isFake == 1') / selComps['Ztt'].tree.GetEntries()
-#        ZJets_ZL_eff = osQCD.Hist('Ztt_ZL').obj.GetEntries() / ZJets_ZL_total_stats
-#        ZJets_ZL_eff_error = math.sqrt(ZJets_ZL_eff * (1 - ZJets_ZL_eff) / ZJets_ZL_total_stats)
-#        ZJets_ZL_tot_error = sqsum(tauEle_fake_error, ZJets_ZL_eff_error/ZJets_ZL_eff)
-#        print 'ZJets_ZL relative tot error:',round(ZJets_ZL_tot_error,2)
-#
-#        #PG Z+jets, jet tau fake
-#        tauJet_fake_error = 0.2
-#        ZJets_ZJ_total_stats = selComps['Ztt'].totEvents * \
-#                               selComps['Ztt'].tree.GetEntries('isFake == 2') / selComps['Ztt'].tree.GetEntries()
-#        ZJets_ZJ_eff = osQCD.Hist('Ztt_ZJ').obj.GetEntries() / ZJets_ZJ_total_stats
-#        ZJets_ZJ_eff_error = math.sqrt(ZJets_ZJ_eff * (1 - ZJets_ZJ_eff) / ZJets_ZJ_total_stats)
-#        ZJets_ZJ_tot_error = sqsum(tauJet_fake_error, ZJets_ZJ_eff_error/ZJets_ZJ_eff)
-#        print 'ZJets_ZJ relative tot error:',round(ZJets_ZJ_tot_error,2)
