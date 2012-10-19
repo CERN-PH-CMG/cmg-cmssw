9a73e4b6d423b881a943d9efa527cfa828722b31
cbern <>
2012-10-19 16:24:33 +0000

    reviving analysis

--- H2TauTau/python/proto/samples/run2012/tauMu_MuRm_ColinOct9.py
+++ H2TauTau/python/proto/samples/run2012/tauMu_MuRm_ColinOct9.py
@@ -13,8 +13,11 @@ from CMGTools.H2TauTau.proto.samples.run2012.ewk import *
 
 from CMGTools.H2TauTau.proto.samples.run2012.triggers_tauMu import data_triggers, mc_triggers
 
+walias = {
+    '/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM/V5_B/PAT_CMG_V5_8_0/TAUMU_2012_TauIsoRel_ColinOct16':'WJets',
+}
 aliases = {
-    '/DYJets.*START53.*':'DYJets',
+    '/DYJets.*':'DYJets',
     '/TauPlusX/Run2012A-13Jul2012.*':'data_Run2012A',
     '/TauPlusX/Run2012A-recover-06Aug2012-v1.*':'data_Run2012A_aug6',
     '/TauPlusX/Run2012B-13Jul2012.*':'data_Run2012B',
@@ -22,24 +25,29 @@ aliases = {
     '/TauPlusX/Run2012C-PromptReco-v2.*':'data_Run2012C_v2'
     }
 
-MC_list = [DYJets]
+MC_list = [DYJets, WJets]
 allsamples = copy.copy(MC_list)
 allsamples.extend(data_list)
 
-connect( allsamples, '%TAUMU_2012_MuRm_OSVFit_09Oct', 'tauMu.*root', aliases, cache=True, verbose=False)
+connect( [WJets], '/WJets%TAUMU_2012%', 'tauMu.*root', walias, cache=True, verbose=False)
+
+# connect( allsamples, '%TAUMU_2012_MuRm_SVFit_Recoil_ZtoW_C_15Oct%', 'tauMu.*root', aliases, cache=True, verbose=False)
+connect( allsamples, '%TAUMU_MuRm_OSVFit_Colin18Oct', 'tauMu.*root', aliases, cache=True, verbose=False)
+
 
 allsamples = []
 for mc in MC_list:
     mc.triggers = mc_triggers
     allsamples.append(mc)
 for data in data_list:
-    data.json = jsonPick( data.files[0])
-    data.triggers = data_triggers
-    allsamples.append(data)
+    if len(data.files):
+        data.json = jsonPick( data.files[0])
+        data.triggers = data_triggers
+        allsamples.append(data)
 for c in allsamples:
     c.splitFactor = splitFactor(c)
 
 
 
-for c in data_list:
-    computeLumi(c, test=True)
+## for c in data_list:
+##     computeLumi(c, test=True)
