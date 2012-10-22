96ae97b2c685a1de365933415e0e166fe4fddc51
ssekmen <>
2012-10-22 15:35:46 +0000

    *** empty log message ***

--- UserArea/TNM/Ntuples/TNMc1/TNMc1_cfg.py
+++ UserArea/TNM/Ntuples/TNMc1/TNMc1_cfg.py
@@ -1,4 +1,4 @@
-#$Revision: 1.7 $
+#$Revision: 1.8 $
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TheNtupleMaker")
@@ -6,13 +6,22 @@ process = cms.Process("TheNtupleMaker")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 # See TheNtupleMaker twiki for a brief explanation
 #process.MessageLogger.destinations = cms.untracked.vstring("cerr")
-#process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.MessageLogger.cerr.default.limit = 5
 
 # This is required in order to configure HLTConfigProducer
 process.load("L1TriggerConfig.L1GtConfigProducers.L1GtConfig_cff")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# Get Will's SUSY MJ objects/sequences:
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+process.load("CMGTools.Susy.RazorMultiJet.razorMultijet_cff")
+process.load("CMGTools.Susy.common.susy_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 # Run on MC or data
 
@@ -21,6 +30,7 @@ runOnMC = True
 # Input file
 
 dataset_user = 'cmgtools' 
+#dataset_name = '/SingleMu/Run2012A-13Jul2012-v1/AOD/PAT_CMG_V5_6_0_B'
 dataset_name = '/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_6_0_B'
 #dataset_name = '/HT/Run2012A-PromptReco-v1/RECO/PAT_CMG_V5_4_0_runrange_190605-194076'
 #dataset_name = '/JetHT/Run2012B-PromptReco-v1/AOD/PAT_CMG_V5_4_0_runrange_194480-195016'
@@ -40,7 +50,7 @@ print 'input:', process.source.fileNames
 
 if runOnMC==False:
     from CMGTools.Common.Tools.applyJSON_cff import *
-    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196531_8TeV_PromptReco_Collisions12_JSON.txt'
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-204567_8TeV_PromptReco_Collisions12_JSON.txt'
     print 'json:', json
     applyJSON(process, json )
 
@@ -78,4 +88,16 @@ process.ak5PFJetsCHSprunedSubJetspuJetId = pileupJetIdProducer.clone(
     )
 
 #process.p = cms.Path(process.selectedPatJetspuJetId * process.selectedPatJetsCHSpuJetId * process.ak5PFJetsCHSprunedSubJetspuJetId * process.demo)
-process.p = cms.Path(process.demo)
+
+process.razorMJObjectSequence.remove(process.razorMJHemiSequence)
+# This is the UCSB tau veto.  Need to remove for SMSs:
+process.razorMJTauSequence.remove(process.razorMJTauVeto)
+
+if runOnMC==True:
+    process.p = cms.Path(process.razorMJObjectSequence+process.susyGenSequence+process.demo)
+else:
+    process.p = cms.Path(process.razorMJObjectSequence+process.demo)
+
+#process.p = cms.Path(process.razorMJObjectSequence+process.susyGenSequence+process.demo)
+
+#process.p = cms.Path(process.demo)
