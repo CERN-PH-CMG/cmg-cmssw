463bd749f79262f51c1bb4c4b75b16d1ba109676
cbern <>
2012-10-23 05:53:40 +0000

    removed obsolete files

--- CMGTools/H2TauTau/python/h2TauTau_cff.py
+++ CMGTools/H2TauTau/python/h2TauTau_cff.py
@@ -1,19 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMGTools.H2TauTau.objects.gen_cff import *
-# to use recoil correction instead of MVAMET for tau-mu, just uncomment
-# the following line...
-# from CMGTools.H2TauTau.objects.tauMuObjects_cff import *
-# and comment this one: 
 from CMGTools.H2TauTau.objects.tauMuObjectsMVAMET_cff import *
-# from CMGTools.H2TauTau.objects.tauEleObjects_cff import *
 from CMGTools.H2TauTau.objects.tauEleObjectsMVAMET_cff import *
-from CMGTools.H2TauTau.objects.muEleObjects_cff import *
-#from CMGTools.H2TauTau.objects.diTauObjects_cff import *
-from CMGTools.H2TauTau.objects.diTauObjects_cff import *
+from CMGTools.H2TauTau.objects.diTauObjectsMVAMET_cff import *
 from CMGTools.H2TauTau.skims.skim_cff import *
 
-#TODO:tau scaling could be general
 
 
 # tau-mu ---
