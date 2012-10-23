b842f28a435583dc32ff489f3e77ad2bc8a697a8
jueugste <>
2012-10-23 08:32:33 +0000

    first commit

---
+++ UserArea/TopMass/python/topinfo.py
@@ -0,0 +1,19 @@
+samples = {
+    '/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 13847500', 
+    '/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 30434389', 
+    '/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 18269870', 
+    '/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM/V5_B/PAT_CMG_V5_10_0':' 57179973', 
+    '/T_s-channel_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'   259961', 
+    '/T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'   497658', 
+    '/Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'   139974', 
+    '/Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'   493460', 
+    '/Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'  1935072', 
+    '/T_t-channel_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':'  3758227', 
+
+    '/TTJets_scaledown_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 10774362', 
+    '/TTJets_scaleup_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 10018976', 
+    '/TTJets_matchingdown_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 10953456', 
+    '/TTJets_matchingup_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0':' 10830020', 
+    }
+
+ 
---
+++ UserArea/TopMass/scripts/submitJobs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import optparse
+import os
+
+##import UserCode.TopMass.topinfo
+
+##sys.path.append('/afs/cern.ch/user/j/jueugste/cmssw/top/CMGTools/CMSSW_5_3_3_patch3/src/CMGTools/UserCode/TopMass/python')
+sys.path.append('../python')
+import topinfo
+
+
+if __name__ == '__main__':
+    print topinfo.samples
+      
+    
+    usage = 'usage: %prog [options]'
+    parser = optparse.OptionParser(usage)
+    
+    #    parser.add_option('-f'               ,     dest='force'          , help='Tag used for the shape file name'                          , default='')
+    parser.add_option('-n', '--nfiles' ,       dest='nfiles'         , help='Number of files per job. Default is 100.'                                  , default=100)
+    parser.add_option('-o', '--outputdir' ,    dest='outputdir'      , help='Name of the local output directory for your jobs. This directory will be created automatically.'    , default=None)
+    parser.add_option('-r', '--remotecopy' ,   dest='remotecopy'     , help='Remote output directory for your jobs. Something of the form \"/store/cmst3/user/USERNAME/bla/bla/bla/\"'    , default=None)
+    
+    (opt, args) = parser.parse_args()
+
+    if opt.outputdir is None:
+        parser.error('No output directory defined!')
+    if opt.remotecopy is None:
+        parser.error('No remote output directory defined!')
+
+    nfiles = opt.nfiles
+    outputdir = opt.outputdir
+    remotecopy = opt.remotecopy
+
+    for sample in topinfo.samples:
+        samplename = sample.split('_Tune')[0].replace('/','')
+        spec = 'sampleName='+samplename+' sampleLocation='+sample+'\''
+        
+        cmd = 'cmsBatch.py '+str(nfiles)+' skimLeptonJets.py -f -c \''+spec+' -o \''+outputdir+'\' -r '+remotecopy+' -b \'bsub -q 2nd < ./batchScript.sh\' '
+        print cmd
+        #os.system(cmd)
