85cc3bfa4c47d1e253104d68c4b6335ae082760b
govoni <>
2012-10-23 15:38:28 +0000

    added some doc

--- CMGTools/H2TauTau/interface/BtagSF.h
+++ CMGTools/H2TauTau/interface/BtagSF.h
@@ -10,6 +10,11 @@ class BtagSF{
   BtagSF( int seed=0 );
   ~BtagSF();
   
+  // this tells whether the jet is btagged or not
+  // intead of checking the discriminator, I call this function
+  // every jet in the same way, but:
+  // - data flag
+  // - 2012 flag
   Bool_t isbtagged(Float_t pt, Float_t eta, Float_t csv, Int_t jetflavor, Bool_t isdata, UInt_t btagsys, UInt_t mistagsys, Bool_t is2012);
   Double_t getSFb(Float_t pt, UInt_t btagsys);
   Double_t getSFl(Float_t pt, Float_t eta, UInt_t mistagsys, Bool_t is2012);
