26e40a702eb88505de3d7dea552e22925ee5b7dc
cbern <>
2012-11-14 16:43:59 +0000

    first version of the Z+jets tutorial

---
+++ ZJetsTutorial/cfg/diMu_2012_cfg.py
@@ -0,0 +1,125 @@
+import copy
+import os 
+import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.RootTools.fwlite.Config import printComps
+
+# from CMGTools.H2TauTau.triggerMap import pathsAndFilters
+from CMGTools.RootTools.RootTools import * 
+
+
+
+eventSelector = cfg.Analyzer(
+    'EventSelector',
+    toSelect = [
+    # here put the event numbers (actual event numbers from CMSSW)
+    ]
+    )
+
+jsonAna = cfg.Analyzer(
+    'JSONAnalyzer',
+    )
+
+# this analyzer is just there to select a list of good primary vertices.
+# in 2012, we don't use it to reweight events for pile-up
+vertexAna = cfg.Analyzer(
+    'VertexAnalyzer',
+    goodVertices = 'offlinePrimaryVertices',
+    vertexWeight = None,
+    fixedWeight = 1,
+    verbose = False
+    )
+
+# this analyzer actually does the pile-up reweighting.
+# see below for the definition of the pile-up histograms. 
+pileUpAna = cfg.Analyzer(
+    'PileUpAnalyzer',
+    true = True
+    )
+
+
+ZMuMuAna = cfg.Analyzer(
+    'ZMuMuAnalyzer',
+    pt1 = 10,
+    eta1 = 2.4,
+    iso1 = 0.3,
+    pt2 = 10,
+    eta2 = 2.4,
+    iso2 = 0.3,
+    m_min = 50,
+    m_max = 120,
+    verbose = False
+    )
+
+
+# defined for vbfAna and eventSorter
+vbfKwargs = dict( Mjj = 500,
+                  deltaEta = 3.5    
+                  )
+
+jetAna = cfg.Analyzer(
+    'JetAnalyzer',
+    jetCol = 'cmgPFJetSel',
+    jetPt = 20.,
+    jetEta = 4.7,
+    cjvPtCut = 30.,
+    btagSFseed = 123456,
+    relaxJetId = True, 
+    **vbfKwargs
+    )
+
+
+treeProducer = cfg.Analyzer(
+    'ZJetsTreeProducer'
+    )
+
+
+###############################################################################
+
+
+from CMGTools.ZJetsTutorial.samples.run2012.diMu_ColinNov13 import *
+
+###############################################################################
+
+
+selectedComponents = allsamples
+
+puFileMC = '/afs/cern.ch/user/a/agilbert/public/HTT_Pileup/28-09-12/MC_Summer12_PU_S10-600bins.root'
+puFileData = '/afs/cern.ch/user/a/agilbert/public/HTT_Pileup/28-09-12/Data_Pileup_2012_HCP-600bins.root'
+for mc in MC_list:
+    mc.puFileMC = puFileMC
+    mc.puFileData = puFileData
+
+sequence = cfg.Sequence( [
+    #     eventSelector,
+    jsonAna,
+    vertexAna,
+    pileUpAna,
+    ZMuMuAna,
+    jetAna,
+    treeProducer
+   ] )
+
+
+# set test = 0 to run all jobs, in case you are using pybatch.py
+test = 1
+if test==1:
+    # test a single component, using a single thread.
+    # necessary to debug the code, until it doesn't crash anymore
+    comp = DYJets
+    comp.files = comp.files[:2]
+    selectedComponents = [comp]
+    comp.splitFactor = 1
+elif test==2:    
+    # test all components (1 thread per component.
+    # important to make sure that your code runs on any kind of component
+    for comp in selectedComponents:
+        comp.splitFactor = 1
+        comp.files = comp.files[:3]
+
+# creation of the processing configuration.
+# we define here on which components to run, and
+# what is the sequence of analyzers to run on each event. 
+config = cfg.Config( components = selectedComponents,
+                     sequence = sequence )
+
+printComps(config.components, True)
---
+++ ZJetsTutorial/python/analyzers/ZJetsTreeProducer.py
@@ -0,0 +1,68 @@
+from CMGTools.RootTools.analyzers.TreeAnalyzerNumpy import TreeAnalyzerNumpy
+from CMGTools.ZJetsTutorial.analyzers.ntuple import *
+
+def var( tree, varName, type=float ):
+    tree.var(varName, type)
+
+def fill( tree, varName, value ):
+    tree.fill( varName, value )
+
+
+class ZJetsTreeProducer( TreeAnalyzerNumpy ):
+    
+    def declareVariables(self):
+
+        tr = self.tree
+        var( tr, 'run', int)
+        var( tr, 'lumi', int)
+        var( tr, 'evt', int)
+
+        bookParticle(tr, 'diL')
+        var(tr, 'diL_mass')
+
+        bookLepton(tr, 'leg1')
+        bookLepton(tr, 'leg2')
+        
+        var( tr, 'nJets')
+        var( tr, 'nJets20')
+        bookJet(tr, 'jet1')
+        bookJet(tr, 'jet2')
+
+        bookVBF( tr, 'VBF' )
+
+        var( tr, 'nVert')
+        var( tr, 'weight')
+        
+
+    def process(self, iEvent, event):
+        
+        tr = self.tree
+        tr.reset()
+
+        fill( tr, 'run', event.run) 
+        fill( tr, 'lumi',event.lumi)
+        fill( tr, 'evt', event.eventId)
+       
+        fillParticle(tr, 'diL', event.diLepton)
+        fill(tr, 'diL_mass', event.diLepton.mass())
+        fillLepton(tr, 'leg1', event.diLepton.leg1())
+        fillLepton(tr, 'leg2', event.diLepton.leg2())
+
+
+        fill(tr, 'nJets20', len(event.cleanJets) )
+        nJets30 = len(event.cleanJets30)
+        fill(tr, 'nJets', nJets30 )
+        nJets = len(event.cleanJets)
+        if nJets>=1:
+            fillJet(tr, 'jet1', event.cleanJets[0] )
+        if nJets>=2:
+            fillJet(tr, 'jet2', event.cleanJets[1] )
+            
+        if hasattr( event, 'vbf'):
+            fillVBF( tr, 'VBF', event.vbf )
+
+        fill(tr, 'nVert', len(event.goodVertices) )
+        fill(tr, 'weight', event.eventWeight )
+        
+        self.tree.tree.Fill()
+       
