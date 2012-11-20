9a898347d1c5757d6398f712afa850db2e2b4907
bachtis <>
2012-11-20 17:29:06 +0000

    New packer

--- HToZZTo4Leptons/scripts/hzz4LPack.py
+++ HToZZTo4Leptons/scripts/hzz4LPack.py
@@ -22,14 +22,34 @@ if __name__ == '__main__':
     if os.path.exists(directory+'/'+options.trigcounter):
         counterFile=open(directory+'/'+options.trigcounter)
         counter=pickle.load(counterFile)
+        print counter
         events=counter[0][1]
+        events4e=counter[1][1]
+        events4mu=counter[2][1]
+        events2e2mu=counter[3][1]
+        eventsZH=counter[4][1]
+        eventsWH=counter[5][1]
+        eventsttH=counter[6][1]
     else:
         print 'problem in file/ cannot count events in ',directory
         print 'If is data you can ignore'
         events=-1
+        events4e=-1
+        events4mu=-1
+        events2e2mu=-1
+        eventsZH=-1
+        eventsWH=-1
+        eventsttH=-1
         
 
     output['events'] = events
+    output['4e'] = events4e
+    output['4mu'] = events4mu
+    output['2e2mu'] = events2e2mu
+    output['ZH'] = eventsZH
+    output['WH'] = eventsWH  
+    output['ttH'] = eventsttH
+  
     output['weight'] = options.sigma/float(events)
     output['sigma']  = options.sigma
 
