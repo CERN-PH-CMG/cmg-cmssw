1cd94a6f1ef437ca923407192a447c9c40040100
cbern <>
2012-11-21 08:27:04 +0000

    added plotting

--- ZJetsTutorial/cfg/diMu_2012_cfg.py
+++ ZJetsTutorial/cfg/diMu_2012_cfg.py
@@ -6,7 +6,15 @@ from CMGTools.RootTools.fwlite.Config import printComps
 # from CMGTools.H2TauTau.triggerMap import pathsAndFilters
 from CMGTools.RootTools.RootTools import * 
 
-
+pathsAndFilters = {
+    "HLT_Mu17_Mu8_v16": ("hltDiMuonMu17Mu8DzFiltered0p2","hltDiMuonMu17Mu8DzFiltered0p2"), 
+    "HLT_Mu17_Mu8_v17": ("hltL3fL1DoubleMu10MuOpenL1f0L2f10L3Filtered17","hltL3fL1DoubleMu10MuOpenL1f0L2f10L3Filtered17"),
+    "HLT_Mu17_Mu8_v18": ("hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17","hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17"), 
+    "HLT_Mu17_Mu8_v19": ("hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17","hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17"), 
+    "HLT_Mu17_Mu8_v20": ("hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17","hltL3fL1DoubleMu10MuOpenOR3p5L1f0L2f10L3Filtered17"),
+    "HLT_Mu17_Mu8_v21": ("hltDiMuonGlb17Glb8DzFiltered0p2","hltDiMuonGlb17Glb8DzFiltered0p2"),
+    "HLT_Mu17_Mu8_v22": ("hltDiMuonGlb17Glb8DzFiltered0p2","hltDiMuonGlb17Glb8DzFiltered0p2"),
+    }
 
 eventSelector = cfg.Analyzer(
     'EventSelector',
@@ -17,6 +25,11 @@ eventSelector = cfg.Analyzer(
 
 jsonAna = cfg.Analyzer(
     'JSONAnalyzer',
+    )
+
+
+triggerAna = cfg.Analyzer(
+    'TriggerAnalyzer',
     )
 
 # this analyzer is just there to select a list of good primary vertices.
@@ -47,7 +60,8 @@ ZMuMuAna = cfg.Analyzer(
     iso2 = 0.3,
     m_min = 50,
     m_max = 120,
-    verbose = False
+    verbose = False,
+    triggerMap = pathsAndFilters
     )
 
 
@@ -81,6 +95,25 @@ from CMGTools.ZJetsTutorial.samples.run2012.diMu_ColinNov13 import *
 ###############################################################################
 
 
+VVgroup = [c.name for c in mc_diboson_inc]
+
+allsamples = copy.copy(mc_diboson_inc)
+allsamples.extend([TTJets, DYJets])
+allsamples.extend(data_list)
+
+for c in allsamples:
+    c.triggers = [
+        'HLT_Mu17_Mu8_v16',
+        'HLT_Mu17_Mu8_v17',
+        'HLT_Mu17_Mu8_v18',
+        'HLT_Mu17_Mu8_v19',
+        'HLT_Mu17_Mu8_v20',
+        'HLT_Mu17_Mu8_v21',
+        'HLT_Mu17_Mu8_v22',
+        ]
+
+# allsamples = data_list
+
 selectedComponents = allsamples
 
 puFileMC = '/afs/cern.ch/user/a/agilbert/public/HTT_Pileup/28-09-12/MC_Summer12_PU_S10-600bins.root'
@@ -92,6 +125,7 @@ for mc in MC_list:
 sequence = cfg.Sequence( [
     #     eventSelector,
     jsonAna,
+    triggerAna,
     vertexAna,
     pileUpAna,
     ZMuMuAna,
@@ -101,14 +135,15 @@ sequence = cfg.Sequence( [
 
 
 # set test = 0 to run all jobs, in case you are using pybatch.py
-test = 1
+test = 0
 if test==1:
     # test a single component, using a single thread.
     # necessary to debug the code, until it doesn't crash anymore
     comp = DYJets
-    comp.files = comp.files[:2]
+    comp.files = comp.files[:10]
     selectedComponents = [comp]
     comp.splitFactor = 1
+    # comp.triggers = ['HLT_Mu17_Mu8_v*']
 elif test==2:    
     # test all components (1 thread per component.
     # important to make sure that your code runs on any kind of component
