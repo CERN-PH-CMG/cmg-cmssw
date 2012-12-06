019d696bee94829913385d6d0b1d5f3dbffe14b0
botta <>
2012-12-06 18:54:00 +0000

    main cfg for 8TeV running

---
+++ CMGTools/TTHAnalysis/cfg/run_ttHLep8TeV_cfg.py
@@ -0,0 +1,161 @@
+import copy
+import os 
+import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.RootTools.fwlite.Config import printComps
+from CMGTools.RootTools.RootTools import *
+
+
+# this analyzer finds the initial events before the skim
+skimAnalyzer = cfg.Analyzer(
+    'skimAnalyzerCount'
+    )
+
+eventSelector = cfg.Analyzer(
+    'EventSelector',
+    toSelect = [
+    # here put the event numbers (actual event numbers from CMSSW)
+    ]
+    )
+
+jsonAna = cfg.Analyzer(
+    'JSONAnalyzer',
+    )
+
+triggerAna = cfg.Analyzer(
+    'TriggerAnalyzer',
+    )
+
+
+# this analyzer is just there to select a list of good primary vertices.
+ttHVertexAna = cfg.Analyzer(
+    'VertexAnalyzer',
+    goodVertices = 'offlinePrimaryVertices',
+    vertexWeight = None,
+    fixedWeight = 1,
+    verbose = False
+    )
+
+
+# this analyzer actually does the pile-up reweighting.
+pileUpAna = cfg.Analyzer(
+    "PileUpAnalyzer",
+    # build unweighted pu distribution using number of pile up interactions if False
+    # otherwise, use fill the distribution using number of true interactions
+    true = True,
+    makeHists=False
+    )
+
+
+# Gen Info Analyzer
+ttHGenAna = cfg.Analyzer(
+    'ttHGenLevelAnalyzer',
+    filterHiggsDecays = [0, 15, 23, 24],
+    verbose = True,
+    )
+
+# Lepton Analyzer
+ttHLepAna = cfg.Analyzer(
+    'ttHLepAnalyzerBase',
+    rhoMuon= 'kt6PFJetsCentralNeutral',
+    rhoElectron = 'kt6PFJets',
+    muons='cmgMuonSel',
+    electrons='cmgElectronSel',
+    )
+
+# Jets Analyzer 
+ttHJetAna = cfg.Analyzer(
+    'JetAnalyzer',
+    jetCol = 'cmgPFJetSel',
+    jetPt = 20.,
+    jetEta = 4.7,
+    cjvPtCut = 30.,
+    btagSFseed = 123456,
+    relaxJetId = False,  
+    )
+
+# Tree Producer
+treeProducer = cfg.Analyzer(
+    'ttHLepTreeProducerBase'
+    )
+
+
+#-------- SAMPLES
+
+from CMGTools.TTHAnalysis.samples.samples_8TeV import * 
+
+#selectedComponents=mcSamples+dataSamplesMu+dataSamplesE+dataSamplesMuE
+
+for mc in mcSamples:
+    mc.triggers = triggersMC_mue
+selectedComponents=mcSamples
+
+
+
+#-------- SEQUENCE
+
+sequence = cfg.Sequence([
+    skimAnalyzer,
+    #eventSelector,
+    #jsonAna,
+    #triggerAna,
+    #pileUpAna,
+    ttHGenAna,
+    ttHVertexAna,
+    ttHLepAna,
+    ttHJetAna,
+    treeProducer,
+    
+    ])
+
+
+#-------- HOW TO RUN
+
+# set test = 0 to run all jobs, in case you are using pybatch.py
+test = 1
+if test==1:
+    # test a single component, using a single thread.
+    # necessary to debug the code, until it doesn't crash anymore
+    comp = TTH
+    comp.files = comp.files[:20]
+    selectedComponents = [comp]
+    comp.splitFactor = 1
+elif test==2:    
+    # test all components (1 thread per component.
+    # important to make sure that your code runs on any kind of component
+    for comp in selectedComponents:
+        comp.splitFactor = 1
+        comp.files = comp.files[:3]
+
+# creation of the processing configuration.
+# we define here on which components to run, and
+# what is the sequence of analyzers to run on each event. 
+config = cfg.Config( components = selectedComponents,
+                     sequence = sequence )
+
+printComps(config.components, True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
