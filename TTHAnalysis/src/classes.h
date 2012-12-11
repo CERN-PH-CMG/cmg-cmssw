c167dbb388f5995c7e5a27b063e86f1a3b05ae2a
gpetrucc <>
2012-12-11 23:04:08 +0000

    lepton MVA tools

--- TTHAnalysis/src/classes.h
+++ TTHAnalysis/src/classes.h
@@ -1,7 +1,9 @@
 #include "CMGTools/TTHAnalysis/interface/SignedImpactParameter.h"
+#include "CMGTools/TTHAnalysis/interface/PyTMVATool.h"
 
 namespace {
     struct dictionary {
         SignedImpactParameter sipc;
+        PyTMVATool tool;
     };
 }
