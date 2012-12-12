1ccca4edfac23e867aa48f0d3cdb9c6f3f059364
cbern <>
2012-12-12 17:23:11 +0000

    clean up: removed VBF Event

--- AnalysisDataFormats/CMGTools/src/VBFEvent.cc
+++
@@ -1,4 +0,0 @@
-#include "AnalysisDataFormats/CMGTools/interface/VBFEventT1T2.h"
-#include "AnalysisDataFormats/CMGTools/interface/VBFEventT1T2Fwd.h"
-
-// this file is just here so that we compile the template instanciations declared in VBFEventT1T2Fwd.h, and include them to the shared library
