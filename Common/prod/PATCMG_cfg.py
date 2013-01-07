f8bdaf2974befac4376c9497fb36f709d9b5bf80
hinzmann <>
2013-01-07 10:07:05 +0000

    remove track jets for CHS jets

--- Common/prod/PATCMG_cfg.py
+++ Common/prod/PATCMG_cfg.py
@@ -95,6 +95,8 @@ process.PATCMGJetCHSSequence.insert( 0, process.ak5PFJetsCHS )
 from CMGTools.Common.Tools.visitorUtils import replaceSrc
 replaceSrc( process.PATCMGJetCHSSequence, 'ak5PFJets', 'ak5PFJetsCHS')
 replaceSrc( process.PATCMGJetCHSSequence, 'particleFlow', 'pfNoPileUp')
+process.PATCMGJetCHSSequence.remove(process.outTracksCHS) # not needed for CHS jets
+process.PATCMGJetCHSSequence.remove(process.ak5SoftTrackJetsForVbfHbbCHS) # not needed for CHS jets
 jecPayload = 'AK5PFchs'
 process.patJetsWithVarCHS.payload = jecPayload
 process.patJetCorrFactorsCHS.payload = jecPayload
