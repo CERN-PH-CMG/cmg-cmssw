6303332243fb8704b66fd28a2f18ec3d7c5ec8d7
gpetrucc <>
2013-01-13 18:31:10 +0000

    support for systematics

--- TTHAnalysis/python/plotter/makeCountingCards.py
+++ TTHAnalysis/python/plotter/makeCountingCards.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from CMGTools.TTHAnalysis.plotter.mcAnalysis import *
+import re, sys
+systs = {}
 
 from optparse import OptionParser
-parser = OptionParser(usage="%prog [options] mc.txt cuts.txt")
+parser = OptionParser(usage="%prog [options] mc.txt cuts.txt systs.txt")
 addMCAnalysisOptions(parser)
 (options, args) = parser.parse_args()
 options.weight = True
@@ -23,6 +25,27 @@ allyields = mergeReports([y for x,y in report.iteritems()])
 if 'data' not in report: 
     report['data'] = allyields
 
+systs = {}
+for sysfile in args[2:]:
+    for line in open(sysfile, 'r'):
+        if re.match("\s*#.*", line): continue
+        line = re.sub("#.*","",line)
+        field = [f.strip() for f in line.split(':')]
+        (name, procmap, binmap, amount) = field
+        if re.match(binmap,binname) == None: continue
+        if name not in systs: systs[name] = []
+        systs[name].append((re.compile(procmap),amount))
+    sys.stderr.write("Loaded %d systematics\n" % len(systs))
+
+for name in systs.keys():
+    effmap = {}
+    for p in procs:
+        effect = "-"
+        for (procmap,amount) in systs[name]:
+            if re.match(procmap, p): effect = amount
+        effmap[p] = effect
+    systs[name] = effmap
+
 print "## Datacard for cut file ",args[1]
 print '##----------------------------------'
 print 'bin         ',binname
@@ -37,3 +60,5 @@ print 'process     '," ".join([kpatt % p        for p in procs])
 print 'process     '," ".join([kpatt % iproc[p] for p in procs])
 print 'rate        '," ".join([fpatt % report[p][-1][1][0] for p in procs])
 print '##----------------------------------'
+for name,effmap in systs.iteritems():
+    print '%-8s lnN' % name," ".join([kpatt % effmap[p]   for p in procs])
