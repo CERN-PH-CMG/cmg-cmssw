310897c46c65c50df6e542c047de21bb4917cffa
benitezj <>
2013-01-16 11:05:29 +0000

    updated b-tag scale factors and pileup weights

--- CMGTools/RootTools/interface/BTagSF.h
+++ CMGTools/RootTools/interface/BTagSF.h
@@ -1,29 +1,31 @@
-#ifndef BTagSF_h
-#define BTagSF_h
+#ifndef BTagSF_hh
+#define BTagSF_hh
 
+#include "TMath.h"
 #include "TRandom3.h"
 #include <iostream>
 
 class BTagSF{
 
  public:
+    
   BTagSF( int seed=0 );
   ~BTagSF();
-  
-  // this tells whether the jet is btagged or not
-  // intead of checking the discriminator, I call this function
-  // every jet in the same way, but:
-  // - data flag
-  // - 2012 flag
+    
   Bool_t isbtagged(Float_t pt, Float_t eta, Float_t csv, Int_t jetflavor, Bool_t isdata, UInt_t btagsys, UInt_t mistagsys, Bool_t is2012);
-  Double_t getSFb(Float_t pt, UInt_t btagsys);
+  Double_t getSFb(Float_t pt, UInt_t btagsys, Bool_t is2012);
+  Double_t getSFc(Float_t pt, UInt_t btagsys, Bool_t is2012);
   Double_t getSFl(Float_t pt, Float_t eta, UInt_t mistagsys, Bool_t is2012);
   Double_t getMistag(Float_t pt, Float_t eta);
-  
-  enum { kNo, kDown, kUp };        // systematic variations 
-  
+
+  enum { kNo, kDown, kUp };                     // systematic variations 
+
+
  private:
-  TRandom3* randm;
   
+  TRandom3* randm;
+
 };
+
+
 #endif
