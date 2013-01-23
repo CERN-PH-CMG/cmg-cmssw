4bb186056b429284ffa6f84de1cf2ef4e38442f7
psilva <>
2013-01-23 15:06:02 +0000

    adding histogram envelope macro

--- CMGTools/HtoZZ2l2nu/src/MacroUtils.cc
+++ CMGTools/HtoZZ2l2nu/src/MacroUtils.cc
@@ -1,7 +1,7 @@
 #include "CMGTools/HtoZZ2l2nu/interface/MacroUtils.h"
 #include "TMath.h"
 #include "TLorentzVector.h"
-
+#include "TCanvas.h"
 
 LorentzVector min(const LorentzVector& a, const LorentzVector& b){
    if(a.pt()<=b.pt())return a;
@@ -207,7 +207,7 @@ double weightNarrowResonnance(std::string SampleName, double m_gen, double mass,
      {
        //other prob function where [0] is the mass of resonance and [1] is the new width
        // e.g. relativistic BW   
-       //TF1 *f=new TF1("relbw","(2*sqrt(2)*[0]*[1]*sqrt(pow([0],2)*(pow([0],2)+pow([1],2)))/(TMath::Pi()*sqrt(pow([0],2)+sqrt(pow([0],2)*(pow([0],2)+pow([1],2))))))/(pow(pow([2],2)-pow([0],2),2)+pow([0]*[1],2))",0,2000);
+       //TF1 *f=new TF1("relbw","(2*sqrt(2)*[0]*[1]*sqrt(pow([0],2)*(pow([0],2)+pow([1],2)))/(TMath::Pi()*sqrt(pow([0],2)+sqrt(pow([0],2)*(pow([0],2)+pow([1],2))))))/(pow(pow(x,2)-pow([0],2),2)+pow([0]*[1],2))",0,2000);
        decayProbPdf->SetParameter(0,m_gen);
        decayProbPdf->SetParameter(1,decay_width);
        double weight_BW = decayProbPdf->Eval( mass );
@@ -218,6 +218,144 @@ double weightNarrowResonnance(std::string SampleName, double m_gen, double mass,
    if(toReturn<0){toReturn=1.0;}
    if(weight_CPS<=0)toReturn = 0.0;
    return toReturn;
+}
+
+
+//
+std::pair<TH1F *,TH1F *> generateHistoEnvelope(TH1F *h,bool debug)
+{
+  std::pair<TH1F *,TH1F *> toReturn(0,0);
+  if(h==0) return toReturn;
+
+  //get bin with min. relative error and use as reference
+  int imin(1);
+  float ymin(h->GetBinContent(1));
+  float ymin_err(h->GetBinError(1));
+  float xmin(h->GetBinCenter(1));
+  for(int ibin=2; ibin<=h->GetXaxis()->GetNbins(); ibin++)
+    {
+      float x     = h->GetBinCenter(ibin);
+      float y     = h->GetBinContent(ibin);
+      float y_err = h->GetBinError(ibin);
+      if(y==0) continue;
+      if(ymin==0)
+	{
+	  imin     = ibin;
+	  xmin     = x;
+	  ymin     = y;
+	  ymin_err = y_err;
+	}
+      else if(y_err/y < ymin_err/ymin)
+	{
+	  imin     = ibin;
+	  xmin     = x;
+	  ymin     = y;
+	  ymin_err = y_err;
+	}
+    }
+  if(ymin==0) return toReturn;
+
+  //now get the max variations to the left and to the right of this point
+  int imaxL(imin),           imaxR(imin);
+  float ymaxL(ymin),         ymaxR(ymin);
+  float ymaxL_err(ymin_err), ymaxR_err(ymin_err);
+  float xmaxL(xmin),         xmaxR(xmin);
+  for(int ibin=1; ibin<=h->GetXaxis()->GetNbins(); ibin++)
+    {
+      if(ibin==imin) continue;
+      float x     = h->GetBinCenter(ibin);
+      float y     = h->GetBinContent(ibin);
+      float y_err = h->GetBinError(ibin);
+      if(y==0) continue;
+      if(x<xmin && ymaxL_err/ymaxL<y_err/y)
+	{
+	  imaxL     = ibin;
+	  xmaxL     = x;
+	  ymaxL     = y;
+	  ymaxL_err = y_err;
+	}
+      if(x>xmin && ymaxR_err/ymaxR<y_err/y)
+	{
+	  imaxR     = ibin;
+	  xmaxR     = x;
+	  ymaxR     = y;
+	  ymaxR_err = y_err;
+	}
+    }
+
+//   cout << xmin << " " << ymin << " " << ymin_err
+//        << " | " << xmaxL << " " << ymaxL << " " << ymaxL_err
+//        << " | " << xmaxR << " " << ymaxR << " " << ymaxR_err
+//        << endl;
+
+  float aL=(TMath::Max(ymaxL+ymaxL_err,Float_t(0.))/ymaxL - TMath::Max(ymin-ymin_err,Float_t(0.))/ymin)/(xmaxL-xmin);
+  float bL= TMath::Max(ymaxL+ymaxL_err,Float_t(0.))/ymaxL -  aL*xmaxL;
+  
+  float aR=(TMath::Max(ymaxR+ymaxR_err,Float_t(0.))/ymaxR - TMath::Max(ymin-ymin_err,Float_t(0.))/ymin)/(xmaxR-xmin);
+  float bR= TMath::Max(ymaxR+ymaxR_err,Float_t(0.))/ymaxR -  aR*xmaxR;
+  
+  TF1 *shiftUp = new TF1("shiftup","(x<[0])*([1]*x+[2])+(x>=[0])*([3]*x+[4])",h->GetXaxis()->GetXmin(),h->GetXaxis()->GetXmax());
+  shiftUp->SetLineColor(kRed);
+  shiftUp->SetParameter(0,xmin);
+  shiftUp->SetParameter(1,aL);
+  shiftUp->SetParameter(2,bL);
+  shiftUp->SetParameter(3,aR);
+  shiftUp->SetParameter(4,bR);
+
+
+  aL=(TMath::Max(ymaxL-ymaxL_err,Float_t(0.))/ymaxL - TMath::Max(ymin+ymin_err,Float_t(0.))/ymin)/(xmaxL-xmin);
+  bL= TMath::Max(ymaxL-ymaxL_err,Float_t(0.))/ymaxL -  aL*xmaxL;
+  
+  aR=(TMath::Max(ymaxR-ymaxR_err,Float_t(0.))/ymaxR - TMath::Max(ymin+ymin_err,Float_t(0.))/ymin)/(xmaxR-xmin);
+  bR= TMath::Max(ymaxR-ymaxR_err,Float_t(0.))/ymaxR -  aR*xmaxR;
+
+
+  TF1 *shiftDown = new TF1("shiftdown","(x<[0])*([1]*x+[2])+(x>=[0])*([3]*x+[4])",h->GetXaxis()->GetXmin(),h->GetXaxis()->GetXmax());
+  shiftDown->SetLineColor(kBlue);
+  shiftDown->SetParameter(0,xmin);
+  shiftDown->SetParameter(1,aL);
+  shiftDown->SetParameter(2,bL);
+  shiftDown->SetParameter(3,aR);
+  shiftDown->SetParameter(4,bR);
+
+  
+  TH1F *hUp   = (TH1F *) h->Clone("hUp");   hUp->Reset("ICE");   hUp->SetLineColor(kRed);    hUp->SetDirectory(0);
+  TH1F *hDown = (TH1F *) h->Clone("hDown"); hDown->Reset("ICE"); hDown->SetLineColor(kBlue); hDown->SetDirectory(0); 
+  for(int ibin=1; ibin<=h->GetXaxis()->GetNbins(); ibin++)
+    {
+      float x=h->GetBinCenter(ibin);
+      float y=h->GetBinContent(ibin);
+
+      hUp->SetBinContent(ibin, y*shiftUp->Eval(x) );
+      hDown->SetBinContent(ibin, y*shiftDown->Eval(x) );
+    }
+
+
+  if(debug)
+    {
+      TCanvas *c = new TCanvas("c","c");
+      c->Divide(1,2);
+      c->cd(1);
+      h->Draw("e1");  
+      h->GetXaxis()->SetTitle("Fantasy variable");
+      h->GetYaxis()->SetTitle("Events (a.u.)");
+      hUp->Draw("histsame");
+      hDown->Draw("histsame");
+      
+      c->cd(2);
+      shiftUp->Draw();
+      shiftUp->GetXaxis()->SetTitle("Fantasy variable");
+      shiftUp->GetYaxis()->SetTitle("Relative shift");
+      shiftDown->Draw("same");
+      shiftUp->GetYaxis()->SetRangeUser(-5,5);
+    }
+
+  delete shiftUp;
+  delete shiftDown;
+
+  toReturn.first=hUp;
+  toReturn.second=hDown;
+  return toReturn;
 }
 
 
