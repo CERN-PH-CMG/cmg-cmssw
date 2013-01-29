bed5c425c116fec50988e748774ab73252c45da4
anantoni <>
2013-01-29 14:35:36 +0000

    Unpublish.py now works properly

--- Production/python/unPublish.py
+++ Production/python/unPublish.py
@@ -3,15 +3,15 @@
 ## @ CERN, Meyrin
 ## April 4th 2012
 
-from CMGTools.Production.castorToDbsFormatter import CastorToDbsFormatter
+#from CMGTools.Production.castorToDbsFormatter import CastorToDbsFormatter
 from CMGTools.Production.savannahBrowser import SavannahBrowser
 from CMGTools.Production.cmgdbToolsApi import CmgdbToolsApi
 import os, getpass, sys, re
 from CMGTools.Production.nameOps import *
 from CMGTools.Production.findDSOnSav import *
+from CMGTools.Production.dataset import *
 
-
-def unPublish(dsName,fileown,user,password):
+def unPublish( dsName, fileown, user, password ):
     
     if re.search("---",dsName):
         fileown = getDbsUser(dsName)
@@ -21,7 +21,6 @@ def unPublish(dsName,fileown,user,password):
             return None
         else:
             dsName = dsName2
-    
     	
     if len(dsName.lstrip(os.sep).rstrip(os.sep).split(os.sep)) < 3:
     	print "Error, "+dsName+" is not valid, please use valid name"
@@ -30,27 +29,36 @@ def unPublish(dsName,fileown,user,password):
     	print "Dataset "+ dsName + "is a CMS base dataset and cannot be published, please use DAS."
     	return None
     
-    formatter = CastorToDbsFormatter(dsName,fileown)
-    procds =formatter.getDataset()
-
     taskID = None
     loginClear = False
     if not validLogin(user, password):
         print "Authentication Failed, exiting\n\n"
         sys.exit(1)
-    	
+
+    dataset = None
+    try:
+        dataset = Dataset( dsName, fileown ) #create the dataset
+    except: 
+        print "Dataset does not exist on eos, proceeding to unpublish"
+
+    cmgdbName = getCMGDBWithUser( dsName, fileown )
     print "\n------DataSet------\n"
-    print "CMGDB name:",getDbsWithUser(dsName, fileown)
-    print "EOS name:  ",dsName
+    print "CMGDB NAME  : " + cmgdbName
+    if dataset:
+	dataset.printInfo()
+    	if dataset.files:               #check if the dataset has root files
+        	print "\nERROR: Dataset has root files present, you can only unpublish empty datasets!" 
+#if it does cancel the unpublishing
+        	exit( -1 ) 
     	
     print "\n------Savanah------\n"
     	
     try:
-        if taskID is None: taskID = getTaskID(procds['PathList'][0], opts['category_id'], user, password, False)
+        if taskID is None: taskID = getTaskID( cmgdbName, opts['category_id'], user, password, False)
     except:
-        taskID = getTaskID(procds['PathList'][0], '103', user, password, False)
+        taskID = getTaskID( cmgdbName, '103', user, password, False)
     if taskID is not None:
-        browser = SavannahBrowser(user,password, taskID)
+        browser = SavannahBrowser( user, password, taskID )
         browser.closeTask()
         print "Task closed on Savannah"
         print "URL: https://savannah.cern.ch/task/?"+taskID
@@ -61,10 +69,10 @@ def unPublish(dsName,fileown,user,password):
     ID = None    
     cmgdbAPI=CmgdbToolsApi()
     cmgdbAPI.connect()
-    ID = cmgdbAPI.getDatasetIDWithName(procds['PathList'][0])
+    ID = cmgdbAPI.getDatasetIDWithName( cmgdbName )
     if ID is not None:
         try:
-            cmgdbAPI.closeDataset(procds['PathList'][0])
+            cmgdbAPI.closeDataset( cmgdbName )
             print "Dataset closed on CMGDB"
         except:
             print "Dataset failed to close"
