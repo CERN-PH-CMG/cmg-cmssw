96b294cec0499ce292cf03f4684742f2d4c90b8b
benitezj <>
2013-03-16 22:40:23 +0000

    Tau ES corrector

--- Utilities/plugins/plugins.h
+++ Utilities/plugins/plugins.h
@@ -2,6 +2,10 @@
 #include "CMGTools/Utilities/interface/RecoilCorrectedMETProducer.h"
 #include "CMGTools/Utilities/interface/MVAMETProducer.h"
 
+#include "CMGTools/Common/plugins/PhysicsObjectProducer.h"
+#include "CMGTools/Utilities/interface/TauESCorrector.h"
+
+
 typedef RecoilCorrectedMETProducer<cmg::TauMu> RecoilCorrectedMETProducerTauMu; 
 typedef RecoilCorrectedMETProducer<cmg::TauEle> RecoilCorrectedMETProducerTauEle; 
 typedef RecoilCorrectedMETProducer<cmg::MuEle> RecoilCorrectedMETProducerMuEle; 
@@ -11,3 +15,4 @@ typedef MVAMETProducer<cmg::TauMu>    MVAMETProducerTauMu;
 typedef MVAMETProducer<cmg::TauEle>    MVAMETProducerTauEle; 
 typedef MVAMETProducer<cmg::DiTau>    MVAMETProducerDiTau; 
 
+typedef PhysicsObjectProducer<cmg::TauESCorrector> TauESCorrectorProducer;
