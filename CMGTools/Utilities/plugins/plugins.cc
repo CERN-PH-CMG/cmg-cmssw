984cf2be5a0b56919987b89f459b7c15aa1b56c4
benitezj <>
2013-03-16 22:40:23 +0000

    Tau ES corrector

--- CMGTools/Utilities/plugins/plugins.cc
+++ CMGTools/Utilities/plugins/plugins.cc
@@ -1,6 +1,7 @@
 #include "CMGTools/Utilities/plugins/plugins.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+
 DEFINE_FWK_MODULE(RecoilCorrectedMETProducerTauMu); 
 DEFINE_FWK_MODULE(RecoilCorrectedMETProducerTauEle); 
 DEFINE_FWK_MODULE(RecoilCorrectedMETProducerMuEle); 
@@ -9,3 +10,5 @@ DEFINE_FWK_MODULE(RecoilCorrectedMETProducerDiTau);
 DEFINE_FWK_MODULE(MVAMETProducerTauMu);
 DEFINE_FWK_MODULE(MVAMETProducerTauEle);
 DEFINE_FWK_MODULE(MVAMETProducerDiTau);
+
+DEFINE_FWK_MODULE(TauESCorrectorProducer);
