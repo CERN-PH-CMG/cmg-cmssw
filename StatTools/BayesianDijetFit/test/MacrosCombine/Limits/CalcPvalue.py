d572cdcbc3d9370623259a800528c8ad19eb01da
mgouzevi <>
2013-03-17 19:25:03 +0000

    Update with many bins

--- StatTools/BayesianDijetFit/test/MacrosCombine/Limits/CalcPvalue.py
+++ StatTools/BayesianDijetFit/test/MacrosCombine/Limits/CalcPvalue.py
@@ -27,67 +27,71 @@ pvalWW=[]
 sigZZ=[]
 pvalZZ=[]
 
-for chan in channel:
+sChan=["WW","ZZ"]
+bin=[0,1,2,3,4,5]
+sBin=["m2mg0", "m2mg1", "m2mg2", "m1mg0", "m1mg1", "m0"]
 
-    masses =[1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0, 2300.0]
-#    masses =[1200.0]
-    for mass in masses:
-        print "mass = ",mass
+for ibin in bin:
+    sigWW=[]
+    pvalWW=[]
+
+    sigZZ=[]
+    pvalZZ=[]
+    for chan in channel:
+
+        masses =[1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0, 2300.0]
+
+        for mass in masses:
+            print "mass = ",mass
         
-        if chan == 0:
-            os.system("combine ../datacards/Xvv.mX"+str(mass)+"_WW_8TeV_channel2.txt -M ProfileLikelihood -v2 -m "+str(mass) + " --signif &>Xvv.mX"+str(mass)+"_WW_8TeV_channel2.out")
-            f_fit=file("Xvv.mX"+str(mass)+"_WW_8TeV_channel2.out")
+            os.system("combine ../datacards/Xvv.mX"+str(mass)+"_"+sChan[chan]+"_8TeV_channel"+str(ibin)+".txt -M ProfileLikelihood -v2 -m "+str(mass) + " --signif &>Xvv.mX"+str(mass)+"_"+sChan[chan]+"_8TeV_channel"+str(ibin)+".out")
+            f_fit=file("Xvv.mX"+str(mass)+"_"+sChan[chan]+"_8TeV_channel"+str(ibin)+".out")
 
-        if chan == 1:
-            os.system("combine ../datacards/Xvv.mX"+str(mass)+"_ZZ_8TeV_channel2.txt -M ProfileLikelihood -v2 -m "+str(mass) + " --signif &>Xvv.mX"+str(mass)+"_ZZ_8TeV_channel2.out")
-            f_fit=file("Xvv.mX"+str(mass)+"_ZZ_8TeV_channel2.out")
  
-        print "we look inside the fit output" 
-        found  = 0
-        for line in f_fit.readlines():
-#            print line
-            if "Significance:" in line:
-                found = 1
-#                limits+=[float(line.split(":")[2])]
-                print float(line.split(":")[1])
-                if chan == 0:
-                    sigWW+=[float(line.split(":")[1])]
-                if chan == 1:
-                    sigZZ+=[float(line.split(":")[1])] 
-            if "(p-value " in line:
-                print float(line.split("=")[1].split(")")[0])
-                if chan == 0:
-                    pvalWW+=[float(line.split("=")[1].split(")")[0])]
-                if chan == 1:
-                    pvalZZ+=[float(line.split("=")[1].split(")")[0])] 
-        f_fit.close()
+            print "we look inside the fit output for bin "+str(ibin)
+            for line in f_fit.readlines():
+                #            print line
+                if "Significance:" in line:
+                    #                limits+=[float(line.split(":")[2])]
+                    print float(line.split(":")[1])
+                    if chan == 0:
+                        sigWW+=[float(line.split(":")[1])]
+                    if chan == 1:
+                        sigZZ+=[float(line.split(":")[1])] 
+                if "(p-value " in line:
+                    print float(line.split("=")[1].split(")")[0])
+                    if chan == 0:
+                        pvalWW+=[float(line.split("=")[1].split(")")[0])]
+                    if chan == 1:
+                        pvalZZ+=[float(line.split("=")[1].split(")")[0])] 
+                    f_fit.close()
 
 
-print "sigWW",[(masses[i],sigWW[i]) for i in range(len(masses))]
-print "sigZZ",[(masses[i],sigZZ[i]) for i in range(len(masses))]
+    print "sigWW",[(masses[i],sigWW[i]) for i in range(len(masses))]
+    print "sigZZ",[(masses[i],sigZZ[i]) for i in range(len(masses))]
 
-print "pvalWW",[(masses[i],pvalWW[i]) for i in range(len(masses))]
-print "pvalZZ",[(masses[i],pvalZZ[i]) for i in range(len(masses))]
+    print "pvalWW",[(masses[i],pvalWW[i]) for i in range(len(masses))]
+    print "pvalZZ",[(masses[i],pvalZZ[i]) for i in range(len(masses))]
 
-sSigWW=[(masses[i],sigWW[i]) for i in range(len(masses))]
-sSigZZ=[(masses[i],sigZZ[i]) for i in range(len(masses))]
+    sSigWW=[(masses[i],sigWW[i]) for i in range(len(masses))]
+    sSigZZ=[(masses[i],sigZZ[i]) for i in range(len(masses))]
 
-sPvalWW=[(masses[i],pvalWW[i]) for i in range(len(masses))]
-sPvalZZ=[(masses[i],pvalZZ[i]) for i in range(len(masses))]
+    sPvalWW=[(masses[i],pvalWW[i]) for i in range(len(masses))]
+    sPvalZZ=[(masses[i],pvalZZ[i]) for i in range(len(masses))]
 
-fWW = open("Xvv_WW_8TeV_Pval_channel2.txt", "w")
-fWW.write(str(sPvalWW))
-fWW.close()
+    fWW = open("Xvv_WW_8TeV_Pval_channel"+str(ibin)+"_"+sBin[ibin]+".txt", "w")
+    fWW.write(str(sPvalWW))
+    fWW.close()
 
-fZZ = open("Xvv_ZZ_8TeV_Pval_channel2.txt", "w")
-fZZ.write(str(sPvalZZ))
-fZZ.close()
+    fZZ = open("Xvv_ZZ_8TeV_Pval_channel"+str(ibin)+"_"+sBin[ibin]+".txt", "w")
+    fZZ.write(str(sPvalZZ))
+    fZZ.close()
 
 
-fWW = open("Xvv_WW_8TeV_Sig_channel2.txt", "w")
-fWW.write(str(sSigWW))
-fWW.close()
+    fWW = open("Xvv_WW_8TeV_Sig_channel"+str(ibin)+"_"+sBin[ibin]+".txt", "w")
+    fWW.write(str(sSigWW))
+    fWW.close()
 
-fZZ = open("Xvv_ZZ_8TeV_Sig_channel2.txt", "w")
-fZZ.write(str(sSigZZ))
-fZZ.close()
+    fZZ = open("Xvv_ZZ_8TeV_Sig_channel"+str(ibin)+"_"+sBin[ibin]+".txt", "w")
+    fZZ.write(str(sSigZZ))
+    fZZ.close()
--- StatTools/BayesianDijetFit/test/MacrosCombine/Limits/CompileSignalStrength.py
+++ StatTools/BayesianDijetFit/test/MacrosCombine/Limits/CompileSignalStrength.py
@@ -22,19 +22,31 @@ gStyle.SetLegendBorderSize(0)
 
 channel=[0, 1]
 sChan=["WW","ZZ"]
+bin=[0,1,2,3,4,5]
+sBin=["m2mg0", "m2mg1", "m2mg2", "m1mg0", "m1mg1", "m0"]
+muExp=[]
 
-for chan in channel:
+for ibin in bin:
+    for chan in channel:
 
-    masses =[1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0, 2300.0]
+        masses =[1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0, 2300.0]
 
-    for mass in masses:
-        print "mass = ",mass
+        muExp=[]
+
+        for mass in masses:
+            print "mass = ",mass
         
-        f_card=file("../datacards/Xvv.mX"+str(mass)+"_"+ sChan[chan] +"_8TeV_channel2.txt")
+            f_card=file("../datacards/Xvv.mX"+str(mass)+"_"+ sChan[chan] +"_8TeV_channel"+str(bin[ibin])+".txt")
 
-        for line in f_card.readlines():
-#            print line
-            if "rate" in line:
-                print float(line.split("                        ")[1].split(" ")[0])
+            for line in f_card.readlines():
+                #            print line
+                if "rate" in line:
+                    print float(line.split("                        ")[1].split(" ")[0])
+                    muExp+=[float(line.split("                        ")[1].split(" ")[0])]
+            f_card.close()
 
-        f_card.close()
+        sMuExp=[(masses[i],muExp[i]) for i in range(len(masses))]
+
+        f_out=open("Xvv.mX_"+sChan[chan]+"_MuExp_8TeV_channel"+str(bin[ibin])+"_"+sBin[ibin]+".txt", "w")
+        f_out.write(str(sMuExp))
+        f_out.close()
