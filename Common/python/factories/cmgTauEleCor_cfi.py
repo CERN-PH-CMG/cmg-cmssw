b0ff68fef8f2aaec19addaa4b416283fe6567c54
hinzmann <>
2013-03-21 11:51:40 +0000

    enable tau+met shift in ditau update factory

--- Common/python/factories/cmgTauEleCor_cfi.py
+++ Common/python/factories/cmgTauEleCor_cfi.py
@@ -5,7 +5,14 @@ cmgTauEleCorFactory = cms.PSet(
     leg1Collection = cms.InputTag(''),
     leg2Collection = cms.InputTag(''),
     metCollection = cms.InputTag('recoilCorrectedMET'),
-    diObjectCollection = cms.InputTag('cmgTauEleSel')
+    diObjectCollection = cms.InputTag('cmgTauEleSel'),
+    nSigma              = cms.double(0),
+    uncertainty         = cms.double(0.03),
+    shift1ProngNoPi0    = cms.double(0.),
+    shift1Prong1Pi0     = cms.double(0.),
+    ptDependence1Pi0    = cms.double(0.),
+    shift3Prong         = cms.double(0.),
+    ptDependence3Prong  = cms.double(0.)
 )
 
 cmgTauEleCor = cms.EDFilter(
