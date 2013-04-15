ba6392d0085bd9f73757590f01b17463a1cb671c
lucieg <>
2013-04-15 12:33:12 +0000

    added isr weight and genParticles for stop polarization

--- Susy/python/common/susy_cff.py
+++ Susy/python/common/susy_cff.py
@@ -7,6 +7,8 @@ from CMGTools.Susy.common.dumpPDFWeights_cff import *
 from CMGTools.Susy.common.simpleGenInfo_cff import *
 from CMGTools.Susy.common.emulate2011Trigger_cff import *
 from CMGTools.Susy.common.topGenInfo_cff import *
+from CMGTools.Susy.common.isrWeight_cfi import *
+from CMGTools.Susy.common.genParticlesStatus3_cfi import *
 
 from CMGTools.RootTools.utils.vertexWeight.vertexWeights2012_cfi import *
 
@@ -18,7 +20,9 @@ susyGenSequence  = cms.Sequence(
     vertexWeightSummer12MCICHEPData+
     vertexWeightSummer12MC53XHCPData+
     vertexWeightSummer12MC53X2012ABCDData+
-    vertexWeightSummer12MC53X2012BCDData
+    vertexWeightSummer12MC53X2012BCDData+
+    isrWeight +
+    genParticlesStatus3
     )
 
 susyDataSequence  = cms.Sequence(
