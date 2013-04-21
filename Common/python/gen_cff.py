1db26482025460c8f826dec118c5e06a773a0aac
cbern <>
2013-04-21 11:14:36 +0000

    removed useless include

--- Common/python/gen_cff.py
+++ Common/python/gen_cff.py
@@ -1,19 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-#from CMGTools.Common.generator.genParticlesStatus3_cfi import *
-#from CMGTools.Common.generator.genLeptons_cff import *
 from CMGTools.Common.generator.genParticlesPruned_cfi import *
 from CMGTools.Common.generator.genJets_cff import *
 from CMGTools.Common.generator.listParticles_cfi import *
-from CMGTools.RootTools.utils.vertexWeight.vertexWeight_cff import *
-
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
 genSequence = cms.Sequence(
-    #+genParticlesStatus3
     genParticlesPruned
-    # + genLeptonsSequence
     + genJetsSequence
     + listParticles
-    # + vertexWeightSequence
     )
