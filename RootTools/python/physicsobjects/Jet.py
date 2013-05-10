6c89839b4b1c98dbeaf8ec2932e69b686d271767
steggema <>
2013-05-10 13:38:28 +0000

    Add new 53X PU ID working point

--- RootTools/python/physicsobjects/Jet.py
+++ RootTools/python/physicsobjects/Jet.py
@@ -7,22 +7,21 @@ loose_WP = [
     (3.0, 5.0, -0.77),
     ]
 
-## integrate these new working points for PU jet ID. 
-## loose_53X_WP = cms.PSet(
-##     #4 Eta Categories  0-2.5 2.5-2.75 2.75-3.0 3.0-5.0
-##     #Loose Id
-##     Pt2030_Loose   = cms.vdouble(-0.63,-0.35,-0.28,-0.27),
-##     Pt3050_Loose   = cms.vdouble(-0.63,-0.35,-0.28,-0.27)
-##     )
+# Working point 2 May 2013 (Phil via H2tau list)
+loose_53X_WP = [
+    (0, 2.5, -0.63),
+    (2.5, 2.75, -0.60),
+    (2.75, 3.0, -0.55),
+    (3.0, 5.0, -0.45),
+    ]
 
 
-
-class Jet( PhysicsObject):
+class Jet(PhysicsObject):
 
     def looseJetId(self):
         '''PF Jet ID
         '''
-        self.pfJetIdPassed = ( abs(self.eta()) <= 2.4                                             and 
+        pfJetIdPassed = ( abs(self.eta()) <= 2.4                                             and 
                               (self.component(1).fraction()                                > 0    and
                                self.component(2).fraction()                                < 0.99 and 
                                self.component(4).fraction()                                < 0.99 and 
@@ -35,17 +34,27 @@ class Jet( PhysicsObject):
                                self.nConstituents()                                        > 1    ) 
                             ) 
         #return self.getSelection('cuts_looseJetId') 
-        return self.pfJetIdPassed 
+        return pfJetIdPassed 
 
-    def puJetId(self):
-        '''Full mva PU jet id with old loose working point used by Josh'''
+    def puJetId(self, wp53x=False):
+        '''Full mva PU jet id (default: old loose working point used by Josh)'''
+
         puMva = self.puMva('full53x')
+        wp = loose_53X_WP
         eta = abs(self.eta())
-        for etamin, etamax, cut in loose_WP:
+        
+        if not wp53x:
+            puMva = self.puMva('full')
+            wp = loose_WP
+
+        for etamin, etamax, cut in wp:
             if not(eta>=etamin and eta<etamax):
                 continue
             return puMva>cut
         
+    def puJetId53X(self):
+        '''53X full mva PU jet id with new working point by Phil (2 May 2013)'''
+        return self.puJetId(wp53x=True)
 
 
 class GenJet( PhysicsObject):
