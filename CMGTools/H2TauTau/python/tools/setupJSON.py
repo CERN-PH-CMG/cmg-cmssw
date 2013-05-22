fd911616a43166672fc30465a86b9779ac053c71
cbern <>
2013-05-22 15:38:10 +0000

    cured a but in json picking. updated the list of official jsons. added testing mode in officialJSONS.py

--- CMGTools/H2TauTau/python/tools/setupJSON.py
+++ CMGTools/H2TauTau/python/tools/setupJSON.py
@@ -1,16 +1,17 @@
 import os 
 from CMGTools.Common.Tools.applyJSON_cff import *
-from CMGTools.H2TauTau.tools.jsonPick import *
+from CMGTools.RootTools.json.jsonPick import *
+from CMGTools.H2TauTau.officialJSONS import jsonMap
 
 def setupJSON( process ):
 
     print 'setting up JSON:'
 
     fileName = process.source.fileNames[0]
-    print fileName
     # in case filename is a local filename, removing CMGLOCALBASEDIR
     #Jose: dont know where this var is set
     #fileName = fileName.replace( os.environ['CMGLOCALBASEDIR'],'' ) 
-    json = jsonPick( fileName, True )
+    json = jsonPick( fileName, jsonMap )
+    print json
     applyJSON(process, json )
     return json
