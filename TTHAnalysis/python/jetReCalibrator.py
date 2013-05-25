112960c1fbdf20f016cd24f0e0ec7254d585f1fd
gpetrucc <>
2013-05-25 23:13:27 +0000

    add option to do JES uncertainty, propagare correction to MET if requested (though missing support upstream)

--- TTHAnalysis/python/jetReCalibrator.py
+++ TTHAnalysis/python/jetReCalibrator.py
@@ -18,16 +18,33 @@ class JetReCalibrator:
             self.vPar.push_back(self.ResJetPar);
         #Step3 (Construct a FactorizedJetCorrector object) 
         self.JetCorrector = ROOT.FactorizedJetCorrector(self.vPar)
-    def correct(self,jet,rho):
+        self.JetUncertainty = ROOT.JetCorrectionUncertainty("%s/%s_Uncertainty_%s.txt" % (path,globalTag,jetFlavour));
+    def correct(self,jet,rho,delta=0,metShift=[0,0]):
         self.JetCorrector.setJetEta(jet.eta())
         self.JetCorrector.setJetPt(jet.pt() * jet.rawFactor())
         self.JetCorrector.setJetA(jet.jetArea())
         self.JetCorrector.setRho(rho)
         corr = self.JetCorrector.getCorrection()
+        self.JetUncertainty.setJetEta(jet.eta())
+        self.JetUncertainty.setJetPt(corr * jet.pt() * jet.rawFactor())
+        try:
+            jet.jetEnergyCorrUncertainty = self.JetUncertainty.getUncertainty(True) 
+        except RuntimeError, r:
+            print "Caught %s when getting uncertainty for jet of pt %.1f, eta %.2f\n" % (r,corr * jet.pt() * jet.rawFactor(),jet.eta())
+            jet.jetEnergyCorrUncertainty = 0.5
+        if delta != 0:
+            #print "   jet with corr pt %6.2f has an uncertainty %.2f " % (jet.pt()*jet.rawFactor()*corr, jet.jetEnergyCorrUncertainty)
+            corr *= max(0, 1+delta*jet.jetEnergyCorrUncertainty)
         #print "   jet with raw pt %6.2f eta %+5.3f phi %+5.3f: previous corr %.4f, my corr %.4f " % (jet.pt()*jet.rawFactor(), jet.eta(), jet.phi(), 1./jet.rawFactor(), corr)
+        if jet.pt() > 10:
+            metShift[0] += jet.px()
+            metShift[1] += jet.py()
         jet.setP4(jet.p4() * (corr * jet.rawFactor()))
         # overload rawFactor() since we don't have setters
         jet._rawFactor = 1.0/corr
         jet.rawFactor = types.MethodType(lambda self : self._rawFactor, jet, jet.__class__)
+        if jet.pt() > 10:
+            metShift[0] -= jet.px()
+            metShift[1] -= jet.py()
         
 
