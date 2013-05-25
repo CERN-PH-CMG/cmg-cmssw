0e2bbb79be386efcee8de105efc3c8bd48baaefb
gpetrucc <>
2013-05-25 23:14:25 +0000

    switch to 2d histograms, add 3l tight

--- CMGTools/TTHAnalysis/python/tools/lepMVA_SF.py
+++ CMGTools/TTHAnalysis/python/tools/lepMVA_SF.py
@@ -3,40 +3,24 @@ from CMGTools.TTHAnalysis.treeReAnalyzer import *
 import re,os
 
 class LepMVA_SF_one:
-    def __init__(self,hists,what,etaEdge):
-        #print "initializing %s %s from %s" % (what,etaEdge,hists.GetName())
-        self._what = what
-        self._etaEdge = etaEdge
-        self._th1s = {}
-        for X in "data", "mc":
-            for B in "ptb", "pte":
-                hist = hists.Get("%s_%s%s" % (what,X,B))
-                if not bool(hist):
-                    hists.ls()
-                    raise RuntimeError, "Missing %s_%s%s in %s " % (what,X,B,hists.GetName())
-                self._th1s[X+B] = hist.Clone("LepMVA_SF_one_"+hist.GetName())
-                #print hists, hists.GetName(), hist
-                #print "%s_%s%s(5) = %s = %s\n" % (what,X,B, hist.GetBinContent(5), self._th1s[X+B].GetBinContent(5))
-                
-    def fetch(self,hname,pt):
-        h = self._th1s[hname]
-        bin = max(1,min(h.GetNbinsX(), h.GetXaxis().FindBin(pt)))
-        #print "fetching bin %d of %s for pt=%s; bin 5 has %s " % (bin,h.GetName(),pt,h.GetBinContent(5))
-        return h.GetBinContent(bin)
+    def __init__(self,h2d):
+        self.h2d = h2d.Clone("LepMVA_one_"+h2d.GetName())
+        self.h2d.SetDirectory(None)
+        self.ptmin = h2d.GetXaxis().GetXmin()+1e-3
+        self.ptmax = h2d.GetXaxis().GetXmax()-1e-3
+        self.etamin = h2d.GetYaxis().GetXmin()+1e-3
+        self.etamax = h2d.GetYaxis().GetXmax()-1e-3
+    def fetch(self,pt,eta):
+        xbin = self.h2d.GetXaxis().FindBin(min(max(pt,self.ptmin),self.ptmax))
+        ybin = self.h2d.GetYaxis().FindBin(min(max(eta,self.etamin),self.etamax))
+        return self.h2d.GetBinContent(xbin,ybin)
     def __call__(self,lepton):
-        B = "ptb" if abs(lepton.eta) < self._etaEdge else "pte"
-        edata = self.fetch("data"+B, lepton.pt)
-        emc   = self.fetch("mc"  +B, lepton.pt)
-        sf = edata/emc
-        #print "%s eff(%s) for id %d pt %.1f: data %.3f, mc %.3f --> sf %.3f %.3f" % (self._what,B,abs(lepton.pdgId),lepton.pt,edata,emc,sf,self._etaEdge)
-        return sf
+        return self.fetch(lepton.pt, lepton.eta)
  
 class LepMVA_SF_both:
     def __init__(self,hists_el,hists_mu,what):
-        #print "reading EL from ",hists_el.GetName()
-        #print "reading MU from ",hists_mu.GetName()
-        self.sf_el = LepMVA_SF_one(hists_el,what,1.479)
-        self.sf_mu = LepMVA_SF_one(hists_mu,what,1.5)
+        self.sf_el = LepMVA_SF_one(hists_el.Get(what))
+        self.sf_mu = LepMVA_SF_one(hists_mu.Get(what))
     def __call__(self,lepton):
         sf = self.sf_mu if abs(lepton.pdgId) == 13 else self.sf_el
         return sf(lepton)
@@ -56,18 +40,22 @@ class AllLepSFs:
     def __init__(self):
         self.f_el = ROOT.TFile("%s/src/CMGTools/TTHAnalysis/data/MVAandTigthChargeSF_ele.root" % os.environ['CMSSW_BASE'])
         self.f_mu = ROOT.TFile("%s/src/CMGTools/TTHAnalysis/data/MVAandTigthChargeSF_mu.root"  % os.environ['CMSSW_BASE'])
-        self.sf_mvaLoose = LepMVA_SF_Event(self.f_el,self.f_mu,"LepMVALooseEff")
-        self.sf_mvaTight = LepMVA_SF_Event(self.f_el,self.f_mu,"LepMVATightEff")
-        self.sf_tightCharge = LepMVA_SF_Event(self.f_el,self.f_mu,"TightChargeEff")
+        self.sf_mvaLoose = LepMVA_SF_Event(self.f_el,self.f_mu,"LepMVALooseSF2D")
+        self.sf_mvaTight = LepMVA_SF_Event(self.f_el,self.f_mu,"LepMVATightSF2D")
+        self.sf_tightCharge = LepMVA_SF_Event(self.f_el,self.f_mu,"TightChargeSF2D")
+        self.f_el.Close()
+        self.f_mu.Close()
     def listBranches(self):
-        return [ 'LepMVALoose_2l', 'LepMVALoose_3l', 'LepMVALoose_4l', 'LepMVATight_2l', 'LepTightCharge_2l' ]
+        return [ 'LepMVALoose_2l', 'LepMVALoose_3l', 'LepMVALoose_4l', 'LepMVATight_2l', 'LepMVATight_3l', 'LepTightCharge_2l', 'LepTightCharge_3l' ]
     def __call__(self,event):
         return {
             'LepMVALoose_2l' : self.sf_mvaLoose(event,2), 
             'LepMVALoose_3l' : self.sf_mvaLoose(event,3), 
             'LepMVALoose_4l' : self.sf_mvaLoose(event,4), 
             'LepMVATight_2l' : self.sf_mvaTight(event,2), 
+            'LepMVATight_3l' : self.sf_mvaTight(event,2), 
             'LepTightCharge_2l' : self.sf_tightCharge(event,2),
+            'LepTightCharge_3l' : self.sf_tightCharge(event,2),
         }
 
 if __name__ == '__main__':
