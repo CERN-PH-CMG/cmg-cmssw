cd78b21eb03df630d8f12d05472298893717d081
gpetrucc <>
2013-05-31 16:49:36 +0000

    don't kill AFS

--- TTHAnalysis/cfg/fastSim/lhe2gen2.sh
+++ TTHAnalysis/cfg/fastSim/lhe2gen2.sh
@@ -4,7 +4,7 @@ TMPDIR=$PWD
 cd /afs/cern.ch/user/g/gpetrucc/w/GENS/CMSSW_5_3_5/src
 export SCRAM_ARCH=slc5_amd64_gcc462
 eval $(scramv1 runtime -sh)
-cd /afs/cern.ch/user/g/gpetrucc/ttH/CMGTools/CMSSW_5_3_5/src/CMGTools/TTHAnalysis/cfg/fastSim
+SRC=/afs/cern.ch/user/g/gpetrucc/ttH/CMGTools/CMSSW_5_3_5/src/CMGTools/TTHAnalysis/cfg/fastSim;
 #### CREATE CFG
 M="default"; if [[ "$1" == "--up" || "$1" == "--down" || "$1" == "--nojets" || "$1" == "--xqtup" || "$1" == "--xqtdown"  ]]; then M="$1"; shift; fi;
 OUTFILE=$1
@@ -33,37 +33,41 @@ while [[ "$1" != "" ]]; do
     fi
 done
 
-cat Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff_py_noCMS_GEN.py > jobs/$OUTBASE.cfg.py
-echo "process.source.fileNames = [ $INFILES ]"                   >> jobs/$OUTBASE.cfg.py
-echo "process.AODSIMoutput.fileName = '$TMPDIR/$OUTBASE.root'"   >> jobs/$OUTBASE.cfg.py
+cd $TMPDIR;
+
+cat $SRC/Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff_py_noCMS_GEN.py > $OUTBASE.cfg.py
+echo "process.source.fileNames = [ $INFILES ]"                   >> $OUTBASE.cfg.py
+echo "process.AODSIMoutput.fileName = '$TMPDIR/$OUTBASE.root'"   >> $OUTBASE.cfg.py
 NJETS=99
 if   echo $OUTFILE | grep '_012jets' -q ; then NJETS=2;
 elif echo $OUTFILE | grep '_01jets'  -q ; then NJETS=1;
 else echo "So il c...o io quanti jet vuoi. NJETS=$NJETS"; fi
-echo "process.generator.jetMatching.MEMAIN_maxjets = $NJETS"   >> jobs/$OUTBASE.cfg.py
+echo "process.generator.jetMatching.MEMAIN_maxjets = $NJETS"   >> $OUTBASE.cfg.py
  
 if [[ "$M" == "--up" ]]; then
     echo "Matching scale UP"
-    echo "process.generator.PythiaParameters.processParameters += [ 'PARP(64)=4.', 'PARP(72)=0.125' ]"   >> jobs/$OUTBASE.cfg.py
+    echo "process.generator.PythiaParameters.processParameters += [ 'PARP(64)=4.', 'PARP(72)=0.125' ]"   >> $OUTBASE.cfg.py
 elif [[ "$M" == "--down" ]]; then
     echo "Matching scale DOWN"
-    echo "process.generator.PythiaParameters.processParameters += [ 'PARP(64)=0.25', 'PARP(72)=0.5' ]"   >> jobs/$OUTBASE.cfg.py
+    echo "process.generator.PythiaParameters.processParameters += [ 'PARP(64)=0.25', 'PARP(72)=0.5' ]"   >> $OUTBASE.cfg.py
 elif [[ "$M" == "--xqtup" ]]; then
     echo "Matching cut DOWN"
-    echo "process.generator.jetMatching.MEMAIN_qcut = 20"   >> jobs/$OUTBASE.cfg.py
+    echo "process.generator.jetMatching.MEMAIN_qcut = 20"   >> $OUTBASE.cfg.py
 elif [[ "$M" == "--xqtdown" ]]; then
     echo "Matching cut DOWN"
-    echo "process.generator.jetMatching.MEMAIN_qcut = 5"   >> jobs/$OUTBASE.cfg.py
+    echo "process.generator.jetMatching.MEMAIN_qcut = 5"   >> $OUTBASE.cfg.py
 elif [[ "$M" == "--nojets" ]]; then
     echo "No jet matching"
-    echo "del process.generator.jetMatching"   >> jobs/$OUTBASE.cfg.py
+    echo "del process.generator.jetMatching"   >> $OUTBASE.cfg.py
 fi
-cat >> jobs/$OUTBASE.cfg.py <<_EOF_
+cat >> $OUTBASE.cfg.py <<_EOF_
 ## Scramble
 import random
 rnd = random.SystemRandom()
 for X in process.RandomNumberGeneratorService.parameterNames_(): 
    if X != 'saveFileName': getattr(process.RandomNumberGeneratorService,X).initialSeed = rnd.randint(1,99999999)
 _EOF_
-cmsRun jobs/$OUTBASE.cfg.py 2>&1 | tee jobs/$OUTBASE.log
+cmsRun $OUTBASE.cfg.py 2>&1 | tee $OUTBASE.log
 test -f $TMPDIR/$OUTBASE.root && cmsStageIn $TMPDIR/$OUTBASE.root $OUTFILE
+~/sh/skimreport $OUTBASE.log > $SRC/jobs/$OUTBASE.skimreport
+gzip $OUTBASE.log && cp -v $OUTBASE.log.gz $SRC/jobs/
