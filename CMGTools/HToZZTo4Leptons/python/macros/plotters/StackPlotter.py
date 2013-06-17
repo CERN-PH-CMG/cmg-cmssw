fd3238a966c862bc68c01f0dbefe42c60fcf1b2f
bachtis <>
2013-06-17 20:47:14 +0000

    *** empty log message ***

--- CMGTools/HToZZTo4Leptons/python/macros/plotters/StackPlotter.py
+++ CMGTools/HToZZTo4Leptons/python/macros/plotters/StackPlotter.py
@@ -21,8 +21,10 @@ def convertToPoisson(h):
             graph.SetPoint(i-1,x,y)
             graph.SetPointEYlow(i-1,yLow)
             graph.SetPointEYhigh(i-1,yHigh)
-            graph.SetPointEXlow(i-1,x-xLow)
-            graph.SetPointEXhigh(i-1,xHigh-x)
+#            graph.SetPointEXlow(i-1,x-xLow)
+#            graph.SetPointEXhigh(i-1,xHigh-x)
+            graph.SetPointEXlow(i-1,0.0)
+            graph.SetPointEXhigh(i-1,0.0)
 
 
     graph.SetMarkerStyle(20)
@@ -47,6 +49,24 @@ class StackPlotter(object):
 
     def drawStack(self,var,cut,lumi,bins,mini,maxi,titlex = "", units = ""):
         canvas = ROOT.TCanvas("canvas","")
+        ROOT.gStyle.SetOptStat(0)
+        ROOT.gStyle.SetOptTitle(0)
+        canvas.Range(-68.75,-7.5,856.25,42.5)
+        canvas.SetFillColor(0)
+        canvas.SetBorderMode(0)
+        canvas.SetBorderSize(2)
+        canvas.SetTickx(1)
+        canvas.SetTicky(1)
+        canvas.SetLeftMargin(0.15)
+        canvas.SetRightMargin(0.05)
+        canvas.SetTopMargin(0.05)
+        canvas.SetBottomMargin(0.15)
+        canvas.SetFrameFillStyle(0)
+        canvas.SetFrameBorderMode(0)
+        canvas.SetFrameFillStyle(0)
+        canvas.SetFrameBorderMode(0)
+
+
         canvas.cd()
         hists=[]
         stack = ROOT.THStack("stack","")
@@ -75,6 +95,7 @@ class StackPlotter(object):
                 hists.append(hist)
                 data=hist
                 dataG=convertToPoisson(hist)
+                dataG.SetLineWidth(1)
                 print label+" : %f\n" % hist.Integral()
 
        
@@ -83,12 +104,31 @@ class StackPlotter(object):
             datamax = ROOT.Math.chisquared_quantile_c((1-0.6827)/2.,2*(data.GetMaximum()+1))/2.
 
             frame = canvas.DrawFrame(mini,0.0,maxi,max(stack.GetMaximum(),datamax)*1.20)
+            frame.GetXaxis().SetLabelFont(42)
+            frame.GetXaxis().SetLabelOffset(0.007)
+            frame.GetXaxis().SetLabelSize(0.045)
+            frame.GetXaxis().SetTitleSize(0.05)
+            frame.GetXaxis().SetTitleOffset(1.15)
+            frame.GetXaxis().SetTitleFont(42)
+            frame.GetYaxis().SetLabelFont(42)
+            frame.GetYaxis().SetLabelOffset(0.007)
+            frame.GetYaxis().SetLabelSize(0.045)
+            frame.GetYaxis().SetTitleSize(0.05)
+            frame.GetYaxis().SetTitleOffset(1.4)
+            frame.GetYaxis().SetTitleFont(42)
+            frame.GetZaxis().SetLabelFont(42)
+            frame.GetZaxis().SetLabelOffset(0.007)
+            frame.GetZaxis().SetLabelSize(0.045)
+            frame.GetZaxis().SetTitleSize(0.05)
+            frame.GetZaxis().SetTitleFont(42)
+
+
             if len(units)>0:
-                frame.GetXaxis().SetTitle(titlex + " [" +units+"]")
+                frame.GetXaxis().SetTitle(titlex + " (" +units+")")
+                frame.GetYaxis().SetTitle("Events / "+str((maxi-mini)/bins)+ " "+units)
             else:    
                 frame.GetXaxis().SetTitle(titlex)
-
-            frame.GetYaxis().SetTitle("Events")
+                frame.GetYaxis().SetTitle("Events")
 
             frame.Draw()
             stack.Draw("A,HIST,SAME")
@@ -98,13 +138,28 @@ class StackPlotter(object):
         else:
             stack.Draw()
         #make legend
-        legend = ROOT.TLegend(0.6,0.6,0.8,0.9)
+#        legend = ROOT.TLegend(0.5,0.6,0.8,0.9)
+        legend = ROOT.TLegend(0.62,0.6,0.92,0.90,"","brNDC")
+	legend.SetBorderSize(0)
+	legend.SetLineColor(1)
+	legend.SetLineStyle(1)
+	legend.SetLineWidth(1)
+	legend.SetFillColor(0)
+	legend.SetFillStyle(0)
+	legend.SetTextFont(42)
+
         legend.SetFillColor(ROOT.kWhite)
         for (histo,label,typeP) in reversed(zip(hists,self.labels,self.types)):
-            if typeP != "data":
+            if typeP != "data" and typeP !='signal':
                 legend.AddEntry(histo,label,"f")
-            else:
+            elif typeP == 'data':
                 legend.AddEntry(histo,label,"p")
+
+        for (histo,label,typeP) in reversed(zip(hists,self.labels,self.types)):
+            if typeP == "signal":
+                legend.AddEntry(histo,label,"f")
+
+
  #       ROOT.SetOwnership(legend,False)
 
         legend.Draw()
@@ -121,13 +176,23 @@ class StackPlotter(object):
         print "Bkg    = %f" %(background)
         if data is not None:
             print "Observed = %f"%(data.Integral())
+
+	pt =ROOT.TPaveText(0.1577181,0.9562937,0.9580537,0.9947552,"brNDC")
+	pt.SetBorderSize(0)
+	pt.SetTextAlign(12)
+	pt.SetFillStyle(0)
+	pt.SetTextFont(42)
+	pt.SetTextSize(0.03)
+	text = pt.AddText(0.01,0.5,"CMS preliminary")
+	text = pt.AddText(0.3,0.6,"#sqrt{s} = 7 TeV, L = 5.1 fb^{-1}  #sqrt{s} = 8 TeV, L = 19.6 fb^{-1}")
+	pt.Draw()   
         
 
-        latex1 = ROOT.TLatex(frame.GetXaxis().GetXmin()*1.01,frame.GetYaxis().GetXmax()*1.01,'CMS Preliminary 2011-2012, #sqrt{s} = 7+8 TeV')
-        latex1.SetTextSize(0.037)
-        latex1.Draw()
+#        latex1 = ROOT.TLatex(frame.GetXaxis().GetXmin()*1.01,frame.GetYaxis().GetXmax()*1.01,'CMS Preliminary 2011-2012, #sqrt{s} = 7+8 TeV')
+#        latex1.SetTextSize(0.037)
+#        latex1.Draw()
 
-        plot={'canvas':canvas,'stack':stack,'legend':legend,'data':data,'dataG':dataG,'latex1':latex1}
+        plot={'canvas':canvas,'stack':stack,'legend':legend,'data':data,'dataG':dataG,'latex1':pt}
 
 
         return plot
