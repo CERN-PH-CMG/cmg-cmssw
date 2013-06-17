0cd1cbc26f2146e98287e2981d2d6327f7d996da
bachtis <>
2013-06-17 21:22:23 +0000

    *** empty log message ***

--- CMGTools/HToZZTo4Leptons/python/samples/getFiles.py
+++
@@ -1,8 +0,0 @@
-def getFiles(dataset, user, pattern):
-    from CMGTools.Production.datasetToSource import datasetToSource
-    # print 'getting files for', dataset,user,pattern
-    ds = datasetToSource( user, dataset, pattern, True )
-    files = ds.fileNames
-    return ['root://eoscms//eos/cms%s' % f for f in files]
-
-
