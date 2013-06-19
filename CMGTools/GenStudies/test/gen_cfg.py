bc1a212582f6ca8699a91ed695ffdf78dee8c053
ytakahas <>
2013-06-19 20:57:06 +0000

    Modified according to the implementation of the DiLepton class to fit the VBF class

--- CMGTools/GenStudies/test/gen_cfg.py
+++ CMGTools/GenStudies/test/gen_cfg.py
@@ -12,7 +12,7 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(100)
 )
 
 
@@ -25,11 +25,15 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 
 # here, you can also read a LHE file as in Phil's example
 # generate the LHE file yourself with POWHEG
-process.source = cms.Source("EmptySource")
+#process.source = cms.Source("EmptySource")
+process.source = cms.Source("LHESource",
+                            fileNames = cms.untracked.vstring('/store/lhe/8245/pwgevents-0001.lhe'))
+                                                                  
 
 # if you read LHE, you need to hadronize the event with pythia,
 # so use Phil's hadronizer instead of this pythia generator
-process.load('CMGTools.GenStudies.generators.pp_ttbar_cfi')
+#process.load('CMGTools.GenStudies.generators.pp_ttbar_cfi')
+process.load('Configuration.GenProduction.EightTeV.POWHEG_PYTHIA6_Tauola_H_tautau_8TeV_cff')
 
 #atlas = True
 atlas = False
@@ -42,6 +46,8 @@ if atlas:
 # gen particle for jet selection
 # gen jet creation
 # I could do that with my system.
+
+                                                                  
 
 process.load('PhysicsTools.HepMCCandAlgos.genParticles_cfi')
 
