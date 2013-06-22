000d3f8fe354489ba5d854d8f80fd183d6856c39
manzoni <>
2013-06-22 13:56:31 +0000

    added json for rereco 22jan2013

--- CMGTools/H2TauTau/python/officialJSONS.py
+++ CMGTools/H2TauTau/python/officialJSONS.py
@@ -1,5 +1,5 @@
 
-# 2011
+# 2011 
 
 may10 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt'
 aug05 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt'
@@ -7,14 +7,19 @@ pr2011 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/
 
 # 2012
 
-pr2012 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt'
-aug6_2012 =  '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'
+pr2012AB = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-196531_8TeV_PromptReco_Collisions12_JSON.txt'
+# the good one:
+pr2012C  = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'
+pr2012CD = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt'
+# the old one 
+# pr2012C = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON.txt'
+aug6_2012 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'
 aug24_2012 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'
 jul13_2012 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'
 
-# 2012 Jan2013 re-reco
-jan22_2013 = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt'
-
+# TauParked ### 2012D missing in json as of 6th of may
+#tauparked = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-203742_8TeV_22Jan2013ReReco_Collisions12_JSON.txt'
+tauparked = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt'
 
 jsonMap = {
     #
@@ -55,62 +60,64 @@ jsonMap = {
     '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
     '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
     '/DoubleMu/StoreResults-DoubleMu_2011B_PR_v1_embedded_trans1_tau115_ptelec1_17had1_17_v3-f456bdbb960236e5c696adfe9b04eaae/USER':pr2011,
+    # 
+    # 2012A & B
     #
-    # ======================================================================================================================================
-    #
-    # 2012A 
-    #
-    '/Tau.*/Run2012A-13Jul2012-v1/AOD':jul13_2012,
-    '/DoubleMu/StoreResults-DoubleMu_Run2012A_13Jul2012_v1_embedded_trans1_tau116_ptmu1_13had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
-    #
-    # 2012B
-    #
-    '/Tau.*/Run2012B-13Jul2012-v1/AOD':jul13_2012,
-    '/DoubleMu/StoreResults-DoubleMu_Run2012B_13Jul2012_v4_embedded_trans1_tau116_ptmu1_13had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
+    '/TauPlusX/Run2012A-PromptReco-v1/AOD':pr2012AB,
+    '/TauPlusX/Run2012B-PromptReco-v1/AOD':pr2012AB,
+    '/TauPlusX/Run2012B-13Jul2012-v1/AOD':jul13_2012,
+    '/TauPlusX/Run2012A-recover-06Aug2012-v1/AOD':aug6_2012,
+    '/TauPlusX/Run2012A-13Jul2012-v1/AOD':jul13_2012,
+    '/Tau/Run2012A-13Jul2012-v1/AOD':jul13_2012,
+    '/Tau/Run2012A-recover-06Aug2012-v1/AOD':aug6_2012,
+    '/Tau/Run2012B-13Jul2012-v1/AOD':jul13_2012,
+    '/DoubleMu/StoreResults-DoubleMu_2012A_PromptReco_v1_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run193752to195135_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run195147to196070_embedded_trans1_tau116_ptmu1_13had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run196090to196531_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012A_PromptReco_v1_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run193752to195135_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_2012B_PromptReco_v1_Run195147to196070_embedded_trans1_tau115_ptelec1_17had1_17_v2-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012AB,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012A_13Jul2012_v1_embedded_trans1_tau115_ptelec1_17had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012A-recover_06Aug2012_v1_embedded_trans1_tau115_ptelec1_17had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':aug6_2012,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012B_13Jul2012_v4_embedded_trans1_tau115_ptelec1_17had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
+    
+    '/DoubleMu/StoreResults-DoubleMu_Run2012A_13Jul2012_v1_embedded_trans1_tau132_pttau1_17had2_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012B_13Jul2012_v4_embedded_trans1_tau132_pttau1_17had2_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':jul13_2012,
     #
     # 2012C
     #
-    '/Tau.*/Run2012C-24Aug2012-v1/AOD':aug24_2012,
-    '/Tau.*/Run2012C-PromptReco-v2/AOD':pr2012,
-    '/Tau.*/Run2012C-PromptReco-v1/AOD':pr2012, # Weird:  /Tau/Run2012C-PromptReco-v1/AOD still on the twiki.
-    '/DoubleMu/StoreResults-DoubleMu_Run2012C_PromptReco_v2.*/USER': pr2012,
+    '/TauPlusX/Run2012C-PromptReco-v1/AOD':pr2012C,
+    '/TauPlusX/Run2012C-24Aug2012-v1/AOD':aug24_2012,
+    '/TauPlusX/Run2012C-PromptReco-v2/AOD':pr2012C,
+    '/Tau/Run2012C-PromptReco-v2/AOD':pr2012CD,
+    '/Tau/Run2012C-24Aug2012-v1/AOD':aug24_2012,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012C_24Aug2012_v1_embedded_trans1_tau132_pttau1_17had2_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':aug24_2012,
+    '/DoubleMu/StoreResults-DoubleMu_Run2012C_24Aug2012_v1_embedded_trans1_tau115_ptelec1_17had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':aug24_2012,##### ???????
+    '/DoubleMu/StoreResults-DoubleMu_Run2012C_PromptReco_v2_embedded_trans1_tau115_ptelec1_17had1_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012C,
+    '/DoubleMu/StoreResults-Run2012C_PromptReco_v2_embedded_trans1_tau132_pttau1_17had2_17_v2-5ef1c0fd428eb740081f19333520fdc8/USER':pr2012CD,
     #
     # 2012D
     #
-    '/Tau.*/Run2012D-PromptReco-v1/AOD':pr2012,
-    '/DoubleMu/StoreResults-DoubleMu_2012D_PromptReco_v1_embedded_.*/USER':pr2012,
+    '/Tau/Run2012D-PromptReco-v1/AOD':pr2012CD,
+    '/DoubleMu/StoreResults-DoubleMu_2012D_PromptReco_v1_embedded_trans1_tau132_pttau1_17had2_17_v1-f456bdbb960236e5c696adfe9b04eaae/USER':pr2012CD,
+    #
+    # TauParked
+    #
+    '/Tau/Run2012A-22Jan2013-v1/AOD'      :tauparked,
+    '/TauParked/Run2012B-22Jan2013-v1/AOD':tauparked,
+    '/TauParked/Run2012C-22Jan2013-v1/AOD':tauparked,
+    '/TauParked/Run2012D-22Jan2013-v1/AOD':tauparked,
+    '/DoubleMu/StoreResults-Run2012A_22Jan2013_v1_RHembedded_trans1_tau132_pthad1_30had2_30_v1-f456bdbb960236e5c696adfe9b04eaae/USER':tauparked,
+    '/DoubleMuParked/StoreResults-Run2012B_22Jan2013_v1_RHembedded_trans1_tau132_pthad1_30had2_30_v1-f456bdbb960236e5c696adfe9b04eaae/USER':tauparked,
+    '/DoubleMuParked/StoreResults-Run2012C_22Jan2013_v1_RHembedded_trans1_tau132_pthad1_30had2_30_v1-f456bdbb960236e5c696adfe9b04eaae/USER':tauparked,
+    '/DoubleMuParked/StoreResults-Run2012D_22Jan2013_v1_RHembedded_trans1_tau132_pthad1_30had2_30_v1-f456bdbb960236e5c696adfe9b04eaae/USER':tauparked,
+
+
+
     #
     # non Tau samples
     #
     '/DoubleMu/Run2011A-16Jan2012-v1/AOD':pr2011,
-    '/DoubleMu/Run2011B-16Jan2012-v1/AOD':pr2011,
-
-    # Jan 13 re-reco - only use one JSON file
-    '.*22Jan2013-v1.*':jan22_2013,
-    '.*22Jan2013_v1.*':jan22_2013,
+    '/DoubleMu/Run2011B-16Jan2012-v1/AOD':pr2011 
     }
-
-
-if __name__ == '__main__':
-
-    from CMGTools.RootTools.json.jsonPick import jsonPick
-
-    samples = [
-        '/TauPlusX/Run2012A-13Jul2012-v1/AOD',
-        '/TauPlusX/Run2012B-13Jul2012-v1/AOD',
-        '/TauPlusX/Run2012C-24Aug2012-v1/AOD',
-        '/TauPlusX/Run2012C-PromptReco-v2/AOD',
-        '/TauPlusX/Run2012D-PromptReco-v1/AOD',
-        '/Tau/Run2012A-13Jul2012-v1/AOD',
-        '/Tau/Run2012B-13Jul2012-v1/AOD',
-        '/Tau/Run2012C-PromptReco-v1/AOD',
-        '/Tau/Run2012C-24Aug2012-v1/AOD',
-        '/Tau/Run2012D-PromptReco-v1/AOD',
-        '/TauPlusX/Run2012D-22Jan2013-v1/AOD/',
-        '/DoubleMuParked/StoreResults-Run2012D_22Jan2013_v1_RHembedded_trans1_tau116_ptmu1_16had1_18_v1-f456bdbb960236e5c696adfe9b04eaae/USER/V5_B'
-        ]
-
-    for sample in samples:
-        print 'Sample', sample
-        print '\tJSON    =', jsonPick( sample, jsonMap )
-
