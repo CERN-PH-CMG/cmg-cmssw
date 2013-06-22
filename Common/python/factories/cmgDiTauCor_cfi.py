2c52240cca22f1f3c99e3afa5462def7cd3bc68f
manzoni <>
2013-06-22 14:29:32 +0000

    new TauES recalibration

--- Common/python/factories/cmgDiTauCor_cfi.py
+++ Common/python/factories/cmgDiTauCor_cfi.py
@@ -2,19 +2,19 @@ import FWCore.ParameterSet.Config as cms
 
 cmgDiTauCorFactory = cms.PSet(
     # leg1 and leg2 are dummy collections here
-    # leg1Collection      = cms.InputTag(''),
-    # leg2Collection      = cms.InputTag(''),
-    # metCollection       = cms.InputTag('recoilCorrectedMET'),
+    leg1Collection      = cms.InputTag(''),
+    leg2Collection      = cms.InputTag(''),
+    metCollection       = cms.InputTag('recoilCorrectedMET'),
     diObjectCollection  = cms.InputTag('cmgDiTauSel'),
     nSigma              = cms.double(0),
     uncertainty         = cms.double(0.03),
     shift1ProngNoPi0    = cms.double(0.),
-    shift1Prong1Pi0     = cms.double(0.015),
+    shift1Prong1Pi0     = cms.double(0.015+1.012), ## by Phil for summer 13 https://indico.cern.ch/getFile.py/access?contribId=24&sessionId=4&resId=0&materialId=slides&confId=252865
     ptDependence1Pi0    = cms.double(0.001),
     ############ for the embedded
-    #shift3Prong         = cms.double(0.012),
+    shift3Prong         = cms.double(0.012),
     ############ for the mc
-    shift3Prong         = cms.double(0.022), # JAN - it's 0.012 according to Monica's twiki
+    #shift3Prong         = cms.double(0.022),
     ptDependence3Prong  = cms.double(0.001)
 )
 
