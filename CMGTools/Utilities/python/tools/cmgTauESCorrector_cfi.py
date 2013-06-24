0049dc353a3c286bc7eb81a435a3ceaddeed2d9f
benitezj <>
2013-06-24 10:06:17 +0000

    updated Tau ES corrections for Summer13

--- CMGTools/Utilities/python/tools/cmgTauESCorrector_cfi.py
+++ CMGTools/Utilities/python/tools/cmgTauESCorrector_cfi.py
@@ -7,7 +7,7 @@ cmgTauESCorrector = cms.EDFilter(
     inputCollection = cms.InputTag("cmgTauSel"),
     ##some parameters of the pT dependence correction are hardcode inside
     OneProngNoPi0Correction = cms.double(1.000),
-    OneProng1Pi0Correction = cms.double(1.015),
+    OneProng1Pi0Correction = cms.double(1.025),
     OneProng1Pi0CorrectionPtSlope = cms.double(0.001),
     ThreeProngCorrection = cms.double(1.012),
     ThreeProngCorrectionPtSlope = cms.double(0.001),
