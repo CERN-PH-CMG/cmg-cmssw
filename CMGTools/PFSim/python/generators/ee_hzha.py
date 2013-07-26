385db6e9c6a611d1b386facacbe556c1611c6bdb
cbern <>
2013-07-26 09:14:11 +0000

    added gun and fully hadronic cards for hzha

--- CMGTools/PFSim/python/generators/ee_hzha.py
+++ CMGTools/PFSim/python/generators/ee_hzha.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 generator = cms.EDFilter("HZHAGeneratorFilter",
-  CardsPath = cms.string("CMGTools/PFSim/python/generators/hzha04.cards")
+  CardsPath = cms.string("CMGTools/PFSim/python/generators/hzha04_ZqqHbb.cards")
 )
 
