267bfce2ae894ef7807bdfa70283a79c03164621
hinzmann <>
2013-08-08 12:59:23 +0000

    add Aug5 samples

--- UserArea/TNM/Ntuples/TNMc1/data/command.sh
+++ UserArea/TNM/Ntuples/TNMc1/data/command.sh
@@ -2,7 +2,7 @@ ProductionTasks.py -w 'patTuple*.root' -c -N 5 -q 2nd -t NTUPLE_Feb15 --output_w
 
 ProductionTasks.py -w 'cmgTuple*.root' -c -N 5 -q 2nd -t NTUPLE_Feb15 --output_wildcard '[!h]*.root' --batch_user tnm_group --cfg TNMc1_Feb15_cfg.py --max_threads 100 `cat data/samples_Moriond.txt`
 
-ProductionTasks.py -w 'PFAOD*.root' -c -N 5 -q 2nd -t NTUPLE_Feb15 --output_wildcard '[!h]*.root' --batch_user tnm_group --cfg TNMc1_Feb15_cfg.py --max_threads 100 `cat data/samples_Moriond.txt`
+ProductionTasks.py -w '*.root' -c -N 1 -q 2nd -t NTUPLE_Aug5 --output_wildcard '[!h]*.root' --batch_user tnm_group --cfg TNMc1_Feb15_cfg.py --max_threads 100 `cat data/samples_Moriond.txt`
 
 
 cmsBatch.py 5 TNMc1_Feb15_cfg.py -b 'bsub -q 1nd < ./batchScript.sh' -f -r /store/cmst3/user/hinzmann/NTUPLE/summer12_WJetsPt100 -o summer12_WJetsPt100
