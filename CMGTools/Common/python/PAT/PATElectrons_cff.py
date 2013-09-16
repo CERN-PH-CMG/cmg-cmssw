e81342c91698c94bab8d1bcdcf6e3201f79218d4
Colin <colin.bernet@cern.ch>
2013-09-16 16:02:21 +0200

    re-enabled MVA electron ID.
    Added all MVA e ID xml weight files.

--- CMGTools/Common/python/PAT/PATElectrons_cff.py
+++ CMGTools/Common/python/PAT/PATElectrons_cff.py
@@ -16,6 +16,7 @@ import FWCore.ParameterSet.Config as cms
 # from PhysicsTools.PatAlgos.recoLayer0.electronId_cff import *
 from PhysicsTools.PatAlgos.recoLayer0.electronIsolation_cff import *
 
+
 # add PAT specifics
 from PhysicsTools.PatAlgos.mcMatchLayer0.electronMatch_cfi import *
 from PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi import *
