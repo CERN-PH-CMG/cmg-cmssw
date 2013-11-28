375bfdd4b750b23ce823fd40432e6146ba779698
Giovanni <gpetruc@gmail.com>
2013-11-28 17:47:31 +0100

    support for vector branches, custom defaults and setting branch titles

--- CMGTools/RootTools/python/statistics/TreeNumpy.py
+++ CMGTools/RootTools/python/statistics/TreeNumpy.py
@@ -5,7 +5,10 @@ class TreeNumpy(object):
     
     def __init__(self, name, title):
         self.vars = {}
+        self.vecvars = {}
         self.tree = TTree(name, title)
+        self.defaults = {}
+        self.vecdefaults = {}
         
     def copyStructure(self, tree):
         for branch in tree.GetListOfBranches():
@@ -16,21 +19,43 @@ class TreeNumpy(object):
                 type = int
             self.var(name, type)            
             
-    def var(self, varName,type=float ):
+    def var(self, varName,type=float, default=-99, title=None ):
         self.vars[varName]=numpy.zeros(1,type)
+        self.defaults[varName] = default
         if type is float  : 
             self.tree.Branch(varName,self.vars[varName],varName+'/D')
         elif type is int    : 
             self.tree.Branch(varName,self.vars[varName],varName+'/I')
         else:
-            print 'Unknown type '
-            
+            print 'Unknown type %s for branch %s' % (type, varName)
+        if title:
+            self.tree.GetBranch(varName).SetTitle(title)
+
+    def vector(self, varName, lenvar, maxlen, type=float, default=-99, title=None ):
+        self.vecvars[varName]=numpy.zeros(maxlen,type)
+        self.vecdefaults[varName] = default
+        if type is float  : 
+            self.tree.Branch(varName,self.vecvars[varName],varName+'['+lenvar+']/D')
+        elif type is int    : 
+            self.tree.Branch(varName,self.vecvars[varName],varName+'['+lenvar+']/I')
+        else:
+            print 'Unknown type %s for branch %s' % (type, varName)
+        if title:
+            self.tree.GetBranch(varName).SetTitle(title)
+
     def reset(self):
         for name,value in self.vars.iteritems():
-            value[0]=-99
+            value[0]=self.defaults[name]
+        for name,value in self.vecvars.iteritems():
+            value.fill(self.vecdefaults[name])
             
     def fill(self, varName, value ):
         self.vars[varName][0]=value
+
+    def vfill(self, varName, values ):
+        a = self.vecvars[varName]
+        for (i,v) in enumerate(values):
+            a[i]=v
 
 if __name__=='__main__':
     
