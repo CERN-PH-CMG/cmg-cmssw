a8bb35bdac1866830455c7fa4ce46f131eb32f27
Giovanni <gpetruc@gmail.com>
2013-12-04 17:51:37 +0100

    use TW V5_17_0 input

--- CMGTools/TTHAnalysis/cfg/run_ttHLep8TeV_newNtpl_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_ttHLep8TeV_newNtpl_cfg.py
@@ -133,7 +133,7 @@ ttHEventAna = cfg.Analyzer(
     minJets25 = 0,
     )
 
-from CMGTools.TTHAnalysis.samples.samples_8TeV import triggers_mumu, triggers_ee, triggers_mue
+#from CMGTools.TTHAnalysis.samples.samples_8TeV import triggers_mumu, triggers_ee, triggers_mue
 # Tree Producer
 treeProducer = cfg.Analyzer(
     #'ttHLepTreeProducerExample',
@@ -141,54 +141,28 @@ treeProducer = cfg.Analyzer(
     vectorTree = True,
     PDFWeights = PDFWeights,
     triggerBits = {
-            'DoubleMu' : triggers_mumu,
-            'DoubleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5" not in t ],
-            'TripleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5"     in t ],
-            'MuEG'     : [ t for t in triggers_mue if "Mu" in t and "Ele" in t ]
+            #'DoubleMu' : triggers_mumu,
+            #'DoubleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5" not in t ],
+            #'TripleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5"     in t ],
+            #'MuEG'     : [ t for t in triggers_mue if "Mu" in t and "Ele" in t ]
         }
     )
 
 
 #-------- SAMPLES
-
-from CMGTools.TTHAnalysis.samples.samples_8TeV import * 
-
-for mc in mcSamples+fastSimSamples:
-    mc.triggers = triggersMC_mue
-#selectedComponents=mcSamples
-#selectedComponents=[TTH,DYJetsM10,DYJetsM50,TTLep]
-#selectedComponents=[DY1JetsM50,DY2JetsM50,DY3JetsM50,DY4JetsM50,W2Jets,WGToLNuG,WJets_HT250To300,WJets_HT300To400,WJets_HT400ToInf]
-#selectedComponents=[W1Jets,W2Jets,WGToLNuG]
-
-for data in dataSamplesE:
-    data.triggers = triggers_ee
-for data in dataSamplesMu:
-    data.triggers = triggers_mumu
-    data.vetoTriggers = triggers_ee
-for data in dataSamplesMuE:
-    data.triggers = triggers_mue
-    data.vetoTriggers=triggers_ee+triggers_mumu
-
-selectedComponents=mcSamples+dataSamplesMu+dataSamplesE+dataSamplesMuE
-
-isSingleMu = False
-if isSingleMu:
-    ttHLepAna.minGoodLeptons=1
-    ttHLepAna.maxGoodLeptons=1
-    ttHEventAna.minJets25 = 3
-    mcSamples = [ TTJets, W2Jets,W3Jets,W4Jets, TtW,TbartW, Ttch,Tbartch,Tsch,Tbarsch, QCDMuPt15, DY2JetsM50,DY3JetsM50,DY4JetsM50 ]
-    for mc in mcSamples+fastSimSamples:
-        mc.triggers = triggersMC_1mu
-    for data in dataSamples1Mu:
-        data.triggers = triggers_1mu
-    selectedComponents = dataSamples1Mu+mcSamples
-    TTJets.splitFactor = 250
-    for X in [W2Jets,W3Jets,W4Jets]: X.splitFactor = 250
-    for X in [DY2JetsM50,DY3JetsM50,DY4JetsM50]: X.splitFactor = 400
-    SingleMuC.splitFactor = 1500
-    SingleMuD.splitFactor = 1500
-    QCDMuPt15.splitFactor = 250
-
+from CMGTools.TTHAnalysis.samples.getFiles import getFiles
+from CMGTools.TTHAnalysis.samples.ComponentCreator import ComponentCreator
+from CMGTools.TTHAnalysis.setup.Efficiencies import *
+kreator = ComponentCreator()
+TW = kreator.makeMCComponent('TW','/T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_17_0/',"cmgtools","cmgTuple.*root")
+TW.isMC   = True
+TW.isData = False
+TW.splitFactor = 50
+TW.puFileMC   = os.environ['CMSSW_BASE']+"/src/CMGTools/TTHAnalysis/data/puProfile_Summer12_53X.root"
+TW.puFileData = os.environ['CMSSW_BASE']+"/src/CMGTools/TTHAnalysis/data/puProfile_Data12.root"
+TW.efficiency = eff2012
+TW.triggers = []
+selectedComponents = [ TW ]
 
 #-------- SEQUENCE
 
@@ -209,35 +183,15 @@ sequence = cfg.Sequence([
     ttHJetMCAna,
     ttHEventAna,
     treeProducer,
-    
     ])
 
 
 #-------- HOW TO RUN
-
-# selectedComponents = [ FastSim_TTWJets, FastSim_TTWJets_MUp, FastSim_TTWJets_MDn ]
-# set test = 0 to run all jobs, in case you are using pybatch.py
-#selectedComponents = mcSamples_1+dataSamplesE+dataSamplesMu+dataSamplesMuE+mcSamples_2
-#selectedComponents =  [ WZJets ]
-#WZJets.splitFactor = 100
-#ZZJets4L.splitFactor = 100 
-#ttHEventAna.minJets25 = 2
-#ttHLepAna.minGoodLeptons = 2
-
-selectedComponents = [ DoubleMuC ]
-#for c in selectedComponents: c.triggers = []
-#selectedComponents=[ TTWJets,TTZJets,TTH ]
-    
-#ttHLepAna.minGoodLeptons=0
-#ttHLepAna.minInclusiveLeptons=0
-#treeProducer.doInclusiveLeptons = True
-#TTH.triggers = []
-
 test = 1
 if test==1:
     # test a single component, using a single thread.
     # necessary to debug the code, until it doesn't crash anymore
-    comp = TTH
+    comp = TW
     #comp = DoubleMuD
     comp.files = comp.files[:1]
     selectedComponents = [comp]
@@ -262,38 +216,8 @@ elif test==3:
     comp.files = comp.files[:20]
     comp.splitFactor = 5
     selectedComponents = [comp]
-elif test==4:
-    # MC sync sample
-    comp = TTH
-    comp.files = [ 'root://pcmssd12//data/gpetrucc/8TeV/ttH/cmgTuple_full.TTH_Inclusive_M-125_8TeV_pythia6_PU_S10_START53_V7A-v1_F8454517-F509-E211-BAB4-003048D47792.root' ]
-    selectedComponents = [comp]
-    comp.splitFactor = 1
-    comp.triggers = [] #triggersMC_mue #[t for t in triggersMC_mue if "Ele15_Ele8_Ele5" not in t ]
-    ttHLepAna.doRochesterCorrections = False
-    ttHLepAna.doElectronScaleCorrections = False
-    ttHLepAna.minGoodLeptons = 2
-    #treeProducer.doJetsFailId = True
-    #ttHJetAna.jetCol = 'cmgPFJetSel'
-elif test==5:
-    # Ad-hoc MC fastsim sample
-    comp = TTJetsSem
-    comp.name = 'TTJetsSem_Filtered'
-    comp.files = [ '/afs/cern.ch/user/g/gpetrucc/ttH/CMGTools/CMSSW_5_3_5/src/CMGTools/Common/prod/patAODSIM-TTJetsSem.root' ]
-    selectedComponents = [comp]
-    comp.splitFactor = 1
-elif test==6:
-    # Ad-hoc Data sample
-    comp = DoubleMuD
-    comp.name = 'DoubleMuAll'
-    comp.json = None
-    comp.triggers = [] 
-    comp.files = [ '/afs/cern.ch/user/g/gpetrucc/ttH/CMGTools/CMSSW_5_3_5/src/CMGTools/TTHAnalysis/../Common/prod/2lss_mumu_tight/patAOD-DoubleMu-all.root' ]
-    #comp.name = 'MuEGAll'
-    #comp.files = [ '/afs/cern.ch/user/g/gpetrucc/ttH/CMGTools/CMSSW_5_3_5/src/CMGTools/Common/prod/2lss_em_tight/patAOD-2lss_em-all.root' ]
-    selectedComponents = [comp]
-    comp.splitFactor = 1
 elif test==7:    
-    # test all components (1 thread per component.
+    # test all components, 1/40 of the jobs, 1/10 of the files
     # important to make sure that your code runs on any kind of component
     for comp in selectedComponents:
         comp.splitFactor = comp.splitFactor / 40
