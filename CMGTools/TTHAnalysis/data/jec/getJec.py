4e086c3d44a3dd46bcbf1d56b0ad776a32e676ad
Giovanni <gpetruc@gmail.com>
2013-12-11 15:28:33 +0100

    2012 ReReco data updates: - new PU weights - new lepton efficiency weights - new jet energy corrections - propagation of JECs to MET when shifting JEC up and down

--- CMGTools/TTHAnalysis/data/jec/getJec.py
+++ CMGTools/TTHAnalysis/data/jec/getJec.py
@@ -5,9 +5,11 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 # define your favorite global tag
 #process.GlobalTag.globaltag = 'START53_V10::All'  # HCP MC
 #process.GlobalTag.globaltag = 'START53_V7G::All'  # Anna's MC
-process.GlobalTag.globaltag = 'START53_V20::All'  # Latests as of 18 Apr 2013
+#process.GlobalTag.globaltag = 'START53_V20::All'  # Latests as of 18 Apr 2013
+process.GlobalTag.globaltag = 'START53_V27::All'  # Latests ReReco 
 #process.GlobalTag.globaltag = 'GR_P_V41_AN1::All' # HCP
 #process.GlobalTag.globaltag = 'GR_P_V42_AN4::All' # Latest
+#process.GlobalTag.globaltag = 'FT_53_V21_AN5::All' # Latest ReReco
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.source = cms.Source("EmptySource")
 process.readAK5PF    = cms.EDAnalyzer('JetCorrectorDBReader',  
