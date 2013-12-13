d90b939b42a655b463edf12f6bbc23731cde3afe
Giovanni <gpetruc@gmail.com>
2013-12-13 16:55:50 +0100

    inclusive taus

--- CMGTools/TTHAnalysis/python/analyzers/ttHTauMCMatchAnalyzer.py
+++ CMGTools/TTHAnalysis/python/analyzers/ttHTauMCMatchAnalyzer.py
@@ -30,8 +30,8 @@ class ttHTauMCMatchAnalyzer( Analyzer ):
 
     def matchTaus(self, event):
         event.genTaus = [ x for x in event.genParticles if x.status() == 3 and abs(x.pdgId()) == 15 ]
-        match = matchObjectCollection3(event.selectedTaus, event.genTaus, deltaRMax = 0.5)
-        for lep in event.selectedTaus:
+        match = matchObjectCollection3(event.inclusiveTaus, event.genTaus, deltaRMax = 0.5)
+        for lep in event.inclusiveTaus:
             gen = match[lep]
             lep.mcMatchId = 1 if gen else 0
 
