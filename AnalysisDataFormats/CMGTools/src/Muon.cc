93655dc35694c9095d5c6018004199aec94b0687
Giovanni <gpetruc@gmail.com>
2014-02-12 09:15:44 +0100

    Implement lepton IDs (at least the POG ones) in the C++

--- AnalysisDataFormats/CMGTools/src/Muon.cc
+++ AnalysisDataFormats/CMGTools/src/Muon.cc
@@ -1,6 +1,59 @@
-# include "AnalysisDataFormats/CMGTools/interface/Muon.h"
+#include "AnalysisDataFormats/CMGTools/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonSelectors.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include <unordered_map>
 
 using namespace cmg;
+
+namespace {
+    std::unordered_map<std::string,cmg::Muon::MuonID> _muon_id_map;
+    const std::unordered_map<std::string,cmg::Muon::MuonID> & fill_muon_id_map() {
+        if (_muon_id_map.empty()) {
+            _muon_id_map["POG_ID_Soft"] = cmg::Muon::POG_ID_Soft;
+            _muon_id_map["POG_ID_Loose"] = cmg::Muon::POG_ID_Loose;
+            _muon_id_map["POG_ID_Tight"] = cmg::Muon::POG_ID_Tight;
+            _muon_id_map["POG_ID_HighPt"] = cmg::Muon::POG_ID_HighPt;
+            _muon_id_map["POG_PFIso_Loose"] = cmg::Muon::POG_PFIso_Loose;
+            _muon_id_map["POG_PFIso_Tight"] = cmg::Muon::POG_PFIso_Tight;
+        }
+        return _muon_id_map;
+    }
+}
+
+bool cmg::Muon::muonID(MuonID id, const reco::Vertex *vtx) const 
+{
+    const pat::Muon &mu = **sourcePtr();
+    switch(id) {
+        case POG_ID_Loose:  
+                return muon::isLooseMuon(mu);
+        case POG_ID_Soft:   
+                if (vtx == 0) throw cms::Exception("InvalidArgument", "POG_ID_Soft requires a vertex");
+                return muon::isSoftMuon(mu, *vtx);
+        case POG_ID_Tight:  
+                if (vtx == 0) throw cms::Exception("InvalidArgument", "POG_ID_Tight requires a vertex");
+                return muon::isTightMuon(mu, *vtx);
+        case POG_ID_HighPt: 
+                if (vtx == 0) throw cms::Exception("InvalidArgument", "POG_ID_HighPt requires a vertex");
+                return muon::isHighPtMuon(mu, *vtx, reco::improvedTuneP);
+        case POG_PFIso_Loose:
+                return (mu.pfIsolationR04().sumChargedHadronPt + std::max(mu.pfIsolationR04().sumNeutralHadronEt + mu.pfIsolationR04().sumPhotonEt - 0.5*mu.pfIsolationR04().sumPUPt,0.0)) < 0.20 * mu.pt();
+        case POG_PFIso_Tight:
+                return (mu.pfIsolationR04().sumChargedHadronPt + std::max(mu.pfIsolationR04().sumNeutralHadronEt + mu.pfIsolationR04().sumPhotonEt - 0.5*mu.pfIsolationR04().sumPUPt,0.0)) < 0.12 * mu.pt();
+    }
+    throw cms::Exception("InvalidArgument", "Unrecognized Muon ID choice");
+}
+bool cmg::Muon::muonID(const std::string &id, const reco::Vertex *vtx) const 
+{
+    static const std::unordered_map<std::string,MuonID> & id_map = fill_muon_id_map();
+    auto match = id_map.find(id);
+    if (match == id_map.end()) throw cms::Exception("InvalidArgument", id+" muon ID not known");
+    return muonID(match->second, vtx);
+}
+bool cmg::Muon::muonID(const char *id, const reco::Vertex *vtx) const 
+{
+    return muonID(std::string(id), vtx);
+}
+
 
 /// charged hadron vetoes    
 reco::isodeposit::AbsVetos Muon::chargedHadronVetos() const {
