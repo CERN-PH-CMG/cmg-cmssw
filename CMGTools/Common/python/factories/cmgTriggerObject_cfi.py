4bf4fd8e449060a42ed898d6f58103cfcf215d8f
Jan Steggemann <jan.steggemann@cern.ch>
2014-02-14 11:11:59 +0100

    Re-add PF ptr creation; change triggerName from * to HLT; update shared library in buildfile to 626 one

--- CMGTools/Common/python/factories/cmgTriggerObject_cfi.py
+++ CMGTools/Common/python/factories/cmgTriggerObject_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 triggerObjectFactory = cms.PSet(
        triggerResults = cms.InputTag("TriggerResults"),
        triggerObjects = cms.InputTag("patTrigger"),
-       processName = cms.untracked.string("*"),
+       processName = cms.untracked.string("HLT"), # FIXME: "*" doesn't work anymore
        useTriggerObjects = cms.untracked.bool(False)
        )
 
