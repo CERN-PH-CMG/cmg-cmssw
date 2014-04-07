0d703ebe990d1f20febdfbfee4b73d3a317af4d4
Giovanni <gpetruc@gmail.com>
2014-04-07 23:27:54 +0200

    temporarily remove CMGJetPUIDSelector which doesn't want to compile

--- CMGTools/Common/plugins/CMGJetPUIDSelector.cc
+++ CMGTools/Common/plugins/CMGJetPUIDSelector.cc
@@ -1,9 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
-#include "CMGTools/Common/interface/CMGJetPUIDSelectorDefinition.h"
+//#include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
+//#include "CMGTools/Common/interface/CMGJetPUIDSelectorDefinition.h"
 
-typedef ObjectSelector<cmg::CMGJetPUIDSelectorDefinition> CMGJetPUIDSelector;
+//typedef ObjectSelector<cmg::CMGJetPUIDSelectorDefinition> CMGJetPUIDSelector;
 
-DEFINE_FWK_MODULE(CMGJetPUIDSelector);
+//DEFINE_FWK_MODULE(CMGJetPUIDSelector);
