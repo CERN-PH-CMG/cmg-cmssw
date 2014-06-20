1f435739f2f66653534918b1fb1daccf83fccfc1
Giovanni <gpetruc@gmail.com>
2014-06-20 19:43:55 +0200

    refactor lepton modules

---
+++ CMGTools/TTHAnalysis/cfg/run_susyMultilepton_cfg.py
@@ -0,0 +1,245 @@
+import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.RootTools.fwlite.Config import printComps
+from CMGTools.RootTools.RootTools import *
+
+PDFWeights = []
+#PDFWeights = [ ("CT10",53), ("MSTW2008lo68cl",41), ("NNPDF21_100",101) ]
+
+# Find the initial events before the skim
+skimAnalyzer = cfg.Analyzer(
+    'skimAnalyzerCount'
+    )
+
+# Pick individual events (normally not in the path)
+eventSelector = cfg.Analyzer(
+    'EventSelector',
+    toSelect = []  # here put the event numbers (actual event numbers from CMSSW)
+    )
+
+# Apply json file (if the dataset has one)
+jsonAna = cfg.Analyzer(
+    'JSONAnalyzer',
+    )
+
+# Filter using the 'triggers' and 'vetoTriggers' specified in the dataset
+triggerAna = cfg.Analyzer(
+    #'TriggerAnalyzer',
+    'triggerBitFilter',
+    )
+
+
+# Select a list of good primary vertices (generic)
+ttHVertexAna = cfg.Analyzer(
+    'VertexAnalyzer',
+    vertexWeight = None,
+    fixedWeight = 1,
+    verbose = False
+    )
+
+
+# This analyzer actually does the pile-up reweighting (generic)
+pileUpAna = cfg.Analyzer(
+    "PileUpAnalyzer",
+    true = True,  # use number of true interactions for reweighting
+    makeHists=False
+    )
+
+
+# Gen Info Analyzer (generic, but should be revised)
+ttHGenAna = cfg.Analyzer(
+    'ttHGenLevelAnalyzer',
+    filterHiggsDecays = [0, 15, 23, 24],
+    verbose = False,
+    PDFWeights = [ pdf for pdf,num in PDFWeights ]
+    )
+
+# Lepton Analyzer (generic)
+susyScanAna = cfg.Analyzer(
+    'susyParameterScanAnalyzer',
+    )
+
+# Lepton Analyzer (generic)
+ttHLepAna = cfg.Analyzer(
+    'ttHLepAnalyzerSusy',
+    # input collections
+    muons='cmgMuonSel',
+    electrons='cmgElectronSel',
+    rhoMuon= 'kt6PFJetsCentralNeutral',
+    rhoElectron = 'kt6PFJets',
+    photons='cmgPhotonSel',
+    # energy scale corrections and ghost muon suppression (off by default)
+    doMuScleFitCorrections=False, # "rereco"
+    doRochesterCorrections=False,
+    doElectronScaleCorrections=False, # "embedded" in 5.18 for regression
+    doSegmentBasedMuonCleaning=False,
+    # inclusive very loose muon selection
+    inclusive_muon_id  = "POG_ID_Loose",
+    inclusive_muon_pt  = 3,
+    inclusive_muon_eta = 2.4,
+    inclusive_muon_dxy = 0.5,
+    inclusive_muon_dz  = 1.0,
+    # loose muon selection
+    loose_muon_id     = "POG_ID_Loose",
+    loose_muon_pt     = 5,
+    loose_muon_eta    = 2.4,
+    loose_muon_dxy    = 0.05,
+    loose_muon_dz     = 0.2,
+    loose_muon_relIso = 0.4,
+    # inclusive very loose electron selection
+    inclusive_electron_id  = "",
+    inclusive_electron_pt  = 5,
+    inclusive_electron_eta = 2.5,
+    inclusive_electron_dxy = 0.5,
+    inclusive_electron_dz  = 1.0,
+    inclusive_electron_lostHits = 1.0,
+    # loose electron selection
+    loose_electron_id     = "POG_MVA_ID_NonTrig",
+    loose_electron_pt     = 7,
+    loose_electron_eta    = 2.4,
+    loose_electron_dxy    = 0.05,
+    loose_electron_dz     = 0.2,
+    loose_electron_relIso = 0.4,
+    loose_electron_lostHits = 1.0,
+    # minimum deltaR between a loose electron and a loose muon (on overlaps, discard the electron)
+    min_dr_electron_muon = 0.02
+    )
+
+# Skim (generic, but requirements depend on the final state)
+ttHLepSkim = cfg.Analyzer(
+    'ttHLepSkimmer',
+    minLeptons = 2,
+    maxLeptons = 999,
+    #idCut  = "lepton.relIso03 < 0.2" # can give a cut
+    #ptCuts = [20,10],                # can give a set of pt cuts on the leptons
+    )
+
+# Lepton MC Matching (generic, must happen early to allow for MVA corrections)
+ttHLepMCAna = cfg.Analyzer(
+    'ttHLepMCMatchAnalyzer',
+    matchAllInclusiveLeptons = False,
+    )
+
+# Tau Analyzer (generic)
+ttHTauAna = cfg.Analyzer(
+    'ttHTauAnalyzer',
+    ptMin = 20,
+    vetoLeptons = True,
+    leptonVetoDR = 0.4,
+    tauID = "byMediumIsolationMVA2",
+    tauLooseID = "decayModeFinding",
+)
+
+# Tau MC Matching (generic)
+ttHTauMCAna = cfg.Analyzer(
+    'ttHTauMCMatchAnalyzer',
+)
+
+
+# Jets Analyzer (generic)
+ttHJetAna = cfg.Analyzer(
+    'ttHJetAnalyzer',
+    jetCol = 'cmgPFJetSelCHS',
+    jetCol4MVA = 'cmgPFJetSel',
+    jetPt = 25.,
+    jetEta = 4.7,
+    jetEtaCentral = 2.4,
+    jetLepDR = 0.4,
+    relaxJetId = False,  
+    doPuId = True,
+    recalibrateJets = False,
+    shiftJEC = 0, # set to +1 or -1 to get +/-1 sigma shifts
+    cleanJetsFromTaus = False,
+    )
+
+# Jet MC Match Analyzer (generic)
+ttHJetMCAna = cfg.Analyzer(
+    'ttHJetMCMatchAnalyzer',
+    smearJets = True,
+    shiftJER = 0, # set to +1 or -1 to get +/-1 sigma shifts
+    )
+
+# Event Analyzer (for susy multi-lepton)
+ttHEventAna = cfg.Analyzer(
+    'ttHLepEventAnalyzer',
+    maxLeps = 4, ## leptons to use
+    verbose = False,
+    minJets25 = 0,
+    )
+
+
+from CMGTools.TTHAnalysis.samples.samples_8TeV_v517 import triggers_mumu, triggers_ee, triggers_mue, triggers_1mu
+# Tree Producer
+treeProducer = cfg.Analyzer(
+    'treeProducerSusyMultilepton',
+    vectorTree = True,
+    PDFWeights = PDFWeights,
+    triggerBits = {
+            'SingleMu' : triggers_1mu,
+            'DoubleMu' : triggers_mumu,
+            'DoubleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5" not in t ],
+            'TripleEl' : [ t for t in triggers_ee if "Ele15_Ele8_Ele5"     in t ],
+            'MuEG'     : [ t for t in triggers_mue if "Mu" in t and "Ele" in t ]
+        }
+    )
+
+
+#-------- SAMPLES AND TRIGGERS -----------
+from CMGTools.TTHAnalysis.samples.samples_8TeV_v517 import * 
+
+for mc in mcSamples+mcSamples+extraMcSamples+fastSimSamples:
+    mc.triggers = triggersMC_mue
+for data in dataSamplesMu:
+    data.triggers = triggers_mumu
+for data in dataSamplesE:
+    data.triggers = triggers_ee
+    data.vetoTriggers = triggers_mumu
+for data in dataSamplesMuE:
+    data.triggers = triggers_mue
+    data.vetoTriggers=triggers_ee+triggers_mumu
+
+
+selectedComponents = [ DY1JetsM50,DY2JetsM50,DY3JetsM50,DY4JetsM50,TTH122,TTH127,TTJetsSem1,TTJetsSem2 ] 
+
+#-------- SEQUENCE
+
+sequence = cfg.Sequence([
+    skimAnalyzer,
+   #eventSelector,
+    jsonAna,
+    triggerAna,
+    pileUpAna,
+    ttHGenAna,
+    susyScanAna,
+    ttHVertexAna,
+    ttHLepAna,
+    ttHLepSkim,
+    ttHLepMCAna,
+    ttHTauAna,
+    ttHTauMCAna,
+    ttHJetAna,
+    ttHJetMCAna,
+    ttHEventAna,
+    treeProducer,
+    ])
+
+
+#-------- HOW TO RUN
+test = 1
+if test==1:
+    # test a single component, using a single thread.
+    comp = TTJets
+    comp.files = comp.files[:1]
+    selectedComponents = [comp]
+    comp.splitFactor = 1
+elif test==2:    
+    # test all components (1 thread per component).
+    for comp in selectedComponents:
+        comp.splitFactor = 1
+        comp.files = comp.files[:1]
+
+
+
+config = cfg.Config( components = selectedComponents,
+                     sequence = sequence )
+
+printComps(config.components, True)
---
+++ CMGTools/TTHAnalysis/python/analyzers/ttHLepAnalyzerSusy.py
@@ -0,0 +1,226 @@
+from CMGTools.RootTools.fwlite.Analyzer import Analyzer
+from CMGTools.RootTools.fwlite.AutoHandle import AutoHandle
+from CMGTools.RootTools.physicsobjects.Electron import Electron
+from CMGTools.RootTools.physicsobjects.Muon import Muon
+from CMGTools.RootTools.physicsobjects.Photon import Photon
+from CMGTools.TTHAnalysis.tools.EfficiencyCorrector import EfficiencyCorrector
+
+from CMGTools.RootTools.utils.DeltaR import bestMatch
+from CMGTools.RootTools.physicsobjects.RochesterCorrections import rochcor
+from CMGTools.RootTools.physicsobjects.MuScleFitCorrector   import MuScleFitCorr
+from CMGTools.RootTools.physicsobjects.ElectronCalibrator import EmbeddedElectronCalibrator
+from CMGTools.TTHAnalysis.electronCalibrator import ElectronCalibrator
+
+from ROOT import CMGMuonCleanerBySegmentsAlgo
+cmgMuonCleanerBySegments = CMGMuonCleanerBySegmentsAlgo()
+
+class ttHLepAnalyzerSusy( Analyzer ):
+
+    
+    def __init__(self, cfg_ana, cfg_comp, looperName ):
+        super(ttHLepAnalyzerSusy,self).__init__(cfg_ana,cfg_comp,looperName)
+        if self.cfg_ana.doMuScleFitCorrections and self.cfg_ana.doMuScleFitCorrections != "none":
+            if self.cfg_ana.doMuScleFitCorrections not in [ "none", "prompt", "prompt-sync", "rereco", "rereco-sync" ]:
+                raise RuntimeError, 'doMuScleFitCorrections must be one of "none", "prompt", "prompt-sync", "rereco", "rereco-sync"'
+            rereco = ("prompt" not in self.cfg_ana.doMuScleFitCorrections)
+            sync   = ("sync"       in self.cfg_ana.doMuScleFitCorrections)
+            self.muscleCorr = MuScleFitCorr(cfg_comp.isMC, rereco, sync)
+            if hasattr(self.cfg_ana, "doRochesterCorrections") and self.cfg_ana.doRochesterCorrections:
+                raise RuntimeError, "You can't run both Rochester and MuScleFit corrections!"
+        else:
+            self.cfg_ana.doMuScleFitCorrections = False
+        if self.cfg_ana.doElectronScaleCorrections == "embedded":
+            self.electronEnergyCalibrator = EmbeddedElectronCalibrator()
+        else:
+            self.electronEnergyCalibrator = ElectronCalibrator(cfg_comp.isMC)
+        if hasattr(cfg_comp,'efficiency'):
+            self.efficiency= EfficiencyCorrector(cfg_comp.efficiency)
+    #----------------------------------------
+    # DECLARATION OF HANDLES OF LEPTONS STUFF   
+    #----------------------------------------
+        
+
+    def declareHandles(self):
+        super(ttHLepAnalyzerSusy, self).declareHandles()
+
+        #leptons
+        self.handles['muons'] = AutoHandle(self.cfg_ana.muons,"std::vector<cmg::Muon>")            
+        self.handles['electrons'] = AutoHandle(self.cfg_ana.electrons,"std::vector<cmg::Electron>")            
+    
+        #rho for muons
+        self.handles['rhoMu'] = AutoHandle( (self.cfg_ana.rhoMuon, 'rho'), 'double')
+        #rho for electrons
+        self.handles['rhoEle'] = AutoHandle( (self.cfg_ana.rhoElectron, 'rho'), 'double')
+
+        #photons (a la hzz4l definition)
+        self.handles['photons'] = AutoHandle( self.cfg_ana.photons,'std::vector<cmg::Photon>')
+
+    def beginLoop(self):
+        super(ttHLepAnalyzerSusy,self).beginLoop()
+        self.counters.addCounter('events')
+        count = self.counters.counter('events')
+        count.register('all events')
+
+    #------------------
+    # MAKE LEPTON LISTS
+    #------------------
+
+    
+    def makeLeptons(self, event):
+        ### inclusive leptons = all leptons that could be considered somewhere in the analysis, with minimal requirements (used e.g. to match to MC)
+        event.inclusiveLeptons = []
+        ### selected leptons = subset of inclusive leptons passing some basic id definition and pt requirement
+        ### other    leptons = subset of inclusive leptons failing some basic id definition and pt requirement
+        event.selectedLeptons = []
+        event.otherLeptons = []
+
+        #muons
+        allmuons = self.makeAllMuons(event)
+
+        for mu in allmuons:
+            # inclusive, very loose, selection
+            if (mu.muonID(self.cfg_ana.inclusive_muon_id) and 
+                    mu.pt()>self.cfg_ana.inclusive_muon_pt and abs(mu.eta())<self.cfg_ana.inclusive_muon_eta and 
+                    abs(mu.dxy())<self.cfg_ana.inclusive_muon_dxy and abs(mu.dz())<self.cfg_ana.inclusive_muon_dz):
+                event.inclusiveLeptons.append(mu)
+                # basic selection
+                if (mu.muonID(self.cfg_ana.loose_muon_id) and 
+                        mu.pt() > self.cfg_ana.loose_muon_pt and abs(mu.eta()) < self.cfg_ana.loose_muon_eta and 
+                        abs(mu.dxy()) < self.cfg_ana.loose_muon_dxy and abs(mu.dz()) < self.cfg_ana.loose_muon_dz and
+                    mu.relIso03 < self.cfg_ana.loose_muon_relIso ):
+                    mu.looseIdSusy = True
+                    event.selectedLeptons.append(mu)
+                else:
+                    mu.looseIdSusy = False
+                    event.otherLeptons.append(mu)
+
+        #electrons        
+        allelectrons = self.makeAllElectrons(event)
+
+        looseMuons = event.selectedLeptons[:]
+        for ele in allelectrons:
+            ## remove muons if muForEleCrossCleaning is not empty
+            ## apply selection
+            if ( ele.electronID(self.cfg_ana.inclusive_electron_id) and
+                    ele.pt()>self.cfg_ana.inclusive_electron_pt and abs(ele.eta())<self.cfg_ana.inclusive_electron_eta and 
+                    abs(ele.dxy())<self.cfg_ana.inclusive_electron_dxy and abs(ele.dz())<self.cfg_ana.inclusive_electron_dz and 
+                    ele.numberOfHits()<=self.cfg_ana.inclusive_electron_lostHits ):
+                event.inclusiveLeptons.append(ele)
+                # basic selection
+                if (ele.electronID(self.cfg_ana.loose_electron_id) and
+                         ele.pt()>self.cfg_ana.loose_electron_pt and abs(ele.eta())<self.cfg_ana.loose_electron_eta and 
+                         abs(ele.dxy()) < self.cfg_ana.loose_electron_dxy and abs(ele.dz())<self.cfg_ana.loose_electron_dz and 
+                         ele.relIso03 <= self.cfg_ana.loose_electron_relIso and
+                         ele.numberOfHits() <= self.cfg_ana.loose_electron_lostHits and
+                         bestMatch(ele, looseMuons)[1] > self.cfg_ana.min_dr_electron_muon ):
+                    event.selectedLeptons.append(ele)
+                    ele.looseIdSusy = True
+                else:
+                    event.otherLeptons.append(ele)
+                    ele.looseIdSusy = False
+
+        event.otherLeptons.sort(key = lambda l : l.pt(), reverse = True)
+        event.selectedLeptons.sort(key = lambda l : l.pt(), reverse = True)
+        event.inclusiveLeptons.sort(key = lambda l : l.pt(), reverse = True)
+
+        for lepton in event.selectedLeptons:
+            if hasattr(self,'efficiency'):
+                self.efficiency.attachToObject(lepton)
+
+    def makePhotons(self, event):
+        event.allphotons = map( Photon, self.handles['photons'].product() )
+        event.allphotons.sort(key = lambda l : l.pt(), reverse = True)
+       
+    def makeAllMuons(self, event):
+        """
+               make a list of all muons, and apply basic corrections to them
+        """
+        # Start from all muons
+        allmuons = map( Muon, self.handles['muons'].product() )
+
+        # Muon scale and resolution corrections (if enabled)
+        if self.cfg_ana.doMuScleFitCorrections:
+            for mu in allmuons:
+                self.muscleCorr.correct(mu, event.run)
+        elif self.cfg_ana.doRochesterCorrections:
+            for mu in allmuons:
+                corp4 = rochcor.corrected_p4(mu, event.run) 
+                mu.setP4( corp4 )
+
+        # Clean up dulicate muons (note: has no effect unless the muon id is removed)
+        if self.cfg_ana.doSegmentBasedMuonCleaning:
+            isgood = cmgMuonCleanerBySegments.clean( self.handles['muons'].product() )
+            newmu = []
+            for i,mu in enumerate(allmuons):
+                if isgood[i]: newmu.append(mu)
+            allmuons = newmu
+
+        # Attach the vertex to them, for dxy/dz calculation
+        for mu in allmuons:
+            mu.associatedVertex = event.goodVertices[0]
+
+        # Compute relIso in 0.3 and 0.4 cones
+        for mu in allmuons:
+            mu.relIso03 = (mu.sourcePtr().pfIsolationR03().sumChargedHadronPt + max( mu.sourcePtr().pfIsolationR03().sumNeutralHadronEt +  mu.sourcePtr().pfIsolationR03().sumPhotonEt -  mu.sourcePtr().pfIsolationR03().sumPUPt/2,0.0))/mu.pt()
+            mu.relIso04 = (mu.sourcePtr().pfIsolationR04().sumChargedHadronPt + max( mu.sourcePtr().pfIsolationR04().sumNeutralHadronEt +  mu.sourcePtr().pfIsolationR04().sumPhotonEt -  mu.sourcePtr().pfIsolationR04().sumPUPt/2,0.0))/mu.pt()
+ 
+        return allmuons
+
+    def makeAllElectrons(self, event):
+        """
+               make a list of all electrons, and apply basic corrections to them
+        """
+        allelectrons = map( Electron, self.handles['electrons'].product() )
+
+        ## Duplicate removal for fast sim (to be checked if still necessary in latest greatest 5.3.X releases)
+        allelenodup = []
+        for e in allelectrons:
+            dup = False
+            for e2 in allelenodup:
+                if abs(e.pt()-e2.pt()) < 1e-6 and abs(e.eta()-e2.eta()) < 1e-6 and abs(e.phi()-e2.phi()) < 1e-6 and e.charge() == e2.charge():
+                    dup = True
+                    break
+            if not dup: allelenodup.append(e)
+        allelectrons = allelenodup
+
+        # fill EA for rho-corrected isolation
+        for ele in allelectrons:
+          ele.rho = float(self.handles['rhoEle'].product()[0])
+          SCEta = abs(ele.sourcePtr().superCluster().eta())
+          if (abs(SCEta) >= 0.0   and abs(SCEta) < 1.0   ) : ele.EffectiveArea = 0.13 # 0.130;
+          if (abs(SCEta) >= 1.0   and abs(SCEta) < 1.479 ) : ele.EffectiveArea = 0.14 # 0.137;
+          if (abs(SCEta) >= 1.479 and abs(SCEta) < 2.0   ) : ele.EffectiveArea = 0.07 # 0.067;
+          if (abs(SCEta) >= 2.0   and abs(SCEta) < 2.2   ) : ele.EffectiveArea = 0.09 # 0.089;
+          if (abs(SCEta) >= 2.2   and abs(SCEta) < 2.3   ) : ele.EffectiveArea = 0.11 # 0.107;
+          if (abs(SCEta) >= 2.3   and abs(SCEta) < 2.4   ) : ele.EffectiveArea = 0.11 # 0.110;
+          if (abs(SCEta) >= 2.4)                           : ele.EffectiveArea = 0.14 # 0.138;
+
+        # Electron scale calibrations
+        if self.cfg_ana.doElectronScaleCorrections:
+            for ele in allelectrons:
+                self.electronEnergyCalibrator.correct(ele, event.run)
+
+        # Attach the vertex
+        for ele in allelectrons:
+            ele.associatedVertex = event.goodVertices[0]
+
+        # Compute relIso with R=0.3 and R=0.4 cones
+        for ele in allelectrons:
+            ele.relIso03 = (ele.chargedHadronIso(0.3) + max(ele.neutralHadronIso(0.3)+ele.photonIso(0.3)-ele.rho*ele.EffectiveArea,0))/ele.pt()
+            ele.relIso04 = (ele.chargedHadronIso(0.4) + max(ele.neutralHadronIso(0.4)+ele.photonIso(0.4)-ele.rho*ele.EffectiveArea,0))/ele.pt()
+
+        # Set tight MVA id
+        for ele in allelectrons:
+            ele.tightIdResult = ele.electronID("POG_MVA_ID_Trig")
+        
+        return allelectrons 
+
+    def process(self, iEvent, event):
+        self.readCollections( iEvent )
+        self.counters.counter('events').inc('all events')
+
+        #call the leptons/photons functions
+        self.makeLeptons(event)
+        self.makePhotons(event)
+
+        return True
