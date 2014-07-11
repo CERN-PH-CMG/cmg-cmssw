97ed644008c4dcd1f304f93af7cf0a578b44bcf8
mariarosaria <dalfonso@cern.ch>
2014-07-11 17:12:35 +0200

    take the dxy and dz directly from the pfCand

--- CMGTools/RootTools/python/physicsobjects/IsoTrack.py
+++ CMGTools/RootTools/python/physicsobjects/IsoTrack.py
@@ -22,17 +22,12 @@ class IsoTrack( Lepton ):
     def dxy(self, vertex=None):
         if vertex is None:
             vertex = self.associatedVertex
-        vtx = self.vertex();  # FIXME 
-        p4 = self.p4();
-        return ( - (vtx.x()-vertex.position().x()) *  p4.y()
-                 + (vtx.y()-vertex.position().y()) *  p4.x() ) /  p4.pt();    
+        return self.physObj.dxy()
 
     def dz(self, vertex=None):
         if vertex is None:
             vertex = self.associatedVertex
-        vtx = self.vertex();  # FIXME 
-        p4 = self.p4();        
-        return  (vtx.z()-vertex.position().z()) - ((vtx.x()-vertex.position().x())*p4.x()+(vtx.y()-vertex.position().y())*p4.y())/ p4.pt() *  p4.z()/ p4.pt();
+        return self.physObj.dz()
 
     def __str__(self):
         lep = super(IsoTrack, self).__str__()
