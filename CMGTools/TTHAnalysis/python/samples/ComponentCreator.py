bd585e92727e8730cba6623f73fe12fc0d43b711
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2014-08-13 11:49:23 +0200

    Added different component creator for privately produced miniAOD datasets

--- CMGTools/TTHAnalysis/python/samples/ComponentCreator.py
+++ CMGTools/TTHAnalysis/python/samples/ComponentCreator.py
@@ -1,5 +1,5 @@
-import os
 import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.Production.dataset import getListOfFilesDBS
 from CMGTools.Production.datasetToSource import datasetToSource
 from CMGTools.Production.datasetInformation import DatasetInformation
 
@@ -56,7 +56,8 @@ class ComponentCreator(object):
             dataset=dataset,
             name = name,
             #files = ['root://eoscms//eos/cms%s' % f for f in filesDBS],
-            files = self.getFilesDBS(dataset, dbsInstance),
+            #files = self.getListOfFilesDBS(dataset, dbsInstance),
+            files = getListOfFilesDBS(dataset, dbsInstance),
             xSection = 1,
             nGenEvents = 1,
             triggers = [],
@@ -99,42 +100,42 @@ class ComponentCreator(object):
         return fraction 
         
 
-    ### MM
-    ### Function to get files on DBS prod/phys03
-    def getFilesDBS(self, dataset, dbsInstance):
-        entries = findMyPrimaryDatasetNumFiles(dataset, dbsInstance, -1, -1)
-        #print entries
-        filesDBS = []
-        dbs = 'das_client.py --query="file dataset=%s instance=prod/%s" --limit=%s' % (dataset, dbsInstance, entries)
-        dbsOut = os.popen(dbs)
-        for line in dbsOut:
-            if line.find('/store')==-1:
-                continue
-            line = line.rstrip()
-            # print 'line',line
-            filesDBS.append(line)
-        return ['root://eoscms//eos/cms%s' % f for f in filesDBS]
-    
-
-def findMyPrimaryDatasetNumFiles(dataset, dbsInstance, runmin, runmax):
-    
-    query, qwhat = dataset, "dataset"
-    if "#" in dataset: qwhat = "block"
-    if runmin >0 or runmax > 0:
-        if runmin == runmax:
-            query = "%s run=%d" % (query,runmin)
-        else:
-            print "WARNING: queries with run ranges are slow in DAS"
-            query = "%s run between [%d, %d]" % (query,runmin if runmin > 0 else 1, runmax if runmax > 0 else 999999)
-    dbs='das_client.py --query="summary %s=%s instance=prod/%s"'%(qwhat, query, dbsInstance)
-    dbsOut = os.popen(dbs).readlines()
-    entries = []
-    for line in dbsOut:
-        line = line.replace('\n','')
-        if "nfiles" in line:
-            entries.append(int(line.split(":")[1]))
-    if entries:
-        return sum(entries)
-    return -1
-    ### MM
+#### MM
+#    ### Function to get files on DBS prod/phys03
+#    def getListOfFilesDBS(self, dataset, dbsInstance):
+#        entries = findMyPrimaryDatasetNumFiles(dataset, dbsInstance, -1, -1)
+#        #print entries
+#        filesDBS = []
+#        dbs = 'das_client.py --query="file dataset=%s instance=prod/%s" --limit=%s' % (dataset, dbsInstance, entries)
+#        dbsOut = os.popen(dbs)
+#        for line in dbsOut:
+#            if line.find('/store')==-1:
+#                continue
+#            line = line.rstrip()
+#            # print 'line',line
+#            filesDBS.append(line)
+#        return ['root://eoscms//eos/cms%s' % f for f in filesDBS]
+#    
+#
+#def findMyPrimaryDatasetNumFiles(dataset, dbsInstance, runmin, runmax):
+#    
+#    query, qwhat = dataset, "dataset"
+#    if "#" in dataset: qwhat = "block"
+#    if runmin >0 or runmax > 0:
+#        if runmin == runmax:
+#            query = "%s run=%d" % (query,runmin)
+#        else:
+#            print "WARNING: queries with run ranges are slow in DAS"
+#            query = "%s run between [%d, %d]" % (query,runmin if runmin > 0 else 1, runmax if runmax > 0 else 999999)
+#    dbs='das_client.py --query="summary %s=%s instance=prod/%s"'%(qwhat, query, dbsInstance)
+#    dbsOut = os.popen(dbs).readlines()
+#    entries = []
+#    for line in dbsOut:
+#        line = line.replace('\n','')
+#        if "nfiles" in line:
+#            entries.append(int(line.split(":")[1]))
+#    if entries:
+#        return sum(entries)
+#    return -1
+#### MM
 
