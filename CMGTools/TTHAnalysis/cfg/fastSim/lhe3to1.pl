9eb8aed4658853bf76ccb5fa5b5336e39acc0f0a
Giovanni <gpetruc@gmail.com>
2014-09-16 10:41:47 +0200

    First set of LMuTau files

--- CMGTools/TTHAnalysis/cfg/fastSim/lhe3to1.pl
+++ CMGTools/TTHAnalysis/cfg/fastSim/lhe3to1.pl
@@ -6,10 +6,11 @@ $_ = <>; s{<LesHouchesEvents version="3.0">}{<LesHouchesEvents version="1.0">}; 
 while (<>) { print; m/<MGVersion>/ and last; }
 $_ = <>; m/^2\.1\.2/ and s/^/# / and print;
 
-while (<>) { m/<slha>/ and last; print; }
+while (<>) { m/<slha>/ and last; m/sys_alpsfact/ and next; print; }
 while (<>) { m/<\/slha>/ and last; }
 
 while (<>) {
     m/^<generator/ and next;
+    m/sys_alpsfact/ and next; # fix bug
     print;
 }
