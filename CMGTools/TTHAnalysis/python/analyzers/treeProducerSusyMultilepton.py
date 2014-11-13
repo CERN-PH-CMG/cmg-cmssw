e1d7fb4461e4f5ee1cb2cdf9e3133bd533a5afb1
botta <cristina.botta@cern.ch>
2014-11-13 10:56:46 +0100

    new variables for soft b-tagging

--- CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyMultilepton.py
+++ CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyMultilepton.py
@@ -19,6 +19,7 @@ class treeProducerSusyMultilepton( treeProducerSusyCore ):
             NTupleVariable("htJet40a", lambda ev : ev.htJet40a, help="H_{T} computed from leptons and jets (with |eta|<4.7, pt > 40 GeV)"),
             NTupleVariable("mhtJet40", lambda ev : ev.mhtJet40, help="H_{T}^{miss} computed from leptons and jets (with |eta|<2.4, pt > 40 GeV)"),
             NTupleVariable("mhtJet40a", lambda ev : ev.mhtJet40a, help="H_{T}^{miss} computed from leptons and jets (with |eta|<4.7, pt > 40 GeV)"),
+            NTupleVariable("nSoftBJetMedium25", lambda ev: sum((sv.mva>0.7 and (sv.jetPt < 25 or sv.jetBTag<0.679)) for sv in ev.ivf) + len(ev.bjetsMedium), int, help="Exclusive sum of jets with pt > 25 passing CSV medium and SV from ivf with medium sv mva"),
             ##--------------------------------------------------            
             NTupleVariable("mZ1", lambda ev : ev.bestZ1[0], help="Best m(ll) SF/OS"),
             NTupleVariable("mZ1SFSS", lambda ev : ev.bestZ1sfss[0], help="Best m(ll) SF/SS"),
