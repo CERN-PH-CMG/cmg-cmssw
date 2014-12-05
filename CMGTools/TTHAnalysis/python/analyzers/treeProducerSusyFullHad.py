e3803f16d5b86f676f2b7348a31e0ab84dac6a22
mariarosaria <dalfonso@cern.ch>
2014-12-05 16:27:15 +0100

    added the nPFLep5LowMT,nPFHad10LowMT,minMTBMet

--- CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyFullHad.py
+++ CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyFullHad.py
@@ -48,7 +48,9 @@ class treeProducerSusyFullHad( treeProducerSusyCore ):
 #            NTupleVariable("mtwTau", lambda ev: ev.mtwTau, int, help="mt(tau,met)"),
 #            NTupleVariable("IsoTrack_mtw", lambda ev: ev.mtwIsoTrack, int, help="mt(isoTrack,met)"),
 #            NTupleVariable("mt2w", lambda ev: ev.mt2w, float, help="mt2w(l,b,met)"),
-
+            NTupleVariable("minMTBMet", lambda ev: ev.minMTBMet, float, help="min Mt(b,met)"),
+            NTupleVariable("nPFLep5LowMT", lambda ev: ev.nPFLep5LowMT, int, help="number of PF leptons (e,mu) with pt > 5, reliso < 0.2, MT < 100 "),
+            NTupleVariable("nPFHad10LowMT", lambda ev: ev.nPFHad10LowMT, int, help="number of PF hadrons with pt > 10, reliso < 0.1, MT < 100 "),
             ##--------------------------------------------------
             # MT2
             ##--------------------------------------------------
