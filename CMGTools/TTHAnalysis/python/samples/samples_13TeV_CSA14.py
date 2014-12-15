18026fdfd2d6e8b7e2497272df1f165f9db1066b
schoef <robert.schoefbeck@cern.ch>
2014-12-15 16:08:23 +0100

    fixed T6qqWW_Sq_950_LSP_300_Chi_350->T6qqWW_Sq_950_LSP_300_Chi_325

--- CMGTools/TTHAnalysis/python/samples/samples_13TeV_CSA14.py
+++ CMGTools/TTHAnalysis/python/samples/samples_13TeV_CSA14.py
@@ -349,9 +349,8 @@ T5qqqqWW_Gl_1400_LSP_100_Chi_325 = kreator.makeMCComponentFromEOS('T5qqqqWW_Gl_1
 T5qqqqWW_Gl_1400_LSP_300_Chi_315 = kreator.makeMCComponentFromEOS('T5qqqqWW_Gl_1400_LSP_300_Chi_315', '/13TeV_T5qqqqWW_Gl_1400_LSP_300_Chi_315/', '/store/cmst3/group/susy/alobanov/MC/MiniAOD_v2/%s')
 
 #SqGltttt_Gl_1300_Sq_1300_LSP_100 = kreator.makeMCComponentFromEOS('SqGltttt_Gl_1300_Sq_1300_LSP_100', '/13TeV_SqGltttt_Gl_1300_Sq_1300_LSP_100/', '/store/cmst3/group/susy/alobanov/MC/MiniAOD_v2/%s')
-T6qqWW_Sq_950_LSP_300_Chi_350 =kreator.makeMCComponentFromEOS('T6qqWW_Sq_950_LSP_300_Chi_350', '/13TeV_T6qqWW_Sq_950_LSP_300_Chi_350/', '/store/cmst3/group/susy/alobanov/MC/MiniAOD_v2/%s')
-    
-acdvSignals= [T1ttbbWW_2J_mGo1000_mCh725_mChi715_3bodydec, T1ttbbWW_2J_mGo1000_mCh725_mChi720_3bodydec, T1ttbbWW_2J_mGo1300_mCh300_mChi290_3bodydec, T1ttbbWW_2J_mGo1300_mCh300_mChi295_3bodydec, T1tttt_gluino_1300_LSP_100, T1tttt_gluino_800_LSP_450, T5qqqqWW_Gl_1400_LSP_100_Chi_325, T5qqqqWW_Gl_1400_LSP_300_Chi_315, T6qqWW_Sq_950_LSP_300_Chi_350]
+T6qqWW_Sq_950_LSP_300_Chi_325 =kreator.makeMCComponentFromEOS('T6qqWW_Sq_950_LSP_300_Chi_325', '/13TeV_T6qqWW_Sq_950_LSP_300_Chi_325/', '/store/cmst3/group/susy/alobanov/MC/MiniAOD_v2/%s')
+acdvSignals= [T1ttbbWW_2J_mGo1000_mCh725_mChi715_3bodydec, T1ttbbWW_2J_mGo1000_mCh725_mChi720_3bodydec, T1ttbbWW_2J_mGo1300_mCh300_mChi290_3bodydec, T1ttbbWW_2J_mGo1300_mCh300_mChi295_3bodydec, T1tttt_gluino_1300_LSP_100, T1tttt_gluino_800_LSP_450, T5qqqqWW_Gl_1400_LSP_100_Chi_325, T5qqqqWW_Gl_1400_LSP_300_Chi_315, T6qqWW_Sq_950_LSP_300_Chi_325]
 mcSamples+=acdvSignals
 #-----------DATA---------------
 
