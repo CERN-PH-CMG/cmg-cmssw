3071c68be89f2990d7914678155037cf7c15e0fc
Giovanni <gpetruc@gmail.com>
2014-12-16 22:29:23 +0100

    Lepton MVA cleanup

--- CMGTools/TTHAnalysis/python/leptonMVA.py
+++ CMGTools/TTHAnalysis/python/leptonMVA.py
@@ -1,13 +1,13 @@
 from array import array
 from math import *
-import os.path
 from PhysicsTools.HeppyCore.utils.deltar import deltaR
 
-import os, ROOT
-if "/smearer_cc.so" not in ROOT.gSystem.GetLibraries(): 
-    ROOT.gROOT.ProcessLine(".L %s/src/CMGTools/TTHAnalysis/python/plotter/smearer.cc+" % os.environ['CMSSW_BASE']);
-if "/mcCorrections_cc.so" not in ROOT.gSystem.GetLibraries(): 
-    ROOT.gROOT.ProcessLine(".L %s/src/CMGTools/TTHAnalysis/python/plotter/mcCorrections.cc+" % os.environ['CMSSW_BASE']);
+import ROOT
+#import os
+#if "/smearer_cc.so" not in ROOT.gSystem.GetLibraries(): 
+#    ROOT.gROOT.ProcessLine(".L %s/src/CMGTools/TTHAnalysis/python/plotter/smearer.cc+" % os.environ['CMSSW_BASE']);
+#if "/mcCorrections_cc.so" not in ROOT.gSystem.GetLibraries(): 
+#    ROOT.gROOT.ProcessLine(".L %s/src/CMGTools/TTHAnalysis/python/plotter/mcCorrections.cc+" % os.environ['CMSSW_BASE']);
 
 
 class MVAVar:
@@ -46,41 +46,25 @@ class CategorizedMVA:
 _CommonSpect = [ 
 ]
 _CommonVars = {
- 'TTH':[ 
-    MVAVar("neuRelIso := relIso - chargedIso/pt",lambda x: x.relIso(dBetaFactor=0.5) - x.chargedHadronIsoR(0.4)/x.pt()),  
-    MVAVar("chRelIso := chargedIso/pt",lambda x: x.chargedHadronIsoR(0.4)/x.pt()),
-    MVAVar("jetDR_in := min(dr_in,0.5)", lambda x : min(deltaR(x.eta(),x.phi(),x.jet.eta(),x.jet.phi()),0.5), corrfunc=ROOT.correctJetDRMC),
-    MVAVar("jetPtRatio_in := min(ptf_in,1.5)", lambda x : min(x.pt()/x.jet.pt(),1.5), corrfunc=ROOT.correctJetPtRatioMC),
-    MVAVar("jetBTagCSV_in := max(CSV_in,0)", lambda x : max( (x.jet.btag('combinedInclusiveSecondaryVertexV2BJetTags') if hasattr(x.jet, 'btag') else -99) ,0.)),
-    MVAVar("sip3d",lambda x: x.sip3D(), corrfunc=ROOT.scaleSip3dMC),
- ],
  'Susy':[ 
     MVAVar("neuRelIso03 := relIso03 - chargedHadRelIso03",lambda x: x.relIso03 - x.chargedHadronIsoR(0.3)/x.pt()),  
     MVAVar("chRelIso03 := chargedHadRelIso03",lambda x: x.chargedHadronIsoR(0.3)/x.pt()),
-    MVAVar("jetDR := min(jetDR,0.5)", lambda x : min(deltaR(x.eta(),x.phi(),x.jet.eta(),x.jet.phi()),0.5), corrfunc=ROOT.correctJetDRMC),
-    MVAVar("jetPtRatio := min(jetPtRatio,1.5)", lambda x : min(x.pt()/x.jet.pt(),1.5), corrfunc=ROOT.correctJetPtRatioMC),
+    MVAVar("jetDR := min(jetDR,0.5)", lambda x : min(deltaR(x.eta(),x.phi(),x.jet.eta(),x.jet.phi()),0.5)), #, corrfunc=ROOT.correctJetDRMC),
+    MVAVar("jetPtRatio := min(jetPtRatio,1.5)", lambda x : min(x.pt()/x.jet.pt(),1.5)), #, corrfunc=ROOT.correctJetPtRatioMC),
     MVAVar("jetBTagCSV := max(jetBTagCSV,0)", lambda x : max( (x.jet.btag('combinedInclusiveSecondaryVertexV2BJetTags') if hasattr(x.jet, 'btag') else -99) ,0.)),
-    MVAVar("sip3d",lambda x: x.sip3D(), corrfunc=ROOT.scaleSip3dMC),
-    MVAVar("dxy := log(abs(dxy))",lambda x: log(abs(x.dxy())), corrfunc=ROOT.scaleDxyMC),
-    MVAVar("dz  := log(abs(dz))", lambda x: log(abs(x.dz())), corrfunc=ROOT.scaleDzMC),
+    MVAVar("sip3d",lambda x: x.sip3D()), #, corrfunc=ROOT.scaleSip3dMC),
+    MVAVar("dxy := log(abs(dxy))",lambda x: log(abs(x.dxy()))), #, corrfunc=ROOT.scaleDxyMC),
+    MVAVar("dz  := log(abs(dz))", lambda x: log(abs(x.dz()))), #, corrfunc=ROOT.scaleDzMC),
  ]
 }
 
 _MuonVars = {
- 'TTH': [
-    MVAVar("dxy := log(abs(dxy))",lambda x: log(abs(x.dxy())), corrfunc=ROOT.scaleDxyMC),
-    MVAVar("dz  := log(abs(dz))", lambda x: log(abs(x.dz())), corrfunc=ROOT.scaleDzMC),
- ],
  'Susy': [
     #MVAVar("mvaId",lambda x: x.mvaId()), # not yet in the training
  ]
 }
 
 _ElectronVars = {
- 'TTH': [
-    MVAVar("mvaId",lambda x: x.mvaNonTrigV0(full5x5=True)),
-    MVAVar("innerHits",lambda x: x.gsfTrack().hitPattern().numberOfLostHits(ROOT.reco.HitPattern.MISSING_INNER_HITS)),
- ],
  'Susy': [
     MVAVar("mvaId",lambda x: x.mvaNonTrigV0(full5x5=True)),
  ]
