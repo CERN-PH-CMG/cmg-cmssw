812683a9d9c23270d390e3387e887ab1f7d8f694
safarzad <safarzad@cern.ch>
2015-01-06 10:53:30 +0100

    migrate to heppy

--- CMGTools/TTHAnalysis/cfg/run_susySinglelepton_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susySinglelepton_cfg.py
@@ -1,21 +1,20 @@
 ##########################################################
-##       CONFIGURATION FOR SUSY MULTILEPTON TREES       ##
-## skim condition: >= 2 loose leptons, no pt cuts or id ##
+##       CONFIGURATION FOR SUSY SingleLep TREES       ##
+## skim condition: >= 1 loose leptons, no pt cuts or id ##
 ##########################################################
+import PhysicsTools.HeppyCore.framework.config as cfg
 
-import CMGTools.RootTools.fwlite.Config as cfg
-from CMGTools.RootTools.fwlite.Config import printComps
-from CMGTools.RootTools.RootTools import *
 
 #Load all analyzers
 from CMGTools.TTHAnalysis.analyzers.susyCore_modules_cff import * 
 
-ttHLepAna.loose_muon_pt  = 10
-ttHLepAna.loose_muon_relIso = 0.4
-ttHLepAna.mu_isoCorr = "deltaBeta" 
-ttHLepAna.loose_electron_pt  = 10
-ttHLepAna.loose_electron_relIso = 0.4
-ttHLepAna.ele_isoCorr = "rhoArea" 
+lepAna.loose_muon_pt  = 5
+lepAna.loose_muon_relIso = 0.5
+lepAna.mu_isoCorr = "deltaBeta" 
+lepAna.loose_electron_pt  = 7
+lepAna.loose_electron_relIso = 0.5
+lepAna.ele_isoCorr = "rhoArea" 
+lepAna.ele_tightId = "Cuts_2012"
 
 
 # Redefine what I need
@@ -23,56 +22,75 @@ ttHLepAna.ele_isoCorr = "rhoArea"
 # --- LEPTON SKIMMING ---
 ttHLepSkim.minLeptons = 1
 ttHLepSkim.maxLeptons = 999
-#ttHLepSkim.idCut  = ""
-#ttHLepSkim.ptCuts = []
+#LepSkim.idCut  = ""
+#LepSkim.ptCuts = []
 
 # --- JET-LEPTON CLEANING ---
-ttHJetAna.minLepPt = 10 
-ttHJetMCAna.smearJets     = False # do we need to smear the jets?
+jetAna.minLepPt = 10 
+#JetMCAna.smearJets     = False # do we need to smear the jets?
+jetAna.smearJets = False
 
-ttHReclusterJets = cfg.Analyzer(
-            'ttHReclusterJetsAnalyzer',
-            )
+#ttHReclusterJets = cfg.Analyzer(
+#            'ttHReclusterJetsAnalyzer',
+#            )
 
 # Event Analyzer for susy multi-lepton (at the moment, it's the TTH one)
+
+
+isoTrackAna.setOff=False
+
+#from CMGTools.TTHAnalysis.analyzers.ttHReclusterJetsAnalyzer  import ttHReclusterJetsAnalyzer
+#ttHReclusterJets = cfg.Analyzer(
+#    ttHReclusterJetsAnalyzer, name="ttHReclusterJetsAnalyzer",
+#    )
+from CMGTools.TTHAnalysis.analyzers.ttHLepEventAnalyzer import ttHLepEventAnalyzer
 ttHEventAna = cfg.Analyzer(
-    'ttHLepEventAnalyzer',
+    ttHLepEventAnalyzer, name="ttHLepEventAnalyzer",
     minJets25 = 0,
     )
 
+## Insert the SV analyzer in the sequence
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+                        ttHFatJetAna)
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+                        ttHSVAna)
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+                        ttHHeavyFlavourHadronAna)
 
-ttHIsoTrackAna.setOff=False
 
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
-                        ttHSVAnalyzer)
-susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna), 
-                        ttHHeavyFlavourHadronAnalyzer)
 
-from CMGTools.TTHAnalysis.samples.samples_8TeV_v517 import triggers_1mu, triggers_1muHT, triggers_1eleHT
+from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14  import *
+
+triggerFlagsAna.triggerBits = {
+#put trigger here for data
+}
 
 # Tree Producer
+from CMGTools.TTHAnalysis.analyzers.treeProducerSusySingleLepton import *
+## Tree Producer
 treeProducer = cfg.Analyzer(
-    'treeProducerSusySingleLepton',
-    vectorTree = True,
-    saveTLorentzVectors = False,  # can set to True to get also the TLorentzVectors, but trees will be bigger
-    PDFWeights = PDFWeights,
-    triggerBits = {
-            'MuHT' : triggers_1muHT,
-            'eleHT' : triggers_1eleHT
-        }
-    )
+     AutoFillTreeProducer, name='treeProducerSusySingleLepton',
+     vectorTree = True,
+     saveTLorentzVectors = False,  # can set to True to get also the TLorentzVectors, but trees will be bigger
+     PDFWeights = PDFWeights,
+     globalVariables = susySingleLepton_globalVariables,
+     globalObjects = susySingleLepton_globalObjects,
+     collections = susySingleLepton_collections,
+)
+
 
 
 #-------- SAMPLES AND TRIGGERS -----------
-from CMGTools.TTHAnalysis.samples.samples_13TeV_CSA14 import *
-selectedComponents = [ SingleMu, DoubleElectron, TTHToWW_PUS14, DYJetsToLL_M50_PU20bx25, TTJets_PUS14 ]
+
+from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14 import *
+#selectedComponents = [ SingleMu, DoubleElectron, TTHToWW_PUS14, DYJetsToLL_M50_PU20bx25, TTJets_PUS14 ]
 
 
 #-------- SEQUENCE
 
 sequence = cfg.Sequence(susyCoreSequence+[
     ttHEventAna,
-    ttHReclusterJets,
+#    ttHReclusterJets,
     treeProducer,
     ])
 
@@ -81,7 +99,7 @@ sequence = cfg.Sequence(susyCoreSequence+[
 test = 1
 if test==1:
     # test a single component, using a single thread.
-    comp = TTJets_PU20bx25
+    comp = TTJets
     comp.files = comp.files[:1]
     selectedComponents = [comp]
     comp.splitFactor = 1
@@ -91,8 +109,8 @@ elif test==2:
         comp.splitFactor = 1
         comp.files = comp.files[:1]
 
-
+from PhysicsTools.HeppyCore.framework.eventsfwlite import Events
 config = cfg.Config( components = selectedComponents,
-                     sequence = sequence )
-
-printComps(config.components, True)
+                     sequence = sequence,
+                     services = [],
+                     events_class = Events)
