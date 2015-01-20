d8755027448b3ffe4411db0455c7ca098331b5bb
Jan Steggemann <jan.steggemann@cern.ch>
2015-01-20 14:54:18 +0100

    Remove superfluous configs

--- CMGTools/H2TauTau/python/objects/tauMu_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CMGTools.Common.factories.cmgTauMu_cfi import *
-from CMGTools.Common.skims.cmgTauMuSel_cfi import *
-
-tauMuSequence = cms.Sequence(
-    cmgTauMu 
-    +cmgTauMuSel 
-)
