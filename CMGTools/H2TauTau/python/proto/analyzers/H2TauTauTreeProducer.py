74cd640a75e766f333399275b47f28934dafa38e
Jan Steggemann <jan.steggemann@cern.ch>
2015-03-18 13:43:07 +0100

    Additional renaming of variables and more pep8tification

--- CMGTools/H2TauTau/python/proto/analyzers/H2TauTauTreeProducer.py
+++ CMGTools/H2TauTau/python/proto/analyzers/H2TauTauTreeProducer.py
@@ -5,8 +5,6 @@ from PhysicsTools.Heppy.analyzers.core.AutoHandle import AutoHandle
 from CMGTools.H2TauTau.proto.analyzers.tauIDs import tauIDs
 from CMGTools.H2TauTau.proto.analyzers.varsDictionary import vars
 
-from math import sqrt
-
 
 class Variable():
     def __init__(self, name, function=None, type=float):
@@ -17,8 +15,31 @@ class Variable():
             self.function = lambda x : getattr(x, self.name) 
         self.type = float
 
-# di-tau object variables
+# event variables
 
+event_vars = [
+    Variable('run', type=int),
+    Variable('lumi', type=int),
+    Variable('event', lambda ev : ev.eventId, type=int),
+    Variable('pass_leptons', lambda ev : ev.isSignal, type=int),
+    Variable('veto_dilepton', lambda ev : ev.leptonAccept, type=int),
+    Variable('veto_thirdlepton', lambda ev : ev.thirdLeptonVeto, type=int),
+    Variable('n_jets', lambda ev : len(ev.cleanJets30), type=int),
+    Variable('n_jets_20', lambda ev : len(ev.cleanJets), type=int),
+    Variable('n_bjets', lambda ev : len(ev.cleanBJets), type=int),
+    Variable('n_jets_csvl', lambda ev : sum(1 for jet in ev.cleanJets if jet.btag('combinedSecondaryVertexBJetTags') > 0.244), type=int),
+    Variable('n_vertices', lambda ev : len(ev.vertices), type=int),
+    Variable('rho', lambda ev : ev.rho),
+    Variable('weight', lambda ev : ev.eventWeight),
+    Variable('weight_vertex', lambda ev : ev.vertexWeight),
+    Variable('weight_embed', lambda ev : ev.embedWeight),
+    Variable('weight_njet', lambda ev : ev.NJetWeight),
+    Variable('weight_hqt', lambda ev : ev.higgsPtWeight),
+    Variable('weight_hqt_up', lambda ev : ev.higgsPtWeightUp),
+    Variable('weight_hqt_down', lambda ev : ev.higgsPtWeightDown),
+]
+
+# di-tau object variables
 ditau_vars = [
     Variable('mvis', lambda dil : dil.mass()),
     Variable('svfit_mass', lambda dil : dil.svfitMass()),
@@ -35,8 +56,8 @@ ditau_vars = [
     Variable('pzeta_vis', lambda dil : dil.pZetaVis()),
     Variable('pzeta_disc', lambda dil : dil.pZetaDisc()),
     Variable('mt', lambda dil : dil.mTLeg2()),
-    Variable('mtleg2', lambda dil : dil.mTLeg2()),
-    Variable('mtleg1', lambda dil : dil.mTLeg1()),
+    Variable('mt_leg2', lambda dil : dil.mTLeg2()),
+    Variable('mt_leg1', lambda dil : dil.mTLeg1()),
     Variable('met_cov00', lambda dil : dil.mvaMetSig(0, 0)),
     Variable('met_cov01', lambda dil : dil.mvaMetSig(0, 1)),
     Variable('met_cov10', lambda dil : dil.mvaMetSig(1, 0)),
@@ -62,20 +83,52 @@ particle_vars = [
     Variable('mass', lambda p: p.mass()),
 ]
 
+# generic lepton
+lepton_vars = [
+    Variable('reliso05', lambda lep : lep.relIso(dBetaFactor=0.5, allCharged=0)),
+    Variable('dxy', lambda lep : lep.dxy()),
+    Variable('dz', lambda lep : lep.dz()),
+    Variable('weight'),
+    Variable('weight_trigger', lambda lep : lep.triggerWeight),
+    Variable('eff_trigger_data', lambda lep : lep.triggerEffData),
+    Variable('eff_trigger_mc', lambda lep : lep.triggerEffMC),
+    Variable('weight_rec_eff', lambda lep : lep.recEffWeight),
+]
 
 # electron
 electron_vars = [
-    Variable('eid_nontrigmva_loose', lambda ele: ele.mvaIDRun2("NonTrigPhys14", "Loose")),
-    Variable('eid_nontrigmva_tight', lambda ele: ele.mvaIDRun2("NonTrigPhys14", "Tight")),
-    Variable('eid_veto', lambda ele: ele.cutBasedId('POG_PHYS14_25ns_v1_Veto')),
-    Variable('eid_loose', lambda ele: ele.cutBasedId('POG_PHYS14_25ns_v1_Loose')),
-    Variable('eid_medium', lambda ele: ele.cutBasedId('POG_PHYS14_25ns_v1_Medium')),
-    Variable('eid_tight', lambda ele: ele.cutBasedId('POG_PHYS14_25ns_v1_Tight')),
-    Variable('nhits_missing', lambda ele: ele.physObj.gsfTrack().hitPattern().numberOfHits(1), int),
-    Variable('pass_conv_veto', lambda ele: ele.passConversionVeto()),
+    Variable('eid_nontrigmva_loose', lambda ele : ele.mvaIDRun2("NonTrigPhys14", "Loose")),
+    Variable('eid_nontrigmva_tight', lambda ele : ele.mvaIDRun2("NonTrigPhys14", "Tight")),
+    Variable('eid_veto', lambda ele : ele.cutBasedId('POG_PHYS14_25ns_v1_Veto')),
+    Variable('eid_loose', lambda ele : ele.cutBasedId('POG_PHYS14_25ns_v1_Loose')),
+    Variable('eid_medium', lambda ele : ele.cutBasedId('POG_PHYS14_25ns_v1_Medium')),
+    Variable('eid_tight', lambda ele : ele.cutBasedId('POG_PHYS14_25ns_v1_Tight')),
+    Variable('nhits_missing', lambda ele : ele.physObj.gsfTrack().hitPattern().numberOfHits(1), int),
+    Variable('pass_conv_veto', lambda ele : ele.passConversionVeto()),
 ]
 
+# muon
+muon_vars = [
+    Variable('muonid_loose', lambda muon : muon.muonID('POG_ID_Loose')),
+    Variable('muonid_medium', lambda muon : muon.muonID('POG_ID_Medium')),
+    Variable('muonid_tight', lambda muon : muon.muonID('POG_ID_Tight')),
+    Variable('muonid_tightnovtx', lambda muon : muon.muonID('POG_ID_TightNoVtx')),
+    Variable('muonid_highpt', lambda muon : muon.muonID('POG_ID_HighPt')),
+]
+
+# tau
+tau_vars = [
+    Variable('decayMode', lambda tau : tau.decayMode()),
+    Variable('zImpact', lambda tau : tau.zImpact())
+]
+for tauID in tauIDs:
+    tau_vars.append(Variable(tauID, lambda tau : tau.tauID(tauID)))
+
+# jet
 jet_vars = [
+    # JAN - only one PU mva working point, but we may want to add more
+    # run in our skimming step
+    # (for which Jet.py would have to be touched again)
     Variable('mva_pu', lambda jet : jet.puMva('pileupJetIdFull:full53xDiscriminant')),
     Variable('id_loose', lambda jet : jet.looseJetId()),
     Variable('id_pu', lambda jet : jet.puJetId()),
@@ -83,16 +136,13 @@ jet_vars = [
     Variable('area', lambda jet : jet.jetArea()),
     Variable('flavour_parton', lambda jet : jet.partonFlavour()),
     Variable('csv', lambda jet : jet.btag('combinedSecondaryVertexBJetTags')),
-    Variable('Bmatch', lambda jet : jet.matchGenParton),
-    Variable('rawFactor', lambda jet : jet.rawFactor()),
-
-        if hasattr(jet, 'matchedGenJet') and jet.matchedGenJet:
-            self.fill(tree, '{p_name}_genJetPt'.format(p_name=p_name), jet.matchedGenJet.pt())
+    Variable('rawfactor', lambda jet : jet.rawFactor()),
+    Variable('genjet_pt', lambda jet : jet.matchedGenJet.pt() if hasattr(jet, 'matchedGenJet') and jet.matchedGenJet else -999.),
 ]
 
 # gen info
 geninfo_vars = [
-    # Variable('geninfo_nup', type=int),
+    Variable('geninfo_nup', lambda ev : ev.NUP if hasattr(ev, 'NUP') else -1, type=int),
     Variable('geninfo_tt', type=int),
     Variable('geninfo_mt', type=int),
     Variable('geninfo_et', type=int),
@@ -210,35 +260,16 @@ class H2TauTauTreeProducer(TreeAnalyzerNumpy):
 
     def declareVariables(self, setup):
 
-        self.bookEvtID(self.tree)
+        self.bookEvent(self.tree)
         self.bookDiLepton(self.tree)
         self.bookGenInfo(self.tree)
         self.bookVBF(self.tree, 'vbf')
 
-        self.var(self.tree, 'pass_leptons')
-        self.var(self.tree, 'veto_dilepton')
-        self.var(self.tree, 'veto_thirdlepton')
-
-        self.var(self.tree, 'n_jets', int)  # pt>30 GeV
-        self.var(self.tree, 'n_jets_20', int)  # pt>20 GeV
         self.bookJet(self.tree, 'jet1')
         self.bookJet(self.tree, 'jet2')
 
-        self.var(self.tree, 'n_jets_csvl', int)
-        self.var(self.tree, 'n_bjets', int)
         self.bookJet(self.tree, 'bjet1')
         self.bookJet(self.tree, 'bjet2')
-
-        self.var(self.tree, 'n_vertices', int)
-        self.var(self.tree, 'rho')
-        self.var(self.tree, 'weight')
-        self.var(self.tree, 'weight_vertex')
-        self.var(self.tree, 'weight_embed')
-        self.var(self.tree, 'weight_njet')
-
-        self.var(self.tree, 'weight_hqt')
-        self.var(self.tree, 'weight_hqt_up')
-        self.var(self.tree, 'weight_hqt_down')
 
         self.bookParticle(self.tree, 'pfmet')
 
@@ -257,43 +288,17 @@ class H2TauTauTreeProducer(TreeAnalyzerNumpy):
         if not eval(self.skimFunction):
             return False
 
-        self.fillEvtID(self.tree, event)
+        self.fillEvent(self.tree, event)
         self.fillDiLepton(self.tree, event.diLepton)
         self.fillGenInfo(self.tree, event)
         if hasattr(event, 'vbf'):
             self.fillVBF(self.tree, 'vbf', event.vbf)
 
-        self.fill(self.tree, 'pass_leptons', event.isSignal)
-        self.fill(self.tree, 'veto_dilepton', event.leptonAccept)
-        self.fill(self.tree, 'veto_thirdlepton', event.thirdLeptonVeto)
-
-        nJets = len(event.cleanJets30)
-        nJets20 = len(event.cleanJets)
-        self.fill(self.tree, 'n_jets', nJets)
-        self.fill(self.tree, 'n_jets_20', nJets20)
         for i, jet in enumerate(event.cleanJets[:2]):
-            self.fillJet(self.tree, 'jet{NUM}'.format(NUM=str(i + 1)), jet)
+            self.fillJet(self.tree, 'jet{n}'.format(n=str(i + 1)), jet)
 
-        n_bjets = len(event.cleanBJets)
-        self.fill(self.tree, 'n_bjets', n_bjets)
         for i, jet in enumerate(event.cleanBJets[:2]):
-            self.fillJet(self.tree, 'bjet{NUM}'.format(NUM=str(i + 1)), jet)
-        # JAN - directly count CSVL jets as done in
-        # other groups. May apply SFs as for CSVM jets
-        # after rewriting BTagSF module in the future
-        n_jets_csvl = sum(1 for jet in event.cleanJets if jet.btag('combinedSecondaryVertexBJetTags') > 0.244)
-        self.fill(self.tree, 'n_jets_csvl', n_jets_csvl)
-
-        self.fill(self.tree, 'n_vertices', len(event.vertices))
-        self.fill(self.tree, 'rho', event.rho)
-        self.fill(self.tree, 'weight', event.eventWeight)
-        self.fill(self.tree, 'weight_vertex', event.vertexWeight)
-        self.fill(self.tree, 'weight_embed', event.embedWeight)
-        self.fill(self.tree, 'weight_njet', event.NJetWeight)
-
-        self.fill(self.tree, 'weight_hqt', event.higgsPtWeight)
-        self.fill(self.tree, 'weight_hqt_up', event.higgsPtWeightUp)
-        self.fill(self.tree, 'weight_hqt_down', event.higgsPtWeightDown)
+            self.fillJet(self.tree, 'bjet{n}'.format(n=str(i + 1)), jet)
 
         if hasattr(event, 'parentBoson'):
             self.fillGenParticle(self.tree, 'genboson', event.parentBoson)
@@ -304,25 +309,19 @@ class H2TauTauTreeProducer(TreeAnalyzerNumpy):
         if type(self) is H2TauTauTreeProducer:
             self.fillTree(event)
 
-    # event ID
-    def bookEvtID(self, tree):
-        self.var(tree, 'run', int)
-        self.var(tree, 'lumi', int)
-        self.var(tree, 'event', int)
+    # event
+    def bookEvent(self, tree):
+        self.bookGeneric(tree, event_vars)
 
-    def fillEvtID(self, tree, event):
-        self.fill(tree, 'run', event.run)
-        self.fill(tree, 'lumi', event.lumi)
-        self.fill(tree, 'event', event.eventId)
+    def fillEvent(self, tree, event):
+        self.fillGeneric(tree, event_vars, event)
 
     # simple particle
     def bookParticle(self, tree, p_name):
-        for var in particle_vars:
-            self.var(tree, '{p_name}_{v_name}'.format(p_name=p_name, v_name=var.name))
+        self.bookGeneric(tree, particle_vars, p_name)
 
     def fillParticle(self, tree, p_name, particle):
-        for var in particle_vars:
-            self.fill(tree, '{p_name}_{v_name}'.format(p_name=p_name, v_name=var.name), var.function(particle))
+        self.fillGeneric(tree, particle_vars, particle, p_name)
 
     # simple gen particle
     def bookGenParticle(self, tree, p_name):
@@ -351,126 +350,48 @@ class H2TauTauTreeProducer(TreeAnalyzerNumpy):
     def bookLepton(self, tree, p_name):
         self.bookParticle(tree, p_name)
         self.bookParticle(tree, p_name + '_jet')
-        self.var(tree, '{p_name}_reliso05'      .format(p_name=p_name))
-        self.var(tree, '{p_name}_dxy'           .format(p_name=p_name))
-        self.var(tree, '{p_name}_dz'            .format(p_name=p_name))
-        self.var(tree, '{p_name}_weight'        .format(p_name=p_name))
-        self.var(tree, '{p_name}_weight_trigger' .format(p_name=p_name))
-        self.var(tree, '{p_name}_eff_trigger_data'.format(p_name=p_name))
-        self.var(tree, '{p_name}_eff_trigger_mc'  .format(p_name=p_name))
-        self.var(tree, '{p_name}_weight_rec_eff'  .format(p_name=p_name))
+        self.bookGeneric(tree, lepton_vars, p_name)
 
     def fillLepton(self, tree, p_name, lepton):
         self.fillParticle(tree, p_name, lepton)
         self.fillParticle(tree, p_name + '_jet', lepton.jet)
-        self.fill(tree, '{p_name}_reliso05'      .format(
-            p_name=p_name), lepton.relIso(dBetaFactor=0.5, allCharged=0))
-        try:
-            self.fill(tree, '{p_name}_dxy'.format(p_name=p_name), lepton.dxy())
-            self.fill(tree, '{p_name}_dz'.format(p_name=p_name), lepton.dz())
-        except:
-            self.fill(tree, '{p_name}_dxy'.format(p_name=p_name), 999.)
-            self.fill(tree, '{p_name}_dz'.format(p_name=p_name), 999.)
-
-        self.fill(
-            tree, '{p_name}_weight'        .format(p_name=p_name), lepton.weight)
-        self.fill(tree, '{p_name}_weight_trigger' .format(
-            p_name=p_name), lepton.triggerWeight)
-        self.fill(tree, '{p_name}_eff_trigger_data'.format(
-            p_name=p_name), lepton.triggerEffData)
-        self.fill(tree, '{p_name}_eff_trigger_mc'  .format(
-            p_name=p_name), lepton.triggerEffMC)
-        self.fill(tree, '{p_name}_weight_rec_eff'  .format(
-            p_name=p_name), lepton.recEffWeight)
+        self.fillGeneric(tree, lepton_vars, lepton, p_name)
 
     # muon
     def bookMuon(self, tree, p_name):
         self.bookLepton(tree, p_name)
-        # JAN FIXME - do we need the MVA iso and does it exist?
-        # var(tree, '{p_name}_mvaIso'.format(p_name=p_name))
-        self.var(tree, '{p_name}_muonid_loose'     .format(p_name=p_name))
-        self.var(tree, '{p_name}_muonid_medium'    .format(p_name=p_name))
-        self.var(tree, '{p_name}_muonid_tight'     .format(p_name=p_name))
-        self.var(tree, '{p_name}_muonid_tightnovtx'.format(p_name=p_name))
-        self.var(tree, '{p_name}_muonid_highpt'    .format(p_name=p_name))
+        self.bookGeneric(tree, muon_vars, p_name)
 
     def fillMuon(self, tree, p_name, muon):
         self.fillLepton(tree, p_name, muon)
-        # JAN FIXME - do we need the MVA iso and does it exist?
-        # fill(tree, '{p_name}_mvaIso'.format(p_name=p_name), muon.mvaIso() )
-        self.fill(tree, '{p_name}_muonid_loose'     .format(
-            p_name=p_name), muon.muonID('POG_ID_Loose'))
-        self.fill(tree, '{p_name}_muonid_medium'    .format(
-            p_name=p_name), muon.muonID('POG_ID_Medium'))
-        self.fill(tree, '{p_name}_muonid_tight'     .format(
-            p_name=p_name), muon.muonID('POG_ID_Tight'))
-        self.fill(tree, '{p_name}_muonid_tightnovtx'.format(
-            p_name=p_name), muon.muonID('POG_ID_TightNoVtx'))
-        self.fill(tree, '{p_name}_muonid_highpt'    .format(
-            p_name=p_name), muon.muonID('POG_ID_HighPt'))
+        self.fillGeneric(tree, muon_vars, muon, p_name)
 
-
+    # ele
     def bookEle(self, tree, p_name):
         self.bookLepton(tree, p_name)
-        for var in electron_vars:
-            self.var(tree, '{p_name}_{v_name}'.format(p_name=p_name, v_name=var.name), var.type)
+        self.bookGeneric(tree, electron_vars, p_name)
 
     def fillEle(self, tree, p_name, ele):
         self.fillLepton(tree, p_name, ele)
-        for var in electron_vars:
-            self.fill(tree, '{p_name}_{v_name}'.format(p_name=p_name, v_name=var.name), var.function(ele))
-
-
+        self.fillGeneric(tree, electron_vars, ele, p_name)
 
     # tau
     def bookTau(self, tree, p_name):
         self.bookLepton(tree, p_name)
-        for tauID in tauIDs:
-            self.var(tree, '{p_name}_{tauID}'.format(p_name=p_name, tauID=tauID))
-        self.var(tree, '{p_name}_eoverp'.format(p_name=p_name))
-        self.var(tree, '{p_name}_decayMode'.format(p_name=p_name))
-        self.var(tree, '{p_name}_zImpact'.format(p_name=p_name))
+        self.bookGeneric(tree, tau_vars, p_name)
 
     def fillTau(self, tree, p_name, tau):
         self.fillLepton(tree, p_name, tau)
-        for tauID in tauIDs:
-            self.fill(tree, '{p_name}_{tauID}'.format(
-                p_name=p_name, tauID=tauID), tau.tauID(tauID))
-        #self.fill(tree, '{p_name}_eoverp'   .format(p_name=p_name), tau.calcEOverP())
-        self.fill(
-            tree, '{p_name}_decayMode'.format(p_name=p_name), tau.decayMode())
-        self.fill(tree, '{p_name}_zImpact'  .format(p_name=p_name), tau.zImpact())
+        self.fillGeneric(tree, tau_vars, tau, p_name)
 
     # jet
     def bookJet(self, tree, p_name):
         self.bookParticle(tree, p_name)
-        self.var(tree, '{p_name}_puMva'.format(p_name=p_name))
-        self.var(tree, '{p_name}_looseJetId'.format(p_name=p_name))
-        self.var(tree, '{p_name}_puJetId'.format(p_name=p_name))
-        self.var(tree, '{p_name}_btagMVA'.format(p_name=p_name))
-        self.var(tree, '{p_name}_area'.format(p_name=p_name))
-        self.var(tree, '{p_name}_genJetPt'.format(p_name=p_name))
-        self.var(tree, '{p_name}_partonFlavour'.format(p_name=p_name))
-        self.var(tree, '{p_name}_csv'.format(p_name=p_name))
-        self.var(tree, '{p_name}_Bmatch'.format(p_name=p_name))
-        self.var(tree, '{p_name}_rawFactor'.format(p_name=p_name))
-
+        self.bookGeneric(tree, jet_vars, p_name)
+        
     def fillJet(self, tree, p_name, jet):
         self.fillParticle(tree, p_name, jet)
-        # JAN - only one PU mva working point, but we may want to add more
-        # run in our skimming step
-        # (for which Jet.py would have to be touched again)
-        self.fill(tree, '{p_name}_puMva'.format(p_name=p_name), jet.puMva('pileupJetIdFull:full53xDiscriminant'))
-        self.fill(tree, '{p_name}_looseJetId'.format(p_name=p_name), jet.looseJetId())
-        self.fill(tree, '{p_name}_puJetId'.format(p_name=p_name), jet.puJetId())
-        self.fill(tree, '{p_name}_btagMVA'.format(p_name=p_name), jet.btagMVA)
-        self.fill(tree, '{p_name}_area'.format(p_name=p_name), jet.jetArea())
-        self.fill(tree, '{p_name}_partonFlavour'.format(p_name=p_name), jet.partonFlavour())
-        self.fill(tree, '{p_name}_csv'.format(p_name=p_name), jet.btag('combinedSecondaryVertexBJetTags'))
-        self.fill(tree, '{p_name}_Bmatch'.format(p_name=p_name), jet.matchGenParton)
-        self.fill(tree, '{p_name}_rawFactor'.format(p_name=p_name), jet.rawFactor())
-        if hasattr(jet, 'matchedGenJet') and jet.matchedGenJet:
-            self.fill(tree, '{p_name}_genJetPt'.format(p_name=p_name), jet.matchedGenJet.pt())
+        self.fillGeneric(tree, jet_vars, jet, p_name)
 
     # vbf
     def bookVBF(self, tree, p_name):
