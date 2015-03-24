3a1c96e8d9575718db4a83cc741fc17058be3cb3
Giovanni <gpetruc@gmail.com>
2015-03-24 23:24:57 +0100

    getHeppyOption, and add tests

--- CMGTools/HToZZ4L/cfg/run_hzz4l_cfg.py
+++ CMGTools/HToZZ4L/cfg/run_hzz4l_cfg.py
@@ -15,17 +15,31 @@ from CMGTools.HToZZ4L.samples.samples_13TeV_PHYS14 import *
 selectedComponents = []
 sequence = cfg.Sequence(hzz4lCoreSequence)
 
+for comp in mcSamples:
+    comp.triggers = triggers_multilep
+    comp.vetoTriggers = []
 
-    
-test = 1
-if test == 1:
+from PhysicsTools.HeppyCore.framework.heppy import getHeppyOption
+test = getHeppyOption('test')
+if test == "1":
     comp = GGHZZ4L
-    comp.files = comp.files[:1]
+    comp.files = [ 'root://eoscms//eos/cms/store/mc/Phys14DR/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/3295EF7C-2070-E411-89C4-7845C4FC35DB.root ' ]
+    comp.splitFactor = 1
+    comp.fineSplitFactor = 1 if getHeppyOption('single') else 5
+    selectedComponents = [ comp ]
+elif test == '2':
+    for comp in selectedComponents:
+        comp.files = comp.files[:1]
+        comp.splitFactor = 1
+        comp.fineSplitFactor = 1
+elif test == 'Debug':
+    comp = GGHZZ4L
     comp.files = [ 'root://eoscms//eos/cms/store/mc/Phys14DR/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/3295EF7C-2070-E411-89C4-7845C4FC35DB.root ' ]
     comp.splitFactor = 1
     comp.fineSplitFactor = 1
     selectedComponents = [ comp ]
-
+    eventSelector.toSelect = [ (1,1,2), (1,1,53), (1,1,69), (1,1,75), (1,1,79) ] 
+    sequence = cfg.Sequence([eventSelector] + hzz4lCoreSequence)
 
 
 # the following is declared in case this cfg is used in input to the heppy.py script
