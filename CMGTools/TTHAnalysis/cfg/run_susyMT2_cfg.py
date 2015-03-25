27feb8152df17d00d4ce28fe5f04296432fc2e6a
maria <dalfonso@cern.ch>
2015-03-25 20:50:25 +0100

    updated SV analyzer: MC matching configurable and fixes cases w/o goodVertices

--- CMGTools/TTHAnalysis/cfg/run_susyMT2_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susyMT2_cfg.py
@@ -70,6 +70,9 @@ metAna.recalibrate = False
 # store all taus by default
 genAna.allGenTaus = True
 
+# switch off the SV and MC matching
+#ttHSVAna.do_mc_match = False
+
 ##------------------------------------------ 
 ##  CONTROL VARIABLES
 ##------------------------------------------ 
@@ -148,6 +151,9 @@ susyCoreSequence.insert(susyCoreSequence.index(skimAnalyzer),
 
 #susyCoreSequence.insert(susyCoreSequence.index(ttHLepSkim),
 #                        ttHZskim)
+
+#susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+#                        ttHSVAna)
 
 sequence = cfg.Sequence(
     susyCoreSequence+[
