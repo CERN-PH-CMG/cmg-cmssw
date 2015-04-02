018c67f76b64186fb565bc2b365752fa7b4fa87f
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-04-02 11:49:02 +0200

    fixed bug: not cutting anymore on parton eta for computation of gamma_drMinParton

--- PhysicsTools/Heppy/python/analyzers/objects/PhotonAnalyzer.py
+++ PhysicsTools/Heppy/python/analyzers/objects/PhotonAnalyzer.py
@@ -108,7 +108,7 @@ class PhotonAnalyzer( Analyzer ):
         match = matchObjectCollection3(event.allphotons, event.genPhotonsMatched, deltaRMax = 0.1)
         matchNoMom = matchObjectCollection3(event.allphotons, event.genPhotonsWithoutMom, deltaRMax = 0.1)
         packedGenParts = [ p for p in self.mchandles['packedGen'].product() if abs(p.eta()) < 3.1 ]
-        partons = [ p for p in self.mchandles['prunedGen'].product() if abs(p.eta()) < 3.1 and (p.status()==23 or p.status()==22) and abs(p.pdgId())<22 ]
+        partons = [ p for p in self.mchandles['prunedGen'].product() if (p.status()==23 or p.status()==22) and abs(p.pdgId())<22 ]
         for gamma in event.allphotons:
           gen = match[gamma]
           gamma.mcGamma = gen
