c22b5cda73769af9ca1a59e30e9ef90129d8d862
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-05-05 11:48:12 +0200

    first shot of an analysis runner script

---
+++ CMGTools/MonoXAnalysis/python/plotter/monojet.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+WHAT=$1; if [[ "$1" == "" ]]; then echo "monojet.sh <what>"; exit 1; fi
+
+if [[ "$HOSTNAME" == "cmsphys06" ]]; then
+    T="/data1/emanuele/monox/TREES_040515_MET200SKIM";
+    J=6;
+else
+    T="/cmshome/dimarcoe/TREES_040515_MET200SKIM";
+    J=6;
+fi
+COREOPT="-P $T --s2v -j $J -l 5.0"
+COREY="mcAnalysis.py ${COREOPT} -G  "
+COREP="mcPlots.py  ${COREOPT} -f --poisson "
+FEV=" -F mjvars/t \"$T/0_eventvars_mj_v1/evVarFriend_{cname}.root\" "
+
+ROOT="plots/050515/v1.0/$WHAT"
+
+RUNY="${COREY} mca-Phys14.txt --s2v "
+RUNYSR="${RUNY} sr/monojet.txt "
+
+PLOT="${COREP} mca-Phys14.txt --s2v "
+PLOTSR="${PLOT} sr/monojet.txt sr/monojet-plots.txt "
+
+case $WHAT in
+sr)
+        SF="-W 'puWeight'"
+        echo "python ${RUNYSR} $FEV $SF "
+        echo "python ${PLOTSR}  $FEV $SF "
+;;
+esac;
