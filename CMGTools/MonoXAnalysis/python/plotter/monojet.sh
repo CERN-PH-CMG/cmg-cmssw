85cc7605b855da221cc68a91d25756c0f0e85b16
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-05-19 13:08:32 +0200

    use the local path of the trees, faster than AFS

--- CMGTools/MonoXAnalysis/python/plotter/monojet.sh
+++ CMGTools/MonoXAnalysis/python/plotter/monojet.sh
@@ -3,7 +3,7 @@
 WHAT=$1; if [[ "$1" == "" ]]; then echo "monojet.sh <what>"; exit 1; fi
 
 if [[ "$HOSTNAME" == "cmsphys06" ]]; then
-    T="/data1/emanuele/monox/TREES_060515_MET200SKIM";
+    T="/data1/emanuele/monox/TREES_150515_MET200SKIM";
     J=6;
 else
     T="/cmshome/dimarcoe/TREES_060515_MET200SKIM";
@@ -27,7 +27,7 @@ sr)
         #SF="-W 'puWeight'"
         SF=" "
         echo "python ${RUNYSR} $FEV $SF "
-        echo "python ${PLOTSR}  $FEV $SF "
+        echo "python ${PLOTSR}  $FEV $SF --pdir plots/sr "
 ;;
 wmunu)
         echo "python ${RUNY} control-samples/w_munu.txt $FEV --sp WJets --xp M10V "
