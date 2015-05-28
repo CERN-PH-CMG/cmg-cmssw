0f94b661b6e2e9926db0c48685c4661726dda26f
Giovanni <gpetruc@gmail.com>
2015-05-28 15:20:41 +0200

    Definition of categories in the event dumper

--- CMGTools/HToZZ4L/python/scripts/eventDumper.py
+++ CMGTools/HToZZ4L/python/scripts/eventDumper.py
@@ -20,6 +20,7 @@ parser.add_option("-j", "--json",   dest="json",  default=None, type="string", h
 parser.add_option("-n", "--maxEvents",  dest="maxEvents", default=-1, type="int", help="Max events")
 parser.add_option("-f", "--format",   dest="fmt",  default=None, type="string",  help="Print this format string")
 parser.add_option("-t", "--tree",          dest="tree", default='tree', help="Pattern for tree name");
+parser.add_option("-E", "--events", dest="events", default=[], action="append", help="Events to select")
 
 (options, args) = parser.parse_args(); sys.argv = []
 
@@ -51,14 +52,64 @@ class BaseDumper(Module):
         self.options = options
     def preselect(self,ev):
         return True
-    def makeVars(self,ev):
+    def makeVars(self,ev,zzkind="zz"):
         jets = Collection(ev,"Jet")
         ev.Jet1_pt_zs = jets[0].pt if len(jets) > 0 else -1.0
         ev.Jet2_pt_zs = jets[1].pt if len(jets) > 1 else -1.0
+        allleps = Collection(ev,"Lep","nLep") 
+        zzs = Collection(ev,zzkind,"n"+zzkind)
+        if len(zzs) == 0:
+            ev.catetory = -1
+            return
+        zz = zzs[0]
+        ils = [ findLepIndex(allleps, zz.z1_l1_pt, zz.z1_l1_eta),
+                findLepIndex(allleps, zz.z1_l2_pt, zz.z1_l2_eta),
+                findLepIndex(allleps, zz.z2_l1_pt, zz.z2_l1_eta),
+                findLepIndex(allleps, zz.z2_l2_pt, zz.z2_l2_eta) ]
+        lepsS = []
+        for i,l in enumerate(allleps):
+            if i in ils: 
+                lepsS.append(l)
+            elif l.tightId and l.relIso04 < (0.4 if abs(l.pdgId)==13 else 0.5) and l.sip3d < 4:
+                lepsS.append(l)
+        ev.nLepSel = len(lepsS)
+        ev.mjj = (jets[0].p4()+jets[1].p4()).M() if len(jets) >= 2 else 0
+        ev.Djet = 0.18*abs(jets[0].eta-jets[1].eta) + 1.92e-04*ev.mjj if len(jets) >= 2 else 0
+        ev.nB = sum([(j.btagCSV > 0.814 and j.pt > 30) for j in jets]) if len(jets) else 0
+        jets40c = [ j for j in jets if j.pt > 40 and abs(j.eta) < 2.4 ]
+        ev.nJet40c = len(jets40c)
+        ev.vhjjTags = 0; ev.mjj40c = 0
+        if len(jets40c) >= 2:
+            for i1,j1 in enumerate(jets40c[:-1]):
+                for j2 in jets40c[i1+1:]:
+                   mjj40c = (j1.p4()+j2.p4()).M()
+                   if 60 < mjj40c and mjj40c < 120:
+                        ev.vhjjTags += 1
+                        if abs(mjj40c - 90) < abs(ev.mjj40c - 90):
+                            ev.mjj40c = mjj40c
+        if ev.nLepSel == 4 and ev.nJet30 >= 2 and ev.nB <= 1 and ev.Djet > 0.5:
+          ev.category = 2
+        elif ev.nLepSel == 4 and ev.nJet40c >= 2 and (60 < ev.mjj40c and ev.mjj40c < 120) and zz.pt > zz.mass:
+          ev.category = 4
+        elif ev.nLepSel == 4 and ev.nJet30 == 2 and ev.nB == 2:
+          ev.category = 4
+        elif ev.nLepSel >= 5 and ev.nJet30 <= 2 and ev.nB == 0:
+          ev.category = 3
+        elif ev.nJet30 >= 3 and ev.nB >= 1:
+          ev.category = 5
+        elif ev.nLepSel >= 5:
+          ev.category = 5
+        elif ev.nJet30 >= 1:
+          ev.category = 1
+        else:
+          ev.category = 0
     def analyze(self,ev):
+        if self.options.events and ( (ev.run, ev.lumi, ev.evt) not in self.options.events ):
+            return False
         try:
             self.makeVars(ev)
         except:
+            raise
             pass
         if self.options.fmt: 
             print string.Formatter().vformat(options.fmt.replace("\\t","\t"),[],ev)
@@ -84,8 +135,9 @@ class BaseDumper(Module):
             print "    photon %d:        pt %5.1f eta %+4.2f phi %+4.2f             reliso% 6.3f (ch %5.1f nh %5.1f ph %5.1f pu %5.1f), closest lepton id %+2d pt %5.1f eta %+4.2f phi %+4.2f dr %.4f " % (i+1, 
                         g.pt, g.eta, g.phi, g.relIso, g.chargedHadIso, g.neutralHadIso, g.photonIso, g.puChargedHadIso, g.closestLepton_pdgId, g.closestLepton_pt, g.closestLepton_eta, g.closestLepton_phi, g.closestLeptonDR)
 
-        for type in "zz", "zz2P2F", "zz3P1F", "zzSS":
+        for type in "zz", "zz2P2F", "zz3P1F", "zzSS", "zzRelII":
             zzs = Collection(ev,type)
+            self.makeVars(ev,zzkind=type)
             if len(zzs):
                 print "    four-lepton candidates of type %s" % type
             for izz,zz in enumerate(zzs):
@@ -99,6 +151,9 @@ class BaseDumper(Module):
                         izz, ils[0]+1,ils[1]+1,ils[2]+1,ils[3]+1, zz.mass, zz.z1_mass, zz.z2_mass, ifsr1+1, ifsr2+1 )
                 print "                   m12 %6.3f  m13 %6.3f  m14 %6.3f  m23 %6.3f  m24 %6.3f  m34 %6.3f" % (
                          zz.mll_12, zz.mll_13, zz.mll_14, zz.mll_23, zz.mll_24, zz.mll_34)
+                print "                   KD %.3f pt4l %6.1f mjj %6.1f Djet %.3f nLepSel %d nJet30 %d nB %d nJet40c %d, mjj40c %6.1f: category %d" % (
+                          zz.KD, zz.pt, ev.mjj, ev.Djet, ev.nLepSel, ev.nJet30, ev.nB, ev.nJet40c, ev.mjj40c, ev.category
+                        )
         print "    met %6.2f (phi %+4.2f)" % (ev.met_pt, ev.met_phi)
         print "    vertices %d" % (ev.nVert)
         print "    HLT: ", " ".join([t for t in "DoubleMu DoubleEl TripleEl MuEG".split() if getattr(ev,"HLT_"+t)])
@@ -107,7 +162,13 @@ class BaseDumper(Module):
 cut = None
 if options.cut:
     cut = options.cut
- 
+if options.events:
+    rles = []
+    for ids in options.events:
+        for i in ids.split():
+            (r,l,e) = map(int, i.split(":"))
+            rles.append((r,l,e))
+    options.events = rles
 file = ROOT.TFile.Open(args[0])
 tree = file.Get(options.tree)
 tree.vectorTree = False
