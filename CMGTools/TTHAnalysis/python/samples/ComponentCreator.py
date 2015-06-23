951110f7ec327990765a62d0fb019c90872e1257
maria <dalfonso@cern.ch>
2015-06-23 10:40:24 +0200

    updated files for skims

--- CMGTools/TTHAnalysis/python/samples/ComponentCreator.py
+++ CMGTools/TTHAnalysis/python/samples/ComponentCreator.py
@@ -1,6 +1,8 @@
 import CMGTools.RootTools.fwlite.Config as cfg
 from CMGTools.Production.datasetToSource import datasetToSource
 from CMGTools.Production.datasetInformation import DatasetInformation
+from CMGTools.Production import eostools
+import re
 
 class ComponentCreator(object):
     def makeMCComponent(self,name,dataset,user,pattern,xSec=1):
@@ -37,6 +39,30 @@ class ComponentCreator(object):
 
  #        print 'Skim Efficiency for ',name,'=', component.skimEfficiency
          return component
+
+    def getFilesFromEOS(self,name,dataset,path,pattern=".*root"):
+        from CMGTools.Production.dataset import getDatasetFromCache, writeDatasetToCache
+        if "%" in path: path = path % dataset;
+        try:
+            files = getDatasetFromCache('EOS%{path}%{pattern}.pck'.format(path = path.replace('/','_'), pattern = pattern))
+        except IOError:
+            files = [ 'root://eoscms.cern.ch/'+x for x in eostools.listFiles('/eos/cms'+path) if re.match(pattern,x) ]
+            if len(files) == 0:
+                raise RuntimeError, "ERROR making component %s: no files found under %s matching '%s'" % (name,path,pattern)
+            writeDatasetToCache('EOS%{path}%{pattern}.pck'.format(path = path.replace('/','_'), pattern = pattern), files)
+        return files
+
+    def makeMCComponentFromEOS(self,name,dataset,path,pattern=".*root",xSec=1):
+        component = cfg.MCComponent(
+            dataset=dataset,
+            name = name,
+            files = self.getFilesFromEOS(name,dataset,path,pattern),
+            xSection = xSec,
+            nGenEvents = 1,
+            triggers = [],
+            effCorrFactor = 1,
+        )
+        return component
 
     def makeDataComponent(self,name,datasets,user,pattern):
          files=[]
