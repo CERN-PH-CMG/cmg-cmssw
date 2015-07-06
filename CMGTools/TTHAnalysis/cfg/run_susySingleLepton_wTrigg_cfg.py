c8f9fe5c3d5412eb69de1709c698b49b8a0af112
Artur Lobanov <artur.lobanov@desy.de>
2015-07-06 14:28:13 +0200

    Use samples from RootTools

--- CMGTools/TTHAnalysis/cfg/run_susySingleLepton_wTrigg_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susySingleLepton_wTrigg_cfg.py
@@ -83,8 +83,8 @@ ttHReclusterJets = cfg.Analyzer(
 	etaSubJet = 5.0,
 			)
 
-#from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14  import *
-from CMGTools.TTHAnalysis.samples.triggers_13_TeV_Spring15 import *
+#from CMGTools.RootTools.samples.samples_13TeV_PHYS14  import *
+from CMGTools.RootTools.samples.triggers_13_TeV_Spring15 import *
 
 triggerFlagsAna.triggerBits = {
 #put trigger here for data
@@ -121,11 +121,11 @@ treeProducer = cfg.Analyzer(
 #-------- SAMPLES AND TRIGGERS -----------
 
 # -- old PHYS14
-#from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14 import *
+#from CMGTools.RootTools.samples.samples_13TeV_PHYS14 import *
 #selectedComponents = [QCD_HT_100To250, QCD_HT_250To500, QCD_HT_500To1000, QCD_HT_1000ToInf,TTJets, TTWJets, TTZJets, TTH, SMS_T1tttt_2J_mGl1500_mLSP100, SMS_T1tttt_2J_mGl1200_mLSP800] + SingleTop + WJetsToLNuHT + DYJetsM50HT + T5ttttDeg + T1ttbbWW + T5qqqqWW
 
 # -- new 74X samples
-from CMGTools.TTHAnalysis.samples.samples_13TeV_74X import *
+from CMGTools.RootTools.samples.samples_13TeV_74X import *
 
 selectedComponents = [
 #TTJets,
@@ -165,7 +165,7 @@ elif test==3:
 		comp.splitFactor = len(comp.files)
 
 elif test=="data":
-	from CMGTools.TTHAnalysis.samples.samples_13TeV_Data import *
+	from CMGTools.RootTools.samples.samples_13TeV_Data import *
 	selectedComponents = [ privEGamma2015A ]
 
 	for comp in selectedComponents:
