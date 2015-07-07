aeef2ad8ece23566618fbb7ecc9b198374ce1600
Artur Lobanov <artur.lobanov@desy.de>
2015-07-07 13:50:00 +0200

    Update triggers

--- CMGTools/SUSYAnalysis/python/tools/eventVars_1l_triggers.py
+++ CMGTools/SUSYAnalysis/python/tools/eventVars_1l_triggers.py
@@ -14,7 +14,7 @@ class EventVars1L_triggers:
             'HLT_SingleMu', 'HLT_MuNoIso', 'HLT_MuHad',
             'HLT_MuHT600', 'HLT_MuHT400MET70','HLT_MuMET120', 'HLT_MuHT400B',
             'HLT_SingleEl', 'HLT_ElNoIso', 'HLT_ElHad',
-            'HLT_EleHT600','HLT_EleHT400MET70','HLT_EleHT200', 'HLT_ElHT400B'
+            'HLT_EleHT600','HLT_EleHT400MET70','HLT_EleHT200', 'HLT_EleHT400B'
             ]
 
     def listBranches(self):
@@ -25,50 +25,31 @@ class EventVars1L_triggers:
         # output dict:
         ret = {}
 
-        '''
         ret['HLT_HT350'] = event.HLT_HT350
+        ret['HLT_HT600'] = event.HLT_HT600
         ret['HLT_HT900'] = event.HLT_HT900
         ret['HLT_MET170'] = event.HLT_MET170
         ret['HLT_HTMET'] = event.HLT_HTMET
+        ret['HLT_Had'] = event.HLT_Had
+
 
         ret['HLT_SingleMu'] = event.HLT_SingleMu
-        ret['HLT_SingleEl'] = event.HLT_SingleEl
+        ret['HLT_MuNoIso'] = event.HLT_MuNoIso
+        ret['HLT_MuHad'] = event.HLT_MuHad
 
         ret['HLT_MuHT600'] = event.HLT_MuHT600
-        ret['HLT_MuHT400MET70'] = event.HLT_MuHT400MET70
         ret['HLT_MuMET120'] = event.HLT_MuMET120
+        ret['HLT_MuHT400MET70'] = event.HLT_MuHT400MET70
+        ret['HLT_MuHT400B'] = event.HLT_MuHT400B
 
-        ret['HLT_EleHT600'] = event.HLT_EleHT600
-        ret['HLT_EleHT400MET70'] = event.HLT_EleHT400MET70
+        ret['HLT_SingleEl'] = event.HLT_SingleEl
+        ret['HLT_ElNoIso'] = event.HLT_ElNoIso
+        ret['HLT_ElHad'] = event.HLT_ElHad
+
         ret['HLT_EleHT200'] = event.HLT_EleHT200
-        '''
-
-        ret['HLT_HT350'] = event.HLT_HT350,
-        ret['HLT_HT600'] = event.HLT_HT600,
-        ret['HLT_HT900'] = event.HLT_HT900,
-        ret['HLT_MET170'] = event.HLT_MET170,
-        ret['HLT_HTMET'] = event.HLT_HTMET,
-        ret['HLT_Had'] = event.HLT_Had,
-
-        ret['HLT_SingleMu'] = event.HLT_SingleMu,
-        ret['HLT_MuNoIso'] = event.HLT_MuNoIso,
-        ret['HLT_MuHad'] = event.HLT_MuHad,
-
-        ret['HLT_MuHT600'] = event.HLT_MuHT600,
-        ret['HLT_MuHT400MET70'] = event.HLT_MuHT400MET70,
-        ret['HLT_MuMET120'] = event.HLT_MuMET120,
-        ret['HLT_MuHT400B'] = event.HLT_MuHT400B,
-
-        ret['HLT_SingleEl'] = event.HLT_SingleEl,
-        ret['HLT_ElNoIso'] = event.HLT_ElNoIso,
-        ret['HLT_ElHad'] = event.HLT_ElHad,
-
-        ret['HLT_EleHT600'] = event.HLT_EleHT600,
-        ret['HLT_EleHT400MET70'] = event.HLT_EleHT400MET70,
-        ret['HLT_EleHT200'] = event.HLT_EleHT200,
-        ret['HLT_ElHT400B'] = event.HLT_ElHT400B]
-
-
+        ret['HLT_EleHT400MET70'] = event.HLT_EleHT400MET70
+        ret['HLT_EleHT600'] = event.HLT_EleHT600
+        ret['HLT_EleHT400B'] = event.HLT_ElHT400B
 
         # return branches
         return ret
