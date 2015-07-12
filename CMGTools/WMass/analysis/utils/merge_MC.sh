9f794598e89b8f7e3ec8ad12a3125baa97d26fd9
Andrea Stacchiotti <andrea.stacchiotti@cern.ch>
2015-07-12 19:02:40 +0200

    Commented out chunks deletion

--- CMGTools/WMass/analysis/utils/merge_MC.sh
+++ CMGTools/WMass/analysis/utils/merge_MC.sh
@@ -60,7 +60,8 @@ for (( id_sample=0; id_sample<${#samples[@]}; id_sample++ ))
         fi
       fi
       hadd -f ${1}/output_${samples[id_sample]}/${analyses[id_ana]}OnDATA.root ${1}/output_${samples[id_sample]}/${analyses[id_ana]}_chunk*.root 
-      rm ${1}/output_${samples[id_sample]}/${analyses[id_ana]}_chunk*.root 
+# Commented to prevent chunks deletion on merge
+#      rm ${1}/output_${samples[id_sample]}/${analyses[id_ana]}_chunk*.root 
       rm -rf ${1}/output_${samples[id_sample]}/LSFJOB_*
       rm -rf ${1}/output_${samples[id_sample]}/*.out
     fi
