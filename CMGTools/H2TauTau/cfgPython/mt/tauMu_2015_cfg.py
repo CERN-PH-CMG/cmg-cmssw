df4dc3fb1419534d3edad4862de9c575b98459d1
Jan Steggemann <jan.steggemann@cern.ch>
2015-07-29 17:19:46 +0200

    Split tau-mu configs

--- CMGTools/H2TauTau/cfgPython/mt/tauMu_2015_cfg.py
+++ CMGTools/H2TauTau/cfgPython/mt/tauMu_2015_cfg.py
@@ -1,138 +1,23 @@
 import PhysicsTools.HeppyCore.framework.config as cfg
+
+from tauMu_2015_base_cfg import sequence
+
 from PhysicsTools.HeppyCore.framework.config import printComps
 from PhysicsTools.HeppyCore.framework.heppy_loop import getHeppyOption
 
-# Tau-tau analyzers
-from CMGTools.H2TauTau.proto.analyzers.TauMuAnalyzer import TauMuAnalyzer
-from CMGTools.H2TauTau.proto.analyzers.H2TauTauTreeProducerTauMu import H2TauTauTreeProducerTauMu
-from CMGTools.H2TauTau.proto.analyzers.TauDecayModeWeighter import TauDecayModeWeighter
-from CMGTools.H2TauTau.proto.analyzers.TauFakeRateWeighter import TauFakeRateWeighter
-from CMGTools.H2TauTau.proto.analyzers.LeptonWeighter import LeptonWeighter
-from CMGTools.H2TauTau.proto.analyzers.SVfitProducer import SVfitProducer
+from CMGTools.RootTools.utils.splitFactor import splitFactor
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+from CMGTools.RootTools.samples.samples_13TeV_74X import TT_pow, DYJetsToLL_M50, WJetsToLNu, WJetsToLNu_HT100to200, WJetsToLNu_HT200to400, WJetsToLNu_HT400to600, WJetsToLNu_HT600toInf
+from CMGTools.H2TauTau.proto.samples.spring15.triggers_tauMu  import mc_triggers as mc_triggers_mt
 
-# from CMGTools.H2TauTau.proto.samples.spring15.connector import httConnector
-
-# common configuration and sequence
-from CMGTools.H2TauTau.htt_ntuple_base_cff import commonSequence, genAna, dyJetsFakeAna, puFileData, puFileMC, eventSelector
-
+from CMGTools.H2TauTau.htt_ntuple_base_cff import puFileData, puFileMC, eventSelector
 
 # Get all heppy options; set via "-o production" or "-o production=True"
 
 # production = True run on batch, production = False (or unset) run locally
 production = getHeppyOption('production')
 production = True
-
-# mu-tau specific configuration settings
-
-shift = None
-syncntuple = True
-computeSVfit = False
 pick_events = False
-
-
-# When ready, include weights from CMGTools.H2TauTau.proto.weights.weighttable
-
-# mc_tauEffWeight_mc = 'effTau_muTau_MC_2012ABCDSummer13'
-# mc_muEffWeight_mc = 'effMu_muTau_MC_2012ABCD'
-# mc_tauEffWeight = 'effTau_muTau_Data_2012ABCDSummer13'
-# mc_muEffWeight = 'effMu_muTau_Data_2012ABCDSummer13'
-
-mc_tauEffWeight_mc = None
-mc_muEffWeight_mc = None
-mc_tauEffWeight = None
-mc_muEffWeight = None
-
-dyJetsFakeAna.channel = 'mt'
-
-# Define mu-tau specific modules
-
-tauMuAna = cfg.Analyzer(
-    TauMuAnalyzer,
-    name='TauMuAnalyzer',
-    pt1=18,
-    eta1=2.1,
-    iso1=0.1,
-    looseiso1=9999.,
-    pt2=20,
-    eta2=2.3,
-    iso2=1.5,
-    looseiso2=9999.,
-    m_min=10,
-    m_max=99999,
-    dR_min=0.5,
-    from_single_objects=True,
-    verbose=False
-)
-
-tauDecayModeWeighter = cfg.Analyzer(
-    TauDecayModeWeighter,
-    name='TauDecayModeWeighter',
-    legs=['leg2']
-)
-
-tauFakeRateWeighter = cfg.Analyzer(
-    TauFakeRateWeighter,
-    name='TauFakeRateWeighter'
-)
-
-tauWeighter = cfg.Analyzer(
-    LeptonWeighter,
-    name='LeptonWeighter_tau',
-    effWeight=None,
-    effWeightMC=None,
-    lepton='leg2',
-    verbose=False,
-    disable=True,
-)
-
-muonWeighter = cfg.Analyzer(
-    LeptonWeighter,
-    name='LeptonWeighter_mu',
-    effWeight=None,
-    effWeightMC=None,
-    lepton='leg1',
-    verbose=False,
-    disable=True,
-    idWeight=None,
-    isoWeight=None
-)
-
-treeProducer = cfg.Analyzer(
-    H2TauTauTreeProducerTauMu,
-    name='H2TauTauTreeProducerTauMu'
-)
-
-syncTreeProducer = cfg.Analyzer(
-    H2TauTauTreeProducerTauMu,
-    name='H2TauTauSyncTreeProducerTauMu',
-    varStyle='sync',
-    # skimFunction='event.isSignal'
-)
-
-svfitProducer = cfg.Analyzer(
-    SVfitProducer,
-    name='SVfitProducer',
-    # integration='VEGAS',
-    integration='MarkovChain',
-    # verbose=True,
-    # order='21', # muon first, tau second
-    l1type='muon',
-    l2type='tau'
-)
-
-###################################################
-### CONNECT SAMPLES TO THEIR ALIASES AND FILES  ###
-###################################################
-# my_connect = httConnector('TAUMU_743_TEST1', 'htautau_group',
-#                           '.*root', 'mt', production=production, 
-#                           splitFactor=1e5)
-# my_connect.connect()
-# MC_list = my_connect.MC_list
-
-from CMGTools.RootTools.utils.splitFactor import splitFactor
-from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
-from CMGTools.RootTools.samples.samples_13TeV_74X import TT_pow, DYJetsToLL_M50, WJetsToLNu, WJetsToLNu_HT100to200, WJetsToLNu_HT200to400, WJetsToLNu_HT400to600, WJetsToLNu_HT600toInf
-from CMGTools.H2TauTau.proto.samples.spring15.triggers_tauMu  import mc_triggers as mc_triggers_mt
 
 creator = ComponentCreator()
 ggh160 = creator.makeMCComponent("GGH160", "/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM", "CMS", ".*root", 1.0)
@@ -180,20 +65,6 @@ selectedComponents = samples + data_list
 # selectedComponents = mc_dict['HiggsGGH125']
 # for c in selectedComponents : c.splitFactor *= 5
 
-###################################################
-###                  SEQUENCE                   ###
-###################################################
-sequence = commonSequence
-sequence.insert(sequence.index(genAna), tauMuAna)
-sequence.append(tauDecayModeWeighter)
-sequence.append(tauFakeRateWeighter)
-sequence.append(tauWeighter)
-sequence.append(muonWeighter)
-if computeSVfit:
-    sequence.append(svfitProducer)
-sequence.append(treeProducer)
-if syncntuple:
-    sequence.append(syncTreeProducer)
 
 ###################################################
 ###             CHERRY PICK EVENTS              ###
@@ -228,8 +99,6 @@ config = cfg.Config(components=selectedComponents,
                     )
 
 printComps(config.components, True)
-
-while len(sequence)<2: sequence.pop()
 
 def modCfgForPlot(config):
     config.components = []
