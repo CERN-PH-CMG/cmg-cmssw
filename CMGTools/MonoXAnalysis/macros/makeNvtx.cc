7dd7ef7a831c4e31565d18f629edcf2b8a6952f3
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-10-09 10:55:28 +0200

    put the signal and data vtx profiles in the same root file

--- CMGTools/MonoXAnalysis/macros/makeNvtx.cc
+++ CMGTools/MonoXAnalysis/macros/makeNvtx.cc
@@ -4,7 +4,7 @@
 #include "TString.h"
 #include <iostream>
 
-void makeNvtx(bool ismc=false) {
+void makeNvtx(bool ismc=false, const char* option="recreate") {
 
   TString treeDir = "/afs/cern.ch/work/e/emanuele/monox/heppy/CMSSW_7_4_12/src/CMGTools/MonoXAnalysis/cfg/TREES_SPRING15_25ns_2LEPSKIM_06Oct2015/";
   TString dataDir = treeDir+"DoubleEG_Run2015D_v3_runs_246908_257599";
@@ -12,25 +12,25 @@ void makeNvtx(bool ismc=false) {
 
 
   if(!ismc) {
-    TFile *dataHFile = TFile::Open("nvtxData.root","recreate");
+    TFile *dataHFile = TFile::Open("nvtx_profile.root",option);
     TFile *fileData = TFile::Open(dataDir+"/treeProducerDarkMatterMonoJet/tree.root");
     fileData->cd();
     TTree *t = (TTree*)fileData->Get("tree");
     std::cout << "tree entries for data " << t->GetEntries() << std::endl;
-    TH1F *nvtxProfile = new TH1F("nvtx","",40,0,40);
-    t->Project("nvtx","nVert");
+    TH1F *nvtxProfile = new TH1F("nvtx_data","",40,0,40);
+    t->Project("nvtx_data","nVert");
   
     dataHFile->cd();
     nvtxProfile->Write();
     dataHFile->Close();
   } else {
-    TFile *mcHFile = TFile::Open("nvtxMC.root","recreate");
+    TFile *mcHFile = TFile::Open("nvtx_profile.root",option);
     TFile *fileMC = TFile::Open(mcDir+"/treeProducerDarkMatterMonoJet/tree.root");
     fileMC->cd();
     TTree *t = (TTree*)fileMC->Get("tree");
     std::cout << "tree entries for MC " << t->GetEntries() << std::endl;
-    TH1F *nvtxProfile = new TH1F("nvtx","",40,0,40);  
-    t->Project("nvtx","nVert");
+    TH1F *nvtxProfile = new TH1F("nvtx_signal","",40,0,40);  
+    t->Project("nvtx_signal","nVert");
 
     mcHFile->cd();  
     nvtxProfile->Write();
@@ -40,6 +40,6 @@ void makeNvtx(bool ismc=false) {
 }
 
 void doAll() {
-  makeNvtx(true);
-  makeNvtx(false);
+  makeNvtx(true,"recreate");
+  makeNvtx(false,"update");
 }
