0c47bcbbcffc1f95a242d60ad0bb8ca56bbc03cd
Andrea Stacchiotti <andrea.stacchiotti@cern.ch>
2015-10-19 21:07:39 +0200

    Fix JobOutputs folder name when varying rap2 bin recoil systs

--- CMGTools/WMass/analysis/launch_analysis.py
+++ CMGTools/WMass/analysis/launch_analysis.py
@@ -52,7 +52,7 @@ WlikeCharge = 1; # Charge of the Wlike (+1,-1)
 
 ### RECOIL
 useRecoilCorr = 2; # 0=none, 1=yes, 2=PDFw3gaus
-RecoilCorrVarDiagoParU1orU2fromDATAorMC = 0; # SYST VARIATIONS: 0=NONE, RAPBIN 1 (1= U1 DATA p1, 2= U1 DATA p2, 3= U2 DATA, 4= U1 MC p1, 5= U1 MC p1, 6= U1 MC p1) RAPBIN 2 (7= U1 DATA p1, 8= U1 DATA p2, 9= U2 DATA, 10= U1 MC p1, 11= U1 MC p1, 12= U1 MC p1)
+RecoilCorrVarDiagoParU1orU2fromDATAorMC = 0; # SYST VARIATIONS: 0=NONE, RAPBIN 1 (1= U1 DATA p1, 2= U1 DATA p2, 3= U2 DATA, 4= U1 MC p1, 5= U1 MC p2, 6= U2 MC) RAPBIN 2 (7= U1 DATA p1, 8= U1 DATA p2, 9= U2 DATA, 10= U1 MC p1, 11= U1 MC p2, 12= U2 MC)
 RecoilCorrVarDiagoParSigmas = 0; # Number of sigmas for recoil syst
 correctToMadgraph = 0; # 0: uses DATA as target --- 1: uses Madgraph as target (also needed for madNoCorr)
 
@@ -246,22 +246,27 @@ if(int(useRecoilCorr)>0):
   outfolder_name+="_RecoilCorr"+str(useRecoilCorr);
   if(int(correctToMadgraph)):
     outfolder_name+="_toMad";
-  if  (int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==1):
-    outfolder_name+="_U1Datap1";
-  elif(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==2):
-    outfolder_name+="_U1Datap2";
-  elif(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==3):
-    outfolder_name+="_U2Data";
-  elif(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==4):
-    outfolder_name+="_U1MCp1";
-  elif(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==5):
-    outfolder_name+="_U1MCp2";
-  elif(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)==6):
-    outfolder_name+="_U2MC";
-  if  (int(RecoilCorrVarDiagoParSigmas)!=0):
-    if(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)<7): outfolder_name+="_Rap1";
-    else: outfolder_name+="_Rap2";
-    outfolder_name+="_RecCorrNSigma_"+str(RecoilCorrVarDiagoParSigmas)
+  if(int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)>0):
+    RecoilCorrVarDiagoParBlock = int(RecoilCorrVarDiagoParU1orU2fromDATAorMC)
+    if(int(RecoilCorrVarDiagoParBlock)<7):
+      outfolder_name+="_Rap1";
+    else:
+      RecoilCorrVarDiagoParBlock = RecoilCorrVarDiagoParBlock - 6
+      outfolder_name+="_Rap2";
+    if  (int(RecoilCorrVarDiagoParBlock)==1):
+      outfolder_name+="_U1Datap1";
+    elif(int(RecoilCorrVarDiagoParBlock)==2):
+      outfolder_name+="_U1Datap2";
+    elif(int(RecoilCorrVarDiagoParBlock)==3):
+      outfolder_name+="_U2Data";
+    elif(int(RecoilCorrVarDiagoParBlock)==4):
+      outfolder_name+="_U1MCp1";
+    elif(int(RecoilCorrVarDiagoParBlock)==5):
+      outfolder_name+="_U1MCp2";
+    elif(int(RecoilCorrVarDiagoParBlock)==6):
+      outfolder_name+="_U2MC";
+    if  (int(RecoilCorrVarDiagoParSigmas)!=0):
+      outfolder_name+="_RecCorrNSigma_"+str(RecoilCorrVarDiagoParSigmas)
 
 if(int(useEffSF)==1): outfolder_name+="_EffSFCorr";
 if(int(useEffSF)>=2): outfolder_name+="_EffHeinerSFCorr";
