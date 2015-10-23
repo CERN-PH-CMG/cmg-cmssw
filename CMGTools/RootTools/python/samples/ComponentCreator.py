ffa6b69255d89d5f09d246d8b9ad3c2a70d0943a
Giovanni <gpetruc@gmail.com>
2015-10-23 18:06:52 +0200

    ComponentCreator: remember the run range when creating a data component

--- CMGTools/RootTools/python/samples/ComponentCreator.py
+++ CMGTools/RootTools/python/samples/ComponentCreator.py
@@ -167,6 +167,7 @@ class ComponentCreator(object):
         component.vetoTriggers = vetoTriggers
         component.dataset_entries = self.getPrimaryDatasetEntries(dataset,user,pattern)
         component.dataset = dataset
+        component.run_range = run_range
         return component
 
     def getFiles(self, dataset, user, pattern, useAAA=False, run_range=None, json=None):
