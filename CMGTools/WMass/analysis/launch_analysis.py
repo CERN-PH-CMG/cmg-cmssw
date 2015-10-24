324cfbc0ce8b518afddfdb69adebb234beab9aab
Andrea Stacchiotti <andrea.stacchiotti@cern.ch>
2015-10-24 18:18:20 +0200

    Improved bjobs parsing

--- CMGTools/WMass/analysis/launch_analysis.py
+++ CMGTools/WMass/analysis/launch_analysis.py
@@ -3,10 +3,8 @@
 import string, os, shutil, sys, subprocess, ROOT
 
 def batch_job_is_running(jobname,chunk):
-  job_running = os.popen("bjobs -w | grep "+jobname).read()
-  job_running = job_running.replace("[","_")
-  job_running = job_running.replace("]","_")
-  jobname = jobname+"_"+chunk+"_"
+  job_running = subprocess.Popen("bjobs -w", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
+  jobname = jobname+"["+chunk+"]"
   if jobname in job_running:
     return True
   else: 
@@ -54,7 +52,7 @@ WlikeCharge = 1; # Charge of the Wlike (+1,-1)
 useRecoilCorr = 2; # 0=none, 1=yes, 2=PDFw3gaus
 RecoilCorrVarDiagoParU1orU2fromDATAorMC = 0; # SYST VARIATIONS: 0=NONE, RAPBIN 1 (1= U1 DATA p1, 2= U1 DATA p2, 3= U2 DATA, 4= U1 MC p1, 5= U1 MC p2, 6= U2 MC) RAPBIN 2 (7= U1 DATA p1, 8= U1 DATA p2, 9= U2 DATA, 10= U1 MC p1, 11= U1 MC p2, 12= U2 MC)
 RecoilCorrVarDiagoParSigmas = 0; # Number of sigmas for recoil syst
-correctToMadgraph = 0; # 0: uses DATA as target --- 1: uses Madgraph as target (also needed for madNoCorr)
+correctToMadgraph = 0; # 0: uses DATA as target -- 1: uses Madgraph as target (also needed to write recoil closure plots)
 
 usePhiMETCorr = 0; # 0=none, 1=yes
 
