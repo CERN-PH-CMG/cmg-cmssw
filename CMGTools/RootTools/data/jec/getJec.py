c61229f477378631f29e05541905bb4088e66820
Henning Kirschenmann <henning.kirschenmann@desy.de>
2015-10-28 10:33:50 +0100

    update getJEC script to work with new global tags (condDBV2)

--- CMGTools/RootTools/data/jec/getJec.py
+++ CMGTools/RootTools/data/jec/getJec.py
@@ -2,8 +2,15 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("jectxt")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-# define your favorite global tag
-process.GlobalTag.globaltag = 'PHYS14_25_V2::All'  
+
+# define your favorite global tag (OLD GTs)
+#process.GlobalTag.globaltag = 'PHYS14_25_V2::All'  
+# define your favorite global tag (newer GTs in condDBv2)
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
+process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = '74X_mcRun2_asymptotic_v2')
+process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_CONDITIONS'
+process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
+
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.source = cms.Source("EmptySource")
 process.read = cms.EDAnalyzer('JetCorrectorDBReader',  
