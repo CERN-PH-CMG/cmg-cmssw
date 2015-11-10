3a18a213ed6d4c28bcf9d64a3f6f12412d223e1e
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-11-10 09:21:54 +0100

    cleanup of samples file, add monojet signals

--- CMGTools/MonoXAnalysis/python/samples/samples_monojet_13TeV_74X.py
+++ CMGTools/MonoXAnalysis/python/samples/samples_monojet_13TeV_74X.py
@@ -7,29 +7,25 @@ kreator = ComponentCreator()
 
 ### common MC samples
 from CMGTools.RootTools.samples.samples_13TeV_RunIISpring15MiniAODv2 import *
+### DM MC samples
+from CMGTools.MonoXAnalysis.samples.samples_monojet_13TeV_74X_signals import *
 
-### additional MC samples
+### --- mc ---
 
-### 25 ns
+# --- 25 ns background samples ---
+DYJetsToNuNu_MJ = [ YJetsToNuNu_M50, ZJetsToNuNuHT ]
+VJets_MJ        = [ WJetsToLNuHT, WJetsToLNu ] + [ DYJetsM50HT, DYJetsToLL_M50, DYJetsToLL_M10to50 ]
+Top_MJ          = [ TTJets_LO, TToLeptons_tch_amcatnlo, TToLeptons_tch_amcatnlo_ext, TBar_tWch, T_tWch ]
+DiBosons_MJ     = [ WW, WZ, ZZ ]
 
-### 50 ns
-
-###
-
-## === 741 RelVals ===
-ADD_MJ = kreator.makeMCComponent("ADD_MJ", "/RelValADDMonoJet_d3MD3_13/CMSSW_7_4_1-MCRUN2_74_V9_gensim_740pre7-v1/MINIAODSIM", "CMS", ".*root")
-TTLep = kreator.makeMCComponent("TTLep", "/RelValTTbarLepton_13/CMSSW_7_4_1-MCRUN2_74_V9_gensim_740pre7-v1/MINIAODSIM", "CMS", ".*root")
-TTbar = kreator.makeMCComponent("TTbar", "/RelValTTbar_13/CMSSW_7_4_1-MCRUN2_74_V9_gensim71X-v1/MINIAODSIM", "CMS", ".*root")
-RSGravGaGa = kreator.makeMCComponent("RSGravGaGa", "/RelValRSGravitonToGaGa_13TeV/CMSSW_7_4_1-MCRUN2_74_V9_gensim71X-v1/MINIAODSIM", "CMS", ".*root")
-
-RelVals741 = [ ADD_MJ, TTLep, TTbar, RSGravGaGa ] 
+mcSamples_monojet_Asymptotic25ns = DYJetsToNuNu_MJ + VJets_MJ + Top_MJ + DiBosons_MJ + QCDHT
+mcSamples_monojet_Asymptotic25ns_signals = DM_Scalars + DM_Pseudoscalars + DM_Vectors + DM_Axials
+mcSamples_monojet = mcSamples_monojet_Asymptotic25ns 
 
 
+### --- 25 ns signal samples ---
+# Scalar Mediator
 
-#-----------DATA--------------- 
-from CMGTools.RootTools.samples.samples_13TeV_DATA2015 import *
-
-json = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258750_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
 
 ### ---- private samples ----
 privEGamma2015Afiles = [ f.strip() for f in open("%s/src/CMGTools/MonoXAnalysis/python/samples/privEGamma_2015A_MINIAOD.txt"  % os.environ['CMSSW_BASE'], "r") ]
@@ -47,16 +43,6 @@ privDataSamples_0T = [ privEGamma2015A_0T, privDoubleEG2015A_0T, privHLTPhysics2
 
 ### ----------------------------- summary ----------------------------------------     
 
-### --- data ---
-dataSamples = dataSamples_Run2015D_v4 + dataSamples_Run2015B_05Oct + dataSamples_Run2015D_05Oct
-
-### --- mc ---
-#mcSamples_monojet_Asymptotic25ns = TTs + SingleTop + [WJetsToLNu, DYJetsToLL_M50, DYJetsToLL_M10to50 ] + ZJetsToNuNuHT + DYJetsM50HT + WJetsToLNuHT + GJetsHT + QCDHT + DiBosons
-mcSamples_monojet_Asymptotic25ns = [ TTJets_LO ] + [WJetsToLNu, DYJetsToLL_M50, DYJetsToLL_M10to50] + ZJetsToNuNuHT + DYJetsM50HT + WJetsToLNuHT + DiBosons
-mcSamples_monojet = mcSamples_monojet_Asymptotic25ns 
-# 50ns not in miniAOD v2
-#mcSamples_monojet_Asymptotic50ns = [ TTJets_50ns, TTJets_LO_50ns, DYJetsToLL_M50_50ns, WJetsToLNu_50ns ] + QCDPt_50ns
-#mcSamples_monojet = RelVals740 + RelVals741 + mcSamples_monojet_Asymptotic25ns + mcSamples_monojet_Asymptotic50ns
 
 
 from CMGTools.TTHAnalysis.setup.Efficiencies import *
@@ -70,10 +56,10 @@ for comp in mcSamples:
     comp.puFileMC=dataDir+"/puProfile_Summer12_53X.root"
     comp.puFileData=dataDir+"/puProfile_Data12.root"
     comp.efficiency = eff2012
-for comp in dataSamples:
-    comp.splitFactor = 1000
-    comp.isMC = False
-    comp.isData = True
+# for comp in dataSamples:
+#     comp.splitFactor = 1000
+#     comp.isMC = False
+#     comp.isData = True
 
 
 if __name__ == "__main__":
