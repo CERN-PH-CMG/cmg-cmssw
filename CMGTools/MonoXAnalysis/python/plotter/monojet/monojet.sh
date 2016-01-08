65a8b600df14c6125690d166f28b664e624e185a
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-01-08 15:47:54 +0100

    finalise single and double electron control regions

--- CMGTools/MonoXAnalysis/python/plotter/monojet/monojet.sh
+++ CMGTools/MonoXAnalysis/python/plotter/monojet/monojet.sh
@@ -9,57 +9,66 @@ else
     BASEDIR="TREES_25ns_MET200SKIM_1DEC2015"
 fi
 
+WORKDIR="monojet"
+
 if [[ "$HOSTNAME" == "cmsphys06" ]]; then
     T="/data1/emanuele/monox/${BASEDIR}";
     #T="/afs/cern.ch/work/e/emanuele/Trees/"
-    J=6;
+    J=8;
 else
     T="/cmshome/dimarcoe/${BASEDIR}";
-    J=6;
+    J=3;
 fi
-
-COREOPT="-P $T --s2v -j $J -l 2.215 -W vtxWeight "
-COREY="mcAnalysis.py ${COREOPT} -G  "
-COREP="mcPlots.py  ${COREOPT} -f --poisson "
-FEV=" -F mjvars/t \"$T/friends/evVarFriend_{cname}.root\" "
-
-ROOT="plots/Summer15/v1.0/$WHAT"
 
 MCA=""
-if [ "$WHAT" == "zee" ] || [ "$WHAT" == "zeeI" ] ; then
-    MCA="mca-74X.txt "
-elif [ "$WHAT" == "wmnI" ] || [ "$WHAT" == "wmn" ] || [ "$WHAT" == "zmmI" ] || [ "$WHAT" == "zmm" ] ; then
-    MCA="mca-74X-Vm.txt "
+if [ "$WHAT" == "wen" ] || [ "$WHAT" == "zee" ] ; then
+    MCA="${WORKDIR}/mca-74X-Ve.txt "
+elif [ "$WHAT" == "wmn" ] || [ "$WHAT" == "zmm" ] ; then
+    MCA="${WORKDIR}/mca-74X-Vm.txt "
 else
-    MCA="mca-74X-analysis.txt "
+    MCA="${WORKDIR}/mca-74X-analysis.txt "
 fi
 
-RUNYSR="${COREY} ${MCA} monojet_twiki.txt "
-RUNY2M="${COREY} ${MCA} zmumu_twiki.txt "
-RUNY2E="${COREY} ${MCA} zee_twiki.txt "
-RUNY1M="${COREY} ${MCA} wmunu_twiki.txt "
-RUNY1G="${COREY} ${MCA} gjets_twiki.txt "
+ROOT="plots/Summer15/v1.0/$WHAT"
+COREOPT="-P $T --s2v -j $J -l 2.215 -W vtxWeight "
+COREY="mcAnalysis.py ${MCA} ${COREOPT} -G  "
+COREP="mcPlots.py ${MCA} ${COREOPT} -f --poisson --pdir ${ROOT} --showRatio --maxRatioRange 0 2 "
+FEV=" -F mjvars/t \"$T/friends/evVarFriend_{cname}.root\" "
 
-PLOT="${COREP} ${MCA} "
-PLOTSR="${PLOT} sr/monojet.txt sr/monojet-plots.txt --noStackSig --showSigShape "
+RUNYSR="${COREY} ${WORKDIR}/monojet_twiki.txt "
+RUNY2M="${COREY} ${WORKDIR}/zmumu_twiki.txt "
+RUNY2E="${COREY} ${WORKDIR}/zee_twiki.txt "
+RUNY1M="${COREY} ${WORKDIR}/wmunu_twiki.txt "
+RUNY1E="${COREY} ${WORKDIR}/wenu_twiki.txt "
+RUNY1G="${COREY} ${WORKDIR}/gjets_twiki.txt "
+
+PLOTSR="${COREP} ${WORKDIR}/monojet_twiki.txt ${WORKDIR}/monojet-plots.txt --noStackSig --showSigShape "
+PLOT2M="${COREP} ${WORKDIR}/zmumu_twiki.txt ${WORKDIR}/zmumu_plots.txt "
+PLOT2E="${COREP} ${WORKDIR}/zee_twiki.txt ${WORKDIR}/zee_plots.txt "
+PLOT1M="${COREP} ${WORKDIR}/wmunu_twiki.txt ${WORKDIR}/wmunu_plots.txt "
+PLOT1E="${COREP} ${WORKDIR}/wenu_twiki.txt ${WORKDIR}/wenu_plots.txt "
 
 case $WHAT in
 sr)
         SF=" "
         echo "python ${RUNYSR} $FEV $SF "
-        echo "python ${PLOTSR}  $FEV $SF --pdir ${ROOT} --showRatio --maxRatioRange 0 2 "
+        echo "python ${PLOTSR} $FEV $SF "
 ;;
 zmm)
-        echo "python ${RUNY2M} $FEV --sp DYJetsHT --xp WJets --xp DYJets "
-        echo "python ${PLOT} zmumu_twiki.txt zmumu_plots.txt $FEV --sp DYJetsHT --xp WJets --xp DYJets --pdir ${ROOT} --showRatio --maxRatioRange 0 2 "
+        echo "python ${RUNY2M} $FEV --sp DYJetsHT "
+        echo "python ${PLOT2M} $FEV --sp DYJetsHT "
 ;;
 wmn)
-        echo "python ${RUNY1M} $FEV --sp WJetsHT --xp WJets --xp DYJets "
-        echo "python ${PLOT} wmunu_twiki.txt wmunu_plots.txt $FEV --sp WJetsHT --xp WJets --xp DYJets -X recoil -X jet100 -X jetclean -X dphijm --pdir ${ROOT} --showRatio --maxRatioRange 0 2 "
+        echo "python ${RUNY1M} $FEV --sp WJetsHT "
+        echo "python ${PLOT1M} $FEV --sp WJetsHT "
 ;;
 zee)
         echo "python ${RUNY2E} $FEV --sp DYJetsHT "
-        echo "python ${PLOT} zee_twiki.txt zee_plots.txt $FEV --sp DYJetsHT --pdir ${ROOT} --showRatio --maxRatioRange 0 2 "
+        echo "python ${PLOT2E} $FEV --sp DYJetsHT "
+;;
+wen)
+        echo "python ${RUNY1E} $FEV --sp WJetsHT "
+        echo "python ${PLOT1E} $FEV --sp WJetsHT "
 ;;
 # zmmI)
 #         echo "python ${RUNY2M} -X recoil -X jet100 -X jetclean -X dphijm $FEV --sp DYJets "
