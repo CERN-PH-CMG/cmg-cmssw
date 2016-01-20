010c4cc31e07c126d84532a4636411f7aa45bda0
Michail <bachtis@cern.ch>
2016-01-20 17:49:27 +0100

    switching to soft drop and puppi for a test

--- CMGTools/VVResonances/cfg/runVV_cfg.py
+++ CMGTools/VVResonances/cfg/runVV_cfg.py
@@ -47,7 +47,7 @@ triggerFlagsAna.triggerBits ={
 
 
 #-------- HOW TO RUN
-test = 4
+test = 1
 if test==1:
     # test a single component, using a single thread.
     selectedComponents = [VBF_RadionToZZ_narrow_4500]
