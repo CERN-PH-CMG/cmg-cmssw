503f0e04b36cccd1040e75e18f47be3ffaa33a10
maria <dalfonso@cern.ch>
2016-01-26 12:41:12 +0100

    add samples with sumET fixes

--- CMGTools/WMass/analysis/launch_analysis.py
+++ CMGTools/WMass/analysis/launch_analysis.py
@@ -11,6 +11,7 @@ useLHAPDF = False
 outfolder_prefix="PREFIX";
 
 ntuple_basepath = "root://eoscms//eos/cms/store/group/phys_smp/Wmass/perrozzi/ntuples/ntuples_2014_05_23_53X/";
+ntuple_basepathFIX = "root://eoscms//eos/cms/store/group/phys_smp/Wmass/perrozzi/ntuples/ntuples_2015_05_24_53X_sumEtFIX/";
 ntuple_basepath_8TeV_ABC = "root://eoscms//eos/cms/store/group/phys_smp/Wmass/perrozzi/ntuples/ntuples_2014_08_19_53X_8TeV/";
 # ntuple_basepath = "root://eoscms//eos/cms/store/group/phys_smp/Wmass/perrozzi/ntuples/ntuples_2013_09_14/";
 # ntuple_basepath = "root://eoscms//eos/cms/store/group/phys_smp/Wmass/perrozzi/ntuples/ntuples_2013_10_15/";
@@ -81,7 +82,7 @@ etaMuonNSteps = "1"; # 5 <-- lenght of the etaMaxMuons
 etaMaxMuons = "0.9"; # 0.6, 0.8, 1.2, 1.6, 2.1
 
 runWanalysis = 0;
-runZanalysis = 0;
+runZanalysis = 1;
 controlplots = 1;
 noLSFJobOutput = 0; # 1: Puts all the batch logs in a single file
 recreateSubPrograms = 0; # 1: Recompiles run?analysis.o and remakes run?analysis.sh
@@ -385,17 +386,17 @@ fWana_str = [
 
 fZana_str = [
 #  ntuple_basepath_8TeV_ABC+"DATA_Run2012ABCD/ZTreeProducer_tree.root", # this is the 8TeV data contain also the tkmetABC
-  ntuple_basepath+"DATA/ZTreeProducer_tree_RecoSkimmed.root",
+  ntuple_basepathFIX+"DATA/ZTreeProducer_tree_RecoSkimmed.root",
   ntuple_basepath+"WJetsLL/ZTreeProducer_tree_SignalRecoSkimmed.root",
   ntuple_basepath+"WJetsLL/ZTreeProducer_tree_SignalRecoSkimmed.root",
   ntuple_basepath+"WJetsLL/ZTreeProducer_tree_SignalRecoSkimmed.root",
   ntuple_basepath+"WJetsLL/ZTreeProducer_tree_FakeRecoSkimmed.root",
-  ntuple_basepath+"DYJetsMM/ZTreeProducer_tree_SignalRecoSkimmed.root",
+  ntuple_basepathFIX+"DYMM/ZTreeProducer_tree_SignalRecoSkimmed.root",
   # ntuple_basepath+"DYJetsMM/InclWeights/ZTreeProducer_tree.root",
   # ntuple_basepath+"DYJetsMM/allEvts/ZTreeProducer_tree.root",
 #  ntuple_basepath_8TeV_ABC+"DYJetsLL/ZTreeProducer_tree_tkmetABC.root",  # this is the 8TeV DYJetsLL contains also the tkmetABC
-  ntuple_basepath+"DYJetsLL/ZTreeProducer_tree_SignalRecoSkimmed.root",
-  ntuple_basepath+"DYJetsLL/ZTreeProducer_tree_FakeRecoSkimmed.root",
+  ntuple_basepathFIX+"DYLL/ZTreeProducer_tree_SignalRecoSkimmed.root",
+  ntuple_basepathFIX+"DYLL/ZTreeProducer_tree_FakeRecoSkimmed.root",
   ntuple_basepath+"TTJets/ZTreeProducer_tree.root",
   ntuple_basepath+"VVJets/ZZ/ZTreeProducer_tree.root",
   ntuple_basepath+"VVJets/WW/ZTreeProducer_tree.root",
