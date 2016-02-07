ebfb973d0a4bb55acb044bdb15ed040f7b0d5237
maria <dalfonso@cern.ch>
2016-02-07 23:38:47 +0100

    finer bins

--- CMGTools/WMass/analysis/AnalysisCode/Zanalysis.C
+++ CMGTools/WMass/analysis/AnalysisCode/Zanalysis.C
@@ -1277,8 +1277,8 @@ void Zanalysis::Loop(int chunk, int Entry_ini, int Entry_fin, int IS_MC_CLOSURE_
 			      // Boson polarization
                               common_stuff::plot2D(Form("hWlike%s_phi_vs_costh_CS_8_JetCut_pdf%d-%d%s%s_eta%s_%d",WCharge_str.Data(),WMass::PDF_sets<0?generated_PDF_set:WMass::PDF_sets,h,RecoilVar_str.Data(),KalmanVars_str.Data(),eta_str.Data(),jZmass_MeV),
 						   costh_CS,TMath::Abs(phi_CS), weight,
-						   h_2d, 12,-1,1,
-						   12,0,TMath::Pi() );
+						   h_2d, 20,-1,1,
+						   8,0,TMath::Pi() );
 
                               common_stuff::plot1D(Form("hWlike%s_costh_CS_8_JetCut_pdf%d-%d%s%s_eta%s_%d",WCharge_str.Data(),WMass::PDF_sets<0?generated_PDF_set:WMass::PDF_sets,h,RecoilVar_str.Data(),KalmanVars_str.Data(),eta_str.Data(),jZmass_MeV),
 						   costh_CS, weight,
@@ -1286,7 +1286,7 @@ void Zanalysis::Loop(int chunk, int Entry_ini, int Entry_fin, int IS_MC_CLOSURE_
 
                               common_stuff::plot1D(Form("hWlike%s_phi_CS_8_JetCut_pdf%d-%d%s%s_eta%s_%d",WCharge_str.Data(),WMass::PDF_sets<0?generated_PDF_set:WMass::PDF_sets,h,RecoilVar_str.Data(),KalmanVars_str.Data(),eta_str.Data(),jZmass_MeV),
 						   TMath::Abs(phi_CS), weight,
-						   h_1d, 12, 0, TMath::Pi() );
+						   h_1d, 16, 0, TMath::Pi() );
 
 			      if(controlplots) {
 				if( TMath::Abs(ZcorrCentral.Rapidity())<2.1 && ZcorrCentral.Pt()<1000 ){
