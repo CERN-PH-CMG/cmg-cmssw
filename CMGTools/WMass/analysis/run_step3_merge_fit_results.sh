222a6dbec3c6fae8c595b0324cc16d48629e2cf2
Andrea Stacchiotti <andrea.stacchiotti@cern.ch>
2016-02-28 01:33:06 +0100

    run_step*.sh: Sed only the necessary config files Now multiple steps on different systs can be run in parallel

--- CMGTools/WMass/analysis/run_step3_merge_fit_results.sh
+++ CMGTools/WMass/analysis/run_step3_merge_fit_results.sh
@@ -4,14 +4,18 @@
 outfolder_prefix="PREFIX"
 WlikeCharge=1 # 1, -1
 
-# RUN Z ANALYSIS ONLY
-sed -i "s/outfolder_prefix =.*/outfolder_prefix = \"${outfolder_prefix}\"/g;\
-        s/runZanalysis =.*/runZanalysis = 0/g;\
-        s/mergeSigEWKbkg =.*/mergeSigEWKbkg = 0/g;\
-        s/runPrepareDataCardsFast =.*/runPrepareDataCardsFast = 0/g;\
-        s/runClosureTestLikeLihoodRatio =.*/runClosureTestLikeLihoodRatio = 0/g;\
-        s/mergeResults =.*/mergeResults = 1/g;\
-        s/WlikeCharge =.*/WlikeCharge = ${WlikeCharge}  # Charge of the Wlike (+1,-1)/g" configdir/*.py
+sed_and_run()
+{
+        # RUN MERGE RESULTS
+        sed -i "s/outfolder_prefix =.*/outfolder_prefix = \"${outfolder_prefix}\"/g;\
+                s/runZanalysis =.*/runZanalysis = 0/g;\
+                s/mergeSigEWKbkg =.*/mergeSigEWKbkg = 0/g;\
+                s/runPrepareDataCardsFast =.*/runPrepareDataCardsFast = 0/g;\
+                s/runClosureTestLikeLihoodRatio =.*/runClosureTestLikeLihoodRatio = 0/g;\
+                s/mergeResults =.*/mergeResults = 1/g;\
+                s/WlikeCharge =.*/WlikeCharge = ${WlikeCharge}  # Charge of the Wlike (+1,-1)/g" configdir/${1}.py
+    python launch_analysis.py ${1}
+}
 
 # SET ENVIRONMENT VARIABLES BEFORE TO LAUNCH. CAVEAT: IT ASSUMES BASH
 eval `scramv1 runtime -sh`
@@ -20,53 +24,53 @@ source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.24/x86_64-slc6-gcc47-opt/root/
 # COMMENT UNWANTED SYSTEMATICS
 
 # CENTRAL
-python launch_analysis.py config_nominal
+sed_and_run config_nominal
 
 # SYST PDF NNPDF2.3
-python launch_analysis.py config_pdf_nnpdf23
+sed_and_run config_pdf_nnpdf23
 
 # SYST EFFICIENCIES
-python launch_analysis.py config_eff_13
-python launch_analysis.py config_eff_14
-python launch_analysis.py config_eff_15
-python launch_analysis.py config_eff_16
+sed_and_run config_eff_13
+sed_and_run config_eff_14
+sed_and_run config_eff_15
+sed_and_run config_eff_16
 
 # SYST MUON -1 sigma
-python launch_analysis.py config_muscale_minus1
-python launch_analysis.py config_mustat_minus1
+sed_and_run config_muscale_minus1
+sed_and_run config_mustat_minus1
 
 # SYST MUON +1 sigma
-python launch_analysis.py config_muscale_plus1
-python launch_analysis.py config_mustat_plus1
+sed_and_run config_muscale_plus1
+sed_and_run config_mustat_plus1
 
 # SYST ALTERNATIVE RECOIL MODEL
-python launch_analysis.py config_recoilmodel
+sed_and_run config_recoilmodel
 
 # SYST RECOIL STAT +1 sigma
-python launch_analysis.py config_recoilstat_p1_plus1
-python launch_analysis.py config_recoilstat_p2_plus1
-python launch_analysis.py config_recoilstat_p3_plus1
-python launch_analysis.py config_recoilstat_p4_plus1
-python launch_analysis.py config_recoilstat_p5_plus1
-python launch_analysis.py config_recoilstat_p6_plus1
-python launch_analysis.py config_recoilstat_p7_plus1
-python launch_analysis.py config_recoilstat_p8_plus1
-python launch_analysis.py config_recoilstat_p9_plus1
-python launch_analysis.py config_recoilstat_p10_plus1
-python launch_analysis.py config_recoilstat_p11_plus1
-python launch_analysis.py config_recoilstat_p12_plus1
+sed_and_run config_recoilstat_p1_plus1
+sed_and_run config_recoilstat_p2_plus1
+sed_and_run config_recoilstat_p3_plus1
+sed_and_run config_recoilstat_p4_plus1
+sed_and_run config_recoilstat_p5_plus1
+sed_and_run config_recoilstat_p6_plus1
+sed_and_run config_recoilstat_p7_plus1
+sed_and_run config_recoilstat_p8_plus1
+sed_and_run config_recoilstat_p9_plus1
+sed_and_run config_recoilstat_p10_plus1
+sed_and_run config_recoilstat_p11_plus1
+sed_and_run config_recoilstat_p12_plus1
 
 # SYST RECOIL STAT -1 sigma
-python launch_analysis.py config_recoilstat_p1_minus1
-python launch_analysis.py config_recoilstat_p2_minus1
-python launch_analysis.py config_recoilstat_p3_minus1
-python launch_analysis.py config_recoilstat_p4_minus1
-python launch_analysis.py config_recoilstat_p5_minus1
-python launch_analysis.py config_recoilstat_p7_minus1
-python launch_analysis.py config_recoilstat_p8_minus1
-python launch_analysis.py config_recoilstat_p6_minus1
-python launch_analysis.py config_recoilstat_p9_minus1
-python launch_analysis.py config_recoilstat_p10_minus1
-python launch_analysis.py config_recoilstat_p11_minus1
-python launch_analysis.py config_recoilstat_p12_minus1
+sed_and_run config_recoilstat_p1_minus1
+sed_and_run config_recoilstat_p2_minus1
+sed_and_run config_recoilstat_p3_minus1
+sed_and_run config_recoilstat_p4_minus1
+sed_and_run config_recoilstat_p5_minus1
+sed_and_run config_recoilstat_p7_minus1
+sed_and_run config_recoilstat_p8_minus1
+sed_and_run config_recoilstat_p6_minus1
+sed_and_run config_recoilstat_p9_minus1
+sed_and_run config_recoilstat_p10_minus1
+sed_and_run config_recoilstat_p11_minus1
+sed_and_run config_recoilstat_p12_minus1
 
