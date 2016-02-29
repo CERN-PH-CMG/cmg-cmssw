89e036b21179e543f8eacbd6754f3bde13122e5d
Andrea Stacchiotti <andrea.stacchiotti@cern.ch>
2016-02-29 15:22:28 +0100

    RecoilCorrector.*: Remove a lot of really old things

--- CMGTools/WMass/analysis/AnalysisCode/RecoilCorrector.h
+++ CMGTools/WMass/analysis/AnalysisCode/RecoilCorrector.h
@@ -40,24 +40,15 @@ using namespace std;
 
 class RecoilCorrector
 {
-  
+
 public:
   RecoilCorrector(bool doKeys, string iNameZDat, string iNameZ_key, TString model_name = "fitresult_Add", TString fNonClosure_name = "");
-  RecoilCorrector(string iNameZDat1, string iPrefix, int iSeed=0xDEADBEEF);
-  ~RecoilCorrector();
-  void CorrectAll(double &met, double &metphi, double bosonPt, double bosonPhi, double iLepPt, double iLepPhi,double &iU1,double &iU2,double iFluc,double iScale=0,int njet=0);
-  void Correct(double &pfmet, double &pfmetphi, double &trkmet, double &trkmetphi, 
-  double bosonPt, double bosonPhi, double iLepPt, double iLepPhi,double iFluc    ,double iScale=0,int njet=0);
-  void CorrectType1(double &pfmet, double &pfmetphi,double bosonPt,double bosonPhi,double iLepPt,double iLepPhi,double &iU1,double &iU2,double iFlucU2,double iFlucU1,double iScale=0,int njet=0);
-  void CorrectType2(double &pfmet, double &pfmetphi,double bosonPt,double bosonPhi,double iLepPt,double iLepPhi,double &iU1,double &iU2,double iFlucU2,double iFlucU1,double iScale=0,int njet=0,bool doSingleGauss=false);
   void reset(int RecoilCorrParMaxU1, int RecoilCorrParMaxU2, int rapBinCorr);
   void CorrectMET3gaus(double &pfmet, double &pfmetphi,double bosonPt,double bosonPhi,double sumLepPt,double sumLepPhi,double &iU1,double &iU2,int RecoilCorrVarDiagoParU1orU2fromDATAorMC,int RecoilCorrVarDiagoParN,int RecoilCorrVarDiagoParSigmas,int njet=0, int recoilCorrSigmas=1);
   void CorrectU1U2(double &pfu1, double &pfu2, double &trku1, double &trku2, 
-  double bosonPt, double bosonPhi, double iLepPt, double iLepPhi,double iFluc,double iScale=0,int njet=0);
+    double bosonPt, double bosonPhi, double iLepPt, double iLepPhi,double iFluc,double iScale=0,int njet=0);
   void addDataFile(std::string iNameDat, std::string iNameKeyDat, TString model_name = "fitresult_Add");
   void addMCFile  (std::string iNameMC, std::string iNameKeyMC, TString model_name = "fitresult_Add");
-  double NonClosure_weight(double iMet,double iMPhi,double bosonPt,double bosonPhi,double iGenRap, double iLepPt,double iLepPhi);
-  double NonClosure_scale(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,double iGenRap, double iLepPt,double iLepPhi);
 
 protected:
   enum Recoil { 
@@ -79,9 +70,6 @@ protected:
     TKS2U2
   };
 
-  // void readRecoil(std::vector<TF1*> &iU1Fit,std::vector<TF1*> &iU1MRMSFit,std::vector<TF1*> &iU1RMS1Fit,std::vector<TF1*> &iU1RMS2Fit,
-  // std::vector<TF1*> &iU2Fit,std::vector<TF1*> &iU2MRMSFit,std::vector<TF1*> &iU2RMS1Fit,std::vector<TF1*> &iU2RMS2Fit,
-  // std::string iFName,std::string iPrefix); 
   void readRecoil(
     std::vector<TF1*> &iU1Fit,std::vector<TF1*> &iU1MRMSFit,
     std::vector<TF1*> &iU1RMS1Fit,std::vector<TF1*> &iU1RMS2Fit,std::vector<TF1*> &iU1RMS3Fit,
@@ -89,71 +77,8 @@ protected:
     std::vector<TF1*> &iU2Fit,std::vector<TF1*> &iU2MRMSFit,
     std::vector<TF1*> &iU2RMS1Fit,std::vector<TF1*> &iU2RMS2Fit,std::vector<TF1*> &iU2RMS3Fit,
     std::vector<TF1*> &iU2FracFit,std::vector<TF1*> &iU2Mean1Fit, std::vector<TF1*> &iU2Mean2Fit,//std::vector<TF1*> &iU2Sig3Fit,
-    std::string iFName,std::string iFKeyName, std::string iPrefix, int mytype, TString model_name = "fitresult_Add");
-                     
-  void readCorr(std::string iName,//int iType=2,
-  std::vector<TF1*> &iF1U1U2Corr  ,std::vector<TF1*> &iF2U1U2Corr,std::vector<TF1*> &iF1F2U1Corr,std::vector<TF1*> &iF1F2U2Corr,
-  std::vector<TF1*> &iF1F2U1U2Corr,std::vector<TF1*> &iF1F2U2U1Corr,int iType=2);
-
-  void metDistribution(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,
-    double iLepPt,double iLepPhi,TRandom3 *iRand,
-    TF1 *iU1RZFit,
-    TF1 *iU1MSZFit,
-    TF1 *iU1S1ZFit,
-    TF1 *iU1S2ZFit,
-    TF1 *iU2MSZFit,
-    TF1 *iU2S1ZFit,
-    TF1 *iU2S2ZFit,
-    TF1 *iU1U2Corr,
-    double &iU1,double &iU2,double iFluc=0,double iScale=0);
-
-  void metDistribution(double &iPFMet,double &iPFMPhi,double &iTKMet,double &iTKMPhi,
-    double bosonPt,double bosonPhi,
-    double iLepPt,double iLepPhi,TRandom3 *iRand,
-    TF1 *iU1RZPFFit,  TF1 *iU1RZTKFit,
-    TF1 *iU1MSZPFFit, TF1 *iU1MSZTKFit,
-    TF1 *iU1S1ZPFFit, TF1 *iU1S1ZTKFit,
-    TF1 *iU1S2ZPFFit, TF1 *iU1S2ZTKFit,
-    TF1 *iU2MSZPFFit, TF1 *iU2MSZTKFit,
-    TF1 *iU2S1ZPFFit, TF1 *iU2S1ZTKFit,
-    TF1 *iU2S2ZPFFit, TF1 *iU2S2ZTKFit,
-    TF1 *iPFU1U2Corr, TF1 *iTKU1U2Corr,
-    TF1 *iPFTKU1Corr, TF1 *iPFTKU2Corr,
-    TF1 *iPFTKU1MCorr,TF1 *iPFTKU2MCorr,
-    double &iU1,double &iU2,double iFluc=0,double iScale=0);
-
-  void metDistributionType1(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,
-    double iLepPt,double iLepPhi,TRandom3 *iRand,
-    TF1 *iU1RZDatFit,  TF1 *iU1RZMCFit,
-    TF1 *iU1MSZDatFit, TF1 *iU1MSZMCFit,
-    TF1 *iU2MSZDatFit, TF1 *iU2MSZMCFit,
-    double &iU1,double &iU2,double iFlucU2=0,double iFlucU1=0,double iScale=0);
-
-  void metDistributionType2(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,
-    double iLepPt,double iLepPhi,
-    TF1 *iU1Default,
-    TF1 *iU1RZDatFit,  TF1 *iU1RZMCFit,
-    TF1 *iU1MSZDatFit, TF1 *iU1MSZMCFit,
-    TF1 *iU1S1ZDatFit, TF1 *iU1S1ZMCFit,
-    TF1 *iU1S2ZDatFit, TF1 *iU1S2ZMCFit,
-    TF1 *iU2MSZDatFit, TF1 *iU2MSZMCFit,
-    TF1 *iU2S1ZDatFit, TF1 *iU2S1ZMCFit,
-    TF1 *iU2S2ZDatFit, TF1 *iU2S2ZMCFit,
-    double &iU1, double &iU2,double iFlucU2=0,double iFlucU1=0,double iScale=0,
-    bool doSingleGauss=false);
-
-  void metDistributionType2CorrU(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,
-    double iLepPt,double iLepPhi,
-    TF1 *iU1Default,
-    TF1 *iU1RZDatFit,  TF1 *iU1RZMCFit,
-    TF1 *iU1MSZDatFit, TF1 *iU1MSZMCFit,
-    TF1 *iU1S1ZDatFit, TF1 *iU1S1ZMCFit,
-    TF1 *iU1S2ZDatFit, TF1 *iU1S2ZMCFit,
-    TF1 *iU2MSZDatFit, TF1 *iU2MSZMCFit,
-    TF1 *iU2S1ZDatFit, TF1 *iU2S1ZMCFit,
-    TF1 *iU2S2ZDatFit, TF1 *iU2S2ZMCFit,
-    double &iU1,double &iU2,double iFlucU2, double iFlucU1, double iScale,
-    bool doSingleGauss);
+    std::string iFName,std::string iFKeyName, std::string iPrefix, int mytype, TString model_name = "fitresult_Add"
+  );
 
   void applyCorrMET3gaus(double &iMet,double &iMPhi,double bosonPt,double bosonPhi,
     double i,double iLepPhi,
@@ -187,28 +112,10 @@ protected:
     int rapbin, int nSigmas, double &pU1, double &pU2
   );
   double calculate(int iMet,double iEPt,double iEPhi,double iWPhi,double iU1,double iU2);
-  double diGausInvGraph(double iPVal,double iFracMC,double iSigma1MC,double iSigma2MC,double iFracDATA,double iSigma1DATA,double iSigma2DATA);
-  double oneGausInvGraph(double iPVal,double iFracMC,double iSigma1MC,double iSigma2MC,double iFracDATA,double iSigma1DATA,double iSigma2DATA);
-  double getFrac2gauss(double RMS, double sigma1, double sigma2);
-  double getFrac3gauss(double RMS, double f1, double sigma1, double sigma2, double sigma3);
   void runDiago(RooWorkspace *w,RooFitResult *result, TString fit, RooAbsReal *&pdfUiCdf);
   void makeKeysVec(RooWorkspace *w, TFile *file, TString fit, std::vector<RooAbsReal*> &pdfUiCdfm, bool isU1);
-  double diGausPVal(double iVal, double iFrac,double iSimga1,double iSigma2);
-  double triGausInvGraph(double iPVal, double meanRMSMC, double iMean1MC, double iMean2MC, double iFrac1MC,double iSigma1MC,double iSigma2MC,double iSigma3MC,/**/ double meanRMSDATA, double iMean1DATA, double iMean2DATA,double iFrac1DATA,double iSigma1DATA,double iSigma2DATA,double iSigma3DATA);
   double triGausInvGraphKeys(double iPVal, double Zpt, std::vector<RooAbsReal*> pdfKeyMCcdf, std::vector<RooAbsReal*> pdfKeyDATAcdf, RooWorkspace *wMC, RooWorkspace *wDATA, bool isU1, double max);
-  // double triGausInvGraphPDF(double iPVal, double Zpt, RooAddPdf *pdfMC, RooAddPdf *pdfDATA, RooWorkspace *wMC, RooWorkspace *wDATA);
   double triGausInvGraphPDF(double iPVal, double Zpt, RooAbsReal *pdfMCcdf, RooAbsReal *pdfDATAcdf, RooWorkspace *wMC, RooWorkspace *wDATA, double max);
-  double diGausPInverse(double iPVal,double iFrac,double iSigma1,double iSigma2);
-  double oneGausPInverse(double iPVal,double iFrac,double iSigma1,double iSigma2);
-  double getError(double iVal,TF1 *iZDatFit,Recoil iType);
-  double getError2(double iVal,TF1 *iFit);
-  double getCorError2(double iVal,TF1 *iFit);
-  double mag(double iV0,double iV1,double iV2,double iV3);
-  double correlatedSeed(double iVal, double iCorr1,double iCorr2,double iCorr3,double iSeed0,double iSeed1,double iSeed2,double iSeed3);
-  double deCorrelate   (double iVal, double iCorr1,double iCorr2,double iCorr3,double iSeed0,double iSeed1,double iSeed2,double iSeed3);
-  TF1*   getFunc(bool iMC, Recoil iType);
-  double CorrVal(double iPt,double iVal,Recoil iType);
-  //void   Correct(double &met, double &metphi, double lGenPt, double lGenPhi, double lepPt, double lepPhi,double iFluc,int njet);
   
   static const int rapbins = 2;
   enum filetype { targetMC , ZDATA , ZMC };
