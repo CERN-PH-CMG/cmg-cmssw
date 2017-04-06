cfe661fe62154e273e38292d3cf184e3914482c4
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-04-06 11:53:15 +0200

    Egamma tools to apply the Et-dependent corrections

---
+++ EgammaAnalysis/ElectronTools/interface/ElectronEnergyCalibratorRun2.h
@@ -0,0 +1,41 @@
+#ifndef ElectronEnergyCalibratorRun2_h
+#define ElectronEnergyCalibratorRun2_h
+
+#include <TRandom.h>
+#include "EgammaAnalysis/ElectronTools/interface/EnergyScaleCorrection_class.h"
+#include "EgammaAnalysis/ElectronTools/interface/SimpleElectron.h"
+#include "EgammaAnalysis/ElectronTools/interface/EpCombinationTool.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+
+#include <vector>
+
+class ElectronEnergyCalibratorRun2 {
+ public:
+  // dummy constructor for persistence
+  ElectronEnergyCalibratorRun2() {}
+  
+  // further configuration will be added when we will learn how it will work
+  ElectronEnergyCalibratorRun2(EpCombinationTool &combinator, bool isMC, bool synchronization, std::string); 
+  ~ElectronEnergyCalibratorRun2() ;
+  
+  /// Initialize with a random number generator (if not done, it will use the CMSSW service)
+  /// Caller code owns the TRandom.
+  void initPrivateRng(TRandom *rnd) ;
+  
+  /// Correct this electron. 
+  void calibrate(SimpleElectron &electron) const ;
+  
+ protected:    
+  // whatever data will be needed
+  EpCombinationTool *epCombinationTool_;
+  bool isMC_, synchronization_;
+  TRandom *rng_;
+
+  /// Return a number distributed as a unit gaussian, drawn from the private RNG if initPrivateRng was called, 
+  /// or from the CMSSW RandomNumberGenerator service
+  /// If synchronization is set to true, it returns a fixed number (1.0)
+  double gauss() const ;
+  EnergyScaleCorrection_class _correctionRetriever;
+};
+
+#endif
