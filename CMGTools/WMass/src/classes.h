c734937d82608c98dab1d274e2184c2805d7dceb
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-04-26 16:17:20 +0200

    add src

---
+++ CMGTools/WMass/src/classes.h
@@ -0,0 +1,7 @@
+#include "EgammaAnalysis/ElectronTools/interface/ElectronEnergyCalibratorRun2.h"
+
+namespace {
+  struct dictionary {
+    ElectronEnergyCalibratorRun2 fuffaElectronCalibratorRun2;
+  };
+}
