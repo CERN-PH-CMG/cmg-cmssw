e9d1811a0e1496f0e5f42a2a8d8f99818c2bb52a
Jonas Rembser <jonas.rembser@cern.ch>
2018-03-05 18:43:43 +0100

    FWLite electron id
    Electron MVA V2 (as in #23700 #23746 #23767)
    
    All event based variables are now handeled by the new helper class
    
    Hardcoding of variables and clips removed for Fall17
    
    Generalized Fall17Iso and Fall17NoIso
    
    Configurable categories and start to move Spring16 MVA
    
    Dedicated variable files for Spring16 and Fall17
    
    Removed Spring16 C++ code
    
    All year specific C++ code removed
    
    Small improvements
    
    Cleared some inconsistencies in variable definitions. All IDs now validated.
    
    Restructured VID python code a bit
    
    New MVAVariableManager helper class
    
    Forgot to actually add the class go git
    
    The raw BDT score is now stored as well
    
    Spring15 PhotonMVA uses now MVAVariableManager
    
    Almost there for photon MVA
    
    All Run2 Photon MVA IDs use now the new PhotonMVAEstimator class
    
    Add variable files
    
    Rewrote some python, ElectronID cuts on raw MWA values and autodetection of gzipped weight files
    
    Implemented V2 MV Ele ID
    
    Cut values are now found with parser as well
    
    Include ntupizer for training
    
    cleaned up a bit
    
    Training Ntuplizer finished
    
    Use auto generatetd config files
    
    Finalized ntuplizer
    
    Auto updated Training ntuplizer
    
    Updated training ntuplizer
    
    Fix in training ntuplizer
    
    fixed wp90 iso V1 parameters
    
    Fix
    
    Added MVA output Validation ntuplizer for photons
    
    Bugfix
    
    Improved exception handling in GBRForestToors
    
    Changed egammaObjectModificationsInMiniAOD_cff.py to comply with new config files
    
    Add two lines in miniAOD_tools.py to cope with ElectronMVAVariableHelper
    
    Fixed Fall17V1 variables
    
    Addressed comments following visual code inspection
    
    Simplified code
    
    Included Fall17 MVAs in MiniAOD and NanoAOD
    
    Added electronMVAVariableHelper to relevant processes
    
    Revisited casting of candidates to electrons/photons
    
    Fixup - prefer dynamic_cast if possible
    
    Corrected the second C style cast in the photons
    
    Avoid undefined category for photons
    
    Changed category not defined error to warning
    
    Changed mvaValue if category not found to -999
    
    Fixed possible memory leak
    
    Drop MVAObjectCache in MVAValueMapProducer
    
    Commit after some code review
    
    Comment out something so branch compiles
    
    Added weightfiles
    
    Some code cleaning (as in ID part of #23743)
    
    Adapt GBRForestTools exception handling to 94X
    
    Fall17 cutbased Electron ID V2 (as in #23477)
    
    Added Photon MVA weight files
    
    Technical Egamma MVA improvements (as in #24131)
    
    Fall17_94X_v2 MVA Photon ID
    
    Fixing mvaClassName in egammaObjectModificationsInMiniAOD_cff.py
    
    Inclusion of V2 IDs in MiniAOD
    
    fix worst charged iso variable
    
    adding photon MVA V2 to the pat::Photon
    
    Fixed copy-paste typo in Spring15 weight files
    
    New MultiTokens (as in #24312 #24423)
    
    cache AOD/miniAOD values to speed up repeated computations
    
    Egamma IDs back to global cache (as in #25101)
    
    Cutbased photonID Fall17 V2
    
    update phoID v2 cutbased
    
    Renamed cut based ID and put in MiniAOD
    
    Modified PhotonNtuplizer to have genMatch information
    
    Added variables from Fall17 ID to PhotonMVANtuplizer
    
    New MVAVariableHelper for extra MVA variables
    
    Moved Photon MVA to new interface
    
    adapted constructors to run MVA estimator in python fwlite
    
    ElectronMVA can be used in FWlite
    
    Egamma Python wrapper class for Electron MVAs
    
    Got rid of ElectronMVAVariableHelper
    
    Adapted PhotonMVANtuplizer
    
    Combined Electron Ntuplizer config files
    
    Fixing problems detected while testing
    
    exposing category in FWLite to be able to apply working point later on
    
    infrastructure to take working points in python+fwlite. implemented for noiso v2 only so far
    
    working points for the other mvas. somthing fishy with GP v1
    
    Spring16 WPs with logistic transform

--- RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_PHYS14_PU20bx25_nonTrig_V1_cff.py
+++ RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_PHYS14_PU20bx25_nonTrig_V1_cff.py
@@ -1,5 +1,5 @@
 from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
-
+from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_tools import *
 import FWCore.ParameterSet.Config as cms
 
 #
@@ -13,12 +13,10 @@ import FWCore.ParameterSet.Config as cms
 #     https://indico.cern.ch/event/367861/contribution/1/material/slides/0.pdf
 #
 
-# This MVA implementation class name
-mvaPhys14NonTrigClassName = "ElectronMVAEstimatorRun2Phys14NonTrig"
 # The tag is an extra string attached to the names of the products
 # such as ValueMaps that needs to distinguish cases when the same MVA estimator
 # class is used with different tuning/weights
-mvaTag = "25nsV1"
+mvaTag = "Phys14NonTrig25nsV1"
 
 # There are 6 categories in this MVA. They have to be configured in this strict order
 # (cuts and weight files order):
@@ -38,45 +36,30 @@ mvaPhys14NonTrigWeightFiles_V1 = cms.vstring(
     "RecoEgamma/ElectronIdentification/data/PHYS14/EIDmva_EE_10_oldscenario2phys14_BDT.weights.xml"
     )
 
-# Load some common definitions for MVA machinery
-from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_tools import *
-
-# The locatoins of value maps with the actual MVA values and categories
-# for all particles.
-# The names for the maps are "<module name>:<MVA class name>Values" 
-# and "<module name>:<MVA class name>Categories"
-mvaProducerModuleLabel = "electronMVAValueMapProducer"
-mvaValueMapName        = mvaProducerModuleLabel + ":" + mvaPhys14NonTrigClassName + mvaTag + "Values"
-mvaCategoriesMapName   = mvaProducerModuleLabel + ":" + mvaPhys14NonTrigClassName + mvaTag + "Categories"
-
 # The working point for this MVA that is expected to have about 80% signal
 # efficiency on in each category
 idName = "mvaEleID-PHYS14-PU20bx25-nonTrig-V1-wp80"
-MVA_WP80 = EleMVA_6Categories_WP(
-    idName,
-    mvaValueMapName,           # map with MVA values for all particles
-    mvaCategoriesMapName, # map with category index for all particles
-    cutCategory0 = -0.253, # EB1 low pt
-    cutCategory1 =  0.081, # EB2 low pt
-    cutCategory2 = -0.081, # EE low pt
-    cutCategory3 =  0.965, # EB1 
-    cutCategory4 =  0.917, # EB2
-    cutCategory5 =  0.683  # EE
+MVA_WP80 = EleMVA_WP(
+    idName, mvaTag,
+    cutCategory0 = "-0.253", # EB1 low pt
+    cutCategory1 = " 0.081", # EB2 low pt
+    cutCategory2 = "-0.081", # EE low pt
+    cutCategory3 = " 0.965", # EB1 
+    cutCategory4 = " 0.917", # EB2
+    cutCategory5 = " 0.683"  # EE
     )
 
 # The working point for this MVA that is expected to have about 90% signal
 # efficiency in each category
 idName = "mvaEleID-PHYS14-PU20bx25-nonTrig-V1-wp90"
-MVA_WP90 = EleMVA_6Categories_WP(
-    idName = idName,
-    mvaValueMapName = mvaValueMapName,           # map with MVA values for all particles
-    mvaCategoriesMapName = mvaCategoriesMapName, # map with category index for all particles
-    cutCategory0 = -0.483, # EB1 low pt
-    cutCategory1 = -0.267, # EB2 low pt
-    cutCategory2 = -0.323, # EE low pt 
-    cutCategory3 =  0.933, # EB1       
-    cutCategory4 =  0.825, # EB2       
-    cutCategory5 =  0.337  # EE        
+MVA_WP90 = EleMVA_WP(
+    idName, mvaTag,
+    cutCategory0 = "-0.483", # EB1 low pt
+    cutCategory1 = "-0.267", # EB2 low pt
+    cutCategory2 = "-0.323", # EE low pt 
+    cutCategory3 = " 0.933", # EB1       
+    cutCategory4 = " 0.825", # EB2       
+    cutCategory5 = " 0.337"  # EE        
     )
 
 #
@@ -85,17 +68,22 @@ MVA_WP90 = EleMVA_6Categories_WP(
 
 # Create the PSet that will be fed to the MVA value map producer
 mvaEleID_PHYS14_PU20bx25_nonTrig_V1_producer_config = cms.PSet( 
-    mvaName            = cms.string(mvaPhys14NonTrigClassName),
+    mvaName            = cms.string(mvaClassName),
     mvaTag             = cms.string(mvaTag),
-    weightFileNames    = mvaPhys14NonTrigWeightFiles_V1
+    # Category parameters
+    nCategories        = cms.int32(6),
+    categoryCuts       = EleMVA_6CategoriesCuts,
+    # Weight files and variable definitions
+    weightFileNames    = mvaPhys14NonTrigWeightFiles_V1,
+    variableDefinition = cms.string(mvaVariablesFileClassic)
     )
 # Create the VPset's for VID cuts
-mvaEleID_PHYS14_PU20bx25_nonTrig_V1_wp80 = configureVIDMVAEleID_V1( MVA_WP80 )
-mvaEleID_PHYS14_PU20bx25_nonTrig_V1_wp90 = configureVIDMVAEleID_V1( MVA_WP90 )
+mvaEleID_PHYS14_PU20bx25_nonTrig_V1_wp80 = configureVIDMVAEleID( MVA_WP80 )
+mvaEleID_PHYS14_PU20bx25_nonTrig_V1_wp90 = configureVIDMVAEleID( MVA_WP90 )
 
 # The MD5 sum numbers below reflect the exact set of cut variables
-# and values above. If anything changes, one has to 
-# 1) comment out the lines below about the registry, 
+# and values above. If anything changes, one has to
+# 1) comment out the lines below about the registry,
 # 2) run "calculateMD5 <this file name> <one of the VID config names just above>
 # 3) update the MD5 sum strings below and uncomment the lines again.
 #
---
+++ RecoEgamma/PhotonIdentification/plugins/PhotonMVAEstimator.cc
@@ -0,0 +1,132 @@
+#include "RecoEgamma/PhotonIdentification/interface/PhotonMVAEstimator.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+PhotonMVAEstimator::PhotonMVAEstimator(const edm::ParameterSet& conf)
+  : AnyMVAEstimatorRun2Base(conf)
+  , mvaVarMngr_ (conf.getParameter<std::string>("variableDefinition"))
+{
+
+  //
+  // Construct the MVA estimators
+  //
+  if (getTag() == "Run2Spring16NonTrigV1") {
+      effectiveAreas_ = std::make_unique<EffectiveAreas>((conf.getParameter<edm::FileInPath>("effAreasConfigFile")).fullPath());
+      phoIsoPtScalingCoeff_ = conf.getParameter<std::vector<double >>("phoIsoPtScalingCoeff");
+      phoIsoCutoff_ = conf.getParameter<double>("phoIsoCutoff");
+  }
+
+  const auto weightFileNames = conf.getParameter<std::vector<std::string> >("weightFileNames");
+  const auto categoryCutStrings = conf.getParameter<std::vector<std::string> >("categoryCuts");
+
+  if( (int)(categoryCutStrings.size()) != getNCategories() )
+    throw cms::Exception("MVA config failure: ")
+      << "wrong number of category cuts in PhotonMVAEstimator" << getTag() << std::endl;
+
+  for (auto const& cut : categoryCutStrings) categoryFunctions_.emplace_back(cut);
+
+  // Initialize GBRForests
+  if( static_cast<int>(weightFileNames.size()) != getNCategories() )
+    throw cms::Exception("MVA config failure: ")
+      << "wrong number of weightfiles in PhotonMVAEstimator" << getTag() << std::endl;
+
+  gbrForests_.clear();
+  // Create a TMVA reader object for each category
+  for(int i=0; i<getNCategories(); i++){
+
+    std::vector<std::string> variableNamesInCategory;
+    std::vector<int> variablesInCategory;
+
+    gbrForests_.push_back( GBRForestTools::createGBRForest( weightFileNames[i], variableNamesInCategory ) );
+
+    nVariables_.push_back(variableNamesInCategory.size());
+
+    variables_.push_back(variablesInCategory);
+
+    for (int j=0; j<nVariables_[i];++j) {
+        int index = mvaVarMngr_.getVarIndex(variableNamesInCategory[j]);
+        if(index == -1) {
+            throw cms::Exception("MVA config failure: ")
+               << "Concerning PhotonMVAEstimator" << getTag() << std::endl
+               << "Variable " << variableNamesInCategory[j]
+               << " not found in variable definition file!" << std::endl;
+        }
+        variables_[i].push_back(index);
+
+    }
+  }
+}
+
+float PhotonMVAEstimator::
+mvaValue(const reco::Candidate* candPtr, std::vector<float> const& auxVars, int &iCategory) const {
+
+  const reco::Photon* phoPtr = dynamic_cast<const reco::Photon*>(candPtr);
+  if( phoPtr == nullptr) {
+    throw cms::Exception("MVA failure: ")
+      << " given particle is expected to be reco::Photon or pat::Photon," << std::endl
+      << " but appears to be neither" << std::endl;
+  }
+
+  iCategory = findCategory( phoPtr );
+
+  std::vector<float> vars;
+
+  for (int i = 0; i < nVariables_[iCategory]; ++i) {
+      vars.push_back(mvaVarMngr_.getValue(variables_[iCategory][i], *phoPtr, auxVars));
+  }
+
+  // Special case for Spring16!
+  if (getTag() == "Run2Spring16NonTrigV1" and iCategory == 1) { // Endcap category
+      // Raw value for EB only, because of loss of transparency in EE
+      // for endcap MVA only in 2016
+      double eA = effectiveAreas_->getEffectiveArea( std::abs(phoPtr->superCluster()->eta()) );
+      double phoIsoCorr = vars[10] - eA*(double)vars[9] - phoIsoPtScalingCoeff_.at(1) * phoPtr->pt();
+      vars[10] = TMath::Max( phoIsoCorr, phoIsoCutoff_);
+  }
+
+  if(isDebug()) {
+    std::cout << " *** Inside PhotonMVAEstimator" << getTag() << std::endl;
+    std::cout << " category " << iCategory << std::endl;
+    for (int i = 0; i < nVariables_[iCategory]; ++i) {
+        std::cout << " " << mvaVarMngr_.getName(variables_[iCategory][i]) << " " << vars[i] << std::endl;
+    }
+  }
+
+  const float response = gbrForests_.at(iCategory)->GetResponse(vars.data());
+
+  if(isDebug()) {
+    std::cout << " ### MVA " << response << std::endl << std::endl;
+  }
+
+  return response;
+}
+
+int PhotonMVAEstimator::findCategory( const reco::Candidate* candPtr) const {
+
+  const reco::Photon* phoPtr = dynamic_cast<const reco::Photon*>(candPtr);
+  if( phoPtr == nullptr ) {
+    throw cms::Exception("MVA failure: ")
+      << " given particle is expected to be reco::Photon or pat::Photon," << std::endl
+      << " but appears to be neither" << std::endl;
+  }
+
+  return findCategory(*phoPtr);
+
+}
+
+int PhotonMVAEstimator::findCategory(reco::Photon const& photon) const {
+
+  for (int i = 0; i < getNCategories(); ++i) {
+      if (categoryFunctions_[i](photon)) return i;
+  }
+
+  edm::LogWarning  ("MVA warning") <<
+      "category not defined for particle with pt " << photon.pt() << " GeV, eta " <<
+          photon.superCluster()->eta() << " in PhotonMVAEstimator" << getTag();
+
+  return -1;
+
+}
+
+DEFINE_EDM_PLUGIN(AnyMVAEstimatorRun2Factory,
+          PhotonMVAEstimator,
+          "PhotonMVAEstimator");
