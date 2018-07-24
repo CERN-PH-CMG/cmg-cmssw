658a460ad6189cba4aab285583eb5423ae8fd980
Gael <gael.touquet@gmail.com>
2018-07-24 17:10:04 +0200

    Tau ID simplified and cleaning

--- PhysicsTools/Heppy/python/physicsobjects/Tau.py
+++ PhysicsTools/Heppy/python/physicsobjects/Tau.py
@@ -30,12 +30,19 @@ class Tau(Lepton):
     def mva_score(self, norm=False):
         '''returns the score of the isolation mva'''
         if norm:
-            return tau_mvaid.score_norm(self)
-        return tau_mvaid.score(self)
+            if self._mvaid_normscore is None:
+                self._mvaid_normscore = tau_mvaid.score_norm(self)
+            return self._mvaid_normscore
+        if self._mvaid_score is None:
+            self._mvaid_score = tau_mvaid.score(self)
+        return self._mvaid_score
 
     def mva_passes(self, working_point):
         '''returns True if the tau passes the given working point of the isolation mva'''
-        return self._mvaid_normscore > tau_mvaid.threshold(self.pt(), working_point)
+        if self._mvaid_normscore is None:
+            self.mva_score(norm=True)
+        passes = self._mvaid_normscore > tau_mvaid.threshold(self.pt(), working_point)
+        return 1. if passes else 0.
 
     def dxy_approx(self, vertex=None):
         '''Returns standard dxy for an arbitrary passed vertex'''
@@ -46,69 +53,65 @@ class Tau(Lepton):
         return ( - (vtx.x()-vertex.position().x()) *  p4.y()
                  + (vtx.y()-vertex.position().y()) *  p4.x() ) /  p4.pt()
 
-    def countWP(self,name, allWP=False):
+    def countWP(self,name):
         '''Returns the number of Working Points 
-        that are passed for given isolation name. 
+        that are passed for given isolation name.
+        
+        For example if a tau only passes VLoose, Loose,
+        and Medium Working Points this will return 3.
+
+        Uses self.tauID() to evaluate the WPs.
         '''
         WPs = ['VVTight', 'VTight', 'Tight', 'Medium',
-               'Loose', 'VLoose', 'VVLoose', ]
-        testname = 'by{}'+name[2:]
+               'Loose', 'VLoose', 'VVLoose']
+        testname = ''.join(['by{}',name[2:]])
         n_WP = 0
         for WP in WPs:
-            
-            # if from heppy-implemented isolation (known list of WPs)
-            if allWP:
-                if not self.tauID(testname.format(WP)):
-                    n_WP += 1 # in this case n_WP is the number of not passed WP
-                else:
-                    return len(WPs) - n_WP #stops counting when reached a passed WP
-
-            # else all WPs will be tested from MINIAOD
-            elif self.physObj.isTauIDAvailable(testname.format(WP)):
-                if self.tauID(testname.format(WP)):
-                    n_WP += 1 # in this case n_WP is the number of passed WP
-
+            if self.tauID(testname.format(WP), verbose=False):
+                n_WP += 1
         return n_WP
 
-    def tauID(self, name):
+    def tauID(self, name, verbose=True):
         '''Returns heppy redefined score if fitting one 
         of implemented names, else calls the wrapped tau 
         tauID(name) function.
+
+        If isolation name doesn't include a WP or "raw",
+        returns self.countWP(name) that gives a count of 
+        the number of passed WPs for this discriminator.
         '''
         if 'IsolationMVArun2017v2DBoldDMwLT' in name:
             #if raw score
             if name == 'byIsolationMVArun2017v2DBoldDMwLTraw2017':
-                if self._mvaid_score is None:
-                    self._mvaid_score = self.mva_score()
-                return self._mvaid_score
-
+                return self.mva_score()
             #if WPs
-            if self._mvaid_normscore is None:
-                self._mvaid_normscore = self.mva_score(norm=True)
-
             if name == 'byVVLooseIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("VVLoose") else 0.
-            elif name == 'byVLooseIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("VLoose") else 0.
-            elif name == 'byLooseIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("Loose") else 0.
-            elif name == 'byMediumIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("Medium") else 0.
-            elif name == 'byTightIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("Tight") else 0.
-            elif name == 'byVTightIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("VTight") else 0.
-            elif name == 'byVVTightIsolationMVArun2017v2DBoldDMwLT2017':
-                return 1. if self.mva_passes("VVTight") else 0.
-
+                return self.mva_passes("VVLoose")
+            if name == 'byVLooseIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("VLoose")
+            if name == 'byLooseIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("Loose")
+            if name == 'byMediumIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("Medium")
+            if name == 'byTightIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("Tight")
+            if name == 'byVTightIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("VTight")
+            if name == 'byVVTightIsolationMVArun2017v2DBoldDMwLT2017':
+                return self.mva_passes("VVTight")
             #if numbering scheme
             if ('byIsolation' in name) and ('raw' not in name):
-                return self.countWP(name, allWP=True)
-
+                return self.countWP(name)
+        #using numbering scheme
         if ('byIsolation' in name) and ('raw' not in name):
             return self.countWP(name)
-
-        return self.physObj.tauID(name)
+        # search in MINIAOD
+        if self.physObj.isTauIDAvailable(name):
+            return self.physObj.tauID(name)
+        # name not available
+        if verbose: 
+            print 'name', name, 'not available'
+        return False
 
     def dxy(self, vertex=None):
         '''More precise dxy calculation as pre-calculated in the tau object
