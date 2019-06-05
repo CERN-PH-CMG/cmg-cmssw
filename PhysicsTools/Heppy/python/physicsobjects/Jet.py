bcdbafa57a00bf3698fed9bc08b1bc189cb7787b
Giovanni <gpetruc@gmail.com>
2019-06-05 14:31:13 +0200

    physicsobjects/Jet: Full run 2 WPs for b-tag

--- PhysicsTools/Heppy/python/physicsobjects/Jet.py
+++ PhysicsTools/Heppy/python/physicsobjects/Jet.py
@@ -47,6 +47,28 @@ _btagWPs = {
     "DeepCSVL": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.1522),
     "DeepCSVM": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.4941),
     "DeepCSVT": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.8001),
+### Full run 2, year-by-year WPs:  
+ # https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation2016Legacy
+ # https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation94X
+ # https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation102X
+    "DeepCSV_2016_L": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.2217),
+    "DeepCSV_2016_M": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.6321),
+    "DeepCSV_2016_T": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.8953),
+    "DeepCSV_2017_L": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.1522),
+    "DeepCSV_2017_M": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.4941),
+    "DeepCSV_2017_T": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.8001),
+    "DeepCSV_2018_L": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.1241),
+    "DeepCSV_2018_M": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.4184),
+    "DeepCSV_2018_T": ("pfDeepCSVJetTags:probb + pfDeepCSVJetTags:probbb", 0.7527),
+    "DeepFlav_2016_L": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.0614),
+    "DeepFlav_2016_M": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.3093),
+    "DeepFlav_2016_T": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.7221),
+    "DeepFlav_2017_L": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.0521),
+    "DeepFlav_2017_M": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.3033),
+    "DeepFlav_2017_T": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.7489),
+    "DeepFlav_2018_L": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.0494),
+    "DeepFlav_2018_M": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.2770),
+    "DeepFlav_2018_T": ("pfDeepFlavourJetTags:probb + pfDeepFlavourJetTags:probbb + pfDeepFlavourJetTags:problepb", 0.7264),
 }
 
 class Jet(PhysicsObject):   
